446d93739465acfd1fc034040496ae7eaacd43e6
olegus8 <olegus8@localhost>
2010-04-03 17:07:19 +0000

    moving music code under platform framework

--- src/iphone/measurer/Classes/iphone_platform_sound.hpp
+++ src/iphone/measurer/Classes/iphone_platform_sound.hpp
@@ -47,6 +47,66 @@ shy_iphone_platform :: sound_set_sample_value
 }
 
 inline
+shy_iphone_platform :: stereo_sound_resource_id
+shy_iphone_platform :: sound_create_stereo_resource_id 
+    ( int_32 resource_index 
+    )
+{
+    stereo_sound_resource_id resource_id ;
+    resource_id . _resource_id = resource_index ;
+    return resource_id ;
+}
+
+inline
+void 
+shy_iphone_platform :: sound_load_stereo_sample_data
+    ( stereo_sound_sample * samples 
+    , int_32 max_samples_count
+    , int_32 & loaded_samples_count
+    , const stereo_sound_resource_id & resource_id 
+    )
+{
+    NSBundle * bundle = [ NSBundle mainBundle ] ;
+    CFURLRef file_url = ( CFURLRef ) [ [ NSURL fileURLWithPath : [ bundle 
+        pathForResource : @"rough_n_heavy_v37" 
+        ofType : @"mp3" 
+        ] ] retain ] ;
+        
+    SInt64 file_length_in_frames = 0 ;
+    AudioStreamBasicDescription file_format ;
+    UInt32 property_size = sizeof ( file_format ) ;
+    ExtAudioFileRef ext_ref = 0 ;
+    AudioStreamBasicDescription output_format ;
+                 
+    ExtAudioFileOpenURL ( file_url , & ext_ref ) ;
+    ExtAudioFileGetProperty ( ext_ref , kExtAudioFileProperty_FileDataFormat , & property_size , & file_format ) ;
+                    
+    output_format . mSampleRate = stereo_sound_samples_per_second ;
+    output_format . mChannelsPerFrame = 2 ;
+    output_format . mFormatID = kAudioFormatLinearPCM ;
+    output_format . mBytesPerPacket = 2 * output_format . mChannelsPerFrame ;
+    output_format . mFramesPerPacket = 1 ;
+    output_format . mBytesPerFrame = 2 * output_format . mChannelsPerFrame ;
+    output_format . mBitsPerChannel = 16 ;
+    output_format . mFormatFlags = kAudioFormatFlagsNativeEndian | kAudioFormatFlagIsPacked | kAudioFormatFlagIsSignedInteger ;
+                    
+    ExtAudioFileSetProperty ( ext_ref , kExtAudioFileProperty_ClientDataFormat , sizeof ( output_format ) , & output_format ) ;
+    property_size = sizeof ( file_length_in_frames ) ;
+    ExtAudioFileGetProperty ( ext_ref , kExtAudioFileProperty_FileLengthFrames , & property_size , & file_length_in_frames ) ;
+    
+    AudioBufferList data_buffer ;
+    data_buffer . mNumberBuffers = 1 ;
+    data_buffer . mBuffers [ 0 ] . mDataByteSize = file_length_in_frames * output_format . mBytesPerFrame ;
+    data_buffer . mBuffers [ 0 ] . mNumberChannels = output_format . mChannelsPerFrame ;
+    data_buffer . mBuffers [ 0 ] . mData = ( void * ) samples ;
+    
+    ExtAudioFileRead ( ext_ref , ( UInt32 * ) & file_length_in_frames , & data_buffer ) ;
+    loaded_samples_count = file_length_in_frames ;
+    ExtAudioFileDispose ( ext_ref ) ;
+    CFRelease ( file_url ) ;
+}
+
+inline
 shy_iphone_platform :: sound_buffer_id 
 shy_iphone_platform :: sound_create_mono_buffer 
     ( shy_iphone_platform :: mono_sound_sample * samples 
@@ -58,9 +118,28 @@ shy_iphone_platform :: sound_create_mono_buffer
     alBufferData 
         ( buffer_id . _buffer_id 
         , AL_FORMAT_MONO8 
-        , ( ALvoid * ) samples 
-        , samples_count
+        , ( ALvoid * ) samples
+        , samples_count * sizeof ( mono_sound_sample )
         , mono_sound_samples_per_second
+        ) ;
+    return buffer_id ;
+}
+
+inline
+shy_iphone_platform :: sound_buffer_id 
+shy_iphone_platform :: sound_create_stereo_buffer 
+    ( stereo_sound_sample * samples 
+    , int_32 samples_count 
+    )
+{
+    sound_buffer_id buffer_id ;
+    alGenBuffers ( 1 , & buffer_id . _buffer_id ) ;
+    alBufferData 
+        ( buffer_id . _buffer_id 
+        , AL_FORMAT_STEREO16 
+        , ( ALvoid * ) samples 
+        , samples_count * sizeof ( stereo_sound_sample )
+        , stereo_sound_samples_per_second
         ) ;
     return buffer_id ;
 }
@@ -173,73 +252,4 @@ shy_iphone_platform :: sound_get_buffer_from_music_EXPERIMENTAL
     sound_buffer_id buffer_id ;
     buffer_id . _buffer_id = _experimental_buffer_id ;
     return buffer_id ;
-}
-
-inline
-shy_iphone_platform :: stereo_sound_resource_id
-shy_iphone_platform :: sound_create_stereo_resource_id 
-    ( int_32 resource_index 
-    )
-{
-    stereo_sound_resource_id resource_id ;
-    resource_id . _resource_id = resource_index ;
-    return resource_id ;
-}
-
-inline
-void 
-shy_iphone_platform :: sound_load_stereo_sample_data
-    ( stereo_sound_sample * samples 
-    , int_32 max_samples_count
-    , int_32 & loaded_samples_count
-    , const stereo_sound_resource_id & resource_id 
-    )
-{
-    NSBundle * bundle = [ NSBundle mainBundle ] ;
-    CFURLRef file_url = ( CFURLRef ) [ [ NSURL fileURLWithPath : [ bundle 
-        pathForResource : @"rough_n_heavy_v37" 
-        ofType : @"mp3" 
-        ] ] retain ] ;
-        
-    SInt64 file_length_in_frames = 0 ;
-    AudioStreamBasicDescription file_format ;
-    UInt32 property_size = sizeof ( file_format ) ;
-    ExtAudioFileRef ext_ref = 0 ;
-    AudioStreamBasicDescription output_format ;
-                 
-    ExtAudioFileOpenURL ( file_url , & ext_ref ) ;
-    ExtAudioFileGetProperty ( ext_ref , kExtAudioFileProperty_FileDataFormat , & property_size , & file_format ) ;
-                    
-    output_format . mSampleRate = stereo_sound_samples_per_second ;
-    output_format . mChannelsPerFrame = 2 ;
-    output_format . mFormatID = kAudioFormatLinearPCM ;
-    output_format . mBytesPerPacket = 2 * output_format . mChannelsPerFrame ;
-    output_format . mFramesPerPacket = 1 ;
-    output_format . mBytesPerFrame = 2 * output_format . mChannelsPerFrame ;
-    output_format . mBitsPerChannel = 16 ;
-    output_format . mFormatFlags = kAudioFormatFlagsNativeEndian | kAudioFormatFlagIsPacked | kAudioFormatFlagIsSignedInteger ;
-                    
-    ExtAudioFileSetProperty ( ext_ref , kExtAudioFileProperty_ClientDataFormat , sizeof ( output_format ) , & output_format ) ;
-    property_size = sizeof ( file_length_in_frames ) ;
-    ExtAudioFileGetProperty ( ext_ref , kExtAudioFileProperty_FileLengthFrames , & property_size , & file_length_in_frames ) ;
-    
-    AudioBufferList data_buffer ;
-    data_buffer . mNumberBuffers = 1 ;
-    data_buffer . mBuffers [ 0 ] . mDataByteSize = file_length_in_frames * output_format . mBytesPerFrame ;
-    data_buffer . mBuffers [ 0 ] . mNumberChannels = output_format . mChannelsPerFrame ;
-    data_buffer . mBuffers [ 0 ] . mData = ( void * ) samples ;
-    
-    ExtAudioFileRead ( ext_ref , ( UInt32 * ) & file_length_in_frames , & data_buffer ) ;
-    loaded_samples_count = file_length_in_frames ;
-    ExtAudioFileDispose ( ext_ref ) ;
-    CFRelease ( file_url ) ;
-}
-
-inline
-shy_iphone_platform :: sound_buffer_id 
-shy_iphone_platform :: sound_create_stereo_buffer 
-    ( stereo_sound_sample * samples 
-    , int_32 samples_count 
-    )
-{
 }
