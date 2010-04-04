a12fcc72695ba42451dc7077ba874353824aacab
olegus8 <olegus8@localhost>
2010-04-04 15:01:59 +0000

    working on touches

--- src/iphone/measurer/Classes/EAGLView.h
+++ src/iphone/measurer/Classes/EAGLView.h
@@ -1,11 +1,3 @@
-//
-//  EAGLView.h
-//  measurer
-//
-//  Created by Oleg Plakhotnyuk on 23.12.09.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
-//
-
 #import <OpenGLES/ES1/gl.h>
 #import <OpenGLES/ES1/glext.h>
 #import <OpenAL/al.h>
@@ -14,33 +6,25 @@
 #import <UIKit/UIKit.h>
 
 #include "iphone_platform.hpp"
-#include "iphone_sound_loader.h"
 #include "measurer_facade.hpp"
 
-// This class wraps the CAEAGLLayer from CoreAnimation into a convenient UIView subclass.
-// The view content is basically an EAGL surface you render your OpenGL scene into.
-// Note that setting the view non-opaque will only work if the EAGL surface has an alpha channel.
 @interface EAGLView : UIView
 {    
 @private	
-	shy_measurer_facade < shy_iphone_platform > shyMeasurer ;
+	shy_measurer_facade < shy_iphone_platform > _shy_measurer ;
 	
-	EAGLContext *context;
-	
-	// The pixel dimensions of the CAEAGLLayer
-	GLint backingWidth;
-	GLint backingHeight;
-	
-	// The OpenGL names for the framebuffer and renderbuffer used to render to this view
-	GLuint defaultFramebuffer, colorRenderbuffer, depthRenderbuffer;
+	EAGLContext * _gl_context ;
+	GLint _gl_backing_width;
+	GLint _gl_backing_height;
+	GLuint _gl_default_framebuffer;
+    GLuint _gl_color_renderbuffer;
+    GLuint _gl_depth_renderbuffer;
     
-    // OpenAL 
-	ALCcontext * mContext;
-	ALCdevice * mDevice;
-	BOOL animating;
+	ALCcontext * _al_context;
+	ALCdevice * _al_device;
+    
+	BOOL _animating;
 }
-
-@property (readonly, nonatomic, getter=isAnimating) BOOL animating;
 
 - (void) startAnimation;
 - (void) stopAnimation;
--- src/iphone/measurer/Classes/EAGLView.mm
+++ src/iphone/measurer/Classes/EAGLView.mm
@@ -10,8 +10,6 @@
 
 @implementation EAGLView
 
-@synthesize animating;
-
 // You must implement this method
 + (Class) layerClass
 {
@@ -38,33 +36,33 @@
                                         [NSNumber numberWithBool:FALSE], kEAGLDrawablePropertyRetainedBacking, kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat, nil];
 		
         [UIApplication sharedApplication].idleTimerDisabled = YES;
-		context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES1];
+		_gl_context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES1];
         
-        if (!context || ![EAGLContext setCurrentContext:context])
+        if (!_gl_context || ![EAGLContext setCurrentContext:_gl_context])
 		{
             [self release];
             return nil;
         }
         
-        mDevice = alcOpenDevice ( NULL ) ;
-        if ( ! mDevice )
+        _al_device = alcOpenDevice ( NULL ) ;
+        if ( ! _al_device )
         {
             NSLog ( @"Failed to open OpenAl device" ) ;
             [ self release ] ;
             return nil ;
         }
         
-        mContext = alcCreateContext ( mDevice , NULL ) ;
-        alcMakeContextCurrent ( mContext ) ;
+        _al_context = alcCreateContext ( _al_device , NULL ) ;
+        alcMakeContextCurrent ( _al_context ) ;
                 
 		// Create default framebuffer object. The backing will be allocated for the current layer in -resizeFromLayer
-		glGenFramebuffersOES(1, &defaultFramebuffer);
-		glBindFramebufferOES(GL_FRAMEBUFFER_OES, defaultFramebuffer);
+		glGenFramebuffersOES(1, &_gl_default_framebuffer);
+		glBindFramebufferOES(GL_FRAMEBUFFER_OES, _gl_default_framebuffer);
         
         shy_iphone_platform :: _sound_loader = [ [ sound_loader alloc ] init ] ;
-   		shyMeasurer . init ( ) ;
+   		_shy_measurer . init ( ) ;
         
-		animating = FALSE;
+		_animating = FALSE;
 	}
 	
     return self;
@@ -72,13 +70,13 @@
 
 - (void) drawView:(id)sender
 {
-    [EAGLContext setCurrentContext:context];
-    glBindFramebufferOES(GL_FRAMEBUFFER_OES, defaultFramebuffer);
-	shyMeasurer . render ( ) ;
-	shyMeasurer . update ( ) ;
-    glBindRenderbufferOES(GL_RENDERBUFFER_OES, colorRenderbuffer);
-    [context presentRenderbuffer:GL_RENDERBUFFER_OES];
-    shyMeasurer . render_finished ( ) ;
+    [EAGLContext setCurrentContext:_gl_context];
+    glBindFramebufferOES(GL_FRAMEBUFFER_OES, _gl_default_framebuffer);
+	_shy_measurer . render ( ) ;
+	_shy_measurer . update ( ) ;
+    glBindRenderbufferOES(GL_RENDERBUFFER_OES, _gl_color_renderbuffer);
+    [_gl_context presentRenderbuffer:GL_RENDERBUFFER_OES];
+    _shy_measurer . render_finished ( ) ;
 	[ NSTimer 
 	    scheduledTimerWithTimeInterval : 0.0
 		target : self
@@ -91,76 +89,81 @@
 - (void) layoutSubviews
 {
 	// Allocate color buffer backing based on the current layer size
-    glGenRenderbuffersOES(1, &colorRenderbuffer);
-    glBindRenderbufferOES(GL_RENDERBUFFER_OES, colorRenderbuffer);
-    [context renderbufferStorage:GL_RENDERBUFFER_OES fromDrawable:(CAEAGLLayer*)self.layer];
-	glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_COLOR_ATTACHMENT0_OES, GL_RENDERBUFFER_OES, colorRenderbuffer);
+    glGenRenderbuffersOES(1, &_gl_color_renderbuffer);
+    glBindRenderbufferOES(GL_RENDERBUFFER_OES, _gl_color_renderbuffer);
+    [_gl_context renderbufferStorage:GL_RENDERBUFFER_OES fromDrawable:(CAEAGLLayer*)self.layer];
+	glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_COLOR_ATTACHMENT0_OES, GL_RENDERBUFFER_OES, _gl_color_renderbuffer);
     
-	glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_WIDTH_OES, &backingWidth);
-    glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_HEIGHT_OES, &backingHeight);
+	glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_WIDTH_OES, &_gl_backing_width);
+    glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_HEIGHT_OES, &_gl_backing_height);
 	
     // Allocate and attach the depth buffer
-    glGenRenderbuffersOES(1, &depthRenderbuffer);
-    glBindRenderbufferOES(GL_RENDERBUFFER_OES, depthRenderbuffer);
-    glRenderbufferStorageOES(GL_RENDERBUFFER_OES, GL_DEPTH_COMPONENT16_OES, backingWidth, backingHeight);
-    glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_DEPTH_ATTACHMENT_OES, GL_RENDERBUFFER_OES, depthRenderbuffer);
+    glGenRenderbuffersOES(1, &_gl_depth_renderbuffer);
+    glBindRenderbufferOES(GL_RENDERBUFFER_OES, _gl_depth_renderbuffer);
+    glRenderbufferStorageOES(GL_RENDERBUFFER_OES, GL_DEPTH_COMPONENT16_OES, _gl_backing_width, _gl_backing_height);
+    glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_DEPTH_ATTACHMENT_OES, GL_RENDERBUFFER_OES, _gl_depth_renderbuffer);
     
     if (glCheckFramebufferStatusOES(GL_FRAMEBUFFER_OES) != GL_FRAMEBUFFER_COMPLETE_OES)
 	{
 		NSLog(@"Failed to make complete framebuffer object %x", glCheckFramebufferStatusOES(GL_FRAMEBUFFER_OES));
     }
     
-    glViewport(0, 0, backingWidth, backingHeight);
+    glViewport(0, 0, _gl_backing_width, _gl_backing_height);
     [self drawView:nil];
 }
 
 - (void) startAnimation
 {
-	if (!animating)
+	if (!_animating)
 	{
 		[NSTimer scheduledTimerWithTimeInterval:0.0 target:self selector:@selector(drawView:) userInfo:nil repeats:NO];
-		animating = TRUE;
-        NSLog(@"start animation");
+		_animating = TRUE;
 	}
 }
 
 - (void)stopAnimation
 {
-	if (animating)
-		animating = FALSE;
+	if (_animating)
+		_animating = FALSE;
 }
 
 - (void) dealloc
 {
-	shyMeasurer . done ( ) ;
+	_shy_measurer . done ( ) ;
     [ shy_iphone_platform :: _sound_loader release ] ;
     shy_iphone_platform :: _sound_loader = nil ;
 	
 	// Tear down GL
-	if (defaultFramebuffer)
+	if (_gl_default_framebuffer)
 	{
-		glDeleteFramebuffersOES(1, &defaultFramebuffer);
-		defaultFramebuffer = 0;
+		glDeleteFramebuffersOES(1, &_gl_default_framebuffer);
+		_gl_default_framebuffer = 0;
 	}
 
-	if (colorRenderbuffer)
+	if (_gl_color_renderbuffer)
 	{
-		glDeleteRenderbuffersOES(1, &colorRenderbuffer);
-		colorRenderbuffer = 0;
+		glDeleteRenderbuffersOES(1, &_gl_color_renderbuffer);
+		_gl_color_renderbuffer = 0;
 	}
+    
+    if (_gl_depth_renderbuffer)
+    {
+		glDeleteRenderbuffersOES(1, &_gl_depth_renderbuffer);
+        _gl_depth_renderbuffer = 0;
+    }
 	
 	// Tear down context
-	if ([EAGLContext currentContext] == context)
+	if ([EAGLContext currentContext] == _gl_context)
         [EAGLContext setCurrentContext:nil];
 	
- 	[context release];
-	context = nil;
+ 	[_gl_context release];
+	_gl_context = nil;
 
     // Turn off Open AL
-	alcDestroyContext ( mContext ) ;
-	alcCloseDevice ( mDevice ) ;
-    mContext = nil ;
-    mDevice = nil ;
+	alcDestroyContext ( _al_context ) ;
+	alcCloseDevice ( _al_device ) ;
+    _al_context = nil ;
+    _al_device = nil ;
 
     [super dealloc];
 }
