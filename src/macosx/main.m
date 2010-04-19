37f6dbfbd720e8aeecb3c49e5fd0bc66d87cd1ed
olegus8 <olegus8@localhost>
2010-04-19 15:29:16 +0000

    working on macosx port

--- src/macosx/main.m
+++ src/macosx/main.m
@@ -1,14 +1,6 @@
-//
-//  main.m
-//  NSOpenGLFullScreen
-//
-//  Created by Troy Stephens on Thu May 08 2003.
-//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, const char *argv[])
+int main ( int argc , const char * argv [ ] )
 {
-    return NSApplicationMain(argc, argv);
+    return NSApplicationMain ( argc , argv ) ;
 }
