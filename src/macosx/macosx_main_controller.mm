e66e0a87ea309465dc5123c36b7e557b5fc3688a
olegus8 <olegus8@localhost>
2010-04-19 14:37:04 +0000

    working on macosx port

--- src/macosx/macosx_main_controller.mm
+++ src/macosx/macosx_main_controller.mm
@@ -1,42 +1,3 @@
-/*
- MainController.m
- A controller object that demonstrates full-screen rendering and interaction using the
- NSOpenGL classes.
-
- Copyright (c) 2003, Apple Computer, Inc., all rights reserved.
-
- IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in
- consideration of your agreement to the following terms, and your use, installation, 
- modification or redistribution of this Apple software constitutes acceptance of these 
- terms.  If you do not agree with these terms, please do not use, install, modify or 
- redistribute this Apple software.
- 
- In consideration of your agreement to abide by the following terms, and subject to these 
- terms, Apple grants you a personal, non-exclusive license, under Apple’s copyrights in 
- this original Apple software (the "Apple Software"), to use, reproduce, modify and 
- redistribute the Apple Software, with or without modifications, in source and/or binary 
- forms; provided that if you redistribute the Apple Software in its entirety and without 
- modifications, you must retain this notice and the following text and disclaimers in all 
- such redistributions of the Apple Software.  Neither the name, trademarks, service marks 
- or logos of Apple Computer, Inc. may be used to endorse or promote products derived from 
- the Apple Software without specific prior written permission from Apple. Except as expressly
- stated in this notice, no other rights or licenses, express or implied, are granted by Apple
- herein, including but not limited to any patent rights that may be infringed by your 
- derivative works or by other works in which the Apple Software may be incorporated.
- 
- The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, 
- EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, 
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS 
- USE AND OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
- 
- IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
- OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
- REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND 
- WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR 
- OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #import "macosx_main_controller.h"
 #import "macosx_opengl_view.h"
 #import "macosx_scene.h"
@@ -44,7 +5,7 @@
 #import <OpenGL/OpenGL.h>
 
 @interface MainController (AnimationMethods)
-- (BOOL) isAnimating;
+- (BOOL) is_animating;
 - (void) startAnimation;
 - (void) stopAnimation;
 - (void) toggleAnimation;
@@ -58,7 +19,7 @@
 
 - (void) awakeFromNib
 {
-    isAnimating = NO;
+    is_animating = NO;
     [self startAnimation];
 }
 
@@ -108,7 +69,7 @@
     }
 
     // Pause animation in the OpenGL view.  While we're in full-screen mode, we'll drive the animation actively instead of using a timer callback.
-    if ([self isAnimating]) {
+    if ([self is_animating]) {
         [self stopAnimationTimer];
     }
 
@@ -199,7 +160,7 @@
     [openGLView setNeedsDisplay:YES];
 
     // Resume animation timer firings.
-    if ([self isAnimating]) {
+    if ([self is_animating]) {
         [self startAnimationTimer];
     }
 }
@@ -228,7 +189,7 @@
 - (void)mouseDown:(NSEvent *)theEvent
 {
     shy_macosx_scene *scene = [openGLView scene];
-    BOOL wasAnimating = [self isAnimating];
+    BOOL wasAnimating = [self is_animating];
     BOOL dragging = YES;
     NSPoint windowPoint;
     NSPoint lastWindowPoint = [theEvent locationInWindow];
@@ -278,15 +239,15 @@
 
 @implementation MainController (AnimationMethods)
 
-- (BOOL) isAnimating
+- (BOOL) is_animating
 {
-    return isAnimating;
+    return is_animating;
 }
 
 - (void) startAnimation
 {
-    if (!isAnimating) {
-        isAnimating = YES;
+    if (!is_animating) {
+        is_animating = YES;
         if (![self isInFullScreenMode]) {
             [self startAnimationTimer];
         }
@@ -295,17 +256,17 @@
 
 - (void) stopAnimation
 {
-    if (isAnimating) {
+    if (is_animating) {
         if (animationTimer != nil) {
             [self stopAnimationTimer];
         }
-        isAnimating = NO;
+        is_animating = NO;
     }
 }
 
 - (void) toggleAnimation
 {
-    if ([self isAnimating]) {
+    if ([self is_animating]) {
         [self stopAnimation];
     } else {
         [self startAnimation];
