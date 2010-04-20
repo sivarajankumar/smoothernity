7fe5069c94b11ff265c98225ad830643849ce079
olegus8 <olegus8@localhost>
2010-04-20 19:44:25 +0000

    working on iphone port

---
+++ src/iphone/Classes/iphone_platform_matrix.hpp
@@ -0,0 +1,166 @@
+inline
+void
+shy_iphone_platform :: matrix_set_axis_x 
+    ( shy_iphone_platform :: matrix_data & matrix 
+    , shy_iphone_platform :: float_32 x 
+    , shy_iphone_platform :: float_32 y 
+    , shy_iphone_platform :: float_32 z 
+    )
+{
+    matrix . _elements [ 0 ] = x ;
+    matrix . _elements [ 1 ] = y ;
+    matrix . _elements [ 2 ] = z ;
+    matrix . _elements [ 3 ] = 0 ;
+}
+
+inline
+void 
+shy_iphone_platform :: matrix_set_axis_y 
+    ( shy_iphone_platform :: matrix_data & matrix 
+    , shy_iphone_platform :: float_32 x 
+    , shy_iphone_platform :: float_32 y 
+    , shy_iphone_platform :: float_32 z 
+    )
+{
+    matrix . _elements [ 4 ] = x ;
+    matrix . _elements [ 5 ] = y ;
+    matrix . _elements [ 6 ] = z ;
+    matrix . _elements [ 7 ] = 0 ;
+}
+
+inline
+void 
+shy_iphone_platform :: matrix_set_axis_z 
+    ( shy_iphone_platform :: matrix_data & matrix 
+    , shy_iphone_platform :: float_32 x 
+    , shy_iphone_platform :: float_32 y 
+    , shy_iphone_platform :: float_32 z 
+    )
+{
+    matrix . _elements [  8 ] = x ;
+    matrix . _elements [  9 ] = y ;
+    matrix . _elements [ 10 ] = z ;
+    matrix . _elements [ 11 ] = 0 ;
+}
+
+inline
+void 
+shy_iphone_platform :: matrix_set_origin 
+    ( shy_iphone_platform :: matrix_data & matrix 
+    , shy_iphone_platform :: float_32 x 
+    , shy_iphone_platform :: float_32 y 
+    , shy_iphone_platform :: float_32 z 
+    )
+{
+    matrix . _elements [ 12 ] = x ;
+    matrix . _elements [ 13 ] = y ;
+    matrix . _elements [ 14 ] = z ;
+    matrix . _elements [ 15 ] = 1 ;
+}
+
+inline
+void 
+shy_iphone_platform :: matrix_set_axis_x 
+    ( shy_iphone_platform :: matrix_data & matrix 
+    , shy_iphone_platform :: vector_data v 
+    )
+{
+    matrix_set_axis_x ( matrix , v . _x , v . _y , v . _z ) ;
+}
+
+inline
+void 
+shy_iphone_platform :: matrix_set_axis_y 
+    ( shy_iphone_platform :: matrix_data & matrix 
+    , shy_iphone_platform :: vector_data v 
+    )
+{
+    matrix_set_axis_y ( matrix , v . _x , v . _y , v . _z ) ;
+}
+
+inline
+void 
+shy_iphone_platform :: matrix_set_axis_z 
+    ( shy_iphone_platform :: matrix_data & matrix 
+    , shy_iphone_platform :: vector_data v 
+    )
+{
+    matrix_set_axis_z ( matrix , v . _x , v . _y , v . _z ) ;
+}
+
+inline
+void 
+shy_iphone_platform :: matrix_set_origin 
+    ( shy_iphone_platform :: matrix_data & matrix 
+    , shy_iphone_platform :: vector_data v 
+    )
+{
+    matrix_set_origin ( matrix , v . _x , v . _y , v . _z ) ;
+}
+
+inline 
+shy_iphone_platform :: vector_data 
+shy_iphone_platform :: matrix_get_axis_x 
+    ( const shy_iphone_platform :: matrix_data & matrix 
+    )
+{
+    return vector_xyz ( matrix . _elements [ 0 ] , matrix . _elements [ 1 ] , matrix . _elements [ 2 ] ) ;
+}
+
+inline
+shy_iphone_platform :: vector_data 
+shy_iphone_platform :: matrix_get_axis_y 
+    ( const shy_iphone_platform :: matrix_data & matrix 
+    )
+{
+    return vector_xyz ( matrix . _elements [ 4 ] , matrix . _elements [ 5 ] , matrix . _elements [ 6 ] ) ;
+}
+
+inline
+shy_iphone_platform :: vector_data 
+shy_iphone_platform :: matrix_get_axis_z 
+    ( const shy_iphone_platform :: matrix_data & matrix 
+    )
+{
+    return vector_xyz ( matrix . _elements [ 8 ] , matrix . _elements [ 9 ] , matrix . _elements [ 10 ] ) ;
+}
+
+inline
+shy_iphone_platform :: vector_data 
+shy_iphone_platform :: matrix_get_origin 
+    ( const shy_iphone_platform :: matrix_data & matrix 
+    )
+{
+    return vector_xyz ( matrix . _elements [ 12 ] , matrix . _elements [ 13 ] , matrix . _elements [ 14 ] ) ;
+}
+
+inline
+void 
+shy_iphone_platform :: matrix_identity 
+    ( shy_iphone_platform :: matrix_data & matrix 
+    )
+{
+    for ( int i = 0 ; i < 16 ; i ++ )
+    {
+        if ( i == 0 || i == 5 || i == 10 || i == 15 )
+            matrix . _elements [ i ] = 1 ;
+        else
+            matrix . _elements [ i ] = 0 ;
+    }
+}
+
+inline
+void 
+shy_iphone_platform :: matrix_inverse_rotation_translation 
+    ( shy_iphone_platform :: matrix_data & matrix 
+    )
+{
+    matrix_set_origin ( matrix , vector_xyz
+        ( - vector_dot_product ( matrix_get_origin ( matrix ) , matrix_get_axis_x ( matrix ) )
+        , - vector_dot_product ( matrix_get_origin ( matrix ) , matrix_get_axis_y ( matrix ) )
+        , - vector_dot_product ( matrix_get_origin ( matrix ) , matrix_get_axis_z ( matrix ) )
+        ) ) ;
+    swap_values ( matrix . _elements [ 1 ] , matrix . _elements [ 4 ] ) ;
+    swap_values ( matrix . _elements [ 2 ] , matrix . _elements [ 8 ] ) ;
+    swap_values ( matrix . _elements [ 6 ] , matrix . _elements [ 9 ] ) ;
+}
---
+++ src/iphone/Classes/iphone_view.h
@@ -0,0 +1,38 @@
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+#import <OpenAL/al.h>
+#import <OpenAL/alc.h>
+#import <QuartzCore/QuartzCore.h>
+#import <UIKit/UIKit.h>
+
+#include "iphone_platform.hpp"
+#include "../../common/facade.hpp"
+
+@interface EAGLView : UIView
+{    
+@private	
+	shy_facade < shy_iphone_platform > _facade ;
+	
+	EAGLContext * _gl_context ;
+	GLint _gl_backing_width;
+	GLint _gl_backing_height;
+	GLuint _gl_default_framebuffer;
+    GLuint _gl_color_renderbuffer;
+    GLuint _gl_depth_renderbuffer;
+    
+	ALCcontext * _al_context;
+	ALCdevice * _al_device;
+    
+	BOOL _animating;
+}
+
+- ( void ) start_animation ;
+- ( void ) stop_animation ;
+- ( void ) draw_view : ( id ) sender ;
+- ( void ) _init_platform ;
+- ( void ) _done_platform ;
+- ( void ) _init_game ;
+- ( void ) _done_game ;
+- ( void ) _schedule_draw ;
+
+@end
