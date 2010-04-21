c1a50e551b39ea33333ed039372738185f9d9bc4
olegus8 <olegus8@localhost>
2010-04-21 08:24:21 +0000

    working on windows port

--- src/win/win_platform_render.hpp
+++ src/win/win_platform_render.hpp
@@ -41,10 +41,14 @@ inline void shy_win_platform :: render_projection_frustum
     , shy_win_platform :: float_32 right 
     , shy_win_platform :: float_32 bottom 
     , shy_win_platform :: float_32 top 
-    , shy_win_platform :: float_32 near 
-    , shy_win_platform :: float_32 far 
+    , shy_win_platform :: float_32 znear 
+    , shy_win_platform :: float_32 zfar 
     )
 {
+    HRESULT hr ;
+	D3DXMATRIX matrix ;
+	D3DXMatrixPerspectiveOffCenterLH ( & matrix , left , right , bottom , top , znear , zfar ) ;
+	V ( DXUTGetD3D9Device ( ) -> SetTransform ( D3DTS_PROJECTION , & matrix ) ) ;
 }
 
 inline void shy_win_platform :: render_projection_ortho 
