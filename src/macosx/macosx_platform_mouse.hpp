702d362c2326e88991fb1423f60773f12691eee7
olegus8 <olegus8@localhost>
2010-04-26 11:07:15 +0000

    working on mac port

---
+++ src/macosx/macosx_platform_mouse.hpp
@@ -0,0 +1,20 @@
+inline
+shy_macosx_platform :: int_32 
+shy_macosx_platform :: mouse_left_button_down ( )
+{
+	return _mouse_left_button_down != false ;
+}
+
+inline
+shy_macosx_platform :: float_32 
+shy_macosx_platform :: mouse_x ( )
+{
+	return _mouse_x ;
+}
+
+inline
+shy_macosx_platform :: float_32 
+shy_macosx_platform :: mouse_y ( )
+{
+	return _mouse_y ;
+}
