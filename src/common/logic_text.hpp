7ab31b706ad8d98fec82eea018d35f429cb7bec6
olegus8 <olegus8@localhost>
2010-04-28 17:46:48 +0000

    working on rasterizer

--- src/common/logic_text.hpp
+++ src/common/logic_text.hpp
@@ -84,8 +84,8 @@ private :
             }
         }
         _generate_font_linear_english_A ( _text_texture_data , TEXT_TEXTURE_SIZE , TEXT_TEXTURE_SIZE , TEXT_TEXTURE_SIZE ) ;
-//        _generate_font_linear_english_A ( _text_texture_data , TEXT_TEXTURE_SIZE , 16 , 16 ) ;
-//        _generate_font_linear_english_A ( _text_texture_data + 32 , TEXT_TEXTURE_SIZE , 32 , 32 ) ;
+        _generate_font_linear_english_A ( _text_texture_data , TEXT_TEXTURE_SIZE , 16 , 16 ) ;
+        _generate_font_linear_english_A ( _text_texture_data + 32 , TEXT_TEXTURE_SIZE , 32 , 32 ) ;
         platform :: render_create_texture_id ( _text_texture_id ) ;
         platform :: render_load_texture_data ( _text_texture_id , TEXT_TEXTURE_SIZE_POW2_BASE , _text_texture_data ) ;
     }
@@ -99,8 +99,8 @@ private :
         texel_data filler ;
         platform :: render_set_texel_color ( filler , 255 , 255 , 255 , 128 ) ;
         _mediator -> rasterize_triangle ( starting_texel , filler , texels_in_row 
-            , 0 , 0
-            , letter_size_x - 1 , letter_size_y / 3
+            , letter_size_x - 1 , 0
+            , 0 , letter_size_y / 3
             , letter_size_x / 2 , letter_size_y - 1
             ) ;
     }
