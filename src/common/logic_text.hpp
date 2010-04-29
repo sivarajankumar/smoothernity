00e07fbf4b6bc64dc9989088f764ce7f14840a6f
olegus8 <olegus8@localhost>
2010-04-29 17:36:00 +0000

    refactoring common part

--- src/common/logic_text.hpp
+++ src/common/logic_text.hpp
@@ -1,6 +1,3 @@
-#define TEXT_TEXTURE_SIZE_POW2_BASE 8
-#define TEXT_TEXTURE_SIZE ( 1 << TEXT_TEXTURE_SIZE_POW2_BASE )
-
 template < typename mediator >
 class shy_logic_text
 {
@@ -13,165 +10,19 @@ class shy_logic_text
     typedef typename mediator :: platform :: texel_data texel_data ;
     typedef typename mediator :: platform :: vertex_data vertex_data ;
     
+    static const int_32 TEXT_TEXTURE_SIZE_POW2_BASE = 8 ;
+    static const int_32 TEXT_TEXTURE_SIZE = 1 << TEXT_TEXTURE_SIZE_POW2_BASE ;
+
 public :
-    shy_logic_text ( mediator * arg_mediator )
-    : _mediator ( arg_mediator )
-    , _text_mesh_created ( false )
-    {
-    }
-    void render_text ( )
-    {
-        if ( _text_mesh_created )
-            _render_text_mesh ( ) ;
-    }
-    void update ( )
-    {
-        if ( ! _text_mesh_created )
-        {
-            _create_text_mesh ( ) ;
-            _create_text_texture ( ) ;
-            _text_mesh_created = true ;
-        }
-    }
+    shy_logic_text ( mediator * arg_mediator ) ;
+    void render_text ( ) ;
+    void update ( ) ;
 private :
-    void _render_text_mesh ( )
-    {
-        platform :: render_enable_texturing ( ) ;
-        platform :: render_use_texture ( _text_texture_id ) ;
-        _mediator -> mesh_render ( _text_mesh_id ) ;
-    }
-    void _create_text_mesh ( )
-    {
-        vertex_data vertices [ 4 ] ;
-        index_data indices [ 4 ] ;
-
-        platform :: render_set_vertex_position  ( vertices [ 0 ] , - 1 , 1 , - 3 ) ;
-        platform :: render_set_vertex_color     ( vertices [ 0 ] , 255 , 255 , 255 , 255 ) ;
-        platform :: render_set_vertex_tex_coord ( vertices [ 0 ] , 0 , 1 ) ;
-        platform :: render_set_index_value      ( indices  [ 0 ] , 0 ) ;
-
-        platform :: render_set_vertex_position  ( vertices [ 1 ] , - 1 , - 1 , - 3 ) ;
-        platform :: render_set_vertex_color     ( vertices [ 1 ] , 255 , 255 , 255 , 255 ) ;
-        platform :: render_set_vertex_tex_coord ( vertices [ 1 ] , 0 , 0 ) ;
-        platform :: render_set_index_value      ( indices  [ 1 ] , 1 ) ;
-
-        platform :: render_set_vertex_position  ( vertices [ 2 ] , 1 , 1 , - 3 ) ;
-        platform :: render_set_vertex_color     ( vertices [ 2 ] , 255 , 255 , 255 , 255 ) ;
-        platform :: render_set_vertex_tex_coord ( vertices [ 2 ] , 1 , 1 ) ;
-        platform :: render_set_index_value      ( indices  [ 2 ] , 2 ) ;
-
-        platform :: render_set_vertex_position  ( vertices [ 3 ] , 1 , - 1 , - 3 ) ;
-        platform :: render_set_vertex_color     ( vertices [ 3 ] , 255 , 255 , 255 , 255 ) ;
-        platform :: render_set_vertex_tex_coord ( vertices [ 3 ] , 1 , 0 ) ;
-        platform :: render_set_index_value      ( indices  [ 3 ] , 3 ) ;
-
-        _text_mesh_id = _mediator -> mesh_create ( vertices , indices , 0 , 4 , 4 , 0 ) ;
-    }
-    void _create_text_texture ( )
-    {
-        for ( int_32 x = 0 ; x < TEXT_TEXTURE_SIZE ; x ++ )
-        {
-            for ( int_32 y = 0 ; y < TEXT_TEXTURE_SIZE ; y ++ )
-            {
-                int_32 c = x ^ y ;
-                platform :: render_set_texel_color
-                    ( _text_texture_data [ x + TEXT_TEXTURE_SIZE * y ]
-                    , ( c % 32 ) * 8
-                    , ( c % 64 ) * 4
-                    , ( c % 128 ) * 2
-                    , ( c % 64 ) * 4
-                    ) ;
-            }
-        }
-        _generate_font_english_B ( _text_texture_data       , TEXT_TEXTURE_SIZE , 16 , 16 ) ;
-        _generate_font_english_B ( _text_texture_data +  32 , TEXT_TEXTURE_SIZE , 32 , 32 ) ;
-        _generate_font_english_B ( _text_texture_data +  64 , TEXT_TEXTURE_SIZE , 64 , 64 ) ;
-        _generate_font_english_B ( _text_texture_data + 128 , TEXT_TEXTURE_SIZE , 128 , 128 ) ;
-        platform :: render_create_texture_id ( _text_texture_id ) ;
-        platform :: render_load_texture_data ( _text_texture_id , TEXT_TEXTURE_SIZE_POW2_BASE , _text_texture_data ) ;
-    }
-    void _generate_font_english_A 
-        ( texel_data * starting_texel 
-        , int_32 texels_in_row 
-        , int_32 letter_size_x 
-        , int_32 letter_size_y
-        )
-    {
-        _mediator -> rasterize_use_context ( starting_texel , texels_in_row ) ;
-
-        texel_data filler ;
-        texel_data eraser ;
-        platform :: render_set_texel_color ( filler , 255 , 255 , 255 , 255 ) ;
-        platform :: render_set_texel_color ( eraser , 0 , 0 , 0 , 0 ) ;
-                
-        int_32 outer_top = letter_size_y - 1 ;
-        int_32 outer_bottom = 0 ;
-        int_32 outer_center = letter_size_x / 2 ;
-        int_32 outer_left = 0 ;
-        int_32 outer_right = letter_size_x - 1 ;
-        _mediator -> rasterize_use_texel ( filler ) ;
-        _mediator -> rasterize_triangle ( outer_center , outer_top , outer_left , outer_bottom , outer_right , outer_bottom ) ;
-                
-        int_32 inner_top = ( letter_size_y * 2 ) / 3 ;
-        int_32 inner_bottom = 0 ;
-        int_32 inner_center = letter_size_x / 2 ;
-        int_32 inner_left = letter_size_x / 5 ;
-        int_32 inner_right = ( letter_size_x * 4 ) / 5 ;
-        _mediator -> rasterize_use_texel ( eraser ) ;
-        _mediator -> rasterize_triangle ( inner_center , inner_top , inner_left , inner_bottom , inner_right , inner_bottom ) ;
-        
-        int_32 board_top = ( letter_size_y * 3 ) / 7 ;
-        int_32 board_bottom = ( letter_size_y * 2 ) / 7 ;
-        int_32 board_top_left     = outer_center + ( ( outer_left  - outer_center ) * ( outer_top - board_top    ) ) / ( outer_top - outer_bottom ) ;
-        int_32 board_bottom_left  = outer_center + ( ( outer_left  - outer_center ) * ( outer_top - board_bottom ) ) / ( outer_top - outer_bottom ) ;
-        int_32 board_top_right    = outer_center + ( ( outer_right - outer_center ) * ( outer_top - board_top    ) ) / ( outer_top - outer_bottom ) ;
-        int_32 board_bottom_right = outer_center + ( ( outer_right - outer_center ) * ( outer_top - board_bottom ) ) / ( outer_top - outer_bottom ) ;
-        _mediator -> rasterize_use_texel ( filler ) ;
-        _mediator -> rasterize_triangle ( board_top_left , board_top , board_bottom_left , board_bottom , board_bottom_right , board_bottom ) ;
-        _mediator -> rasterize_triangle ( board_top_left , board_top , board_top_right , board_top , board_bottom_right , board_bottom ) ;
-    }
-    void _generate_font_english_B
-        ( texel_data * starting_texel 
-        , int_32 texels_in_row 
-        , int_32 letter_size_x 
-        , int_32 letter_size_y
-        )
-    {
-        _mediator -> rasterize_use_context ( starting_texel , texels_in_row ) ;
-        
-        texel_data filler ;
-        texel_data eraser ;
-        platform :: render_set_texel_color ( filler , 255 , 255 , 255 , 255 ) ;
-        platform :: render_set_texel_color ( eraser , 0 , 0 , 0 , 0 ) ;
-
-        _mediator -> rasterize_use_texel ( filler ) ;
-        _mediator -> rasterize_ellipse_in_rect ( 0 , 0 , letter_size_x - 1 , letter_size_y / 2 ) ;
-        _mediator -> rasterize_ellipse_in_rect ( 0 , letter_size_y / 2 , letter_size_x - 1 , letter_size_y - 1 ) ;
-
-        int_32 spine_left = 0 ;
-        int_32 spine_right = letter_size_x / 2 ;
-        int_32 spine_top = letter_size_y - 1 ;
-        int_32 spine_bottom = 0 ;
-        _mediator -> rasterize_use_texel ( filler ) ;
-        _mediator -> rasterize_triangle ( spine_left , spine_top , spine_left , spine_bottom , spine_right , spine_bottom ) ;
-        _mediator -> rasterize_triangle ( spine_left , spine_top , spine_right , spine_top , spine_right , spine_bottom ) ;
-                
-        int_32 hole_left = ( letter_size_x * 4 ) / 16 ;
-        int_32 hole_right = ( letter_size_x * 12 ) / 16 ;
-        int_32 hole_top = ( letter_size_y * 13 ) / 16 ;
-        int_32 hole_bottom = ( letter_size_y * 3 ) / 16 ;
-        int_32 hole_height = ( letter_size_y * 3 ) / 16 ;
-        _mediator -> rasterize_use_texel ( eraser ) ;
-        _mediator -> rasterize_ellipse_in_rect ( hole_left , hole_top , hole_right , hole_top - hole_height ) ;
-        _mediator -> rasterize_ellipse_in_rect ( hole_left , hole_bottom , hole_right , hole_bottom + hole_height ) ;
-
-        int_32 hole_center_x = ( hole_left + hole_right ) / 2 ;
-        _mediator -> rasterize_use_texel ( eraser ) ;
-        _mediator -> rasterize_triangle ( hole_left , hole_top , hole_left , hole_top - hole_height , hole_center_x , hole_top - hole_height ) ;
-        _mediator -> rasterize_triangle ( hole_left , hole_top , hole_center_x , hole_top , hole_center_x , hole_top - hole_height ) ;
-        _mediator -> rasterize_triangle ( hole_left , hole_bottom , hole_left , hole_bottom + hole_height , hole_center_x , hole_bottom + hole_height ) ;
-        _mediator -> rasterize_triangle ( hole_left , hole_bottom , hole_center_x , hole_bottom , hole_center_x , hole_bottom + hole_height ) ;
-    }
+    void _render_text_mesh ( ) ;
+    void _create_text_mesh ( ) ;
+    void _create_text_texture ( ) ;
+    void _generate_font_english_A ( texel_data * starting_texel , int_32 texels_in_row , int_32 letter_size_x , int_32 letter_size_y ) ;
+    void _generate_font_english_B ( texel_data * starting_texel , int_32 texels_in_row , int_32 letter_size_x , int_32 letter_size_y ) ;
 private :
     mediator * _mediator ;
     int_32 _text_mesh_created ;
@@ -179,3 +30,176 @@ private :
     render_texture_id _text_texture_id ;
     texel_data _text_texture_data [ TEXT_TEXTURE_SIZE * TEXT_TEXTURE_SIZE ] ;
 } ;
+
+template < typename mediator >
+shy_logic_text < mediator > :: shy_logic_text ( mediator * arg_mediator )
+: _mediator ( arg_mediator )
+, _text_mesh_created ( false )
+{
+}
+
+template < typename mediator >
+void shy_logic_text < mediator > :: render_text ( )
+{
+    if ( _text_mesh_created )
+        _render_text_mesh ( ) ;
+}
+
+template < typename mediator >
+void shy_logic_text < mediator > :: update ( )
+{
+    if ( ! _text_mesh_created )
+    {
+        _create_text_mesh ( ) ;
+        _create_text_texture ( ) ;
+        _text_mesh_created = true ;
+    }
+}
+
+template < typename mediator >
+void shy_logic_text < mediator > :: _render_text_mesh ( )
+{
+    platform :: render_enable_texturing ( ) ;
+    platform :: render_use_texture ( _text_texture_id ) ;
+    _mediator -> mesh_render ( _text_mesh_id ) ;
+}
+
+template < typename mediator >
+void shy_logic_text < mediator > :: _create_text_mesh ( )
+{
+    vertex_data vertices [ 4 ] ;
+    index_data indices [ 4 ] ;
+
+    platform :: render_set_vertex_position  ( vertices [ 0 ] , - 1 , 1 , - 3 ) ;
+    platform :: render_set_vertex_color     ( vertices [ 0 ] , 255 , 255 , 255 , 255 ) ;
+    platform :: render_set_vertex_tex_coord ( vertices [ 0 ] , 0 , 1 ) ;
+    platform :: render_set_index_value      ( indices  [ 0 ] , 0 ) ;
+
+    platform :: render_set_vertex_position  ( vertices [ 1 ] , - 1 , - 1 , - 3 ) ;
+    platform :: render_set_vertex_color     ( vertices [ 1 ] , 255 , 255 , 255 , 255 ) ;
+    platform :: render_set_vertex_tex_coord ( vertices [ 1 ] , 0 , 0 ) ;
+    platform :: render_set_index_value      ( indices  [ 1 ] , 1 ) ;
+
+    platform :: render_set_vertex_position  ( vertices [ 2 ] , 1 , 1 , - 3 ) ;
+    platform :: render_set_vertex_color     ( vertices [ 2 ] , 255 , 255 , 255 , 255 ) ;
+    platform :: render_set_vertex_tex_coord ( vertices [ 2 ] , 1 , 1 ) ;
+    platform :: render_set_index_value      ( indices  [ 2 ] , 2 ) ;
+
+    platform :: render_set_vertex_position  ( vertices [ 3 ] , 1 , - 1 , - 3 ) ;
+    platform :: render_set_vertex_color     ( vertices [ 3 ] , 255 , 255 , 255 , 255 ) ;
+    platform :: render_set_vertex_tex_coord ( vertices [ 3 ] , 1 , 0 ) ;
+    platform :: render_set_index_value      ( indices  [ 3 ] , 3 ) ;
+
+    _text_mesh_id = _mediator -> mesh_create ( vertices , indices , 0 , 4 , 4 , 0 ) ;
+}
+
+template < typename mediator >
+void shy_logic_text < mediator > :: _create_text_texture ( )
+{
+    for ( int_32 x = 0 ; x < TEXT_TEXTURE_SIZE ; x ++ )
+    {
+        for ( int_32 y = 0 ; y < TEXT_TEXTURE_SIZE ; y ++ )
+        {
+            int_32 c = x ^ y ;
+            platform :: render_set_texel_color
+                ( _text_texture_data [ x + TEXT_TEXTURE_SIZE * y ]
+                , ( c % 32 ) * 8
+                , ( c % 64 ) * 4
+                , ( c % 128 ) * 2
+                , ( c % 64 ) * 4
+                ) ;
+        }
+    }
+    _generate_font_english_B ( _text_texture_data       , TEXT_TEXTURE_SIZE , 16 , 16 ) ;
+    _generate_font_english_B ( _text_texture_data +  32 , TEXT_TEXTURE_SIZE , 32 , 32 ) ;
+    _generate_font_english_B ( _text_texture_data +  64 , TEXT_TEXTURE_SIZE , 64 , 64 ) ;
+    _generate_font_english_B ( _text_texture_data + 128 , TEXT_TEXTURE_SIZE , 128 , 128 ) ;
+    platform :: render_create_texture_id ( _text_texture_id ) ;
+    platform :: render_load_texture_data ( _text_texture_id , TEXT_TEXTURE_SIZE_POW2_BASE , _text_texture_data ) ;
+}
+
+template < typename mediator >
+void shy_logic_text < mediator > :: _generate_font_english_A 
+    ( texel_data * starting_texel 
+    , int_32 texels_in_row 
+    , int_32 letter_size_x 
+    , int_32 letter_size_y
+    )
+{
+    _mediator -> rasterize_use_context ( starting_texel , texels_in_row ) ;
+
+    texel_data filler ;
+    texel_data eraser ;
+    platform :: render_set_texel_color ( filler , 255 , 255 , 255 , 255 ) ;
+    platform :: render_set_texel_color ( eraser , 0 , 0 , 0 , 0 ) ;
+            
+    int_32 outer_top = letter_size_y - 1 ;
+    int_32 outer_bottom = 0 ;
+    int_32 outer_center = letter_size_x / 2 ;
+    int_32 outer_left = 0 ;
+    int_32 outer_right = letter_size_x - 1 ;
+    _mediator -> rasterize_use_texel ( filler ) ;
+    _mediator -> rasterize_triangle ( outer_center , outer_top , outer_left , outer_bottom , outer_right , outer_bottom ) ;
+            
+    int_32 inner_top = ( letter_size_y * 2 ) / 3 ;
+    int_32 inner_bottom = 0 ;
+    int_32 inner_center = letter_size_x / 2 ;
+    int_32 inner_left = letter_size_x / 5 ;
+    int_32 inner_right = ( letter_size_x * 4 ) / 5 ;
+    _mediator -> rasterize_use_texel ( eraser ) ;
+    _mediator -> rasterize_triangle ( inner_center , inner_top , inner_left , inner_bottom , inner_right , inner_bottom ) ;
+    
+    int_32 board_top = ( letter_size_y * 3 ) / 7 ;
+    int_32 board_bottom = ( letter_size_y * 2 ) / 7 ;
+    int_32 board_top_left     = outer_center + ( ( outer_left  - outer_center ) * ( outer_top - board_top    ) ) / ( outer_top - outer_bottom ) ;
+    int_32 board_bottom_left  = outer_center + ( ( outer_left  - outer_center ) * ( outer_top - board_bottom ) ) / ( outer_top - outer_bottom ) ;
+    int_32 board_top_right    = outer_center + ( ( outer_right - outer_center ) * ( outer_top - board_top    ) ) / ( outer_top - outer_bottom ) ;
+    int_32 board_bottom_right = outer_center + ( ( outer_right - outer_center ) * ( outer_top - board_bottom ) ) / ( outer_top - outer_bottom ) ;
+    _mediator -> rasterize_use_texel ( filler ) ;
+    _mediator -> rasterize_triangle ( board_top_left , board_top , board_bottom_left , board_bottom , board_bottom_right , board_bottom ) ;
+    _mediator -> rasterize_triangle ( board_top_left , board_top , board_top_right , board_top , board_bottom_right , board_bottom ) ;
+}
+
+template < typename mediator >
+void shy_logic_text < mediator > :: _generate_font_english_B
+    ( texel_data * starting_texel 
+    , int_32 texels_in_row 
+    , int_32 letter_size_x 
+    , int_32 letter_size_y
+    )
+{
+    _mediator -> rasterize_use_context ( starting_texel , texels_in_row ) ;
+    
+    texel_data filler ;
+    texel_data eraser ;
+    platform :: render_set_texel_color ( filler , 255 , 255 , 255 , 255 ) ;
+    platform :: render_set_texel_color ( eraser , 0 , 0 , 0 , 0 ) ;
+
+    _mediator -> rasterize_use_texel ( filler ) ;
+    _mediator -> rasterize_ellipse_in_rect ( 0 , 0 , letter_size_x - 1 , letter_size_y / 2 ) ;
+    _mediator -> rasterize_ellipse_in_rect ( 0 , letter_size_y / 2 , letter_size_x - 1 , letter_size_y - 1 ) ;
+
+    int_32 spine_left = 0 ;
+    int_32 spine_right = letter_size_x / 2 ;
+    int_32 spine_top = letter_size_y - 1 ;
+    int_32 spine_bottom = 0 ;
+    _mediator -> rasterize_use_texel ( filler ) ;
+    _mediator -> rasterize_triangle ( spine_left , spine_top , spine_left , spine_bottom , spine_right , spine_bottom ) ;
+    _mediator -> rasterize_triangle ( spine_left , spine_top , spine_right , spine_top , spine_right , spine_bottom ) ;
+            
+    int_32 hole_left = ( letter_size_x * 4 ) / 16 ;
+    int_32 hole_right = ( letter_size_x * 12 ) / 16 ;
+    int_32 hole_top = ( letter_size_y * 13 ) / 16 ;
+    int_32 hole_bottom = ( letter_size_y * 3 ) / 16 ;
+    int_32 hole_height = ( letter_size_y * 3 ) / 16 ;
+    _mediator -> rasterize_use_texel ( eraser ) ;
+    _mediator -> rasterize_ellipse_in_rect ( hole_left , hole_top , hole_right , hole_top - hole_height ) ;
+    _mediator -> rasterize_ellipse_in_rect ( hole_left , hole_bottom , hole_right , hole_bottom + hole_height ) ;
+
+    int_32 hole_center_x = ( hole_left + hole_right ) / 2 ;
+    _mediator -> rasterize_use_texel ( eraser ) ;
+    _mediator -> rasterize_triangle ( hole_left , hole_top , hole_left , hole_top - hole_height , hole_center_x , hole_top - hole_height ) ;
+    _mediator -> rasterize_triangle ( hole_left , hole_top , hole_center_x , hole_top , hole_center_x , hole_top - hole_height ) ;
+    _mediator -> rasterize_triangle ( hole_left , hole_bottom , hole_left , hole_bottom + hole_height , hole_center_x , hole_bottom + hole_height ) ;
+    _mediator -> rasterize_triangle ( hole_left , hole_bottom , hole_center_x , hole_bottom , hole_center_x , hole_bottom + hole_height ) ;
+}
