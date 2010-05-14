ddffe0d9a0dcb30f22e4329d669496b586d3d49b
olegus8 <olegus8@localhost>
2010-05-14 12:23:49 +0000

    working on image loading

--- src/macosx/macosx_texture_loader.m
+++ src/macosx/macosx_texture_loader.m
@@ -22,45 +22,43 @@
     to_buffer : ( void * ) buffer
     with_side_size_of : ( int ) side_size
 {
-    NSUInteger              width, height;
-    NSURL                   *url = nil;
-    CGImageSourceRef        src;
-    CGImageRef              image;
-    CGContextRef            context = nil;
-    CGColorSpaceRef         colorSpace;
-    GLubyte                 *data = (GLubyte*) buffer;
-    
-    url = [NSURL fileURLWithPath: [[NSBundle mainBundle] pathForResource:[ NSString stringWithFormat : @"texture_resource_%i" , resource_index ] ofType:@"png"]];
-    src = CGImageSourceCreateWithURL((CFURLRef)url, NULL);
-    
-    if (!src) {
-        NSLog(@"No image");
-        return;
+    NSUInteger width ;
+    NSUInteger height ;
+    NSURL * url = nil ;
+    CGImageSourceRef src ;
+    CGImageRef image ;
+    CGContextRef context = nil ;
+    CGColorSpaceRef colorSpace ;
+    GLubyte * data = ( GLubyte * ) buffer ;
+    url = [ NSURL fileURLWithPath : [ [ NSBundle mainBundle ] pathForResource : 
+        [ NSString stringWithFormat : @"texture_resource_%i" , resource_index ] 
+        ofType : @"png"
+        ] ] ;
+    src = CGImageSourceCreateWithURL ( ( CFURLRef ) url , 0 ) ;
+    if ( src )
+    {
+        image = CGImageSourceCreateImageAtIndex ( src , 0 , 0 ) ;
+        CFRelease ( src ) ;
+        width = CGImageGetWidth ( image ) ;
+        height = CGImageGetHeight ( image ) ;
+        colorSpace = CGColorSpaceCreateDeviceRGB ( ) ;
+        context = CGBitmapContextCreate 
+            ( data 
+            , width 
+            , height 
+            , 8 
+            , 4 * width 
+            , colorSpace 
+            , kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host 
+            ) ;
+        CGColorSpaceRelease ( colorSpace ) ;
+        CGContextTranslateCTM ( context , 0 , height ) ;
+        CGContextScaleCTM ( context , 1 , - 1 ) ;
+        CGContextSetBlendMode ( context , kCGBlendModeCopy ) ;
+        CGContextDrawImage ( context , CGRectMake ( 0 , 0 , width , height ) , image ) ;
+        CGContextRelease ( context ) ;
+        CGImageRelease ( image ) ;
     }
-    
-    image = CGImageSourceCreateImageAtIndex(src, 0, NULL);
-    CFRelease(src);
-    
-    width = CGImageGetWidth(image);
-    height = CGImageGetHeight(image);
-    
-    colorSpace = CGColorSpaceCreateDeviceRGB();
-    context = CGBitmapContextCreate(data, width, height, 8, 4 * width, colorSpace, kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host);
-    CGColorSpaceRelease(colorSpace);
-    
-    // Core Graphics referential is upside-down compared to OpenGL referential
-    // Flip the Core Graphics context here
-    // An alternative is to use flipped OpenGL texture coordinates when drawing textures
-    CGContextTranslateCTM(context, 0.0, height);
-    CGContextScaleCTM(context, 1.0, -1.0);
-    
-    // Set the blend mode to copy before drawing since the previous contents of memory aren't used. This avoids unnecessary blending.
-    CGContextSetBlendMode(context, kCGBlendModeCopy);
-    
-    CGContextDrawImage(context, CGRectMake(0, 0, width, height), image);
-    
-    CGContextRelease(context);
-    CGImageRelease(image);
 }
 
 @end
