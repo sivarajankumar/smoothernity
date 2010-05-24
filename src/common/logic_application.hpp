7563f927683f588a7bb2fa85f66c4b7e44552337
olegus8 <olegus8@localhost>
2010-05-24 18:36:02 +0000

    working on numeric math

--- src/common/logic_application.hpp
+++ src/common/logic_application.hpp
@@ -2,28 +2,28 @@ template < typename mediator >
 class shy_logic_application
 {
     typedef typename mediator :: platform platform ;
-    typedef typename mediator :: platform :: int_32 int_32 ;
     typedef typename mediator :: platform :: num_fract num_fract ;
+    typedef typename mediator :: platform :: num_whole num_whole ;
 public :
     shy_logic_application ( mediator * arg_mediator ) ;
     void application_render ( ) ;
     void application_update ( ) ;
 private :
     mediator * _mediator ;
-    int_32 _application_launched ;
+    num_whole _application_launched ;
 } ;
 
 template < typename mediator >
 shy_logic_application < mediator > :: shy_logic_application ( mediator * arg_mediator )
 : _mediator ( arg_mediator )
-, _application_launched ( false )
 {
+    platform :: math_make_num_whole ( _application_launched , false ) ;
 }
 
 template < typename mediator >
 void shy_logic_application < mediator > :: application_render ( )
 {
-    if ( _application_launched )
+    if ( platform :: condition_true ( _application_launched ) )
         _mediator -> game_render ( ) ;
     else
     {
@@ -36,9 +36,9 @@ void shy_logic_application < mediator > :: application_render ( )
 template < typename mediator >
 void shy_logic_application < mediator > :: application_update ( )
 {
-    if ( ! _application_launched )
+    if ( platform :: condition_false ( _application_launched ) )
     {
-        _application_launched = true ;
+        platform :: math_make_num_whole ( _application_launched , true ) ;
         _mediator -> game_launch_permit ( ) ;
     }
     else
