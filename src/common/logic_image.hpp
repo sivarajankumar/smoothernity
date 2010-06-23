75b3bc369257f2a6e37ba0e71817e0022e125d5d
olegus8 <olegus8@localhost>
2010-06-23 16:20:51 +0000

    working on messaging

--- src/common/logic_image.hpp
+++ src/common/logic_image.hpp
@@ -229,39 +229,34 @@ void shy_logic_image < mediator > :: _create_image_mesh ( )
     platform_math :: make_num_fract ( color_a , _image_a , 255 ) ;
     platform_math :: make_num_whole ( vertices_count , 4 ) ;
 
-    platform_math :: make_num_whole ( index , 0 ) ;
-    platform_render :: set_vertex_position  ( platform_static_array :: element ( vertices , platform :: math_consts . whole_0 ) , x_left , y_top , z ) ;
-    platform_render :: set_vertex_color     ( platform_static_array :: element ( vertices , platform :: math_consts . whole_0 ) , color_r , color_g , color_b , color_a ) ;
-    platform_render :: set_vertex_tex_coord ( platform_static_array :: element ( vertices , platform :: math_consts . whole_0 ) , u_left , v_top ) ;
-    platform_render :: set_index_value      ( platform_static_array :: element ( indices  , platform :: math_consts . whole_0 ) , index ) ;
-
-    platform_math :: make_num_whole ( index , 1 ) ;
-    platform_render :: set_vertex_position  ( platform_static_array :: element ( vertices , platform :: math_consts . whole_1 ) , x_left , y_bottom , z ) ;
-    platform_render :: set_vertex_color     ( platform_static_array :: element ( vertices , platform :: math_consts . whole_1 ) , color_r , color_g , color_b , color_a ) ;
-    platform_render :: set_vertex_tex_coord ( platform_static_array :: element ( vertices , platform :: math_consts . whole_1 ) , u_left , v_bottom ) ;
-    platform_render :: set_index_value      ( platform_static_array :: element ( indices  , platform :: math_consts . whole_1 ) , index ) ;
-
-    platform_math :: make_num_whole ( index , 2 ) ;
-    platform_render :: set_vertex_position  ( platform_static_array :: element ( vertices , platform :: math_consts . whole_2 ) , x_right , y_top , z ) ;
-    platform_render :: set_vertex_color     ( platform_static_array :: element ( vertices , platform :: math_consts . whole_2 ) , color_r , color_g , color_b , color_a ) ;
-    platform_render :: set_vertex_tex_coord ( platform_static_array :: element ( vertices , platform :: math_consts . whole_2 ) , u_right , v_top ) ;
-    platform_render :: set_index_value      ( platform_static_array :: element ( indices  , platform :: math_consts . whole_2 ) , index ) ;
-
-    platform_math :: make_num_whole ( index , 3 ) ;
-    platform_render :: set_vertex_position  ( platform_static_array :: element ( vertices , platform :: math_consts . whole_3 ) , x_right , y_bottom , z ) ;
-    platform_render :: set_vertex_color     ( platform_static_array :: element ( vertices , platform :: math_consts . whole_3 ) , color_r , color_g , color_b , color_a ) ;
-    platform_render :: set_vertex_tex_coord ( platform_static_array :: element ( vertices , platform :: math_consts . whole_3 ) , u_right , v_bottom ) ;
-    platform_render :: set_index_value      ( platform_static_array :: element ( indices  , platform :: math_consts . whole_3 ) , index ) ;
-
     _mediator . get ( ) . mesh_create
         ( _image_mesh_id 
-        , vertices 
-        , indices 
-        , indices
-        , vertices_count 
-        , vertices_count 
+        , platform :: math_consts . whole_4 
+        , platform :: math_consts . whole_4 
         , platform :: math_consts . whole_0 
         ) ;
+        
+    _mediator . get ( ) . mesh_set_vertex_position  ( _image_mesh_id , platform :: math_consts . whole_0 , x_left , y_top , z ) ;
+    _mediator . get ( ) . mesh_set_vertex_color     ( _image_mesh_id , platform :: math_consts . whole_0 , color_r , color_g , color_b , color_a ) ;
+    _mediator . get ( ) . mesh_set_vertex_tex_coord ( _image_mesh_id , platform :: math_consts . whole_0 , u_left , v_top ) ;
+    _mediator . get ( ) . mesh_set_triangle_strip_index_value      ( _image_mesh_id , platform :: math_consts . whole_0 , platform :: math_consts . whole_0 ) ;
+
+    _mediator . get ( ) . mesh_set_vertex_position  ( _image_mesh_id , platform :: math_consts . whole_1 , x_left , y_bottom , z ) ;
+    _mediator . get ( ) . mesh_set_vertex_color     ( _image_mesh_id , platform :: math_consts . whole_1 , color_r , color_g , color_b , color_a ) ;
+    _mediator . get ( ) . mesh_set_vertex_tex_coord ( _image_mesh_id , platform :: math_consts . whole_1 , u_left , v_bottom ) ;
+    _mediator . get ( ) . mesh_set_triangle_strip_index_value      ( _image_mesh_id , platform :: math_consts . whole_1 , platform :: math_consts . whole_1 ) ;
+
+    _mediator . get ( ) . mesh_set_vertex_position  ( _image_mesh_id , platform :: math_consts . whole_2 , x_right , y_top , z ) ;
+    _mediator . get ( ) . mesh_set_vertex_color     ( _image_mesh_id , platform :: math_consts . whole_2 , color_r , color_g , color_b , color_a ) ;
+    _mediator . get ( ) . mesh_set_vertex_tex_coord ( _image_mesh_id , platform :: math_consts . whole_2 , u_right , v_top ) ;
+    _mediator . get ( ) . mesh_set_triangle_strip_index_value      ( _image_mesh_id , platform :: math_consts . whole_2 , platform :: math_consts . whole_2 ) ;
+
+    _mediator . get ( ) . mesh_set_vertex_position  ( _image_mesh_id , platform :: math_consts . whole_3 , x_right , y_bottom , z ) ;
+    _mediator . get ( ) . mesh_set_vertex_color     ( _image_mesh_id , platform :: math_consts . whole_3 , color_r , color_g , color_b , color_a ) ;
+    _mediator . get ( ) . mesh_set_vertex_tex_coord ( _image_mesh_id , platform :: math_consts . whole_3 , u_right , v_bottom ) ;
+    _mediator . get ( ) . mesh_set_triangle_strip_index_value      ( _image_mesh_id , platform :: math_consts . whole_3 , platform :: math_consts . whole_3 ) ;
+
+    _mediator . get ( ) . mesh_finalize ( _image_mesh_id ) ;
 }
 
 template < typename mediator >
