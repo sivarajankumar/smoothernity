0e68fe6cf0e902ac92b4e1564b10748099241740
olegus8 <olegus8@localhost>
2010-07-30 14:26:19 +0000

    working on mediator decomposition

---
+++ src/common/logic/land/stateless.hpp
@@ -0,0 +1,29 @@
+template < typename mediator >
+class shy_logic_land_stateless
+{
+    typedef typename mediator :: platform :: platform_pointer platform_pointer ;
+public :
+    class logic_land_messages
+    {
+    public :
+    } ;
+    
+    template < typename receivers >
+    class logic_land_sender
+    {
+    public :
+        void set_receivers ( typename platform_pointer :: template pointer < const receivers > arg_receivers ) ;
+        void send ( ) { }
+    private :
+        typename platform_pointer :: template pointer < const receivers > _receivers ;
+    } ;
+} ;
+
+template < typename mediator >
+template < typename receivers >
+void shy_logic_land_stateless < mediator >
+:: logic_land_sender < receivers >
+:: set_receivers ( typename platform_pointer :: template pointer < const receivers > arg_receivers )
+{
+    _receivers = arg_receivers ;
+}
