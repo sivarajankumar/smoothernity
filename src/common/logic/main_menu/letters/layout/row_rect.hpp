5522bba7ded989f9d71309432ccdd52921bf4a34
olegus8 <olegus8@localhost>
2010-09-09 16:54:14 +0000

    working on main menu

--- src/common/logic/main_menu/letters/layout/row_rect.hpp
+++ src/common/logic/main_menu/letters/layout/row_rect.hpp
@@ -2,11 +2,13 @@ template < typename mediator >
 class shy_logic_main_menu_letters_layout_row_rect
 {
     typedef typename mediator :: engine_math :: rect rect ;
+    typedef typename mediator :: logic_main_menu_letters_layout_stateless logic_main_menu_letters_layout_stateless ;
     typedef typename mediator :: logic_main_menu_letters_layout_stateless :: logic_main_menu_letters_layout_stateless_consts_type logic_main_menu_letters_layout_stateless_consts_type ;
     typedef typename mediator :: logic_main_menu_letters_meshes_stateless :: logic_main_menu_letters_meshes_stateless_consts_type logic_main_menu_letters_meshes_stateless_consts_type ;
     typedef typename mediator :: messages messages ;
     typedef typename mediator :: platform platform ;
     typedef typename mediator :: platform :: platform_conditions platform_conditions ;
+    typedef typename mediator :: platform :: platform_math platform_math ;
     typedef typename mediator :: platform :: platform_math :: num_fract num_fract ;
     typedef typename mediator :: platform :: platform_math :: num_whole num_whole ;
     typedef typename mediator :: platform :: platform_math_consts platform_math_consts ;
@@ -17,6 +19,12 @@ class shy_logic_main_menu_letters_layout_row_rect
     public :
         num_whole requested ;
         num_whole requested_row ;
+        
+        num_fract unscaled_menu_width ;
+        num_fract unscaled_menu_height ;
+        num_fract menu_scale ;
+
+        rect menu_rect ;
         rect row_rect ;
     } ;
     
@@ -50,6 +58,7 @@ private :
     void _obtain_aspect_ratio ( ) ;
     void _received_aspect_ratio ( ) ;
     void _compute_row_rect ( ) ;
+    void _decorate_row_rect ( ) ;
     void _reply_row_rect ( ) ;
 private :
     typename platform_pointer :: template pointer < mediator > _mediator ;
@@ -150,18 +159,77 @@ template < typename mediator >
 void shy_logic_main_menu_letters_layout_row_rect < mediator > :: _received_aspect_ratio ( )
 {
     _compute_row_rect ( ) ;
+    _decorate_row_rect ( ) ;
     _reply_row_rect ( ) ;
 }
 
 template < typename mediator >
 void shy_logic_main_menu_letters_layout_row_rect < mediator > :: _compute_row_rect ( )
 {
+    logic_main_menu_letters_layout_stateless :: compute_unscaled_menu_size 
+        ( _logic_main_menu_letters_layout_row_rect_state . unscaled_menu_width
+        , _logic_main_menu_letters_layout_row_rect_state . unscaled_menu_height
+        , _logic_main_menu_letters_boundaries_state . cols
+        , _logic_main_menu_letters_boundaries_state . rows
+        , _logic_main_menu_letters_layout_stateless_consts . get ( ) . letter_size_fract_horizontal_spacing
+        , _logic_main_menu_letters_layout_stateless_consts . get ( ) . letter_size_fract_vertical_spacing
+        , _logic_main_menu_letters_layout_stateless_consts . get ( ) . letter_size_fract_horizontal_border
+        , _logic_main_menu_letters_layout_stateless_consts . get ( ) . letter_size_fract_vertical_border
+        , _logic_main_menu_letters_meshes_stateless_consts . get ( ) . letter_mesh_size
+        ) ;
+    logic_main_menu_letters_layout_stateless :: compute_menu_scale 
+        ( _logic_main_menu_letters_layout_row_rect_state . menu_scale
+        , _engine_render_aspect_state . width
+        , _engine_render_aspect_state . height
+        , _logic_main_menu_letters_layout_row_rect_state . unscaled_menu_width
+        , _logic_main_menu_letters_layout_row_rect_state . unscaled_menu_height
+        ) ;
+    logic_main_menu_letters_layout_stateless :: compute_menu_rect 
+        ( _logic_main_menu_letters_layout_row_rect_state . menu_rect
+        , _logic_main_menu_letters_layout_row_rect_state . menu_scale
+        , _logic_main_menu_letters_layout_row_rect_state . unscaled_menu_width
+        , _logic_main_menu_letters_layout_row_rect_state . unscaled_menu_height
+        ) ;
+    logic_main_menu_letters_layout_stateless :: compute_row_rect 
+        ( _logic_main_menu_letters_layout_row_rect_state . row_rect
+        , _logic_main_menu_letters_layout_row_rect_state . requested_row
+        , _logic_main_menu_letters_boundaries_state . cols
+        , _logic_main_menu_letters_layout_row_rect_state . menu_scale
+        , _logic_main_menu_letters_layout_row_rect_state . menu_rect
+        , _logic_main_menu_letters_layout_stateless_consts . get ( ) . letter_size_fract_vertical_border
+        , _logic_main_menu_letters_layout_stateless_consts . get ( ) . letter_size_fract_horizontal_spacing
+        , _logic_main_menu_letters_layout_stateless_consts . get ( ) . letter_size_fract_vertical_spacing
+        , _logic_main_menu_letters_meshes_stateless_consts . get ( ) . letter_mesh_size
+        ) ;
+}
+
+template < typename mediator >
+void shy_logic_main_menu_letters_layout_row_rect < mediator > :: _decorate_row_rect ( )
+{
     rect row_rect ;
+    num_fract horizontal_border ;
+    num_fract vertical_spacing ;
+    num_fract letter_mesh_scale ;
+    num_fract menu_scale ;
+    num_fract scaled_half_vertical_spacing ;
+    num_fract scaled_horizontal_border ;
+    num_fract final_scale ;
     
-    row_rect . left = _platform_math_consts . get ( ) . fract_minus_1 ;
-    row_rect . right = _platform_math_consts . get ( ) . fract_1 ;
-    row_rect . bottom = _platform_math_consts . get ( ) . fract_minus_1 ;
-    row_rect . top = _platform_math_consts . get ( ) . fract_1 ;
+    row_rect = _logic_main_menu_letters_layout_row_rect_state . row_rect ;
+    vertical_spacing = _logic_main_menu_letters_layout_stateless_consts . get ( ) . letter_size_fract_vertical_spacing ;
+    horizontal_border = _logic_main_menu_letters_layout_stateless_consts . get ( ) . letter_size_fract_horizontal_border ;
+    letter_mesh_scale = _logic_main_menu_letters_meshes_stateless_consts . get ( ) . letter_mesh_size ;
+    menu_scale = _logic_main_menu_letters_layout_row_rect_state . menu_scale ;
+    
+    platform_math :: mul_fracts ( final_scale , menu_scale , letter_mesh_scale ) ;
+    platform_math :: div_fracts ( scaled_half_vertical_spacing , vertical_spacing , _platform_math_consts . get ( ) . fract_2 ) ;
+    platform_math :: mul_fract_by ( scaled_half_vertical_spacing , final_scale ) ;
+    platform_math :: add_to_fract ( row_rect . top , scaled_half_vertical_spacing ) ;
+    platform_math :: add_to_fract ( row_rect . bottom , scaled_half_vertical_spacing ) ;
+    
+    platform_math :: mul_fracts ( scaled_horizontal_border , horizontal_border , final_scale ) ;
+    platform_math :: sub_from_fract ( row_rect . left , scaled_horizontal_border ) ;
+    platform_math :: add_to_fract ( row_rect . right , scaled_horizontal_border ) ;
     
     _logic_main_menu_letters_layout_row_rect_state . row_rect = row_rect ;
 }
