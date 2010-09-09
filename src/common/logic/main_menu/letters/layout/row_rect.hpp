59f5ebd43981a038316e35683833d58a0d8a3a97
olegus8 <olegus8@localhost>
2010-09-09 15:43:41 +0000

    working on main menu

--- src/common/logic/main_menu/letters/layout/row_rect.hpp
+++ src/common/logic/main_menu/letters/layout/row_rect.hpp
@@ -3,15 +3,30 @@ class shy_logic_main_menu_letters_layout_row_rect
 {
     typedef typename mediator :: messages messages ;
     typedef typename mediator :: platform platform ;
+    typedef typename mediator :: platform :: platform_conditions platform_conditions ;
+    typedef typename mediator :: platform :: platform_math :: num_whole num_whole ;
     typedef typename mediator :: platform :: platform_math_consts platform_math_consts ;
     typedef typename mediator :: platform :: platform_pointer platform_pointer ;
+    
+    class _logic_main_menu_letters_layout_row_rect_state_type
+    {
+    public :
+        num_whole requested ;
+        num_whole requested_row ;
+    } ;
+    
 public :
     void set_mediator ( typename platform_pointer :: template pointer < mediator > ) ;
     void receive ( typename messages :: init ) ;
     void receive ( typename messages :: logic_main_menu_letters_layout_row_rect_request ) ;
 private :
+    void _proceed_with_row_rect ( ) ;
+    void _reply_row_rect ( ) ;
+private :
     typename platform_pointer :: template pointer < mediator > _mediator ;
     typename platform_pointer :: template pointer < const platform_math_consts > _platform_math_consts ;
+    
+    _logic_main_menu_letters_layout_row_rect_state_type _logic_main_menu_letters_layout_row_rect_state ;
 } ;
 
 template < typename mediator >
@@ -31,8 +46,26 @@ void shy_logic_main_menu_letters_layout_row_rect < mediator > :: receive ( typen
 template < typename mediator >
 void shy_logic_main_menu_letters_layout_row_rect < mediator > :: receive ( typename messages :: logic_main_menu_letters_layout_row_rect_request msg )
 {
+    _logic_main_menu_letters_layout_row_rect_state . requested = _platform_math_consts . get ( ) . whole_true ;
+    _logic_main_menu_letters_layout_row_rect_state . requested_row = msg . row ;
+    _proceed_with_row_rect ( ) ;
+}
+
+template < typename mediator >
+void shy_logic_main_menu_letters_layout_row_rect < mediator > :: _proceed_with_row_rect ( )
+{
+    if ( platform_conditions :: whole_is_true ( _logic_main_menu_letters_layout_row_rect_state . requested ) )
+    {
+        _logic_main_menu_letters_layout_row_rect_state . requested = _platform_math_consts . get ( ) . whole_false ;
+        _reply_row_rect ( ) ;
+    }
+}
+
+template < typename mediator >
+void shy_logic_main_menu_letters_layout_row_rect < mediator > :: _reply_row_rect ( )
+{
     typename messages :: logic_main_menu_letters_layout_row_rect_reply reply_msg ;
-    reply_msg . row = msg . row ;
+    reply_msg . row = _logic_main_menu_letters_layout_row_rect_state . requested_row ;
     reply_msg . row_rect . left = _platform_math_consts . get ( ) . fract_minus_1 ;
     reply_msg . row_rect . right = _platform_math_consts . get ( ) . fract_1 ;
     reply_msg . row_rect . bottom = _platform_math_consts . get ( ) . fract_minus_1 ;
