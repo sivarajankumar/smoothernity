232a5e698b80bbe55c9803383d81be6a44e1c564
olegus8 <olegus8@localhost>
2010-10-01 12:01:42 +0000

    working on main menu

--- src/common/logic/main_menu/selection/animation/idle.hpp
+++ src/common/logic/main_menu/selection/animation/idle.hpp
@@ -20,12 +20,6 @@ class shy_logic_main_menu_selection_animation_idle
         _logic_main_menu_selection_animation_idle_consts_type ( ) ;
     public :
         num_fract position_z ;
-        num_fract horizontal_scale_min ;
-        num_fract horizontal_scale_max ;
-        num_fract horizontal_scale_period_in_seconds ;
-        num_fract vertical_scale_min ;
-        num_fract vertical_scale_max ;
-        num_fract vertical_scale_period_in_seconds ;
     } ;
 
     class _logic_main_menu_selection_animation_idle_transform_state_type
@@ -34,8 +28,6 @@ class shy_logic_main_menu_selection_animation_idle
         num_whole requested ;
         num_whole row_is_selected ;
         num_whole selected_row_index ;
-        num_fract horizontal_animation_scale ;
-        num_fract vertical_animation_scale ;
         vector_data position ;
         num_fract scale_x ;
         num_fract scale_y ;
@@ -50,13 +42,6 @@ class shy_logic_main_menu_selection_animation_idle
         rect row_rect ;
     } ;
 
-    class _logic_main_menu_update_state_type
-    {
-    public :
-        num_whole launch_permitted ;
-        num_fract time ;
-    } ;
-
 public :
     shy_logic_main_menu_selection_animation_idle ( ) ;
     void set_mediator ( typename platform_pointer :: template pointer < mediator > ) ;
@@ -65,22 +50,12 @@ public :
     void receive ( typename messages :: logic_main_menu_selection_animation_idle_row_selected ) ;
     void receive ( typename messages :: logic_main_menu_selection_animation_idle_void_selected ) ;
     void receive ( typename messages :: logic_main_menu_letters_layout_row_rect_reply ) ;
-    void receive ( typename messages :: logic_main_menu_update ) ;
-    void receive ( typename messages :: logic_main_menu_launch_permit ) ;
 private :
     shy_logic_main_menu_selection_animation_idle < mediator > & operator= ( const shy_logic_main_menu_selection_animation_idle < mediator > & ) ;
     void _proceed_with_transform ( ) ;
     void _obtain_row_rect ( ) ;
     void _received_row_rect ( ) ;
     void _reply_transform ( ) ;
-    void _compute_horizontal_animation_scale ( ) ;
-    void _compute_vertical_animation_scale ( ) ;
-    void _compute_animation_scale
-        ( num_fract & scale
-        , num_fract scale_min
-        , num_fract scale_max
-        , num_fract period_in_seconds
-        ) ;
     void _compute_row_rect_mesh_transform ( ) ;
     void _compute_empty_mesh_transform ( ) ;
 private :
@@ -91,19 +66,12 @@ private :
     
     _logic_main_menu_selection_animation_idle_transform_state_type _logic_main_menu_selection_animation_idle_transform_state ;
     _logic_main_menu_letters_layout_row_rect_state_type _logic_main_menu_letters_layout_row_rect_state ;
-    _logic_main_menu_update_state_type _logic_main_menu_update_state ;
 } ;
 
 template < typename mediator >
 shy_logic_main_menu_selection_animation_idle < mediator > :: _logic_main_menu_selection_animation_idle_consts_type :: _logic_main_menu_selection_animation_idle_consts_type ( )
 {
     platform_math :: make_num_fract ( position_z , - 3 , 1 ) ;
-    platform_math :: make_num_fract ( horizontal_scale_min , 19 , 20 ) ;
-    platform_math :: make_num_fract ( horizontal_scale_max , 20 , 20 ) ;
-    platform_math :: make_num_fract ( horizontal_scale_period_in_seconds , 2 , 1 ) ;
-    platform_math :: make_num_fract ( vertical_scale_min , 20 , 10 ) ;
-    platform_math :: make_num_fract ( vertical_scale_max , 23 , 10 ) ;
-    platform_math :: make_num_fract ( vertical_scale_period_in_seconds , 1 , 1 ) ;
 }
 
 template < typename mediator >
@@ -124,24 +92,6 @@ void shy_logic_main_menu_selection_animation_idle < mediator > :: receive ( type
     _mediator . get ( ) . logic_main_menu_selection_stateless_consts ( _logic_main_menu_selection_stateless_consts ) ;
     _mediator . get ( ) . platform_obj ( platform_obj ) ;
     _platform_math_consts = platform_obj . get ( ) . math_consts ;    
-}
-
-template < typename mediator >
-void shy_logic_main_menu_selection_animation_idle < mediator > :: receive ( typename messages :: logic_main_menu_launch_permit )
-{
-    _logic_main_menu_update_state . launch_permitted = _platform_math_consts . get ( ) . whole_true ;
-    _logic_main_menu_update_state . time = _platform_math_consts . get ( ) . fract_0 ;
-}
-
-template < typename mediator >
-void shy_logic_main_menu_selection_animation_idle < mediator > :: receive ( typename messages :: logic_main_menu_update )
-{
-    if ( platform_conditions :: whole_is_true ( _logic_main_menu_update_state . launch_permitted ) )
-    {
-        num_fract time_step ;
-        platform_math :: make_num_fract ( time_step , 1 , platform :: frames_per_second ) ;
-        platform_math :: add_to_fract ( _logic_main_menu_update_state . time , time_step ) ;
-    }
 }
 
 template < typename mediator >
@@ -207,11 +157,7 @@ template < typename mediator >
 void shy_logic_main_menu_selection_animation_idle < mediator > :: _received_row_rect ( )
 {
     if ( platform_conditions :: whole_is_true ( _logic_main_menu_selection_animation_idle_transform_state . row_is_selected ) )
-    {
-        _compute_horizontal_animation_scale ( ) ;
-        _compute_vertical_animation_scale ( ) ;
         _compute_row_rect_mesh_transform ( ) ;
-    }
     else
         _compute_empty_mesh_transform ( ) ;
     _reply_transform ( ) ;
@@ -245,8 +191,6 @@ void shy_logic_main_menu_selection_animation_idle < mediator > :: _compute_empty
 template < typename mediator >
 void shy_logic_main_menu_selection_animation_idle < mediator > :: _compute_row_rect_mesh_transform ( )
 {
-    num_fract horizontal_animation_scale ;
-    num_fract vertical_animation_scale ;
     num_fract zero ;
     num_fract half ;
     num_fract scale_x ;
@@ -260,8 +204,6 @@ void shy_logic_main_menu_selection_animation_idle < mediator > :: _compute_row_r
     rect row_rect ;
     vector_data position ;
     
-    horizontal_animation_scale = _logic_main_menu_selection_animation_idle_transform_state . horizontal_animation_scale ;
-    vertical_animation_scale = _logic_main_menu_selection_animation_idle_transform_state . vertical_animation_scale ;
     row_rect = _logic_main_menu_letters_layout_row_rect_state . row_rect ;
     mesh_size = _logic_main_menu_selection_stateless_consts . get ( ) . mesh_size ;
     zero = _platform_math_consts . get ( ) . fract_0 ;
@@ -271,9 +213,6 @@ void shy_logic_main_menu_selection_animation_idle < mediator > :: _compute_row_r
     
     platform_math :: div_fracts ( scale_x , width , mesh_size ) ;
     platform_math :: div_fracts ( scale_y , height , mesh_size ) ;
-
-    platform_math :: mul_fract_by ( scale_x , horizontal_animation_scale ) ;
-    platform_math :: mul_fract_by ( scale_y , vertical_animation_scale ) ;
 
     platform_math :: add_fracts ( pos_x , row_rect . right , row_rect . left ) ;
     platform_math :: add_fracts ( pos_y , row_rect . top , row_rect . bottom ) ;
@@ -286,55 +225,4 @@ void shy_logic_main_menu_selection_animation_idle < mediator > :: _compute_row_r
     _logic_main_menu_selection_animation_idle_transform_state . position = position ;
     _logic_main_menu_selection_animation_idle_transform_state . scale_x = scale_x ;
     _logic_main_menu_selection_animation_idle_transform_state . scale_y = scale_y ;
-}
-
-template < typename mediator >
-void shy_logic_main_menu_selection_animation_idle < mediator > :: _compute_horizontal_animation_scale ( )
-{
-    _compute_animation_scale
-        ( _logic_main_menu_selection_animation_idle_transform_state . horizontal_animation_scale
-        , _logic_main_menu_selection_animation_idle_consts . horizontal_scale_min
-        , _logic_main_menu_selection_animation_idle_consts . horizontal_scale_max
-        , _logic_main_menu_selection_animation_idle_consts . horizontal_scale_period_in_seconds
-        ) ;
-}
-
-template < typename mediator >
-void shy_logic_main_menu_selection_animation_idle < mediator > :: _compute_vertical_animation_scale ( )
-{
-    _compute_animation_scale
-        ( _logic_main_menu_selection_animation_idle_transform_state . vertical_animation_scale
-        , _logic_main_menu_selection_animation_idle_consts . vertical_scale_min
-        , _logic_main_menu_selection_animation_idle_consts . vertical_scale_max
-        , _logic_main_menu_selection_animation_idle_consts . vertical_scale_period_in_seconds
-        ) ;
-}
-
-template < typename mediator >
-void shy_logic_main_menu_selection_animation_idle < mediator > :: _compute_animation_scale 
-    ( num_fract & scale
-    , num_fract scale_min
-    , num_fract scale_max
-    , num_fract period_in_seconds
-    )
-{
-    num_fract time ;
-    num_fract phase ;
-    num_fract amplitude ;
-    num_fract offset ;
-    
-    time = _logic_main_menu_update_state . time ;
-    
-    platform_math :: div_fracts ( phase , time , period_in_seconds ) ;
-    platform_math :: mul_fract_by ( phase , _platform_math_consts . get ( ) . fract_2pi ) ;
-    
-    platform_math :: sub_fracts ( amplitude , scale_max , scale_min ) ;
-    platform_math :: div_fract_by ( amplitude , _platform_math_consts . get ( ) . fract_2 ) ;
-    
-    platform_math :: add_fracts ( offset , scale_max , scale_min ) ;
-    platform_math :: div_fract_by ( offset , _platform_math_consts . get ( ) . fract_2 ) ;
-    
-    platform_math :: sin ( scale , phase ) ;
-    platform_math :: mul_fract_by ( scale , amplitude ) ;
-    platform_math :: add_to_fract ( scale , offset ) ;        
 }
--- src/common/logic/main_menu/stateless.hpp
+++ src/common/logic/main_menu/stateless.hpp
@@ -120,7 +120,7 @@ void shy_logic_main_menu_stateless < mediator >
     _receivers . get ( ) . logic_main_menu_letters_animation_disappear . get ( ) . receive ( msg ) ;
     _receivers . get ( ) . logic_main_menu_letters_animation_idle . get ( ) . receive ( msg ) ;
     _receivers . get ( ) . logic_main_menu_letters_animation_selection . get ( ) . receive ( msg ) ;
-    _receivers . get ( ) . logic_main_menu_selection_animation_idle . get ( ) . receive ( msg ) ;
+    _receivers . get ( ) . logic_main_menu_selection_animation_idle_attention . get ( ) . receive ( msg ) ;
 }
 
 template < typename mediator >
@@ -178,7 +178,7 @@ void shy_logic_main_menu_stateless < mediator >
     _receivers . get ( ) . logic_main_menu_letters_meshes_creation_director . get ( ) . receive ( msg ) ;
     _receivers . get ( ) . logic_main_menu_selection_animation_appear . get ( ) . receive ( msg ) ;
     _receivers . get ( ) . logic_main_menu_selection_animation_disappear . get ( ) . receive ( msg ) ;
-    _receivers . get ( ) . logic_main_menu_selection_animation_idle . get ( ) . receive ( msg ) ;
+    _receivers . get ( ) . logic_main_menu_selection_animation_idle_attention . get ( ) . receive ( msg ) ;
     _receivers . get ( ) . logic_main_menu_selection_animation_push . get ( ) . receive ( msg ) ;
     _receivers . get ( ) . logic_main_menu_selection_animation_select . get ( ) . receive ( msg ) ;
     _receivers . get ( ) . logic_main_menu_selection_animation_unselect . get ( ) . receive ( msg ) ;
