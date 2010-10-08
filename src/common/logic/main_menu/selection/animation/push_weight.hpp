a763fc6cdd7f6fda8de7796f906d01b44ad5911b
olegus8 <olegus8@localhost>
2010-10-08 15:06:09 +0000

    working on controls

--- src/common/logic/main_menu/selection/animation/push_weight.hpp
+++ src/common/logic/main_menu/selection/animation/push_weight.hpp
@@ -9,7 +9,6 @@ class shy_logic_main_menu_selection_animation_push_weight
     typedef typename mediator :: platform :: platform_math :: num_fract num_fract ;
     typedef typename mediator :: platform :: platform_math :: num_whole num_whole ;
     typedef typename mediator :: platform :: platform_math_consts platform_math_consts ;
-    typedef typename mediator :: platform :: platform_mouse platform_mouse ;
     typedef typename mediator :: platform :: platform_pointer platform_pointer ;
     
     class _logic_main_menu_selection_animation_push_weight_consts_type
@@ -28,10 +27,19 @@ class shy_logic_main_menu_selection_animation_push_weight
     public :
         num_fract weight ;
     } ;
-    
+
+    class _logic_controls_state_type
+    {
+    public :
+        num_whole requested ;
+        num_whole replied ;
+        num_whole primary_button_down ;
+    } ;    
+
     class _logic_main_menu_update_state_type
     {
     public :
+        num_whole requested ;
         num_whole clicked ;
         num_fract time ;
     } ;
@@ -40,22 +48,26 @@ public :
     shy_logic_main_menu_selection_animation_push_weight ( ) ;
     void set_mediator ( typename platform_pointer :: template pointer < mediator > ) ;
     void receive ( typename messages :: init ) ;
+    void receive ( typename messages :: logic_controls_state_reply ) ;
     void receive ( typename messages :: logic_main_menu_selection_animation_push_weight_request ) ;
     void receive ( typename messages :: logic_main_menu_update ) ;
     void receive ( typename messages :: logic_main_menu_void_chosen ) ;
     void receive ( typename messages :: logic_main_menu_row_chosen ) ;
 private :
+    void _proceed_with_update ( ) ;
+    void _obtain_controls_state ( ) ;
+    void _controls_state_received ( ) ;
     void _compute_weight ( ) ;
     void _reply_weight ( ) ;
     shy_logic_main_menu_selection_animation_push_weight < mediator > & operator= ( const shy_logic_main_menu_selection_animation_push_weight < mediator > & ) ;
 private :
     typename platform_pointer :: template pointer < mediator > _mediator ;
-    typename platform_pointer :: template pointer < platform_mouse > _platform_mouse ;
     typename platform_pointer :: template pointer < const platform_math_consts > _platform_math_consts ;
     const _logic_main_menu_selection_animation_push_weight_consts_type _logic_main_menu_selection_animation_push_weight_consts ;
     
     _logic_main_menu_selection_animation_push_weight_state_type _logic_main_menu_selection_animation_push_weight_state ;
     _logic_main_menu_update_state_type _logic_main_menu_update_state ;
+    _logic_controls_state_type _logic_controls_state ;
 } ;
 
 template < typename mediator >
@@ -85,7 +97,6 @@ void shy_logic_main_menu_selection_animation_push_weight < mediator > :: receive
 {
     typename platform_pointer :: template pointer < const platform > platform_obj ;
     _mediator . get ( ) . platform_obj ( platform_obj ) ;
-    _platform_mouse = platform_obj . get ( ) . mouse ;
     _platform_math_consts = platform_obj . get ( ) . math_consts ;
     
     _logic_main_menu_update_state . clicked = _platform_math_consts . get ( ) . whole_false ;
@@ -116,16 +127,65 @@ void shy_logic_main_menu_selection_animation_push_weight < mediator > :: receive
 template < typename mediator >
 void shy_logic_main_menu_selection_animation_push_weight < mediator > :: receive ( typename messages :: logic_main_menu_update )
 {
-    num_whole mouse_button ;
-    _platform_mouse . get ( ) . left_button_down ( mouse_button ) ;
-    if ( platform_conditions :: whole_is_true ( mouse_button ) )
-        _logic_main_menu_update_state . clicked = _platform_math_consts . get ( ) . whole_true ;
-    if ( platform_conditions :: whole_is_true ( _logic_main_menu_update_state . clicked ) )
+    _logic_main_menu_update_state . requested = _platform_math_consts . get ( ) . whole_true ;
+    _proceed_with_update ( ) ;
+}
+
+template < typename mediator >
+void shy_logic_main_menu_selection_animation_push_weight < mediator > :: receive ( typename messages :: logic_controls_state_reply msg )
+{
+    if ( platform_conditions :: whole_is_true ( _logic_controls_state . requested ) )
     {
-        num_fract time_step ;
-        platform_math :: make_num_fract ( time_step , 1 , platform :: frames_per_second ) ;
-        platform_math :: add_to_fract ( _logic_main_menu_update_state . time , time_step ) ;
+        _logic_controls_state . requested = _platform_math_consts . get ( ) . whole_false ;
+        _logic_controls_state . replied = _platform_math_consts . get ( ) . whole_true ;
+        _logic_controls_state . primary_button_down = msg . primary_button_down ;
+        _proceed_with_update ( ) ;
     }
+}
+
+template < typename mediator >
+void shy_logic_main_menu_selection_animation_push_weight < mediator > :: _proceed_with_update ( )
+{
+    if ( platform_conditions :: whole_is_true ( _logic_main_menu_update_state . requested ) )
+    {
+        _logic_main_menu_update_state . requested = _platform_math_consts . get ( ) . whole_false ;
+        _obtain_controls_state ( ) ;
+    }
+    if ( platform_conditions :: whole_is_true ( _logic_controls_state . replied ) )
+    {
+        _logic_controls_state . replied = _platform_math_consts . get ( ) . whole_false ;
+        _controls_state_received ( ) ;
+    }
+}
+
+template < typename mediator >
+void shy_logic_main_menu_selection_animation_push_weight < mediator > :: _obtain_controls_state ( )
+{
+    _logic_controls_state . requested = _platform_math_consts . get ( ) . whole_true ;
+    _mediator . get ( ) . send ( typename messages :: logic_controls_state_request ( ) ) ;
+}
+
+template < typename mediator >
+void shy_logic_main_menu_selection_animation_push_weight < mediator > :: _controls_state_received ( )
+{
+    num_fract time ;
+    num_fract time_step ;
+    num_whole clicked ;
+    num_whole primary_button_down ;
+
+    time = _logic_main_menu_update_state . time ;
+    clicked = _logic_main_menu_update_state . clicked ;
+    primary_button_down = _logic_controls_state . primary_button_down ;
+    platform_math :: make_num_fract ( time_step , 1 , platform :: frames_per_second ) ;
+
+    if ( platform_conditions :: whole_is_true ( primary_button_down ) )
+        clicked = _platform_math_consts . get ( ) . whole_true ;
+
+    if ( platform_conditions :: whole_is_true ( clicked ) )
+        platform_math :: add_to_fract ( time , time_step ) ;
+
+    _logic_main_menu_update_state . time = time ;
+    _logic_main_menu_update_state . clicked = clicked ;
 }
 
 template < typename mediator >
