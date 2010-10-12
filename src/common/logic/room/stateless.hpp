0c8325477dff64311ed68dacd4f882c77d9456d5
olegus8 <olegus8@localhost>
2010-10-12 09:26:05 +0000

    working on room

---
+++ src/common/logic/room/stateless.hpp
@@ -0,0 +1,73 @@
+template < typename mediator >
+class shy_logic_room_stateless
+{
+    typedef typename mediator :: platform :: platform_pointer platform_pointer ;
+public :
+    class logic_room_messages
+    {
+    public :
+        class logic_room_creation_permit { } ;
+        class logic_room_launch_permit { } ;
+        class logic_room_render { } ;
+        class logic_room_update { } ;
+    } ;
+
+    template < typename receivers >
+    class logic_room_sender
+    {
+    public :
+        void set_receivers ( typename platform_pointer :: template pointer < const receivers > ) ;
+        void send ( typename logic_room_messages :: logic_room_creation_permit ) ;
+        void send ( typename logic_room_messages :: logic_room_launch_permit ) ;
+        void send ( typename logic_room_messages :: logic_room_render ) ;
+        void send ( typename logic_room_messages :: logic_room_update ) ;
+    private :
+        typename platform_pointer :: template pointer < const receivers > _receivers ;
+    } ;
+} ;
+
+template < typename mediator >
+template < typename receivers >
+void shy_logic_room_stateless < mediator >
+:: logic_room_sender < receivers >
+:: set_receivers ( typename platform_pointer :: template pointer < const receivers > arg_receivers )
+{
+    _receivers = arg_receivers ;
+}
+
+template < typename mediator >
+template < typename receivers >
+void shy_logic_room_stateless < mediator >
+:: logic_room_sender < receivers >
+:: send ( typename logic_room_messages :: logic_room_creation_permit msg )
+{
+    _receivers . get ( ) . logic_room . get ( ) . receive ( msg ) ;
+}
+
+template < typename mediator >
+template < typename receivers >
+void shy_logic_room_stateless < mediator >
+:: logic_room_sender < receivers >
+:: send ( typename logic_room_messages :: logic_room_launch_permit msg )
+{
+    _receivers . get ( ) . logic_room . get ( ) . receive ( msg ) ;
+}
+
+template < typename mediator >
+template < typename receivers >
+void shy_logic_room_stateless < mediator >
+:: logic_room_sender < receivers >
+:: send ( typename logic_room_messages :: logic_room_render msg )
+{
+    _receivers . get ( ) . logic_room . get ( ) . receive ( msg ) ;
+}
+
+template < typename mediator >
+template < typename receivers >
+void shy_logic_room_stateless < mediator >
+:: logic_room_sender < receivers >
+:: send ( typename logic_room_messages :: logic_room_update msg )
+{
+    _receivers . get ( ) . logic_room . get ( ) . receive ( msg ) ;
+}
+
