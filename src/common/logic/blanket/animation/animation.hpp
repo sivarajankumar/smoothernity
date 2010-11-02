4c8c2c9200026247869ed055a87262ea729e2bfb
olegus8 <olegus8@localhost>
2010-11-02 10:21:40 +0000

    working on blanket disappear animation

--- src/common/logic/blanket/animation/animation.hpp
+++ src/common/logic/blanket/animation/animation.hpp
@@ -5,6 +5,7 @@ class shy_logic_blanket_animation
     typedef typename mediator :: platform platform ;
     typedef typename mediator :: platform :: platform_math platform_math ;
     typedef typename mediator :: platform :: platform_math :: num_fract num_fract ;
+    typedef typename mediator :: platform :: platform_math :: num_whole num_whole ;
     typedef typename mediator :: platform :: platform_math_consts platform_math_consts ;
     typedef typename mediator :: platform :: platform_matrix platform_matrix ;
     typedef typename mediator :: platform :: platform_matrix :: matrix_data matrix_data ;
@@ -20,17 +21,42 @@ class shy_logic_blanket_animation
         num_fract origin_z ;
     } ;
 
+    class _logic_blanket_animation_transform_state_type
+    {
+    public :
+        num_whole requested ;
+        matrix_data transform ;
+    } ;
+
+    class _logic_blanket_animation_disappear_transform_state_type
+    {
+    public :
+        num_whole requested ;
+        num_whole replied ;
+        num_fract scale ;
+        num_fract rotation ;
+    } ;
+
 public :
     shy_logic_blanket_animation ( ) ;
     void set_mediator ( typename platform_pointer :: template pointer < mediator > ) ;
     void receive ( typename messages :: init ) ;
     void receive ( typename messages :: logic_blanket_animation_transform_request ) ;
+    void receive ( typename messages :: logic_blanket_animation_disappear_transform_reply ) ;
 private :
     shy_logic_blanket_animation < mediator > & operator= ( const shy_logic_blanket_animation < mediator > & ) ;
+    void _proceed_with_transform ( ) ;
+    void _request_disappear_transform ( ) ;
+    void _reply_computed_transform ( ) ;
+    void _compute_transform ( ) ;
+    void _reply_transform ( ) ;
 private :
     typename platform_pointer :: template pointer < mediator > _mediator ;
     typename platform_pointer :: template pointer < const platform_math_consts > _platform_math_consts ;
     const _logic_blanket_animation_consts_type _logic_blanket_animation_consts ;
+
+    _logic_blanket_animation_transform_state_type _logic_blanket_animation_transform_state ;
+    _logic_blanket_animation_disappear_transform_state_type _logic_blanket_animation_disappear_transform_state ;
 } ;
 
 template < typename mediator >
@@ -63,6 +89,34 @@ void shy_logic_blanket_animation < mediator > :: receive ( typename messages :: 
 template < typename mediator >
 void shy_logic_blanket_animation < mediator > :: receive ( typename messages :: logic_blanket_animation_transform_request )
 {
+    _reply_computed_transform ( ) ;
+}
+
+template < typename mediator >
+void shy_logic_blanket_animation < mediator > :: receive ( typename messages :: logic_blanket_animation_disappear_transform_reply msg )
+{
+}
+
+template < typename mediator >
+void shy_logic_blanket_animation < mediator > :: _proceed_with_transform ( )
+{
+}
+
+template < typename mediator >
+void shy_logic_blanket_animation < mediator > :: _request_disappear_transform ( )
+{
+}
+
+template < typename mediator >
+void shy_logic_blanket_animation < mediator > :: _reply_computed_transform ( )
+{
+    _compute_transform ( ) ;
+    _reply_transform ( ) ;
+}
+
+template < typename mediator >
+void shy_logic_blanket_animation < mediator > :: _compute_transform ( )
+{
     num_fract origin_x ;
     num_fract origin_y ;
     num_fract origin_z ;
@@ -75,8 +129,14 @@ void shy_logic_blanket_animation < mediator > :: receive ( typename messages :: 
     platform_matrix :: identity ( transform ) ;
     platform_matrix :: set_origin ( transform , origin_x , origin_y , origin_z ) ;
 
+    _logic_blanket_animation_transform_state . transform = transform ;
+}
+
+template < typename mediator >
+void shy_logic_blanket_animation < mediator > :: _reply_transform ( )
+{
     typename messages :: logic_blanket_animation_transform_reply msg ;
-    msg . transform = transform ;
+    msg . transform = _logic_blanket_animation_transform_state . transform ;
     _mediator . get ( ) . send ( msg ) ;
 }
 
--- src/common/logic/blanket/animation/stateless.hpp
+++ src/common/logic/blanket/animation/stateless.hpp
@@ -1,12 +1,15 @@
 template < typename mediator >
 class shy_logic_blanket_animation_stateless
 {
+    typedef typename mediator :: platform :: platform_math :: num_fract num_fract ;
     typedef typename mediator :: platform :: platform_matrix :: matrix_data matrix_data ;
     typedef typename mediator :: platform :: platform_pointer platform_pointer ;
 public :
     class logic_blanket_animation_messages
     {
     public :
+        class logic_blanket_animation_disappear_transform_reply { public : num_fract scale ; num_fract rotation ; } ;
+        class logic_blanket_animation_disappear_transform_request { } ;
         class logic_blanket_animation_transform_reply { public : matrix_data transform ; } ;
         class logic_blanket_animation_transform_request { } ;
     } ;
@@ -16,6 +19,8 @@ public :
     {
     public :
         void set_receivers ( typename platform_pointer :: template pointer < const receivers > ) ;
+        void send ( typename logic_blanket_animation_messages :: logic_blanket_animation_disappear_transform_reply ) ;
+        void send ( typename logic_blanket_animation_messages :: logic_blanket_animation_disappear_transform_request ) ;
         void send ( typename logic_blanket_animation_messages :: logic_blanket_animation_transform_reply ) ;
         void send ( typename logic_blanket_animation_messages :: logic_blanket_animation_transform_request ) ;
     private :
@@ -30,6 +35,24 @@ void shy_logic_blanket_animation_stateless < mediator >
 :: set_receivers ( typename platform_pointer :: template pointer < const receivers > arg_receivers )
 {
     _receivers = arg_receivers ;
+}
+
+template < typename mediator >
+template < typename receivers >
+void shy_logic_blanket_animation_stateless < mediator >
+:: logic_blanket_animation_sender < receivers >
+:: send ( typename logic_blanket_animation_messages :: logic_blanket_animation_disappear_transform_reply msg )
+{
+    _receivers . get ( ) . logic_blanket_animation . get ( ) . receive ( msg ) ;
+}
+
+template < typename mediator >
+template < typename receivers >
+void shy_logic_blanket_animation_stateless < mediator >
+:: logic_blanket_animation_sender < receivers >
+:: send ( typename logic_blanket_animation_messages :: logic_blanket_animation_disappear_transform_request msg )
+{
+    _receivers . get ( ) . logic_blanket_animation_disappear . get ( ) . receive ( msg ) ;
 }
 
 template < typename mediator >
