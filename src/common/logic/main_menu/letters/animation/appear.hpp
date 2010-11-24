72ddd2eeced78336506fb1bb0859b74430256f66
olegus8 <olegus8@localhost>
2010-11-24 16:49:24 +0000

    working on moving consts to stateless

--- src/common/logic/main_menu/letters/animation/appear.hpp
+++ src/common/logic/main_menu/letters/animation/appear.hpp
@@ -2,6 +2,7 @@ template < typename mediator >
 class shy_logic_main_menu_letters_animation_appear
 {
     typedef typename mediator :: engine_math engine_math ;
+    typedef typename mediator :: logic_main_menu_letters_animation_stateless :: logic_main_menu_letters_animation_stateless_consts_type logic_main_menu_letters_animation_stateless_consts_type ;
     typedef typename mediator :: messages messages ;
     typedef typename mediator :: platform platform ;
     typedef typename mediator :: platform :: platform_conditions platform_conditions ;
@@ -10,20 +11,6 @@ class shy_logic_main_menu_letters_animation_appear
     typedef typename mediator :: platform :: platform_math :: num_whole num_whole ;
     typedef typename mediator :: platform :: platform_math_consts platform_math_consts ;
     typedef typename mediator :: platform :: platform_pointer platform_pointer ;
-
-    class _logic_main_menu_letters_animation_appear_consts_type
-    {
-    public :
-        _logic_main_menu_letters_animation_appear_consts_type ( ) ;
-    public :
-        num_fract time_from_begin_to_middle_in_seconds ;
-        num_fract time_from_middle_to_end_in_seconds ;
-        num_fract delay_per_col_in_seconds ;
-        num_fract delay_per_row_in_seconds ;
-        num_fract scale_begin ;
-        num_fract scale_middle ;
-        num_fract scale_end ;
-    } ;
 
     class _logic_main_menu_letters_animation_appear_transform_state_type
     {
@@ -63,23 +50,11 @@ private :
 private :
     typename platform_pointer :: template pointer < mediator > _mediator ;
     typename platform_pointer :: template pointer < const platform_math_consts > _platform_math_consts ;
-    const _logic_main_menu_letters_animation_appear_consts_type _logic_main_menu_letters_animation_appear_consts ;
+    typename platform_pointer :: template pointer < const logic_main_menu_letters_animation_stateless_consts_type > _logic_main_menu_letters_animation_stateless_consts ;
     
     _logic_main_menu_letters_animation_appear_transform_state_type _logic_main_menu_letters_animation_appear_transform_state ;
     _logic_main_menu_update_state_type _logic_main_menu_update_state ;
 } ;
-
-template < typename mediator >
-shy_logic_main_menu_letters_animation_appear < mediator > :: _logic_main_menu_letters_animation_appear_consts_type :: _logic_main_menu_letters_animation_appear_consts_type ( )
-{
-    platform_math :: make_num_fract ( time_from_begin_to_middle_in_seconds , 1 , 10 ) ;
-    platform_math :: make_num_fract ( time_from_middle_to_end_in_seconds , 7 , 10 ) ;
-    platform_math :: make_num_fract ( delay_per_col_in_seconds , 2 , 100 ) ;
-    platform_math :: make_num_fract ( delay_per_row_in_seconds , 5 , 100 ) ;
-    platform_math :: make_num_fract ( scale_begin , 0 , 1 ) ;
-    platform_math :: make_num_fract ( scale_middle , 3 , 2 ) ;
-    platform_math :: make_num_fract ( scale_end , 1 , 1 ) ;
-}
 
 template < typename mediator >
 shy_logic_main_menu_letters_animation_appear < mediator > :: shy_logic_main_menu_letters_animation_appear ( )
@@ -97,6 +72,7 @@ void shy_logic_main_menu_letters_animation_appear < mediator > :: receive ( type
 {
     typename platform_pointer :: template pointer < const platform > platform_obj ;
     _mediator . get ( ) . platform_obj ( platform_obj ) ;
+    _mediator . get ( ) . logic_main_menu_letters_animation_stateless_consts ( _logic_main_menu_letters_animation_stateless_consts ) ;
     _platform_math_consts = platform_obj . get ( ) . math_consts ;
 }
 
@@ -159,8 +135,8 @@ void shy_logic_main_menu_letters_animation_appear < mediator > :: _compute_delay
     
     platform_math :: make_fract_from_whole ( row , _logic_main_menu_letters_animation_appear_transform_state . row ) ;
     platform_math :: make_fract_from_whole ( col , _logic_main_menu_letters_animation_appear_transform_state . col ) ;
-    delay_per_row = _logic_main_menu_letters_animation_appear_consts . delay_per_row_in_seconds ;
-    delay_per_col = _logic_main_menu_letters_animation_appear_consts . delay_per_col_in_seconds ;
+    delay_per_row = _logic_main_menu_letters_animation_stateless_consts . get ( ) . appear_delay_per_row_in_seconds ;
+    delay_per_col = _logic_main_menu_letters_animation_stateless_consts . get ( ) . appear_delay_per_col_in_seconds ;
     platform_math :: mul_fracts ( delay_for_row , delay_per_row , row ) ;
     platform_math :: mul_fracts ( delay_for_col , delay_per_col , col ) ;
     platform_math :: add_fracts ( delay , delay_for_row , delay_for_col ) ;
@@ -179,8 +155,8 @@ void shy_logic_main_menu_letters_animation_appear < mediator > :: _compute_time 
     num_fract time_from_middle_to_end ;
     
     delay = _logic_main_menu_letters_animation_appear_transform_state . delay ;
-    time_from_begin_to_middle = _logic_main_menu_letters_animation_appear_consts . time_from_begin_to_middle_in_seconds ;
-    time_from_middle_to_end = _logic_main_menu_letters_animation_appear_consts . time_from_middle_to_end_in_seconds ;
+    time_from_begin_to_middle = _logic_main_menu_letters_animation_stateless_consts . get ( ) . appear_time_from_begin_to_middle_in_seconds ;
+    time_from_middle_to_end = _logic_main_menu_letters_animation_stateless_consts . get ( ) . appear_time_from_middle_to_end_in_seconds ;
     time_begin = delay ;
     platform_math :: add_fracts ( time_middle , time_begin , time_from_begin_to_middle ) ;
     platform_math :: add_fracts ( time_end , time_middle , time_from_middle_to_end ) ;
@@ -206,9 +182,9 @@ void shy_logic_main_menu_letters_animation_appear < mediator > :: _compute_trans
     time_middle = _logic_main_menu_letters_animation_appear_transform_state . time_middle ;
     time_end = _logic_main_menu_letters_animation_appear_transform_state . time_end ;
     time = _logic_main_menu_update_state . time ;
-    scale_begin = _logic_main_menu_letters_animation_appear_consts . scale_begin ;
-    scale_middle = _logic_main_menu_letters_animation_appear_consts . scale_middle ;
-    scale_end = _logic_main_menu_letters_animation_appear_consts . scale_end ;
+    scale_begin = _logic_main_menu_letters_animation_stateless_consts . get ( ) . appear_scale_begin ;
+    scale_middle = _logic_main_menu_letters_animation_stateless_consts . get ( ) . appear_scale_middle ;
+    scale_end = _logic_main_menu_letters_animation_stateless_consts . get ( ) . appear_scale_end ;
     
     engine_math :: hard_attack_easy_decay
         ( scale
