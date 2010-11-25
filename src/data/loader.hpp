b86019dfafe4055deb1ac657192fd19f478adb9c
olegus8 <olegus8@localhost>
2010-11-25 19:51:36 +0000

    data reflection binding summary

--- src/data/loader.hpp
+++ src/data/loader.hpp
@@ -16,19 +16,35 @@ class shy_data_loader
         class reflection_binder
         {
         public :
+            reflection_binder ( )
+            : _modules ( 0 )
+            , _consts ( 0 )
+            {
+            }
             void module ( const char * name )
             {
+                ++ _modules ;
                 std :: cout << std :: endl ;
                 std :: cout << std :: string ( "consts " ) << std :: string ( name ) << std :: endl ;
             }
             void bind ( const char * name , const num_fract & value )
             {
+                ++ _consts ;
                 std :: cout << std :: string ( "    " ) << std :: string ( name ) << std :: endl ;
             }
             void bind ( const char * name , const num_whole & value )
             {
+                ++ _consts ;
                 std :: cout << std :: string ( "    " ) << std :: string ( name ) << std :: endl ;
             }
+            void report ( )
+            {
+                std :: cout << std :: endl ;
+                std :: cout << std :: string ( "summary: " ) << _consts << std::string ( " consts in " ) << _modules << std :: string ( " modules" ) << std :: endl ;
+            }
+        private :
+            int _modules ;
+            int _consts ;
         } ;
     } ;
 
@@ -40,6 +56,7 @@ public :
         arg_facade . mediator_obj ( mediator_obj ) ;
         platform_pointer :: bind ( reflection_binder_obj , _reflection_binder ) ;
         _reflection . bind_all ( mediator_obj , reflection_binder_obj ) ;
+        _reflection_binder . report ( ) ;
     }
 
 private :
