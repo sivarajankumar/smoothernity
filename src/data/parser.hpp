cc9b14fc1b17dd2bd7f9bf51c9fd79c62539bc14
olegus8 <olegus8@localhost>
2010-12-12 20:23:36 +0000

    working on fsm parser

--- src/data/parser.hpp
+++ src/data/parser.hpp
@@ -107,6 +107,7 @@ class shy_data_parser
         _state_reading_next_condition_in_group ,
         _state_reading_parametric_condition_token ,
         _state_reading_state_condition_machine_name ,
+        _state_reading_state_condition_is_token ,
         _state_reading_command_condition_command_name ,
         _state_reading_transition_state_name ,
         _state_reading_transition_if_token
@@ -156,6 +157,7 @@ private :
     void _handle_state_reading_next_condition_in_group ( ) ;
     void _handle_state_reading_parametric_condition_token ( ) ;
     void _handle_state_reading_state_condition_machine_name ( ) ;
+    void _handle_state_reading_state_condition_is_token ( ) ;
     void _handle_state_reading_command_condition_command_name ( ) ;
     void _handle_state_reading_transition_state_name ( ) ;
     void _handle_state_reading_transition_if_token ( ) ;
@@ -175,7 +177,9 @@ private :
     void _store_transition_state_name ( std :: string ) ;
     void _store_input_actions_conditions ( ) ;
     void _store_transition_conditions ( ) ;
+    void _store_condition_group ( ) ;
     void _store_input_condition ( std :: string ) ;
+    void _store_state_condition_machine_name ( std :: string ) ;
     void _select_entry_actions_container ( ) ;
     void _select_exit_actions_container ( ) ;
     void _select_input_actions_container ( ) ;
@@ -299,6 +303,8 @@ void shy_data_parser < data_parser_types > :: parse ( std :: string line )
             _handle_state_reading_parametric_condition_token ( ) ;
         else if ( _state == _state_reading_state_condition_machine_name )
             _handle_state_reading_state_condition_machine_name ( ) ;
+        else if ( _state == _state_reading_state_condition_is_token )
+            _handle_state_reading_state_condition_is_token ( ) ;
         else if ( _state == _state_reading_command_condition_command_name )
             _handle_state_reading_command_condition_command_name ( ) ;
         else if ( _state == _state_reading_transition_state_name )
@@ -819,6 +825,7 @@ void shy_data_parser < data_parser_types > :: _handle_state_reading_next_conditi
     }
     else if ( _token_class == _token_class_brace_close )
     {
+        _store_condition_group ( ) ;
         _read_next_token ( ) ;
         _state = _state_reading_next_condition_group ;
     }
@@ -851,6 +858,22 @@ void shy_data_parser < data_parser_types > :: _handle_state_reading_parametric_c
 
 template < typename data_parser_types >
 void shy_data_parser < data_parser_types > :: _handle_state_reading_state_condition_machine_name ( )
+{
+    if ( _token_class == _token_class_identifier )
+    {
+        _store_state_condition_machine_name ( _token ) ;
+        _read_next_token ( ) ;
+        _state = _state_reading_state_condition_is_token ;
+    }
+    else
+    {
+        _store_error ( _consts :: error_expected_machine_name_instead_of ( _token ) ) ;
+        _state = _state_error ;
+    }
+}
+
+template < typename data_parser_types >
+void shy_data_parser < data_parser_types > :: _handle_state_reading_state_condition_is_token ( )
 {
 }
 
@@ -1147,7 +1170,17 @@ void shy_data_parser < data_parser_types > :: _store_transition_conditions ( )
 }
 
 template < typename data_parser_types >
+void shy_data_parser < data_parser_types > :: _store_condition_group ( )
+{
+}
+
+template < typename data_parser_types >
 void shy_data_parser < data_parser_types > :: _store_input_condition ( std :: string )
+{
+}
+
+template < typename data_parser_types >
+void shy_data_parser < data_parser_types > :: _store_state_condition_machine_name ( std :: string )
 {
 }
 
