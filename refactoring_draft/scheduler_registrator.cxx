18ea85572873171a01e08731c746996c44558761
olegus8 <olegus8@localhost>
2010-12-26 20:10:11 +0000

    working on major refactoring draft

---
+++ refactoring_draft/scheduler_registrator.cxx
@@ -0,0 +1,8 @@
+#include "scheduler_registrator.hpp"
+
+void my_scheduler_registrator :: perform ( )
+{
+    so_called_module1 :: register_in_scheduler ( ) ;
+    so_called_module2 :: register_in_scheduler ( ) ;
+}
+
---
+++ refactoring_draft/scheduler_registrator.hpp
@@ -0,0 +1,11 @@
+#ifndef _scheduler_registrator_
+#define _scheduler_registrator_
+
+class my_scheduler_registrator
+{
+public :
+    static void perform ( ) ;
+} ;
+
+#endif
+
