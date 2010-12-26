6ad3020fccfc2ed86be8ce2595d701391cd3af01
olegus8 <olegus8@localhost>
2010-12-26 20:19:35 +0000

    working on major refactoring draft

--- refactoring_draft/platform.hpp
+++ refactoring_draft/platform.hpp
@@ -1,14 +1,14 @@
 #ifndef _platform_included_
 #define _platform_included_
 
-#ifdef build_for_iphone
+#if build_for_iphone
     #include "iphone_platform.hpp"
     typedef iphone_platform so_called_platform ;
-#endif
-
-#ifdef build_for_macosx
+#elif build_for_macosx
     #include "macosx_platform.hpp"
     typedef macosx_platform so_called_platform ;
+#else
+    #error build target must be specified
 #endif
 
 #endif
