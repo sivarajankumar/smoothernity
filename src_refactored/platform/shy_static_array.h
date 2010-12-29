43738f928a781cd1c520f8db24a8184e73cdf485
olegus8 <olegus8@localhost>
2010-12-29 19:36:37 +0000

    working on mac os x main application refactoring

--- src_refactored/platform/shy_static_array.h
+++ src_refactored/platform/shy_static_array.h
@@ -1,9 +1,35 @@
 #ifndef _shy_platform_static_array_included
 #define _shy_platform_static_array_included
 
+class shy_platform_static_array_insider ;
+
 class shy_platform_static_array
 {
+    typedef so_called_platform_math :: const_int_32 const_int_32 ;
+    typedef so_called_platform_math :: num_whole num_whole ;
+public :
+    template < typename data_type , const_int_32 size >
+    class static_array
+    {
+        friend class shy_platform_static_array ;
+        friend class shy_platform_static_array_insider ;
+    private :
+        typedef data_type _data_type ;
+        static const_int_32 _array_size = size ;
+        data_type _elements [ _array_size ] ;
+    } ;
+public :
+    template < typename array_type , typename pointer_type >
+    static void element_ptr ( pointer_type & , array_type & , num_whole ) ;
 } ;
+
+template < typename array_type , typename pointer_type >
+void shy_platform_static_array :: element_ptr ( pointer_type & element_ptr , array_type & array , num_whole index )
+{
+    int index_int = 0 ;
+    so_called_platform_math_insider :: num_whole_value_get ( index_int , index ) ;
+    so_called_platform_pointer :: bind ( element_ptr , array . _elements [ index_int ] ) ;
+}
 
 #endif
 
