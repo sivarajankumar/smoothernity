f7af945b026a04572ccd26b29b09a71610e6783d
olegus8 <olegus8@localhost>
2010-12-29 19:39:20 +0000

    working on mac os x main application refactoring

--- src_refactored/platform/shy_static_array_insider.h
+++ src_refactored/platform/shy_static_array_insider.h
@@ -3,6 +3,33 @@
 
 class shy_platform_static_array_insider
 {
+public :
+    template < typename static_array >
+    static void elements_ptr ( typename static_array :: _data_type * & , static_array & ) ;
+    
+    template < typename static_array >
+    static void elements_ptr ( const typename static_array :: _data_type * & , const static_array & ) ;
+    
+    template < typename static_array >
+    static void elements_count ( int & ) ;
 } ;
+
+template < typename static_array >
+void shy_platform_static_array_insider :: elements_ptr ( typename static_array :: _data_type * & result , static_array & array )
+{
+    result = array . _elements ;
+}
+
+template < typename static_array >
+void shy_platform_static_array_insider :: elements_ptr ( const typename static_array :: _data_type * & result , const static_array & array )
+{
+    result = array . _elements ;
+}
+
+template < typename static_array >
+void shy_platform_static_array_insider :: elements_count ( int & count )
+{
+    count = int ( static_array :: _array_size ) ;
+}
 
 #endif
