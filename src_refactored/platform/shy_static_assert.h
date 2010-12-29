eba6ca96188802dceef19f934e7c90198a05b60c
olegus8 <olegus8@localhost>
2010-12-29 19:40:33 +0000

    working on mac os x main application refactoring

--- src_refactored/platform/shy_static_assert.h
+++ src_refactored/platform/shy_static_assert.h
@@ -3,6 +3,12 @@
 
 class shy_platform_static_assert
 {
+public :
+    template < bool condition >
+    class shy_static_assert
+    {
+        char _data [ condition ? 1 : - 1 ] ;
+    } ;
 } ;
 
 #endif
