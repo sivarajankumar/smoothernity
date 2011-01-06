5cb0b3ba24d8554ad447ae16b73da95083ece4b7
olegus8 <olegus8@localhost>
2011-01-06 18:23:27 +0000

    working on common engine refactoring

--- src_refactored/common/engine/render/shy_sender_blend_src_alpha_dst_one_minus_alpha.hpp
+++ src_refactored/common/engine/render/shy_sender_blend_src_alpha_dst_one_minus_alpha.hpp
@@ -1,5 +1,3 @@
-#include "shy_sender_blend_src_alpha_dst_one_minus_alpha.h"
-
 void shy_sender_common_engine_render_blend_src_alpha_dst_one_minus_alpha :: send ( so_called_message_common_engine_render_blend_src_alpha_dst_one_minus_alpha msg )
 {
     so_called_common_engine_render :: receive ( msg ) ;
--- src_refactored/common/engine/render/shy_sender_blend_src_alpha_dst_one_minus_alpha_injections.cpp
+++ src_refactored/common/engine/render/shy_sender_blend_src_alpha_dst_one_minus_alpha_injections.cpp
@@ -1,3 +1,5 @@
+#include "shy_sender_blend_src_alpha_dst_one_minus_alpha_injections.h"
+
 #include "shy_render_injections.h"
 
 #include "shy_sender_blend_src_alpha_dst_one_minus_alpha.hpp"
