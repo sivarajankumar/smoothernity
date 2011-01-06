f053767afcc3686bd7e037e2c8e966ebd10b4ed1
olegus8 <olegus8@localhost>
2011-01-06 18:11:29 +0000

    working on common engine refactoring

--- src_refactored/common/engine/shy_camera_stateless_injections.cpp
+++ src_refactored/common/engine/shy_camera_stateless_injections.cpp
@@ -1,11 +1,7 @@
-#include "../../injections/shy_type_platform_math_const_int_32.h"
-#include "../../injections/shy_type_platform_math_num_fract.h"
-#include "../../injections/shy_type_platform_math_num_whole.h"
-#include "../../injections/shy_platform_math.h"
-#include "../../injections/shy_type_platform_vector_data.h"
-#include "../../injections/shy_platform_vector.h"
-#include "../../injections/shy_type_platform_matrix_data.h"
+#include "shy_camera_stateless_injections.h"
+
 #include "../../injections/shy_platform_matrix.h"
+#include "../../injections/shy_platform_vector.h"
 
 #include "shy_camera_stateless.hpp"
 
