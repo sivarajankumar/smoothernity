5eda2c5b0514c9738e12e1131ac67dd20e69dd6f
olegus8 <olegus8@localhost>
2011-01-10 13:09:55 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/shy_render_injections.h
+++ src_refactored/common/engine/render/shy_render_injections.h
@@ -3,7 +3,7 @@
 
 #include "message/aspect_request/shy_aspect_request_injections.h"
 #include "message/blend_disable/shy_blend_disable_injections.h"
-#include "message/shy_blend_src_alpha_dst_one_minus_alpha_injections.h"
+#include "message/blend_src_alpha_dst_one_minus_alpha/shy_blend_src_alpha_dst_one_minus_alpha_injections.h"
 #include "message/shy_clear_screen_injections.h"
 #include "message/shy_disable_depth_test_injections.h"
 #include "message/shy_enable_depth_test_injections.h"
