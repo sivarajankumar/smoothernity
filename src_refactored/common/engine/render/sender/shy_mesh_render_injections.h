e43589685595298f8aa2016c7268f1506e2853ee
olegus8 <olegus8@localhost>
2011-01-10 19:37:28 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/shy_mesh_render_injections.h
+++ src_refactored/common/engine/render/sender/shy_mesh_render_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_sender_common_engine_render_mesh_render_injections_included
 #define _shy_sender_common_engine_render_mesh_render_injections_included
 
-#include "../message/shy_mesh_render_injections.h"
+#include "../message/mesh_render/shy_mesh_render_injections.h"
 
 #include "shy_mesh_render.h"
 
