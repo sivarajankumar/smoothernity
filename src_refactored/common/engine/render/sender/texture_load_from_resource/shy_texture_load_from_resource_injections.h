436ecbe7b5a5215c920327a64c1825359de84786
olegus8 <olegus8@localhost>
2011-01-11 19:09:59 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/texture_load_from_resource/shy_texture_load_from_resource_injections.h
+++ src_refactored/common/engine/render/sender/texture_load_from_resource/shy_texture_load_from_resource_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_texture_load_from_resource_injections_included
 #define _shy_sender_common_engine_render_texture_load_from_resource_injections_included
 
-#include "../message/texture_load_from_resource/shy_texture_load_from_resource_injections.h"
+#include "../../message/texture_load_from_resource/shy_texture_load_from_resource_injections.h"
 
-#include "shy_texture_load_from_resource.h"
+#include "./shy_texture_load_from_resource.h"
 
 typedef shy_sender_common_engine_render_texture_load_from_resource so_called_sender_common_engine_render_texture_load_from_resource ;
 
