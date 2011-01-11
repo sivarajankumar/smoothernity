9da954c1d82a018a06a78488f0b97827ea52ed0b
olegus8 <olegus8@localhost>
2011-01-11 18:09:20 +0000

    working on folders refactoring

---
+++ src_refactored/common/engine/render/sender/disable_depth_test/shy_disable_depth_test_injections.cpp
@@ -0,0 +1,6 @@
+#include "./shy_disable_depth_test_injections.h"
+
+#include "../../shy_render_injections.h"
+
+#include "./shy_disable_depth_test.hpp"
+
--- src_refactored/common/engine/render/sender/disable_depth_test/shy_disable_depth_test_injections.h
+++ src_refactored/common/engine/render/sender/disable_depth_test/shy_disable_depth_test_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_disable_depth_test_injections_included
 #define _shy_sender_common_engine_render_disable_depth_test_injections_included
 
-#include "../message/disable_depth_test/shy_disable_depth_test_injections.h"
+#include "../../message/disable_depth_test/shy_disable_depth_test_injections.h"
 
-#include "shy_disable_depth_test.h"
+#include "./shy_disable_depth_test.h"
 
 typedef shy_sender_common_engine_render_disable_depth_test so_called_sender_common_engine_render_disable_depth_test ;
 
