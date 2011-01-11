8033b72e5c9d009d4870f5f27674845bd783d8fd
olegus8 <olegus8@localhost>
2011-01-11 17:57:09 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/aspect_reply/shy_aspect_reply_injections.h
+++ src_refactored/common/engine/render/sender/aspect_reply/shy_aspect_reply_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_aspect_reply_injections_included
 #define _shy_sender_common_engine_render_aspect_reply_injections_included
 
-#include "../message/aspect_reply/shy_aspect_reply_injections.h"
+#include "../../message/aspect_reply/shy_aspect_reply_injections.h"
 
-#include "shy_aspect_reply.h"
+#include "./shy_aspect_reply.h"
 
 typedef shy_sender_common_engine_render_aspect_reply so_called_sender_common_engine_render_aspect_reply ;
 
