ac65b426d61e33f2bff47b4c9f517bdfae57bc22
olegus8 <olegus8@localhost>
2011-01-11 18:39:12 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/mesh_finalize/shy_mesh_finalize_injections.h
+++ src_refactored/common/engine/render/sender/mesh_finalize/shy_mesh_finalize_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_mesh_finalize_injections_included
 #define _shy_sender_common_engine_render_mesh_finalize_injections_included
 
-#include "../message/mesh_finalize/shy_mesh_finalize_injections.h"
+#include "../../message/mesh_finalize/shy_mesh_finalize_injections.h"
 
-#include "shy_mesh_finalize.h"
+#include "./shy_mesh_finalize.h"
 
 typedef shy_sender_common_engine_render_mesh_finalize so_called_sender_common_engine_render_mesh_finalize ;
 
--- src_refactored/common/engine/render/sender/shy_mesh_finalize_injections.cpp
+++
@@ -1,6 +0,0 @@
-#include "shy_mesh_finalize_injections.h"
-
-#include "../shy_render_injections.h"
-
-#include "shy_mesh_finalize.hpp"
-
