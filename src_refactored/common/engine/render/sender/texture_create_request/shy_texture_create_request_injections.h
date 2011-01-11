e5b54c2098b8e45c65cca871c401d7c15f0cf48a
olegus8 <olegus8@localhost>
2011-01-11 19:06:19 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/texture_create_request/shy_texture_create_request_injections.h
+++ src_refactored/common/engine/render/sender/texture_create_request/shy_texture_create_request_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_texture_create_request_injections_included
 #define _shy_sender_common_engine_render_texture_create_request_injections_included
 
-#include "../message/texture_create_request/shy_texture_create_request_injections.h"
+#include "../../message/texture_create_request/shy_texture_create_request_injections.h"
 
-#include "shy_texture_create_request.h"
+#include "./shy_texture_create_request.h"
 
 typedef shy_sender_common_engine_render_texture_create_request so_called_sender_common_engine_render_texture_create_request ;
 
