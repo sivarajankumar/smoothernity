e14e72d86d425c31748d020171d5678c74f791f5
olegus8 <olegus8@localhost>
2011-01-11 17:24:32 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/message/texture_finalize/shy_texture_finalize_injections.h
+++ src_refactored/common/engine/render/message/texture_finalize/shy_texture_finalize_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_message_common_engine_render_texture_finalize_injections_included
 #define _shy_message_common_engine_render_texture_finalize_injections_included
 
-#include "../shy_type_texture_id_injections.h"
+#include "../../shy_type_texture_id_injections.h"
 
-#include "shy_texture_finalize.h"
+#include "./shy_texture_finalize.h"
 
 typedef shy_message_common_engine_render_texture_finalize so_called_message_common_engine_render_texture_finalize ;
 
--- src_refactored/common/engine/render/sender/shy_texture_finalize_injections.h
+++ src_refactored/common/engine/render/sender/shy_texture_finalize_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_sender_common_engine_render_texture_finalize_injections_included
 #define _shy_sender_common_engine_render_texture_finalize_injections_included
 
-#include "../message/shy_texture_finalize_injections.h"
+#include "../message/texture_finalize/shy_texture_finalize_injections.h"
 
 #include "shy_texture_finalize.h"
 
