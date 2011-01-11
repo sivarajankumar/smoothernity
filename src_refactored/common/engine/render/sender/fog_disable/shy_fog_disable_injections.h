2c0dcc81f09cfa5afe1966815491ed3963c042c3
olegus8 <olegus8@localhost>
2011-01-11 18:15:29 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/fog_disable/shy_fog_disable_injections.h
+++ src_refactored/common/engine/render/sender/fog_disable/shy_fog_disable_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_fog_disable_injections_included
 #define _shy_sender_common_engine_render_fog_disable_injections_included
 
-#include "../message/fog_disable/shy_fog_disable_injections.h"
+#include "../../message/fog_disable/shy_fog_disable_injections.h"
 
-#include "shy_fog_disable.h"
+#include "./shy_fog_disable.h"
 
 typedef shy_sender_common_engine_render_fog_disable so_called_sender_common_engine_render_fog_disable ;
 
