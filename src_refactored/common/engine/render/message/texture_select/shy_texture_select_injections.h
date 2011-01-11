6fcc441a8c3103e27055be57f39f03b119348dc8
olegus8 <olegus8@localhost>
2011-01-11 17:40:35 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/message/texture_select/shy_texture_select_injections.h
+++ src_refactored/common/engine/render/message/texture_select/shy_texture_select_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_message_common_engine_render_texture_select_injections_included
 #define _shy_message_common_engine_render_texture_select_injections_included
 
-#include "../shy_type_texture_id_injections.h"
+#include "../../shy_type_texture_id_injections.h"
 
-#include "shy_texture_select.h"
+#include "./shy_texture_select.h"
 
 typedef shy_message_common_engine_render_texture_select so_called_message_common_engine_render_texture_select ;
 
