074cfe9037587f8dbaeeb87762301ecafc78705f
olegus8 <olegus8@localhost>
2011-01-11 19:14:16 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/shy_texture_loader_ready_request_injections.cpp
+++
@@ -1,6 +0,0 @@
-#include "shy_texture_loader_ready_request_injections.h"
-
-#include "../shy_render_injections.h"
-
-#include "shy_texture_loader_ready_request.hpp"
-
---
+++ src_refactored/common/engine/render/sender/texture_loader_ready_request/shy_texture_loader_ready_request_injections.cpp
@@ -0,0 +1,6 @@
+#include "./shy_texture_loader_ready_request_injections.h"
+
+#include "../../shy_render_injections.h"
+
+#include "./shy_texture_loader_ready_request.hpp"
+
