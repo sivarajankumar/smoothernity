96dc62f43fa6ea66bfc905c113805a11d668d422
olegus8 <olegus8@localhost>
2011-01-11 19:18:13 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/shy_texture_select_injections.cpp
+++
@@ -1,6 +0,0 @@
-#include "shy_texture_select_injections.h"
-
-#include "../shy_render_injections.h"
-
-#include "shy_texture_select.hpp"
-
--- src_refactored/common/engine/render/sender/texture_select/shy_texture_select_injections.h
+++ src_refactored/common/engine/render/sender/texture_select/shy_texture_select_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_texture_select_injections_included
 #define _shy_sender_common_engine_render_texture_select_injections_included
 
-#include "../message/texture_select/shy_texture_select_injections.h"
+#include "../../message/texture_select/shy_texture_select_injections.h"
 
-#include "shy_texture_select.h"
+#include "./shy_texture_select.h"
 
 typedef shy_sender_common_engine_render_texture_select so_called_sender_common_engine_render_texture_select ;
 
