52f665e7d1d4c99560ad5bc58bf3ba37231d251d
olegus8 <olegus8@localhost>
2011-01-11 18:01:48 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/blend_disable/shy_blend_disable_injections.h
+++ src_refactored/common/engine/render/sender/blend_disable/shy_blend_disable_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_blend_disable_injections_included
 #define _shy_sender_common_engine_render_blend_disable_injections_included
 
-#include "../message/blend_disable/shy_blend_disable_injections.h"
+#include "../../message/blend_disable/shy_blend_disable_injections.h"
 
-#include "shy_blend_disable.h"
+#include "./shy_blend_disable.h"
 
 typedef shy_sender_common_engine_render_blend_disable so_called_sender_common_engine_render_blend_disable ;
 
--- src_refactored/common/engine/render/sender/shy_blend_disable_injections.cpp
+++
@@ -1,6 +0,0 @@
-#include "shy_blend_disable_injections.h"
-
-#include "../shy_render_injections.h"
-
-#include "shy_blend_disable.hpp"
-
