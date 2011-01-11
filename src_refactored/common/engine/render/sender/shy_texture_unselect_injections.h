a94987dfc2d336f4ed7ee1bafcd07154a66a5709
olegus8 <olegus8@localhost>
2011-01-11 17:54:08 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/shy_texture_unselect_injections.h
+++ src_refactored/common/engine/render/sender/shy_texture_unselect_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_sender_common_engine_render_texture_unselect_injections_included
 #define _shy_sender_common_engine_render_texture_unselect_injections_included
 
-#include "../message/shy_texture_unselect_injections.h"
+#include "../message/texture_unselect/shy_texture_unselect_injections.h"
 
 #include "shy_texture_unselect.h"
 
