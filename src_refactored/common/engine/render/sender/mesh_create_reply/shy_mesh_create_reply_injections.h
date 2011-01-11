771eb0d314f321e7a1d5d4c5a424229985e1ff62
olegus8 <olegus8@localhost>
2011-01-11 18:33:29 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/mesh_create_reply/shy_mesh_create_reply_injections.h
+++ src_refactored/common/engine/render/sender/mesh_create_reply/shy_mesh_create_reply_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_mesh_create_reply_injections_included
 #define _shy_sender_common_engine_render_mesh_create_reply_injections_included
 
-#include "../message/mesh_create_reply/shy_mesh_create_reply_injections.h"
+#include "../../message/mesh_create_reply/shy_mesh_create_reply_injections.h"
 
-#include "shy_mesh_create_reply.h"
+#include "./shy_mesh_create_reply.h"
 
 typedef shy_sender_common_engine_render_mesh_create_reply so_called_sender_common_engine_render_mesh_create_reply ;
 
