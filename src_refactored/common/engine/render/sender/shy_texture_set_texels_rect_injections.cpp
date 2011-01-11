b4fa21ea5e6d47bc9a2c21476652615c8cffb7a4
olegus8 <olegus8@localhost>
2011-01-11 19:20:12 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/shy_texture_set_texels_rect_injections.cpp
+++
@@ -1,6 +0,0 @@
-#include "shy_texture_set_texels_rect_injections.h"
-
-#include "../shy_render_injections.h"
-
-#include "shy_texture_set_texels_rect.hpp"
-
--- src_refactored/common/engine/render/sender/texture_set_texels_rect/shy_texture_set_texels_rect_injections.h
+++ src_refactored/common/engine/render/sender/texture_set_texels_rect/shy_texture_set_texels_rect_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_texture_set_texels_rect_injections_included
 #define _shy_sender_common_engine_render_texture_set_texels_rect_injections_included
 
-#include "../message/texture_set_texels_rect/shy_texture_set_texels_rect_injections.h"
+#include "../../message/texture_set_texels_rect/shy_texture_set_texels_rect_injections.h"
 
-#include "shy_texture_set_texels_rect.h"
+#include "./shy_texture_set_texels_rect.h"
 
 typedef shy_sender_common_engine_render_texture_set_texels_rect so_called_sender_common_engine_render_texture_set_texels_rect ;
 
