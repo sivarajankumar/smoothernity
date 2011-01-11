51aaa5bbd47510d156bf3c0b70185dfc1e9fee38
olegus8 <olegus8@localhost>
2011-01-11 17:59:15 +0000

    working on folders refactoring

---
+++ src_refactored/common/engine/render/sender/aspect_request/shy_aspect_request_injections.cpp
@@ -0,0 +1,6 @@
+#include "./shy_aspect_request_injections.h"
+
+#include "../../shy_render_injections.h"
+
+#include "./shy_aspect_request.hpp"
+
--- src_refactored/common/engine/render/sender/shy_aspect_request_injections.cpp
+++
@@ -1,6 +0,0 @@
-#include "shy_aspect_request_injections.h"
-
-#include "../shy_render_injections.h"
-
-#include "shy_aspect_request.hpp"
-
