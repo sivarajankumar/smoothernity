9f9e502f1a7902a24d2653f3a5a00ebc70c3ef51
olegus8 <olegus8@localhost>
2011-01-11 19:22:22 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/shy_texture_set_texel_rgba_injections.cpp
+++
@@ -1,6 +0,0 @@
-#include "shy_texture_set_texel_rgba_injections.h"
-
-#include "../shy_render_injections.h"
-
-#include "shy_texture_set_texel_rgba.hpp"
-
---
+++ src_refactored/common/engine/render/sender/texture_set_texel_rgba/shy_texture_set_texel_rgba_injections.cpp
@@ -0,0 +1,6 @@
+#include "./shy_texture_set_texel_rgba_injections.h"
+
+#include "../../shy_render_injections.h"
+
+#include "./shy_texture_set_texel_rgba.hpp"
+
