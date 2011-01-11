02c2956ffcba062087d4aa6126325c3c99f529c7
olegus8 <olegus8@localhost>
2011-01-11 18:24:48 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/matrix_identity/shy_matrix_identity_injections.h
+++ src_refactored/common/engine/render/sender/matrix_identity/shy_matrix_identity_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_matrix_identity_injections_included
 #define _shy_sender_common_engine_render_matrix_identity_injections_included
 
-#include "../message/matrix_identity/shy_matrix_identity_injections.h"
+#include "../../message/matrix_identity/shy_matrix_identity_injections.h"
 
-#include "shy_matrix_identity.h"
+#include "./shy_matrix_identity.h"
 
 typedef shy_sender_common_engine_render_matrix_identity so_called_sender_common_engine_render_matrix_identity ;
 
