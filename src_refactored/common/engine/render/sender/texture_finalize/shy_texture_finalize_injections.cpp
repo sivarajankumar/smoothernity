64626a93be2f5222f4665a047ca3d2c84f48cdaa
olegus8 <olegus8@localhost>
2011-01-11 19:08:13 +0000

    working on folders refactoring

---
+++ src_refactored/common/engine/render/sender/texture_finalize/shy_texture_finalize_injections.cpp
@@ -0,0 +1,6 @@
+#include "./shy_texture_finalize_injections.h"
+
+#include "../../shy_render_injections.h"
+
+#include "./shy_texture_finalize.hpp"
+
--- src_refactored/common/engine/render/sender/texture_finalize/shy_texture_finalize_injections.h
+++ src_refactored/common/engine/render/sender/texture_finalize/shy_texture_finalize_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_texture_finalize_injections_included
 #define _shy_sender_common_engine_render_texture_finalize_injections_included
 
-#include "../message/texture_finalize/shy_texture_finalize_injections.h"
+#include "../../message/texture_finalize/shy_texture_finalize_injections.h"
 
-#include "shy_texture_finalize.h"
+#include "./shy_texture_finalize.h"
 
 typedef shy_sender_common_engine_render_texture_finalize so_called_sender_common_engine_render_texture_finalize ;
 
