59e46e1799be206d584b43d2ce87604260686268
olegus8 <olegus8@localhost>
2011-01-11 19:00:54 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/projection_ortho/shy_projection_ortho_injections.h
+++ src_refactored/common/engine/render/sender/projection_ortho/shy_projection_ortho_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_engine_render_projection_ortho_injections_included
 #define _shy_sender_common_engine_render_projection_ortho_injections_included
 
-#include "../message/projection_ortho/shy_projection_ortho_injections.h"
+#include "../../message/projection_ortho/shy_projection_ortho_injections.h"
 
-#include "shy_projection_ortho.h"
+#include "./shy_projection_ortho.h"
 
 typedef shy_sender_common_engine_render_projection_ortho so_called_sender_common_engine_render_projection_ortho ;
 
--- src_refactored/common/engine/render/sender/shy_projection_ortho_injections.cpp
+++
@@ -1,6 +0,0 @@
-#include "shy_projection_ortho_injections.h"
-
-#include "../shy_render_injections.h"
-
-#include "shy_projection_ortho.hpp"
-
