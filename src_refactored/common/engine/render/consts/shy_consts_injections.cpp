79f90ea710239a5113673af870d57c37c95de649
olegus8 <olegus8@localhost>
2011-01-11 19:28:19 +0000

    working on folders refactoring

---
+++ src_refactored/common/engine/render/consts/shy_consts_injections.cpp
@@ -0,0 +1,6 @@
+#include "./shy_consts_injections.h"
+
+#include "../../../../injections/shy_platform_math.h"
+
+#include "./shy_consts.hpp"
+
--- src_refactored/common/engine/render/shy_consts_injections.cpp
+++
@@ -1,6 +0,0 @@
-#include "shy_consts_injections.h"
-
-#include "../../../injections/shy_platform_math.h"
-
-#include "shy_consts.hpp"
-
