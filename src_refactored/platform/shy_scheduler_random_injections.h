85ba537b9b1fec4d5ebdb36898f9125eef250ba4
olegus8 <olegus8@localhost>
2011-01-12 19:08:33 +0000

    working on folders refactoring

--- src_refactored/platform/shy_scheduler_random_injections.h
+++ src_refactored/platform/shy_scheduler_random_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_platform_scheduler_random_injections_included
 #define _shy_platform_scheduler_random_injections_included
 
-#include "../injections/shy_platform_static_assert.h"
+#include "../injections/platform/static_assert/shy_static_assert.h"
 #include <new>
 
 #include "shy_scheduler_random.h"
