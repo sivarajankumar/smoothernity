3ec0b1f4beef988eddb42d0fd59b7efc6ff62a8c
olegus8 <olegus8@localhost>
2011-01-12 11:48:34 +0000

    working on folders refactoring

---
+++ src_refactored/common/engine/camera_stateless/shy_camera_stateless_injections.cpp
@@ -0,0 +1,7 @@
+#include "./shy_camera_stateless_injections.h"
+
+#include "../../../injections/shy_platform_matrix.h"
+#include "../../../injections/shy_platform_vector.h"
+
+#include "./shy_camera_stateless.hpp"
+
--- src_refactored/common/engine/shy_camera_stateless_injections.cpp
+++
@@ -1,7 +0,0 @@
-#include "shy_camera_stateless_injections.h"
-
-#include "../../injections/shy_platform_matrix.h"
-#include "../../injections/shy_platform_vector.h"
-
-#include "shy_camera_stateless.hpp"
-
