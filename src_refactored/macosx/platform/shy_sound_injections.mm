a96625a3290bbbef99fbc584a2ad24b55664f7f2
olegus8 <olegus8@localhost>
2011-01-12 19:16:17 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/shy_sound_injections.mm
+++ src_refactored/macosx/platform/shy_sound_injections.mm
@@ -1,7 +1,7 @@
 #include "shy_sound_injections.h"
 
 #include "../../injections/platform/math/insider/shy_insider.h"
-#include "../../injections/shy_platform_vector_insider.h"
+#include "../../injections/platform/vector/insider/shy_insider.h"
 #include "../shy_macosx_sound_loader.h"
 #include <OpenAL/alc.h>
 #include <OpenAL/MacOSX_OALExtensions.h>
--- src_refactored/platform/shy_matrix_float_injections.hpp
+++ src_refactored/platform/shy_matrix_float_injections.hpp
@@ -2,7 +2,7 @@
 
 #include "../injections/platform/math/insider/shy_insider.h"
 #include "../injections/shy_platform_vector.h"
-#include "../injections/shy_platform_vector_insider.h"
+#include "../injections/platform/vector/insider/shy_insider.h"
 
 #include "shy_matrix_float.hpp"
 
