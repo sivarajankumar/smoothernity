3a1ad5ee408d44ca0e066ff8e5e802cab6b0a0d4
olegus8 <olegus8@localhost>
2011-01-12 13:07:51 +0000

    working on folders refactoring

--- src_refactored/common/engine/type/rect/shy_rect_injections.h
+++ src_refactored/common/engine/type/rect/shy_rect_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_type_common_engine_rect_injections_included
 #define _shy_type_common_engine_rect_injections_included
 
-#include "../../injections/shy_type_platform_math_num_fract.h"
+#include "../../../../injections/shy_type_platform_math_num_fract.h"
 
-#include "shy_type_rect.h"
+#include "./shy_type_rect.h"
 
 typedef shy_type_common_engine_rect so_called_type_common_engine_rect ;
 
