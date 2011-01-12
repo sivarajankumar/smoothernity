c64eed10ac497ae9373b4014ff71ec30924b6f5f
olegus8 <olegus8@localhost>
2011-01-12 13:27:05 +0000

    working on folders refactoring

--- src_refactored/common/engine/render/sender/mesh_create_reply/shy_mesh_create_reply_injections.cpp
+++ src_refactored/common/engine/render/sender/mesh_create_reply/shy_mesh_create_reply_injections.cpp
@@ -7,7 +7,7 @@
 #include "../../../../logic/image/shy_image_injections.h"
 #include "../../../../logic/land/shy_land_injections.h"
 #include "../../../../logic/main_menu/letters/meshes_creator/shy_meshes_creator_injections.h"
-#include "../../../../logic/main_menu/selection/shy_mesh_injections.h"
+#include "../../../../logic/main_menu/selection/mesh/shy_mesh_injections.h"
 #include "../../../../logic/room/shy_mesh_injections.h"
 #include "../../../../logic/text/shy_text_injections.h"
 #include "../../../../logic/title/shy_title_injections.h"
