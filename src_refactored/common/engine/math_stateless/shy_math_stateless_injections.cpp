bb2367e2c99661f8f84c7d419e3423f6add253ab
olegus8 <olegus8@localhost>
2011-01-12 14:04:01 +0000

    working on folders refactoring

--- src_refactored/common/engine/math_stateless/shy_math_stateless_injections.cpp
+++ src_refactored/common/engine/math_stateless/shy_math_stateless_injections.cpp
@@ -1,6 +1,6 @@
 #include "./shy_math_stateless_injections.h"
 
-#include "../../../injections/shy_platform_conditions.h"
+#include "../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../injections/shy_platform_math.h"
 #include "../../../injections/shy_platform_matrix.h"
 #include "../../../injections/shy_platform_vector.h"
