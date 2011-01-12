ef5a3c9e3caa77f420afe587ea49c481c1698c9e
olegus8 <olegus8@localhost>
2011-01-12 19:11:12 +0000

    working on folders refactoring

--- src_refactored/injections/platform/touch/insider/shy_insider.h
+++ src_refactored/injections/platform/touch/insider/shy_insider.h
@@ -2,7 +2,7 @@
 #define _shy_injections_platform_touch_insider_included
 
 #ifdef shy_build_for_macosx
-    #include "../macosx/platform/shy_touch_insider_injections.h"
+    #include "../../../../macosx/platform/shy_touch_insider_injections.h"
 #endif
 
 #endif
