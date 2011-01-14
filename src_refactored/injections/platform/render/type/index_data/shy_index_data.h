fef82e7c08e66f033c2f5bde85eca350b0e48883
olegus8 <olegus8@localhost>
2011-01-14 15:34:23 +0000

    working on folders refactoring

--- src_refactored/injections/platform/render/type/index_data/shy_index_data.h
+++ src_refactored/injections/platform/render/type/index_data/shy_index_data.h
@@ -2,7 +2,7 @@
 #define _shy_injections_type_platform_render_index_data_included
 
 #ifdef shy_build_for_macosx
-    #include "../macosx/platform/shy_type_render_index_data_injections.h"
+    #include "../../../../../macosx/platform/shy_type_render_index_data_injections.h"
 #endif
 
 #endif
