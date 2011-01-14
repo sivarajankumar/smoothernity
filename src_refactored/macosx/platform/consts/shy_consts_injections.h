cfa47eb7148f5697cddfad79bc33a2da6ab2b95d
olegus8 <olegus8@localhost>
2011-01-14 18:47:09 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/consts/shy_consts_injections.h
+++ src_refactored/macosx/platform/consts/shy_consts_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_macosx_platform_consts_included
 #define _shy_macosx_platform_consts_included
 
-#include "../../platform/shy_consts_injections.h"
+#include "../../../platform/shy_consts_injections.h"
 typedef shy_platform_consts shy_macosx_platform_consts ;
 
 #endif
