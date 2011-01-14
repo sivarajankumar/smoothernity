56418f4f0ac4ad189851c059d13745caaa1be358
olegus8 <olegus8@localhost>
2011-01-14 19:01:40 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/math/insider/shy_insider_injections.h
+++ src_refactored/macosx/platform/math/insider/shy_insider_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_macosx_platform_math_insider_injections_included
 #define _shy_macosx_platform_math_insider_injections_included
 
-#include "../../platform/shy_math_int_float_insider_injections.h"
+#include "../../../../platform/shy_math_int_float_insider_injections.h"
 typedef shy_platform_math_int_float_insider so_called_platform_math_insider ;
 
 #endif
--- src_refactored/macosx/platform/shy_math_insider_injections.mm
+++
@@ -1,2 +0,0 @@
-#include "../../platform/shy_math_int_float_insider_injections.hpp"
-
