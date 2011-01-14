66a5d25aa0470ea3e98f3db5fab9e5c514024032
olegus8 <olegus8@localhost>
2011-01-14 18:44:28 +0000

    working on folders refactoring

--- src_refactored/injections/platform/consts/insider/shy_insider.h
+++ src_refactored/injections/platform/consts/insider/shy_insider.h
@@ -2,7 +2,7 @@
 #define _shy_injections_platform_consts_insider_included
 
 #ifdef shy_build_for_macosx
-    #include "../../../../macosx/platform/shy_consts_insider_injections.h"
+    #include "../../../../macosx/platform/consts/insider/shy_insider_injections.h"
 #endif
 
 #endif
