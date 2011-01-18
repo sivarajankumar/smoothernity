9115e72754cf84ba207587afcf4d84de15b115c2
olegus8 <olegus8@localhost>
2011-01-18 16:34:09 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/pointer/shy_pointer_injections.h
+++ src_refactored/macosx/platform/pointer/shy_pointer_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_macosx_platform_pointer_injections_included
 #define _shy_macosx_platform_pointer_injections_included
 
-#include "../../platform/shy_pointer_injections.h"
+#include "../../../platform/shy_pointer_injections.h"
 typedef shy_platform_pointer so_called_platform_pointer ;
 
 #endif
