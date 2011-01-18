88fc8ef0033d5d4382ace09bbe67a32874cf6582
olegus8 <olegus8@localhost>
2011-01-18 17:18:17 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/render/insider/shy_insider_injections.mm
+++ src_refactored/macosx/platform/render/insider/shy_insider_injections.mm
@@ -1,6 +1,6 @@
 #include "./shy_insider_injections.h"
 
-#include "../shy_render_injections.h"
+#include "../../../../injections/platform/render/shy_render.h"
 
 #include "./shy_insider.hpp"
 
--- src_refactored/macosx/platform/sound/insider/shy_insider_injections.mm
+++ src_refactored/macosx/platform/sound/insider/shy_insider_injections.mm
@@ -1,6 +1,6 @@
 #include "./shy_insider_injections.h"
 
-#include "../../shy_sound_injections.h"
+#include "../../../../injections/platform/sound/shy_sound.h"
 
 #include "./shy_insider.hpp"
 
