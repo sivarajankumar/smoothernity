167d966dae34d1b3d5f631dbbea9c2057ebf4a2c
olegus8 <olegus8@localhost>
2011-01-19 19:46:10 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/consts/insider/shy_insider_injections.h
+++ src_refactored/macosx/platform/consts/insider/shy_insider_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_macosx_platform_consts_insider_injections_included
 #define _shy_macosx_platform_consts_insider_injections_included
 
-#include "../../../../platform/shy_consts_insider_injections.h"
+#include "../../../../platform/consts/insider/shy_insider_injections.h"
 typedef shy_platform_consts_insider so_called_platform_consts_insider ;
 
 #endif
--- src_refactored/platform/consts/insider/shy_insider_injections.h
+++ src_refactored/platform/consts/insider/shy_insider_injections.h
@@ -1,6 +1,6 @@
 #ifndef _shy_platform_consts_insider_injections_included
 #define _shy_platform_consts_insider_injections_included
 
-#include "shy_consts_insider.h"
+#include "./shy_insider.h"
 
 #endif
