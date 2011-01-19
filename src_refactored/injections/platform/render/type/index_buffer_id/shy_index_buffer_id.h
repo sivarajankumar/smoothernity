2dcbe59a918824a6085b0ac04408d4236096023a
olegus8 <olegus8@localhost>
2011-01-19 17:36:08 +0000

    working on folders refactoring

--- src_refactored/injections/platform/render/type/index_buffer_id/shy_index_buffer_id.h
+++ src_refactored/injections/platform/render/type/index_buffer_id/shy_index_buffer_id.h
@@ -2,7 +2,7 @@
 #define _shy_injections_type_platform_render_index_buffer_id_included
 
 #ifdef shy_build_for_macosx
-    #include "../../../../../macosx/platform/shy_type_render_index_buffer_id_injections.h"
+    #include "../../../../../macosx/platform/render/type/index_buffer_id/shy_index_buffer_id_injections.h"
 #endif
 
 #endif
--- src_refactored/macosx/platform/render/type/index_buffer_id/shy_index_buffer_id_injections.h
+++ src_refactored/macosx/platform/render/type/index_buffer_id/shy_index_buffer_id_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_type_macosx_platform_render_index_buffer_id_injections_included
 #define _shy_type_macosx_platform_render_index_buffer_id_injections_included
 
-#include "shy_type_render_index_buffer_id.h"
+#include "./shy_index_buffer_id.h"
 
 typedef shy_type_macosx_platform_render_index_buffer_id so_called_type_platform_render_index_buffer_id ;
 
