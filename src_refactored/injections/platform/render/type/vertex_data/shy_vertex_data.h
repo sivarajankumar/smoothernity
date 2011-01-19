3729e158e085aa9cb5d086022e44475ba671cfc5
olegus8 <olegus8@localhost>
2011-01-19 18:49:13 +0000

    working on folders refactoring

--- src_refactored/injections/platform/render/type/vertex_data/shy_vertex_data.h
+++ src_refactored/injections/platform/render/type/vertex_data/shy_vertex_data.h
@@ -2,7 +2,7 @@
 #define _shy_injections_type_platform_render_vertex_data_included
 
 #ifdef shy_build_for_macosx
-    #include "../../../../../macosx/platform/shy_type_render_vertex_data_injections.h"
+    #include "../../../../../macosx/platform/render/type/vertex_data/shy_vertex_data_injections.h"
 #endif
 
 #endif
