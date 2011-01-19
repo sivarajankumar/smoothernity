f0b0b102639735b04d6e1e836ca9cdca02561811
olegus8 <olegus8@localhost>
2011-01-19 19:42:01 +0000

    working on folders refactoring

---
+++ src_refactored/platform/conditions/shy_conditions_injections.hpp
@@ -0,0 +1,6 @@
+#include "./shy_conditions_injections.h"
+
+#include "../../injections/platform/math/insider/shy_insider.h"
+
+#include "./shy_conditions.hpp"
+
--- src_refactored/platform/shy_conditions_injections.hpp
+++
@@ -1,6 +0,0 @@
-#include "shy_conditions_injections.h"
-
-#include "../injections/platform/math/insider/shy_insider.h"
-
-#include "shy_conditions.hpp"
-
