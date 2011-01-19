b8f6ebd4d507d455060e79386c1ab4d8bff2ccd1
olegus8 <olegus8@localhost>
2011-01-19 15:26:57 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/static_array/insider/shy_insider_injections.h
+++ src_refactored/macosx/platform/static_array/insider/shy_insider_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_macosx_platform_static_array_insider_injections_included
 #define _shy_macosx_platform_static_array_insider_injections_included
 
-#include "../../platform/shy_static_array_insider_injections.h"
+#include "../../../../platform/shy_static_array_insider_injections.h"
 typedef shy_platform_static_array_insider so_called_platform_static_array_insider ;
 
 #endif
