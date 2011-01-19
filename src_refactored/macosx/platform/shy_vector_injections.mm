5d3c45cd2fc8d3017a4d3b9fefd4053d2742956d
olegus8 <olegus8@localhost>
2011-01-19 19:37:07 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/shy_vector_injections.mm
+++
@@ -1,2 +0,0 @@
-#include "../../platform/shy_vector_float_injections.hpp"
-
--- src_refactored/macosx/platform/vector/shy_vector_injections.h
+++ src_refactored/macosx/platform/vector/shy_vector_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_macosx_platform_vector_injections_included
 #define _shy_macosx_platform_vector_injections_included
 
-#include "../../platform/shy_vector_float_injections.h"
+#include "../../../platform/shy_vector_float_injections.h"
 typedef shy_platform_vector_float so_called_platform_vector ;
 
 #endif
