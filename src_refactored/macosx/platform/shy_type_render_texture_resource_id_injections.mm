6122222c9e37faea145c636ce131fcf3dd0b87f1
olegus8 <olegus8@localhost>
2011-01-19 18:03:10 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/shy_type_render_texture_resource_id_injections.mm
+++
@@ -1,6 +0,0 @@
-#include "shy_type_render_texture_resource_id_injections.h"
-
-#include "../../injections/platform/consts/insider/shy_insider.h"
-
-#include "shy_type_render_texture_resource_id.hpp"
-
