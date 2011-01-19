4f0e1fff8d8a5f65d2dbfff17b8848502d882961
olegus8 <olegus8@localhost>
2011-01-19 17:42:17 +0000

    working on folders refactoring

---
+++ src_refactored/macosx/platform/render/type/index_buffer_mapped_data/shy_index_buffer_mapped_data_injections.mm
@@ -0,0 +1,6 @@
+#include "./shy_index_buffer_mapped_data_injections.h"
+
+#include "../../../../../injections/platform/consts/insider/shy_insider.h"
+
+#include "./shy_index_buffer_mapped_data.hpp"
+
