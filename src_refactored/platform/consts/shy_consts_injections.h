08c6a14c6279aecdf8590d46bb6682154460463c
olegus8 <olegus8@localhost>
2011-01-19 19:47:58 +0000

    working on folders refactoring

--- src_refactored/platform/consts/shy_consts_injections.h
+++ src_refactored/platform/consts/shy_consts_injections.h
@@ -1,8 +1,8 @@
 #ifndef _shy_platform_consts_injections_included
 #define _shy_platform_consts_injections_included
 
-#include "../injections/shy_type_platform_math_const_int_32.h"
+#include "../../injections/shy_type_platform_math_const_int_32.h"
 
-#include "shy_consts.h"
+#include "./shy_consts.h"
 
 #endif
