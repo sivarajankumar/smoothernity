65c23f65fd167e06cca24809103c413ccf7c0324
olegus8 <olegus8@localhost>
2011-01-19 17:52:59 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/render/type/texel_data/shy_texel_data_injections.h
+++ src_refactored/macosx/platform/render/type/texel_data/shy_texel_data_injections.h
@@ -3,7 +3,7 @@
 
 #include <OpenGL/gl.h>
 
-#include "shy_type_render_texel_data.h"
+#include "./shy_texel_data.h"
 
 typedef shy_type_macosx_platform_render_texel_data so_called_type_platform_render_texel_data ;
 
