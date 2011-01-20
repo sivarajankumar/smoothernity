608938d5f73bc59c008b31bb206f141e84e486f0
olegus8 <olegus8@localhost>
2011-01-20 13:15:33 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/scheduler/shy_scheduler_injections.h
+++ src_refactored/macosx/platform/scheduler/shy_scheduler_injections.h
@@ -7,7 +7,7 @@
 #endif
 
 #ifdef shy_scheduling_mode_direct_call
-    #include "../../../platform/shy_scheduler_direct_call_injections.h"
+    #include "../../../platform/scheduler/direct_call/shy_direct_call_injections.h"
     typedef shy_platform_scheduler_direct_call so_called_platform_scheduler ;
 #endif
 
--- src_refactored/platform/scheduler/direct_call/shy_direct_call_injections.h
+++ src_refactored/platform/scheduler/direct_call/shy_direct_call_injections.h
@@ -1,6 +1,6 @@
 #ifndef _shy_platform_scheduler_direct_call_injections_included
 #define _shy_platform_scheduler_direct_call_injections_included
 
-#include "shy_scheduler_direct_call.h"
+#include "./shy_direct_call.h"
 
 #endif
