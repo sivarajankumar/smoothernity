95621c93ceb7bf429236c946a7a6c47b8adfe773
olegus8 <olegus8@localhost>
2011-01-20 13:02:20 +0000

    working on folders refactoring

---
+++ src_refactored/platform/mouse/shy_mouse_injections.hpp
@@ -0,0 +1,6 @@
+#include "./shy_mouse_injections.h"
+
+#include "../../injections/platform/math/insider/shy_insider.h"
+#include "../../injections/platform/mouse/insider/shy_insider.h"
+
+#include "./shy_mouse.hpp"
