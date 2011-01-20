be02f0a7157edeccdb6dbae4cfc1d0f4195dc553
olegus8 <olegus8@localhost>
2011-01-20 13:11:06 +0000

    working on folders refactoring

--- src_refactored/platform/shy_pointer_injections.h
+++
@@ -1,9 +0,0 @@
-#ifndef _shy_platform_pointer_injections_included
-#define _shy_platform_pointer_injections_included
-
-#include "../injections/platform/consts/insider/shy_insider.h"
-#include "../injections/platform/math/insider/shy_insider.h"
-
-#include "shy_pointer.h"
-
-#endif
