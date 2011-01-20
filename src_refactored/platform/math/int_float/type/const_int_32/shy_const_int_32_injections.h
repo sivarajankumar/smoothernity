7f8b59176b10c2cbceaf739b7d07cf05553af11d
olegus8 <olegus8@localhost>
2011-01-20 13:41:35 +0000

    working on folders refactoring

--- src_refactored/platform/math/int_float/type/const_int_32/shy_const_int_32_injections.h
+++ src_refactored/platform/math/int_float/type/const_int_32/shy_const_int_32_injections.h
@@ -1,6 +1,6 @@
 #ifndef _shy_type_platform_math_int_float_const_int_32_injections_included
 #define _shy_type_platform_math_int_float_const_int_32_injections_included
 
-#include "shy_type_math_int_float_const_int_32.h"
+#include "./shy_const_int_32.h"
 
 #endif
