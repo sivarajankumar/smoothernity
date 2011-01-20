deb047c948d9d34a9f6998376ad6c50749acbedf
olegus8 <olegus8@localhost>
2011-01-20 14:43:09 +0000

    working on folders refactoring

--- src_refactored/common/engine/camera/stateless/shy_stateless_injections.h
+++ src_refactored/common/engine/camera/stateless/shy_stateless_injections.h
@@ -1,10 +1,10 @@
 #ifndef _shy_common_engine_camera_stateless_injections_included
 #define _shy_common_engine_camera_stateless_injections_included
 
-#include "../../../injections/platform/matrix/type/data/shy_data.h"
-#include "../../../injections/platform/vector/type/data/shy_data.h"
+#include "../../../../injections/platform/matrix/type/data/shy_data.h"
+#include "../../../../injections/platform/vector/type/data/shy_data.h"
 
-#include "./shy_camera_stateless.h"
+#include "./shy_stateless.h"
 
 typedef shy_common_engine_camera_stateless so_called_common_engine_camera_stateless ;
 
--- src_refactored/common/engine/camera_stateless/shy_camera_stateless_injections.cpp
+++
@@ -1,7 +0,0 @@
-#include "./shy_camera_stateless_injections.h"
-
-#include "../../../injections/platform/matrix/shy_matrix.h"
-#include "../../../injections/platform/vector/shy_vector.h"
-
-#include "./shy_camera_stateless.hpp"
-
