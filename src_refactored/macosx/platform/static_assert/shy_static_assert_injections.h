0145403aea53085d2fb15e1ab8b26a5b94170043
olegus8 <olegus8@localhost>
2011-01-20 13:30:14 +0000

    working on folders refactoring

--- src_refactored/macosx/platform/static_assert/shy_static_assert_injections.h
+++ src_refactored/macosx/platform/static_assert/shy_static_assert_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_macosx_platform_static_assert_injections_included
 #define _shy_macosx_platform_static_assert_injections_included
 
-#include "../../../platform/shy_static_assert_injections.h"
+#include "../../../platform/static_assert/shy_static_assert_injections.h"
 typedef shy_platform_static_assert so_called_platform_static_assert ;
 
 #endif
