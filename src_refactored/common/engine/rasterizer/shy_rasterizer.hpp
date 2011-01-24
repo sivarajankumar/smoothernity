1497b6f62d3d8251ef6473883f4023ba2cc72bea
olegus8 <olegus8@localhost>
2011-01-24 18:32:29 +0000

    working on common engine rasterizer refactoring

--- src_refactored/common/engine/rasterizer/shy_rasterizer.hpp
+++ src_refactored/common/engine/rasterizer/shy_rasterizer.hpp
@@ -174,6 +174,114 @@ void shy_guts :: rasterize_bottom_triangle_part
     }
 }
 
+void shy_guts :: rasterize_bresenham_ellipse
+    ( so_called_type_platform_math_num_whole cx
+    , so_called_type_platform_math_num_whole cy
+    , so_called_type_platform_math_num_whole x_radius
+    , so_called_type_platform_math_num_whole y_radius
+    )
+{
+    so_called_type_platform_math_num_whole x ;
+    so_called_type_platform_math_num_whole y ;
+    so_called_type_platform_math_num_whole x_change ;
+    so_called_type_platform_math_num_whole y_change ;
+    so_called_type_platform_math_num_whole ellipse_error ;
+    so_called_type_platform_math_num_whole two_a_square ;
+    so_called_type_platform_math_num_whole two_b_square ;
+    so_called_type_platform_math_num_whole stopping_x ;
+    so_called_type_platform_math_num_whole stopping_y ;
+    so_called_type_platform_math_num_whole x_radius_sq ;
+    so_called_type_platform_math_num_whole y_radius_sq ;
+    so_called_type_platform_math_num_whole x_radius_mul_2 ;
+    so_called_type_platform_math_num_whole y_radius_mul_2 ;
+    
+    so_called_platform_math :: mul_wholes ( x_radius_sq , x_radius , x_radius ) ;
+    so_called_platform_math :: mul_wholes ( y_radius_sq , y_radius , y_radius ) ;
+    so_called_platform_math :: mul_wholes ( x_radius_mul_2 , x_radius , so_called_platform_math_consts :: whole_2 ) ;
+    so_called_platform_math :: mul_wholes ( y_radius_mul_2 , y_radius , so_called_platform_math_consts :: whole_2 ) ;
+    so_called_platform_math :: mul_wholes ( two_a_square , x_radius_sq , so_called_platform_math_consts :: whole_2 ) ;
+    so_called_platform_math :: mul_wholes ( two_b_square , y_radius_sq , so_called_platform_math_consts :: whole_2 ) ;
+
+    x = x_radius ;
+    y = so_called_platform_math_consts :: whole_0 ;
+    so_called_platform_math :: sub_wholes ( x_change , so_called_platform_math_consts :: whole_1 , x_radius_mul_2 ) ;
+    so_called_platform_math :: mul_whole_by ( x_change , y_radius_sq ) ;
+    y_change = x_radius_sq ;
+    ellipse_error = so_called_platform_math_consts :: whole_0 ;
+    so_called_platform_math :: mul_wholes ( stopping_x , two_b_square , x_radius ) ;    
+    stopping_y = so_called_platform_math_consts :: whole_0 ;
+    
+    while ( so_called_platform_conditions :: whole_greater_or_equal_to_whole ( stopping_x , stopping_y ) )
+    {
+        so_called_type_platform_math_num_whole cx_minus_x ;
+        so_called_type_platform_math_num_whole cx_plus_x ;
+        so_called_type_platform_math_num_whole cy_minus_y ;
+        so_called_type_platform_math_num_whole cy_plus_y ;
+        so_called_type_platform_math_num_whole ellipse_error_mul_2_plus_x_change ;
+
+        so_called_platform_math :: sub_wholes ( cx_minus_x , cx , x ) ;
+        so_called_platform_math :: add_wholes ( cx_plus_x , cx , x ) ;
+        so_called_platform_math :: sub_wholes ( cy_minus_y , cy , y ) ;
+        so_called_platform_math :: add_wholes ( cy_plus_y , cy , y ) ;
+    
+        rasterize_horizontal_line ( cx_minus_x , cx_plus_x , cy_minus_y ) ;
+        rasterize_horizontal_line ( cx_minus_x , cx_plus_x , cy_plus_y ) ;
+        
+        so_called_platform_math :: inc_whole ( y ) ;
+        so_called_platform_math :: add_to_whole ( stopping_y , two_a_square ) ;
+        so_called_platform_math :: add_to_whole ( ellipse_error , y_change ) ;
+        so_called_platform_math :: add_to_whole ( y_change , two_a_square ) ;
+        so_called_platform_math :: mul_wholes ( ellipse_error_mul_2_plus_x_change , ellipse_error , so_called_platform_math_consts :: whole_2 ) ;
+        so_called_platform_math :: add_to_whole ( ellipse_error_mul_2_plus_x_change , x_change ) ;
+        if ( so_called_platform_conditions :: whole_greater_than_zero ( ellipse_error_mul_2_plus_x_change ) )
+        {
+            so_called_platform_math :: dec_whole ( x ) ;
+            so_called_platform_math :: sub_from_whole ( stopping_x , two_b_square ) ;
+            so_called_platform_math :: add_to_whole ( ellipse_error , x_change ) ;
+            so_called_platform_math :: add_to_whole ( x_change , two_b_square ) ;
+        }
+    }
+    
+    x = so_called_platform_math_consts :: whole_0 ;
+    y = y_radius ;
+    x_change = y_radius_sq ;
+    so_called_platform_math :: sub_wholes ( y_change , so_called_platform_math_consts :: whole_1 , y_radius_mul_2 ) ;
+    so_called_platform_math :: mul_whole_by ( y_change , x_radius_sq ) ;
+    ellipse_error = so_called_platform_math_consts :: whole_0 ;
+    stopping_x = so_called_platform_math_consts :: whole_0 ;
+    so_called_platform_math :: mul_wholes ( stopping_y , two_a_square , y_radius ) ;
+    
+    while ( so_called_platform_conditions :: whole_less_or_equal_to_whole ( stopping_x , stopping_y ) )
+    {
+        so_called_type_platform_math_num_whole cx_minus_x ;
+        so_called_type_platform_math_num_whole cx_plus_x ;
+        so_called_type_platform_math_num_whole cy_minus_y ;
+        so_called_type_platform_math_num_whole cy_plus_y ;
+        so_called_type_platform_math_num_whole ellipse_error_mul_2_plus_y_change ;
+
+        so_called_platform_math :: sub_wholes ( cx_minus_x , cx , x ) ;
+        so_called_platform_math :: add_wholes ( cx_plus_x , cx , x ) ;
+        so_called_platform_math :: sub_wholes ( cy_minus_y , cy , y ) ;
+        so_called_platform_math :: add_wholes ( cy_plus_y , cy , y ) ;
+        
+        rasterize_horizontal_line ( cx_minus_x , cx_plus_x , cy_minus_y ) ;
+        rasterize_horizontal_line ( cx_minus_x , cx_plus_x , cy_plus_y ) ;
+        
+        so_called_platform_math :: dec_whole ( x ) ;
+        so_called_platform_math :: add_to_whole ( stopping_x , two_b_square ) ;
+        so_called_platform_math :: add_to_whole ( ellipse_error , x_change ) ;
+        so_called_platform_math :: add_to_whole ( x_change , two_b_square ) ;
+        so_called_platform_math :: mul_wholes ( ellipse_error_mul_2_plus_y_change , ellipse_error , so_called_platform_math_consts :: whole_2 ) ;
+        so_called_platform_math :: add_to_whole ( ellipse_error_mul_2_plus_y_change , y_change ) ;
+        if ( so_called_platform_conditions :: whole_greater_than_zero ( ellipse_error_mul_2_plus_y_change ) )
+        {
+            so_called_platform_math :: dec_whole ( y ) ;
+            so_called_platform_math :: sub_from_whole ( stopping_y , two_a_square ) ;
+            so_called_platform_math :: add_to_whole ( ellipse_error , y_change ) ;
+            so_called_platform_math :: add_to_whole ( y_change , two_a_square ) ;
+        }
+    }
+}
 void _shy_common_engine_rasterizer :: receive ( so_called_message_common_engine_rasterizer_draw_ellipse_in_rect )
 {
 }
