c524166bd01cff4df7a9ed716a65cfbe446ffb25
olegus8 <olegus8@localhost>
2011-01-24 18:17:08 +0000

    working on common engine rasterizer refactoring

--- src_refactored/common/engine/rasterizer/shy_rasterizer_injections.cpp
+++ src_refactored/common/engine/rasterizer/shy_rasterizer_injections.cpp
@@ -5,6 +5,7 @@
 #include "../math/stateless/shy_stateless_injections.h"
 #include "../render/sender/texture_set_texels_rect/shy_texture_set_texels_rect_injections.h"
 
+#include "../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../injections/platform/math/shy_math.h"
 
