7fe5c2db374081ba4bc56227cfbe13033109a489
olegus8 <olegus8@localhost>
2011-01-25 17:23:04 +0000

    working on common messages refactoring

--- src_refactored/common/logic/blanket/animation/disappear/shy_disappear_injections.h
+++ src_refactored/common/logic/blanket/animation/disappear/shy_disappear_injections.h
@@ -3,8 +3,10 @@
 
 #include "../../../../message/init/shy_init_injections.h"
 
+#include "../../../../../injections/platform/scheduler/shy_scheduler.h"
+
 #include "./shy_disappear.h"
 
-typedef shy_common_logic_blanket_animation_disappear so_called_common_logic_blanket_animation_disappear ;
+typedef shy_common_logic_blanket_animation_disappear_scheduled so_called_common_logic_blanket_animation_disappear ;
 
 #endif
