92895c7d84198d8e16144534bd40fd1562627699
olegus8 <olegus8@localhost>
2011-01-25 17:37:30 +0000

    working on common messages refactoring

--- src_refactored/common/logic/controls/shy_controls.h
+++ src_refactored/common/logic/controls/shy_controls.h
@@ -1,5 +1,7 @@
-class shy_common_logic_controls
+class _shy_common_logic_controls
 {
 public :
     static void receive ( so_called_message_common_init ) ;
 } ;
+
+typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_controls > :: module shy_common_logic_controls_scheduled ;
