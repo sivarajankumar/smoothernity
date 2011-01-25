956db3665cab5fdc6118a8de607d005cbd2df2f3
olegus8 <olegus8@localhost>
2011-01-25 17:39:29 +0000

    working on common messages refactoring

--- src_refactored/common/logic/door/animation/appear/shy_appear_injections.h
+++ src_refactored/common/logic/door/animation/appear/shy_appear_injections.h
@@ -3,8 +3,10 @@
 
 #include "../../../../message/init/shy_init_injections.h"
 
+#include "../../../../../injections/platform/scheduler/shy_scheduler.h"
+
 #include "./shy_appear.h"
 
-typedef shy_common_logic_door_animation_appear so_called_common_logic_door_animation_appear ;
+typedef shy_common_logic_door_animation_appear_scheduled so_called_common_logic_door_animation_appear ;
 
 #endif
