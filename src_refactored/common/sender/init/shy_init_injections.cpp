b0b5a854439da7e5b63f57f44fae6d8d2290e017
olegus8 <olegus8@localhost>
2011-01-25 15:21:16 +0000

    working on common messages refactoring

--- src_refactored/common/sender/init/shy_init_injections.cpp
+++ src_refactored/common/sender/init/shy_init_injections.cpp
@@ -9,6 +9,7 @@
 #include "../../logic/blanket/animation/disappear/shy_disappear_injections.h"
 #include "../../logic/blanket/animation/fit/shy_fit_injections.h"
 #include "../../logic/blanket/animation/shy_animation_injections.h"
+#include "../../logic/blanket/mesh/shy_mesh_injections.h"
 #include "../../logic/blanket/shy_blanket_injections.h"
 #include "../../logic/core/shy_core_injections.h"
 
