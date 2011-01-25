6502e4f294716562f21247cb2533ac49862650b7
olegus8 <olegus8@localhost>
2011-01-25 17:16:58 +0000

    working on common messages refactoring

--- src_refactored/common/logic/application/fsm/shy_fsm.h
+++ src_refactored/common/logic/application/fsm/shy_fsm.h
@@ -1,5 +1,7 @@
-class shy_common_logic_application_fsm
+class _shy_common_logic_application_fsm
 {
 public :
     static void receive ( so_called_message_common_init ) ;
 } ;
+
+typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_application_fsm > :: module shy_common_logic_application_fsm_scheduled ;
--- src_refactored/common/logic/application/fsm/shy_fsm_injections.h
+++ src_refactored/common/logic/application/fsm/shy_fsm_injections.h
@@ -3,8 +3,10 @@
 
 #include "../../../message/init/shy_init_injections.h"
 
+#include "../../../../injections/platform/scheduler/shy_scheduler.h"
+
 #include "./shy_fsm.h"
 
-typedef shy_common_logic_application_fsm so_called_common_logic_application_fsm ;
+typedef shy_common_logic_application_fsm_scheduled so_called_common_logic_application_fsm ;
 
 #endif
