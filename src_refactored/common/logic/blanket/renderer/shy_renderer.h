9a553e1eed9f745fb5bba716a49ace5ec319c405
olegus8 <olegus8@localhost>
2011-01-25 17:30:43 +0000

    working on common messages refactoring

--- src_refactored/common/logic/blanket/renderer/shy_renderer.h
+++ src_refactored/common/logic/blanket/renderer/shy_renderer.h
@@ -1,5 +1,7 @@
-class shy_common_logic_blanket_renderer
+class _shy_common_logic_blanket_renderer
 {
 public :
     static void receive ( so_called_message_common_init ) ;
 } ;
+
+typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_blanket_renderer > :: module shy_common_logic_blanket_renderer_scheduled ;
