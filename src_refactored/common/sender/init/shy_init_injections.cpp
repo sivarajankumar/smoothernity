087bb0a715ac276bd52e70ab3ed2547afd28320e
olegus8 <olegus8@localhost>
2011-01-25 14:45:47 +0000

    working on common messages refactoring

--- src_refactored/common/sender/init/shy_init_injections.cpp
+++ src_refactored/common/sender/init/shy_init_injections.cpp
@@ -2,6 +2,7 @@
 
 #include "../../engine/rasterizer/shy_rasterizer_injections.h"
 #include "../../engine/render/shy_render_injections.h"
+#include "../../logic/amusement/renderer/shy_renderer_injections.h"
 #include "../../logic/amusement/shy_amusement_injections.h"
 #include "../../logic/core/shy_core_injections.h"
 
