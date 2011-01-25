74023dac72dc1f94c8e655cc686460a480a83439
olegus8 <olegus8@localhost>
2011-01-25 17:14:14 +0000

    working on common messages refactoring

--- src_refactored/common/logic/amusement/renderer/shy_renderer.hpp
+++ src_refactored/common/logic/amusement/renderer/shy_renderer.hpp
@@ -1,3 +1,6 @@
-void shy_common_logic_amusement_renderer :: receive ( so_called_message_common_init )
+typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_amusement_renderer > _scheduled_context_type ;
+template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void _shy_common_logic_amusement_renderer :: receive ( so_called_message_common_init )
 {
 }
--- src_refactored/common/logic/amusement/renderer/shy_renderer_injections.h
+++ src_refactored/common/logic/amusement/renderer/shy_renderer_injections.h
@@ -3,8 +3,10 @@
 
 #include "../../../message/init/shy_init_injections.h"
 
+#include "../../../../injections/platform/scheduler/shy_scheduler.h"
+
 #include "./shy_renderer.h"
 
-typedef shy_common_logic_amusement_renderer so_called_common_logic_amusement_renderer ;
+typedef shy_common_logic_amusement_renderer_scheduled so_called_common_logic_amusement_renderer ;
 
 #endif
