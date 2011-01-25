7b51301b753a4053864fa7d6f7d1bf2225ba7e59
olegus8 <olegus8@localhost>
2011-01-25 17:21:12 +0000

    working on common messages refactoring

--- src_refactored/common/logic/blanket/animation/appear/shy_appear.h
+++ src_refactored/common/logic/blanket/animation/appear/shy_appear.h
@@ -1,5 +1,7 @@
-class shy_common_logic_blanket_animation_appear
+class _shy_common_logic_blanket_animation_appear
 {
 public :
     static void receive ( so_called_message_common_init ) ;
 } ;
+
+typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_blanket_animation_appear > :: module shy_common_logic_blanket_animation_appear_scheduled ;
--- src_refactored/macosx/clean.sh
+++ src_refactored/macosx/clean.sh
@@ -1,2 +1,1 @@
-xcodebuild -project smoothernity.xcodeproj -target "smoothernity (Upgraded)" -configuration Development clean
-xcodebuild -project smoothernity.xcodeproj -target "smoothernity (Upgraded)" -configuration Deployment clean
+rm -rf build
