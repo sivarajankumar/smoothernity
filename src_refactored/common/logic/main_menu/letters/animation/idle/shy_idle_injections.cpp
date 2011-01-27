8a9487dba769449e487026340eb963dc6f2cef8b
olegus8 <olegus8@localhost>
2011-01-27 12:51:55 +0000

    working on common messages refactoring

---
+++ src_refactored/common/logic/main_menu/letters/animation/idle/shy_idle_injections.cpp
@@ -0,0 +1,3 @@
+#include "./shy_idle_injections.h"
+
+#include "./shy_idle.hpp"
--- src_refactored/common/sender/init/shy_init.hpp
+++ src_refactored/common/sender/init/shy_init.hpp
@@ -35,8 +35,8 @@ void shy_sender_common_init :: send ( so_called_message_common_init msg )
     so_called_common_logic_main_menu_letters_animation :: receive ( msg ) ;
     so_called_common_logic_main_menu_letters_animation_appear :: receive ( msg ) ;
     so_called_common_logic_main_menu_letters_animation_disappear :: receive ( msg ) ;
+    so_called_common_logic_main_menu_letters_animation_idle :: receive ( msg ) ;
 /*
-    _receivers . get ( ) . logic_main_menu_letters_animation_idle . get ( ) . receive ( msg ) ;
     _receivers . get ( ) . logic_main_menu_letters_animation_selection . get ( ) . receive ( msg ) ;
     _receivers . get ( ) . logic_main_menu_letters_animation_selection_push . get ( ) . receive ( msg ) ;
     _receivers . get ( ) . logic_main_menu_letters_animation_selection_weight . get ( ) . receive ( msg ) ;
