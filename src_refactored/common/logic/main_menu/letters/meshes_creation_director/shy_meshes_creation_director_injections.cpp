50f86e412d606b1b7dc78c7cdd7c282ec3bf5039
olegus8 <olegus8@localhost>
2011-01-27 14:57:06 +0000

    working on common messages refactoring

---
+++ src_refactored/common/logic/main_menu/letters/meshes_creation_director/shy_meshes_creation_director_injections.cpp
@@ -0,0 +1,3 @@
+#include "./shy_meshes_creation_director_injections.h"
+
+#include "./shy_meshes_creation_director.hpp"
--- src_refactored/common/sender/init/shy_init_injections.cpp
+++ src_refactored/common/sender/init/shy_init_injections.cpp
@@ -41,6 +41,7 @@
 #include "../../logic/main_menu/letters/animation/unselection_weight/shy_unselection_weight_injections.h"
 #include "../../logic/main_menu/letters/layout/position/shy_position_injections.h"
 #include "../../logic/main_menu/letters/layout/row_rect/shy_row_rect_injections.h"
+#include "../../logic/main_menu/letters/meshes_creation_director/shy_meshes_creation_director_injections.h"
 #include "../../logic/main_menu/letters/storage/shy_storage_injections.h"
 #include "../../logic/main_menu/shy_main_menu_injections.h"
 
