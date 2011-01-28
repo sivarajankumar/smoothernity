53b7f59c304ef278756c8ff2d89ebf6b5161a77e
olegus8 <olegus8@localhost>
2011-01-28 15:39:54 +0000

    working on common messages refactoring

---
+++ src_refactored/common/sender/done/shy_done.hpp
@@ -0,0 +1,4 @@
+void shy_sender_common_done :: send ( so_called_message_common_done msg )
+{
+    //_receivers . get ( ) . engine_render . get ( ) . receive ( msg ) ;
+}
---
+++ src_refactored/common/sender/done/shy_done_injections.cpp
@@ -0,0 +1,3 @@
+#include "./shy_done_injections.h"
+
+#include "./shy_done.hpp"
