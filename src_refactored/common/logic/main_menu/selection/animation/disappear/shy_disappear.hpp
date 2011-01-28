b8b1d6b8e7780848456fef6c0036369a544a82ec
olegus8 <olegus8@localhost>
2011-01-28 12:11:19 +0000

    working on common messages refactoring

---
+++ src_refactored/common/logic/main_menu/selection/animation/disappear/shy_disappear.hpp
@@ -0,0 +1,6 @@
+typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_main_menu_selection_animation_disappear > _scheduled_context_type ;
+template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void _shy_common_logic_main_menu_selection_animation_disappear :: receive ( so_called_message_common_init )
+{
+}
