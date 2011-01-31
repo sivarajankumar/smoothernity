0797f3a0199b8c0a47371c5b65cb1f627e0b1bea
olegus8 <olegus8@localhost>
2011-01-31 17:54:00 +0000

    working on common logic core refactoring

--- src_refactored/common/logic/core/shy_core.hpp
+++ src_refactored/common/logic/core/shy_core.hpp
@@ -7,13 +7,43 @@ public :
         static const so_called_type_platform_math_num_fract z_far ;
         static const so_called_type_platform_math_num_fract z_near ;
     } ;
+public :
+    static void init_render ( ) ;
+    static void get_near_plane_distance ( so_called_type_platform_math_num_fract & ) ;
+public :
+    static so_called_type_platform_math_num_whole fidget_prepared ;
+    
+    static so_called_type_platform_math_num_whole render_aspect_requested ;
+    static so_called_type_platform_math_num_fract render_aspect_width ;
+    static so_called_type_platform_math_num_fract render_aspect_height ;
+    
+    static so_called_type_platform_math_num_whole handling_near_plane_distance_request ;
+    static so_called_type_platform_math_num_whole handling_use_ortho_projection_request ;
+    static so_called_type_platform_math_num_whole handling_use_perspective_projection_request ;
 } ;
 
 const so_called_type_platform_math_num_fract shy_guts :: consts :: z_far = so_called_platform_math :: init_num_fract ( 50 , 1 ) ;
 const so_called_type_platform_math_num_fract shy_guts :: consts :: z_near = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
 
+so_called_type_platform_math_num_whole shy_guts :: fidget_prepared ;
+
+so_called_type_platform_math_num_whole shy_guts :: render_aspect_requested ;
+so_called_type_platform_math_num_fract shy_guts :: render_aspect_width ;
+so_called_type_platform_math_num_fract shy_guts :: render_aspect_height ;
+
+so_called_type_platform_math_num_whole shy_guts :: handling_near_plane_distance_request ;
+so_called_type_platform_math_num_whole shy_guts :: handling_use_ortho_projection_request ;
+so_called_type_platform_math_num_whole shy_guts :: handling_use_perspective_projection_request ;
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_core > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: init_render ( )
+{
+    so_called_sender_common_engine_render_blend_disable :: send ( so_called_message_common_engine_render_blend_disable ( ) ) ;
+    so_called_sender_common_engine_render_enable_face_culling :: send ( so_called_message_common_engine_render_enable_face_culling ( ) ) ;
+    so_called_sender_common_engine_render_texture_mode_modulate :: send ( so_called_message_common_engine_render_texture_mode_modulate ( ) ) ;
+}
 
 void _shy_common_logic_core :: receive ( so_called_message_common_engine_render_aspect_reply )
 {
@@ -21,6 +51,12 @@ void _shy_common_logic_core :: receive ( so_called_message_common_engine_render_
 
 void _shy_common_logic_core :: receive ( so_called_message_common_init )
 {
+    shy_guts :: render_aspect_requested = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: handling_near_plane_distance_request = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: handling_use_ortho_projection_request = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: handling_use_perspective_projection_request = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: fidget_prepared = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: init_render ( ) ;
 }
 
 void _shy_common_logic_core :: receive ( so_called_message_common_logic_core_near_plane_distance_request )
--- src_refactored/common/logic/core/shy_core_injections.cpp
+++ src_refactored/common/logic/core/shy_core_injections.cpp
@@ -1,5 +1,10 @@
 #include "./shy_core_injections.h"
 
+#include "../../engine/render/sender/blend_disable/shy_blend_disable_injections.h"
+#include "../../engine/render/sender/enable_face_culling/shy_enable_face_culling_injections.h"
+#include "../../engine/render/sender/texture_mode_modulate/shy_texture_mode_modulate_injections.h"
+
+#include "../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../injections/platform/math/shy_math.h"
 
 #include "./shy_core.hpp"
