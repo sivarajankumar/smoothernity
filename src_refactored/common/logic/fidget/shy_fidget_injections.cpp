c94fbdf5299f5aca95d9c37b3f5f913ea0621438
olegus8 <olegus8@localhost>
2011-02-01 16:46:23 +0000

    working on common logic fidget refactoring

--- src_refactored/common/logic/fidget/shy_fidget_injections.cpp
+++ src_refactored/common/logic/fidget/shy_fidget_injections.cpp
@@ -8,8 +8,12 @@
 #include "../../engine/render/sender/aspect_request/shy_aspect_request_injections.h"
 #include "../../engine/render/sender/frame_loss_request/shy_frame_loss_request_injections.h"
 #include "../../engine/render/sender/mesh_create_request/shy_mesh_create_request_injections.h"
+#include "../../engine/render/sender/mesh_finalize/shy_mesh_finalize_injections.h"
 #include "../../engine/render/sender/mesh_render/shy_mesh_render_injections.h"
 #include "../../engine/render/sender/mesh_set_transform/shy_mesh_set_transform_injections.h"
+#include "../../engine/render/sender/mesh_set_triangle_fan_index_value/shy_mesh_set_triangle_fan_index_value_injections.h"
+#include "../../engine/render/sender/mesh_set_vertex_color/shy_mesh_set_vertex_color_injections.h"
+#include "../../engine/render/sender/mesh_set_vertex_position/shy_mesh_set_vertex_position_injections.h"
 #include "../../engine/render/sender/texture_unselect/shy_texture_unselect_injections.h"
 
 #include "../../../injections/platform/conditions/shy_conditions.h"
