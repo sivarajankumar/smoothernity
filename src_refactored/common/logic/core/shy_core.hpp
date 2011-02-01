d32b166e1060754acc21590ea83c088fd368039d
olegus8 <olegus8@localhost>
2011-02-01 16:02:40 +0000

    working on guts refactoring

--- src_refactored/common/logic/core/shy_core.hpp
+++ src_refactored/common/logic/core/shy_core.hpp
@@ -1,16 +1,14 @@
-class shy_guts
+namespace shy_guts
 {
-public :
-    class consts
+    namespace consts
     {
-    public :
-        static const so_called_type_platform_math_num_fract z_far ;
-        static const so_called_type_platform_math_num_fract z_near ;
-    } ;
-public :
+        static const so_called_type_platform_math_num_fract z_far = so_called_platform_math :: init_num_fract ( 50 , 1 ) ;
+        static const so_called_type_platform_math_num_fract z_near = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
+    }
+
     static void init_render ( ) ;
     static void get_near_plane_distance ( so_called_type_platform_math_num_fract & ) ;
-public :
+
     static so_called_type_platform_math_num_whole fidget_prepared ;
     
     static so_called_type_platform_math_num_whole render_aspect_requested ;
@@ -20,20 +18,7 @@ public :
     static so_called_type_platform_math_num_whole handling_near_plane_distance_request ;
     static so_called_type_platform_math_num_whole handling_use_ortho_projection_request ;
     static so_called_type_platform_math_num_whole handling_use_perspective_projection_request ;
-} ;
-
-const so_called_type_platform_math_num_fract shy_guts :: consts :: z_far = so_called_platform_math :: init_num_fract ( 50 , 1 ) ;
-const so_called_type_platform_math_num_fract shy_guts :: consts :: z_near = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
-
-so_called_type_platform_math_num_whole shy_guts :: fidget_prepared ;
-
-so_called_type_platform_math_num_whole shy_guts :: render_aspect_requested ;
-so_called_type_platform_math_num_fract shy_guts :: render_aspect_width ;
-so_called_type_platform_math_num_fract shy_guts :: render_aspect_height ;
-
-so_called_type_platform_math_num_whole shy_guts :: handling_near_plane_distance_request ;
-so_called_type_platform_math_num_whole shy_guts :: handling_use_ortho_projection_request ;
-so_called_type_platform_math_num_whole shy_guts :: handling_use_perspective_projection_request ;
+}
 
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_core > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
