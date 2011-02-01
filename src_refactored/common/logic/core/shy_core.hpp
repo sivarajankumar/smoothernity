00df074029b4f3a2c216abb98cbf535e2553e9ac
olegus8 <olegus8@localhost>
2011-02-01 14:34:11 +0000

    working on common logic core refactoring

--- src_refactored/common/logic/core/shy_core.hpp
+++ src_refactored/common/logic/core/shy_core.hpp
@@ -45,8 +45,74 @@ void shy_guts :: init_render ( )
     so_called_sender_common_engine_render_texture_mode_modulate :: send ( so_called_message_common_engine_render_texture_mode_modulate ( ) ) ;
 }
 
-void _shy_common_logic_core :: receive ( so_called_message_common_engine_render_aspect_reply )
+void shy_guts :: get_near_plane_distance ( so_called_type_platform_math_num_fract & result )
 {
+    so_called_platform_math :: add_fracts ( result , shy_guts :: render_aspect_width , shy_guts :: render_aspect_height ) ;
+}
+
+void _shy_common_logic_core :: receive ( so_called_message_common_engine_render_aspect_reply msg )
+{
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: render_aspect_requested ) )
+    {
+        shy_guts :: render_aspect_requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: render_aspect_width = msg . width ;
+        shy_guts :: render_aspect_height = msg . height ;
+        if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: handling_near_plane_distance_request ) )
+        {
+            shy_guts :: handling_near_plane_distance_request = so_called_platform_math_consts :: whole_false ;
+            so_called_message_common_logic_core_near_plane_distance_reply near_plane_distance_reply_msg ;
+            shy_guts :: get_near_plane_distance ( near_plane_distance_reply_msg . distance ) ;
+            so_called_sender_common_logic_core_near_plane_distance_reply :: send ( near_plane_distance_reply_msg ) ;
+        }
+        if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: handling_use_ortho_projection_request ) )
+        {
+            shy_guts :: handling_use_ortho_projection_request = so_called_platform_math_consts :: whole_false ;
+            
+            so_called_type_platform_math_num_fract width = shy_guts :: render_aspect_width ;
+            so_called_type_platform_math_num_fract height = shy_guts :: render_aspect_height ;
+            so_called_type_platform_math_num_fract neg_width ;
+            so_called_type_platform_math_num_fract neg_height ;
+            so_called_platform_math :: neg_fract ( neg_width , width ) ;
+            so_called_platform_math :: neg_fract ( neg_height , height ) ;
+            
+            so_called_message_common_engine_render_projection_ortho proj_msg ;
+            proj_msg . x_left = neg_width ;
+            proj_msg . x_right = width ;
+            proj_msg . y_bottom = neg_height ;
+            proj_msg . y_top = height ;
+            proj_msg . z_near = shy_guts :: consts :: z_near ;
+            proj_msg . z_far = shy_guts :: consts :: z_far ;
+            so_called_sender_common_engine_render_projection_ortho :: send ( proj_msg ) ;
+            
+            so_called_sender_common_engine_render_matrix_identity :: send ( so_called_message_common_engine_render_matrix_identity ( ) ) ;
+            so_called_sender_common_logic_core_use_ortho_projection_reply :: send ( so_called_message_common_logic_core_use_ortho_projection_reply ( ) ) ;
+        }
+        if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: handling_use_perspective_projection_request ) )
+        {
+            shy_guts :: handling_use_perspective_projection_request = so_called_platform_math_consts :: whole_false ;
+        
+            so_called_type_platform_math_num_fract width = shy_guts :: render_aspect_width ;
+            so_called_type_platform_math_num_fract height = shy_guts :: render_aspect_height ;
+            so_called_type_platform_math_num_fract neg_width ;
+            so_called_type_platform_math_num_fract neg_height ;
+            so_called_type_platform_math_num_fract z_near ;
+            shy_guts :: get_near_plane_distance ( z_near ) ;
+            so_called_platform_math :: neg_fract ( neg_width , width ) ;
+            so_called_platform_math :: neg_fract ( neg_height , height ) ;
+            
+            so_called_message_common_engine_render_projection_frustum proj_msg ;
+            proj_msg . x_left = neg_width ;
+            proj_msg . x_right = width ;
+            proj_msg . y_bottom = neg_height ;
+            proj_msg . y_top = height ;
+            proj_msg . z_near = z_near ;
+            proj_msg . z_far = shy_guts :: consts :: z_far ;
+            so_called_sender_common_engine_render_projection_frustum :: send ( proj_msg ) ;
+            
+            so_called_sender_common_engine_render_matrix_identity :: send ( so_called_message_common_engine_render_matrix_identity ( ) ) ;
+            so_called_sender_common_logic_core_use_perspective_projection_reply :: send ( so_called_message_common_logic_core_use_perspective_projection_reply ( ) ) ;
+        }
+    }
 }
 
 void _shy_common_logic_core :: receive ( so_called_message_common_init )
--- src_refactored/common/logic/core/shy_core_injections.cpp
+++ src_refactored/common/logic/core/shy_core_injections.cpp
@@ -1,5 +1,9 @@
 #include "./shy_core_injections.h"
 
+#include "./sender/near_plane_distance_reply/shy_near_plane_distance_reply_injections.h"
+#include "./sender/use_ortho_projection_reply/shy_use_ortho_projection_reply_injections.h"
+#include "./sender/use_perspective_projection_reply/shy_use_perspective_projection_reply_injections.h"
+
 #include "../application/sender/render/shy_render_injections.h"
 #include "../application/sender/update/shy_update_injections.h"
 #include "../fidget/sender/prepare_permit/shy_prepare_permit_injections.h"
@@ -8,6 +12,9 @@
 #include "../../engine/render/sender/aspect_request/shy_aspect_request_injections.h"
 #include "../../engine/render/sender/blend_disable/shy_blend_disable_injections.h"
 #include "../../engine/render/sender/enable_face_culling/shy_enable_face_culling_injections.h"
+#include "../../engine/render/sender/matrix_identity/shy_matrix_identity_injections.h"
+#include "../../engine/render/sender/projection_frustum/shy_projection_frustum_injections.h"
+#include "../../engine/render/sender/projection_ortho/shy_projection_ortho_injections.h"
 #include "../../engine/render/sender/texture_mode_modulate/shy_texture_mode_modulate_injections.h"
 
 #include "../../../injections/platform/conditions/shy_conditions.h"
