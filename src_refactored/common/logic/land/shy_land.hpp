9d6bb637c54d9daf5781f8011db3de47d69f2eb4
olegus8 <olegus8@localhost>
2011-02-07 17:05:35 +0000

    working on common logic land refactoring

--- src_refactored/common/logic/land/shy_land.hpp
+++ src_refactored/common/logic/land/shy_land.hpp
@@ -17,10 +17,42 @@ namespace shy_guts
         static so_called_type_platform_math_num_fract land_g = so_called_platform_math :: init_num_fract ( 255 , 255 ) ;
         static so_called_type_platform_math_num_fract land_b = so_called_platform_math :: init_num_fract ( 255 , 255 ) ;
     }
+
+    static void render_land ( ) ;
+    static void create_land_mesh ( ) ;
+    static void create_land_texture ( ) ;
+    static void mesh_set_triangle_strip_index_value ( so_called_type_platform_math_num_whole offset , so_called_type_platform_math_num_whole index ) ;
+
+    static so_called_type_platform_math_num_whole land_mesh_created ;
+    static so_called_type_platform_math_num_whole land_texture_created ;
+    static so_called_type_platform_math_num_whole land_prepare_permitted ;
+    static so_called_type_platform_math_num_whole land_texture_creation_row ;
+    static so_called_type_platform_math_num_fract land_scale ;
+    static so_called_type_platform_math_num_whole texture_create_requested ;
+    static so_called_type_platform_math_num_whole texture_create_replied ;
+    static so_called_type_platform_math_num_whole mesh_create_requested ;
+    static so_called_type_common_engine_render_mesh_id land_mesh_id ;
+    static so_called_type_common_engine_render_texture_id land_texture_id ;
 }
 
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_land > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: render_land ( )
+{
+}
+
+void shy_guts :: create_land_mesh ( )
+{
+}
+
+void shy_guts :: create_land_texture ( )
+{
+}
+
+void shy_guts :: mesh_set_triangle_strip_index_value ( so_called_type_platform_math_num_whole offset , so_called_type_platform_math_num_whole index )
+{
+}
 
 void _shy_common_logic_land :: receive ( so_called_message_common_engine_render_mesh_create_reply )
 {
--- src_refactored/common/logic/land/shy_land_injections.cpp
+++ src_refactored/common/logic/land/shy_land_injections.cpp
@@ -1,5 +1,8 @@
 #include "./shy_land_injections.h"
 
+#include "../../engine/render/type/mesh_id/shy_mesh_id_injections.h"
+#include "../../engine/render/type/texture_id/shy_texture_id_injections.h"
+
 #include "../../../injections/platform/math/shy_math.h"
 
 #include "./shy_land.hpp"
