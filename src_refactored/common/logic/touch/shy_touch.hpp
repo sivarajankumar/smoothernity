658e3b5729b7f72ad557bcd1b61f5c8ec473fe82
olegus8 <olegus8@localhost>
2011-02-10 16:18:59 +0000

    working on common logic touch refactoring

--- src_refactored/common/logic/touch/shy_touch.hpp
+++ src_refactored/common/logic/touch/shy_touch.hpp
@@ -1,3 +1,35 @@
+namespace shy_guts
+{
+    namespace consts
+    {
+        static const so_called_type_platform_math_num_fract spot_r = so_called_platform_math :: init_num_fract ( 255 , 255 ) ;
+        static const so_called_type_platform_math_num_fract spot_g = so_called_platform_math :: init_num_fract ( 255 , 255 ) ;
+        static const so_called_type_platform_math_num_fract spot_b = so_called_platform_math :: init_num_fract ( 255 , 255 ) ;
+        static const so_called_type_platform_math_num_fract spot_pos_z = so_called_platform_math :: init_num_fract ( - 3 , 1 ) ;
+        static const so_called_type_platform_math_num_fract spot_size = so_called_platform_math :: init_num_fract ( 3 , 10 ) ;
+        static const so_called_type_platform_math_num_whole spot_edges = so_called_platform_math :: init_num_whole ( 32 ) ;
+        static const so_called_type_platform_math_num_whole spot_lifetime_in_frames = so_called_platform_math :: init_num_whole ( 60 ) ;
+    }
+
+    static void update_spot ( ) ;
+	static void decrease_spot_lifetime ( ) ;
+	static void poll_touchscreen ( ) ;
+    static void poll_mouse ( ) ;
+	static void place_new_spot ( ) ;
+    static void render_spot_mesh ( ) ;
+    static void create_spot_mesh ( ) ;
+
+    static so_called_type_platform_math_num_whole spot_frames_left ;
+    static so_called_type_platform_math_num_whole spot_mesh_created ;
+    static so_called_type_platform_math_num_whole spot_prepare_permitted ;
+	static so_called_type_platform_math_num_whole should_place_new_spot ;
+    static so_called_type_platform_math_num_whole mesh_create_requested ;
+	static so_called_type_platform_math_num_fract spot_x ;
+	static so_called_type_platform_math_num_fract spot_y ;
+    static so_called_type_common_engine_render_mesh_id spot_mesh_id ;
+    static so_called_type_platform_vector_data spot_position ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_touch > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
--- src_refactored/common/logic/touch/shy_touch_injections.cpp
+++ src_refactored/common/logic/touch/shy_touch_injections.cpp
@@ -1,4 +1,6 @@
 #include "./shy_touch_injections.h"
 
-#include "./shy_touch.hpp"
+#include "../../../injections/platform/math/shy_math.h"
+#include "../../../injections/platform/vector/shy_vector.h" 
 
+#include "./shy_touch.hpp"
