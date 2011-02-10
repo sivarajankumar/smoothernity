cae8db78c4262f620f7063de20380cdee06eb844
olegus8 <olegus8@localhost>
2011-02-10 15:59:23 +0000

    working on common logic touch refactoring

--- src_refactored/common/logic/touch/shy_touch.hpp
+++ src_refactored/common/logic/touch/shy_touch.hpp
@@ -12,3 +12,7 @@ void _shy_common_logic_touch :: receive ( so_called_message_common_init )
 void _shy_common_logic_touch :: receive ( so_called_message_common_logic_touch_prepare_permit )
 {
 }
+
+void _shy_common_logic_touch :: receive ( so_called_message_common_logic_touch_render )
+{
+}
--- src_refactored/common/logic/touch/shy_touch_injections.h
+++ src_refactored/common/logic/touch/shy_touch_injections.h
@@ -2,6 +2,7 @@
 #define _shy_common_logic_touch_injections_included
 
 #include "./message/prepare_permit/shy_prepare_permit_injections.h"
+#include "./message/render/shy_render_injections.h"
 
 #include "../../engine/render/message/mesh_create_reply/shy_mesh_create_reply_injections.h"
 
