6566ce2edb0dbfa96497bce8f797b5ec595a0a3f
olegus8 <olegus8@localhost>
2011-02-11 15:17:40 +0000

    working on common logic text refactoring

---
+++ src_refactored/common/logic/text/consts/shy_consts.h
@@ -0,0 +1,36 @@
+class shy_common_logic_text_consts
+{
+public :
+    class alphabet_english
+    {
+    public :
+        static const so_called_type_common_logic_text_letter_id A ;
+        static const so_called_type_common_logic_text_letter_id B ;
+        static const so_called_type_common_logic_text_letter_id C ;
+        static const so_called_type_common_logic_text_letter_id D ;
+        static const so_called_type_common_logic_text_letter_id E ;
+        static const so_called_type_common_logic_text_letter_id F ;
+        static const so_called_type_common_logic_text_letter_id G ;
+        static const so_called_type_common_logic_text_letter_id H ;
+        static const so_called_type_common_logic_text_letter_id I ;
+        static const so_called_type_common_logic_text_letter_id J ;
+        static const so_called_type_common_logic_text_letter_id K ;
+        static const so_called_type_common_logic_text_letter_id L ;
+        static const so_called_type_common_logic_text_letter_id M ;
+        static const so_called_type_common_logic_text_letter_id N ;
+        static const so_called_type_common_logic_text_letter_id O ;
+        static const so_called_type_common_logic_text_letter_id P ;
+        static const so_called_type_common_logic_text_letter_id Q ;
+        static const so_called_type_common_logic_text_letter_id R ;
+        static const so_called_type_common_logic_text_letter_id S ;
+        static const so_called_type_common_logic_text_letter_id T ;
+        static const so_called_type_common_logic_text_letter_id U ;
+        static const so_called_type_common_logic_text_letter_id V ;
+        static const so_called_type_common_logic_text_letter_id W ;
+        static const so_called_type_common_logic_text_letter_id X ;
+        static const so_called_type_common_logic_text_letter_id Y ;
+        static const so_called_type_common_logic_text_letter_id Z ;
+    } ;
+
+    static const so_called_type_common_logic_text_letter_id whitespace ;
+} ;
---
+++ src_refactored/common/logic/text/consts/shy_consts_injections.h
@@ -0,0 +1,10 @@
+#ifndef _shy_common_logic_text_consts_injections_included
+#define _shy_common_logic_text_consts_injections_included
+
+#include "../type/letter_id/shy_letter_id_injections.h"
+
+#include "./shy_consts.h"
+
+typedef shy_common_logic_text_consts so_called_common_logic_text_consts ;
+
+#endif
