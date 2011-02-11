71de951338194b227a140cef7981a20c430759c2
olegus8 <olegus8@localhost>
2011-02-11 16:07:23 +0000

    working on common logic text refactoring

---
+++ src_refactored/common/logic/text/message/letter_small_tex_coords_reply/shy_letter_small_tex_coords_reply.h
@@ -0,0 +1,9 @@
+class shy_message_common_logic_text_letter_small_tex_coords_reply
+{
+public :
+    so_called_type_platform_math_num_fract left ;
+    so_called_type_platform_math_num_fract bottom ;
+    so_called_type_platform_math_num_fract right ;
+    so_called_type_platform_math_num_fract top ;
+    so_called_type_common_logic_text_letter_id letter ;
+} ;
