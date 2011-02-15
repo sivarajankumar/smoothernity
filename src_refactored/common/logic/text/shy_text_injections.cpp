92c7023759574f615d36e534e962d045ba267b7d
olegus8 <olegus8@localhost>
2011-02-15 15:08:27 +0000

    working on common logic text refactoring

--- src_refactored/common/logic/text/shy_text_injections.cpp
+++ src_refactored/common/logic/text/shy_text_injections.cpp
@@ -6,8 +6,10 @@
 #include "./sender/render_reply/shy_render_reply_injections.h"
 #include "./sender/use_text_texture_reply/shy_use_text_texture_reply_injections.h"
 
+#include "../../engine/math/stateless/shy_stateless_injections.h"
 #include "../../engine/rasterizer/sender/finalize_request/shy_finalize_request_injections.h"
 #include "../../engine/render/sender/mesh_create_request/shy_mesh_create_request_injections.h"
+#include "../../engine/render/sender/mesh_set_transform/shy_mesh_set_transform_injections.h"
 #include "../../engine/render/sender/texture_create_request/shy_texture_create_request_injections.h"
 #include "../../engine/render/sender/texture_finalize/shy_texture_finalize_injections.h"
 #include "../../engine/render/sender/texture_select/shy_texture_select_injections.h"
@@ -15,6 +17,7 @@
 #include "../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../injections/platform/math/shy_math.h"
+#include "../../../injections/platform/matrix/shy_matrix.h"
 #include "../../../injections/platform/pointer/type/data/shy_data.h"
 #include "../../../injections/platform/render/type/texel_data/shy_texel_data.h"
 #include "../../../injections/platform/static_array/shy_static_array.h"
