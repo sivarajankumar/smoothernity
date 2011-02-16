24299b738785a4980e5521a58825b3db10a0c2bc
olegus8 <olegus8@localhost>
2011-02-16 16:12:04 +0000

    working on common logic text refactoring

--- src_refactored/common/logic/text/shy_text.hpp
+++ src_refactored/common/logic/text/shy_text.hpp
@@ -1381,6 +1381,35 @@ void shy_guts :: rasterize_font_english_Y ( )
 
 void shy_guts :: rasterize_font_english_Z ( )
 {
+    shy_guts :: prepare_rasterizer_for_drawing ( ) ;
+    
+    so_called_type_platform_math_num_whole hor_left ;
+    so_called_type_platform_math_num_whole hor_right ;
+    so_called_type_platform_math_num_whole high_top ;
+    so_called_type_platform_math_num_whole high_bottom ;
+    hor_left = so_called_platform_math_consts :: whole_0 ;
+    so_called_platform_math :: mul_wholes ( hor_right , shy_guts :: letter_size_x , so_called_platform_math_consts :: whole_4 ) ;
+    so_called_platform_math :: div_whole_by ( hor_right , so_called_platform_math_consts :: whole_5 ) ;    
+    so_called_platform_math :: sub_wholes ( high_top , shy_guts :: letter_size_y , so_called_platform_math_consts :: whole_1 ) ;    
+    so_called_platform_math :: mul_wholes ( high_bottom , shy_guts :: letter_size_y , so_called_platform_math_consts :: whole_4 ) ;
+    so_called_platform_math :: div_whole_by ( high_bottom , so_called_platform_math_consts :: whole_5 ) ;    
+    shy_guts :: rasterize_use_texel ( shy_guts :: filler ) ;
+    shy_guts :: rasterize_rect ( hor_left , high_top , hor_right , high_bottom ) ;
+    
+    so_called_type_platform_math_num_whole low_top ;
+    so_called_type_platform_math_num_whole low_bottom ;
+    so_called_platform_math :: div_wholes ( low_top , shy_guts :: letter_size_y , so_called_platform_math_consts :: whole_5 ) ;    
+    low_bottom = so_called_platform_math_consts :: whole_0 ;
+    shy_guts :: rasterize_use_texel ( shy_guts :: filler ) ;
+    shy_guts :: rasterize_rect ( hor_left , low_top , hor_right , low_bottom ) ;
+    
+    so_called_type_platform_math_num_whole board_width ;
+    so_called_type_platform_math_num_whole right_minus_board_width ;
+    so_called_platform_math :: div_wholes ( board_width , shy_guts :: letter_size_y , so_called_platform_math_consts :: whole_4 ) ;    
+    so_called_platform_math :: sub_wholes ( right_minus_board_width , hor_right , board_width ) ;
+    shy_guts :: rasterize_use_texel ( shy_guts :: filler ) ;
+    shy_guts :: rasterize_triangle ( right_minus_board_width , high_bottom , hor_right , high_bottom , board_width , low_top ) ;
+    shy_guts :: rasterize_triangle ( right_minus_board_width , high_bottom , hor_left , low_top , board_width , low_top ) ;
 }
 
 void shy_guts :: rasterize_use_texel ( so_called_type_platform_render_texel_data texel )
