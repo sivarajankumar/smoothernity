a9135a969a99e43ee92d25d1cfcc913df7f29422
olegus8 <olegus8@localhost>
2011-02-16 16:35:58 +0000

    working on common logic title refactoring

---
+++ src_refactored/common/autogenerated/shy_autogenerated.hpp
@@ -0,0 +1,12 @@
+const so_called_type_platform_math_num_whole shy_common_logic_title_consts :: appear_duration_in_frames = so_called_platform_math :: init_num_whole ( 250 ) ;
+const so_called_type_platform_math_num_whole shy_common_logic_title_consts :: disappear_duration_in_frames = so_called_platform_math :: init_num_whole ( 150 ) ;
+const so_called_type_platform_math_num_whole shy_common_logic_title_consts :: frames_between_letters = so_called_platform_math :: init_num_whole ( 5 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_title_consts :: appear_pos_angle_periods = so_called_platform_math :: init_num_fract ( 11 , 2 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_title_consts :: appear_rubber_first = so_called_platform_math :: init_num_fract ( 19 , 20 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_title_consts :: appear_rubber_last = so_called_platform_math :: init_num_fract ( 19 , 20 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_title_consts :: disappear_pos_angle_periods = so_called_platform_math :: init_num_fract ( 22 , 2 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_title_consts :: disappear_rubber_first = so_called_platform_math :: init_num_fract ( 59 , 60 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_title_consts :: disappear_rubber_last = so_called_platform_math :: init_num_fract ( 29 , 30 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_title_consts :: scene_scale_max = so_called_platform_math :: init_num_fract ( 9 , 10 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_title_consts :: scene_scale_min = so_called_platform_math :: init_num_fract ( 7 , 10 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_title_consts :: spin_radius_in_letters = so_called_platform_math :: init_num_fract ( 2 , 1 ) ;
