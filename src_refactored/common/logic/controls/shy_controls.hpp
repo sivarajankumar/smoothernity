4ab9903630815d35c069a411d572fa3898d2dda3
olegus8 <olegus8@localhost>
2011-02-18 17:33:39 +0000

    working on common logic controls refactoring

--- src_refactored/common/logic/controls/shy_controls.hpp
+++ src_refactored/common/logic/controls/shy_controls.hpp
@@ -21,6 +21,13 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void shy_guts :: compute_identity_state ( )
 {
+    so_called_type_platform_math_num_fract cursor_x ;
+    so_called_type_platform_math_num_fract cursor_y ;
+    so_called_type_platform_math_num_whole primary_button_down ;
+
+    cursor_x = so_called_platform_math_consts :: fract_0 ;
+    cursor_y = so_called_platform_math_consts :: fract_0 ;
+    primary_button_down = so_called_platform_math_consts :: whole_false ;
 }
 
 void shy_guts :: compute_mouse_state ( )
@@ -41,4 +48,8 @@ void _shy_common_logic_controls :: receive ( so_called_message_common_init )
 
 void _shy_common_logic_controls :: receive ( so_called_message_common_logic_controls_state_request )
 {
+    shy_guts :: compute_identity_state ( ) ;
+    shy_guts :: compute_mouse_state ( ) ;
+    shy_guts :: compute_touch_state ( ) ;
+    shy_guts :: reply_controls_state ( ) ;
 }
