33f5076f8bcd322ca404cc4b17bc476a55a15374
olegus8 <olegus8@localhost>
2011-02-18 17:30:49 +0000

    working on common logic controls refactoring

--- src_refactored/common/logic/controls/shy_controls.hpp
+++ src_refactored/common/logic/controls/shy_controls.hpp
@@ -1,5 +1,39 @@
+namespace shy_guts
+{
+    class logic_controls_state_type
+    {
+    public :
+        so_called_type_platform_math_num_whole primary_button_down ;
+        so_called_type_platform_math_num_fract cursor_x ;
+        so_called_type_platform_math_num_fract cursor_y ;
+    } ;
+
+    static void compute_identity_state ( ) ;
+    static void compute_mouse_state ( ) ;
+    static void compute_touch_state ( ) ;
+    static void reply_controls_state ( ) ;
+
+    static logic_controls_state_type logic_controls_state ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_controls > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: compute_identity_state ( )
+{
+}
+
+void shy_guts :: compute_mouse_state ( )
+{
+}
+
+void shy_guts :: compute_touch_state ( )
+{
+}
+
+void shy_guts :: reply_controls_state ( )
+{
+}
 
 void _shy_common_logic_controls :: receive ( so_called_message_common_init )
 {
--- src_refactored/common/logic/controls/shy_controls_injections.cpp
+++ src_refactored/common/logic/controls/shy_controls_injections.cpp
@@ -1,3 +1,5 @@
 #include "./shy_controls_injections.h"
 
+#include "../../../injections/platform/math/shy_math.h"
+
 #include "./shy_controls.hpp"
