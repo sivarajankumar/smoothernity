fc07e0b7ae7f01b4c2cb03fb6ea197e7a71245d3
olegus8 <olegus8@localhost>
2011-02-18 17:43:40 +0000

    working on common logic controls refactoring

--- src_refactored/common/logic/controls/shy_controls.hpp
+++ src_refactored/common/logic/controls/shy_controls.hpp
@@ -1,19 +1,16 @@
 namespace shy_guts
 {
-    class logic_controls_state_type
+    namespace logic_controls_state
     {
-    public :
-        so_called_type_platform_math_num_whole primary_button_down ;
-        so_called_type_platform_math_num_fract cursor_x ;
-        so_called_type_platform_math_num_fract cursor_y ;
-    } ;
+        static so_called_type_platform_math_num_whole primary_button_down ;
+        static so_called_type_platform_math_num_fract cursor_x ;
+        static so_called_type_platform_math_num_fract cursor_y ;
+    }
 
     static void compute_identity_state ( ) ;
     static void compute_mouse_state ( ) ;
     static void compute_touch_state ( ) ;
     static void reply_controls_state ( ) ;
-
-    static logic_controls_state_type logic_controls_state ;
 }
 
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_controls > _scheduled_context_type ;
@@ -32,14 +29,51 @@ void shy_guts :: compute_identity_state ( )
 
 void shy_guts :: compute_mouse_state ( )
 {
+    so_called_type_platform_math_num_fract cursor_x ;
+    so_called_type_platform_math_num_fract cursor_y ;
+    so_called_type_platform_math_num_whole mouse_enabled ;
+    so_called_type_platform_math_num_whole primary_button_down ;
+
+    so_called_platform_mouse :: enabled ( mouse_enabled ) ;
+    if ( so_called_platform_conditions :: whole_is_true ( mouse_enabled ) )
+    {
+        so_called_platform_mouse :: left_button_down ( primary_button_down ) ;
+        so_called_platform_mouse :: x ( cursor_x ) ;
+        so_called_platform_mouse :: y ( cursor_y ) ;
+
+        shy_guts :: logic_controls_state :: cursor_x = cursor_x ;
+        shy_guts :: logic_controls_state :: cursor_y = cursor_y ;
+        shy_guts :: logic_controls_state :: primary_button_down = primary_button_down ;
+    }
 }
 
 void shy_guts :: compute_touch_state ( )
 {
+    so_called_type_platform_math_num_fract cursor_x ;
+    so_called_type_platform_math_num_fract cursor_y ;
+    so_called_type_platform_math_num_whole touch_enabled ;
+    so_called_type_platform_math_num_whole primary_button_down ;
+
+    so_called_platform_touch :: enabled ( touch_enabled ) ;
+    if ( so_called_platform_conditions :: whole_is_true ( touch_enabled ) )
+    {
+        so_called_platform_touch :: occured ( primary_button_down ) ;
+        so_called_platform_touch :: x ( cursor_x ) ;
+        so_called_platform_touch :: y ( cursor_y ) ;
+
+        shy_guts :: logic_controls_state :: cursor_x = cursor_x ;
+        shy_guts :: logic_controls_state :: cursor_y = cursor_y ;
+        shy_guts :: logic_controls_state :: primary_button_down = primary_button_down ;
+    }
 }
 
 void shy_guts :: reply_controls_state ( )
 {
+    so_called_message_common_logic_controls_state_reply msg ;
+    msg . primary_button_down = shy_guts :: logic_controls_state :: primary_button_down ;
+    msg . cursor_x = shy_guts :: logic_controls_state :: cursor_x ;
+    msg . cursor_y = shy_guts :: logic_controls_state :: cursor_y ;
+    so_called_sender_common_logic_controls_state_reply :: send ( msg ) ;
 }
 
 void _shy_common_logic_controls :: receive ( so_called_message_common_init )
