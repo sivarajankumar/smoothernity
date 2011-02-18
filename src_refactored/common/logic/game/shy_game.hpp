36bad6315c4573d0d112dda8e866c1306fb761cf
olegus8 <olegus8@localhost>
2011-02-18 15:41:29 +0000

    working on common logic game refactoring

--- src_refactored/common/logic/game/shy_game.hpp
+++ src_refactored/common/logic/game/shy_game.hpp
@@ -1,5 +1,70 @@
+namespace shy_guts
+{
+    namespace consts
+    {
+        static const so_called_type_platform_math_num_fract final_r = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+        static const so_called_type_platform_math_num_fract final_g = so_called_platform_math :: init_num_fract ( 1 , 10 ) ;
+        static const so_called_type_platform_math_num_fract final_b = so_called_platform_math :: init_num_fract ( 4 , 10 ) ;
+        static const so_called_type_platform_math_num_fract fog_far_shift = so_called_platform_math :: init_num_fract ( 20 , 1 ) ;
+        static const so_called_type_platform_math_num_fract fog_near_shift = so_called_platform_math :: init_num_fract ( 10 , 1 ) ;
+        static const so_called_type_platform_math_num_whole fade_in_frames = so_called_platform_math :: init_num_whole ( 90 ) ;
+    }
+
+    static void clear_screen ( ) ;
+    static void update_color ( ) ;
+    static void proceed_with_render ( ) ;
+
+    static so_called_type_platform_math_num_fract color_r ;
+    static so_called_type_platform_math_num_fract color_g ;
+    static so_called_type_platform_math_num_fract color_b ;
+    static so_called_type_platform_math_num_whole color_frames ;
+    static so_called_type_platform_math_num_whole game_launched ;
+    static so_called_type_platform_math_num_whole game_launch_permitted ;
+    
+    static so_called_type_platform_math_num_whole near_plane_distance_requested ;
+    static so_called_type_platform_math_num_whole near_plane_distance_replied ;
+    static so_called_type_platform_math_num_fract near_plane_distance ;
+    
+    static so_called_type_platform_math_num_whole camera_matrix_requested ;
+    static so_called_type_platform_math_num_whole camera_matrix_replied ;
+    static so_called_type_platform_matrix_data camera_matrix ;
+    
+    static so_called_type_platform_math_num_whole use_perspective_projection_requested ;
+    static so_called_type_platform_math_num_whole use_perspective_projection_replied ;
+
+    static so_called_type_platform_math_num_whole use_ortho_projection_requested ;
+    static so_called_type_platform_math_num_whole use_ortho_projection_replied ;
+    
+    static so_called_type_platform_math_num_whole land_render_requested ;
+    static so_called_type_platform_math_num_whole land_render_replied ;
+    
+    static so_called_type_platform_math_num_whole entities_render_requested ;
+    static so_called_type_platform_math_num_whole entities_render_replied ;
+    
+    static so_called_type_platform_math_num_whole fidget_render_requested ;
+    static so_called_type_platform_math_num_whole fidget_render_replied ;
+    
+    static so_called_type_platform_math_num_whole text_render_requested ;
+    static so_called_type_platform_math_num_whole text_render_replied ;
+
+    static so_called_type_platform_math_num_whole image_render_requested ;
+    static so_called_type_platform_math_num_whole image_render_replied ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_game > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: clear_screen ( )
+{
+}
+
+void shy_guts :: update_color ( )
+{
+}
+
+void shy_guts :: proceed_with_render ( )
+{
+}
 
 void _shy_common_logic_game :: receive ( so_called_message_common_init )
 {
--- src_refactored/common/logic/game/shy_game_injections.cpp
+++ src_refactored/common/logic/game/shy_game_injections.cpp
@@ -1,3 +1,5 @@
 #include "./shy_game_injections.h"
 
+#include "../../../injections/platform/math/shy_math.h"
+
 #include "./shy_game.hpp"
