c7ab3980e164338820fc8a91b5009f90a04aef19
olegus8 <olegus8@localhost>
2011-02-21 14:32:08 +0000

    working on common logic ortho refactoring

---
+++ src_refactored/common/logic/ortho/sender/planes_request/shy_planes_request_injections.cpp
@@ -0,0 +1,5 @@
+#include "./shy_planes_request_injections.h"
+
+#include "../../shy_ortho_injections.h"
+
+#include "./shy_planes_request.hpp"
--- src_refactored/common/logic/ortho/shy_ortho.hpp
+++ src_refactored/common/logic/ortho/shy_ortho.hpp
@@ -8,3 +8,7 @@ void _shy_common_logic_ortho :: receive ( so_called_message_common_engine_render
 void _shy_common_logic_ortho :: receive ( so_called_message_common_init )
 {
 }
+
+void _shy_common_logic_ortho :: receive ( so_called_message_common_logic_ortho_planes_request )
+{
+}
