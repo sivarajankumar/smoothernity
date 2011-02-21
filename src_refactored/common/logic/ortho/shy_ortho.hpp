76cec5dbcdde44c0918793c12b905f1c5f2d1925
olegus8 <olegus8@localhost>
2011-02-21 14:38:30 +0000

    working on common logic ortho refactoring

--- src_refactored/common/logic/ortho/shy_ortho.hpp
+++ src_refactored/common/logic/ortho/shy_ortho.hpp
@@ -1,5 +1,78 @@
+namespace shy_guts
+{
+    namespace logic_ortho_planes_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_fract x_left ;
+        static so_called_type_platform_math_num_fract x_right ;
+        static so_called_type_platform_math_num_fract y_bottom ;
+        static so_called_type_platform_math_num_fract y_top ;
+        static so_called_type_platform_math_num_fract z_near ;
+        static so_called_type_platform_math_num_fract z_far ;
+    }
+
+    namespace engine_render_aspect_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole replied ;
+        static so_called_type_platform_math_num_fract width ;
+        static so_called_type_platform_math_num_fract height ;
+    }
+
+    static void proceed_with_planes ( ) ;
+    static void request_aspect ( ) ;
+    static void compute_x_left ( ) ;
+    static void compute_x_right ( ) ;
+    static void compute_y_bottom ( ) ;
+    static void compute_y_top ( ) ;
+    static void compute_z_near ( ) ;
+    static void compute_z_far ( ) ;
+    static void reply_planes ( ) ;
+    static void reply_computed_planes ( ) ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_ortho > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: proceed_with_planes ( )
+{
+}
+
+void shy_guts :: request_aspect ( )
+{
+}
+
+void shy_guts :: compute_x_left ( )
+{
+}
+
+void shy_guts :: compute_x_right ( )
+{
+}
+
+void shy_guts :: compute_y_bottom ( )
+{
+}
+
+void shy_guts :: compute_y_top ( )
+{
+}
+
+void shy_guts :: compute_z_near ( )
+{
+}
+
+void shy_guts :: compute_z_far ( )
+{
+}
+
+void shy_guts :: reply_planes ( )
+{
+}
+
+void shy_guts :: reply_computed_planes ( )
+{
+}
 
 void _shy_common_logic_ortho :: receive ( so_called_message_common_engine_render_aspect_reply )
 {
--- src_refactored/common/logic/ortho/shy_ortho_injections.cpp
+++ src_refactored/common/logic/ortho/shy_ortho_injections.cpp
@@ -1,4 +1,6 @@
 #include "./shy_ortho_injections.h"
 
+#include "../../../injections/platform/math/shy_math.h"
+
 #include "./shy_ortho.hpp"
 
