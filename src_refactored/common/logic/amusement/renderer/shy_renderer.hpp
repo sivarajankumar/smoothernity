fc920d9578c3c4e2c176d5c0d5fc5b72a06ef5ef
olegus8 <olegus8@localhost>
2011-02-24 12:34:08 +0000

    working on common logic amusement renderer refactoring

--- src_refactored/common/logic/amusement/renderer/shy_renderer.hpp
+++ src_refactored/common/logic/amusement/renderer/shy_renderer.hpp
@@ -13,6 +13,10 @@ void _shy_common_logic_amusement_renderer :: receive ( so_called_message_common_
 {
 }
 
+void _shy_common_logic_amusement_renderer :: receive ( so_called_message_common_logic_door_render_reply )
+{
+}
+
 void _shy_common_logic_amusement_renderer :: receive ( so_called_message_common_logic_observer_animation_transform_reply )
 {
 }
