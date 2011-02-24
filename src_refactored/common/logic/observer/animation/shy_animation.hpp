c15708b41d9874109eaefd609e99edb9f9781a09
olegus8 <olegus8@localhost>
2011-02-24 15:07:39 +0000

    working on common logic observer animation refactoring

--- src_refactored/common/logic/observer/animation/shy_animation.hpp
+++ src_refactored/common/logic/observer/animation/shy_animation.hpp
@@ -33,18 +33,50 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void shy_guts :: proceed_with_transform ( )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_observer_animation_transform_state :: requested ) )
+    {
+        shy_guts :: logic_observer_animation_transform_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: request_flight_transform ( ) ;
+    }
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_observer_animation_flight_transform_state :: replied ) )
+    {
+        shy_guts :: logic_observer_animation_flight_transform_state :: replied = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: reply_computed_transform ( ) ;
+    }
 }
 
 void shy_guts :: request_flight_transform ( )
 {
+    shy_guts :: logic_observer_animation_flight_transform_state :: requested = so_called_platform_math_consts :: whole_true ;
+    so_called_sender_common_logic_observer_animation_flight_transform_request :: send ( so_called_message_common_logic_observer_animation_flight_transform_request ( ) ) ;
 }
 
 void shy_guts :: reply_computed_transform ( )
 {
+    shy_guts :: compute_transform ( ) ;
+    shy_guts :: reply_transform ( ) ;
 }
 
 void shy_guts :: compute_transform ( )
 {
+    so_called_type_platform_math_num_fract up_x ;
+    so_called_type_platform_math_num_fract up_y ;
+    so_called_type_platform_math_num_fract up_z ;
+    so_called_type_platform_vector_data up ;
+    so_called_type_platform_vector_data eye ;
+    so_called_type_platform_vector_data target ;
+    so_called_type_platform_matrix_data transform ;
+
+    up_x = shy_guts :: logic_observer_animation_consts :: up_x ;
+    up_y = shy_guts :: logic_observer_animation_consts :: up_y ;
+    up_z = shy_guts :: logic_observer_animation_consts :: up_z ;
+    eye = shy_guts :: logic_observer_animation_flight_transform_state :: eye ;
+    target = shy_guts :: logic_observer_animation_flight_transform_state :: target ;
+
+    so_called_platform_vector :: xyz ( up , up_x , up_y , up_z ) ;
+    so_called_common_engine_camera_stateless :: matrix_look_at ( transform , eye , target , up ) ;
+
+    shy_guts :: logic_observer_animation_transform_state :: transform = transform ;
 }
 
 void shy_guts :: reply_transform ( )
--- src_refactored/common/logic/observer/animation/shy_animation_injections.cpp
+++ src_refactored/common/logic/observer/animation/shy_animation_injections.cpp
@@ -1,9 +1,12 @@
 #include "./shy_animation_injections.h"
 
+#include "./sender/flight_transform_request/shy_flight_transform_request_injections.h"
+
 #include "../../../engine/camera/stateless/shy_stateless_injections.h"
 
 #include "../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../../injections/platform/math/shy_math.h"
+#include "../../../../injections/platform/vector/shy_vector.h"
 
 #include "./shy_animation.hpp"
