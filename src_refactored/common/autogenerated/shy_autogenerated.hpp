63cca909c448210b6101dae81ce1d16f699938f8
olegus8 <olegus8@localhost>
2011-02-24 16:19:14 +0000

    working on common logic blanket refactoring

--- src_refactored/common/autogenerated/shy_autogenerated.hpp
+++ src_refactored/common/autogenerated/shy_autogenerated.hpp
@@ -2,6 +2,16 @@ const so_called_type_platform_math_num_fract shy_common_logic_amusement_consts :
 const so_called_type_platform_math_num_fract shy_common_logic_amusement_consts :: renderer_clear_color_g = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
 const so_called_type_platform_math_num_fract shy_common_logic_amusement_consts :: renderer_clear_color_r = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
 
+const so_called_type_platform_math_num_fract shy_common_logic_blanket_consts :: mesh_color_a = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_blanket_consts :: mesh_color_b = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_blanket_consts :: mesh_color_g = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_blanket_consts :: mesh_color_r = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_blanket_consts :: mesh_vertex_x_left = so_called_platform_math :: init_num_fract ( - 1 , 2 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_blanket_consts :: mesh_vertex_x_right = so_called_platform_math :: init_num_fract ( 1 , 2 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_blanket_consts :: mesh_vertex_y_bottom = so_called_platform_math :: init_num_fract ( - 1 , 2 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_blanket_consts :: mesh_vertex_y_top = so_called_platform_math :: init_num_fract ( 1 , 2 ) ;
+const so_called_type_platform_math_num_fract shy_common_logic_blanket_consts :: mesh_vertex_z = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+
 const so_called_type_platform_math_num_fract shy_common_logic_observer_animation_consts :: flight_horizontal_offset_amplitude = so_called_platform_math :: init_num_fract ( 1 , 3 ) ;
 const so_called_type_platform_math_num_fract shy_common_logic_observer_animation_consts :: flight_horizontal_offset_period = so_called_platform_math :: init_num_fract ( 11 , 10 ) ;
 const so_called_type_platform_math_num_fract shy_common_logic_observer_animation_consts :: flight_target_z = so_called_platform_math :: init_num_fract ( - 1 , 1 ) ;
---
+++ src_refactored/common/logic/blanket/consts/shy_consts.h
@@ -0,0 +1,13 @@
+class shy_common_logic_blanket_consts
+{
+public :
+    static const so_called_type_platform_math_num_fract mesh_vertex_x_left ;
+    static const so_called_type_platform_math_num_fract mesh_vertex_x_right ;
+    static const so_called_type_platform_math_num_fract mesh_vertex_y_bottom ;
+    static const so_called_type_platform_math_num_fract mesh_vertex_y_top ;
+    static const so_called_type_platform_math_num_fract mesh_vertex_z ;
+    static const so_called_type_platform_math_num_fract mesh_color_r ;
+    static const so_called_type_platform_math_num_fract mesh_color_g ;
+    static const so_called_type_platform_math_num_fract mesh_color_b ;
+    static const so_called_type_platform_math_num_fract mesh_color_a ;
+} ;
