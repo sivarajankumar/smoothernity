4090c92778efe102d1731b92af2742cb858eacf8
olegus8 <olegus8@localhost>
2011-02-24 12:58:39 +0000

    working on common logic amusement renderer refactoring

--- src_refactored/common/logic/amusement/renderer/shy_renderer_injections.cpp
+++ src_refactored/common/logic/amusement/renderer/shy_renderer_injections.cpp
@@ -1,7 +1,5 @@
 #include "./shy_renderer_injections.h"
 
-#include "../../../../injections/platform/math/type/num_fract/shy_num_fract.h"
-#include "../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
-#include "../../../../injections/platform/matrix/type/data/shy_data.h"
+#include "../../../../injections/platform/math/consts/shy_consts.h"
 
 #include "./shy_renderer.hpp"
