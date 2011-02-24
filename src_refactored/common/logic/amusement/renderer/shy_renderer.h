a85e6a6fe07e1ad8fe7ee46c7202f76030464588
olegus8 <olegus8@localhost>
2011-02-24 12:23:53 +0000

    working on common logic amusement renderer refactoring

--- src_refactored/common/logic/amusement/renderer/shy_renderer.h
+++ src_refactored/common/logic/amusement/renderer/shy_renderer.h
@@ -3,6 +3,7 @@ class _shy_common_logic_amusement_renderer
 public :
     static void receive ( so_called_message_common_init ) ;
     static void receive ( so_called_message_common_logic_amusement_render ) ;
+    static void receive ( so_called_message_common_logic_blanket_render_reply ) ;
     static void receive ( so_called_message_common_logic_observer_animation_transform_reply ) ;
     static void receive ( so_called_message_common_logic_ortho_planes_reply ) ;
     static void receive ( so_called_message_common_logic_perspective_planes_reply ) ;
