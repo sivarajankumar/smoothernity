217cf501eddffaee61c8df5d8f1a9a9cd78b3e9a
olegus8 <olegus8@localhost>
2011-02-24 15:15:29 +0000

    working on common logic observer animation refactoring

--- src_refactored/common/logic/observer/animation/flight/shy_flight.hpp
+++ src_refactored/common/logic/observer/animation/flight/shy_flight.hpp
@@ -1,5 +1,47 @@
+namespace shy_guts
+{
+    namespace logic_observer_animation_flight_transform_state
+    {
+        static so_called_type_platform_vector_data eye ;
+        static so_called_type_platform_vector_data target ;
+        static so_called_type_platform_math_num_fract vertical_offset ;
+        static so_called_type_platform_math_num_fract horizontal_offset ;
+    }
+
+    namespace logic_observer_update_state
+    {
+        static so_called_type_platform_math_num_fract time ;
+    }
+
+    static void compute_vertical_offset ( ) ;
+    static void compute_horizontal_offset ( ) ;
+    static void compute_eye ( ) ;
+    static void compute_target ( ) ;
+    static void reply_transform ( ) ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_observer_animation_flight > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: compute_vertical_offset ( )
+{
+}
+
+void shy_guts :: compute_horizontal_offset ( )
+{
+}
+
+void shy_guts :: compute_eye ( )
+{
+}
+
+void shy_guts :: compute_target ( )
+{
+}
+
+void shy_guts :: reply_transform ( )
+{
+}
 
 void _shy_common_logic_observer_animation_flight :: receive ( so_called_message_common_init )
 {
--- src_refactored/common/logic/observer/animation/flight/shy_flight_injections.cpp
+++ src_refactored/common/logic/observer/animation/flight/shy_flight_injections.cpp
@@ -1,3 +1,5 @@
 #include "./shy_flight_injections.h"
 
+#include "../../../../../injections/platform/vector/shy_vector.h"
+
 #include "./shy_flight.hpp"
