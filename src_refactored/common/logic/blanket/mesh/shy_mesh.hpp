0bd6f74eb85f567b908eb0823f33c7aa2342b22f
olegus8 <olegus8@localhost>
2011-02-25 13:10:51 +0000

    working on common logic blanket mesh refactoring

--- src_refactored/common/logic/blanket/mesh/shy_mesh.hpp
+++ src_refactored/common/logic/blanket/mesh/shy_mesh.hpp
@@ -1,3 +1,26 @@
+namespace shy_guts
+{
+    namespace logic_blanket_mesh_consts
+    {
+        static const so_called_type_platform_math_num_whole vertices = so_called_platform_math :: init_num_whole ( 4 ) ;
+        static const so_called_type_platform_math_num_whole triangle_strip_indices = so_called_platform_math :: init_num_whole ( 4 ) ;
+        static const so_called_type_platform_math_num_whole triangle_fan_indices = so_called_platform_math :: init_num_whole ( 0 ) ;
+    }
+
+    namespace logic_blanket_mesh_create_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+    }
+
+    namespace engine_render_mesh_create_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole replied ;
+        static so_called_type_platform_math_num_whole finalized ;
+        static so_called_type_common_engine_render_mesh_id mesh ;
+    }
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_blanket_mesh > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
--- src_refactored/common/logic/blanket/mesh/shy_mesh_injections.cpp
+++ src_refactored/common/logic/blanket/mesh/shy_mesh_injections.cpp
@@ -1,4 +1,6 @@
 #include "./shy_mesh_injections.h"
 
+#include "../../../../injections/platform/math/shy_math.h"
+
 #include "./shy_mesh.hpp"
 
