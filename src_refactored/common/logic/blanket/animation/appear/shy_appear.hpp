c761d00eef6f149c5bfd391ab28f52dc65c61c9a
olegus8 <olegus8@localhost>
2011-02-25 18:09:39 +0000

    working on common logic blanket animation appear refactoring

--- src_refactored/common/logic/blanket/animation/appear/shy_appear.hpp
+++ src_refactored/common/logic/blanket/animation/appear/shy_appear.hpp
@@ -1,5 +1,36 @@
+namespace shy_guts
+{
+    namespace logic_blanket_animation_appear_transform_state
+    {
+        static so_called_type_platform_math_num_fract scale ;
+        static so_called_type_platform_math_num_fract rotation ;
+    }
+
+    namespace logic_blanket_update_state
+    {
+        static so_called_type_platform_math_num_whole started ;
+        static so_called_type_platform_math_num_fract time ;
+    }
+
+    static void compute_scale ( ) ;
+    static void compute_rotation ( ) ;
+    static void reply_transform ( ) ; 
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_blanket_animation_appear > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: compute_scale ( )
+{
+}
+
+void shy_guts :: compute_rotation ( )
+{
+}
+
+void shy_guts :: reply_transform ( )
+{
+}
 
 void _shy_common_logic_blanket_animation_appear :: receive ( so_called_message_common_init )
 {
