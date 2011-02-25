60edc660d0fb46522ac46fde70bddc91d3a17cfa
olegus8 <olegus8@localhost>
2011-02-25 15:11:45 +0000

    working on common logic blanket animation refactoring

--- src_refactored/common/logic/blanket/animation/shy_animation_injections.h
+++ src_refactored/common/logic/blanket/animation/shy_animation_injections.h
@@ -1,6 +1,7 @@
 #ifndef _shy_common_logic_blanket_animation_injections_included
 #define _shy_common_logic_blanket_animation_injections_included
 
+#include "./message/appear_transform_reply/shy_appear_transform_reply_injections.h"
 #include "./message/transform_request/shy_transform_request_injections.h"
 
 #include "../../../message/init/shy_init_injections.h"
