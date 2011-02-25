b40e9791e18f366475b5459791b3757b80f0a5ef
olegus8 <olegus8@localhost>
2011-02-25 12:39:24 +0000

    working on common logic blanket refactoring

--- src_refactored/common/logic/blanket/shy_blanket.hpp
+++ src_refactored/common/logic/blanket/shy_blanket.hpp
@@ -1,3 +1,8 @@
+namespace shy_guts
+{
+    static so_called_type_platform_math_num_whole created ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_blanket > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
--- src_refactored/common/logic/blanket/shy_blanket_injections.cpp
+++ src_refactored/common/logic/blanket/shy_blanket_injections.cpp
@@ -1,3 +1,5 @@
 #include "./shy_blanket_injections.h"
 
+#include "../../../injections/platform/math/type/num_whole/shy_num_whole.h"
+
 #include "./shy_blanket.hpp"
