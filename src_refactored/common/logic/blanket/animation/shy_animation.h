72e78627e233618e71b485139ce890f3dab390ed
olegus8 <olegus8@localhost>
2011-02-25 18:05:09 +0000

    working on common logic blanket animation refactoring

--- src_refactored/common/logic/blanket/animation/shy_animation.h
+++ src_refactored/common/logic/blanket/animation/shy_animation.h
@@ -1,7 +1,6 @@
 class _shy_common_logic_blanket_animation
 {
 public :
-    static void receive ( so_called_message_common_init ) ;
     static void receive ( so_called_message_common_logic_blanket_animation_appear_transform_reply ) ;
     static void receive ( so_called_message_common_logic_blanket_animation_disappear_transform_reply ) ;
     static void receive ( so_called_message_common_logic_blanket_animation_fit_transform_reply ) ;
--- src_refactored/common/logic/blanket/animation/shy_animation.hpp
+++ src_refactored/common/logic/blanket/animation/shy_animation.hpp
@@ -145,10 +145,6 @@ void shy_guts :: reply_transform ( )
     so_called_sender_common_logic_blanket_animation_transform_reply :: send ( msg ) ;
 }
 
-void _shy_common_logic_blanket_animation :: receive ( so_called_message_common_init )
-{
-}
-
 void _shy_common_logic_blanket_animation :: receive ( so_called_message_common_logic_blanket_animation_appear_transform_reply msg )
 {
     if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_blanket_animation_appear_transform_state :: requested ) )
