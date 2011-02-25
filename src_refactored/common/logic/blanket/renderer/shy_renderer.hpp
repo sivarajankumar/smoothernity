3a4258f35c60304801517bbf0701f87dd02e34c4
olegus8 <olegus8@localhost>
2011-02-25 14:43:00 +0000

    working on common logic blanket renderer refactoring

--- src_refactored/common/logic/blanket/renderer/shy_renderer.hpp
+++ src_refactored/common/logic/blanket/renderer/shy_renderer.hpp
@@ -1,5 +1,45 @@
+namespace shy_guts
+{
+    namespace logic_blanket_render_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+    }
+
+    namespace logic_blanket_mesh_render_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole replied ;
+    }
+
+    static void proceed_with_render ( ) ;
+    static void render_requested ( ) ;
+    static void prepare_render_state ( ) ;
+    static void request_blanket_render ( ) ;
+    static void reply_render ( ) ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_blanket_renderer > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: proceed_with_render ( )
+{
+}
+
+void shy_guts :: render_requested ( )
+{
+}
+
+void shy_guts :: prepare_render_state ( )
+{
+}
+
+void shy_guts :: request_blanket_render ( )
+{
+}
+
+void shy_guts :: reply_render ( )
+{
+}
 
 void _shy_common_logic_blanket_renderer :: receive ( so_called_message_common_init )
 {
