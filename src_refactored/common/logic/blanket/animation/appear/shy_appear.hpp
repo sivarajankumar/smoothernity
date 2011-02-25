c43a92c6f732ebbb318cb7f84b24d848826ab2c4
olegus8 <olegus8@localhost>
2011-02-25 18:13:27 +0000

    working on common logic blanket animation appear refactoring

--- src_refactored/common/logic/blanket/animation/appear/shy_appear.hpp
+++ src_refactored/common/logic/blanket/animation/appear/shy_appear.hpp
@@ -34,10 +34,14 @@ void shy_guts :: reply_transform ( )
 
 void _shy_common_logic_blanket_animation_appear :: receive ( so_called_message_common_init )
 {
+    shy_guts :: logic_blanket_update_state :: started = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: logic_blanket_update_state :: time = so_called_platform_math_consts :: fract_0 ;
 }
 
 void _shy_common_logic_blanket_animation_appear :: receive ( so_called_message_common_logic_blanket_animation_appear_start )
 {
+    shy_guts :: logic_blanket_update_state :: started = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: logic_blanket_update_state :: time = so_called_platform_math_consts :: fract_0 ;
 }
 
 void _shy_common_logic_blanket_animation_appear :: receive ( so_called_message_common_logic_blanket_animation_appear_transform_request )
@@ -46,4 +50,28 @@ void _shy_common_logic_blanket_animation_appear :: receive ( so_called_message_c
 
 void _shy_common_logic_blanket_animation_appear :: receive ( so_called_message_common_logic_blanket_update )
 {
+/*
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_blanket_update_state :: started ) )
+    {
+        so_called_type_platform_math_num_fract time ;
+        so_called_type_platform_math_num_fract time_step ;
+        so_called_type_platform_math_num_fract time_from_begin_to_end ;
+        so_called_type_platform_math_num_whole started ;
+
+        time = shy_guts :: logic_blanket_update_state :: time ;
+        so_called_platform_math :: make_num_fract ( time_step , 1 , so_called_platform :: frames_per_second ) ;
+        time_from_begin_to_end = _logic_blanket_animation_stateless_consts . get ( ) . appear_time_from_begin_to_end ;
+        started = _logic_blanket_update_state . started ;
+
+        platform_math :: add_to_fract ( time , time_step ) ;
+        if ( platform_conditions :: fract_greater_than_fract ( time , time_from_begin_to_end ) )
+        {
+            started = _platform_math_consts . get ( ) . whole_false ;
+            _mediator . get ( ) . send ( typename messages :: logic_blanket_animation_appear_finished ( ) ) ;
+        }
+
+        _logic_blanket_update_state . started = started ;
+        _logic_blanket_update_state . time = time ;
+    }
+*/
 }
