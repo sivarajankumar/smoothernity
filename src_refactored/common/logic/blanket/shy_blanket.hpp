72b85e611be2a2a8ce1b08d3892e7c2fc6b31cb9
olegus8 <olegus8@localhost>
2011-02-25 12:45:46 +0000

    working on common logic blanket refactoring

--- src_refactored/common/logic/blanket/shy_blanket.hpp
+++ src_refactored/common/logic/blanket/shy_blanket.hpp
@@ -23,4 +23,6 @@ void _shy_common_logic_blanket :: receive ( so_called_message_common_logic_blank
 
 void _shy_common_logic_blanket :: receive ( so_called_message_common_logic_blanket_update )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: created ) )
+        so_called_sender_common_logic_blanket_place :: send ( so_called_message_common_logic_blanket_place ( ) ) ;
 }
