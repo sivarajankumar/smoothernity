0635ee841f7d829f466d612492b31e77ccde30a1
olegus8 <olegus8@localhost>
2011-02-28 15:13:06 +0000

    working on common logic door refactoring

--- src_refactored/common/autogenerated/shy_autogenerated_injections.cpp
+++ src_refactored/common/autogenerated/shy_autogenerated_injections.cpp
@@ -1,6 +1,7 @@
 #include "../logic/amusement/consts/shy_consts_injections.h"
 #include "../logic/blanket/animation/consts/shy_consts_injections.h"
 #include "../logic/blanket/consts/shy_consts_injections.h"
+#include "../logic/door/consts/shy_consts_injections.h"
 #include "../logic/observer/animation/consts/shy_consts_injections.h"
 #include "../logic/ortho/consts/shy_consts_injections.h"
 #include "../logic/perspective/consts/shy_consts_injections.h"
