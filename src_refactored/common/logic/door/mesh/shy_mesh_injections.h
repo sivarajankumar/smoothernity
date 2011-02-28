3ec69b7d2ea0983803b00b43034a34fe486fcc8d
olegus8 <olegus8@localhost>
2011-02-28 15:23:16 +0000

    working on common logic door refactoring

--- src_refactored/common/logic/door/mesh/shy_mesh_injections.h
+++ src_refactored/common/logic/door/mesh/shy_mesh_injections.h
@@ -1,8 +1,9 @@
 #ifndef _shy_common_logic_door_mesh_injections_included
 #define _shy_common_logic_door_mesh_injections_included
 
-#include "../../../engine/render/message/mesh_create_reply/shy_mesh_create_reply_injections.h"
+#include "../message/mesh_create/shy_mesh_create_injections.h"
 
+#include "../../../engine/render/message/mesh_create_reply/shy_mesh_create_reply_injections.h"
 #include "../../../message/init/shy_init_injections.h"
 
 #include "../../../../injections/platform/scheduler/shy_scheduler.h"
---
+++ src_refactored/common/logic/door/message/mesh_create/shy_mesh_create.h
@@ -0,0 +1,3 @@
+class shy_message_common_logic_door_mesh_create
+{
+} ;
