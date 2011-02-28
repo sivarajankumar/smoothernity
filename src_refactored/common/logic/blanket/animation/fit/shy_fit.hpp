e6842fa36b2be16e690fbfaa3edcfafd7998d1c1
olegus8 <olegus8@localhost>
2011-02-28 14:38:40 +0000

    working on common logic blanket animation fit refactoring

--- src_refactored/common/logic/blanket/animation/fit/shy_fit.hpp
+++ src_refactored/common/logic/blanket/animation/fit/shy_fit.hpp
@@ -1,5 +1,48 @@
+namespace shy_guts
+{
+    namespace logic_blanket_animation_fit_transform_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_fract scale ;
+    }
+
+    namespace engine_render_aspect_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole replied ;
+        static so_called_type_platform_math_num_fract width ;
+        static so_called_type_platform_math_num_fract height ;
+    }
+
+    static void proceed_with_transform ( ) ;
+    static void request_aspect_ratio ( ) ;
+    static void reply_computed_transform ( ) ;
+    static void compute_transform ( ) ;
+    static void reply_transform ( ) ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_blanket_animation_fit > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: proceed_with_transform ( )
+{
+}
+
+void shy_guts :: request_aspect_ratio ( )
+{
+}
+
+void shy_guts :: reply_computed_transform ( )
+{
+}
+
+void shy_guts :: compute_transform ( )
+{
+}
+
+void shy_guts :: reply_transform ( )
+{
+}
 
 void _shy_common_logic_blanket_animation_fit :: receive ( so_called_message_common_engine_render_aspect_reply )
 {
--- src_refactored/common/logic/blanket/animation/fit/shy_fit_injections.cpp
+++ src_refactored/common/logic/blanket/animation/fit/shy_fit_injections.cpp
@@ -1,4 +1,6 @@
 #include "./shy_fit_injections.h"
 
+#include "../../../../../injections/platform/math/consts/shy_consts.h"
+
 #include "./shy_fit.hpp"
 
