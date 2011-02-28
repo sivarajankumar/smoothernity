88d59d96934560e27e8a52849b39c58d4f39c850
olegus8 <olegus8@localhost>
2011-02-28 14:27:45 +0000

    working on common logic blanket animation disappear refactoring

--- src_refactored/common/logic/blanket/animation/disappear/shy_disappear.hpp
+++ src_refactored/common/logic/blanket/animation/disappear/shy_disappear.hpp
@@ -22,10 +22,14 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void _shy_common_logic_blanket_animation_disappear :: receive ( so_called_message_common_init )
 {
+    shy_guts :: logic_blanket_update_state :: started = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: logic_blanket_update_state :: time = so_called_platform_math_consts :: fract_0 ;
 }
 
 void _shy_common_logic_blanket_animation_disappear :: receive ( so_called_message_common_logic_blanket_animation_disappear_start )
 {
+    shy_guts :: logic_blanket_update_state :: started = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: logic_blanket_update_state :: time = so_called_platform_math_consts :: fract_0 ;
 }
 
 void _shy_common_logic_blanket_animation_disappear :: receive ( so_called_message_common_logic_blanket_animation_disappear_transform_request )
@@ -34,4 +38,27 @@ void _shy_common_logic_blanket_animation_disappear :: receive ( so_called_messag
 
 void _shy_common_logic_blanket_animation_disappear :: receive ( so_called_message_common_logic_blanket_update )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_blanket_update_state :: started ) )
+    {
+        so_called_type_platform_math_num_fract time ;
+        so_called_type_platform_math_num_fract time_step ;
+        so_called_type_platform_math_num_fract time_from_begin_to_end ;
+        so_called_type_platform_math_num_whole started ;
+
+        time = shy_guts :: logic_blanket_update_state :: time ;
+        started = shy_guts :: logic_blanket_update_state :: started ;
+        so_called_platform_math :: make_num_fract ( time_step , 1 , so_called_platform_consts :: frames_per_second ) ;
+        time_from_begin_to_end = so_called_common_logic_blanket_animation_consts :: disappear_time_from_begin_to_end ;
+
+        so_called_platform_math :: add_to_fract ( time , time_step ) ;
+
+        if ( so_called_platform_conditions :: fract_greater_than_fract ( time , time_from_begin_to_end ) )
+        {
+            started = so_called_platform_math_consts :: whole_false ;
+            so_called_sender_common_logic_blanket_animation_disappear_finished :: send ( so_called_message_common_logic_blanket_animation_disappear_finished ( ) ) ;
+        }
+
+        shy_guts :: logic_blanket_update_state :: time = time ;
+        shy_guts :: logic_blanket_update_state :: started = started ;
+    }
 }
--- src_refactored/common/logic/blanket/animation/disappear/shy_disappear_injections.cpp
+++ src_refactored/common/logic/blanket/animation/disappear/shy_disappear_injections.cpp
@@ -1,5 +1,11 @@
 #include "./shy_disappear_injections.h"
 
+#include "../consts/shy_consts_injections.h"
+#include "../sender/disappear_finished/shy_disappear_finished_injections.h"
+
+#include "../../../../../injections/platform/conditions/shy_conditions.h"
+#include "../../../../../injections/platform/consts/shy_consts.h"
 #include "../../../../../injections/platform/math/consts/shy_consts.h"
+#include "../../../../../injections/platform/math/shy_math.h"
 
 #include "./shy_disappear.hpp"
