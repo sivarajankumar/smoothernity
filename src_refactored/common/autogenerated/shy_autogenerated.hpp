a6bdc80f8616a0c78278a7ba24715994d96a370b
olegus8 <olegus8@localhost>
2011-03-01 14:11:36 +0000

    working on common logic door animation refactoring

--- src_refactored/common/autogenerated/shy_autogenerated.hpp
+++ src_refactored/common/autogenerated/shy_autogenerated.hpp
@@ -53,6 +53,13 @@ const so_called_type_platform_math_num_fract so_called_common_logic_door_consts 
 const so_called_type_platform_math_num_fract so_called_common_logic_door_consts :: texture_pen_g = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
 const so_called_type_platform_math_num_fract so_called_common_logic_door_consts :: texture_pen_r = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
 
+const so_called_type_platform_math_num_fract so_called_common_logic_door_animation_consts :: animation_origin_x = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+const so_called_type_platform_math_num_fract so_called_common_logic_door_animation_consts :: animation_origin_y = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+const so_called_type_platform_math_num_fract so_called_common_logic_door_animation_consts :: animation_origin_z = so_called_platform_math :: init_num_fract ( - 3 , 1 ) ;
+const so_called_type_platform_math_num_fract so_called_common_logic_door_animation_consts :: appear_scale_begin = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+const so_called_type_platform_math_num_fract so_called_common_logic_door_animation_consts :: appear_scale_end = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
+const so_called_type_platform_math_num_fract so_called_common_logic_door_animation_consts :: appear_time_from_begin_to_end = so_called_platform_math :: init_num_fract ( 2 , 1 ) ;
+
 const so_called_type_platform_math_num_fract so_called_common_logic_observer_animation_consts :: flight_horizontal_offset_amplitude = so_called_platform_math :: init_num_fract ( 1 , 3 ) ;
 const so_called_type_platform_math_num_fract so_called_common_logic_observer_animation_consts :: flight_horizontal_offset_period = so_called_platform_math :: init_num_fract ( 11 , 10 ) ;
 const so_called_type_platform_math_num_fract so_called_common_logic_observer_animation_consts :: flight_target_z = so_called_platform_math :: init_num_fract ( - 1 , 1 ) ;
