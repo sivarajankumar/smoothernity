a8a8346cbe37e954c60d6b796cacb94f8d6c7d0a
olegus8 <olegus8@localhost>
2011-03-01 15:44:33 +0000

    working on common logic door refactoring

--- src_refactored/common/logic/door/shy_door.hpp
+++ src_refactored/common/logic/door/shy_door.hpp
@@ -7,10 +7,6 @@ namespace shy_guts
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_door > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
-void _shy_common_logic_door :: receive ( so_called_message_common_init )
-{
-}
-
 void _shy_common_logic_door :: receive ( so_called_message_common_logic_door_creation_permit )
 {
 }
--- src_refactored/common/sender/init/shy_init_injections.cpp
+++ src_refactored/common/sender/init/shy_init_injections.cpp
@@ -13,7 +13,6 @@
 #include "../../logic/door/mesh/shy_mesh_injections.h"
 #include "../../logic/door/placement/shy_placement_injections.h"
 #include "../../logic/door/renderer/shy_renderer_injections.h"
-#include "../../logic/door/shy_door_injections.h"
 #include "../../logic/door/texture/shy_texture_injections.h"
 #include "../../logic/entities/shy_entities_injections.h"
 #include "../../logic/fidget/shy_fidget_injections.h"
