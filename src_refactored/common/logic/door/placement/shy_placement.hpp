950dbe0e63ddd354c7c132bc7fea0569f12adf35
olegus8 <olegus8@localhost>
2011-03-01 16:41:39 +0000

    working on common logic door placement refactoring

--- src_refactored/common/logic/door/placement/shy_placement.hpp
+++ src_refactored/common/logic/door/placement/shy_placement.hpp
@@ -1,8 +1,11 @@
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_door_placement > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
-void _shy_common_logic_door_placement :: receive ( so_called_message_common_logic_door_animation_transform_reply )
+void _shy_common_logic_door_placement :: receive ( so_called_message_common_logic_door_animation_transform_reply msg )
 {
+    so_called_message_common_logic_door_mesh_set_transform set_transform_msg ;
+    set_transform_msg . transform = msg . transform ;
+    so_called_sender_common_logic_door_mesh_set_transform :: send ( set_transform_msg ) ;
 }
 
 void _shy_common_logic_door_placement :: receive ( so_called_message_common_logic_door_place )
--- src_refactored/common/logic/door/placement/shy_placement_injections.cpp
+++ src_refactored/common/logic/door/placement/shy_placement_injections.cpp
@@ -1,5 +1,6 @@
 #include "./shy_placement_injections.h"
 
 #include "../animation/sender/transform_request/shy_transform_request_injections.h"
+#include "../sender/mesh_set_transform/shy_mesh_set_transform_injections.h"
 
 #include "./shy_placement.hpp"
