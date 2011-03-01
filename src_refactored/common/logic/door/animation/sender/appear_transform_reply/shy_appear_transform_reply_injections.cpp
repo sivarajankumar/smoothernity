1361e559e9031bac9156c964c8dda8e5f4bab624
olegus8 <olegus8@localhost>
2011-03-01 14:47:01 +0000

    working on common logic door animation refactoring

---
+++ src_refactored/common/logic/door/animation/sender/appear_transform_reply/shy_appear_transform_reply_injections.cpp
@@ -0,0 +1,5 @@
+#include "./shy_appear_transform_reply_injections.h"
+
+#include "../../shy_animation_injections.h"
+
+#include "./shy_appear_transform_reply.hpp"
--- src_refactored/common/logic/door/animation/shy_animation_injections.h
+++ src_refactored/common/logic/door/animation/shy_animation_injections.h
@@ -1,6 +1,8 @@
 #ifndef _shy_common_logic_door_animation_injections_included
 #define _shy_common_logic_door_animation_injections_included
 
+#include "./message/appear_transform_reply/shy_appear_transform_reply_injections.h"
+
 #include "../../../message/init/shy_init_injections.h"
 
 #include "../../../../injections/platform/scheduler/shy_scheduler.h"
