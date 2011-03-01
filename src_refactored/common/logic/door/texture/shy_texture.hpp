176b1a2bf1290fc5d3c6fea1efdd162b94cbab67
olegus8 <olegus8@localhost>
2011-03-01 14:06:06 +0000

    working on common logic door refactoring

--- src_refactored/common/logic/door/texture/shy_texture.hpp
+++ src_refactored/common/logic/door/texture/shy_texture.hpp
@@ -16,3 +16,7 @@ void _shy_common_logic_door_texture :: receive ( so_called_message_common_init )
 void _shy_common_logic_door_texture :: receive ( so_called_message_common_logic_door_texture_create )
 {
 }
+
+void _shy_common_logic_door_texture :: receive ( so_called_message_common_logic_door_texture_select_request )
+{
+}
