21d388295187a23002f517255c9be8070849e84f
olegus8 <olegus8@localhost>
2011-03-01 16:17:08 +0000

    working on common logic door mesh refactoring

--- src_refactored/common/logic/door/mesh/shy_mesh.hpp
+++ src_refactored/common/logic/door/mesh/shy_mesh.hpp
@@ -55,10 +55,26 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void shy_guts :: proceed_with_creation ( )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_door_mesh_create_state :: requested ) )
+    {
+        shy_guts :: logic_door_mesh_create_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: request_mesh_create ( ) ;
+    }
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: engine_render_mesh_create_state :: replied ) )
+    {
+        shy_guts :: engine_render_mesh_create_state :: replied = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: mesh_created ( ) ;
+    }
 }
 
 void shy_guts :: request_mesh_create ( )
 {
+    shy_guts :: engine_render_mesh_create_state :: requested = so_called_platform_math_consts :: whole_true ;
+    so_called_message_common_engine_render_mesh_create_request msg ;
+    msg . vertices = shy_guts :: logic_door_mesh_consts :: mesh_vertices_count ;
+    msg . triangle_strip_indices = shy_guts :: logic_door_mesh_consts :: mesh_triangle_strip_indices_count ;
+    msg . triangle_fan_indices = shy_guts :: logic_door_mesh_consts :: mesh_triangle_fan_indices_count ;
+    so_called_sender_common_engine_render_mesh_create_request :: send ( msg ) ;
 }
 
 void shy_guts :: mesh_created ( )
@@ -111,8 +127,15 @@ void shy_guts :: mesh_set_vertex_color
 {
 }
 
-void _shy_common_logic_door_mesh :: receive ( so_called_message_common_engine_render_mesh_create_reply )
+void _shy_common_logic_door_mesh :: receive ( so_called_message_common_engine_render_mesh_create_reply msg )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: engine_render_mesh_create_state :: requested ) )
+    {
+        shy_guts :: engine_render_mesh_create_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: engine_render_mesh_create_state :: replied = so_called_platform_math_consts :: whole_true ;
+        shy_guts :: engine_render_mesh_create_state :: mesh = msg . mesh ;
+        shy_guts :: proceed_with_creation ( ) ;
+    }
 }
 
 void _shy_common_logic_door_mesh :: receive ( so_called_message_common_logic_door_mesh_create )
