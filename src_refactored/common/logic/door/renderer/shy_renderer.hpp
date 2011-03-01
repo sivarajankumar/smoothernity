960d9fd4a7f6bf451855636536ba4b2bb0a6a994
olegus8 <olegus8@localhost>
2011-03-01 16:54:28 +0000

    working on common logic door renderer refactoring

--- src_refactored/common/logic/door/renderer/shy_renderer.hpp
+++ src_refactored/common/logic/door/renderer/shy_renderer.hpp
@@ -30,6 +30,21 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void shy_guts :: proceed_with_render ( )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_door_render_state :: requested ) )
+    {
+        shy_guts :: logic_door_render_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: render_requested ( ) ;
+    }
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_door_texture_select_state :: replied ) )
+    {
+        shy_guts :: logic_door_texture_select_state :: replied = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: request_mesh_render ( ) ;
+    }
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_door_mesh_render_state :: replied ) )
+    {
+        shy_guts :: logic_door_mesh_render_state :: replied = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: reply_door_render ( ) ;
+    }
 }
 
 void shy_guts :: render_requested ( )
