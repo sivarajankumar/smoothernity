af351344a2717907ae045dcbe7b7efd71a06a2b5
olegus8 <olegus8@localhost>
2011-03-02 14:54:54 +0000

    working on common logic door animation appear refactoring

--- src_refactored/common/logic/door/animation/appear/shy_appear.hpp
+++ src_refactored/common/logic/door/animation/appear/shy_appear.hpp
@@ -1,5 +1,30 @@
+namespace shy_guts
+{
+    namespace logic_door_animation_appear_transform_state
+    {
+        static so_called_type_platform_math_num_fract scale ;
+    }
+
+    namespace logic_door_update_state
+    {
+        static so_called_type_platform_math_num_whole started ;
+        static so_called_type_platform_math_num_fract time ;
+    }
+
+    static void compute_transform ( ) ;
+    static void reply_transform ( ) ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_door_animation_appear > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: compute_transform ( )
+{
+}
+
+void shy_guts :: reply_transform ( )
+{
+}
 
 void _shy_common_logic_door_animation_appear :: receive ( so_called_message_common_init )
 {
--- src_refactored/common/logic/door/animation/appear/shy_appear_injections.cpp
+++ src_refactored/common/logic/door/animation/appear/shy_appear_injections.cpp
@@ -1,3 +1,5 @@
 #include "./shy_appear_injections.h"
 
+#include "../../../../../injections/platform/math/consts/shy_consts.h"
+
 #include "./shy_appear.hpp"
