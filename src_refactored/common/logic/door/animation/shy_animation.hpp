01c4a4bb97c15307c160272c854e173cc0bd749c
olegus8 <olegus8@localhost>
2011-03-02 14:36:33 +0000

    working on common logic door animation refactoring

--- src_refactored/common/logic/door/animation/shy_animation.hpp
+++ src_refactored/common/logic/door/animation/shy_animation.hpp
@@ -43,10 +43,19 @@ void shy_guts :: reply_transform ( )
 {
 }
 
-void _shy_common_logic_door_animation :: receive ( so_called_message_common_logic_door_animation_appear_transform_reply )
+void _shy_common_logic_door_animation :: receive ( so_called_message_common_logic_door_animation_appear_transform_reply msg )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_door_animation_appear_transform_state :: requested ) )
+    {
+        shy_guts :: logic_door_animation_appear_transform_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: logic_door_animation_appear_transform_state :: replied = so_called_platform_math_consts :: whole_true ;
+        shy_guts :: logic_door_animation_appear_transform_state :: scale = msg . scale ;
+        shy_guts :: proceed_with_transform ( ) ;
+    }
 }
 
 void _shy_common_logic_door_animation :: receive ( so_called_message_common_logic_door_animation_transform_request )
 {
+    shy_guts :: logic_door_animation_transform_state :: requested = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: proceed_with_transform ( ) ;
 }
