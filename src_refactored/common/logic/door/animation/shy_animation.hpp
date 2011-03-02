5ed445d4b0690a8b9584c2b75f9d3268ff605839
olegus8 <olegus8@localhost>
2011-03-02 14:51:28 +0000

    working on common logic door animation refactoring

--- src_refactored/common/logic/door/animation/shy_animation.hpp
+++ src_refactored/common/logic/door/animation/shy_animation.hpp
@@ -45,14 +45,39 @@ void shy_guts :: request_appear_transform ( )
 
 void shy_guts :: reply_computed_transform ( )
 {
+    shy_guts :: compute_transform ( ) ;
+    shy_guts :: reply_transform ( ) ;
 }
 
 void shy_guts :: compute_transform ( )
 {
+    so_called_type_platform_math_num_fract origin_x ;
+    so_called_type_platform_math_num_fract origin_y ;
+    so_called_type_platform_math_num_fract origin_z ;
+    so_called_type_platform_math_num_fract scale ;
+    so_called_type_platform_math_num_fract zero ;
+    so_called_type_platform_matrix_data transform ;
+
+    origin_x = so_called_common_logic_door_animation_consts :: animation_origin_x ;
+    origin_y = so_called_common_logic_door_animation_consts :: animation_origin_y ;
+    origin_z = so_called_common_logic_door_animation_consts :: animation_origin_z ;
+    scale = shy_guts :: logic_door_animation_appear_transform_state :: scale ;
+    zero = so_called_platform_math_consts :: fract_0 ;
+
+    so_called_platform_matrix :: identity ( transform ) ;
+    so_called_platform_matrix :: set_axis_x ( transform , scale , zero , zero ) ;
+    so_called_platform_matrix :: set_axis_y ( transform , zero , scale , zero ) ;
+    so_called_platform_matrix :: set_axis_z ( transform , zero , zero , scale ) ;
+    so_called_platform_matrix :: set_origin ( transform , origin_x , origin_y , origin_z ) ;
+
+    shy_guts :: logic_door_animation_transform_state :: transform = transform ;
 }
 
 void shy_guts :: reply_transform ( )
 {
+    so_called_message_common_logic_door_animation_transform_reply msg ;
+    msg . transform = shy_guts :: logic_door_animation_transform_state :: transform ;
+    so_called_sender_common_logic_door_animation_transform_reply :: send ( msg ) ;
 }
 
 void _shy_common_logic_door_animation :: receive ( so_called_message_common_logic_door_animation_appear_transform_reply msg )
