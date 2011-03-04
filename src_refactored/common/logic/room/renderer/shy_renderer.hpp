faf584f486ff658f23c16e1b1b8d7d3ef9eb5cc2
olegus8 <olegus8@localhost>
2011-03-04 15:10:42 +0000

    working on common logic room renderer refactoring

--- src_refactored/common/logic/room/renderer/shy_renderer.hpp
+++ src_refactored/common/logic/room/renderer/shy_renderer.hpp
@@ -1,5 +1,57 @@
+namespace shy_guts
+{
+    namespace logic_room_render_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole render_permitted ;
+    }
+
+    namespace logic_room_mesh_render_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole replied ;
+    }
+
+    namespace logic_room_texture_select_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole replied ;
+    }
+
+    static void proceed_with_render ( ) ;
+    static void render_requested ( ) ;
+    static void request_texture_select ( ) ;
+    static void request_mesh_render ( ) ;
+    static void prepare_render_state ( ) ;
+    static void reply_room_render ( ) ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_room_renderer > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: proceed_with_render ( )
+{
+}
+
+void shy_guts :: render_requested ( )
+{
+}
+
+void shy_guts :: request_texture_select ( )
+{
+}
+
+void shy_guts :: request_mesh_render ( )
+{
+}
+
+void shy_guts :: prepare_render_state ( )
+{
+}
+
+void shy_guts :: reply_room_render ( )
+{
+}
 
 void _shy_common_logic_room_renderer :: receive ( so_called_message_common_init )
 {
