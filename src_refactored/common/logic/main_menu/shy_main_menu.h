f27f26d1adbc66a7a0f9fb84e453911735976665
olegus8 <olegus8@localhost>
2011-03-09 14:15:54 +0000

    working on common logic main menu refactoring

--- src_refactored/common/logic/main_menu/shy_main_menu.h
+++ src_refactored/common/logic/main_menu/shy_main_menu.h
@@ -2,6 +2,7 @@ class _shy_common_logic_main_menu
 {
 public :
     static void receive ( so_called_message_common_init ) ;
+    static void receive ( so_called_message_common_logic_main_menu_creation_permit ) ;
 } ;
 
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_main_menu > :: module shy_common_logic_main_menu_scheduled ;
