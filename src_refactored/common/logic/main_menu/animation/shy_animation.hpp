d69b882b91888d9202a0d70dcabe1f600e827b42
olegus8 <olegus8@localhost>
2011-03-09 16:48:52 +0000

    working on common logic main menu animation refactoring

--- src_refactored/common/logic/main_menu/animation/shy_animation.hpp
+++ src_refactored/common/logic/main_menu/animation/shy_animation.hpp
@@ -4,3 +4,7 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 void _shy_common_logic_main_menu_animation :: receive ( so_called_message_common_init )
 {
 }
+
+void _shy_common_logic_main_menu_animation :: receive ( so_called_message_common_logic_main_menu_animation_shake_transform_reply )
+{
+}
