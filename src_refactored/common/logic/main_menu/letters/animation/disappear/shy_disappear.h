cdb29289d6621796aaf53a1d1e7111fb846b45bb
olegus8 <olegus8@localhost>
2011-03-11 17:14:21 +0000

    working on common logic main menu letters animation refactoring

--- src_refactored/common/logic/main_menu/letters/animation/disappear/shy_disappear.h
+++ src_refactored/common/logic/main_menu/letters/animation/disappear/shy_disappear.h
@@ -4,6 +4,7 @@ public :
     static void receive ( so_called_message_common_init ) ;
     static void receive ( so_called_message_common_logic_main_menu_launch_permit ) ;
     static void receive ( so_called_message_common_logic_main_menu_letters_animation_disappear_start ) ;
+    static void receive ( so_called_message_common_logic_main_menu_letters_animation_disappear_transform_request ) ;
     static void receive ( so_called_message_common_logic_main_menu_update ) ;
 } ;
 
--- src_refactored/common/logic/main_menu/letters/animation/disappear/shy_disappear_injections.h
+++ src_refactored/common/logic/main_menu/letters/animation/disappear/shy_disappear_injections.h
@@ -2,6 +2,7 @@
 #define _shy_common_logic_main_menu_letters_animation_disappear_injections_included
 
 #include "../message/disappear_start/shy_disappear_start_injections.h"
+#include "../message/disappear_transform_request/shy_disappear_transform_request_injections.h"
 
 #include "../../../message/launch_permit/shy_launch_permit_injections.h"
 #include "../../../message/update/shy_update_injections.h"
