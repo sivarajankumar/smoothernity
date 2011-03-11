51f862254d90baee9c2fa5647d6bc4d89659ae04
olegus8 <olegus8@localhost>
2011-03-11 17:27:54 +0000

    working on common logic main menu letters animation refactoring

--- src_refactored/common/logic/main_menu/letters/animation/shy_animation_injections.h
+++ src_refactored/common/logic/main_menu/letters/animation/shy_animation_injections.h
@@ -3,6 +3,7 @@
 
 #include "./message/appear_transform_reply/shy_appear_transform_reply_injections.h"
 #include "./message/disappear_transform_reply/shy_disappear_transform_reply_injections.h"
+#include "./message/idle_transform_reply/shy_idle_transform_reply_injections.h"
 
 #include "../../../../message/init/shy_init_injections.h"
 
