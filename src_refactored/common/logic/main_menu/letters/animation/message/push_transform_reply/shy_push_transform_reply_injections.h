f2440dad56fbf735da7c790384eb35b83ca59ed8
olegus8 <olegus8@localhost>
2011-03-12 14:05:50 +0000

    working on common logic main menu letters animation refactoring

---
+++ src_refactored/common/logic/main_menu/letters/animation/message/push_transform_reply/shy_push_transform_reply_injections.h
@@ -0,0 +1,11 @@
+#ifndef _shy_message_common_logic_main_menu_letters_animation_push_transform_reply_injections_included
+#define _shy_message_common_logic_main_menu_letters_animation_push_transform_reply_injections_included
+
+#include "../../../../../../../injections/platform/math/type/num_fract/shy_num_fract.h"
+#include "../../../../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
+
+#include "./shy_push_transform_reply.h"
+
+typedef shy_message_common_logic_main_menu_letters_animation_push_transform_reply so_called_message_common_logic_main_menu_letters_animation_push_transform_reply ;
+
+#endif
