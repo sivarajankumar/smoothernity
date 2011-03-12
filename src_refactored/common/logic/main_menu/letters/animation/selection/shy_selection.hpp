62f960c61fb7ce3f68800a0e196468bd19701339
olegus8 <olegus8@localhost>
2011-03-12 14:46:46 +0000

    working on common logic main menu letters animation refactoring

--- src_refactored/common/logic/main_menu/letters/animation/selection/shy_selection.hpp
+++ src_refactored/common/logic/main_menu/letters/animation/selection/shy_selection.hpp
@@ -9,6 +9,10 @@ void _shy_common_logic_main_menu_letters_animation_selection :: receive ( so_cal
 {
 }
 
+void _shy_common_logic_main_menu_letters_animation_selection :: receive ( so_called_message_common_logic_main_menu_letters_animation_selection_transform_request )
+{
+}
+
 void _shy_common_logic_main_menu_letters_animation_selection :: receive ( so_called_message_common_logic_main_menu_update )
 {
 }
