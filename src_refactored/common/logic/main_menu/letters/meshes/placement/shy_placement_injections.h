2f6b75147724fd66f65ae5735492c14045bc7819
olegus8 <olegus8@localhost>
2011-03-15 16:04:37 +0000

    working on common logic main menu letters animation refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/placement/shy_placement_injections.h
+++ src_refactored/common/logic/main_menu/letters/meshes/placement/shy_placement_injections.h
@@ -1,6 +1,8 @@
 #ifndef _shy_common_logic_main_menu_letters_meshes_placement_injections_included
 #define _shy_common_logic_main_menu_letters_meshes_placement_injections_included
 
+#include "../../animation/message/transform_reply/shy_transform_reply_injections.h"
+
 #include "../../../../../message/init/shy_init_injections.h"
 
 #include "../../../../../../injections/platform/scheduler/shy_scheduler.h"
