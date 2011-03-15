8bb18b9ffc8c5c8a9af1b7c766e10e156fcc395c
olegus8 <olegus8@localhost>
2011-03-15 15:08:01 +0000

    working on common logic main menu letters animation refactoring

---
+++ src_refactored/common/logic/main_menu/letters/animation/sender/unselection_weight_request/shy_unselection_weight_request_injections.cpp
@@ -0,0 +1,5 @@
+#include "./shy_unselection_weight_request_injections.h"
+
+#include "../../unselection_weight/shy_unselection_weight_injections.h"
+
+#include "./shy_unselection_weight_request.hpp"
