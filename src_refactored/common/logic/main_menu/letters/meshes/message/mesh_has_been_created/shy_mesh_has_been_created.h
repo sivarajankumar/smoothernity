a64e99836c6b75b49ff6c1adcd2f9f26de229a8d
olegus8 <olegus8@localhost>
2011-03-16 14:40:59 +0000

    working on common logic main menu letters meshes refactoring

---
+++ src_refactored/common/logic/main_menu/letters/meshes/message/mesh_has_been_created/shy_mesh_has_been_created.h
@@ -0,0 +1,7 @@
+class shy_message_common_logic_main_menu_letters_meshes_mesh_has_been_created
+{
+public :
+    so_called_type_platform_math_num_whole row ;
+    so_called_type_platform_math_num_whole col ;
+    so_called_type_common_engine_render_mesh_id mesh ;
+} ;
--- src_refactored/common/logic/main_menu/letters/meshes/storage/shy_storage_injections.h
+++ src_refactored/common/logic/main_menu/letters/meshes/storage/shy_storage_injections.h
@@ -1,6 +1,8 @@
 #ifndef _shy_common_logic_main_menu_letters_meshes_storage_injections_included
 #define _shy_common_logic_main_menu_letters_meshes_storage_injections_included
 
+#include "../message/mesh_has_been_created/shy_mesh_has_been_created_injections.h"
+
 #include "../../../../../message/init/shy_init_injections.h"
 
 #include "../../../../../../injections/platform/scheduler/shy_scheduler.h"
