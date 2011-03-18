ab03d41d475f2a6d230a74b273b454412ee457b4
olegus8 <olegus8@localhost>
2011-03-18 14:09:25 +0000

    working on common logic main menu letters meshes refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/destroyer/shy_destroyer.h
+++ src_refactored/common/logic/main_menu/letters/meshes/destroyer/shy_destroyer.h
@@ -2,6 +2,7 @@ class _shy_common_logic_main_menu_letters_meshes_destroyer
 {
 public :
     static void receive ( so_called_message_common_init ) ;
+    static void receive ( so_called_message_common_logic_main_menu_letters_meshes_destroy_request ) ;
 } ;
 
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_main_menu_letters_meshes_destroyer > :: module shy_common_logic_main_menu_letters_meshes_destroyer_scheduled ;
--- src_refactored/common/logic/main_menu/letters/meshes/destroyer/shy_destroyer_injections.h
+++ src_refactored/common/logic/main_menu/letters/meshes/destroyer/shy_destroyer_injections.h
@@ -1,6 +1,8 @@
 #ifndef _shy_common_logic_main_menu_letters_meshes_destroyer_injections_included
 #define _shy_common_logic_main_menu_letters_meshes_destroyer_injections_included
 
+#include "../message/destroy_request/shy_destroy_request_injections.h"
+
 #include "../../../../../message/init/shy_init_injections.h"
 
 #include "../../../../../../injections/platform/scheduler/shy_scheduler.h"
