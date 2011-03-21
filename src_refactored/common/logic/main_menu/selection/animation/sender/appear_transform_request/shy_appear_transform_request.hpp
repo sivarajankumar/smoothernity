c6044c384fded6e33e1c815f0045421182fb599f
olegus8 <olegus8@localhost>
2011-03-21 10:36:52 +0000

    working on common logic main menu selection animation refactoring

---
+++ src_refactored/common/logic/main_menu/selection/animation/sender/appear_transform_request/shy_appear_transform_request.hpp
@@ -0,0 +1,4 @@
+void shy_sender_common_logic_main_menu_selection_animation_appear_transform_request :: send ( so_called_message_common_logic_main_menu_selection_animation_appear_transform_request msg )
+{
+    so_called_common_logic_main_menu_selection_animation_appear :: receive ( msg ) ;
+}
