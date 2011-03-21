e6ba1c14acc06c9590a5be28f2e5ee0e68720238
olegus8 <olegus8@localhost>
2011-03-21 16:56:13 +0000

    working on common logic main menu selection animation refactoring

--- src_refactored/common/logic/main_menu/selection/animation/idle_attention/shy_idle_attention.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/idle_attention/shy_idle_attention.hpp
@@ -9,6 +9,10 @@ void _shy_common_logic_main_menu_selection_animation_idle_attention :: receive (
 {
 }
 
+void _shy_common_logic_main_menu_selection_animation_idle_attention :: receive ( so_called_message_common_logic_main_menu_selection_animation_idle_attention_transform_request )
+{
+}
+
 void _shy_common_logic_main_menu_selection_animation_idle_attention :: receive ( so_called_message_common_logic_main_menu_update )
 {
 }
