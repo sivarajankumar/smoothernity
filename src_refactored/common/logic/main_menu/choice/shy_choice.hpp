27ee59da7f5d54af780e9f5b35c97646b6373906
olegus8 <olegus8@localhost>
2011-03-26 15:21:30 +0000

    working on common logic main menu choice refactoring

--- src_refactored/common/logic/main_menu/choice/shy_choice.hpp
+++ src_refactored/common/logic/main_menu/choice/shy_choice.hpp
@@ -24,10 +24,22 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void shy_guts :: proceed_with_update ( )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_update_state :: requested ) )
+    {
+        shy_guts :: logic_main_menu_update_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: obtain_controls_state ( ) ;
+    }
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_controls_state :: replied ) )
+    {
+        shy_guts :: logic_controls_state :: replied = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: controls_state_received ( ) ;
+    }
 }
 
 void shy_guts :: obtain_controls_state ( )
 {
+    shy_guts :: logic_controls_state :: requested = so_called_platform_math_consts :: whole_true ;
+    so_called_sender_common_logic_controls_state_request :: send ( so_called_message_common_logic_controls_state_request ( ) ) ;
 }
 
 void shy_guts :: controls_state_received ( )
