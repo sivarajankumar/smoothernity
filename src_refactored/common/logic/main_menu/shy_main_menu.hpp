cbc3f936a9a2a62c7dbeca8e82e2293b55d20895
olegus8 <olegus8@localhost>
2011-03-26 14:26:53 +0000

    working on common logic main menu refactoring

--- src_refactored/common/logic/main_menu/shy_main_menu.hpp
+++ src_refactored/common/logic/main_menu/shy_main_menu.hpp
@@ -1,8 +1,22 @@
+namespace shy_guts
+{
+    static so_called_type_platform_math_num_whole creation_permitted ;
+    static so_called_type_platform_math_num_whole launch_permitted ;
+    static so_called_type_platform_math_num_whole created ;
+    static so_called_type_platform_math_num_whole launched ;
+    static so_called_type_platform_math_num_whole disappearing ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_main_menu > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
 void _shy_common_logic_main_menu :: receive ( so_called_message_common_init )
 {
+    shy_guts :: launch_permitted = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: creation_permitted = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: launched = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: created = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: disappearing = so_called_platform_math_consts :: whole_false ;
 }
 
 void _shy_common_logic_main_menu :: receive ( so_called_message_common_logic_main_menu_creation_permit )
--- src_refactored/common/logic/main_menu/shy_main_menu_injections.cpp
+++ src_refactored/common/logic/main_menu/shy_main_menu_injections.cpp
@@ -1,3 +1,5 @@
 #include "./shy_main_menu_injections.h"
 
+#include "../../../injections/platform/math/consts/shy_consts.h"
+
 #include "./shy_main_menu.hpp"
