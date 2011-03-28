beaaf912e9715ec71715e72f67b1304b3043a1f3
olegus8 <olegus8@localhost>
2011-03-28 15:10:33 +0000

    working on common logic main menu animation shake refactoring

--- src_refactored/common/logic/main_menu/animation/shake/shy_shake.hpp
+++ src_refactored/common/logic/main_menu/animation/shake/shy_shake.hpp
@@ -37,6 +37,26 @@ void _shy_common_logic_main_menu_animation_shake :: receive ( so_called_message_
 
 void _shy_common_logic_main_menu_animation_shake :: receive ( so_called_message_common_logic_main_menu_update )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_update_state :: started ) )
+    {
+        so_called_type_platform_math_num_fract time ;
+        so_called_type_platform_math_num_fract time_step ;
+        so_called_type_platform_math_num_fract time_total ;
+        so_called_type_platform_math_num_fract time_to_begin ;
+        so_called_type_platform_math_num_fract time_from_begin_to_end ;
+
+        time = shy_guts :: logic_main_menu_update_state :: time ;
+        time_to_begin = so_called_common_logic_main_menu_animation_consts :: shake_time_to_begin ;
+        time_from_begin_to_end = so_called_common_logic_main_menu_animation_consts :: shake_time_from_begin_to_end ;
+        so_called_platform_math :: make_num_fract ( time_step , 1 , so_called_platform_consts :: frames_per_second ) ;
+
+        so_called_platform_math :: add_fracts ( time_total , time_to_begin , time_from_begin_to_end ) ;
+        so_called_platform_math :: add_to_fract ( time , time_step ) ;
+        if ( so_called_platform_conditions :: fract_greater_than_fract ( time , time_total ) )
+            shy_guts :: logic_main_menu_update_state :: started = so_called_platform_math_consts :: whole_false ;
+
+        shy_guts :: logic_main_menu_update_state :: time = time ;
+    }
 }
 
 void _shy_common_logic_main_menu_animation_shake :: receive ( so_called_message_common_logic_main_menu_void_chosen )
