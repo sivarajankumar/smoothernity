04cc4149254abca1e3041664a508ea4f3010a68b
olegus8 <olegus8@localhost>
2011-03-28 15:51:43 +0000

    working on common logic main menu letters storage refactoring

--- src_refactored/common/logic/main_menu/letters/storage/shy_storage.hpp
+++ src_refactored/common/logic/main_menu/letters/storage/shy_storage.hpp
@@ -1,5 +1,35 @@
+namespace shy_guts
+{
+    class col_state_type
+    {
+    public :
+        so_called_type_common_logic_text_letter_id letter ;
+    } ;
+    
+    class row_state_type
+    {
+    public :
+        so_called_type_platform_static_array_data < col_state_type , so_called_common_logic_main_menu_consts :: max_cols > cols ;
+        so_called_type_platform_math_num_whole cols_count ;
+    } ;
+    
+    namespace rows_state
+    {
+        static so_called_type_platform_static_array_data < row_state_type , so_called_common_logic_main_menu_consts :: max_rows > rows ;
+        static so_called_type_platform_math_num_whole rows_count ;
+    }
+
+    static so_called_type_platform_math_num_whole max_cols ;
+
+    static void next_row ( ) ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_main_menu_letters_storage > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: next_row ( )
+{
+}
 
 void _shy_common_logic_main_menu_letters_storage :: receive ( so_called_message_common_init )
 {
--- src_refactored/common/logic/main_menu/letters/storage/shy_storage_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/storage/shy_storage_injections.cpp
@@ -1,3 +1,7 @@
 #include "./shy_storage_injections.h"
 
+#include "../../consts/shy_consts_injections.h"
+
+#include "../../../../../injections/platform/static_array/type/data/shy_data.h"
+
 #include "./shy_storage.hpp"
