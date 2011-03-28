878c8d6571fe45cfa80e1d8d1770b4c723aefc3c
olegus8 <olegus8@localhost>
2011-03-28 15:32:59 +0000

    working on common logic main menu letters creation director refactoring

--- src_refactored/common/logic/main_menu/letters/creation_director/shy_creation_director.hpp
+++ src_refactored/common/logic/main_menu/letters/creation_director/shy_creation_director.hpp
@@ -8,8 +8,11 @@ namespace shy_guts
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_main_menu_letters_creation_director > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
-void shy_guts :: add_letter ( so_called_type_common_logic_text_letter_id )
+void shy_guts :: add_letter ( so_called_type_common_logic_text_letter_id letter )
 {
+    so_called_message_common_logic_main_menu_letters_letter_add msg ;
+    msg . letter = letter ;
+    so_called_sender_common_logic_main_menu_letters_letter_add :: send ( msg ) ;
 }
 
 void shy_guts :: next_row ( )
--- src_refactored/common/logic/main_menu/letters/creation_director/shy_creation_director_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/creation_director/shy_creation_director_injections.cpp
@@ -1,5 +1,7 @@
 #include "./shy_creation_director_injections.h"
 
+#include "../sender/letter_add/shy_letter_add_injections.h"
+
 #include "../../../text/consts/shy_consts_injections.h"
 
 #include "./shy_creation_director.hpp"
