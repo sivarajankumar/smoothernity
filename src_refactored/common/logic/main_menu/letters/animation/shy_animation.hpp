7adc565af391b5dc1e8883d21614c4db6b217535
olegus8 <olegus8@localhost>
2011-03-28 16:25:05 +0000

    working on common logic main menu letters animation refactoring

--- src_refactored/common/logic/main_menu/letters/animation/shy_animation.hpp
+++ src_refactored/common/logic/main_menu/letters/animation/shy_animation.hpp
@@ -156,8 +156,12 @@ void _shy_common_logic_main_menu_letters_animation :: receive ( so_called_messag
 {
 }
 
-void _shy_common_logic_main_menu_letters_animation :: receive ( so_called_message_common_logic_main_menu_letters_animation_transform_request )
+void _shy_common_logic_main_menu_letters_animation :: receive ( so_called_message_common_logic_main_menu_letters_animation_transform_request msg )
 {
+    shy_guts :: logic_main_menu_letters_animation_transform_state :: requested = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: logic_main_menu_letters_animation_transform_state :: row = msg . row ;
+    shy_guts :: logic_main_menu_letters_animation_transform_state :: col = msg . col ;
+    shy_guts :: proceed_with_transform ( ) ;
 }
 
 void _shy_common_logic_main_menu_letters_animation :: receive ( so_called_message_common_logic_main_menu_letters_animation_unselection_weight_reply )
--- src_refactored/common/logic/main_menu/letters/animation/shy_animation_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/animation/shy_animation_injections.cpp
@@ -1,5 +1,6 @@
 #include "./shy_animation_injections.h"
 
+#include "../../../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../../../injections/platform/matrix/type/data/shy_data.h"
 
 #include "./shy_animation.hpp"
