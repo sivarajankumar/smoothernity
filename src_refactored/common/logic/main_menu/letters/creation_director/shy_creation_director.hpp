049a756294db93f0c399f28c25f59135693f1184
olegus8 <olegus8@localhost>
2011-03-28 15:38:19 +0000

    working on common logic main menu letters creation director refactoring

--- src_refactored/common/logic/main_menu/letters/creation_director/shy_creation_director.hpp
+++ src_refactored/common/logic/main_menu/letters/creation_director/shy_creation_director.hpp
@@ -22,6 +22,7 @@ void shy_guts :: next_row ( )
 
 void shy_guts :: text_create_finished ( )
 {
+    so_called_sender_common_logic_main_menu_letters_create_finished :: send ( so_called_message_common_logic_main_menu_letters_create_finished ( ) ) ;
 }
 
 void _shy_common_logic_main_menu_letters_creation_director :: receive ( so_called_message_common_logic_main_menu_letters_create )
--- src_refactored/common/logic/main_menu/letters/creation_director/shy_creation_director_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/creation_director/shy_creation_director_injections.cpp
@@ -1,5 +1,6 @@
 #include "./shy_creation_director_injections.h"
 
+#include "../sender/create_finished/shy_create_finished_injections.h"
 #include "../sender/letter_add/shy_letter_add_injections.h"
 #include "../sender/next_row/shy_next_row_injections.h"
 
