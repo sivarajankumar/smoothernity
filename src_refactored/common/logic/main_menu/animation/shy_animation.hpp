1205226f995cbcfb1d0eb8175472f4842b3db910
olegus8 <olegus8@localhost>
2011-03-28 12:31:18 +0000

    working on common logic main menu animation refactoring

--- src_refactored/common/logic/main_menu/animation/shy_animation.hpp
+++ src_refactored/common/logic/main_menu/animation/shy_animation.hpp
@@ -25,10 +25,22 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void shy_guts :: proceed_with_transform ( )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_animation_transform_state :: requested ) )
+    {
+        shy_guts :: logic_main_menu_animation_transform_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: obtain_shake_transform ( ) ;
+    }
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_animation_shake_transform_state :: replied ) )
+    {
+        shy_guts :: logic_main_menu_animation_shake_transform_state :: replied = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: compute_and_reply_transform ( ) ;
+    }
 }
 
 void shy_guts :: obtain_shake_transform ( )
 {
+    shy_guts :: logic_main_menu_animation_shake_transform_state :: requested = so_called_platform_math_consts :: whole_true ;
+    so_called_sender_common_logic_main_menu_animation_shake_transform_request :: send ( so_called_message_common_logic_main_menu_animation_shake_transform_request ( ) ) ;
 }
 
 void shy_guts :: compute_and_reply_transform ( )
@@ -43,10 +55,19 @@ void shy_guts :: reply_transform ( )
 {
 }
 
-void _shy_common_logic_main_menu_animation :: receive ( so_called_message_common_logic_main_menu_animation_shake_transform_reply )
+void _shy_common_logic_main_menu_animation :: receive ( so_called_message_common_logic_main_menu_animation_shake_transform_reply msg )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_animation_shake_transform_state :: requested ) )
+    {
+        shy_guts :: logic_main_menu_animation_shake_transform_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: logic_main_menu_animation_shake_transform_state :: replied = so_called_platform_math_consts :: whole_true ;
+        shy_guts :: logic_main_menu_animation_shake_transform_state :: shift_x = msg . shift_x ;
+        shy_guts :: proceed_with_transform ( ) ;
+    }
 }
 
 void _shy_common_logic_main_menu_animation :: receive ( so_called_message_common_logic_main_menu_animation_transform_request )
 {
+    shy_guts :: logic_main_menu_animation_transform_state :: requested = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: proceed_with_transform ( ) ;
 }
--- src_refactored/common/logic/main_menu/animation/shy_animation_injections.cpp
+++ src_refactored/common/logic/main_menu/animation/shy_animation_injections.cpp
@@ -1,5 +1,8 @@
 #include "./shy_animation_injections.h"
 
+#include "./sender/shake_transform_request/shy_shake_transform_request_injections.h"
+
+#include "../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../../injections/platform/matrix/shy_matrix.h"
 
