62e0e8ad27a058b794ba749fa6120de9cca90e67
olegus8 <olegus8@localhost>
2011-03-28 16:35:35 +0000

    working on common logic main menu letters animation refactoring

--- src_refactored/common/logic/main_menu/letters/animation/shy_animation.hpp
+++ src_refactored/common/logic/main_menu/letters/animation/shy_animation.hpp
@@ -146,8 +146,18 @@ void _shy_common_logic_main_menu_letters_animation :: receive ( so_called_messag
     }
 }
 
-void _shy_common_logic_main_menu_letters_animation :: receive ( so_called_message_common_logic_main_menu_letters_animation_disappear_transform_reply )
+void _shy_common_logic_main_menu_letters_animation :: receive ( so_called_message_common_logic_main_menu_letters_animation_disappear_transform_reply msg )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: requested )
+      && so_called_platform_conditions :: wholes_are_equal ( shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: requested_row , msg . row )
+      && so_called_platform_conditions :: wholes_are_equal ( shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: requested_col , msg . col )
+       )
+    {
+        shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: replied = so_called_platform_math_consts :: whole_true ;
+        shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: scale = msg . scale ;
+        shy_guts :: proceed_with_transform ( ) ;
+    }
 }
 
 void _shy_common_logic_main_menu_letters_animation :: receive ( so_called_message_common_logic_main_menu_letters_animation_idle_transform_reply msg )
