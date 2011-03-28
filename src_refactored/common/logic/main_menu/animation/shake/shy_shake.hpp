8c0ef76658dc6c824a389d8636ffe58ec9bee02a
olegus8 <olegus8@localhost>
2011-03-28 15:05:34 +0000

    working on common logic main menu animation shake refactoring

--- src_refactored/common/logic/main_menu/animation/shake/shy_shake.hpp
+++ src_refactored/common/logic/main_menu/animation/shake/shy_shake.hpp
@@ -1,8 +1,34 @@
+namespace shy_guts
+{
+    namespace logic_main_menu_animation_shake_transform_state
+    {
+        so_called_type_platform_math_num_fract shift_x ;
+    }
+
+    namespace logic_main_menu_update_state
+    {
+        so_called_type_platform_math_num_whole started ;
+        so_called_type_platform_math_num_fract time ;
+    }
+
+    static void compute_transform ( ) ;
+    static void reply_transform ( ) ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_main_menu_animation_shake > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
+void shy_guts :: compute_transform ( )
+{
+}
+
+void shy_guts :: reply_transform ( )
+{
+}
+
 void _shy_common_logic_main_menu_animation_shake :: receive ( so_called_message_common_init )
 {
+    shy_guts :: logic_main_menu_update_state :: started = so_called_platform_math_consts :: whole_false ;
 }
 
 void _shy_common_logic_main_menu_animation_shake :: receive ( so_called_message_common_logic_main_menu_animation_shake_transform_request )
