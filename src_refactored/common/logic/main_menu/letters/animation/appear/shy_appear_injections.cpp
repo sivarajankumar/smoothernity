432613df4cdb552f8d3999b0ae5705d18a1f59f1
olegus8 <olegus8@localhost>
2011-03-29 14:32:57 +0000

    working on common logic main menu letters animation appear refactoring

--- src_refactored/common/logic/main_menu/letters/animation/appear/shy_appear_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/animation/appear/shy_appear_injections.cpp
@@ -1,6 +1,7 @@
 #include "./shy_appear_injections.h"
 
 #include "../consts/shy_consts_injections.h"
+#include "../sender/appear_transform_reply/shy_appear_transform_reply_injections.h"
 
 #include "../../../../../engine/math/stateless/shy_stateless_injections.h"
 
