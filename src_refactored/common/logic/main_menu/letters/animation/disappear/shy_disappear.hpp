a3dd93a9aa7680d3548fd4ec3e8ec7514414710c
olegus8 <olegus8@localhost>
2011-03-29 14:46:05 +0000

    working on common logic main menu letters animation disappear refactoring

--- src_refactored/common/logic/main_menu/letters/animation/disappear/shy_disappear.hpp
+++ src_refactored/common/logic/main_menu/letters/animation/disappear/shy_disappear.hpp
@@ -31,14 +31,40 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void shy_guts :: proceed_with_transform ( )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: requested ) )
+    {
+        shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: transform_request_received ( ) ;
+    }
 }
 
 void shy_guts :: transform_request_received ( )
 {
+    shy_guts :: compute_delay ( ) ;
+    shy_guts :: compute_time ( ) ;
+    shy_guts :: compute_transform ( ) ;
+    shy_guts :: reply_transform ( ) ;
 }
 
 void shy_guts :: compute_delay ( )
 {
+    so_called_type_platform_math_num_fract delay_for_row ;
+    so_called_type_platform_math_num_fract delay_for_col ;
+    so_called_type_platform_math_num_fract delay_per_row ;
+    so_called_type_platform_math_num_fract delay_per_col ;
+    so_called_type_platform_math_num_fract delay ;
+    so_called_type_platform_math_num_fract row ;
+    so_called_type_platform_math_num_fract col ;
+    
+    so_called_platform_math :: make_fract_from_whole ( row , shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: row ) ;
+    so_called_platform_math :: make_fract_from_whole ( col , shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: col ) ;
+    delay_per_row = so_called_common_logic_main_menu_letters_animation_consts :: disappear_delay_per_row_in_seconds ;
+    delay_per_col = so_called_common_logic_main_menu_letters_animation_consts :: disappear_delay_per_col_in_seconds ;
+    so_called_platform_math :: mul_fracts ( delay_for_row , delay_per_row , row ) ;
+    so_called_platform_math :: mul_fracts ( delay_for_col , delay_per_col , col ) ;
+    so_called_platform_math :: add_fracts ( delay , delay_for_row , delay_for_col ) ;
+    
+    shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: delay = delay ;
 }
 
 void shy_guts :: compute_time ( )
@@ -67,8 +93,12 @@ void _shy_common_logic_main_menu_letters_animation_disappear :: receive ( so_cal
     shy_guts :: logic_main_menu_update_state :: started = so_called_platform_math_consts :: whole_true ;
 }
 
-void _shy_common_logic_main_menu_letters_animation_disappear :: receive ( so_called_message_common_logic_main_menu_letters_animation_disappear_transform_request )
+void _shy_common_logic_main_menu_letters_animation_disappear :: receive ( so_called_message_common_logic_main_menu_letters_animation_disappear_transform_request msg )
 {
+    shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: requested = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: row = msg . row ;
+    shy_guts :: logic_main_menu_letters_animation_disappear_transform_state :: col = msg . col ;
+    shy_guts :: proceed_with_transform ( ) ;
 }
 
 void _shy_common_logic_main_menu_letters_animation_disappear :: receive ( so_called_message_common_logic_main_menu_update )
--- src_refactored/common/logic/main_menu/letters/animation/disappear/shy_disappear_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/animation/disappear/shy_disappear_injections.cpp
@@ -1,6 +1,9 @@
 #include "./shy_disappear_injections.h"
 
+#include "../consts/shy_consts_injections.h"
+
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
+#include "../../../../../../injections/platform/math/shy_math.h"
 
 #include "./shy_disappear.hpp"
