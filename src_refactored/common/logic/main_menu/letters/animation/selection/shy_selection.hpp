983bf568f5fd0cd776af9a3ca3c9cab1ea15831f
olegus8 <olegus8@localhost>
2011-03-29 15:41:57 +0000

    working on common logic main menu letters animation selection refactoring

--- src_refactored/common/logic/main_menu/letters/animation/selection/shy_selection.hpp
+++ src_refactored/common/logic/main_menu/letters/animation/selection/shy_selection.hpp
@@ -46,6 +46,8 @@ void shy_guts :: reply_transform ( )
 
 void _shy_common_logic_main_menu_letters_animation_selection :: receive ( so_called_message_common_logic_main_menu_launch_permit )
 {
+    shy_guts :: logic_main_menu_update_state :: launch_permitted = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: logic_main_menu_update_state :: time = so_called_platform_math_consts :: fract_0 ;
 }
 
 void _shy_common_logic_main_menu_letters_animation_selection :: receive ( so_called_message_common_logic_main_menu_letters_animation_selection_transform_request )
@@ -54,4 +56,10 @@ void _shy_common_logic_main_menu_letters_animation_selection :: receive ( so_cal
 
 void _shy_common_logic_main_menu_letters_animation_selection :: receive ( so_called_message_common_logic_main_menu_update )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_update_state :: launch_permitted ) )
+    {
+        so_called_type_platform_math_num_fract time_step ;
+        so_called_platform_math :: make_num_fract ( time_step , 1 , so_called_platform_consts :: frames_per_second ) ;
+        so_called_platform_math :: add_to_fract ( shy_guts :: logic_main_menu_update_state :: time , time_step ) ;
+    }
 }
