f14e7ba5c4b0a98a057cc2e4fee9ce33fb9b0047
olegus8 <olegus8@localhost>
2011-03-30 17:01:35 +0000

    working on common logic main menu letters animation unselection weight refactoring

--- src_refactored/common/logic/main_menu/letters/animation/unselection_weight/shy_unselection_weight_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/animation/unselection_weight/shy_unselection_weight_injections.cpp
@@ -1,5 +1,8 @@
 #include "./shy_unselection_weight_injections.h"
 
+#include "../../../../../../injections/platform/conditions/shy_conditions.h"
+#include "../../../../../../injections/platform/consts/shy_consts.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
+#include "../../../../../../injections/platform/math/shy_math.h"
 
 #include "./shy_unselection_weight.hpp"
