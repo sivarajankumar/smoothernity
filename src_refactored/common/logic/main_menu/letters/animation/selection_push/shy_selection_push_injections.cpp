155b50aa3135a787663f521e31020cb353063131
olegus8 <olegus8@localhost>
2011-03-30 16:21:19 +0000

    working on common logic main menu letters animation selection push refactoring

--- src_refactored/common/logic/main_menu/letters/animation/selection_push/shy_selection_push_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/animation/selection_push/shy_selection_push_injections.cpp
@@ -3,6 +3,8 @@
 #include "../../../../controls/sender/state_request/shy_state_request_injections.h"
 
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
+#include "../../../../../../injections/platform/consts/shy_consts.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
+#include "../../../../../../injections/platform/math/shy_math.h"
 
 #include "./shy_selection_push.hpp"
