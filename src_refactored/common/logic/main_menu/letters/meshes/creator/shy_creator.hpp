08419ce04a866e38bd9ca5db40888467e64ae385
olegus8 <olegus8@localhost>
2011-03-31 13:28:09 +0000

    working on common logic main menu letters meshes creator refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/creator/shy_creator.hpp
+++ src_refactored/common/logic/main_menu/letters/meshes/creator/shy_creator.hpp
@@ -120,6 +120,8 @@ void shy_guts :: proceed_with_creation ( )
 
 void shy_guts :: obtain_rows_count ( )
 {
+    shy_guts :: logic_main_menu_letters_rows_state :: requested = so_called_platform_math_consts :: whole_true ;
+    so_called_sender_common_logic_main_menu_letters_rows_request :: send ( so_called_message_common_logic_main_menu_letters_rows_request ( ) ) ;
 }
 
 void shy_guts :: start_first_row ( )
--- src_refactored/common/logic/main_menu/letters/meshes/creator/shy_creator_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/meshes/creator/shy_creator_injections.cpp
@@ -1,5 +1,7 @@
 #include "./shy_creator_injections.h"
 
+#include "../../sender/rows_request/shy_rows_request_injections.h"
+
 #include "../../../../text/stateless/shy_stateless_injections.h"
 
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
