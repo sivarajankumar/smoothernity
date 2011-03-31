995e01a4849a28c61ad2e5d435a49498af4c8dd5
olegus8 <olegus8@localhost>
2011-03-31 16:01:47 +0000

    working on common logic main menu letters meshes storage refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/storage/shy_storage.hpp
+++ src_refactored/common/logic/main_menu/letters/meshes/storage/shy_storage.hpp
@@ -37,6 +37,21 @@ void _shy_common_logic_main_menu_letters_meshes_storage :: receive ( so_called_m
 
 void _shy_common_logic_main_menu_letters_meshes_storage :: receive ( so_called_message_common_logic_main_menu_letters_meshes_iterate_start )
 {
+    for ( so_called_type_platform_math_num_whole i = so_called_platform_math_consts :: whole_0
+        ; so_called_platform_conditions :: whole_less_than_whole ( i , shy_guts :: meshes_count )
+        ; so_called_platform_math :: inc_whole ( i )
+        )
+    {
+        so_called_type_platform_pointer_data < shy_guts :: mesh_state > mesh_state ;
+        so_called_platform_static_array :: element_ptr ( mesh_state , shy_guts :: meshes , i ) ;
+        
+        so_called_message_common_logic_main_menu_letters_meshes_iteration iteration_msg ;
+        iteration_msg . row = mesh_state . get ( ) . row ;
+        iteration_msg . col = mesh_state . get ( ) . col ;
+        iteration_msg . mesh = mesh_state . get ( ) . mesh ;
+        so_called_sender_common_logic_main_menu_letters_meshes_iteration :: send ( iteration_msg ) ;
+    }
+    so_called_sender_common_logic_main_menu_letters_meshes_iterate_finished :: send ( so_called_message_common_logic_main_menu_letters_meshes_iterate_finished ( ) ) ;
 }
 
 void _shy_common_logic_main_menu_letters_meshes_storage :: receive ( so_called_message_common_logic_main_menu_letters_meshes_mesh_has_been_created msg )
--- src_refactored/common/logic/main_menu/letters/meshes/storage/shy_storage_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/meshes/storage/shy_storage_injections.cpp
@@ -1,11 +1,14 @@
 #include "./shy_storage_injections.h"
 
 #include "../sender/count_reply/shy_count_reply_injections.h"
+#include "../sender/iterate_finished/shy_iterate_finished_injections.h"
+#include "../sender/iteration/shy_iteration_injections.h"
 #include "../sender/mesh_id_reply/shy_mesh_id_reply_injections.h"
 #include "../sender/mesh_row_col_reply/shy_mesh_row_col_reply_injections.h"
 
 #include "../../../consts/shy_consts_injections.h"
 
+#include "../../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../../../../injections/platform/math/shy_math.h"
 #include "../../../../../../injections/platform/pointer/type/data/shy_data.h"
