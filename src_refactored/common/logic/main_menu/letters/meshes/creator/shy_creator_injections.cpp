e566c3e47d5906098a4026c7b1c840a21454f240
olegus8 <olegus8@localhost>
2011-03-31 14:27:32 +0000

    working on common logic main menu letters meshes creator refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/creator/shy_creator_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/meshes/creator/shy_creator_injections.cpp
@@ -15,6 +15,7 @@
 #include "../../../../../engine/render/sender/mesh_create_request/shy_mesh_create_request_injections.h"
 #include "../../../../../engine/render/sender/mesh_finalize/shy_mesh_finalize_injections.h"
 #include "../../../../../engine/render/sender/mesh_set_vertex_position/shy_mesh_set_vertex_position_injections.h"
+#include "../../../../../engine/render/sender/mesh_set_vertex_tex_coord/shy_mesh_set_vertex_tex_coord_injections.h"
 
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
