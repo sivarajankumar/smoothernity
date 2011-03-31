31a1f68bc5745407733038036ae050f85c17b5df
olegus8 <olegus8@localhost>
2011-03-31 14:49:23 +0000

    working on common logic main menu letters meshes placement refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/placement/shy_placement.hpp
+++ src_refactored/common/logic/main_menu/letters/meshes/placement/shy_placement.hpp
@@ -89,10 +89,6 @@ void shy_guts :: move_to_next_mesh ( )
 {
 }
 
-void _shy_common_logic_main_menu_letters_meshes_placement :: receive ( so_called_message_common_init )
-{
-}
-
 void _shy_common_logic_main_menu_letters_meshes_placement :: receive ( so_called_message_common_logic_main_menu_letters_animation_transform_reply )
 {
 }
