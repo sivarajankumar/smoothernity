de299ce0476b5ec8c764ae4768be550124dbcba8
olegus8 <olegus8@localhost>
2011-03-31 15:04:01 +0000

    working on common logic main menu letters meshes placement refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/placement/shy_placement.hpp
+++ src_refactored/common/logic/main_menu/letters/meshes/placement/shy_placement.hpp
@@ -84,6 +84,8 @@ void shy_guts :: proceed_with_placement ( )
 
 void shy_guts :: obtain_meshes_count ( )
 {
+    shy_guts :: logic_main_menu_letters_meshes_count_state :: requested = so_called_platform_math_consts :: whole_true ;
+    so_called_sender_common_logic_main_menu_letters_meshes_count_request :: send ( so_called_message_common_logic_main_menu_letters_meshes_count_request ( ) ) ;
 }
 
 void shy_guts :: obtain_first_mesh_row_col ( )
