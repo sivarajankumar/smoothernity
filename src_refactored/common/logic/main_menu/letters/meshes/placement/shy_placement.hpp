b538493d49e637ed0da8c413f02dbd99c12e09e6
olegus8 <olegus8@localhost>
2011-03-31 15:14:28 +0000

    working on common logic main menu letters meshes placement refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/placement/shy_placement.hpp
+++ src_refactored/common/logic/main_menu/letters/meshes/placement/shy_placement.hpp
@@ -116,6 +116,14 @@ void shy_guts :: obtain_current_mesh_id ( )
 
 void shy_guts :: obtain_animated_transform ( )
 {
+    shy_guts :: logic_main_menu_letters_animation_transform_state :: requested = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: logic_main_menu_letters_animation_transform_state :: requested_row = shy_guts :: logic_main_menu_letters_mesh_row_col_state :: row ;
+    shy_guts :: logic_main_menu_letters_animation_transform_state :: requested_col = shy_guts :: logic_main_menu_letters_mesh_row_col_state :: col ;
+
+    so_called_message_common_logic_main_menu_letters_animation_transform_request msg ;
+    msg . row = shy_guts :: logic_main_menu_letters_mesh_row_col_state :: row ;
+    msg . col = shy_guts :: logic_main_menu_letters_mesh_row_col_state :: col ;
+    so_called_sender_common_logic_main_menu_letters_animation_transform_request :: send ( msg ) ;
 }
 
 void shy_guts :: animated_transform_received ( )
--- src_refactored/common/logic/main_menu/letters/meshes/placement/shy_placement_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/meshes/placement/shy_placement_injections.cpp
@@ -4,6 +4,8 @@
 #include "../sender/mesh_id_request/shy_mesh_id_request_injections.h"
 #include "../sender/mesh_row_col_request/shy_mesh_row_col_request_injections.h"
 
+#include "../../animation/sender/transform_request/shy_transform_request_injections.h"
+
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
 
