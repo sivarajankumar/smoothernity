96692ab273f48d691ad0d421c8fa77059568d53e
olegus8 <olegus8@localhost>
2011-03-31 12:34:05 +0000

    working on common logic main menu letters meshes creation director refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/creation_director/shy_creation_director.hpp
+++ src_refactored/common/logic/main_menu/letters/meshes/creation_director/shy_creation_director.hpp
@@ -1,8 +1,16 @@
+namespace shy_guts
+{
+    static so_called_type_platform_math_num_whole creation_in_progress ;
+    static so_called_type_platform_math_num_fract time_passed ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_main_menu_letters_meshes_creation_director > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
 void _shy_common_logic_main_menu_letters_meshes_creation_director :: receive ( so_called_message_common_init )
 {
+    shy_guts :: creation_in_progress = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: time_passed = so_called_platform_math_consts :: fract_0 ;
 }
 
 void _shy_common_logic_main_menu_letters_meshes_creation_director :: receive ( so_called_message_common_logic_main_menu_letters_meshes_create )
--- src_refactored/common/logic/main_menu/letters/meshes/creation_director/shy_creation_director_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/meshes/creation_director/shy_creation_director_injections.cpp
@@ -1,3 +1,5 @@
 #include "./shy_creation_director_injections.h"
 
+#include "../../../../../../injections/platform/math/consts/shy_consts.h"
+
 #include "./shy_creation_director.hpp"
