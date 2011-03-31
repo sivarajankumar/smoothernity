e96a158b48e74287c970a1d629685a131de9f542
olegus8 <olegus8@localhost>
2011-03-31 14:37:55 +0000

    working on common logic main menu letters meshes destroyer refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/destroyer/shy_destroyer.hpp
+++ src_refactored/common/logic/main_menu/letters/meshes/destroyer/shy_destroyer.hpp
@@ -8,6 +8,8 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void _shy_common_logic_main_menu_letters_meshes_destroyer :: receive ( so_called_message_common_logic_main_menu_letters_meshes_destroy_request )
 {
+    shy_guts :: iteration_in_progress = so_called_platform_math_consts :: whole_true ;
+    so_called_sender_common_logic_main_menu_letters_meshes_iterate_start :: send ( so_called_message_common_logic_main_menu_letters_meshes_iterate_start ( ) ) ;
 }
 
 void _shy_common_logic_main_menu_letters_meshes_destroyer :: receive ( so_called_message_common_logic_main_menu_letters_meshes_iterate_finished )
--- src_refactored/common/logic/main_menu/letters/meshes/destroyer/shy_destroyer_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/meshes/destroyer/shy_destroyer_injections.cpp
@@ -1,3 +1,7 @@
 #include "./shy_destroyer_injections.h"
 
+#include "../sender/iterate_start/shy_iterate_start_injections.h"
+
+#include "../../../../../../injections/platform/math/consts/shy_consts.h"
+
 #include "./shy_destroyer.hpp"
