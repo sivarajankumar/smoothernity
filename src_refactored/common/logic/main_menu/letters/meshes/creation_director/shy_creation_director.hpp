44acfbeba1651be620bae39c71507c8b55bc178b
olegus8 <olegus8@localhost>
2011-03-31 12:43:42 +0000

    working on common logic main menu letters meshes creation director refactoring

--- src_refactored/common/logic/main_menu/letters/meshes/creation_director/shy_creation_director.hpp
+++ src_refactored/common/logic/main_menu/letters/meshes/creation_director/shy_creation_director.hpp
@@ -25,4 +25,22 @@ void _shy_common_logic_main_menu_letters_meshes_creation_director :: receive ( s
 
 void _shy_common_logic_main_menu_letters_meshes_creation_director :: receive ( so_called_message_common_logic_main_menu_update )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: creation_in_progress ) )
+    {
+        so_called_type_platform_math_num_fract frame_time ;
+        so_called_type_platform_math_num_fract time_between_creation ;
+
+        time_between_creation = so_called_common_logic_main_menu_letters_meshes_consts :: time_between_creation ;
+
+        so_called_platform_math :: make_num_fract ( frame_time , 1 , so_called_platform_consts :: frames_per_second ) ;
+        so_called_platform_math :: add_to_fract ( shy_guts :: time_passed , frame_time ) ;
+
+        while ( so_called_platform_conditions :: fract_greater_than_fract ( shy_guts :: time_passed , time_between_creation ) )
+        {
+            so_called_platform_math :: sub_from_fract ( shy_guts :: time_passed , time_between_creation ) ;
+            so_called_sender_common_logic_main_menu_letters_meshes_mesh_create_next :: send ( so_called_message_common_logic_main_menu_letters_meshes_mesh_create_next ( ) ) ;
+            if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: creation_in_progress ) )
+                break ;
+        }
+    }
 }
--- src_refactored/common/logic/main_menu/letters/meshes/creation_director/shy_creation_director_injections.cpp
+++ src_refactored/common/logic/main_menu/letters/meshes/creation_director/shy_creation_director_injections.cpp
@@ -1,5 +1,11 @@
 #include "./shy_creation_director_injections.h"
 
+#include "../consts/shy_consts_injections.h"
+#include "../sender/mesh_create_next/shy_mesh_create_next_injections.h"
+
+#include "../../../../../../injections/platform/conditions/shy_conditions.h"
+#include "../../../../../../injections/platform/consts/shy_consts.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
+#include "../../../../../../injections/platform/math/shy_math.h"
 
 #include "./shy_creation_director.hpp"
