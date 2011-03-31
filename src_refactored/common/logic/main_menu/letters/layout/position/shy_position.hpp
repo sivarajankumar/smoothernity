27cd91107974b0a851d290e916afca2827802013
olegus8 <olegus8@localhost>
2011-03-31 11:06:27 +0000

    working on common logic main menu letters layout position refactoring

--- src_refactored/common/logic/main_menu/letters/layout/position/shy_position.hpp
+++ src_refactored/common/logic/main_menu/letters/layout/position/shy_position.hpp
@@ -88,8 +88,17 @@ void _shy_common_logic_main_menu_letters_layout_position :: receive ( so_called_
 {
 }
 
-void _shy_common_logic_main_menu_letters_layout_position :: receive ( so_called_message_common_logic_main_menu_letters_cols_reply )
+void _shy_common_logic_main_menu_letters_layout_position :: receive ( so_called_message_common_logic_main_menu_letters_cols_reply msg )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_letters_cols_state :: requested )
+      && so_called_platform_conditions :: wholes_are_equal ( shy_guts :: logic_main_menu_letters_cols_state :: requested_row , msg . row )
+       )
+    {
+        shy_guts :: logic_main_menu_letters_cols_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: logic_main_menu_letters_cols_state :: replied = so_called_platform_math_consts :: whole_true ;
+        shy_guts :: logic_main_menu_letters_cols_state :: cols = msg . cols ;
+        shy_guts :: proceed_with_layout ( ) ;
+    }
 }
 
 void _shy_common_logic_main_menu_letters_layout_position :: receive ( so_called_message_common_logic_main_menu_letters_layout_position_request msg )
