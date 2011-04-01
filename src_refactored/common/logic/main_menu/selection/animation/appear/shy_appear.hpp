bd49d68d4014b61384b9f1bf80919c48ec1c1e90
olegus8 <olegus8@localhost>
2011-04-01 15:44:48 +0000

    working on common logic main menu selection animation appear refactoring

--- src_refactored/common/logic/main_menu/selection/animation/appear/shy_appear.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/appear/shy_appear.hpp
@@ -34,6 +34,8 @@ void shy_guts :: reply_computed_transform ( )
 
 void _shy_common_logic_main_menu_selection_animation_appear :: receive ( so_called_message_common_logic_main_menu_selection_animation_appear_start )
 {
+    shy_guts :: logic_main_menu_update_state :: started = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: logic_main_menu_update_state :: time = so_called_platform_math_consts :: fract_0 ;
 }
 
 void _shy_common_logic_main_menu_selection_animation_appear :: receive ( so_called_message_common_logic_main_menu_selection_animation_appear_transform_request )
@@ -42,4 +44,24 @@ void _shy_common_logic_main_menu_selection_animation_appear :: receive ( so_call
 
 void _shy_common_logic_main_menu_selection_animation_appear :: receive ( so_called_message_common_logic_main_menu_update )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_update_state :: started ) )
+    {
+        so_called_type_platform_math_num_fract time ;
+        so_called_type_platform_math_num_fract time_step ;
+        so_called_type_platform_math_num_fract total_animation_time ;
+        
+        time = shy_guts :: logic_main_menu_update_state :: time ;
+        total_animation_time = so_called_common_logic_main_menu_selection_animation_consts :: appear_total_animation_time ;
+        so_called_platform_math :: make_num_fract ( time_step , 1 , so_called_platform_consts :: frames_per_second ) ;
+        
+        so_called_platform_math :: add_to_fract ( time , time_step ) ;
+        
+        if ( so_called_platform_conditions :: fract_greater_than_fract ( time , total_animation_time ) )
+        {
+            shy_guts :: logic_main_menu_update_state :: started = so_called_platform_math_consts :: whole_false ;
+            so_called_sender_common_logic_main_menu_selection_animation_appear_finished :: send ( so_called_message_common_logic_main_menu_selection_animation_appear_finished ( ) ) ;
+        }
+
+        shy_guts :: logic_main_menu_update_state :: time = time ;
+    }
 }
