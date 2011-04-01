c20323f6acd064af247c97fe87b917d5a77aaddb
olegus8 <olegus8@localhost>
2011-04-01 10:48:08 +0000

    working on common logic main menu selection tracker refactoring

--- src_refactored/common/logic/main_menu/selection/tracker/shy_tracker_injections.cpp
+++ src_refactored/common/logic/main_menu/selection/tracker/shy_tracker_injections.cpp
@@ -1,5 +1,7 @@
 #include "./shy_tracker_injections.h"
 
+#include "../../../controls/sender/state_request/shy_state_request_injections.h"
+
 #include "../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../../../injections/platform/matrix/type/data/shy_data.h"
