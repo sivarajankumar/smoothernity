b7a76f6940ff1a5c5c447b22bca43dd14a0a5b14
olegus8 <olegus8@localhost>
2011-04-01 20:52:55 +0000

    correct clean script

--- src_refactored/macosx/clean.sh
+++ src_refactored/macosx/clean.sh
@@ -1,1 +1,3 @@
+xcodebuild -project smoothernity.xcodeproj -target "smoothernity (Upgraded)" -configuration Development clean
+xcodebuild -project smoothernity.xcodeproj -target "smoothernity (Upgraded)" -configuration Deployment clean
 rm -rf build
