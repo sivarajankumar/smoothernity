bfc3ef8edd240c28573606ba9af30be9e2991279
olegus8 <olegus8@localhost>
2011-04-01 14:53:18 +0000

    working on common logic main menu selection animation refactoring

--- src_refactored/common/logic/main_menu/selection/animation/shy_animation.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/shy_animation.hpp
@@ -168,6 +168,10 @@ void shy_guts :: obtain_idle_attention_transform ( )
 
 void shy_guts :: obtain_appear_transform ( )
 {
+    shy_guts :: logic_main_menu_selection_animation_appear_transform_state :: requested = so_called_platform_math_consts :: whole_true ;
+    so_called_sender_common_logic_main_menu_selection_animation_appear_transform_request :: send 
+        ( so_called_message_common_logic_main_menu_selection_animation_appear_transform_request ( ) 
+        ) ;
 }
 
 void shy_guts :: obtain_disappear_transform ( )
