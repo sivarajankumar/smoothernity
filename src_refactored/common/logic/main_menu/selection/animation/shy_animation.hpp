5356e857e6cecb9318daa14bf4b5bb959cfb559c
olegus8 <olegus8@localhost>
2011-04-01 14:13:49 +0000

    working on common logic main menu selection animation refactoring

--- src_refactored/common/logic/main_menu/selection/animation/shy_animation.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/shy_animation.hpp
@@ -156,8 +156,16 @@ void _shy_common_logic_main_menu_selection_animation :: receive ( so_called_mess
 {
 }
 
-void _shy_common_logic_main_menu_selection_animation :: receive ( so_called_message_common_logic_main_menu_selection_animation_idle_attention_transform_reply )
+void _shy_common_logic_main_menu_selection_animation :: receive ( so_called_message_common_logic_main_menu_selection_animation_idle_attention_transform_reply msg )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_selection_animation_idle_attention_transform_state :: requested ) )
+    {
+        shy_guts :: logic_main_menu_selection_animation_idle_attention_transform_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: logic_main_menu_selection_animation_idle_attention_transform_state :: replied = so_called_platform_math_consts :: whole_true ;
+        shy_guts :: logic_main_menu_selection_animation_idle_attention_transform_state :: scale_x = msg . scale_x ;
+        shy_guts :: logic_main_menu_selection_animation_idle_attention_transform_state :: scale_y = msg . scale_y ;
+        shy_guts :: proceed_with_transform ( ) ;
+    }
 }
 
 void _shy_common_logic_main_menu_selection_animation :: receive ( so_called_message_common_logic_main_menu_selection_animation_idle_transform_reply msg )
