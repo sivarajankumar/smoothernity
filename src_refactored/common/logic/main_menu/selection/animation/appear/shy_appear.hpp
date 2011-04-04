4e3b5e723a768db2fda2540a06bf11d18d616a4c
olegus8 <olegus8@localhost>
2011-04-04 15:15:51 +0000

    working on common logic main menu selection animation appear refactoring

--- src_refactored/common/logic/main_menu/selection/animation/appear/shy_appear.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/appear/shy_appear.hpp
@@ -74,6 +74,10 @@ void shy_guts :: compute_vertical_scale ( )
 
 void shy_guts :: reply_computed_transform ( )
 {
+    so_called_message_common_logic_main_menu_selection_animation_appear_transform_reply msg ;
+    msg . scale_x = shy_guts :: logic_main_menu_selection_animation_appear_transform_state :: horizontal_scale ;
+    msg . scale_y = shy_guts :: logic_main_menu_selection_animation_appear_transform_state :: vertical_scale ;
+    so_called_sender_common_logic_main_menu_selection_animation_appear_transform_reply :: send ( msg ) ;
 }
 
 void _shy_common_logic_main_menu_selection_animation_appear :: receive ( so_called_message_common_logic_main_menu_selection_animation_appear_start )
