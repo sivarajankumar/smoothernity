1c9175074c14701b77c53e2f7f450d465d5a0e6d
olegus8 <olegus8@localhost>
2011-04-04 15:12:49 +0000

    working on common logic main menu selection animation appear refactoring

--- src_refactored/common/logic/main_menu/selection/animation/appear/shy_appear.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/appear/shy_appear.hpp
@@ -45,6 +45,31 @@ void shy_guts :: compute_horizontal_scale ( )
 
 void shy_guts :: compute_vertical_scale ( )
 {
+    so_called_type_platform_math_num_fract vertical_scale_time_to_begin ;
+    so_called_type_platform_math_num_fract vertical_scale_time_from_begin_to_middle ;
+    so_called_type_platform_math_num_fract vertical_scale_time_from_middle_to_end ;
+    so_called_type_platform_math_num_fract time_begin ;
+    so_called_type_platform_math_num_fract time_middle ;
+    so_called_type_platform_math_num_fract time_end ;
+    
+    vertical_scale_time_to_begin = so_called_common_logic_main_menu_selection_animation_consts :: appear_vertical_scale_time_to_begin ;
+    vertical_scale_time_from_begin_to_middle = so_called_common_logic_main_menu_selection_animation_consts :: appear_vertical_scale_time_from_begin_to_middle ;
+    vertical_scale_time_from_middle_to_end = so_called_common_logic_main_menu_selection_animation_consts :: appear_vertical_scale_time_from_middle_to_end ;
+    
+    time_begin = vertical_scale_time_to_begin ;
+    so_called_platform_math :: add_fracts ( time_middle , time_begin , vertical_scale_time_from_begin_to_middle ) ;
+    so_called_platform_math :: add_fracts ( time_end , time_middle , vertical_scale_time_from_middle_to_end ) ;
+    
+    so_called_common_engine_math_stateless :: hard_attack_easy_decay
+        ( shy_guts :: logic_main_menu_selection_animation_appear_transform_state :: vertical_scale
+        , shy_guts :: logic_main_menu_update_state :: time
+        , so_called_common_logic_main_menu_selection_animation_consts :: appear_vertical_scale_value_begin
+        , time_begin
+        , so_called_common_logic_main_menu_selection_animation_consts :: appear_vertical_scale_value_middle
+        , time_middle
+        , so_called_common_logic_main_menu_selection_animation_consts :: appear_vertical_scale_value_end
+        , time_end
+        ) ;
 }
 
 void shy_guts :: reply_computed_transform ( )
