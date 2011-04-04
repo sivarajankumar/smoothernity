2b9f681360ea79ce8980c066517e10ddfef8e4a6
olegus8 <olegus8@localhost>
2011-04-04 16:03:42 +0000

    working on common logic main menu selection animation idle refactoring

--- src_refactored/common/logic/main_menu/selection/animation/idle/shy_idle.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/idle/shy_idle.hpp
@@ -1,5 +1,57 @@
+namespace shy_guts
+{
+    namespace logic_main_menu_selection_animation_idle_transform_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole row_is_selected ;
+        static so_called_type_platform_math_num_whole selected_row_index ;
+        static so_called_type_platform_vector_data position ;
+        static so_called_type_platform_math_num_fract scale_x ;
+        static so_called_type_platform_math_num_fract scale_y ;
+    }
+
+    namespace logic_main_menu_letters_layout_row_rect_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole requested_row ;
+        static so_called_type_platform_math_num_whole replied ;
+        static so_called_type_common_engine_rect row_rect ;
+    }
+
+    static void proceed_with_transform ( ) ;
+    static void obtain_row_rect ( ) ;
+    static void received_row_rect ( ) ;
+    static void reply_transform ( ) ;
+    static void compute_row_rect_mesh_transform ( ) ;
+    static void compute_empty_mesh_transform ( ) ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_main_menu_selection_animation_idle > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: proceed_with_transform ( )
+{
+}
+
+void shy_guts :: obtain_row_rect ( )
+{
+}
+
+void shy_guts :: received_row_rect ( )
+{
+}
+
+void shy_guts :: reply_transform ( )
+{
+}
+
+void shy_guts :: compute_row_rect_mesh_transform ( )
+{
+}
+
+void shy_guts :: compute_empty_mesh_transform ( )
+{
+}
 
 void _shy_common_logic_main_menu_selection_animation_idle :: receive ( so_called_message_common_init )
 {
--- src_refactored/common/logic/main_menu/selection/animation/idle/shy_idle_injections.cpp
+++ src_refactored/common/logic/main_menu/selection/animation/idle/shy_idle_injections.cpp
@@ -1,3 +1,6 @@
 #include "./shy_idle_injections.h"
 
+#include "../../../../../../injections/platform/math/consts/shy_consts.h"
+#include "../../../../../../injections/platform/vector/shy_vector.h"
+
 #include "./shy_idle.hpp"
