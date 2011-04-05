51615bfc020cea1d2e8baeed80f8aebc9a8e450f
olegus8 <olegus8@localhost>
2011-04-05 15:09:47 +0000

    working on common logic main menu selection animation push refactoring

--- src_refactored/common/logic/main_menu/selection/animation/push/shy_push.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/push/shy_push.hpp
@@ -37,6 +37,16 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void shy_guts :: proceed_with_update ( )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_main_menu_update_state :: requested ) )
+    {
+        shy_guts :: logic_main_menu_update_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: obtain_controls_state ( ) ;
+    }
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_controls_state :: replied ) )
+    {
+        shy_guts :: logic_controls_state :: replied = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: controls_state_received ( ) ;
+    }
 }
 
 void shy_guts :: obtain_controls_state ( )
@@ -82,6 +92,10 @@ void _shy_common_logic_main_menu_selection_animation_push :: receive ( so_called
 
 void _shy_common_logic_main_menu_selection_animation_push :: receive ( so_called_message_common_logic_main_menu_selection_animation_push_transform_request )
 {
+    shy_guts :: calculate_time ( ) ;
+    shy_guts :: calculate_vertical_scale ( ) ;
+    shy_guts :: calculate_horizontal_scale ( ) ;
+    shy_guts :: reply_transform ( ) ;
 }
 
 void _shy_common_logic_main_menu_selection_animation_push :: receive ( so_called_message_common_logic_main_menu_update )
