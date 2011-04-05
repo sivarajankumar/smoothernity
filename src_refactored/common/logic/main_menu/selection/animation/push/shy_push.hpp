f5db7134c4e04f60b3bbe53fe4faab8951562c88
olegus8 <olegus8@localhost>
2011-04-05 15:04:56 +0000

    working on common logic main menu selection animation push refactoring

--- src_refactored/common/logic/main_menu/selection/animation/push/shy_push.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/push/shy_push.hpp
@@ -1,8 +1,72 @@
+namespace shy_guts
+{
+    namespace logic_controls_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole replied ;
+        static so_called_type_platform_math_num_whole primary_button_down ;
+    }
+
+    namespace logic_main_menu_update_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static so_called_type_platform_math_num_whole clicked ;
+        static so_called_type_platform_math_num_fract time ;
+    }
+
+    namespace logic_main_menu_selection_animation_push_transform_state
+    {
+        static so_called_type_platform_math_num_fract time_begin ;
+        static so_called_type_platform_math_num_fract time_middle ;
+        static so_called_type_platform_math_num_fract time_end ;
+        static so_called_type_platform_math_num_fract vertical_scale ;
+        static so_called_type_platform_math_num_fract horizontal_scale ;
+    }
+
+    static void proceed_with_update ( ) ;
+    static void obtain_controls_state ( ) ;
+    static void controls_state_received ( ) ;
+    static void calculate_time ( ) ;
+    static void calculate_horizontal_scale ( ) ;
+    static void calculate_vertical_scale ( ) ;
+    static void reply_transform ( ) ;
+}
+
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_main_menu_selection_animation_push > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
+void shy_guts :: proceed_with_update ( )
+{
+}
+
+void shy_guts :: obtain_controls_state ( )
+{
+}
+
+void shy_guts :: controls_state_received ( )
+{
+}
+
+void shy_guts :: calculate_time ( )
+{
+}
+
+void shy_guts :: calculate_horizontal_scale ( )
+{
+}
+
+void shy_guts :: calculate_vertical_scale ( )
+{
+}
+
+void shy_guts :: reply_transform ( )
+{
+}
+
 void _shy_common_logic_main_menu_selection_animation_push :: receive ( so_called_message_common_init )
 {
+    shy_guts :: logic_main_menu_update_state :: clicked = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: logic_main_menu_update_state :: time = so_called_platform_math_consts :: fract_0 ;
 }
 
 void _shy_common_logic_main_menu_selection_animation_push :: receive ( so_called_message_common_logic_controls_state_reply )
--- src_refactored/common/logic/main_menu/selection/animation/push/shy_push_injections.cpp
+++ src_refactored/common/logic/main_menu/selection/animation/push/shy_push_injections.cpp
@@ -1,3 +1,5 @@
 #include "./shy_push_injections.h"
 
+#include "../../../../../../injections/platform/math/consts/shy_consts.h"
+
 #include "./shy_push.hpp"
