6cb9cb9c5c3ccd375e0a9850fe8d67def1bc14f3
olegus8 <olegus8@localhost>
2011-04-06 16:39:51 +0000

    working on common logic main menu selection animation select refactoring

--- src_refactored/common/logic/main_menu/selection/animation/select/shy_select.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/select/shy_select.hpp
@@ -39,6 +39,8 @@ void shy_guts :: reply_computed_transform ( )
 
 void _shy_common_logic_main_menu_selection_animation_select :: receive ( so_called_message_common_logic_main_menu_selection_animation_select_start )
 {
+    shy_guts :: logic_main_menu_update_state :: select_started = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: logic_main_menu_update_state :: time = so_called_platform_math_consts :: fract_0 ;
 }
 
 void _shy_common_logic_main_menu_selection_animation_select :: receive ( so_called_message_common_logic_main_menu_selection_animation_select_transform_request )
