33b916d8b54c8e1430115c2f732be9b1d993cb38
olegus8 <olegus8@localhost>
2011-04-06 15:47:34 +0000

    working on common logic main menu selection animation push weight refactoring

--- src_refactored/common/logic/main_menu/selection/animation/push_weight/shy_push_weight_injections.cpp
+++ src_refactored/common/logic/main_menu/selection/animation/push_weight/shy_push_weight_injections.cpp
@@ -1,5 +1,7 @@
 #include "./shy_push_weight_injections.h"
 
+#include "../../../../controls/sender/state_request/shy_state_request_injections.h"
+
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
 
