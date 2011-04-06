b202e1e53431ea97fca121e658b5f44746927356
olegus8 <olegus8@localhost>
2011-04-06 15:09:53 +0000

    working on common logic main menu selection animation push attention refactoring

--- src_refactored/common/logic/main_menu/selection/animation/push_attention/shy_push_attention_injections.cpp
+++ src_refactored/common/logic/main_menu/selection/animation/push_attention/shy_push_attention_injections.cpp
@@ -1,5 +1,7 @@
 #include "./shy_push_attention_injections.h"
 
+#include "../sender/push_attention_transform_reply/shy_push_attention_transform_reply_injections.h"
+
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../../injections/platform/consts/shy_consts.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
