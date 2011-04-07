08ce107708b35e32a529e2e9fe4dd326c29e335a
olegus8 <olegus8@localhost>
2011-04-07 13:37:27 +0000

    working on common logic main menu selection animation unselect refactoring

--- src_refactored/common/logic/main_menu/selection/animation/unselect/shy_unselect.hpp
+++ src_refactored/common/logic/main_menu/selection/animation/unselect/shy_unselect.hpp
@@ -23,6 +23,25 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void shy_guts :: compute_horizontal_scale ( )
 {
+    so_called_type_platform_math_num_fract horizontal_scale_time_to_begin ;
+    so_called_type_platform_math_num_fract horizontal_scale_time_from_begin_to_end ;
+    so_called_type_platform_math_num_fract time_begin ;
+    so_called_type_platform_math_num_fract time_end ;
+    
+    horizontal_scale_time_to_begin = so_called_common_logic_main_menu_selection_animation_consts :: unselect_horizontal_scale_time_to_begin ;
+    horizontal_scale_time_from_begin_to_end = so_called_common_logic_main_menu_selection_animation_consts :: unselect_horizontal_scale_time_from_begin_to_end ;
+    
+    time_begin = horizontal_scale_time_to_begin ;
+    so_called_platform_math :: add_fracts ( time_end , time_begin , horizontal_scale_time_from_begin_to_end ) ;
+    
+    so_called_common_engine_math_stateless :: easy_in_hard_out
+        ( shy_guts :: logic_main_menu_selection_animation_unselect_transform_state :: horizontal_scale
+        , shy_guts :: logic_main_menu_update_state :: time
+        , so_called_common_logic_main_menu_selection_animation_consts :: unselect_horizontal_scale_value_begin
+        , time_begin
+        , so_called_common_logic_main_menu_selection_animation_consts :: unselect_horizontal_scale_value_end
+        , time_end
+        ) ;
 }
 
 void shy_guts :: compute_vertical_scale ( )
