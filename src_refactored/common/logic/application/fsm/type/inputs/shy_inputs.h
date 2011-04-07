cff282b17ff6339ffd65c345488a4d19463f8aa2
olegus8 <olegus8@localhost>
2011-04-07 14:27:06 +0000

    working on common logic application fsm inputs refactoring

---
+++ src_refactored/common/logic/application/fsm/type/inputs/shy_inputs.h
@@ -0,0 +1,20 @@
+class logic_application_fsm_inputs_type
+{
+public :
+    so_called_type_platform_math_num_whole logic_amusement_created ;
+    so_called_type_platform_math_num_whole logic_amusement_finished ;
+    so_called_type_platform_math_num_whole logic_application_render ;
+    so_called_type_platform_math_num_whole logic_application_update ;
+    so_called_type_platform_math_num_whole logic_text_prepared ;
+    so_called_type_platform_math_num_whole logic_title_created ;
+    so_called_type_platform_math_num_whole logic_title_finished ;
+    so_called_type_platform_math_num_whole logic_main_menu_created ;
+    so_called_type_platform_math_num_whole logic_main_menu_finished ;
+    so_called_type_platform_math_num_whole stage_amusement_disabled ;
+    so_called_type_platform_math_num_whole stage_amusement_enabled ;
+    so_called_type_platform_math_num_whole stage_main_menu_disabled ;
+    so_called_type_platform_math_num_whole stage_main_menu_enabled ;
+    so_called_type_platform_math_num_whole stage_title_disabled ;
+    so_called_type_platform_math_num_whole stage_title_enabled ;
+} ;
+
---
+++ src_refactored/common/logic/application/fsm/type/inputs/shy_inputs_injections.h
@@ -0,0 +1,10 @@
+#ifndef _shy_type_common_logic_application_fsm_inputs_injections_included
+#define _shy_type_common_logic_application_fsm_inputs_injections_included
+
+#include "../../../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
+
+#include "./shy_inputs.h"
+
+typedef shy_type_common_logic_application_fsm_inputs so_called_type_common_logic_application_fsm_inputs ;
+
+#endif
