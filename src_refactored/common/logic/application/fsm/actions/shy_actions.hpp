08f73f93f63cef203bfeaf76ed8a306598f242b3
olegus8 <olegus8@localhost>
2011-04-07 15:09:34 +0000

    working on common logic application fsm actions refactoring

--- src_refactored/common/logic/application/fsm/actions/shy_actions.hpp
+++ src_refactored/common/logic/application/fsm/actions/shy_actions.hpp
@@ -1,80 +1,80 @@
 void shy_common_logic_application_fsm_actions :: logic_amusement_creation_permit ( )
 {
-//    so_called_sender_common_logic_amusement_creation_permit :: send ( so_called_message_common_logic_amusement_creation_permit ( ) ) ;
+    so_called_sender_common_logic_amusement_creation_permit :: send ( so_called_message_common_logic_amusement_creation_permit ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_amusement_launch_permit ( )
 {
-//    so_called_sender_common_logic_amusement_launch_permit :: send ( so_called_message_common_logic_amusement_launch_permit ( ) ) ;
+    so_called_sender_common_logic_amusement_launch_permit :: send ( so_called_message_common_logic_amusement_launch_permit ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_amusement_render ( )
 {
-//    so_called_sender_common_logic_amusement_render :: send ( so_called_message_common_logic_amusement_render ( ) ) ;
+    so_called_sender_common_logic_amusement_render :: send ( so_called_message_common_logic_amusement_render ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_amusement_update ( )
 {
-//    so_called_sender_common_logic_amusement_update :: send ( so_called_message_common_logic_amusement_update ( ) ) ;
+    so_called_sender_common_logic_amusement_update :: send ( so_called_message_common_logic_amusement_update ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_game_launch_permit ( )
 {
-//    so_called_sender_common_logic_game_launch_permit :: send ( so_called_message_common_logic_game_launch_permit ( ) ) ;
+    so_called_sender_common_logic_game_launch_permit :: send ( so_called_message_common_logic_game_launch_permit ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_game_render ( )
 {
-//    so_called_sender_common_logic_game_render :: send ( so_called_message_common_logic_game_render ( ) ) ;
+    so_called_sender_common_logic_game_render :: send ( so_called_message_common_logic_game_render ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_game_update ( )
 {
-//    so_called_sender_common_logic_game_update :: send ( so_called_message_common_logic_game_update ( ) ) ;
+    so_called_sender_common_logic_game_update :: send ( so_called_message_common_logic_game_update ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_main_menu_creation_permit ( )
 {
-//    so_called_sender_common_logic_main_menu_creation_permit :: send ( so_called_message_common_logic_main_menu_creation_permit ( ) ) ;
+    so_called_sender_common_logic_main_menu_creation_permit :: send ( so_called_message_common_logic_main_menu_creation_permit ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_main_menu_launch_permit ( )
 {
-//    so_called_sender_common_logic_main_menu_launch_permit :: send ( so_called_message_common_logic_main_menu_launch_permit ( ) ) ;
+    so_called_sender_common_logic_main_menu_launch_permit :: send ( so_called_message_common_logic_main_menu_launch_permit ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_main_menu_render ( )
 {
-//    so_called_sender_common_logic_main_menu_render :: send ( so_called_message_common_logic_main_menu_render ( ) ) ;
+    so_called_sender_common_logic_main_menu_render :: send ( so_called_message_common_logic_main_menu_render ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_main_menu_update ( )
 {
-//    so_called_sender_common_logic_main_menu_update :: send ( so_called_message_common_logic_main_menu_update ( ) ) ;
+    so_called_sender_common_logic_main_menu_update :: send ( so_called_message_common_logic_main_menu_update ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_text_prepare_permit ( )
 {
-//    so_called_sender_common_logic_text_prepare_permit :: send ( so_called_message_common_logic_text_prepare_permit ( ) ) ;
+    so_called_sender_common_logic_text_prepare_permit :: send ( so_called_message_common_logic_text_prepare_permit ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_text_update ( )
 {
-//    so_called_sender_common_logic_text_update :: send ( so_called_message_common_logic_text_update ( ) ) ;
+    so_called_sender_common_logic_text_update :: send ( so_called_message_common_logic_text_update ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_title_launch_permit ( )
 {
-//    so_called_sender_common_logic_title_launch_permit :: send ( so_called_message_common_logic_title_launch_permit ( ) ) ;
+    so_called_sender_common_logic_title_launch_permit :: send ( so_called_message_common_logic_title_launch_permit ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_title_render ( )
 {
-//    so_called_sender_common_logic_title_render :: send ( so_called_message_common_logic_title_render ( ) ) ;
+    so_called_sender_common_logic_title_render :: send ( so_called_message_common_logic_title_render ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_title_update ( )
 {
-//    so_called_sender_common_logic_title_update :: send ( so_called_message_common_logic_title_update ( ) ) ;
+    so_called_sender_common_logic_title_update :: send ( so_called_message_common_logic_title_update ( ) ) ;
 }
 
