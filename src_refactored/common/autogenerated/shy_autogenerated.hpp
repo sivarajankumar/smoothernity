254652faf417a01aaa642922168a51aaf75a8f70
olegus8 <olegus8@localhost>
2011-04-07 14:19:11 +0000

    working on common logic application consts refactoring

--- src_refactored/common/autogenerated/shy_autogenerated.hpp
+++ src_refactored/common/autogenerated/shy_autogenerated.hpp
@@ -2,6 +2,10 @@ const so_called_type_platform_math_num_fract so_called_common_logic_amusement_co
 const so_called_type_platform_math_num_fract so_called_common_logic_amusement_consts :: renderer_clear_color_g = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
 const so_called_type_platform_math_num_fract so_called_common_logic_amusement_consts :: renderer_clear_color_r = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
 
+const so_called_type_platform_math_num_whole so_called_common_logic_application_consts :: skip_amusement = so_called_platform_math :: init_num_whole ( 0 ) ;
+const so_called_type_platform_math_num_whole so_called_common_logic_application_consts :: skip_main_menu = so_called_platform_math :: init_num_whole ( 0 ) ;
+const so_called_type_platform_math_num_whole so_called_common_logic_application_consts :: skip_title = so_called_platform_math :: init_num_whole ( 0 ) ;
+
 const so_called_type_platform_math_num_fract so_called_common_logic_blanket_consts :: mesh_color_a = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
 const so_called_type_platform_math_num_fract so_called_common_logic_blanket_consts :: mesh_color_b = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
 const so_called_type_platform_math_num_fract so_called_common_logic_blanket_consts :: mesh_color_g = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
---
+++ src_refactored/common/logic/application/consts/shy_consts.h
@@ -0,0 +1,6 @@
+class shy_common_logic_application_consts
+{
+    static const so_called_type_platform_math_num_whole skip_title ;
+    static const so_called_type_platform_math_num_whole skip_main_menu ;
+    static const so_called_type_platform_math_num_whole skip_amusement ;
+} ;
