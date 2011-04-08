616299697755e352775c6adb7124734e54864562
olegus8 <olegus8@localhost>
2011-04-08 14:10:08 +0000

    working on common logic application fsm refactoring

--- src_refactored/common/logic/application/fsm/shy_fsm.hpp
+++ src_refactored/common/logic/application/fsm/shy_fsm.hpp
@@ -9,6 +9,15 @@ template < > _scheduled_context_type _scheduled_context_type :: _singleton = _sc
 
 void _shy_common_logic_application_fsm :: reset_input_events ( )
 {
+    shy_guts :: current_inputs . logic_amusement_created = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: current_inputs . logic_amusement_finished = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: current_inputs . logic_application_render = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: current_inputs . logic_application_update = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: current_inputs . logic_main_menu_created = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: current_inputs . logic_main_menu_finished = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: current_inputs . logic_text_prepared = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: current_inputs . logic_title_created = so_called_platform_math_consts :: whole_false ;
+    shy_guts :: current_inputs . logic_title_finished = so_called_platform_math_consts :: whole_false ;
 }
 
 void _shy_common_logic_application_fsm :: recalc_current_inputs ( )
