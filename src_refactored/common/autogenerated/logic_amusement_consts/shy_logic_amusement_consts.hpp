083d33ebb4211ab0d9364880ec30780c50857096
olegus8 <olegus8@localhost>
2011-04-12 16:03:49 +0000

    working on common autogenerated consts refactoring

---
+++ src_refactored/common/autogenerated/logic_amusement_consts/shy_logic_amusement_consts.hpp
@@ -0,0 +1,4 @@
+const so_called_type_platform_math_num_fract so_called_common_logic_amusement_consts :: renderer_clear_color_b = so_called_platform_math :: init_num_fract ( 1 , 3 ) ;
+const so_called_type_platform_math_num_fract so_called_common_logic_amusement_consts :: renderer_clear_color_g = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+const so_called_type_platform_math_num_fract so_called_common_logic_amusement_consts :: renderer_clear_color_r = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+
--- src_refactored/common/autogenerated/shy_autogenerated.hpp
+++ src_refactored/common/autogenerated/shy_autogenerated.hpp
@@ -1,7 +1,3 @@
-const so_called_type_platform_math_num_fract so_called_common_logic_amusement_consts :: renderer_clear_color_b = so_called_platform_math :: init_num_fract ( 1 , 3 ) ;
-const so_called_type_platform_math_num_fract so_called_common_logic_amusement_consts :: renderer_clear_color_g = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
-const so_called_type_platform_math_num_fract so_called_common_logic_amusement_consts :: renderer_clear_color_r = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
-
 const so_called_type_platform_math_num_whole so_called_common_logic_application_consts :: skip_amusement = so_called_platform_math :: init_num_whole ( 0 ) ;
 const so_called_type_platform_math_num_whole so_called_common_logic_application_consts :: skip_main_menu = so_called_platform_math :: init_num_whole ( 0 ) ;
 const so_called_type_platform_math_num_whole so_called_common_logic_application_consts :: skip_title = so_called_platform_math :: init_num_whole ( 0 ) ;
