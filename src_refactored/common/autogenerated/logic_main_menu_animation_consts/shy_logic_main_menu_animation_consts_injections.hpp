6a2d97f7b4ae47d9ec7e7a15b98bfacacc02441a
olegus8 <olegus8@localhost>
2011-04-13 10:15:54 +0000

    working on common autogenerated consts refactoring

---
+++ src_refactored/common/autogenerated/logic_main_menu_animation_consts/shy_logic_main_menu_animation_consts_injections.hpp
@@ -0,0 +1,3 @@
+#include "../../../injections/platform/math/shy_math.h"
+
+#include "./shy_logic_main_menu_animation_consts.hpp"
--- src_refactored/common/autogenerated/shy_autogenerated.hpp
+++ src_refactored/common/autogenerated/shy_autogenerated.hpp
@@ -1,9 +1,4 @@
 const so_called_type_platform_math_num_fract so_called_common_logic_main_menu_animation_consts :: shake_shift_x_amplitude_begin = so_called_platform_math :: init_num_fract ( 20 , 1000 ) ;
-const so_called_type_platform_math_num_fract so_called_common_logic_main_menu_animation_consts :: shake_shift_x_amplitude_end = so_called_platform_math :: init_num_fract ( 5 , 1000 ) ;
-const so_called_type_platform_math_num_fract so_called_common_logic_main_menu_animation_consts :: shake_shift_x_period_in_seconds = so_called_platform_math :: init_num_fract ( 2 , 10 ) ;
-const so_called_type_platform_math_num_fract so_called_common_logic_main_menu_animation_consts :: shake_time_from_begin_to_end = so_called_platform_math :: init_num_fract ( 6 , 10 ) ;
-const so_called_type_platform_math_num_fract so_called_common_logic_main_menu_animation_consts :: shake_time_to_begin = so_called_platform_math :: init_num_fract ( 0 , 10 ) ;
-
 const so_called_type_platform_math_num_fract so_called_common_logic_main_menu_letters_animation_consts :: appear_delay_per_col_in_seconds = so_called_platform_math :: init_num_fract ( 2 , 100 ) ;
 const so_called_type_platform_math_num_fract so_called_common_logic_main_menu_letters_animation_consts :: appear_delay_per_row_in_seconds = so_called_platform_math :: init_num_fract ( 5 , 100 ) ;
 const so_called_type_platform_math_num_fract so_called_common_logic_main_menu_letters_animation_consts :: appear_scale_begin = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
