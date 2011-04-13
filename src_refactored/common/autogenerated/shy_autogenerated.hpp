2402021abece6a87e1c60376ef53035ef82a822f
olegus8 <olegus8@localhost>
2011-04-13 11:24:48 +0000

    working on common autogenerated consts refactoring

--- src_refactored/common/autogenerated/shy_autogenerated.hpp
+++ src_refactored/common/autogenerated/shy_autogenerated.hpp
@@ -1,6 +1,3 @@
-const so_called_type_platform_math_num_fract so_called_common_logic_ortho_consts :: z_far = so_called_platform_math :: init_num_fract ( 50 , 1 ) ;
-const so_called_type_platform_math_num_fract so_called_common_logic_ortho_consts :: z_near = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
-
 const so_called_type_platform_math_num_fract so_called_common_logic_perspective_consts :: z_far_unscaled = so_called_platform_math :: init_num_fract ( 50 , 1 ) ;
 
 const so_called_type_platform_math_num_whole so_called_common_logic_room_consts :: texture_grid_size = so_called_platform_math :: init_num_whole ( 10 ) ;
