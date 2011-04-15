7da9bb84278bbebf20512fe0e2778f3795482485
olegus8 <olegus8@localhost>
2011-04-15 10:49:14 +0000

    working on loadable consts reflection refactoring

---
+++ src_refactored/loadable/consts/reflection/logic_main_menu_selection/shy_logic_main_menu_selection.hpp
@@ -0,0 +1,10 @@
+void shy_loadable_consts_reflection_logic_main_menu_selection :: prepare ( )
+{
+    so_called_loadable_consts_binder :: module ( "logic_main_menu_selection" ) ;
+    so_called_loadable_consts_binder :: bind ( "mesh_size" , so_called_common_logic_main_menu_selection_consts :: mesh_size ) ;
+    so_called_loadable_consts_binder :: bind ( "mesh_color_r" , so_called_common_logic_main_menu_selection_consts :: mesh_color_r ) ;
+    so_called_loadable_consts_binder :: bind ( "mesh_color_g" , so_called_common_logic_main_menu_selection_consts :: mesh_color_g ) ;
+    so_called_loadable_consts_binder :: bind ( "mesh_color_b" , so_called_common_logic_main_menu_selection_consts :: mesh_color_b ) ;
+    so_called_loadable_consts_binder :: bind ( "mesh_color_a" , so_called_common_logic_main_menu_selection_consts :: mesh_color_a ) ;
+    so_called_loadable_consts_binder :: bind ( "selected_rect_vertical_scale" , so_called_common_logic_main_menu_selection_consts :: selected_rect_vertical_scale ) ;
+}
--- src_refactored/loadable/consts/reflection/shy_reflection_injections.cpp
+++ src_refactored/loadable/consts/reflection/shy_reflection_injections.cpp
@@ -11,6 +11,7 @@
     #include "./logic_main_menu_letters_animation/shy_logic_main_menu_letters_animation_injections.h"
     #include "./logic_main_menu_letters_layout/shy_logic_main_menu_letters_layout_injections.h"
     #include "./logic_main_menu_letters_meshes/shy_logic_main_menu_letters_meshes_injections.h"
+    #include "./logic_main_menu_selection/shy_logic_main_menu_selection_injections.h"
 
     #include "./shy_reflection_injections.h"
     #include "./shy_reflection.hpp"
