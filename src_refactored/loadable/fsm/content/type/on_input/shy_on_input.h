ca9a4ffe5e7619c370bce777bafc0632985c917c
olegus8 <olegus8@localhost>
2011-04-15 14:55:11 +0000

    working on loadable fsm content refactoring

--- src_refactored/loadable/fsm/content/type/on_input/shy_on_input.h
+++ src_refactored/loadable/fsm/content/type/on_input/shy_on_input.h
@@ -1,3 +1,6 @@
 class shy_type_loadable_fsm_content_on_input
 {
+public :
+    so_called_type_loadable_fsm_content_condition_group_container condition_groups ;
+    so_called_type_loadable_fsm_content_actions actions ;
 } ;
