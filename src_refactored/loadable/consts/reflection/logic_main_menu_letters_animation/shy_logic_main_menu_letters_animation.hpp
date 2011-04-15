5a48ddd96f8c28e13eb015f9f5cc7affac630bb9
olegus8 <olegus8@localhost>
2011-04-15 10:25:21 +0000

    working on loadable consts reflection refactoring

---
+++ src_refactored/loadable/consts/reflection/logic_main_menu_letters_animation/shy_logic_main_menu_letters_animation.hpp
@@ -0,0 +1,37 @@
+void shy_loadable_consts_reflection_logic_main_menu_letters_animation :: prepare ( )
+{
+    so_called_loadable_consts_binder :: module ( "logic_main_menu_letters_animation" ) ;
+    so_called_loadable_consts_binder :: bind ( "appear_time_from_begin_to_middle_in_seconds" , so_called_common_logic_main_menu_letters_animation_consts :: appear_time_from_begin_to_middle_in_seconds ) ;
+    so_called_loadable_consts_binder :: bind ( "appear_time_from_middle_to_end_in_seconds" , so_called_common_logic_main_menu_letters_animation_consts :: appear_time_from_middle_to_end_in_seconds ) ;
+    so_called_loadable_consts_binder :: bind ( "appear_delay_per_col_in_seconds" , so_called_common_logic_main_menu_letters_animation_consts :: appear_delay_per_col_in_seconds ) ;
+    so_called_loadable_consts_binder :: bind ( "appear_delay_per_row_in_seconds" , so_called_common_logic_main_menu_letters_animation_consts :: appear_delay_per_row_in_seconds ) ;
+    so_called_loadable_consts_binder :: bind ( "appear_scale_begin" , so_called_common_logic_main_menu_letters_animation_consts :: appear_scale_begin ) ;
+    so_called_loadable_consts_binder :: bind ( "appear_scale_middle" , so_called_common_logic_main_menu_letters_animation_consts :: appear_scale_middle ) ;
+    so_called_loadable_consts_binder :: bind ( "appear_scale_end" , so_called_common_logic_main_menu_letters_animation_consts :: appear_scale_end ) ;
+    so_called_loadable_consts_binder :: bind ( "disappear_animation_time_in_seconds" , so_called_common_logic_main_menu_letters_animation_consts :: disappear_animation_time_in_seconds ) ;
+    so_called_loadable_consts_binder :: bind ( "disappear_time_from_begin_to_end_in_seconds" , so_called_common_logic_main_menu_letters_animation_consts :: disappear_time_from_begin_to_end_in_seconds ) ;
+    so_called_loadable_consts_binder :: bind ( "disappear_delay_per_row_in_seconds" , so_called_common_logic_main_menu_letters_animation_consts :: disappear_delay_per_row_in_seconds ) ;
+    so_called_loadable_consts_binder :: bind ( "disappear_delay_per_col_in_seconds" , so_called_common_logic_main_menu_letters_animation_consts :: disappear_delay_per_col_in_seconds ) ;
+    so_called_loadable_consts_binder :: bind ( "disappear_scale_begin" , so_called_common_logic_main_menu_letters_animation_consts :: disappear_scale_begin ) ;
+    so_called_loadable_consts_binder :: bind ( "disappear_scale_end" , so_called_common_logic_main_menu_letters_animation_consts :: disappear_scale_end ) ;
+    so_called_loadable_consts_binder :: bind ( "idle_vertical_shift_period_in_seconds" , so_called_common_logic_main_menu_letters_animation_consts :: idle_vertical_shift_period_in_seconds ) ;
+    so_called_loadable_consts_binder :: bind ( "idle_vertical_shift_phase_per_col" , so_called_common_logic_main_menu_letters_animation_consts :: idle_vertical_shift_phase_per_col ) ;
+    so_called_loadable_consts_binder :: bind ( "idle_vertical_shift_phase_per_row" , so_called_common_logic_main_menu_letters_animation_consts :: idle_vertical_shift_phase_per_row ) ;
+    so_called_loadable_consts_binder :: bind ( "idle_vertical_shift_amplitude" , so_called_common_logic_main_menu_letters_animation_consts :: idle_vertical_shift_amplitude ) ;
+    so_called_loadable_consts_binder :: bind ( "idle_horizontal_shift_period_in_seconds" , so_called_common_logic_main_menu_letters_animation_consts :: idle_horizontal_shift_period_in_seconds ) ;
+    so_called_loadable_consts_binder :: bind ( "idle_horizontal_shift_phase_per_row" , so_called_common_logic_main_menu_letters_animation_consts :: idle_horizontal_shift_phase_per_row ) ;
+    so_called_loadable_consts_binder :: bind ( "idle_horizontal_shift_amplitude" , so_called_common_logic_main_menu_letters_animation_consts :: idle_horizontal_shift_amplitude ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_time_stable" , so_called_common_logic_main_menu_letters_animation_consts :: selection_time_stable ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_time_transition" , so_called_common_logic_main_menu_letters_animation_consts :: selection_time_transition ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_scale_min" , so_called_common_logic_main_menu_letters_animation_consts :: selection_scale_min ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_scale_max" , so_called_common_logic_main_menu_letters_animation_consts :: selection_scale_max ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_push_time_from_begin_to_middle" , so_called_common_logic_main_menu_letters_animation_consts :: selection_push_time_from_begin_to_middle ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_push_time_from_middle_to_end" , so_called_common_logic_main_menu_letters_animation_consts :: selection_push_time_from_middle_to_end ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_push_scale_begin" , so_called_common_logic_main_menu_letters_animation_consts :: selection_push_scale_begin ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_push_scale_middle" , so_called_common_logic_main_menu_letters_animation_consts :: selection_push_scale_middle ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_push_scale_end" , so_called_common_logic_main_menu_letters_animation_consts :: selection_push_scale_end ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_weight_time_to_begin" , so_called_common_logic_main_menu_letters_animation_consts :: selection_weight_time_to_begin ) ;
+    so_called_loadable_consts_binder :: bind ( "selection_weight_time_from_begin_to_end" , so_called_common_logic_main_menu_letters_animation_consts :: selection_weight_time_from_begin_to_end ) ;
+    so_called_loadable_consts_binder :: bind ( "unselection_weight_time_to_begin" , so_called_common_logic_main_menu_letters_animation_consts :: unselection_weight_time_to_begin ) ;
+    so_called_loadable_consts_binder :: bind ( "unselection_weight_time_from_begin_to_end" , so_called_common_logic_main_menu_letters_animation_consts :: unselection_weight_time_from_begin_to_end ) ;
+}
--- src_refactored/loadable/consts/reflection/shy_reflection.hpp
+++ src_refactored/loadable/consts/reflection/shy_reflection.hpp
@@ -8,7 +8,7 @@ void shy_loadable_consts_reflection :: prepare ( )
     so_called_loadable_consts_reflection_logic_door_animation :: prepare ( ) ;
     so_called_loadable_consts_reflection_logic_fidget :: prepare ( ) ;
     so_called_loadable_consts_reflection_logic_main_menu_animation :: prepare ( ) ;
-//    so_called_loadable_consts_reflection_logic_main_menu_letters_animation :: prepare ( ) ;
+    so_called_loadable_consts_reflection_logic_main_menu_letters_animation :: prepare ( ) ;
 //    so_called_loadable_consts_reflection_logic_main_menu_letters_layout :: prepare ( ) ;
 //    so_called_loadable_consts_reflection_logic_main_menu_letters_meshes :: prepare ( ) ;
 //    so_called_loadable_consts_reflection_logic_main_menu_selection :: prepare ( ) ;
