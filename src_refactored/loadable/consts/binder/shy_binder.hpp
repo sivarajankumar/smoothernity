2c70eb92559eced2ea1b13a579b45b64c1739d96
olegus8 <olegus8@localhost>
2011-04-15 13:27:28 +0000

    working on loadable consts binder refactoring

--- src_refactored/loadable/consts/binder/shy_binder.hpp
+++ src_refactored/loadable/consts/binder/shy_binder.hpp
@@ -1,11 +1,31 @@
-void shy_loadable_consts_binder :: module ( so_called_std_string )
+namespace shy_guts
 {
+    static so_called_type_loadable_consts_content_module * current_module = 0 ;
 }
 
-void shy_loadable_consts_binder :: bind ( so_called_std_string , const so_called_type_platform_math_num_fract & )
+void shy_loadable_consts_binder :: module ( so_called_std_string name )
 {
+    so_called_type_loadable_consts_content_module_container * module_container = 0 ;
+    so_called_loadable_consts_content :: get_module_container ( module_container ) ;
+    ( * module_container ) [ name ] = so_called_type_loadable_consts_content_module ( ) ;
+    shy_guts :: current_module = & ( ( * module_container ) [ name ] ) ;
 }
 
-void shy_loadable_consts_binder :: bind ( so_called_std_string , const so_called_type_platform_math_num_whole & )
+void shy_loadable_consts_binder :: bind ( so_called_std_string name , const so_called_type_platform_math_num_fract & value )
 {
+    if ( shy_guts :: current_module )
+    {
+        shy_guts :: current_module -> name_to_fract [ name ] = so_called_type_loadable_consts_content_value_fract ( ) ;
+        shy_guts :: current_module -> name_to_fract [ name ] . binding = const_cast < so_called_type_platform_math_num_fract * > ( & value ) ;
+    }
 }
+
+void shy_loadable_consts_binder :: bind ( so_called_std_string name , const so_called_type_platform_math_num_whole & value )
+{
+    if ( shy_guts :: current_module )
+    {
+        shy_guts :: current_module -> name_to_whole [ name ] = so_called_type_loadable_consts_content_value_whole ( ) ;
+        shy_guts :: current_module -> name_to_whole [ name ] . binding = const_cast < so_called_type_platform_math_num_whole * > ( & value ) ;
+    }
+}
+
--- src_refactored/loadable/consts/binder/shy_binder_injections.cpp
+++ src_refactored/loadable/consts/binder/shy_binder_injections.cpp
@@ -1,7 +1,8 @@
 #ifdef shy_build_loadable_way
 
-    #include "./shy_binder_injections.h"
+    #include "../content/shy_content_injections.h"
 
+    #include "./shy_binder_injections.h"
     #include "./shy_binder.hpp"
 
 #endif
