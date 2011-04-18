fdc04df7d223625ab2583e7a5302749d3154bfbe
olegus8 <olegus8@localhost>
2011-04-18 13:43:10 +0000

    working on loadable parser refactoring

--- src_refactored/loadable/parser/shy_parser.hpp
+++ src_refactored/loadable/parser/shy_parser.hpp
@@ -455,6 +455,19 @@ void shy_guts :: handle_state_error ( )
 
 void shy_guts :: handle_state_reading_module_name ( )
 {
+    if ( shy_guts :: token_class == shy_guts :: token_class_identifier )
+    {
+        shy_guts :: store_module_name ( shy_guts :: token ) ;
+        shy_guts :: read_next_token ( ) ;
+        shy_guts :: state = shy_guts :: state_reading_attribute_name ;
+    }
+    else
+    {
+        so_called_std_string error ;
+        shy_guts :: errors :: expected_module_name_instead_of ( error , shy_guts :: token ) ;
+        shy_guts :: store_error ( error ) ;
+        shy_guts :: state = shy_guts :: state_error ;
+    }
 }
 
 void shy_guts :: handle_state_reading_attribute_name ( )
