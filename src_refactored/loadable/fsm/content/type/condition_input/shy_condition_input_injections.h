24eb0c253394e8a1a5816ea76262636cd068e8c1
olegus8 <olegus8@localhost>
2011-04-18 10:54:36 +0000

    working on loadable fsm content refactoring

--- src_refactored/loadable/fsm/content/type/condition_input/shy_condition_input_injections.h
+++ src_refactored/loadable/fsm/content/type/condition_input/shy_condition_input_injections.h
@@ -1,6 +1,8 @@
 #ifndef _shy_type_loadable_fsm_content_condition_input_injections_included
 #define _shy_type_loadable_fsm_content_condition_input_injections_included
 
+#include "../../../../../injections/std/shy_std.h"
+
 #include "./shy_condition_input.h"
 
 typedef shy_type_loadable_fsm_content_condition_input so_called_type_loadable_fsm_content_condition_input ;
