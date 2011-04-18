ac49166c6de0c953dc63a582399cc41a9093cb0b
olegus8 <olegus8@localhost>
2011-04-18 15:22:40 +0000

    working on loadable parser refactoring

--- src_refactored/loadable/parser/shy_parser.hpp
+++ src_refactored/loadable/parser/shy_parser.hpp
@@ -1042,6 +1042,19 @@ void shy_guts :: handle_state_reading_state_condition_state_name ( )
 
 void shy_guts :: handle_state_reading_state_condition_parenthesis_close ( )
 {
+    if ( shy_guts :: token_class == shy_guts :: token_class_parenthesis_close )
+    {
+        shy_guts :: store_state_condition ( ) ;
+        shy_guts :: read_next_token ( ) ;
+        shy_guts :: state = shy_guts :: state_reading_next_condition_in_group ;
+    }
+    else
+    {
+        so_called_std_string error ;
+        shy_guts :: errors :: expected_parenthesis_close_instead_of ( error , shy_guts :: token ) ;
+        shy_guts :: store_error ( error ) ;
+        shy_guts :: state = shy_guts :: state_error ;
+    }
 }
 
 void shy_guts :: handle_state_reading_command_condition_command_name ( )
