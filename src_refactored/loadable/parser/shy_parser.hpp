0189e1d32ff7d30530d20418d361dbe8f08fb62c
olegus8 <olegus8@localhost>
2011-04-19 15:41:09 +0000

    working on loadable parser refactoring

--- src_refactored/loadable/parser/shy_parser.hpp
+++ src_refactored/loadable/parser/shy_parser.hpp
@@ -437,6 +437,8 @@ void shy_guts :: handle_token_class_number ( )
 
 void shy_guts :: handle_token_class_terminator ( )
 {
+    shy_guts :: move_first_char_to_token ( ) ;
+    shy_guts :: continue_reading_next_token = so_called_std_false ;
 }
 
 void shy_guts :: handle_token_class_divide ( )
