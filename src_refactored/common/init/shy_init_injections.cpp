5076381745916bb5dbb42739188d99c11f071327
olegus8 <olegus8@localhost>
2011-04-23 15:52:19 +0000

    working on register in scheduler

--- src_refactored/common/init/shy_init_injections.cpp
+++ src_refactored/common/init/shy_init_injections.cpp
@@ -36,6 +36,7 @@
 #include "../logic/main_menu/letters/animation/selection_push/shy_selection_push_injections.h"
 #include "../logic/main_menu/letters/animation/selection_weight/shy_selection_weight_injections.h"
 #include "../logic/main_menu/letters/animation/shy_animation_injections.h"
+#include "../logic/main_menu/letters/animation/unselection_weight/shy_unselection_weight_injections.h"
 
 #include "./shy_init_injections.h"
 #include "./shy_init.hpp"
