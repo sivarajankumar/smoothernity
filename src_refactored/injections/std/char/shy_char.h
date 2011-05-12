ab646f1d005f65e5b32126deeedd6d39739c6876
olegus8 <olegus8@localhost>
2011-05-12 15:58:11 +0000

    working on split std injections

---
+++ src_refactored/injections/std/char/shy_char.h
@@ -0,0 +1,8 @@
+#ifndef _shy_injections_std_char_included
+#define _shy_injections_std_char_included
+
+#ifdef shy_build_loadable_way
+    typedef char so_called_std_char ;
+#endif
+
+#endif
--- src_refactored/injections/std/shy_std.h
+++ src_refactored/injections/std/shy_std.h
@@ -8,7 +8,6 @@
     #include <stdint.h>
     #include <string>
 
-    typedef char so_called_std_char ;
     typedef int32_t so_called_std_int32_t ;
     typedef std :: istringstream so_called_std_istringstream ;
     typedef std :: locale so_called_std_locale ;
