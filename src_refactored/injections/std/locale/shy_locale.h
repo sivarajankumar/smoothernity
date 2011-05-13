16e1e54eccbb252a596a748ba91a6f3af165f703
olegus8 <olegus8@localhost>
2011-05-13 10:03:55 +0000

    working on split std injections

---
+++ src_refactored/injections/std/locale/shy_locale.h
@@ -0,0 +1,9 @@
+#ifndef _shy_injections_std_locale_included
+#define _shy_injections_std_locale_included
+
+#ifdef shy_build_loadable_way
+    #include <locale>
+    typedef std :: locale so_called_std_locale ;
+#endif
+
+#endif
--- src_refactored/injections/std/shy_std.h
+++ src_refactored/injections/std/shy_std.h
@@ -2,13 +2,8 @@
 #define _shy_injections_std_included
 
 #ifdef shy_build_loadable_way
-
-    #include <locale>
     #include <string>
-
-    typedef std :: locale so_called_std_locale ;
     typedef std :: string so_called_std_string ;
-
 #endif
 
 #endif
