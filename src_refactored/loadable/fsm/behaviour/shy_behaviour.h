1257d3cc6763864fadaddfbf07586a87c24d027d
olegus8 <olegus8@localhost>
2011-05-16 10:44:19 +0000

    working on loadable fsm behaviour

--- src_refactored/loadable/fsm/behaviour/shy_behaviour.h
+++ src_refactored/loadable/fsm/behaviour/shy_behaviour.h
@@ -13,9 +13,41 @@ public :
     void tick_all_fsms ( ) ;
     void update_fixed_behaviour_inputs ( ) ;
 private :
+    class type_fsm_state
+    : public so_called_type_common_engine_fsm_state
+    {
+    public :
+        virtual void on_entry ( ) ;
+        virtual void on_exit ( ) ;
+        virtual void on_input ( ) ;
+        virtual so_called_type_common_engine_fsm_state & transition ( ) ;
+    } ;
+
     so_called_type_platform_math_num_whole _fsm_running ;
     so_called_type_platform_pointer_data < type_fsm_inputs > _inputs ;
+    so_called_std_vector < type_fsm_state > _states ;
 } ;
+
+template < typename type_fsm_inputs >
+void shy_loadable_fsm_behaviour < type_fsm_inputs > :: type_fsm_state :: on_entry ( )
+{
+}
+
+template < typename type_fsm_inputs >
+void shy_loadable_fsm_behaviour < type_fsm_inputs > :: type_fsm_state :: on_exit ( )
+{
+}
+
+template < typename type_fsm_inputs >
+void shy_loadable_fsm_behaviour < type_fsm_inputs > :: type_fsm_state :: on_input ( )
+{
+}
+
+template < typename type_fsm_inputs >
+so_called_type_common_engine_fsm_state & shy_loadable_fsm_behaviour < type_fsm_inputs > :: type_fsm_state :: transition ( )
+{
+    return so_called_type_common_engine_fsm_state :: transition ( ) ;
+}
 
 template < typename type_fsm_inputs >
 void shy_loadable_fsm_behaviour < type_fsm_inputs > :: determine_behaviour_inputs_change ( so_called_type_platform_math_num_whole & )
--- src_refactored/loadable/fsm/behaviour/shy_behaviour_injections.h
+++ src_refactored/loadable/fsm/behaviour/shy_behaviour_injections.h
@@ -1,10 +1,12 @@
 #ifndef _shy_loadable_fsm_behaviour_injections_included
 #define _shy_loadable_fsm_behaviour_injections_included
 
+#include "../../../common/engine/fsm/type/state/shy_state_injections.h"
 #include "../../../injections/platform/math/shy_math.h"
 #include "../../../injections/platform/pointer/type/data/shy_data.h"
 #include "../../../injections/std/false/shy_false.h"
 #include "../../../injections/std/true/shy_true.h"
+#include "../../../injections/std/vector/shy_vector.h"
 
 #include "./shy_behaviour.h"
 
