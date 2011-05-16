b5d40082e1e060a344e2de87536c2f49a8cce990
olegus8 <olegus8@localhost>
2011-05-16 08:27:39 +0000

    working on loadable fsm behaviour

---
+++ src_refactored/injections/std/string/shy_string.h
@@ -0,0 +1,9 @@
+#ifndef _shy_injections_std_string_included
+#define _shy_injections_std_string_included
+
+#ifdef shy_build_loadable_way
+    #include <string>
+    typedef std :: string so_called_std_string ;
+#endif
+
+#endif
