2874c08918f970b6f4266bddbfce47b14a9d4602
olegus8 <olegus8@localhost>
2011-05-17 11:13:43 +0000

    working on loadable fsm behaviour

--- src_refactored/loadable/fsm/behaviour/shy_behaviour.h
+++ src_refactored/loadable/fsm/behaviour/shy_behaviour.h
@@ -19,6 +19,7 @@ private :
     } ;
 
 public :
+    shy_loadable_fsm_behaviour ( ) ;
     void determine_behaviour_inputs_change ( so_called_type_platform_math_num_whole & ) ;
     void init ( ) ;
     void is_fsm_running ( so_called_type_platform_math_num_whole & ) ;
@@ -26,16 +27,18 @@ public :
     void reset_behaviour_input_events ( ) ;
     void run_fsm_begin ( ) ;
     void run_fsm_end ( ) ;
-    void set_system_binding ( so_called_type_loadable_fsm_content_system_binding ) ;
     void set_inputs ( so_called_type_platform_pointer_data < type_fsm_inputs > ) ;
+    void set_system_binding ( so_called_type_loadable_fsm_content_system_binding ) ;
     void tick_all_fsms ( ) ;
     void update_fixed_behaviour_inputs ( ) ;
 private :
     void _init_machines ( ) ;
+    void _init_system ( ) ;
 private :
     so_called_type_platform_math_num_whole _fsm_running ;
     so_called_type_platform_pointer_data < type_fsm_inputs > _inputs ;
     so_called_std_map < so_called_std_string , type_fsm_machine > _machines ;
+    so_called_type_loadable_fsm_content_system * _system ;
     so_called_type_loadable_fsm_content_system_binding _system_binding ;
 } ;
 
@@ -66,9 +69,17 @@ void shy_loadable_fsm_behaviour < type_fsm_inputs > :: determine_behaviour_input
 }
 
 template < typename type_fsm_inputs >
+shy_loadable_fsm_behaviour < type_fsm_inputs > :: shy_loadable_fsm_behaviour ( )
+: _system ( 0 )
+, _system_binding ( 0 )
+{
+}
+
+template < typename type_fsm_inputs >
 void shy_loadable_fsm_behaviour < type_fsm_inputs > :: init ( )
 {
     so_called_platform_math :: make_num_whole ( _fsm_running , so_called_std_false ) ;
+    _init_system ( ) ;
     _init_machines ( ) ;
 }
 
@@ -120,6 +131,20 @@ void shy_loadable_fsm_behaviour < type_fsm_inputs > :: tick_all_fsms ( )
 template < typename type_fsm_inputs >
 void shy_loadable_fsm_behaviour < type_fsm_inputs > :: update_fixed_behaviour_inputs ( )
 {
+}
+
+template < typename type_fsm_inputs >
+void shy_loadable_fsm_behaviour < type_fsm_inputs > :: _init_system ( )
+{
+    so_called_type_loadable_fsm_content_system_binding_container * system_binding_container = 0 ;
+    so_called_type_loadable_fsm_content_system_container * system_container = 0 ;
+    so_called_std_string system_name ;
+
+    so_called_loadable_fsm_content :: get_system_binding_container ( system_binding_container ) ;
+    so_called_loadable_fsm_content :: get_system_container ( system_container ) ;
+
+    system_name = ( * system_binding_container ) [ _system_binding ] ;
+    _system = & ( ( * system_container ) [ system_name ] ) ;
 }
 
 template < typename type_fsm_inputs >
