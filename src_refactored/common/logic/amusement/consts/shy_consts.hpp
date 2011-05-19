7356b4c2f3eba0958ea95eb479761e90e491284c
olegus8 <olegus8@localhost>
2011-05-19 16:36:24 +0000

    removing autogenerated dependencies from development configuration

---
+++ src_refactored/common/logic/amusement/consts/shy_consts.hpp
@@ -0,0 +1,4 @@
+const so_called_type_platform_math_num_fract shy_common_logic_amusement_consts :: renderer_clear_color_b ;
+const so_called_type_platform_math_num_fract shy_common_logic_amusement_consts :: renderer_clear_color_g ;
+const so_called_type_platform_math_num_fract shy_common_logic_amusement_consts :: renderer_clear_color_r ;
+
