8445be7ea63627dc702922278a4fc77b8c0e1822
olegus8 <olegus8@localhost>
2011-05-19 17:06:09 +0000

    removing autogenerated dependencies from development configuration

---
+++ src_refactored/common/logic/main_menu/selection/consts/shy_consts.hpp
@@ -0,0 +1,7 @@
+const so_called_type_platform_math_num_fract shy_common_logic_main_menu_selection_consts :: mesh_color_a ;
+const so_called_type_platform_math_num_fract shy_common_logic_main_menu_selection_consts :: mesh_color_b ;
+const so_called_type_platform_math_num_fract shy_common_logic_main_menu_selection_consts :: mesh_color_g ;
+const so_called_type_platform_math_num_fract shy_common_logic_main_menu_selection_consts :: mesh_color_r ;
+const so_called_type_platform_math_num_fract shy_common_logic_main_menu_selection_consts :: mesh_size ;
+const so_called_type_platform_math_num_fract shy_common_logic_main_menu_selection_consts :: selected_rect_vertical_scale ;
+
