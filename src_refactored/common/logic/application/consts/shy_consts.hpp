dbe4f4574e9cabb21c490526112d9ab79ce6b450
olegus8 <olegus8@localhost>
2011-05-19 16:39:58 +0000

    removing autogenerated dependencies from development configuration

---
+++ src_refactored/common/logic/application/consts/shy_consts.hpp
@@ -0,0 +1,4 @@
+const so_called_type_platform_math_num_whole shy_common_logic_application_consts :: skip_amusement ;
+const so_called_type_platform_math_num_whole shy_common_logic_application_consts :: skip_main_menu ;
+const so_called_type_platform_math_num_whole shy_common_logic_application_consts :: skip_title ;
+
