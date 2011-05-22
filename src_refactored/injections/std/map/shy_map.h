95568d776b375c2ea8c4564a14b477ce2c99269e
olegus8 <olegus8@localhost>
2011-05-22 16:28:50 +0000

    making std injections system specific

--- src_refactored/injections/std/map/shy_map.h
+++ src_refactored/injections/std/map/shy_map.h
@@ -1,9 +1,8 @@
 #ifndef _shy_injections_std_map_included
 #define _shy_injections_std_map_included
 
-#ifdef shy_build_loadable_way
-    #include <map>
-    #define so_called_std_map std :: map
+#ifdef shy_build_for_macosx
+    #include "../../../macosx/std/map/shy_map_injections.h"
 #endif
 
 #endif
