bccc7ccb885049e4fc8384dab460da54fdb6f270
olegus8 <olegus8@localhost>
2011-05-22 16:53:40 +0000

    making std injections system specific

--- src_refactored/injections/std/string/shy_string.h
+++ src_refactored/injections/std/string/shy_string.h
@@ -1,9 +1,8 @@
 #ifndef _shy_injections_std_string_included
 #define _shy_injections_std_string_included
 
-#ifdef shy_build_loadable_way
-    #include <string>
-    typedef std :: string so_called_std_string ;
+#ifdef shy_build_for_macosx
+    #include "../../../macosx/std/string/shy_string_injections.h"
 #endif
 
 #endif
---
+++ src_refactored/macosx/std/string/shy_string_injections.h
@@ -0,0 +1,9 @@
+#ifndef _shy_macosx_std_string_injections_included
+#define _shy_macosx_std_string_injections_included 
+
+#ifdef shy_build_development
+    #include "../../../std/string/shy_string_injections.h"
+    typedef shy_std_string so_called_std_string ;
+#endif
+
+#endif
