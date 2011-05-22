393bf5f6b0c84910156b4caacd4d5f0cdd85817a
olegus8 <olegus8@localhost>
2011-05-22 15:39:31 +0000

    making std injections system specific

--- src_refactored/injections/std/false/shy_false.h
+++ src_refactored/injections/std/false/shy_false.h
@@ -1,8 +1,8 @@
 #ifndef _shy_injections_std_false_included
 #define _shy_injections_std_false_included
 
-#ifdef shy_build_loadable_way
-    #define so_called_std_false false
+#ifdef shy_build_for_macosx
+    #include "../../../macosx/std/false/shy_false_injections.h"
 #endif
 
 #endif
---
+++ src_refactored/std/false/shy_false_injections.h
@@ -0,0 +1,7 @@
+#ifndef _shy_std_false_injections_included
+#define _shy_std_false_injections_included
+
+#define shy_std_false false
+
+#endif
+
