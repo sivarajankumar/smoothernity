18e53d3af00c448ff5ee02a08eb0acc7dba75503
olegus8 <olegus8@localhost>
2011-05-22 15:12:43 +0000

    making std injections system specific

--- src_refactored/injections/std/char/shy_char.h
+++ src_refactored/injections/std/char/shy_char.h
@@ -1,8 +1,8 @@
 #ifndef _shy_injections_std_char_included
 #define _shy_injections_std_char_included
 
-#ifdef shy_build_loadable_way
-    typedef char so_called_std_char ;
+#ifdef shy_build_for_macosx
+    #include "../../../macosx/std/char/shy_char_injections.h"
 #endif
 
 #endif
---
+++ src_refactored/std/char/shy_char_injections.h
@@ -0,0 +1,7 @@
+#ifndef _shy_std_char_injections_included
+#define _shy_std_char_injections_included
+
+typedef char shy_std_char ;
+
+#endif
+
