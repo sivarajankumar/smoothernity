5b89c235cc7602ed783f9a1693cfbd2004fd3a2e
olegus8 <olegus8@localhost>
2011-05-22 16:04:23 +0000

    making std injections system specific

--- src_refactored/injections/std/int32_t/shy_int32_t.h
+++ src_refactored/injections/std/int32_t/shy_int32_t.h
@@ -1,9 +1,8 @@
 #ifndef _shy_injections_std_int32_t_included
 #define _shy_injections_std_int32_t_included
 
-#ifdef shy_build_loadable_way
-    #include <stdint.h>
-    typedef int32_t so_called_std_int32_t ;
+#ifdef shy_build_for_macosx
+    #include "../../../macosx/std/int32_t/shy_int32_t_injections.h"
 #endif
 
 #endif
---
+++ src_refactored/std/int32_t/shy_int32_t_injections.h
@@ -0,0 +1,7 @@
+#ifndef _shy_std_int32_t_injections_included
+#define _shy_std_int32_t_injections_included
+
+#include <stdint.h>
+typedef int32_t shy_std_int32_t ;
+
+#endif
