fae79e836efd19e678212b999b8fd7752cc3f659
olegus8 <olegus8@localhost>
2011-05-22 16:15:23 +0000

    making std injections system specific

--- src_refactored/injections/std/isspace/shy_isspace.h
+++ src_refactored/injections/std/isspace/shy_isspace.h
@@ -1,9 +1,8 @@
 #ifndef _shy_injections_std_isspace_included
 #define _shy_injections_std_isspace_included
 
-#ifdef shy_build_loadable_way
-    #include <locale>
-    #define so_called_std_isspace std :: isspace
+#ifdef shy_build_for_macosx
+    #include "../../../macosx/std/isspace/shy_isspace_injections.h"
 #endif
 
 #endif
