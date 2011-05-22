4934cdf256837119c3757e1fdcd63d1088eb7d98
olegus8 <olegus8@localhost>
2011-05-22 15:35:42 +0000

    making std injections system specific

--- src_refactored/injections/std/endl/shy_endl.h
+++ src_refactored/injections/std/endl/shy_endl.h
@@ -1,9 +1,8 @@
 #ifndef _shy_injections_std_endl_included
 #define _shy_injections_std_endl_included
 
-#ifdef shy_build_loadable_way
-    #include <ostream>
-    #define so_called_std_endl std :: endl
+#ifdef shy_build_for_macosx
+    #include "../../../macosx/std/endl/shy_endl_injections.h"
 #endif
 
 #endif
---
+++ src_refactored/macosx/std/endl/shy_endl_injections.h
@@ -0,0 +1,9 @@
+#ifndef _shy_macosx_std_endl_injections_included
+#define _shy_macosx_std_endl_injections_included 
+
+#ifdef shy_build_development
+    #include "../../../std/endl/shy_endl_injections.h"
+    #define so_called_std_endl shy_std_endl
+#endif
+
+#endif
