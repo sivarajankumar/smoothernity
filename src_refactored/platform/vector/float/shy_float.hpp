b04dda6c87222981a86b13c16ab0536dc8937f72
olegus8 <olegus8@localhost>
2011-05-25 12:13:23 +0000

    injecting lib/std dependencies into platform

--- src_refactored/platform/vector/float/shy_float.hpp
+++ src_refactored/platform/vector/float/shy_float.hpp
@@ -73,7 +73,7 @@ void shy_platform_vector_float :: mul
     , so_called_type_platform_math_num_fract f 
     )
 {
-    float f_float = 0 ;
+    so_called_lib_std_float f_float = 0 ;
     so_called_platform_math_insider :: num_fract_value_get ( f_float , f ) ;
     result . _x = f_float * v . _x ;
     result . _y = f_float * v . _y ;
@@ -82,7 +82,7 @@ void shy_platform_vector_float :: mul
 
 void shy_platform_vector_float :: mul_by ( so_called_type_platform_vector_data & v , so_called_type_platform_math_num_fract f )
 {
-    float f_float = 0 ;
+    so_called_lib_std_float f_float = 0 ;
     so_called_platform_math_insider :: num_fract_value_get ( f_float , f ) ;
     v . _x *= f_float ;
     v . _y *= f_float ;
@@ -91,12 +91,12 @@ void shy_platform_vector_float :: mul_by ( so_called_type_platform_vector_data &
 
 void shy_platform_vector_float :: length ( so_called_type_platform_math_num_fract & result , so_called_type_platform_vector_data v )
 {
-    so_called_platform_math_insider :: num_fract_value_set ( result , float ( sqrt ( v . _x * v . _x + v . _y * v . _y + v . _z * v . _z ) ) ) ;
+    so_called_platform_math_insider :: num_fract_value_set ( result , so_called_lib_std_float ( sqrt ( v . _x * v . _x + v . _y * v . _y + v . _z * v . _z ) ) ) ;
 }
 
 void shy_platform_vector_float :: normalize ( so_called_type_platform_vector_data & result , so_called_type_platform_vector_data v )
 {
-    float inv_length = 1.0f / float ( sqrt ( v . _x * v . _x + v . _y * v . _y + v . _z * v . _z ) ) ;
+    so_called_lib_std_float inv_length = so_called_lib_std_float ( 1 ) / so_called_lib_std_float ( sqrt ( v . _x * v . _x + v . _y * v . _y + v . _z * v . _z ) ) ;
     result . _x = inv_length * v . _x ;
     result . _y = inv_length * v . _y ;
     result . _z = inv_length * v . _z ;
--- src_refactored/platform/vector/float/shy_float_injections.hpp
+++ src_refactored/platform/vector/float/shy_float_injections.hpp
@@ -1,5 +1,6 @@
 #include "./shy_float_injections.h"
 
+#include "../../../injections/lib/std/float/shy_float.h"
 #include "../../../injections/platform/math/insider/shy_insider.h"
 
 #include "./shy_float.hpp"
