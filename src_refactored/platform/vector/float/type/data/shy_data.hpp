3d4c1869277de9babfed9c4f578c97bb7439a652
olegus8 <olegus8@localhost>
2011-05-25 12:10:54 +0000

    injecting lib/std dependencies into platform

--- src_refactored/platform/vector/float/type/data/shy_data.hpp
+++ src_refactored/platform/vector/float/type/data/shy_data.hpp
@@ -1,7 +1,7 @@
 shy_type_platform_vector_float_data :: shy_type_platform_vector_float_data ( )
-: _x ( ( float ) so_called_platform_consts_insider :: uninitialized_value )
-, _y ( ( float ) so_called_platform_consts_insider :: uninitialized_value )
-, _z ( ( float ) so_called_platform_consts_insider :: uninitialized_value )
+: _x ( ( so_called_lib_std_float ) so_called_platform_consts_insider :: uninitialized_value )
+, _y ( ( so_called_lib_std_float ) so_called_platform_consts_insider :: uninitialized_value )
+, _z ( ( so_called_lib_std_float ) so_called_platform_consts_insider :: uninitialized_value )
 {
 }
 
