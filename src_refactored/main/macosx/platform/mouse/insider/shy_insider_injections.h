0a599fb6d4086510b8fcc777925c87cbb5756392
olegus8 <olegus8@localhost>
2011-05-25 11:16:07 +0000

    injecting lib/std dependencies into platform

--- src_refactored/main/macosx/platform/mouse/insider/shy_insider_injections.h
+++ src_refactored/main/macosx/platform/mouse/insider/shy_insider_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_macosx_platform_mouse_insider_injections_included
 #define _shy_macosx_platform_mouse_insider_injections_included
 
-#include "../../../../../platform/mouse/insider/shy_insider.h"
+#include "../../../../../platform/mouse/insider/shy_insider_injections.h"
 typedef shy_platform_mouse_insider so_called_platform_mouse_insider ;
 
 #endif
