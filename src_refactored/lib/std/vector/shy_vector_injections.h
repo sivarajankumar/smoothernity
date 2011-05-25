bb46879767c7b8f48eff550537b1d2c873292db9
olegus8 <olegus8@localhost>
2011-05-25 14:08:46 +0000

    placing aliases in platform modules

--- src_refactored/lib/std/vector/shy_vector_injections.h
+++ src_refactored/lib/std/vector/shy_vector_injections.h
@@ -2,6 +2,6 @@
 #define _shy_lib_std_vector_injections_included
 
 #include <vector>
-#define shy_lib_std_vector std :: vector
+#define so_called_lib_std_vector std :: vector
 
 #endif
--- src_refactored/main/macosx/lib/std/vector/shy_vector_injections.h
+++ src_refactored/main/macosx/lib/std/vector/shy_vector_injections.h
@@ -3,7 +3,6 @@
 
 #ifdef shy_build_development
     #include "../../../../../lib/std/vector/shy_vector_injections.h"
-    #define so_called_lib_std_vector shy_lib_std_vector
 #endif
 
 #endif
