60f93c466a011c2c518279b646910d5ba9f11c83
olegus8 <olegus8@localhost>
2011-05-25 14:00:26 +0000

    placing aliases in platform modules

--- src_refactored/lib/std/isspace/shy_isspace_injections.h
+++ src_refactored/lib/std/isspace/shy_isspace_injections.h
@@ -2,6 +2,6 @@
 #define _shy_lib_std_isspace_injections_included
 
 #include <locale>
-#define shy_lib_std_isspace std :: isspace
+#define so_called_lib_std_isspace std :: isspace
 
 #endif
--- src_refactored/main/macosx/lib/std/isspace/shy_isspace_injections.h
+++ src_refactored/main/macosx/lib/std/isspace/shy_isspace_injections.h
@@ -3,7 +3,6 @@
 
 #ifdef shy_build_development
     #include "../../../../../lib/std/isspace/shy_isspace_injections.h"
-    #define so_called_lib_std_isspace shy_lib_std_isspace
 #endif
 
 #endif
