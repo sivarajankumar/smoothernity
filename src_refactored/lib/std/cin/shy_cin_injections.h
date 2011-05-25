0418aee55715936b00e863c101f707bf34961251
olegus8 <olegus8@localhost>
2011-05-25 13:25:41 +0000

    placing aliases in platform modules

--- src_refactored/lib/std/cin/shy_cin_injections.h
+++ src_refactored/lib/std/cin/shy_cin_injections.h
@@ -2,6 +2,6 @@
 #define _shy_lib_std_cin_injections_included
 
 #include <iostream>
-#define shy_lib_std_cin std :: cin
+#define so_called_lib_std_cin std :: cin
 
 #endif
--- src_refactored/main/macosx/lib/std/cin/shy_cin_injections.h
+++ src_refactored/main/macosx/lib/std/cin/shy_cin_injections.h
@@ -3,7 +3,6 @@
 
 #ifdef shy_build_development
     #include "../../../../../lib/std/cin/shy_cin_injections.h"
-    #define so_called_lib_std_cin shy_lib_std_cin
 #endif
 
 #endif
