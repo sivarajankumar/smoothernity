c2ba8551d16636330620beac43e4a06f18c35fc4
olegus8 <olegus8@localhost>
2011-05-25 11:55:08 +0000

    injecting lib/std dependencies into platform

--- src_refactored/platform/static_array/shy_static_array.h
+++ src_refactored/platform/static_array/shy_static_array.h
@@ -8,7 +8,7 @@ public :
 template < typename array_type , typename pointer_type >
 void shy_platform_static_array :: element_ptr ( pointer_type & element_ptr , array_type & array , so_called_type_platform_math_num_whole index )
 {
-    int index_int = 0 ;
+    so_called_lib_std_int32_t index_int = 0 ;
     so_called_platform_math_insider :: num_whole_value_get ( index_int , index ) ;
     so_called_platform_pointer :: bind ( element_ptr , array . _elements [ index_int ] ) ;
 }
