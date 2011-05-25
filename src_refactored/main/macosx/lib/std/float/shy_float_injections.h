c957a56010731cd0f81376953d373535c407af79
olegus8 <olegus8@localhost>
2011-05-25 13:49:00 +0000

    placing aliases in platform modules

--- src_refactored/main/macosx/lib/std/float/shy_float_injections.h
+++ src_refactored/main/macosx/lib/std/float/shy_float_injections.h
@@ -2,6 +2,5 @@
 #define _shy_macosx_lib_std_float_injections_included 
 
 #include "../../../../../lib/std/float/shy_float_injections.h"
-typedef shy_lib_std_float so_called_lib_std_float ;
 
 #endif
