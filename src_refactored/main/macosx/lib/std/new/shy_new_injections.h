3dfdc29284565eade85965bd4312c74af298b74e
olegus8 <olegus8@localhost>
2011-05-25 11:51:26 +0000

    injecting lib/std dependencies into platform

--- src_refactored/main/macosx/lib/std/new/shy_new_injections.h
+++ src_refactored/main/macosx/lib/std/new/shy_new_injections.h
@@ -1,8 +1,10 @@
 #ifndef _shy_macosx_lib_std_new_injections_included
 #define _shy_macosx_lib_std_new_injections_included 
 
-#include "../../../../../lib/std/new/shy_new_injections.h"
-#define so_called_lib_std_new shy_lib_std_new
+#ifdef shy_build_development
+    #include "../../../../../lib/std/new/shy_new_injections.h"
+    #define so_called_lib_std_new shy_lib_std_new
+#endif
 
 #endif
 
