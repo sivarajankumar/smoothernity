22f1ef569a8d1e12f6a02fb5fff10a2a83a17d05
olegus8 <olegus8@localhost>
2011-05-25 13:46:46 +0000

    placing aliases in platform modules

--- src_refactored/lib/std/false/shy_false_injections.h
+++ src_refactored/lib/std/false/shy_false_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_lib_std_false_injections_included
 #define _shy_lib_std_false_injections_included
 
-#define shy_lib_std_false false
+#define so_called_lib_std_false false
 
 #endif
 
--- src_refactored/main/macosx/lib/std/false/shy_false_injections.h
+++ src_refactored/main/macosx/lib/std/false/shy_false_injections.h
@@ -2,6 +2,5 @@
 #define _shy_macosx_lib_std_false_injections_included 
 
 #include "../../../../../lib/std/false/shy_false_injections.h"
-#define so_called_lib_std_false shy_lib_std_false
 
 #endif
