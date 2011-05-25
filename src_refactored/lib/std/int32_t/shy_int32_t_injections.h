4cc5ea162c23e4a71f758b0f8b0cb7872ab657f5
olegus8 <olegus8@localhost>
2011-05-25 13:52:27 +0000

    placing aliases in platform modules

--- src_refactored/lib/std/int32_t/shy_int32_t_injections.h
+++ src_refactored/lib/std/int32_t/shy_int32_t_injections.h
@@ -2,6 +2,6 @@
 #define _shy_lib_std_int32_t_injections_included
 
 #include <stdint.h>
-typedef int32_t shy_lib_std_int32_t ;
+typedef int32_t so_called_lib_std_int32_t ;
 
 #endif
--- src_refactored/main/macosx/lib/std/int32_t/shy_int32_t_injections.h
+++ src_refactored/main/macosx/lib/std/int32_t/shy_int32_t_injections.h
@@ -2,6 +2,5 @@
 #define _shy_macosx_lib_std_int32_t_injections_included 
 
 #include "../../../../../lib/std/int32_t/shy_int32_t_injections.h"
-typedef shy_lib_std_int32_t so_called_lib_std_int32_t ;
 
 #endif
