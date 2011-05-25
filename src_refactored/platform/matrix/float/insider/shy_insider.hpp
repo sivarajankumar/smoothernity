5cca1931564447cbda5e854a52f292740dd7883d
olegus8 <olegus8@localhost>
2011-05-25 10:38:33 +0000

    injecting lib/std dependencies into platform

--- src_refactored/platform/matrix/float/insider/shy_insider.hpp
+++ src_refactored/platform/matrix/float/insider/shy_insider.hpp
@@ -1,9 +1,9 @@
-void shy_platform_matrix_float_insider :: elements_ptr ( float * & result , so_called_type_platform_matrix_data & matrix )
+void shy_platform_matrix_float_insider :: elements_ptr ( so_called_lib_std_float * & result , so_called_type_platform_matrix_data & matrix )
 {
     result = matrix . _elements ;
 }
 
-void shy_platform_matrix_float_insider :: elements_ptr ( const float * & result , const so_called_type_platform_matrix_data & matrix )
+void shy_platform_matrix_float_insider :: elements_ptr ( const so_called_lib_std_float * & result , const so_called_type_platform_matrix_data & matrix )
 {
     result = matrix . _elements ;
 }
