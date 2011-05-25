1d77f23452f044ac66225cd5cd52057b9f2d4e95
olegus8 <olegus8@localhost>
2011-05-25 12:08:30 +0000

    injecting lib/std dependencies into platform

--- src_refactored/platform/vector/float/insider/shy_insider.hpp
+++ src_refactored/platform/vector/float/insider/shy_insider.hpp
@@ -1,29 +1,29 @@
-void shy_platform_vector_float_insider :: x_get ( float & x , so_called_type_platform_vector_data v )
+void shy_platform_vector_float_insider :: x_get ( so_called_lib_std_float & x , so_called_type_platform_vector_data v )
 {
     x = v . _x ;
 }
 
-void shy_platform_vector_float_insider :: y_get ( float & y , so_called_type_platform_vector_data v )
+void shy_platform_vector_float_insider :: y_get ( so_called_lib_std_float & y , so_called_type_platform_vector_data v )
 {
     y = v . _y ;
 }
 
-void shy_platform_vector_float_insider :: z_get ( float & z , so_called_type_platform_vector_data v )
+void shy_platform_vector_float_insider :: z_get ( so_called_lib_std_float & z , so_called_type_platform_vector_data v )
 {
     z = v . _z ;
 }
 
-void shy_platform_vector_float_insider :: x_set ( so_called_type_platform_vector_data & v , float x )
+void shy_platform_vector_float_insider :: x_set ( so_called_type_platform_vector_data & v , so_called_lib_std_float x )
 {
     v . _x = x ;
 }
 
-void shy_platform_vector_float_insider :: y_set ( so_called_type_platform_vector_data & v , float y )
+void shy_platform_vector_float_insider :: y_set ( so_called_type_platform_vector_data & v , so_called_lib_std_float y )
 {
     v . _y = y ;
 }
 
-void shy_platform_vector_float_insider :: z_set ( so_called_type_platform_vector_data & v , float z )
+void shy_platform_vector_float_insider :: z_set ( so_called_type_platform_vector_data & v , so_called_lib_std_float z )
 {
     v . _z = z ;
 }
