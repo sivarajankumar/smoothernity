4dbb18d3410b625a534e09f8776359ca182a6b1d
olegus8 <olegus8@localhost>
2011-05-25 10:08:49 +0000

    injecting lib/std dependencies into platform

--- src_refactored/platform/math/int_float/insider/shy_insider.h
+++ src_refactored/platform/math/int_float/insider/shy_insider.h
@@ -1,10 +1,10 @@
 class shy_platform_math_int_float_insider
 {
 public :
-    static void num_whole_value_get ( int & , so_called_type_platform_math_num_whole ) ;
-    static void num_whole_value_set ( so_called_type_platform_math_num_whole & , int ) ;
-    static void num_fract_value_get ( float & , so_called_type_platform_math_num_fract ) ;
-    static void num_fract_value_set ( so_called_type_platform_math_num_fract & , float ) ;
-    static so_called_type_platform_math_num_whole init_num_whole ( int ) ;
-    static so_called_type_platform_math_num_fract init_num_fract ( float ) ;
+    static void num_whole_value_get ( so_called_lib_std_int32_t & , so_called_type_platform_math_num_whole ) ;
+    static void num_whole_value_set ( so_called_type_platform_math_num_whole & , so_called_lib_std_int32_t ) ;
+    static void num_fract_value_get ( so_called_lib_std_float & , so_called_type_platform_math_num_fract ) ;
+    static void num_fract_value_set ( so_called_type_platform_math_num_fract & , so_called_lib_std_float ) ;
+    static so_called_type_platform_math_num_whole init_num_whole ( so_called_lib_std_int32_t ) ;
+    static so_called_type_platform_math_num_fract init_num_fract ( so_called_lib_std_float ) ;
 } ;
--- src_refactored/platform/math/int_float/insider/shy_insider.hpp
+++ src_refactored/platform/math/int_float/insider/shy_insider.hpp
@@ -1,31 +1,31 @@
-void shy_platform_math_int_float_insider :: num_whole_value_get ( int & value , so_called_type_platform_math_num_whole num )
+void shy_platform_math_int_float_insider :: num_whole_value_get ( so_called_lib_std_int32_t & value , so_called_type_platform_math_num_whole num )
 {
     value = num . _value ;
 }
 
-void shy_platform_math_int_float_insider :: num_whole_value_set ( so_called_type_platform_math_num_whole & num , int value )
+void shy_platform_math_int_float_insider :: num_whole_value_set ( so_called_type_platform_math_num_whole & num , so_called_lib_std_int32_t value )
 {
     num . _value = value ;
 }
 
-void shy_platform_math_int_float_insider :: num_fract_value_get ( float & value , so_called_type_platform_math_num_fract num )
+void shy_platform_math_int_float_insider :: num_fract_value_get ( so_called_lib_std_float & value , so_called_type_platform_math_num_fract num )
 {
     value = num . _value ;
 }
 
-void shy_platform_math_int_float_insider :: num_fract_value_set ( so_called_type_platform_math_num_fract & num , float value )
+void shy_platform_math_int_float_insider :: num_fract_value_set ( so_called_type_platform_math_num_fract & num , so_called_lib_std_float value )
 {
     num . _value = value ;
 }
 
-so_called_type_platform_math_num_whole shy_platform_math_int_float_insider :: init_num_whole ( int value )
+so_called_type_platform_math_num_whole shy_platform_math_int_float_insider :: init_num_whole ( so_called_lib_std_int32_t value )
 {
     so_called_type_platform_math_num_whole result ;
     result . _value = value ;
     return result ;
 }
 
-so_called_type_platform_math_num_fract shy_platform_math_int_float_insider :: init_num_fract ( float value )
+so_called_type_platform_math_num_fract shy_platform_math_int_float_insider :: init_num_fract ( so_called_lib_std_float value )
 {
     so_called_type_platform_math_num_fract result ;
     result . _value = value ;
