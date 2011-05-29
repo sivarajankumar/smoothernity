85e6e8d012d13a98d2439391c6a775d2c593c89e
olegus8 <olegus8@localhost>
2011-05-29 18:20:36 +0000

    working on iphone port

--- src_refactored/injections/platform/conditions/shy_conditions.h
+++ src_refactored/injections/platform/conditions/shy_conditions.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_platform_conditions_included
 #define _shy_injections_platform_conditions_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../main/iphone/platform/conditions/shy_conditions_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../main/macosx/platform/conditions/shy_conditions_injections.h"
 #endif
