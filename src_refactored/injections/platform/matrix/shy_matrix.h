bf409c9c3b48fb3e8f66dc1131b36c0100b05d83
olegus8 <olegus8@localhost>
2011-05-30 07:12:21 +0000

    working on iphone port

--- src_refactored/injections/platform/matrix/shy_matrix.h
+++ src_refactored/injections/platform/matrix/shy_matrix.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_platform_matrix_included
 #define _shy_injections_platform_matrix_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../main/iphone/platform/matrix/shy_matrix_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../main/macosx/platform/matrix/shy_matrix_injections.h"
 #endif
--- src_refactored/injections/platform/matrix/type/data/shy_data.h
+++ src_refactored/injections/platform/matrix/type/data/shy_data.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_type_platform_matrix_data_included
 #define _shy_injections_type_platform_matrix_data_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../../../main/iphone/platform/matrix/type/data/shy_data_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../../../main/macosx/platform/matrix/type/data/shy_data_injections.h"
 #endif
