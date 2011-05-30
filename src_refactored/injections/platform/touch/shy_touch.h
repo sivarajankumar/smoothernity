91d92cadab6d761eb5b460c2eac0574c7a286ca1
olegus8 <olegus8@localhost>
2011-05-30 07:40:41 +0000

    working on iphone port

--- src_refactored/injections/platform/touch/shy_touch.h
+++ src_refactored/injections/platform/touch/shy_touch.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_platform_touch_included
 #define _shy_injections_platform_touch_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../main/iphone/platform/touch/shy_touch_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../main/macosx/platform/touch/shy_touch_injections.h"
 #endif
