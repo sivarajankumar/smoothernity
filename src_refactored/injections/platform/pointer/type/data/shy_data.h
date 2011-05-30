600e896eff6970b5026b032cf40e8923938196c6
olegus8 <olegus8@localhost>
2011-05-30 07:17:04 +0000

    working on iphone port

--- src_refactored/injections/platform/pointer/type/data/shy_data.h
+++ src_refactored/injections/platform/pointer/type/data/shy_data.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_type_platform_pointer_data_included
 #define _shy_injections_type_platform_pointer_data_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../../../main/iphone/platform/pointer/type/data/shy_data_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../../../main/macosx/platform/pointer/type/data/shy_data_injections.h"
 #endif
