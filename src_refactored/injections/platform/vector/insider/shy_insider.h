98fb877c5fed559297d03c1e07263bd13a439c2b
olegus8 <olegus8@localhost>
2011-05-30 07:43:12 +0000

    working on iphone port

--- src_refactored/injections/platform/vector/insider/shy_insider.h
+++ src_refactored/injections/platform/vector/insider/shy_insider.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_platform_vector_insider_included
 #define _shy_injections_platform_vector_insider_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../../main/iphone/platform/vector/insider/shy_insider_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../../main/macosx/platform/vector/insider/shy_insider_injections.h"
 #endif
--- src_refactored/injections/platform/vector/shy_vector.h
+++ src_refactored/injections/platform/vector/shy_vector.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_platform_vector_included
 #define _shy_injections_platform_vector_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../main/iphone/platform/vector/shy_vector_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../main/macosx/platform/vector/shy_vector_injections.h"
 #endif
