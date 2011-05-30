4ac0194d2dddd8d4c786c274ec3c2cc70688fafd
olegus8 <olegus8@localhost>
2011-05-30 07:14:22 +0000

    working on iphone port

--- src_refactored/injections/platform/mouse/insider/shy_insider.h
+++ src_refactored/injections/platform/mouse/insider/shy_insider.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_platform_mouse_insider_included
 #define _shy_injections_platform_mouse_insider_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../../main/iphone/platform/mouse/insider/shy_insider_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../../main/macosx/platform/mouse/insider/shy_insider_injections.h"
 #endif
--- src_refactored/injections/platform/mouse/shy_mouse.h
+++ src_refactored/injections/platform/mouse/shy_mouse.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_platform_mouse_included
 #define _shy_injections_platform_mouse_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../main/iphone/platform/mouse/shy_mouse_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../main/macosx/platform/mouse/shy_mouse_injections.h"
 #endif
