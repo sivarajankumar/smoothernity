b909135e68b10b07304d538142177bfb0a419836
olegus8 <olegus8@localhost>
2011-05-30 07:26:15 +0000

    working on iphone port

--- src_refactored/injections/platform/scheduler/shy_scheduler.h
+++ src_refactored/injections/platform/scheduler/shy_scheduler.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_platform_scheduler_included
 #define _shy_injections_platform_scheduler_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../main/iphone/platform/scheduler/shy_scheduler_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../main/macosx/platform/scheduler/shy_scheduler_injections.h"
 #endif
---
+++ src_refactored/main/iphone/platform/scheduler/shy_scheduler_injections.h
@@ -0,0 +1,8 @@
+#ifndef _shy_iphone_platform_scheduler_injections_included
+#define _shy_iphone_platform_scheduler_injections_included
+
+#include "../../../../platform/scheduler/direct_call/shy_direct_call_injections.h"
+typedef so_called_platform_scheduler_direct_call so_called_platform_scheduler ;
+
+#endif
+
