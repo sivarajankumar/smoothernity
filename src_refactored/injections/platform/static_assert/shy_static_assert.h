689f6c2ae2ef1688743f46bbe3430be79f2d8a2c
olegus8 <olegus8@localhost>
2011-05-30 07:39:01 +0000

    working on iphone port

--- src_refactored/injections/platform/static_assert/shy_static_assert.h
+++ src_refactored/injections/platform/static_assert/shy_static_assert.h
@@ -1,6 +1,10 @@
 #ifndef _shy_injections_platform_static_assert_included
 #define _shy_injections_platform_static_assert_included
 
+#ifdef shy_build_for_iphone
+    #include "../../../main/iphone/platform/static_assert/shy_static_assert_injections.h"
+#endif
+
 #ifdef shy_build_for_macosx
     #include "../../../main/macosx/platform/static_assert/shy_static_assert_injections.h"
 #endif
---
+++ src_refactored/main/iphone/platform/static_assert/shy_static_assert_injections.h
@@ -0,0 +1,6 @@
+#ifndef _shy_iphone_platform_static_assert_injections_included
+#define _shy_iphone_platform_static_assert_injections_included
+
+#include "../../../../platform/static_assert/shy_static_assert_injections.h"
+
+#endif
