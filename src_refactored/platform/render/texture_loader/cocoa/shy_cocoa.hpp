8967b1801ed12682881816ff3178db07f8ab0963
olegus8 <olegus8@localhost>
2011-05-31 13:22:48 +0000

    moving cocoa texture loader to platform

--- src_refactored/platform/render/texture_loader/cocoa/shy_cocoa.hpp
+++ src_refactored/platform/render/texture_loader/cocoa/shy_cocoa.hpp
@@ -82,44 +82,49 @@
 
 - ( void ) _perform_load
 {
-    NSUInteger width ;
-    NSUInteger height ;
-    NSURL * url = nil ;
-    CGDataProviderRef src ;
-    CGImageRef image ;
-    CGContextRef context = nil ;
-    CGColorSpaceRef color_space ;
-    url = [ NSURL fileURLWithPath : [ [ NSBundle mainBundle ] pathForResource : 
-        [ NSString stringWithFormat : @"texture_resource_%i" , ( int ) _resource_index ] 
+    so_called_lib_cocoa_NSUInteger width ;
+    so_called_lib_cocoa_NSUInteger height ;
+    so_called_lib_cocoa_NSURL * url = nil ;
+    so_called_lib_cocoa_CGDataProviderRef src ;
+    so_called_lib_cocoa_CGImageRef image ;
+    so_called_lib_cocoa_CGContextRef context = nil ;
+    so_called_lib_cocoa_CGColorSpaceRef color_space ;
+    url = [ so_called_lib_cocoa_NSURL fileURLWithPath : [ [ so_called_lib_cocoa_NSBundle mainBundle ] pathForResource : 
+        [ so_called_lib_cocoa_NSString stringWithFormat : @"texture_resource_%i" , ( so_called_lib_std_int32_t ) _resource_index ] 
         ofType : @"png"
         ] ] ;
-    src = CGDataProviderCreateWithURL ( ( CFURLRef ) url ) ;
+    src = so_called_lib_cocoa_CGDataProviderCreateWithURL ( ( so_called_lib_cocoa_CFURLRef ) url ) ;
     if ( src )
     {
-        image = CGImageCreateWithPNGDataProvider ( src , 0 , false , kCGRenderingIntentDefault ) ;
-        CGDataProviderRelease ( src ) ;
-        width = CGImageGetWidth ( image ) ;
-        height = CGImageGetHeight ( image ) ;
-        if ( width == ( NSUInteger ) _side_size && height == ( NSUInteger ) _side_size )
+        image = so_called_lib_cocoa_CGImageCreateWithPNGDataProvider 
+            ( src 
+            , 0 
+            , so_called_lib_std_false 
+            , so_called_lib_cocoa_kCGRenderingIntentDefault 
+            ) ;
+        so_called_lib_cocoa_CGDataProviderRelease ( src ) ;
+        width = so_called_lib_cocoa_CGImageGetWidth ( image ) ;
+        height = so_called_lib_cocoa_CGImageGetHeight ( image ) ;
+        if ( width == ( so_called_lib_cocoa_NSUInteger ) _side_size && height == ( so_called_lib_cocoa_NSUInteger ) _side_size )
         {
-            color_space = CGColorSpaceCreateDeviceRGB ( ) ;
-            context = CGBitmapContextCreate 
+            color_space = so_called_lib_cocoa_CGColorSpaceCreateDeviceRGB ( ) ;
+            context = so_called_lib_cocoa_CGBitmapContextCreate 
                 ( _buffer
                 , width 
                 , height 
                 , 8 
                 , 4 * width 
                 , color_space 
-                , kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host 
+                , so_called_lib_cocoa_kCGImageAlphaPremultipliedFirst | so_called_lib_cocoa_kCGBitmapByteOrder32Host 
                 ) ;
-            CGColorSpaceRelease ( color_space ) ;
-            CGContextTranslateCTM ( context , 0 , height ) ;
-            CGContextScaleCTM ( context , 1 , - 1 ) ;
-            CGContextSetBlendMode ( context , kCGBlendModeCopy ) ;
-            CGContextDrawImage ( context , CGRectMake ( 0 , 0 , width , height ) , image ) ;
-            CGContextRelease ( context ) ;
+            so_called_lib_cocoa_CGColorSpaceRelease ( color_space ) ;
+            so_called_lib_cocoa_CGContextTranslateCTM ( context , 0 , height ) ;
+            so_called_lib_cocoa_CGContextScaleCTM ( context , 1 , - 1 ) ;
+            so_called_lib_cocoa_CGContextSetBlendMode ( context , so_called_lib_cocoa_kCGBlendModeCopy ) ;
+            so_called_lib_cocoa_CGContextDrawImage ( context , so_called_lib_cocoa_CGRectMake ( 0 , 0 , width , height ) , image ) ;
+            so_called_lib_cocoa_CGContextRelease ( context ) ;
         }
-        CGImageRelease ( image ) ;
+        so_called_lib_cocoa_CGImageRelease ( image ) ;
     }
 }
 
