f0b7880546707ba9730339f5725f77f546b891d0
olegus8 <olegus8@localhost>
2011-06-01 16:02:07 +0000

    working on consts reflection helper macros

--- src_refactored/loadable/consts/reflection/logic_ortho/shy_logic_ortho.hpp
+++ src_refactored/loadable/consts/reflection/logic_ortho/shy_logic_ortho.hpp
@@ -1,6 +1,21 @@
+#define shy_bind_module_helper(module) \
+    so_called_loadable_consts_binder :: bind_module \
+        ( #module \
+        , & so_called_common_##module##_consts :: binding \
+        )
+
+#define shy_bind_value_helper(value) \
+    so_called_loadable_consts_binder :: bind_value \
+        ( #value \
+        , so_called_common_logic_ortho_consts :: value \
+        )
+
 void shy_loadable_consts_reflection_logic_ortho :: prepare ( )
 {
-    so_called_loadable_consts_binder :: module ( "logic_ortho" ) ;
-    so_called_loadable_consts_binder :: bind ( "z_near" , so_called_common_logic_ortho_consts :: z_near ) ;
-    so_called_loadable_consts_binder :: bind ( "z_far" , so_called_common_logic_ortho_consts :: z_far ) ;
+    shy_bind_module_helper ( logic_ortho ) ;
+    shy_bind_value_helper ( z_near ) ;
+    shy_bind_value_helper ( z_far ) ;
 }
+
+#undef shy_bind_module_helper
+#undef shy_bind_value_helper
