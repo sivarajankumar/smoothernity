e5214ff70a51196ee22462536d7e3df901222ac9
olegus8 <olegus8@localhost>
2011-06-01 16:11:59 +0000

    working on consts reflection helper macros

--- src_refactored/loadable/consts/binder/shy_binder.h
+++ src_refactored/loadable/consts/binder/shy_binder.h
@@ -1,10 +1,6 @@
 class shy_loadable_consts_binder
 {
 public :
-    static void module ( const so_called_lib_std_char * ) ;
-    static void bind ( const so_called_lib_std_char * , so_called_type_loadable_consts_content_value_fract_binding ) ;
-    static void bind ( const so_called_lib_std_char * , so_called_type_loadable_consts_content_value_whole_binding ) ;
-
     static void bind_module ( const so_called_lib_std_char * , so_called_type_loadable_consts_content_module_binding ) ;
     static void bind_value ( const so_called_lib_std_char * , so_called_type_loadable_consts_content_value_fract_binding ) ;
     static void bind_value ( const so_called_lib_std_char * , so_called_type_loadable_consts_content_value_whole_binding ) ;
