7b8cac5e1dd6161a4c6e91ccfb7dfeaf34cb0878
olegus8 <olegus8@localhost>
2011-06-01 11:07:58 +0000

    transforming logic title into logic salutation

--- src_refactored/common/init/shy_init.hpp
+++ src_refactored/common/init/shy_init.hpp
@@ -73,6 +73,7 @@ void shy_common_init :: init ( )
     so_called_common_logic_room_mesh :: register_in_scheduler ( ) ;
     so_called_common_logic_room_renderer :: register_in_scheduler ( ) ;
     so_called_common_logic_room_texture :: register_in_scheduler ( ) ;
+    so_called_common_logic_salutation :: register_in_scheduler ( ) ;
     so_called_common_logic_sound :: register_in_scheduler ( ) ;
     so_called_common_logic_text :: register_in_scheduler ( ) ;
     so_called_common_logic_title :: register_in_scheduler ( ) ;
---
+++ src_refactored/common/logic/salutation/shy_salutation_injections.cpp
@@ -0,0 +1,2 @@
+#include "./shy_salutation_injections.h"
+#include "./shy_salutation.hpp"
