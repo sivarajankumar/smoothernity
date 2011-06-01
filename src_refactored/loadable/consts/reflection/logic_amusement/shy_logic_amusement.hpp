3f350a98734d6665575b9fd3337f26eb867a4ec2
olegus8 <olegus8@localhost>
2011-06-01 14:03:19 +0000

    working on consts reflection helper macros

--- src_refactored/loadable/consts/reflection/logic_amusement/shy_logic_amusement.hpp
+++ src_refactored/loadable/consts/reflection/logic_amusement/shy_logic_amusement.hpp
@@ -1,7 +1,15 @@
+#define shy_bind_value_helper(value) \
+    so_called_loadable_consts_binder :: bind \
+        ( #value \
+        , so_called_common_logic_amusement_consts :: value \
+        )
+
 void shy_loadable_consts_reflection_logic_amusement :: prepare ( )
 {
     so_called_loadable_consts_binder :: module ( "logic_amusement" ) ;
-    so_called_loadable_consts_binder :: bind ( "renderer_clear_color_r" , so_called_common_logic_amusement_consts :: renderer_clear_color_r ) ;
-    so_called_loadable_consts_binder :: bind ( "renderer_clear_color_g" , so_called_common_logic_amusement_consts :: renderer_clear_color_g ) ;
-    so_called_loadable_consts_binder :: bind ( "renderer_clear_color_b" , so_called_common_logic_amusement_consts :: renderer_clear_color_b ) ;
+    shy_bind_value_helper ( renderer_clear_color_r ) ;
+    shy_bind_value_helper ( renderer_clear_color_g ) ;
+    shy_bind_value_helper ( renderer_clear_color_b ) ;
 }
+
+#undef shy_bind_value_helper
