ecd20337cca484346eea8b14f43c7b2c075d82d6
olegus8 <olegus8@localhost>
2011-06-02 16:00:58 +0000

    working on reflection binding helpers

--- src_refactored/loadable/fsm/reflection/logic_application/shy_logic_application.hpp
+++ src_refactored/loadable/fsm/reflection/logic_application/shy_logic_application.hpp
@@ -1,66 +1,49 @@
-#define shy_bind_system_helper(system) \
-    so_called_loadable_fsm_binder :: bind_system \
-        ( #system \
-        , & so_called_common_##system##_fsm_binding \
-        )
-
-#define shy_bind_system_action_helper(system,action) \
-    so_called_loadable_fsm_binder :: bind_action \
-        ( #action \
-        , & so_called_common_##system##_fsm_actions :: action \
-        )
-
-#define shy_bind_system_input_helper(system,input) \
-    so_called_loadable_fsm_binder :: bind_input \
-        ( #input \
-        , reinterpret_cast < so_called_type_loadable_fsm_content_input_binding > ( & so_called_type_common_##system##_fsm_inputs :: input ) \
-        )
-
-#define shy_bind_action_helper(action) shy_bind_system_action_helper ( logic_application , action )
-#define shy_bind_input_helper(input) shy_bind_system_input_helper ( logic_application , input )
+#define shy_guts_bind_action(action) so_called_loadable_fsm_binder_helper_action ( logic_application , action )
+#define shy_guts_bind_input(input) so_called_loadable_fsm_binder_helper_input ( logic_application , input )
+#define shy_guts_bind_system(system) so_called_loadable_fsm_binder_helper_system ( system )
 
 void shy_loadable_fsm_reflection_logic_application :: prepare ( )
 {
-    shy_bind_system_helper ( logic_application ) ;
+    shy_guts_bind_system ( logic_application ) ;
 
-    shy_bind_action_helper ( logic_amusement_creation_permit ) ;
-    shy_bind_action_helper ( logic_amusement_launch_permit ) ;
-    shy_bind_action_helper ( logic_amusement_render ) ;
-    shy_bind_action_helper ( logic_amusement_update ) ;
-    shy_bind_action_helper ( logic_game_launch_permit ) ;
-    shy_bind_action_helper ( logic_game_render ) ;
-    shy_bind_action_helper ( logic_game_update ) ;
-    shy_bind_action_helper ( logic_main_menu_creation_permit ) ;
-    shy_bind_action_helper ( logic_main_menu_launch_permit ) ;
-    shy_bind_action_helper ( logic_main_menu_render ) ;
-    shy_bind_action_helper ( logic_main_menu_update ) ;
-    shy_bind_action_helper ( logic_salutation_creation_permit ) ;
-    shy_bind_action_helper ( logic_salutation_launch_permit ) ;
-    shy_bind_action_helper ( logic_salutation_render ) ;
-    shy_bind_action_helper ( logic_salutation_update ) ;
-    shy_bind_action_helper ( logic_text_prepare_permit ) ;
-    shy_bind_action_helper ( logic_text_update ) ;
-    shy_bind_action_helper ( logic_title_launch_permit ) ;
-    shy_bind_action_helper ( logic_title_render ) ;
-    shy_bind_action_helper ( logic_title_update ) ;
+    shy_guts_bind_action ( logic_amusement_creation_permit ) ;
+    shy_guts_bind_action ( logic_amusement_launch_permit ) ;
+    shy_guts_bind_action ( logic_amusement_render ) ;
+    shy_guts_bind_action ( logic_amusement_update ) ;
+    shy_guts_bind_action ( logic_game_launch_permit ) ;
+    shy_guts_bind_action ( logic_game_render ) ;
+    shy_guts_bind_action ( logic_game_update ) ;
+    shy_guts_bind_action ( logic_main_menu_creation_permit ) ;
+    shy_guts_bind_action ( logic_main_menu_launch_permit ) ;
+    shy_guts_bind_action ( logic_main_menu_render ) ;
+    shy_guts_bind_action ( logic_main_menu_update ) ;
+    shy_guts_bind_action ( logic_salutation_creation_permit ) ;
+    shy_guts_bind_action ( logic_salutation_launch_permit ) ;
+    shy_guts_bind_action ( logic_salutation_render ) ;
+    shy_guts_bind_action ( logic_salutation_update ) ;
+    shy_guts_bind_action ( logic_text_prepare_permit ) ;
+    shy_guts_bind_action ( logic_text_update ) ;
+    shy_guts_bind_action ( logic_title_launch_permit ) ;
+    shy_guts_bind_action ( logic_title_render ) ;
+    shy_guts_bind_action ( logic_title_update ) ;
 
-    shy_bind_input_helper ( logic_amusement_created ) ;
-    shy_bind_input_helper ( logic_amusement_finished ) ;
-    shy_bind_input_helper ( logic_application_render ) ;
-    shy_bind_input_helper ( logic_application_update ) ;
-    shy_bind_input_helper ( logic_text_prepared ) ;
-    shy_bind_input_helper ( logic_title_created ) ;
-    shy_bind_input_helper ( logic_title_finished ) ;
-    shy_bind_input_helper ( logic_main_menu_created ) ;
-    shy_bind_input_helper ( logic_main_menu_finished ) ;
-    shy_bind_input_helper ( logic_salutation_created ) ;
-    shy_bind_input_helper ( logic_salutation_finished ) ;
-    shy_bind_input_helper ( stage_amusement_disabled ) ;
-    shy_bind_input_helper ( stage_amusement_enabled ) ;
-    shy_bind_input_helper ( stage_main_menu_disabled ) ;
-    shy_bind_input_helper ( stage_main_menu_enabled ) ;
-    shy_bind_input_helper ( stage_salutation_disabled ) ;
-    shy_bind_input_helper ( stage_salutation_enabled ) ;
-    shy_bind_input_helper ( stage_title_disabled ) ;
-    shy_bind_input_helper ( stage_title_enabled ) ;
+    shy_guts_bind_input ( logic_amusement_created ) ;
+    shy_guts_bind_input ( logic_amusement_finished ) ;
+    shy_guts_bind_input ( logic_application_render ) ;
+    shy_guts_bind_input ( logic_application_update ) ;
+    shy_guts_bind_input ( logic_text_prepared ) ;
+    shy_guts_bind_input ( logic_title_created ) ;
+    shy_guts_bind_input ( logic_title_finished ) ;
+    shy_guts_bind_input ( logic_main_menu_created ) ;
+    shy_guts_bind_input ( logic_main_menu_finished ) ;
+    shy_guts_bind_input ( logic_salutation_created ) ;
+    shy_guts_bind_input ( logic_salutation_finished ) ;
+    shy_guts_bind_input ( stage_amusement_disabled ) ;
+    shy_guts_bind_input ( stage_amusement_enabled ) ;
+    shy_guts_bind_input ( stage_main_menu_disabled ) ;
+    shy_guts_bind_input ( stage_main_menu_enabled ) ;
+    shy_guts_bind_input ( stage_salutation_disabled ) ;
+    shy_guts_bind_input ( stage_salutation_enabled ) ;
+    shy_guts_bind_input ( stage_title_disabled ) ;
+    shy_guts_bind_input ( stage_title_enabled ) ;
 }
--- src_refactored/loadable/fsm/reflection/logic_application/shy_logic_application_injections.cpp
+++ src_refactored/loadable/fsm/reflection/logic_application/shy_logic_application_injections.cpp
@@ -1,6 +1,8 @@
 #ifdef shy_build_loadable_way
 
-    #include "../../binder/shy_binder_injections.h"
+    #include "../../binder/helper/action/shy_action_injections.h"
+    #include "../../binder/helper/input/shy_input_injections.h"
+    #include "../../binder/helper/system/shy_system_injections.h"
     #include "../../../../common/logic/application/fsm/actions/shy_actions_injections.h"
     #include "../../../../common/logic/application/fsm/binding/shy_binding_injections.h"
     #include "../../../../common/logic/application/fsm/type/inputs/shy_inputs_injections.h"
