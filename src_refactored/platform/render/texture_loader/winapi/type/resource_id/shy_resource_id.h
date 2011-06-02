5ab43c03fa8868544e8d1453093fab37597bf78c
olegus8 <olegus8@localhost>
2011-06-02 13:34:14 +0000

    working on windows port

--- src_refactored/platform/render/texture_loader/winapi/type/resource_id/shy_resource_id.h
+++ src_refactored/platform/render/texture_loader/winapi/type/resource_id/shy_resource_id.h
@@ -1,8 +1,8 @@
-class shy_type_platform_render_texture_loader_cocoa_resource_id
+class shy_type_platform_render_texture_loader_winapi_resource_id
 {
-    friend class shy_platform_render_texture_loader_cocoa ;
+    friend class shy_platform_render_texture_loader_winapi ;
 public :
-    shy_type_platform_render_texture_loader_cocoa_resource_id ( ) ;
+    shy_type_platform_render_texture_loader_winapi_resource_id ( ) ;
 private :
     so_called_lib_std_int32_t _resource_id ;
 } ;
--- src_refactored/platform/render/texture_loader/winapi/type/resource_id/shy_resource_id_injections.h
+++ src_refactored/platform/render/texture_loader/winapi/type/resource_id/shy_resource_id_injections.h
@@ -1,9 +1,9 @@
-#ifndef _shy_type_platform_render_texture_loader_cocoa_resource_id_injections_included
-#define _shy_type_platform_render_texture_loader_cocoa_resource_id_injections_included
+#ifndef _shy_type_platform_render_texture_loader_winapi_resource_id_injections_included
+#define _shy_type_platform_render_texture_loader_winapi_resource_id_injections_included
 
 #include "../../../../../../injections/lib/std/int32_t/shy_int32_t.h"
 #include "./shy_resource_id.h"
 
-typedef shy_type_platform_render_texture_loader_cocoa_resource_id so_called_type_platform_render_texture_loader_cocoa_resource_id ;
+typedef shy_type_platform_render_texture_loader_winapi_resource_id so_called_type_platform_render_texture_loader_winapi_resource_id ;
 
 #endif
