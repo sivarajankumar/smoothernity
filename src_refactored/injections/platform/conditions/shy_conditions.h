ef489c3c4a944abd598f617e377bdaa7b3995357
olegus8 <olegus8@localhost>
2011-06-02 13:56:29 +0000

    working on windows port

--- src_refactored/injections/platform/conditions/shy_conditions.h
+++ src_refactored/injections/platform/conditions/shy_conditions.h
@@ -9,4 +9,8 @@
     #include "../../../main/macosx/platform/conditions/shy_conditions_injections.h"
 #endif
 
+#ifdef shy_build_for_win
+    #include "../../../main/win/platform/conditions/shy_conditions_injections.h"
+#endif
+
 #endif
