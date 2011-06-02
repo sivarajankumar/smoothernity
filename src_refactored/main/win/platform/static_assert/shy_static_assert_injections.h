997fa8c91739b326c844a45269af510904984310
olegus8 <olegus8@localhost>
2011-06-02 15:06:32 +0000

    working on windows port

---
+++ src_refactored/main/win/platform/static_assert/shy_static_assert_injections.h
@@ -0,0 +1,6 @@
+#ifndef _shy_macosx_platform_static_assert_injections_included
+#define _shy_macosx_platform_static_assert_injections_included
+
+#include "../../../../platform/static_assert/shy_static_assert_injections.h"
+
+#endif
