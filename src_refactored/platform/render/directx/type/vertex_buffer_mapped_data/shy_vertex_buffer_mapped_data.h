1e89b7eeca2fdf3026a47603f7659127cc7f401c
olegus8 <olegus8@localhost>
2011-06-02 09:06:39 +0000

    working on windows port

--- src_refactored/platform/render/directx/type/vertex_buffer_mapped_data/shy_vertex_buffer_mapped_data.h
+++ src_refactored/platform/render/directx/type/vertex_buffer_mapped_data/shy_vertex_buffer_mapped_data.h
@@ -1,8 +1,8 @@
-class shy_type_platform_render_opengl_vertex_buffer_mapped_data
+class shy_type_platform_render_directx_vertex_buffer_mapped_data
 {
-    friend class shy_platform_render_opengl ;
+    friend class shy_platform_render_directx ;
 public :
-    shy_type_platform_render_opengl_vertex_buffer_mapped_data ( ) ;
+    shy_type_platform_render_directx_vertex_buffer_mapped_data ( ) ;
 private :
     void * _data ;
 } ;
