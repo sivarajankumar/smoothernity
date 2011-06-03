23aff76f3aab0e429505b4b178dd8753954b0053
olegus8 <olegus8@localhost>
2011-06-03 07:15:22 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_fidget/shy_logic_fidget.hpp
+++ src_refactored/loadable/consts/reflection/logic_fidget/shy_logic_fidget.hpp
@@ -1,30 +1,18 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_fidget_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_fidget , value )
 
 void shy_loadable_consts_reflection_logic_fidget :: prepare ( )
 {
-    shy_bind_module_helper ( logic_fidget ) ;
-    shy_bind_value_helper ( fidget_size ) ;
-    shy_bind_value_helper ( fidget_r ) ;
-    shy_bind_value_helper ( fidget_g ) ;
-    shy_bind_value_helper ( fidget_b ) ;
-    shy_bind_value_helper ( mesh_x ) ;
-    shy_bind_value_helper ( mesh_y_from_top ) ;
-    shy_bind_value_helper ( mesh_z ) ;
-    shy_bind_value_helper ( angle_delta ) ;
-    shy_bind_value_helper ( fidget_edges ) ;
-    shy_bind_value_helper ( scale_in_frames ) ;
-    shy_bind_value_helper ( should_render_fidget ) ;
+    shy_guts_bind_module ( logic_fidget ) ;
+    shy_guts_bind_value ( fidget_size ) ;
+    shy_guts_bind_value ( fidget_r ) ;
+    shy_guts_bind_value ( fidget_g ) ;
+    shy_guts_bind_value ( fidget_b ) ;
+    shy_guts_bind_value ( mesh_x ) ;
+    shy_guts_bind_value ( mesh_y_from_top ) ;
+    shy_guts_bind_value ( mesh_z ) ;
+    shy_guts_bind_value ( angle_delta ) ;
+    shy_guts_bind_value ( fidget_edges ) ;
+    shy_guts_bind_value ( scale_in_frames ) ;
+    shy_guts_bind_value ( should_render_fidget ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
