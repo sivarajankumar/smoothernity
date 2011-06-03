2d70a0caf0673c0ac4233c50df59c95dfe5d64bc
olegus8 <olegus8@localhost>
2011-06-03 07:34:36 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_observer_animation/shy_logic_observer_animation.hpp
+++ src_refactored/loadable/consts/reflection/logic_observer_animation/shy_logic_observer_animation.hpp
@@ -1,24 +1,12 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_observer_animation_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_observer_animation , value )
 
 void shy_loadable_consts_reflection_logic_observer_animation :: prepare ( )
 {
-    shy_bind_module_helper ( logic_observer_animation ) ;
-    shy_bind_value_helper ( flight_target_z ) ;
-    shy_bind_value_helper ( flight_horizontal_offset_period ) ;
-    shy_bind_value_helper ( flight_horizontal_offset_amplitude ) ;
-    shy_bind_value_helper ( flight_vertical_offset_period ) ;
-    shy_bind_value_helper ( flight_vertical_offset_amplitude ) ;
+    shy_guts_bind_module ( logic_observer_animation ) ;
+    shy_guts_bind_value ( flight_target_z ) ;
+    shy_guts_bind_value ( flight_horizontal_offset_period ) ;
+    shy_guts_bind_value ( flight_horizontal_offset_amplitude ) ;
+    shy_guts_bind_value ( flight_vertical_offset_period ) ;
+    shy_guts_bind_value ( flight_vertical_offset_amplitude ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
