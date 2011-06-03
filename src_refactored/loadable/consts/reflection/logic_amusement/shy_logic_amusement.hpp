1601d2f82bf933142a8537ccd1298dde469f6c1e
olegus8 <olegus8@localhost>
2011-06-03 06:58:09 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_amusement/shy_logic_amusement.hpp
+++ src_refactored/loadable/consts/reflection/logic_amusement/shy_logic_amusement.hpp
@@ -1,10 +1,10 @@
-#define shy_bind_module_helper(module) so_called_loadable_consts_binder_helper_module ( module )
-#define shy_bind_value_helper(value) so_called_loadable_consts_binder_helper_value ( logic_amusement , value )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_amusement , value )
 
 void shy_loadable_consts_reflection_logic_amusement :: prepare ( )
 {
-    shy_bind_module_helper ( logic_amusement ) ;
-    shy_bind_value_helper ( renderer_clear_color_r ) ;
-    shy_bind_value_helper ( renderer_clear_color_g ) ;
-    shy_bind_value_helper ( renderer_clear_color_b ) ;
+    shy_guts_bind_module ( logic_amusement ) ;
+    shy_guts_bind_value ( renderer_clear_color_r ) ;
+    shy_guts_bind_value ( renderer_clear_color_g ) ;
+    shy_guts_bind_value ( renderer_clear_color_b ) ;
 }
--- src_refactored/loadable/consts/reflection/logic_application/shy_logic_application.hpp
+++ src_refactored/loadable/consts/reflection/logic_application/shy_logic_application.hpp
@@ -1,11 +1,11 @@
-#define shy_bind_module_helper(module) so_called_loadable_consts_binder_helper_module ( module )
-#define shy_bind_value_helper(value) so_called_loadable_consts_binder_helper_value ( logic_application , value )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_application , value )
 
 void shy_loadable_consts_reflection_logic_application :: prepare ( )
 {
-    shy_bind_module_helper ( logic_application ) ;
-    shy_bind_value_helper ( skip_amusement ) ;
-    shy_bind_value_helper ( skip_main_menu ) ;
-    shy_bind_value_helper ( skip_salutation ) ;
-    shy_bind_value_helper ( skip_title ) ;
+    shy_guts_bind_module ( logic_application ) ;
+    shy_guts_bind_value ( skip_amusement ) ;
+    shy_guts_bind_value ( skip_main_menu ) ;
+    shy_guts_bind_value ( skip_salutation ) ;
+    shy_guts_bind_value ( skip_title ) ;
 }
