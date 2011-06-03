4bbc6413ba44be7f6fb34cb806755faff954fe42
olegus8 <olegus8@localhost>
2011-06-03 07:36:05 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_ortho/shy_logic_ortho.hpp
+++ src_refactored/loadable/consts/reflection/logic_ortho/shy_logic_ortho.hpp
@@ -1,21 +1,9 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_ortho_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_ortho , value )
 
 void shy_loadable_consts_reflection_logic_ortho :: prepare ( )
 {
-    shy_bind_module_helper ( logic_ortho ) ;
-    shy_bind_value_helper ( z_near ) ;
-    shy_bind_value_helper ( z_far ) ;
+    shy_guts_bind_module ( logic_ortho ) ;
+    shy_guts_bind_value ( z_near ) ;
+    shy_guts_bind_value ( z_far ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
