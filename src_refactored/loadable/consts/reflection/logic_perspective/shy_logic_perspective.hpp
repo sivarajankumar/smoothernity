974164630d19d91d6eca9304108c2be117b8e23c
olegus8 <olegus8@localhost>
2011-06-03 07:37:43 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_perspective/shy_logic_perspective.hpp
+++ src_refactored/loadable/consts/reflection/logic_perspective/shy_logic_perspective.hpp
@@ -1,20 +1,8 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_perspective_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_perspective , value )
 
 void shy_loadable_consts_reflection_logic_perspective :: prepare ( )
 {
-    shy_bind_module_helper ( logic_perspective ) ;
-    shy_bind_value_helper ( z_far_unscaled ) ;
+    shy_guts_bind_module ( logic_perspective ) ;
+    shy_guts_bind_value ( z_far_unscaled ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
