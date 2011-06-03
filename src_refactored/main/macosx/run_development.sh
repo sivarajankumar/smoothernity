c98bc6b498a7fb035d9cd9b49b7c27d31cc03735
olegus8 <olegus8@localhost>
2011-06-03 07:46:29 +0000

    run script

--- src_refactored/main/macosx/run_development.sh
+++ src_refactored/main/macosx/run_development.sh
@@ -1,1 +1,1 @@
-./build/Development/Smoothernity.app/Contents/MacOS/Smoothernity
+find ../../../data -name '*.shy' -exec cat {} \; | ./build/Development/Smoothernity.app/Contents/MacOS/Smoothernity | python
