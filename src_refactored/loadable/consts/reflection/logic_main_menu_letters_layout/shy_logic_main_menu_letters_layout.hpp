08fb1bbebb9b8acf13cba331624a26f1e986581a
olegus8 <olegus8@localhost>
2011-06-03 07:24:40 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_main_menu_letters_layout/shy_logic_main_menu_letters_layout.hpp
+++ src_refactored/loadable/consts/reflection/logic_main_menu_letters_layout/shy_logic_main_menu_letters_layout.hpp
@@ -1,24 +1,12 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_main_menu_letters_layout_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_main_menu_letters_layout , value )
 
 void shy_loadable_consts_reflection_logic_main_menu_letters_layout :: prepare ( )
 {
-    shy_bind_module_helper ( logic_main_menu_letters_layout ) ;
-    shy_bind_value_helper ( letter_size_fract_horizontal_spacing ) ;
-    shy_bind_value_helper ( letter_size_fract_vertical_spacing ) ;
-    shy_bind_value_helper ( letter_size_fract_horizontal_border ) ;
-    shy_bind_value_helper ( letter_size_fract_vertical_border ) ;
-    shy_bind_value_helper ( menu_position_z ) ;
+    shy_guts_bind_module ( logic_main_menu_letters_layout ) ;
+    shy_guts_bind_value ( letter_size_fract_horizontal_spacing ) ;
+    shy_guts_bind_value ( letter_size_fract_vertical_spacing ) ;
+    shy_guts_bind_value ( letter_size_fract_horizontal_border ) ;
+    shy_guts_bind_value ( letter_size_fract_vertical_border ) ;
+    shy_guts_bind_value ( menu_position_z ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
