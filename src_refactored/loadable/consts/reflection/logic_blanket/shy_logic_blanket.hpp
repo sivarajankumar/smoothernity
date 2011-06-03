51ddbc2b1b2dc79d84bf8eba17af5f018ed635c0
olegus8 <olegus8@localhost>
2011-06-03 07:06:22 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_blanket/shy_logic_blanket.hpp
+++ src_refactored/loadable/consts/reflection/logic_blanket/shy_logic_blanket.hpp
@@ -1,28 +1,16 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_blanket_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_blanket , value )
 
 void shy_loadable_consts_reflection_logic_blanket :: prepare ( )
 {
-    shy_bind_module_helper ( logic_blanket ) ;
-    shy_bind_value_helper ( mesh_vertex_x_left ) ;
-    shy_bind_value_helper ( mesh_vertex_x_right ) ;
-    shy_bind_value_helper ( mesh_vertex_y_bottom ) ;
-    shy_bind_value_helper ( mesh_vertex_y_top ) ;
-    shy_bind_value_helper ( mesh_vertex_z ) ;
-    shy_bind_value_helper ( mesh_color_r ) ;
-    shy_bind_value_helper ( mesh_color_g ) ;
-    shy_bind_value_helper ( mesh_color_b ) ;
-    shy_bind_value_helper ( mesh_color_a ) ;
+    shy_guts_bind_module ( logic_blanket ) ;
+    shy_guts_bind_value ( mesh_vertex_x_left ) ;
+    shy_guts_bind_value ( mesh_vertex_x_right ) ;
+    shy_guts_bind_value ( mesh_vertex_y_bottom ) ;
+    shy_guts_bind_value ( mesh_vertex_y_top ) ;
+    shy_guts_bind_value ( mesh_vertex_z ) ;
+    shy_guts_bind_value ( mesh_color_r ) ;
+    shy_guts_bind_value ( mesh_color_g ) ;
+    shy_guts_bind_value ( mesh_color_b ) ;
+    shy_guts_bind_value ( mesh_color_a ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
