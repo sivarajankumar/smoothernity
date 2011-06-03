3120bc0210f280f73ba639588d0b3361bfeafa19
olegus8 <olegus8@localhost>
2011-06-03 07:40:36 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_title/shy_logic_title.hpp
+++ src_refactored/loadable/consts/reflection/logic_title/shy_logic_title.hpp
@@ -1,31 +1,19 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_title_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_title , value )
 
 void shy_loadable_consts_reflection_logic_title :: prepare ( )
 {
-    shy_bind_module_helper ( logic_title ) ;
-    shy_bind_value_helper ( appear_pos_angle_periods ) ;
-    shy_bind_value_helper ( appear_rubber_first ) ;
-    shy_bind_value_helper ( appear_rubber_last ) ;
-    shy_bind_value_helper ( appear_duration_in_frames ) ;
-    shy_bind_value_helper ( disappear_pos_angle_periods ) ;
-    shy_bind_value_helper ( disappear_rubber_first ) ;
-    shy_bind_value_helper ( disappear_rubber_last ) ;
-    shy_bind_value_helper ( disappear_duration_in_frames ) ;
-    shy_bind_value_helper ( scene_scale_min ) ;
-    shy_bind_value_helper ( scene_scale_max ) ;
-    shy_bind_value_helper ( spin_radius_in_letters ) ;
-    shy_bind_value_helper ( frames_between_letters ) ;
+    shy_guts_bind_module ( logic_title ) ;
+    shy_guts_bind_value ( appear_pos_angle_periods ) ;
+    shy_guts_bind_value ( appear_rubber_first ) ;
+    shy_guts_bind_value ( appear_rubber_last ) ;
+    shy_guts_bind_value ( appear_duration_in_frames ) ;
+    shy_guts_bind_value ( disappear_pos_angle_periods ) ;
+    shy_guts_bind_value ( disappear_rubber_first ) ;
+    shy_guts_bind_value ( disappear_rubber_last ) ;
+    shy_guts_bind_value ( disappear_duration_in_frames ) ;
+    shy_guts_bind_value ( scene_scale_min ) ;
+    shy_guts_bind_value ( scene_scale_max ) ;
+    shy_guts_bind_value ( spin_radius_in_letters ) ;
+    shy_guts_bind_value ( frames_between_letters ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
