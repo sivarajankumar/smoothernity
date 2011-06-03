5b54fd3ac249581925abd6d5c4fd5117958f1d78
olegus8 <olegus8@localhost>
2011-06-03 07:18:18 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_main_menu_animation/shy_logic_main_menu_animation.hpp
+++ src_refactored/loadable/consts/reflection/logic_main_menu_animation/shy_logic_main_menu_animation.hpp
@@ -1,24 +1,12 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_main_menu_animation_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_main_menu_animation , value )
 
 void shy_loadable_consts_reflection_logic_main_menu_animation :: prepare ( )
 {
-    shy_bind_module_helper ( logic_main_menu_animation ) ;
-    shy_bind_value_helper ( shake_time_to_begin ) ;
-    shy_bind_value_helper ( shake_time_from_begin_to_end ) ;
-    shy_bind_value_helper ( shake_shift_x_amplitude_begin ) ;
-    shy_bind_value_helper ( shake_shift_x_amplitude_end ) ;
-    shy_bind_value_helper ( shake_shift_x_period_in_seconds ) ;
+    shy_guts_bind_module ( logic_main_menu_animation ) ;
+    shy_guts_bind_value ( shake_time_to_begin ) ;
+    shy_guts_bind_value ( shake_time_from_begin_to_end ) ;
+    shy_guts_bind_value ( shake_shift_x_amplitude_begin ) ;
+    shy_guts_bind_value ( shake_shift_x_amplitude_end ) ;
+    shy_guts_bind_value ( shake_shift_x_period_in_seconds ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
