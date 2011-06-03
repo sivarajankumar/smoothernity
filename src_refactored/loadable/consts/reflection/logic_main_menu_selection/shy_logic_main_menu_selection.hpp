a06ebfc6990dd7e4c8f165eb30c3622c811037a1
olegus8 <olegus8@localhost>
2011-06-03 07:29:53 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_main_menu_selection/shy_logic_main_menu_selection.hpp
+++ src_refactored/loadable/consts/reflection/logic_main_menu_selection/shy_logic_main_menu_selection.hpp
@@ -1,25 +1,13 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_main_menu_selection_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_main_menu_selection , value )
 
 void shy_loadable_consts_reflection_logic_main_menu_selection :: prepare ( )
 {
-    shy_bind_module_helper ( logic_main_menu_selection ) ;
-    shy_bind_value_helper ( mesh_size ) ;
-    shy_bind_value_helper ( mesh_color_r ) ;
-    shy_bind_value_helper ( mesh_color_g ) ;
-    shy_bind_value_helper ( mesh_color_b ) ;
-    shy_bind_value_helper ( mesh_color_a ) ;
-    shy_bind_value_helper ( selected_rect_vertical_scale ) ;
+    shy_guts_bind_module ( logic_main_menu_selection ) ;
+    shy_guts_bind_value ( mesh_size ) ;
+    shy_guts_bind_value ( mesh_color_r ) ;
+    shy_guts_bind_value ( mesh_color_g ) ;
+    shy_guts_bind_value ( mesh_color_b ) ;
+    shy_guts_bind_value ( mesh_color_a ) ;
+    shy_guts_bind_value ( selected_rect_vertical_scale ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
