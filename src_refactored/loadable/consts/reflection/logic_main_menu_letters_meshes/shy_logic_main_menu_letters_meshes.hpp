e109b149696834ae3d419c4a33cf2717b36e479e
olegus8 <olegus8@localhost>
2011-06-03 07:28:17 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_main_menu_letters_meshes/shy_logic_main_menu_letters_meshes.hpp
+++ src_refactored/loadable/consts/reflection/logic_main_menu_letters_meshes/shy_logic_main_menu_letters_meshes.hpp
@@ -1,25 +1,13 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_main_menu_letters_meshes_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_main_menu_letters_meshes , value )
 
 void shy_loadable_consts_reflection_logic_main_menu_letters_meshes :: prepare ( )
 {
-    shy_bind_module_helper ( logic_main_menu_letters_meshes ) ;
-    shy_bind_value_helper ( letter_mesh_size ) ;
-    shy_bind_value_helper ( letter_mesh_color_r ) ;
-    shy_bind_value_helper ( letter_mesh_color_g ) ;
-    shy_bind_value_helper ( letter_mesh_color_b ) ;
-    shy_bind_value_helper ( letter_mesh_color_a ) ;
-    shy_bind_value_helper ( time_between_creation ) ;
+    shy_guts_bind_module ( logic_main_menu_letters_meshes ) ;
+    shy_guts_bind_value ( letter_mesh_size ) ;
+    shy_guts_bind_value ( letter_mesh_color_r ) ;
+    shy_guts_bind_value ( letter_mesh_color_g ) ;
+    shy_guts_bind_value ( letter_mesh_color_b ) ;
+    shy_guts_bind_value ( letter_mesh_color_a ) ;
+    shy_guts_bind_value ( time_between_creation ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
