e6720a2c5e7cc96f7e95184a9a533e23c063ef91
olegus8 <olegus8@localhost>
2011-06-03 07:08:51 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_blanket_animation/shy_logic_blanket_animation.hpp
+++ src_refactored/loadable/consts/reflection/logic_blanket_animation/shy_logic_blanket_animation.hpp
@@ -1,32 +1,20 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_blanket_animation_consts :: value \
-        )
+#define shy_guts_bind_module(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_guts_bind_value(value) so_called_loadable_consts_binder_helper_value ( logic_blanket_animation , value )
 
 void shy_loadable_consts_reflection_logic_blanket_animation :: prepare ( )
 {
-    shy_bind_module_helper ( logic_blanket_animation ) ;
-    shy_bind_value_helper ( animation_origin_x ) ;
-    shy_bind_value_helper ( animation_origin_y ) ;
-    shy_bind_value_helper ( animation_origin_z ) ;
-    shy_bind_value_helper ( appear_scale_begin ) ;
-    shy_bind_value_helper ( appear_scale_end ) ;
-    shy_bind_value_helper ( appear_rotation_begin ) ;
-    shy_bind_value_helper ( appear_rotation_end ) ;
-    shy_bind_value_helper ( appear_time_from_begin_to_end ) ;
-    shy_bind_value_helper ( disappear_scale_begin ) ;
-    shy_bind_value_helper ( disappear_scale_end ) ;
-    shy_bind_value_helper ( disappear_rotation_begin ) ;
-    shy_bind_value_helper ( disappear_rotation_end ) ;
-    shy_bind_value_helper ( disappear_time_from_begin_to_end ) ;
+    shy_guts_bind_module ( logic_blanket_animation ) ;
+    shy_guts_bind_value ( animation_origin_x ) ;
+    shy_guts_bind_value ( animation_origin_y ) ;
+    shy_guts_bind_value ( animation_origin_z ) ;
+    shy_guts_bind_value ( appear_scale_begin ) ;
+    shy_guts_bind_value ( appear_scale_end ) ;
+    shy_guts_bind_value ( appear_rotation_begin ) ;
+    shy_guts_bind_value ( appear_rotation_end ) ;
+    shy_guts_bind_value ( appear_time_from_begin_to_end ) ;
+    shy_guts_bind_value ( disappear_scale_begin ) ;
+    shy_guts_bind_value ( disappear_scale_end ) ;
+    shy_guts_bind_value ( disappear_rotation_begin ) ;
+    shy_guts_bind_value ( disappear_rotation_end ) ;
+    shy_guts_bind_value ( disappear_time_from_begin_to_end ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
