07a20e1b27541d53d61c4a0a4140836e304729ea
olegus8 <olegus8@localhost>
2011-06-03 06:52:47 +0000

    working on reflection binding helpers

--- src_refactored/loadable/consts/reflection/logic_amusement/shy_logic_amusement.hpp
+++ src_refactored/loadable/consts/reflection/logic_amusement/shy_logic_amusement.hpp
@@ -1,14 +1,5 @@
-#define shy_bind_module_helper(module) \
-    so_called_loadable_consts_binder :: bind_module \
-        ( #module \
-        , & so_called_common_##module##_consts :: binding \
-        )
-
-#define shy_bind_value_helper(value) \
-    so_called_loadable_consts_binder :: bind_value \
-        ( #value \
-        , so_called_common_logic_amusement_consts :: value \
-        )
+#define shy_bind_module_helper(module) so_called_loadable_consts_binder_helper_module ( module )
+#define shy_bind_value_helper(value) so_called_loadable_consts_binder_helper_value ( logic_amusement , value )
 
 void shy_loadable_consts_reflection_logic_amusement :: prepare ( )
 {
@@ -17,6 +8,3 @@ void shy_loadable_consts_reflection_logic_amusement :: prepare ( )
     shy_bind_value_helper ( renderer_clear_color_g ) ;
     shy_bind_value_helper ( renderer_clear_color_b ) ;
 }
-
-#undef shy_bind_module_helper
-#undef shy_bind_value_helper
--- src_refactored/loadable/consts/reflection/logic_amusement/shy_logic_amusement_injections.cpp
+++ src_refactored/loadable/consts/reflection/logic_amusement/shy_logic_amusement_injections.cpp
@@ -1,6 +1,7 @@
 #ifdef shy_build_loadable_way
 
-    #include "../../binder/shy_binder_injections.h"
+    #include "../../binder/helper/module/shy_module_injections.h"
+    #include "../../binder/helper/value/shy_value_injections.h"
 
     #include "../../../../common/logic/amusement/consts/shy_consts_injections.h"
 
