c116baa42cef30391a436181b4c5f636aaca2737
olegus8 <olegus8@localhost>
2011-06-07 13:30:39 +0000

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/sender/generate/shy_generate.hpp
+++ src/common/logic/salutation/letters/sender/generate/shy_generate.hpp
@@ -1,3 +1,4 @@
-void shy_sender_common_logic_salutation_letters_generate :: send ( so_called_message_common_logic_salutation_letters_generate )
+void shy_sender_common_logic_salutation_letters_generate :: send ( so_called_message_common_logic_salutation_letters_generate msg )
 {
+    so_called_common_logic_salutation_letters_generator :: receive ( msg ) ;
 }
