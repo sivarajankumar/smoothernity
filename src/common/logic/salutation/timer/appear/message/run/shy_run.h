658faff29976bdddfe26bd7ccfac04c5f5287ba4
olegus8 <olegus8@localhost>
2011-06-09 15:13:24 +0000

    transforming logic title into logic salutation

---
+++ src/common/logic/salutation/timer/appear/message/run/shy_run.h
@@ -0,0 +1,3 @@
+class shy_message_common_logic_salutation_timer_appear_run
+{
+} ;
--- src/loadable/fsm/reflection/logic_application/shy_logic_application.hpp
+++ src/loadable/fsm/reflection/logic_application/shy_logic_application.hpp
@@ -28,6 +28,7 @@ void shy_loadable_fsm_reflection_logic_application :: prepare ( )
     shy_guts_bind_action ( logic_salutation_letters_text_generator_generate ) ;
     shy_guts_bind_action ( logic_salutation_render ) ;
     shy_guts_bind_action ( logic_salutation_renderer_render ) ;
+    shy_guts_bind_action ( logic_salutation_timer_appear_run ) ;
     shy_guts_bind_action ( logic_salutation_timer_disappear_run ) ;
     shy_guts_bind_action ( logic_salutation_timer_update ) ;
     shy_guts_bind_action ( logic_salutation_update ) ;
