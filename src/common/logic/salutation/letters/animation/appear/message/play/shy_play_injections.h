a661429fd70db56962bbde8326b1d07d72f82a8e
olegus8 <olegus8@localhost>
2011-06-09 15:31:23 +0000

    transforming logic title into logic salutation

---
+++ src/common/logic/salutation/letters/animation/appear/message/play/shy_play_injections.h
@@ -0,0 +1,8 @@
+#ifndef _shy_message_common_logic_salutation_letters_animation_appear_play_injections_included
+#define _shy_message_common_logic_salutation_letters_animation_appear_play_injections_included
+
+#include "./shy_play.h"
+
+typedef shy_message_common_logic_salutation_letters_animation_appear_play so_called_message_common_logic_salutation_letters_animation_appear_play ;
+
+#endif
---
+++ src/common/logic/salutation/letters/animation/appear/sender/play/shy_play.hpp
@@ -0,0 +1,4 @@
+void shy_sender_common_logic_salutation_letters_animation_appear_play :: send ( so_called_message_common_logic_salutation_letters_animation_appear_play msg )
+{
+    so_called_common_logic_salutation_letters_animation_appear :: receive ( msg ) ;
+}
