dad13b3b170e7c4a55140620bb5bfa0501924b0b
olegus8 <olegus8@localhost>
2011-06-14 12:41:59 +0000

    working on action discard

---
+++ src/loadable/fsm/content/type/action_discard/shy_action_discard.h
@@ -0,0 +1,5 @@
+class shy_type_loadable_fsm_content_action_discard
+{
+public :
+    so_called_lib_std_string input ;
+} ;
--- src/loadable/fsm/content/type/actions/shy_actions_injections.h
+++ src/loadable/fsm/content/type/actions/shy_actions_injections.h
@@ -2,6 +2,7 @@
 #define _shy_type_loadable_fsm_content_actions_injections_included
 
 #include "../action_command_container/shy_action_command_container_injections.h"
+#include "../action_discard_container/shy_action_discard_container_injections.h"
 #include "../action_do_container/shy_action_do_container_injections.h"
 
 #include "./shy_actions.h"
