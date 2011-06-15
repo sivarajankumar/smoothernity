7a1a62a445d25b9aad80a33d470c76e9be3f8135
olegus8 <olegus8@localhost>
2011-06-15 16:57:45 +0000

    working on action discard

--- src/loadable/fsm/behaviour/trace/shy_trace.h
+++ src/loadable/fsm/behaviour/trace/shy_trace.h
@@ -2,6 +2,7 @@ class shy_loadable_fsm_behaviour_trace
 {
 public :
     static void machine_state_action_command ( so_called_lib_std_string , so_called_lib_std_string , so_called_lib_std_string , so_called_lib_std_string ) ;
+    static void machine_state_action_discard ( so_called_lib_std_string , so_called_lib_std_string , so_called_lib_std_string ) ;
     static void machine_state_action_do ( so_called_lib_std_string , so_called_lib_std_string , so_called_lib_std_string ) ;
     static void machine_state_on_entry ( so_called_lib_std_string , so_called_lib_std_string ) ;
     static void machine_state_on_exit ( so_called_lib_std_string , so_called_lib_std_string ) ;
