5fcbd26816ddb6644c86e7ece78f64a9efa5561b
olegus8 <olegus8@localhost>
2011-06-16 14:08:28 +0000

    working on logic blanket initialization

--- src/common/logic/blanket/shy_blanket.hpp
+++ src/common/logic/blanket/shy_blanket.hpp
@@ -6,6 +6,11 @@ namespace shy_guts
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_blanket > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
+void _shy_common_logic_blanket :: receive ( so_called_message_common_init )
+{
+    shy_guts :: created = so_called_platform_math_consts :: whole_false ;
+}
+
 void _shy_common_logic_blanket :: receive ( so_called_message_common_logic_blanket_creation_permit )
 {
     so_called_sender_common_logic_blanket_mesh_create :: send ( so_called_message_common_logic_blanket_mesh_create ( ) ) ;
--- src/common/logic/blanket/shy_blanket_injections.h
+++ src/common/logic/blanket/shy_blanket_injections.h
@@ -4,7 +4,7 @@
 #include "./message/creation_permit/shy_creation_permit_injections.h"
 #include "./message/mesh_creation_finished/shy_mesh_creation_finished_injections.h"
 #include "./message/update/shy_update_injections.h"
-
+#include "../../message/init/shy_init_injections.h"
 #include "../../../injections/platform/scheduler/shy_scheduler.h"
 
 #include "./shy_blanket.h"
