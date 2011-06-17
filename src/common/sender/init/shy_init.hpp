a0757ecadfc9262d5035a9ce44b9b2599f5a6907
olegus8 <olegus8@localhost>
2011-06-17 13:22:12 +0000

    working on logic perspective initialization

--- src/common/sender/init/shy_init.hpp
+++ src/common/sender/init/shy_init.hpp
@@ -63,6 +63,7 @@ void shy_sender_common_init :: send ( so_called_message_common_init msg )
     so_called_common_logic_observer_animation :: receive ( msg ) ;
     so_called_common_logic_observer_animation_flight :: receive ( msg ) ;
     so_called_common_logic_ortho :: receive ( msg ) ;
+    so_called_common_logic_perspective :: receive ( msg ) ;
     so_called_common_logic_sound :: receive ( msg ) ;
     so_called_common_logic_text :: receive ( msg ) ;
     so_called_common_logic_title :: receive ( msg ) ;
