89c772f27917acb92b1a442e80df2f2b80402f13
olegus8 <olegus8@localhost>
2011-06-17 12:30:58 +0000

    working on logic main menu initialization

--- src/common/logic/main_menu/selection/animation/push_attention/shy_push_attention_injections.h
+++ src/common/logic/main_menu/selection/animation/push_attention/shy_push_attention_injections.h
@@ -2,10 +2,9 @@
 #define _shy_common_logic_main_menu_selection_animation_push_attention_injections_included
 
 #include "../message/push_attention_transform_request/shy_push_attention_transform_request_injections.h"
-
 #include "../../../message/launch_permit/shy_launch_permit_injections.h"
 #include "../../../message/update/shy_update_injections.h"
-
+#include "../../../../../message/init/shy_init_injections.h"
 #include "../../../../../../injections/platform/scheduler/shy_scheduler.h"
 
 #include "./shy_push_attention.h"
