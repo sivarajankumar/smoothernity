a4afdb6d9e55c18707d8f1a1aad12ad9a7fddb16
olegus8 <olegus8@localhost>
2011-06-17 12:42:08 +0000

    working on logic main menu initialization

--- src/common/logic/main_menu/selection/animation/unselect/shy_unselect_injections.h
+++ src/common/logic/main_menu/selection/animation/unselect/shy_unselect_injections.h
@@ -3,9 +3,8 @@
 
 #include "../message/unselect_start/shy_unselect_start_injections.h"
 #include "../message/unselect_transform_request/shy_unselect_transform_request_injections.h"
-
 #include "../../../message/update/shy_update_injections.h"
-
+#include "../../../../../message/init/shy_init_injections.h"
 #include "../../../../../../injections/platform/scheduler/shy_scheduler.h"
 
 #include "./shy_unselect.h"
