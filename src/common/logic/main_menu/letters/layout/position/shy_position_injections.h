2e3b8859c14e4afeb7c10edc611adf100e4cee39
olegus8 <olegus8@localhost>
2011-06-17 11:12:46 +0000

    working on logic main menu initialization

--- src/common/logic/main_menu/letters/layout/position/shy_position_injections.h
+++ src/common/logic/main_menu/letters/layout/position/shy_position_injections.h
@@ -2,12 +2,10 @@
 #define _shy_common_logic_main_menu_letters_layout_position_injections_included
 
 #include "../message/position_request/shy_position_request_injections.h"
-
 #include "../../message/boundaries_reply/shy_boundaries_reply_injections.h"
 #include "../../message/cols_reply/shy_cols_reply_injections.h"
-
 #include "../../../../../engine/render/message/aspect_reply/shy_aspect_reply_injections.h"
-
+#include "../../../../../message/init/shy_init_injections.h"
 #include "../../../../../../injections/platform/scheduler/shy_scheduler.h"
 
 #include "./shy_position.h"
