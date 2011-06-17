621fb29fbe562906a7aed3cdaafdc757c55d8537
olegus8 <olegus8@localhost>
2011-06-17 13:10:52 +0000

    working on logic observer initialization

--- src/common/sender/init/shy_init.hpp
+++ src/common/sender/init/shy_init.hpp
@@ -60,6 +60,7 @@ void shy_sender_common_init :: send ( so_called_message_common_init msg )
     so_called_common_logic_main_menu_selection_mesh :: receive ( msg ) ;
     so_called_common_logic_main_menu_selection_tracker :: receive ( msg ) ;
     so_called_common_logic_main_menu_selection_tracking_director :: receive ( msg ) ;
+    so_called_common_logic_observer_animation :: receive ( msg ) ;
     so_called_common_logic_observer_animation_flight :: receive ( msg ) ;
     so_called_common_logic_sound :: receive ( msg ) ;
     so_called_common_logic_text :: receive ( msg ) ;
