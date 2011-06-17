5cea1ebad108f203fc438b907933c3324ff74527
olegus8 <olegus8@localhost>
2011-06-17 12:50:29 +0000

    working on logic main menu initialization

--- src/common/sender/init/shy_init.hpp
+++ src/common/sender/init/shy_init.hpp
@@ -47,6 +47,7 @@ void shy_sender_common_init :: send ( so_called_message_common_init msg )
     so_called_common_logic_main_menu_letters_meshes_storage :: receive ( msg ) ;
     so_called_common_logic_main_menu_letters_storage :: receive ( msg ) ;
     so_called_common_logic_main_menu_renderer :: receive ( msg ) ;
+    so_called_common_logic_main_menu_selection_animation :: receive ( msg ) ;
     so_called_common_logic_main_menu_selection_animation_appear :: receive ( msg ) ;
     so_called_common_logic_main_menu_selection_animation_disappear :: receive ( msg ) ;
     so_called_common_logic_main_menu_selection_animation_idle :: receive ( msg ) ;
