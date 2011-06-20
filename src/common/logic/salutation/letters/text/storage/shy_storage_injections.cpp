5a38983efe0829eafea4cffa26bf0686a135e788
olegus8 <olegus8@localhost>
2011-06-20 13:12:22 +0000

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/text/storage/shy_storage_injections.cpp
+++ src/common/logic/salutation/letters/text/storage/shy_storage_injections.cpp
@@ -1,2 +1,11 @@
 #include "./shy_storage_injections.h"
+
+#include "../../../../../../injections/platform/conditions/shy_conditions.h"
+#include "../../../../../../injections/platform/math/consts/shy_consts.h"
+#include "../../../../../../injections/platform/math/shy_math.h"
+#include "../../../../../../injections/platform/pointer/shy_pointer.h"
+#include "../../../../../../injections/platform/pointer/type/data/shy_data.h"
+#include "../../../../../../injections/platform/static_array/shy_static_array.h"
+#include "../../../../../../injections/platform/static_array/type/data/shy_data.h"
+
 #include "./shy_storage.hpp"
