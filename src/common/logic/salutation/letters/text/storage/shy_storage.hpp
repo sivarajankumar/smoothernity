0ef6112e3150b003ae69b4c9f8a5711a81a34838
olegus8 <olegus8@localhost>
2011-06-20 13:34:17 +0000

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/text/storage/shy_storage.hpp
+++ src/common/logic/salutation/letters/text/storage/shy_storage.hpp
@@ -6,13 +6,7 @@ namespace shy_guts
         so_called_type_common_logic_text_letter_id letter ;
     } ;
 
-    namespace consts
-    {
-        static so_called_type_platform_math_const_int_32 max_entries_int = 32 ;
-        static so_called_type_platform_math_num_whole max_entries = so_called_platform_math :: init_num_whole ( max_entries_int ) ;
-    }
-
-    static so_called_type_platform_static_array_data < type_entry , shy_guts :: consts :: max_entries_int > entries_data ;
+    static so_called_type_platform_static_array_data < type_entry , so_called_common_logic_salutation_letters_consts :: max_letters > entries_data ;
     so_called_type_platform_math_num_whole entries_count ;
 }
 
@@ -26,7 +20,9 @@ void _shy_common_logic_salutation_letters_text_storage :: receive ( so_called_me
 
 void _shy_common_logic_salutation_letters_text_storage :: receive ( so_called_message_common_logic_salutation_letters_text_storage_add_letter msg )
 {
-    if ( so_called_platform_conditions :: whole_less_than_whole ( shy_guts :: entries_count , shy_guts :: consts :: max_entries ) )
+    so_called_type_platform_math_num_whole whole_max_entries ;
+    so_called_platform_math :: make_num_whole ( whole_max_entries , so_called_common_logic_salutation_letters_consts :: max_letters ) ;
+    if ( so_called_platform_conditions :: whole_less_than_whole ( shy_guts :: entries_count , whole_max_entries ) )
     {
         so_called_type_platform_pointer_data < shy_guts :: type_entry > entry ;
         so_called_platform_static_array :: element_ptr ( entry , shy_guts :: entries_data , shy_guts :: entries_count ) ;
