45aba230b218eba902b188b3f0e4c723207461c1
olegus8 <olegus8@localhost>
2011-06-21 14:27:00 +0000

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/meshes/generator/shy_generator_injections.cpp
+++ src/common/logic/salutation/letters/meshes/generator/shy_generator_injections.cpp
@@ -1,8 +1,11 @@
 #include "./shy_generator_injections.h"
 
 #include "./sender/generate_finished/shy_generate_finished_injections.h"
+#include "../consts/shy_consts_injections.h"
 #include "../../text/storage/sender/size_request/shy_size_request_injections.h"
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
+#include "../../../../../../injections/platform/consts/shy_consts.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
+#include "../../../../../../injections/platform/math/shy_math.h"
 
 #include "./shy_generator.hpp"
