7d6d7b2df7597c43313d35c22f02a2731249bfd4
olegus8 <olegus8@localhost>
2011-06-22 15:26:53 +0000

    working on logic text letter mesh

--- src/common/logic/text/sender/letter_big_tex_coords_reply/shy_letter_big_tex_coords_reply_injections.cpp
+++ src/common/logic/text/sender/letter_big_tex_coords_reply/shy_letter_big_tex_coords_reply_injections.cpp
@@ -1,6 +1,6 @@
 #include "./shy_letter_big_tex_coords_reply_injections.h"
 
-#include "../../../main_menu/letters/meshes/creator/shy_creator_injections.h"
+#include "../../letter/mesh/shy_mesh_injections.h"
 #include "../../../title/shy_title_injections.h"
 
 #include "./shy_letter_big_tex_coords_reply.hpp"
