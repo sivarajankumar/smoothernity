a29731f4bee6309e7a33d213d5a16aebdb6a4598
olegus8 <olegus8@localhost>
2011-06-22 12:26:24 +0000

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/meshes/shaper/shy_shaper_injections.cpp
+++ src/common/logic/salutation/letters/meshes/shaper/shy_shaper_injections.cpp
@@ -1,6 +1,7 @@
 #include "./shy_shaper_injections.h"
 
 #include "./sender/shape_reply/shy_shape_reply_injections.h"
+#include "../../text/storage/sender/letter_request/shy_letter_request_injections.h"
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
 
---
+++ src/common/logic/salutation/letters/text/storage/message/letter_reply/shy_letter_reply.h
@@ -0,0 +1,6 @@
+class shy_message_common_logic_salutation_letters_text_storage_letter_reply
+{
+public :
+    so_called_type_common_logic_text_letter_id letter_id ;
+    so_called_type_platform_math_num_whole letter_index ;
+} ;
