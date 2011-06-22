dae0d96adc6296bfe6bfce79790b18810574b074
olegus8 <olegus8@localhost>
2011-06-22 15:32:36 +0000

    working on logic text letter mesh

--- src/common/engine/render/stateless/shy_stateless_injections.h
+++ src/common/engine/render/stateless/shy_stateless_injections.h
@@ -1,6 +1,7 @@
 #ifndef _shy_common_engine_render_stateless_injections_included
 #define _shy_common_engine_render_stateless_injections_included
 
+#include "../type/mesh_id/shy_mesh_id_injections.h"
 #include "../../../../injections/platform/math/type/num_fract/shy_num_fract.h"
 #include "../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
 #include "../../../../injections/platform/render/texture_loader/type/resource_id/shy_resource_id.h"
