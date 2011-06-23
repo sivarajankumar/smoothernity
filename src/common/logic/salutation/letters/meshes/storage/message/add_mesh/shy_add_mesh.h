81af57af58bcf0d89a8a5aab9adf21cf2c518b7d
olegus8 <olegus8@localhost>
2011-06-23 11:52:28 +0000

    transforming logic title into logic salutation

---
+++ src/common/logic/salutation/letters/meshes/storage/message/add_mesh/shy_add_mesh.h
@@ -0,0 +1,5 @@
+class shy_message_common_logic_salutation_letters_meshes_storage_add_mesh
+{
+public :
+    so_called_type_common_engine_render_mesh_id mesh ;
+} ;
---
+++ src/common/logic/salutation/letters/meshes/storage/shy_storage.h
@@ -0,0 +1,12 @@
+class _shy_common_logic_salutation_letters_meshes_storage
+{
+public :
+    static void receive ( so_called_message_common_init ) ;
+    static void receive ( so_called_message_common_logic_salutation_letters_meshes_storage_add_mesh ) ;
+    static void register_in_scheduler ( ) ;
+} ;
+
+typedef so_called_platform_scheduler :: scheduled_context
+    < _shy_common_logic_salutation_letters_meshes_storage 
+    > :: module
+    shy_common_logic_salutation_letters_meshes_storage_scheduled ;
