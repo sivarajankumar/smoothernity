ebefb90460e97e43d4d24a7315834c69424dc830
olegus8 <olegus8@localhost>
2011-07-06 16:20:32 +0000

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/text/storage/shy_storage.hpp
+++ src/common/logic/salutation/letters/text/storage/shy_storage.hpp
@@ -11,12 +11,44 @@ namespace shy_guts
         static const so_called_type_platform_math_num_whole trace_enabled = so_called_platform_math_consts :: whole_true ;
     }
 
+    static void trace_entries_in_use ( ) ;
+    static void trace_entries_overflow_error ( ) ;
+    static void trace_entry_index_is_out_of_range_error ( so_called_type_platform_math_num_whole ) ;
+
     static so_called_type_platform_static_array_data < type_entry , so_called_common_logic_salutation_letters_consts :: max_letters_int > entries_data ;
     static so_called_type_platform_math_num_whole entries_count ;
 }
 
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_salutation_letters_text_storage > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: trace_entries_in_use ( )
+{
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: consts :: trace_enabled ) )
+    {
+        so_called_trace_common_logic_salutation_letters_text_storage :: entries_in_use
+            ( shy_guts :: entries_count
+            , so_called_common_logic_salutation_letters_consts :: max_letters
+            ) ;
+    }
+}
+
+void shy_guts :: trace_entries_overflow_error ( )
+{
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: consts :: trace_enabled ) )
+        so_called_trace_common_logic_salutation_letters_text_storage :: entries_overflow_error ( ) ;
+}
+
+void shy_guts :: trace_entry_index_is_out_of_range_error ( so_called_type_platform_math_num_whole index )
+{
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: consts :: trace_enabled ) )
+    {
+        so_called_trace_common_logic_salutation_letters_text_storage :: entry_index_is_out_of_range_error
+            ( index
+            , shy_guts :: entries_count
+            ) ;
+    }
+}
 
 void _shy_common_logic_salutation_letters_text_storage :: receive ( so_called_message_common_init )
 {
@@ -31,12 +63,16 @@ void _shy_common_logic_salutation_letters_text_storage :: receive ( so_called_me
         so_called_platform_static_array :: element_ptr ( entry , shy_guts :: entries_data , shy_guts :: entries_count ) ;
         entry . get ( ) . letter = msg . letter ;
         so_called_platform_math :: inc_whole ( shy_guts :: entries_count ) ;
+        shy_guts :: trace_entries_in_use ( ) ;
     }
+    else
+        shy_guts :: trace_entries_overflow_error ( ) ;
 }
 
 void _shy_common_logic_salutation_letters_text_storage :: receive ( so_called_message_common_logic_salutation_letters_text_storage_clean )
 {
     shy_guts :: entries_count = so_called_platform_math_consts :: whole_0 ;
+    shy_guts :: trace_entries_in_use ( ) ;
 }
 
 void _shy_common_logic_salutation_letters_text_storage :: receive ( so_called_message_common_logic_salutation_letters_text_storage_letter_request msg )
@@ -49,7 +85,10 @@ void _shy_common_logic_salutation_letters_text_storage :: receive ( so_called_me
         letter_id = entry . get ( ) . letter ;
     }
     else
+    {
         letter_id = so_called_common_logic_text_consts :: whitespace ;
+        shy_guts :: trace_entry_index_is_out_of_range_error ( msg . letter_index ) ;
+    }
 
     so_called_message_common_logic_salutation_letters_text_storage_letter_reply reply_msg ;
     reply_msg . letter_index = msg . letter_index ;
---
+++ src/trace/common/logic/salutation/letters/text/storage/worker/shy_worker.h
@@ -0,0 +1,7 @@
+class shy_trace_common_logic_salutation_letters_text_storage_worker
+{
+public :
+    static void entries_in_use ( so_called_type_platform_math_num_whole , so_called_type_platform_math_num_whole ) ;
+    static void entries_overflow_error ( ) ;
+    static void entry_index_is_out_of_range_error ( so_called_type_platform_math_num_whole , so_called_type_platform_math_num_whole ) ;
+} ;
