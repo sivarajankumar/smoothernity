ea671ad27bd1d9c15f2b37a24ca94e3a20181151
olegus8 <olegus8@localhost>
2011-07-06 15:50:22 +0000

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/consts/shy_consts_injections.h
+++ src/common/logic/salutation/letters/consts/shy_consts_injections.h
@@ -2,6 +2,7 @@
 #define _shy_common_logic_salutation_letters_consts_injections_included
 
 #include "../../../../../injections/platform/math/type/const_int_32/shy_const_int_32.h"
+#include "../../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
 
 #include "./shy_consts.h"
 
