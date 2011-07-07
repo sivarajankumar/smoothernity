a68f35bd96b3a71cf1d36f3a714362dd70112e76
olegus8 <olegus8@localhost>
2011-07-07 12:31:52 +0000

    working on loadable parser traces

--- src/trace/loadable/parser/worker/shy_worker.hpp
+++ src/trace/loadable/parser/worker/shy_worker.hpp
@@ -109,6 +109,19 @@ void shy_trace_loadable_parser_worker :: expected_consts_or_system_instead_of_to
     so_called_platform_trace :: trace_end ( ) ;
 }
 
+void shy_trace_loadable_parser_worker :: expected_denominator_instead_of_token_error
+    ( const so_called_lib_std_char * token
+    )
+{
+    so_called_platform_trace :: trace_begin ( shy_guts :: consts :: module_name ) ;
+    so_called_platform_trace :: trace_string_error ( "Error. Expected denominator, but got " ) ;
+    so_called_platform_trace :: trace_string_name_error ( "\"" ) ;
+    so_called_platform_trace :: trace_string_name_error ( token ) ;
+    so_called_platform_trace :: trace_string_name_error ( "\"" ) ;
+    so_called_platform_trace :: trace_string_error ( " instead." ) ;
+    so_called_platform_trace :: trace_end ( ) ;
+}
+
 void shy_trace_loadable_parser_worker :: whole_line_containing_error
     ( const so_called_lib_std_char * line
     )
