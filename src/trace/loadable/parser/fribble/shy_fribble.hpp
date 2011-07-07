3369b354521f84765032546a824cc576880b712e
olegus8 <olegus8@localhost>
2011-07-07 14:01:56 +0000

    working on loadable parser traces

--- src/trace/loadable/parser/fribble/shy_fribble.hpp
+++ src/trace/loadable/parser/fribble/shy_fribble.hpp
@@ -62,6 +62,10 @@ void shy_trace_loadable_parser_fribble :: expected_machine_or_command_instead_of
 {
 }
 
+void shy_trace_loadable_parser_fribble :: expected_machine_or_system_or_consts_instead_of_token_error ( const so_called_lib_std_char * )
+{
+}
+
 void shy_trace_loadable_parser_fribble :: whole_line_containing_error ( const so_called_lib_std_char * )
 {
 }
