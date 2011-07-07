2f0a8b7d8ef6ba31c04e7ef52c10bf5f60e77432
olegus8 <olegus8@localhost>
2011-07-07 13:54:30 +0000

    working on loadable parser traces

--- src/trace/loadable/parser/worker/shy_worker.hpp
+++ src/trace/loadable/parser/worker/shy_worker.hpp
@@ -247,6 +247,23 @@ void shy_trace_loadable_parser_worker :: expected_machine_name_instead_of_token_
     so_called_platform_trace :: trace_end ( ) ;
 }
 
+void shy_trace_loadable_parser_worker :: expected_machine_or_command_instead_of_token_error
+    ( const so_called_lib_std_char * token
+    )
+{
+    so_called_platform_trace :: trace_begin ( shy_guts :: consts :: module_name ) ;
+    so_called_platform_trace :: trace_string_error ( "Error. Expected " ) ;
+    so_called_platform_trace :: trace_string_name_error ( "\"machine\"" ) ;
+    so_called_platform_trace :: trace_string_error ( " or " ) ;
+    so_called_platform_trace :: trace_string_name_error ( "\"command\"" ) ;
+    so_called_platform_trace :: trace_string_error ( ", but got " ) ;
+    so_called_platform_trace :: trace_string_name_error ( "\"" ) ;
+    so_called_platform_trace :: trace_string_name_error ( token ) ;
+    so_called_platform_trace :: trace_string_name_error ( "\"" ) ;
+    so_called_platform_trace :: trace_string_error ( " instead." ) ;
+    so_called_platform_trace :: trace_end ( ) ;
+}
+
 void shy_trace_loadable_parser_worker :: whole_line_containing_error
     ( const so_called_lib_std_char * line
     )
