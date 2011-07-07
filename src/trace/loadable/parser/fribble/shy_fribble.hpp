db8fb65d0469958bfbbe36fe1aec9dab9bbfd448
olegus8 <olegus8@localhost>
2011-07-07 12:41:25 +0000

    working on loadable parser traces

--- src/trace/loadable/parser/fribble/shy_fribble.hpp
+++ src/trace/loadable/parser/fribble/shy_fribble.hpp
@@ -30,6 +30,10 @@ void shy_trace_loadable_parser_fribble :: expected_denominator_instead_of_token_
 {
 }
 
+void shy_trace_loadable_parser_fribble :: expected_divide_or_identifier_instead_of_token_error ( const so_called_lib_std_char * )
+{
+}
+
 void shy_trace_loadable_parser_fribble :: whole_line_containing_error ( const so_called_lib_std_char * )
 {
 }
