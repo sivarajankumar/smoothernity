54afc4382f82141bd234e8f3e13a42571a13264e
olegus8 <olegus8@localhost>
2011-07-07 15:25:54 +0000

    working on loadable parser traces

--- src/loadable/parser/shy_parser.hpp
+++ src/loadable/parser/shy_parser.hpp
@@ -85,7 +85,6 @@ namespace shy_guts
     namespace errors
     {
         static void remove_me_after_refactoring ( so_called_lib_std_string & ) ;
-        static void expected_system_name_instead_of ( so_called_lib_std_string & , so_called_lib_std_string ) ;
         static void expected_to_instead_of ( so_called_lib_std_string & , so_called_lib_std_string ) ;
         static void unknown_fract_attribute_in_module ( so_called_lib_std_string & , so_called_lib_std_string attribute , so_called_lib_std_string ) ;
         static void unknown_fsm_action ( so_called_lib_std_string & , so_called_lib_std_string , so_called_lib_std_string ) ;
@@ -215,11 +214,6 @@ namespace shy_guts
 void shy_guts :: errors :: remove_me_after_refactoring ( so_called_lib_std_string & error )
 {
     error = so_called_lib_std_string ( "dummy error" ) ;
-}
-
-void shy_guts :: errors :: expected_system_name_instead_of ( so_called_lib_std_string & error , so_called_lib_std_string token )
-{
-    error = so_called_lib_std_string ( "expected system name, but got '" ) + token + so_called_lib_std_string ( "'" ) ;
 }
 
 void shy_guts :: errors :: expected_to_instead_of ( so_called_lib_std_string & error , so_called_lib_std_string token ) 
@@ -548,9 +542,13 @@ void shy_guts :: handle_state_reading_system_name ( )
     }
     else
     {
+        if ( shy_guts :: consts :: trace_errors )
+            so_called_trace_loadable_parser :: expected_system_name_instead_of_token_error ( shy_guts :: token . c_str ( ) ) ;
+
         so_called_lib_std_string error ;
-        shy_guts :: errors :: expected_system_name_instead_of ( error , shy_guts :: token ) ;
+        shy_guts :: errors :: remove_me_after_refactoring ( error ) ;
         shy_guts :: store_error ( error ) ;
+
         shy_guts :: state = shy_guts :: state_error ;
     }
 }
--- src/trace/loadable/parser/worker/shy_worker.hpp
+++ src/trace/loadable/parser/worker/shy_worker.hpp
@@ -383,6 +383,19 @@ void shy_trace_loadable_parser_worker :: expected_state_or_machine_or_system_or_
     so_called_platform_trace :: trace_end ( ) ;
 }
 
+void shy_trace_loadable_parser_worker :: expected_system_name_instead_of_token_error
+    ( const so_called_lib_std_char * token
+    )
+{
+    so_called_platform_trace :: trace_begin ( shy_guts :: consts :: module_name ) ;
+    so_called_platform_trace :: trace_string_error ( "Error. Expected system name, but got " ) ;
+    so_called_platform_trace :: trace_string_name_error ( "\"" ) ;
+    so_called_platform_trace :: trace_string_name_error ( token ) ;
+    so_called_platform_trace :: trace_string_name_error ( "\"" ) ;
+    so_called_platform_trace :: trace_string_error ( " instead." ) ;
+    so_called_platform_trace :: trace_end ( ) ;
+}
+
 void shy_trace_loadable_parser_worker :: whole_line_containing_error
     ( const so_called_lib_std_char * line
     )
