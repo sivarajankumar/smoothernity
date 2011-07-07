f6dbe7718bf2302690ebc2026ed02c00e752abb9
olegus8 <olegus8@localhost>
2011-07-07 14:22:35 +0000

    working on loadable parser traces

--- src/trace/loadable/parser/fribble/shy_fribble.hpp
+++ src/trace/loadable/parser/fribble/shy_fribble.hpp
@@ -70,6 +70,10 @@ void shy_trace_loadable_parser_fribble :: expected_module_name_instead_of_token_
 {
 }
 
+void shy_trace_loadable_parser_fribble :: expected_numerator_instead_of_token_error ( const so_called_lib_std_char * )
+{
+}
+
 void shy_trace_loadable_parser_fribble :: whole_line_containing_error ( const so_called_lib_std_char * )
 {
 }
