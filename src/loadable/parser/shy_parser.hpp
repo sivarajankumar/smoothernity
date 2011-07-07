f707474f8153dcaf0d7d919f63f584035a6e171e
olegus8 <olegus8@localhost>
2011-07-07 15:31:44 +0000

    working on loadable parser traces

--- src/loadable/parser/shy_parser.hpp
+++ src/loadable/parser/shy_parser.hpp
@@ -85,7 +85,6 @@ namespace shy_guts
     namespace errors
     {
         static void remove_me_after_refactoring ( so_called_lib_std_string & ) ;
-        static void expected_to_instead_of ( so_called_lib_std_string & , so_called_lib_std_string ) ;
         static void unknown_fract_attribute_in_module ( so_called_lib_std_string & , so_called_lib_std_string attribute , so_called_lib_std_string ) ;
         static void unknown_fsm_action ( so_called_lib_std_string & , so_called_lib_std_string , so_called_lib_std_string ) ;
         static void unknown_fsm_input ( so_called_lib_std_string & , so_called_lib_std_string , so_called_lib_std_string ) ;
@@ -214,11 +213,6 @@ namespace shy_guts
 void shy_guts :: errors :: remove_me_after_refactoring ( so_called_lib_std_string & error )
 {
     error = so_called_lib_std_string ( "dummy error" ) ;
-}
-
-void shy_guts :: errors :: expected_to_instead_of ( so_called_lib_std_string & error , so_called_lib_std_string token ) 
-{
-    error = so_called_lib_std_string ( "expected 'to', but got '" ) + token + so_called_lib_std_string ( "'" ) ;
 }
 
 void shy_guts :: errors :: unknown_fract_attribute_in_module ( so_called_lib_std_string & error , so_called_lib_std_string attribute , so_called_lib_std_string module )
@@ -880,9 +874,13 @@ void shy_guts :: handle_state_reading_action_command_to_token ( )
     }
     else
     {
+        if ( shy_guts :: consts :: trace_errors )
+            so_called_trace_loadable_parser :: expected_to_instead_of_token_error ( shy_guts :: token . c_str ( ) ) ;
+
         so_called_lib_std_string error ;
-        shy_guts :: errors :: expected_to_instead_of ( error , shy_guts :: token ) ;
+        shy_guts :: errors :: remove_me_after_refactoring ( error ) ;
         shy_guts :: store_error ( error ) ;
+
         shy_guts :: state = shy_guts :: state_error ;
     }
 }
