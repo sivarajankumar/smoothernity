4791eabb5b3483cfa2162be56a6a0730e05456f4
olegus8 <olegus8@localhost>
2011-07-07 14:22:35 +0000

    working on loadable parser traces

--- src/trace/loadable/parser/worker/shy_worker.hpp
+++ src/trace/loadable/parser/worker/shy_worker.hpp
@@ -296,6 +296,19 @@ void shy_trace_loadable_parser_worker :: expected_module_name_instead_of_token_e
     so_called_platform_trace :: trace_end ( ) ;
 }
 
+void shy_trace_loadable_parser_worker :: expected_numerator_instead_of_token_error
+    ( const so_called_lib_std_char * token
+    )
+{
+    so_called_platform_trace :: trace_begin ( shy_guts :: consts :: module_name ) ;
+    so_called_platform_trace :: trace_string_error ( "Error. Expected numerator, but got " ) ;
+    so_called_platform_trace :: trace_string_name_error ( "\"" ) ;
+    so_called_platform_trace :: trace_string_name_error ( token ) ;
+    so_called_platform_trace :: trace_string_name_error ( "\"" ) ;
+    so_called_platform_trace :: trace_string_error ( " instead." ) ;
+    so_called_platform_trace :: trace_end ( ) ;
+}
+
 void shy_trace_loadable_parser_worker :: whole_line_containing_error
     ( const so_called_lib_std_char * line
     )
