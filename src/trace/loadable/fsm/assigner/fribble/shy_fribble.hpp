3487847f27e25e6cbaf765ce0847594fe9ffb0a1
olegus8 <olegus8@localhost>
2011-07-08 12:34:34 +0000

    working on loadable fsm assigner traces

---
+++ src/trace/loadable/fsm/assigner/fribble/shy_fribble.hpp
@@ -0,0 +1,3 @@
+void shy_trace_loadable_fsm_assigner_fribble :: no_initial_state_in_machine_of_system_error ( const so_called_lib_std_char * , const so_called_lib_std_char * )
+{
+}
---
+++ src/trace/loadable/fsm/assigner/worker/shy_worker_injections.h
@@ -0,0 +1,10 @@
+#ifndef _shy_trace_loadable_fsm_assigner_worker_injections_included
+#define _shy_trace_loadable_fsm_assigner_worker_injections_included
+
+#include "../../../../../injections/lib/std/char/shy_char.h"
+
+#include "./shy_worker.h"
+
+typedef shy_trace_loadable_fsm_assigner_worker so_called_trace_loadable_fsm_assigner_worker ;
+
+#endif
