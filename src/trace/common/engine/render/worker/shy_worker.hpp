de80c55f6933bd66a333e07831fc944a1f2dcc8f
olegus8 <olegus8@localhost>
2011-07-08 15:30:37 +0000

    stabilizing render

--- src/trace/common/engine/render/worker/shy_worker.hpp
+++ src/trace/common/engine/render/worker/shy_worker.hpp
@@ -34,6 +34,69 @@ void shy_trace_common_engine_render_worker :: meshes_underflow_error ( )
     so_called_platform_trace :: trace_end ( ) ;
 }
 
+void shy_trace_common_engine_render_worker :: mesh_index_out_of_range_error
+    ( so_called_type_platform_math_num_whole current
+    , so_called_type_platform_math_num_whole total
+    )
+{
+    so_called_platform_trace :: trace_begin ( shy_guts :: consts :: module_name ) ;
+    so_called_platform_trace :: trace_string_error ( "Error. Mesh index subscript of " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( current ) ;
+    so_called_platform_trace :: trace_string_error ( " exceeds indices count of " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( total ) ;
+    so_called_platform_trace :: trace_string_error ( "." ) ;
+    so_called_platform_trace :: trace_end ( ) ;
+}
+
+void shy_trace_common_engine_render_worker :: mesh_vertex_out_of_range_error
+    ( so_called_type_platform_math_num_whole current
+    , so_called_type_platform_math_num_whole total
+    )
+{
+    so_called_platform_trace :: trace_begin ( shy_guts :: consts :: module_name ) ;
+    so_called_platform_trace :: trace_string_error ( "Error. Mesh vertex subscript of " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( current ) ;
+    so_called_platform_trace :: trace_string_error ( " exceeds vertices count of " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( total ) ;
+    so_called_platform_trace :: trace_string_error ( "." ) ;
+    so_called_platform_trace :: trace_end ( ) ;
+}
+
+void shy_trace_common_engine_render_worker :: mesh_too_many_indices_error
+    ( so_called_type_platform_math_num_whole current
+    , so_called_type_platform_math_num_whole total
+    )
+{
+    so_called_platform_trace :: trace_begin ( shy_guts :: consts :: module_name ) ;
+    so_called_platform_trace :: trace_string_error ( "Error. Requested mesh indices count of " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( current ) ;
+    so_called_platform_trace :: trace_string_error ( " exceeds maximum indices count of " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( total ) ;
+    so_called_platform_trace :: trace_string_error ( "." ) ;
+    so_called_platform_trace :: trace_end ( ) ;
+}
+
+void shy_trace_common_engine_render_worker :: mesh_too_many_vertices_error
+    ( so_called_type_platform_math_num_whole current
+    , so_called_type_platform_math_num_whole total
+    )
+{
+    so_called_platform_trace :: trace_begin ( shy_guts :: consts :: module_name ) ;
+    so_called_platform_trace :: trace_string_error ( "Error. Requested mesh vertices count of " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( current ) ;
+    so_called_platform_trace :: trace_string_error ( " exceeds maximum vertices count of " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( total ) ;
+    so_called_platform_trace :: trace_string_error ( "." ) ;
+    so_called_platform_trace :: trace_end ( ) ;
+}
+
+void shy_trace_common_engine_render_worker :: trying_to_modify_finalized_mesh_error ( )
+{
+    so_called_platform_trace :: trace_begin ( shy_guts :: consts :: module_name ) ;
+    so_called_platform_trace :: trace_string_error ( "Error. Trying to modify mesh that has been already finalized." ) ;
+    so_called_platform_trace :: trace_end ( ) ;
+}
+
 void shy_trace_common_engine_render_worker :: textures_in_use
     ( so_called_type_platform_math_num_whole current 
     , so_called_type_platform_math_num_whole total
