48d868b03931e93fc54b95477f8517aa09628bce
olegus8 <olegus8@localhost>
2011-07-12 15:39:35 +0000

    working on loadable consts limits

--- src/loadable/consts/binder/shy_binder_injections.cpp
+++ src/loadable/consts/binder/shy_binder_injections.cpp
@@ -1,8 +1,6 @@
 #ifdef shy_build_loadable_way
-
-    #include "../content/shy_content_injections.h"
-
     #include "./shy_binder_injections.h"
+    #include "../content/shy_content_injections.h"
+    #include "../../../injections/lib/std/true/shy_true.h"
     #include "./shy_binder.hpp"
-
 #endif
