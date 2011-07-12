dcbf3c5bf984f0b6b224b3801c88b06e2a933caf
olegus8 <olegus8@localhost>
2011-07-12 08:40:10 +0000

    trace refactoring

--- src/trace/loadable/consts/assigner/fribble/shy_fribble.hpp
+++
@@ -1,7 +0,0 @@
-void shy_trace_loadable_consts_assigner_fribble :: no_value_assigned_to_module_attribute_fract_error ( const so_called_lib_std_char * , const so_called_lib_std_char * )
-{
-}
-
-void shy_trace_loadable_consts_assigner_fribble :: no_value_assigned_to_module_attribute_whole_error ( const so_called_lib_std_char * , const so_called_lib_std_char * )
-{
-}
--- src/trace/loadable/consts/assigner/worker/shy_worker_injections.hpp
+++
@@ -1,5 +0,0 @@
-#include "./shy_worker_injections.h"
-
-#include "../../../../../injections/platform/trace/shy_trace.h"
-
-#include "./shy_worker.hpp"
