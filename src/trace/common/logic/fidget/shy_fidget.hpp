06fc0e8e8e4ce2194348a529ef097f3f72f41fad
olegus8 <olegus8@localhost>
2011-07-12 13:30:02 +0000

    stabilizing common logic fidget

--- src/trace/common/logic/fidget/shy_fidget.hpp
+++ src/trace/common/logic/fidget/shy_fidget.hpp
@@ -13,12 +13,12 @@ void shy_trace_common_logic_fidget :: edges_out_of_range_error
     )
 {
     so_called_platform_trace :: trace_begin ( shy_guts :: consts :: module_name ) ;
-    so_called_platform_trace :: trace_string ( "Edges count of " ) ;
-    so_called_platform_trace :: trace_num_whole ( edges ) ;
-    so_called_platform_trace :: trace_string ( " is out of acceptable range from " ) ;
-    so_called_platform_trace :: trace_num_whole ( edges_min ) ;
-    so_called_platform_trace :: trace_string ( " to " ) ;
-    so_called_platform_trace :: trace_num_whole ( edges_max ) ;
-    so_called_platform_trace :: trace_string ( "." ) ;
+    so_called_platform_trace :: trace_string_error ( "Edges count of " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( edges ) ;
+    so_called_platform_trace :: trace_string_error ( " is out of acceptable range from " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( edges_min ) ;
+    so_called_platform_trace :: trace_string_error ( " to " ) ;
+    so_called_platform_trace :: trace_num_whole_error ( edges_max ) ;
+    so_called_platform_trace :: trace_string_error ( "." ) ;
     so_called_platform_trace :: trace_end ( ) ;
 }
