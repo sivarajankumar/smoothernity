2be2751560a70b7036b62efb28e1300f294a9397
olegus8 <olegus8@localhost>
2011-07-12 17:06:31 +0000

    working on loadable consts limits

--- src/platform/trace/cerr/shy_cerr.hpp
+++ src/platform/trace/cerr/shy_cerr.hpp
@@ -3,7 +3,7 @@ namespace shy_guts
     namespace consts
     {
         static const so_called_lib_std_int32_t digits_per_frame = 4 ;
-        static void trace_finished ( so_called_lib_std_string & ) ;
+        static const so_called_lib_std_char divide = '/' ;
         static void trace_module_name_begin ( so_called_lib_std_string & ) ;
         static void trace_module_name_end ( so_called_lib_std_string & ) ;
         static void trace_number_begin ( so_called_lib_std_string & ) ;
@@ -11,7 +11,6 @@ namespace shy_guts
         static void trace_number_error_begin ( so_called_lib_std_string & ) ;
         static void trace_number_error_end ( so_called_lib_std_string & ) ;
         static void trace_stamp ( so_called_lib_std_string & , so_called_lib_std_string , so_called_lib_std_string ) ;
-        static void trace_started ( so_called_lib_std_string & ) ;
         static void trace_string_error_begin ( so_called_lib_std_string & ) ;
         static void trace_string_error_end ( so_called_lib_std_string & ) ;
         static void trace_string_name_begin ( so_called_lib_std_string & ) ;
@@ -96,11 +95,6 @@ void shy_guts :: consts :: trace_string_error_end ( so_called_lib_std_string & r
     result = so_called_platform_terminal_consts :: reset_to_default ;
 }
 
-void shy_guts :: consts :: trace_finished ( so_called_lib_std_string & result )
-{
-    result = "Trace finished." ;
-}
-
 void shy_guts :: consts :: trace_stamp 
     ( so_called_lib_std_string & result
     , so_called_lib_std_string frame
@@ -129,13 +123,6 @@ void shy_guts :: consts :: trace_stamp
     result += "\"" ;
     result += name_end ;
     result += ". " ;
-}
-
-void shy_guts :: consts :: trace_started ( so_called_lib_std_string & result )
-{
-    result . clear ( ) ;
-    result += so_called_platform_terminal_consts :: reset_to_default ;
-    result += "Trace started." ;
 }
 
 void shy_platform_trace_cerr :: trace_begin ( const so_called_lib_std_char * module )
@@ -209,6 +196,42 @@ void shy_platform_trace_cerr :: trace_const_int_32_error ( so_called_type_platfo
 
     so_called_lib_std_cerr << str_begin ;
     so_called_lib_std_cerr << num ;
+    so_called_lib_std_cerr << str_end ;
+}
+
+void shy_platform_trace_cerr :: trace_num_fract 
+    ( so_called_type_platform_math_const_int_32 numerator
+    , so_called_type_platform_math_const_int_32 denominator
+    )
+{
+    so_called_lib_std_string str_begin ;
+    so_called_lib_std_string str_end ;
+
+    shy_guts :: consts :: trace_number_begin ( str_begin ) ;
+    shy_guts :: consts :: trace_number_end ( str_end ) ;
+
+    so_called_lib_std_cerr << str_begin ;
+    so_called_lib_std_cerr << numerator ;
+    so_called_lib_std_cerr << shy_guts :: consts :: divide ;
+    so_called_lib_std_cerr << denominator ;
+    so_called_lib_std_cerr << str_end ;
+}
+
+void shy_platform_trace_cerr :: trace_num_fract_error
+    ( so_called_type_platform_math_const_int_32 numerator
+    , so_called_type_platform_math_const_int_32 denominator
+    )
+{
+    so_called_lib_std_string str_begin ;
+    so_called_lib_std_string str_end ;
+
+    shy_guts :: consts :: trace_number_error_begin ( str_begin ) ;
+    shy_guts :: consts :: trace_number_error_end ( str_end ) ;
+
+    so_called_lib_std_cerr << str_begin ;
+    so_called_lib_std_cerr << numerator ;
+    so_called_lib_std_cerr << shy_guts :: consts :: divide ;
+    so_called_lib_std_cerr << denominator ;
     so_called_lib_std_cerr << str_end ;
 }
 
