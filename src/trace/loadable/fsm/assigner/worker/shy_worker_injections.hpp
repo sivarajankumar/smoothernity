5995f78f74204193743449f10198f74490bf6302
olegus8 <olegus8@localhost>
2011-07-12 08:44:28 +0000

    trace refactoring

--- src/trace/loadable/fsm/assigner/worker/shy_worker_injections.hpp
+++
@@ -1,6 +0,0 @@
-#include "./shy_worker_injections.h"
-
-#include "../../../../../injections/platform/trace/shy_trace.h"
-#include "../../../../../loadable/fsm/consts/shy_consts_injections.h"
-
-#include "./shy_worker.hpp"
