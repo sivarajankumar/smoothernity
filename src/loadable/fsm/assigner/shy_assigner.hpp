e4eb46e0b35a9df78ec6d3a7a664b1f067098368
olegus8 <olegus8@localhost>
2011-07-14 16:22:43 +0000

    loadable fsm stability improved

--- src/loadable/fsm/assigner/shy_assigner.hpp
+++ src/loadable/fsm/assigner/shy_assigner.hpp
@@ -22,22 +22,23 @@ void shy_loadable_fsm_assigner :: assign ( )
 {
     so_called_type_loadable_fsm_content_system_container * system_container = 0 ;
     so_called_loadable_fsm_content :: get_system_container ( system_container ) ;
-    for ( so_called_type_loadable_fsm_content_system_container :: const_iterator fsm_system_i = system_container -> begin ( )
+    for ( so_called_type_loadable_fsm_content_system_container :: iterator fsm_system_i = system_container -> begin ( )
         ; fsm_system_i != system_container -> end ( )
         ; ++ fsm_system_i
         )
     {
         so_called_lib_std_string fsm_system_name = fsm_system_i -> first ;
-        const so_called_type_loadable_fsm_content_system & fsm_system = fsm_system_i -> second ;
-        for ( so_called_type_loadable_fsm_content_machine_container :: const_iterator fsm_machine_i = fsm_system . machines . begin ( )
+        so_called_type_loadable_fsm_content_system & fsm_system = fsm_system_i -> second ;
+        for ( so_called_type_loadable_fsm_content_machine_container :: iterator fsm_machine_i = fsm_system . machines . begin ( )
             ; fsm_machine_i != fsm_system . machines . end ( )
             ; ++ fsm_machine_i
             )
         {
             so_called_lib_std_string fsm_machine_name = fsm_machine_i -> first ;
-            const so_called_type_loadable_fsm_content_machine & fsm_machine = fsm_machine_i -> second ;
+            so_called_type_loadable_fsm_content_machine & fsm_machine = fsm_machine_i -> second ;
             if ( fsm_machine . states . find ( so_called_loadable_fsm_consts :: state_initial ) == fsm_machine . states . end ( ) )
             {
+                fsm_machine . states [ so_called_loadable_fsm_consts :: state_initial ] = so_called_type_loadable_fsm_content_state ( ) ;
                 if ( shy_guts :: consts :: trace_enabled )
                     so_called_trace ( so_called_trace_loadable_fsm_assigner :: no_initial_state_in_machine_of_system_error ( fsm_machine_name . c_str ( ) , fsm_system_name . c_str ( ) ) ) ;
                 shy_guts :: error = so_called_lib_std_true ;
