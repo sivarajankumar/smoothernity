ed6fe5cdca425cd75231bb2097dd5453943561ed
olegus8 <olegus8@localhost>
2011-07-15 14:19:31 +0000

    colorizing output

--- src/main/iphone/platform/render/texture_loader/shy_texture_loader_injections.h
+++ src/main/iphone/platform/render/texture_loader/shy_texture_loader_injections.h
@@ -1,5 +1,5 @@
-#ifndef _shy_macosx_platform_render_texture_loader_injections_included
-#define _shy_macosx_platform_render_texture_loader_injections_included
+#ifndef _shy_iphone_platform_render_texture_loader_injections_included
+#define _shy_iphone_platform_render_texture_loader_injections_included
 
 #include "../../../../../platform/render/texture_loader/cocoa/shy_cocoa_injections.h"
 typedef so_called_platform_render_texture_loader_cocoa so_called_platform_render_texture_loader ;
