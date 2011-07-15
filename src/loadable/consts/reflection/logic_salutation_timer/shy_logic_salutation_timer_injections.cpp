c9260ddcaeef1d087e719a68f99bea78782731b6
olegus8 <olegus8@localhost>
2011-07-15 12:53:52 +0000

    transforming logic title into logic salutation

---
+++ src/loadable/consts/reflection/logic_salutation_timer/shy_logic_salutation_timer_injections.cpp
@@ -0,0 +1,11 @@
+#ifdef shy_build_loadable_way
+
+    #include "../../binder/helper/module/shy_module_injections.h"
+    #include "../../binder/helper/value/shy_value_injections.h"
+
+    #include "../../../../common/logic/salutation/timer/consts/shy_consts_injections.h"
+
+    #include "./shy_logic_salutation_timer_injections.h"
+    #include "./shy_logic_salutation_timer.hpp"
+
+#endif
