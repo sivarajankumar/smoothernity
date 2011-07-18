50feacafb7d9b492381082a88a1a14853c1b0602
olegus8 <olegus8@localhost>
2011-07-18 16:53:52 +0000

    transforming logic title into logic salutation

--- src/common/logic/salutation/timer/appear/shy_appear.hpp
+++ src/common/logic/salutation/timer/appear/shy_appear.hpp
@@ -13,11 +13,23 @@ void _shy_common_logic_salutation_timer_appear :: receive ( so_called_message_co
 
 void _shy_common_logic_salutation_timer_appear :: receive ( so_called_message_common_logic_salutation_timer_appear_run )
 {
-    so_called_sender_common_logic_salutation_timer_appear_run_finished :: send ( so_called_message_common_logic_salutation_timer_appear_run_finished ( ) ) ;
+    shy_guts :: time = so_called_common_logic_salutation_timer_consts :: time_appear ;
 }
 
 void _shy_common_logic_salutation_timer_appear :: receive ( so_called_message_common_logic_salutation_timer_update )
 {
+    if ( so_called_platform_conditions :: fract_greater_than_fract ( shy_guts :: time , so_called_platform_math_consts :: fract_0 ) )
+    {
+        so_called_type_platform_math_num_fract time_step ;
+        so_called_platform_math :: make_num_fract ( time_step , 1 , so_called_platform_consts :: frames_per_second ) ;
+        if ( so_called_platform_conditions :: fract_greater_than_fract ( shy_guts :: time , time_step ) )
+            so_called_platform_math :: sub_from_fract ( shy_guts :: time , time_step ) ;
+        else
+        {
+            shy_guts :: time = so_called_platform_math_consts :: fract_0 ;
+            so_called_sender_common_logic_salutation_timer_appear_run_finished :: send ( so_called_message_common_logic_salutation_timer_appear_run_finished ( ) ) ;
+        }
+    }
 }
 
 void _shy_common_logic_salutation_timer_appear :: register_in_scheduler ( )
