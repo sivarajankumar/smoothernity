16204f9f9a7bcf762e761c198056dc2020c3c56a
olegus8 <olegus8@localhost>
2011-07-18 16:58:28 +0000

    transforming logic title into logic salutation

--- src/common/logic/salutation/timer/disappear/shy_disappear_injections.cpp
+++ src/common/logic/salutation/timer/disappear/shy_disappear_injections.cpp
@@ -1,5 +1,10 @@
 #include "./shy_disappear_injections.h"
 
 #include "./sender/run_finished/shy_run_finished_injections.h"
+#include "../consts/shy_consts_injections.h"
+#include "../../../../../injections/platform/conditions/shy_conditions.h"
+#include "../../../../../injections/platform/consts/shy_consts.h"
+#include "../../../../../injections/platform/math/consts/shy_consts.h"
+#include "../../../../../injections/platform/math/shy_math.h"
 
 #include "./shy_disappear.hpp"
