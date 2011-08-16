9a8fb58c92a1610f0da13f579f7520b8cbd53e1e
olegus8 <olegus8@localhost>
2011-08-16 19:28:47 +0400

    transforming logic title into logic salutation

---
+++ src/common/logic/salutation/letters/placement/shy_placement.hpp
@@ -0,0 +1,15 @@
+namespace shy_guts
+{
+}
+
+typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_salutation_letters_placement > _scheduled_context_type ;
+template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void _shy_common_logic_salutation_letters_placement :: receive ( so_called_message_common_init )
+{
+}
+
+void _shy_common_logic_salutation_letters_placement :: register_in_scheduler ( )
+{
+    _scheduled_context_type :: register_in_scheduler ( ) ;
+}
