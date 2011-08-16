922d5902898fa61d4160bea82f3e1e69ff1508c1
olegus8 <olegus8@localhost>
2011-08-16 19:42:10 +0400

    transforming logic title into logic salutation

---
+++ src/common/logic/salutation/letters/renderer/shy_renderer_injections.h
@@ -0,0 +1,11 @@
+#ifndef _shy_common_logic_salutation_letters_renderer_injections_included
+#define _shy_common_logic_salutation_letters_renderer_injections_included
+
+#include "./message/render/shy_render_injections.h"
+#include "../../../../../injections/platform/scheduler/shy_scheduler.h"
+
+#include "./shy_renderer.h"
+
+typedef shy_common_logic_salutation_letters_renderer_scheduled so_called_common_logic_salutation_letters_renderer ;
+
+#endif
