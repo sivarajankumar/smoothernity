376a1a8354755e53f32cb60300c083bed9f30bbe
olegus8 <olegus8@localhost>
2011-08-17 16:54:57 +0400

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/renderer/shy_renderer.hpp
+++ src/common/logic/salutation/letters/renderer/shy_renderer.hpp
@@ -3,11 +3,27 @@ namespace shy_guts
     namespace logic_salutation_letters_renderer_render_state
     {
         static so_called_type_platform_math_num_whole requested ;
+        static void on_requested ( ) ;
     }
+
+    static void work ( ) ;
 }
 
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_salutation_letters_renderer > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void shy_guts :: work ( )
+{
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_salutation_letters_renderer_render_state :: requested ) )
+    {
+        shy_guts :: logic_salutation_letters_renderer_render_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: logic_salutation_letters_renderer_render_state :: on_requested ( ) ;
+    }
+}
+
+void shy_guts :: logic_salutation_letters_renderer_render_state :: on_requested ( )
+{
+}
 
 void _shy_common_logic_salutation_letters_renderer :: receive ( so_called_message_common_init )
 {
@@ -16,6 +32,8 @@ void _shy_common_logic_salutation_letters_renderer :: receive ( so_called_messag
 
 void _shy_common_logic_salutation_letters_renderer :: receive ( so_called_message_common_logic_salutation_letters_renderer_render )
 {
+    shy_guts :: logic_salutation_letters_renderer_render_state :: requested = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: work ( ) ;
 }
 
 void _shy_common_logic_salutation_letters_renderer :: register_in_scheduler ( )
