83ee3738ab5514ab907505f764f5c0ba442ac2f2
olegus8 <olegus8@localhost>
2011-08-17 16:49:11 +0400

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/renderer/shy_renderer.hpp
+++ src/common/logic/salutation/letters/renderer/shy_renderer.hpp
@@ -1,9 +1,18 @@
 namespace shy_guts
 {
+    namespace logic_salutation_letters_renderer_render_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+    }
 }
 
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_salutation_letters_renderer > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
+
+void _shy_common_logic_salutation_letters_renderer :: receive ( so_called_message_common_init )
+{
+    shy_guts :: logic_salutation_letters_renderer_render_state :: requested = so_called_platform_math_consts :: whole_false ;
+}
 
 void _shy_common_logic_salutation_letters_renderer :: receive ( so_called_message_common_logic_salutation_letters_renderer_render )
 {
