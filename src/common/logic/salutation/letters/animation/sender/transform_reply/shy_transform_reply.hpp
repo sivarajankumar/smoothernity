494cb02e26789e4d5b18364212fbc2a0dd894035
olegus8 <olegus8@localhost>
2011-08-18 19:38:58 +0400

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/animation/sender/transform_reply/shy_transform_reply.hpp
+++ src/common/logic/salutation/letters/animation/sender/transform_reply/shy_transform_reply.hpp
@@ -1,3 +1,4 @@
 void shy_sender_common_logic_salutation_letters_animation_transform_reply :: send ( so_called_message_common_logic_salutation_letters_animation_transform_reply msg )
 {
+    so_called_common_logic_salutation_letters_renderer :: receive ( msg ) ;
 }
