9a3995c3525509e3e98fbf6a77dd1e7e256443d6
olegus8 <olegus8@localhost>
2011-08-19 19:35:16 +0400

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/animation/layout/shy_layout.hpp
+++ src/common/logic/salutation/letters/animation/layout/shy_layout.hpp
@@ -9,8 +9,24 @@ void _shy_common_logic_salutation_letters_animation_layout :: receive ( so_calle
 {
 }
 
-void _shy_common_logic_salutation_letters_animation_layout :: receive ( so_called_message_common_logic_salutation_letters_animation_layout_transform_request )
+void _shy_common_logic_salutation_letters_animation_layout :: receive ( so_called_message_common_logic_salutation_letters_animation_layout_transform_request msg )
 {
+    so_called_type_platform_vector_data origin ;
+    so_called_platform_vector :: xyz
+        ( origin
+        , so_called_platform_math_consts :: fract_0
+        , so_called_platform_math_consts :: fract_0
+        , so_called_platform_math_consts :: fract_0
+        ) ;
+
+    so_called_type_platform_math_num_fract scale ;
+    scale = so_called_platform_math_consts :: fract_1 ;
+
+    so_called_message_common_logic_salutation_letters_animation_layout_transform_reply msg_reply ;
+    msg_reply . letter = msg . letter ;
+    msg_reply . origin = origin ;
+    msg_reply . scale = scale ;
+    so_called_sender_common_logic_salutation_letters_animation_layout_transform_reply :: send ( msg_reply ) ;
 }
 
 void _shy_common_logic_salutation_letters_animation_layout :: register_in_scheduler ( )
--- src/common/logic/salutation/letters/animation/layout/shy_layout_injections.cpp
+++ src/common/logic/salutation/letters/animation/layout/shy_layout_injections.cpp
@@ -1,2 +1,7 @@
 #include "./shy_layout_injections.h"
+
+#include "./sender/transform_reply/shy_transform_reply_injections.h"
+#include "../../../../../../injections/platform/math/consts/shy_consts.h"
+#include "../../../../../../injections/platform/vector/shy_vector.h"
+
 #include "./shy_layout.hpp"
