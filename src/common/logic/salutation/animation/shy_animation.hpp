3c792b3a94e708a69cd33609314924f514d3a195
olegus8 <olegus8@localhost>
2011-08-19 17:06:22 +0400

    transforming logic title into logic salutation

--- src/common/logic/salutation/animation/shy_animation.hpp
+++ src/common/logic/salutation/animation/shy_animation.hpp
@@ -6,16 +6,29 @@ namespace shy_guts
         static so_called_type_platform_math_num_fract origin_y = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
         static so_called_type_platform_math_num_fract origin_z = so_called_platform_math :: init_num_fract ( - 3 , 1 ) ;
     }
+
+    namespace logic_salutation_animation_transform_state
+    {
+        static so_called_type_platform_math_num_whole requested ;
+        static void on_requested ( ) ;
+    }
+
+    static void work ( ) ;
 }
 
 typedef so_called_platform_scheduler :: scheduled_context < _shy_common_logic_salutation_animation > _scheduled_context_type ;
 template < > _scheduled_context_type _scheduled_context_type :: _singleton = _scheduled_context_type ( ) ;
 
-void _shy_common_logic_salutation_animation :: receive ( so_called_message_common_init )
+void shy_guts :: work ( )
 {
+    if ( so_called_platform_conditions :: whole_is_true ( shy_guts :: logic_salutation_animation_transform_state :: requested ) )
+    {
+        shy_guts :: logic_salutation_animation_transform_state :: requested = so_called_platform_math_consts :: whole_false ;
+        shy_guts :: logic_salutation_animation_transform_state :: on_requested ( ) ;
+    }
 }
 
-void _shy_common_logic_salutation_animation :: receive ( so_called_message_common_logic_salutation_animation_transform_request )
+void shy_guts :: logic_salutation_animation_transform_state :: on_requested ( )
 {
     so_called_type_platform_vector_data origin ;
     so_called_platform_vector :: xyz 
@@ -29,6 +42,17 @@ void _shy_common_logic_salutation_animation :: receive ( so_called_message_commo
     so_called_platform_matrix :: identity ( msg . transform ) ;
     so_called_platform_matrix :: set_origin ( msg . transform , origin ) ;
     so_called_sender_common_logic_salutation_animation_transform_reply :: send ( msg ) ;
+}
+
+void _shy_common_logic_salutation_animation :: receive ( so_called_message_common_init )
+{
+    shy_guts :: logic_salutation_animation_transform_state :: requested = so_called_platform_math_consts :: whole_false ;
+}
+
+void _shy_common_logic_salutation_animation :: receive ( so_called_message_common_logic_salutation_animation_transform_request )
+{
+    shy_guts :: logic_salutation_animation_transform_state :: requested = so_called_platform_math_consts :: whole_true ;
+    shy_guts :: work ( ) ;
 }
 
 void _shy_common_logic_salutation_animation :: receive ( so_called_message_common_logic_salutation_animation_zoom_transform_reply )
