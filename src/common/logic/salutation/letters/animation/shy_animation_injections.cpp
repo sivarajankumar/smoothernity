36a46f0429c48d6e812ee07d1b03d019b621b4c7
olegus8 <olegus8@localhost>
2011-08-22 17:31:21 +0400

    transforming logic title into logic salutation

--- src/common/logic/salutation/letters/animation/shy_animation_injections.cpp
+++ src/common/logic/salutation/letters/animation/shy_animation_injections.cpp
@@ -1,6 +1,9 @@
 #include "./shy_animation_injections.h"
 
+#include "./layout/sender/transform_request/shy_transform_request_injections.h"
 #include "./sender/transform_reply/shy_transform_reply_injections.h"
+#include "../../../../../injections/platform/conditions/shy_conditions.h"
+#include "../../../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../../../injections/platform/matrix/shy_matrix.h"
 
 #include "./shy_animation.hpp"
