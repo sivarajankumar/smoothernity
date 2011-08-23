c50627dacb67732a46558553e062631558faa374
olegus8 <olegus8@localhost>
2011-08-23 18:38:59 +0400

    working on message taker abstraction

--- src/common/logic/salutation/letters/animation/layout/shy_layout_injections.cpp
+++ src/common/logic/salutation/letters/animation/layout/shy_layout_injections.cpp
@@ -2,6 +2,7 @@
 
 #include "./sender/transform_reply/shy_transform_reply_injections.h"
 #include "../../meshes/storage/sender/size_request/shy_size_request_injections.h"
+#include "../../meshes/storage/size/checker/shy_checker_injections.h"
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../../../../injections/platform/vector/shy_vector.h"
---
+++ src/common/logic/salutation/letters/meshes/storage/size/checker/shy_checker.h
@@ -0,0 +1,9 @@
+class shy_common_logic_salutation_letters_meshes_storage_size_checker
+{
+public :
+    static void check 
+        ( so_called_type_platform_math_num_whole & 
+        , so_called_message_common_logic_salutation_letters_meshes_storage_size_request
+        , so_called_message_common_logic_salutation_letters_meshes_storage_size_reply
+        ) ;
+} ;
