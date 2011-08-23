4c7a4e5ef748ad9c55e30c87b5851dacf0665900
olegus8 <olegus8@localhost>
2011-08-23 18:48:10 +0400

    working on message taker abstraction

---
+++ src/common/engine/taker/helper/shy_helper.h
@@ -0,0 +1,7 @@
+#define shy_common_engine_taker_helper(what) \
+    so_called_common_engine_taker \
+        < so_called_common_##what##_checker \
+        , so_called_message_common_##what##_reply \
+        , so_called_message_common_##what##_request \
+        , so_called_sender_common_##what##_request \
+        >
--- src/common/logic/salutation/letters/animation/layout/shy_layout_injections.cpp
+++ src/common/logic/salutation/letters/animation/layout/shy_layout_injections.cpp
@@ -3,7 +3,7 @@
 #include "./sender/transform_reply/shy_transform_reply_injections.h"
 #include "../../meshes/storage/sender/size_request/shy_size_request_injections.h"
 #include "../../meshes/storage/size/checker/shy_checker_injections.h"
-#include "../../../../../engine/taker/shy_taker_injections.h"
+#include "../../../../../engine/taker/helper/shy_helper_injections.h"
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../../../../injections/platform/vector/shy_vector.h"
