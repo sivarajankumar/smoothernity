3ef4989d7d537ebae6999279399e29ae39cdeda4
olegus8 <olegus8@localhost>
2011-08-24 16:36:02 +0400

    working on message taker abstraction

---
+++ src/common/logic/text/use_text_texture/checker/shy_checker.h
@@ -0,0 +1,9 @@
+class shy_common_logic_text_use_text_texture_checker
+{
+public :
+    static void check 
+        ( so_called_type_platform_math_num_whole & 
+        , so_called_message_common_logic_text_use_text_texture_request
+        , so_called_message_common_logic_text_use_text_texture_reply
+        ) ;
+} ;
---
+++ src/common/logic/text/use_text_texture/checker/shy_checker.hpp
@@ -0,0 +1,8 @@
+void shy_common_logic_text_use_text_texture_checker :: check 
+    ( so_called_type_platform_math_num_whole & result
+    , so_called_message_common_logic_text_use_text_texture_request
+    , so_called_message_common_logic_text_use_text_texture_reply
+    )
+{
+    result = so_called_platform_math_consts :: whole_true ;
+}
