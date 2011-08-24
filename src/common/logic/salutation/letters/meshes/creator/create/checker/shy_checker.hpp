beff8650a01d7f72edc942f1590e050d492aa22b
olegus8 <olegus8@localhost>
2011-08-24 17:43:03 +0400

    making salutation use taker abstraction

---
+++ src/common/logic/salutation/letters/meshes/creator/create/checker/shy_checker.hpp
@@ -0,0 +1,11 @@
+void shy_common_logic_salutation_letters_meshes_creator_create_checker :: check 
+    ( so_called_type_platform_math_num_whole & result
+    , so_called_message_common_logic_salutation_letters_meshes_creator_create_request msg_request
+    , so_called_message_common_logic_salutation_letters_meshes_creator_create_reply msg_reply
+    )
+{
+    if ( so_called_platform_conditions :: wholes_are_equal ( msg_request . letter_index , msg_reply . letter_index ) )
+        result = so_called_platform_math_consts :: whole_true ;
+    else
+        result = so_called_platform_math_consts :: whole_false ;
+}
