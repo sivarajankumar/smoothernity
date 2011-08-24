31434428e4610dafc7245da90aefb0bbff773e5b
olegus8 <olegus8@localhost>
2011-08-24 15:46:51 +0400

    making salutation use taker abstraction

---
+++ src/common/logic/ortho/planes/checker/shy_checker.h
@@ -0,0 +1,9 @@
+class shy_common_logic_ortho_planes_checker
+{
+public :
+    static void check 
+        ( so_called_type_platform_math_num_whole & 
+        , so_called_message_common_logic_ortho_planes_request
+        , so_called_message_common_logic_ortho_planes_reply
+        ) ;
+} ;
---
+++ src/common/logic/ortho/planes/checker/shy_checker.hpp
@@ -0,0 +1,8 @@
+void shy_common_logic_ortho_planes_checker :: check 
+    ( so_called_type_platform_math_num_whole & result
+    , so_called_message_common_logic_ortho_planes_request
+    , so_called_message_common_logic_ortho_planes_reply
+    )
+{
+    result = so_called_platform_math_consts :: whole_true ;
+}
