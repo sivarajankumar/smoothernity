1d9b546ae819a0438c4e1562a7b53b39c09c9806
olegus8 <olegus8@localhost>
2011-08-24 17:17:14 +0400

    working on message taker abstraction

---
+++ src/common/logic/salutation/letters/text/storage/letter/checker/shy_checker.h
@@ -0,0 +1,9 @@
+class shy_common_logic_salutation_letters_text_storage_letter_checker
+{
+public :
+    static void check 
+        ( so_called_type_platform_math_num_whole & 
+        , so_called_message_common_logic_salutation_letters_text_storage_letter_request
+        , so_called_message_common_logic_salutation_letters_text_storage_letter_reply
+        ) ;
+} ;
---
+++ src/common/logic/salutation/letters/text/storage/letter/checker/shy_checker_injections.h
@@ -0,0 +1,11 @@
+#ifndef _shy_common_logic_salutation_letters_text_storage_letter_checker_injections_included
+#define _shy_common_logic_salutation_letters_text_storage_letter_checker_injections_included
+
+#include "../../message/letter_reply/shy_letter_reply_injections.h"
+#include "../../message/letter_request/shy_letter_request_injections.h"
+
+#include "./shy_checker.h"
+
+typedef shy_common_logic_salutation_letters_text_storage_letter_checker so_called_common_logic_salutation_letters_text_storage_letter_checker ;
+
+#endif
