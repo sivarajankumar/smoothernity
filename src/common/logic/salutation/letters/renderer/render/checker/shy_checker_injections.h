38d28585b0ecc05c84f2f1a195d690ac5b3f536b
olegus8 <olegus8@localhost>
2011-08-24 16:28:06 +0400

    working on message taker abstraction

---
+++ src/common/logic/salutation/letters/renderer/render/checker/shy_checker_injections.h
@@ -0,0 +1,12 @@
+#ifndef _shy_common_logic_salutation_letters_renderer_render_checker_injections_included
+#define _shy_common_logic_salutation_letters_renderer_render_checker_injections_included
+
+#include "../../message/render_reply/shy_render_reply_injections.h"
+#include "../../message/render_request/shy_render_request_injections.h"
+#include "../../../../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
+
+#include "./shy_checker.h"
+
+typedef shy_common_logic_salutation_letters_renderer_render_checker so_called_common_logic_salutation_letters_renderer_render_checker ;
+
+#endif
