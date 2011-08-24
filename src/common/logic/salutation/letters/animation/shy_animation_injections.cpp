d9099046a97ec491cfc4f756d99f6bbd961a9e88
olegus8 <olegus8@localhost>
2011-08-24 15:20:21 +0400

    making salutation use taker abstraction

--- src/common/logic/salutation/letters/animation/shy_animation_injections.cpp
+++ src/common/logic/salutation/letters/animation/shy_animation_injections.cpp
@@ -1,8 +1,10 @@
 #include "./shy_animation_injections.h"
 
 #include "./layout/sender/transform_request/shy_transform_request_injections.h"
+#include "./layout/transform/checker/shy_checker_injections.h"
 #include "./sender/transform_reply/shy_transform_reply_injections.h"
 #include "../../../../engine/math/stateless/shy_stateless_injections.h"
+#include "../../../../engine/taker/helper/shy_helper_injections.h"
 #include "../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../injections/platform/math/consts/shy_consts.h"
 #include "../../../../../injections/platform/matrix/shy_matrix.h"
