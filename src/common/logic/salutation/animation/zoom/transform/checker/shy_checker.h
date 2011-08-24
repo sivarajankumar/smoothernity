4eeb7b1acc6e9e61429651d0848f5c05847b8046
olegus8 <olegus8@localhost>
2011-08-24 15:35:21 +0400

    making salutation use taker abstraction

---
+++ src/common/logic/salutation/animation/zoom/transform/checker/shy_checker.h
@@ -0,0 +1,9 @@
+class shy_common_logic_salutation_animation_zoom_transform_checker
+{
+public :
+    static void check 
+        ( so_called_type_platform_math_num_whole & 
+        , so_called_message_common_logic_salutation_animation_zoom_transform_request
+        , so_called_message_common_logic_salutation_animation_zoom_transform_reply
+        ) ;
+} ;
---
+++ src/common/logic/salutation/animation/zoom/transform/checker/shy_checker_injections.cpp
@@ -0,0 +1,5 @@
+#include "./shy_checker_injections.h"
+
+#include "../../../../../../../injections/platform/math/consts/shy_consts.h"
+
+#include "./shy_checker.hpp"
