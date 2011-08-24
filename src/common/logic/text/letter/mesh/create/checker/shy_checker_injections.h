29c841a24f59111c4968115ea9ea5622acb58491
olegus8 <olegus8@localhost>
2011-08-24 17:30:28 +0400

    making salutation use taker abstraction

---
+++ src/common/logic/text/letter/mesh/create/checker/shy_checker_injections.h
@@ -0,0 +1,11 @@
+#ifndef _shy_common_logic_text_letter_mesh_create_checker_injections_included
+#define _shy_common_logic_text_letter_mesh_create_checker_injections_included
+
+#include "../../message/create_reply/shy_create_reply_injections.h"
+#include "../../message/create_request/shy_create_request_injections.h"
+
+#include "./shy_checker.h"
+
+typedef shy_common_logic_text_letter_mesh_create_checker so_called_common_logic_text_letter_mesh_create_checker ;
+
+#endif
