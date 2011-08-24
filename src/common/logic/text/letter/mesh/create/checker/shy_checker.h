5dbd33c30a06bd5ad2f34b6903d5052a0ce86d75
olegus8 <olegus8@localhost>
2011-08-24 17:30:28 +0400

    making salutation use taker abstraction

---
+++ src/common/logic/text/letter/mesh/create/checker/shy_checker.h
@@ -0,0 +1,9 @@
+class shy_common_logic_text_letter_mesh_create_checker
+{
+public :
+    static void check 
+        ( so_called_type_platform_math_num_whole & 
+        , so_called_message_common_logic_text_letter_mesh_create_request
+        , so_called_message_common_logic_text_letter_mesh_create_reply
+        ) ;
+} ;
