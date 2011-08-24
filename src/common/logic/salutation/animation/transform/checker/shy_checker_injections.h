090f455d1f9d7add33ed3bf3278121bf44ba153d
olegus8 <olegus8@localhost>
2011-08-24 15:59:48 +0400

    making salutation use taker abstraction

---
+++ src/common/logic/salutation/animation/transform/checker/shy_checker_injections.h
@@ -0,0 +1,12 @@
+#ifndef _shy_common_logic_salutation_animation_transform_checker_injections_included
+#define _shy_common_logic_salutation_animation_transform_checker_injections_included
+
+#include "../../message/transform_reply/shy_transform_reply_injections.h"
+#include "../../message/transform_request/shy_transform_request_injections.h"
+#include "../../../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
+
+#include "./shy_checker.h"
+
+typedef shy_common_logic_salutation_animation_transform_checker so_called_common_logic_salutation_animation_transform_checker ;
+
+#endif
