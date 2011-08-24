e12cbb9bdef99ecc61f785ae9518719da8c5c811
olegus8 <olegus8@localhost>
2011-08-24 15:46:51 +0400

    making salutation use taker abstraction

---
+++ src/common/logic/ortho/planes/checker/shy_checker_injections.h
@@ -0,0 +1,12 @@
+#ifndef _shy_common_logic_ortho_planes_checker_injections_included
+#define _shy_common_logic_ortho_planes_checker_injections_included
+
+#include "../../message/planes_reply/shy_planes_reply_injections.h"
+#include "../../message/planes_request/shy_planes_request_injections.h"
+#include "../../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
+
+#include "./shy_checker.h"
+
+typedef shy_common_logic_ortho_planes_checker so_called_common_logic_ortho_planes_checker ;
+
+#endif
