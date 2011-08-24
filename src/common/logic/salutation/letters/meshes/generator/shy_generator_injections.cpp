d71f3b6b09649fa5c635adf28fc7bbf77157f071
olegus8 <olegus8@localhost>
2011-08-24 18:19:14 +0400

    making salutation use taker abstraction

--- src/common/logic/salutation/letters/meshes/generator/shy_generator_injections.cpp
+++ src/common/logic/salutation/letters/meshes/generator/shy_generator_injections.cpp
@@ -2,8 +2,10 @@
 
 #include "./sender/generate_finished/shy_generate_finished_injections.h"
 #include "../consts/shy_consts_injections.h"
+#include "../creator/create/checker/shy_checker_injections.h"
 #include "../creator/sender/create_request/shy_create_request_injections.h"
 #include "../../text/storage/sender/size_request/shy_size_request_injections.h"
+#include "../../../../../engine/taker/helper/shy_helper_injections.h"
 #include "../../../../../../injections/platform/conditions/shy_conditions.h"
 #include "../../../../../../injections/platform/consts/shy_consts.h"
 #include "../../../../../../injections/platform/math/consts/shy_consts.h"
