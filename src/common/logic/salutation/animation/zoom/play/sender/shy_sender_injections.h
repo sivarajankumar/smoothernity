bf07b21d30b1deb7056ba92847b873925c8d12da
olegus8 <olegus8@localhost>
2011-08-25 18:32:04 +0400

    making postfix message folder scheme for logic salutation

--- src/common/logic/salutation/animation/zoom/play/sender/shy_sender_injections.h
+++ src/common/logic/salutation/animation/zoom/play/sender/shy_sender_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_sender_common_logic_salutation_animation_zoom_play_injections_included
 #define _shy_sender_common_logic_salutation_animation_zoom_play_injections_included
 
-#include "../../message/play/shy_play_injections.h"
+#include "../message/shy_message_injections.h"
 
-#include "./shy_play.h"
+#include "./shy_sender.h"
 
 typedef shy_sender_common_logic_salutation_animation_zoom_play so_called_sender_common_logic_salutation_animation_zoom_play ;
 
