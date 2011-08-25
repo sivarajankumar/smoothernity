0b2527f4c69534a4bff5dc588ea81103ab0c3b2f
olegus8 <olegus8@localhost>
2011-08-25 18:44:56 +0400

    making postfix message folder scheme for logic salutation

--- src/common/logic/salutation/letters/animation/appear/sender/play/shy_play_injections.cpp
+++
@@ -1,5 +0,0 @@
-#include "./shy_play_injections.h"
-
-#include "../../shy_appear_injections.h"
-
-#include "./shy_play.hpp"
--- src/common/logic/salutation/letters/animation/appear/shy_appear_injections.h
+++ src/common/logic/salutation/letters/animation/appear/shy_appear_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_common_logic_salutation_letters_animation_appear_injections_included
 #define _shy_common_logic_salutation_letters_animation_appear_injections_included
 
-#include "./message/play/shy_play_injections.h"
+#include "./play/message/shy_message_injections.h"
 #include "../message/update/shy_update_injections.h"
 #include "../../../../../../injections/platform/scheduler/shy_scheduler.h"
 
