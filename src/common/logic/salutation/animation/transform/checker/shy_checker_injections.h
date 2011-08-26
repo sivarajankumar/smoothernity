bd3395997b4274473da96abc5e50179367861734
olegus8 <olegus8@localhost>
2011-08-26 19:39:56 +0400

    making same include base for logic salutation

--- src/common/logic/salutation/animation/transform/checker/shy_checker_injections.h
+++ src/common/logic/salutation/animation/transform/checker/shy_checker_injections.h
@@ -1,9 +1,9 @@
 #ifndef _shy_common_logic_salutation_animation_transform_checker_injections_included
 #define _shy_common_logic_salutation_animation_transform_checker_injections_included
 
-#include "../reply/message/shy_message_injections.h"
-#include "../request/message/shy_message_injections.h"
-#include "../../../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
+#include "common/logic/salutation/animation/transform/reply/message/shy_message_injections.h"
+#include "common/logic/salutation/animation/transform/request/message/shy_message_injections.h"
+#include "injections/platform/math/type/num_whole/shy_num_whole.h"
 
 #include "./shy_checker.h"
 
