b18f0b463ebe4e354b42294584aca6dc9af8257f
olegus8 <olegus8@localhost>
2011-08-29 17:03:12 +0400

    making same include base for logic salutation

--- src/common/logic/salutation/letters/animation/layout/transform/reply/sender/shy_sender_injections.cpp
+++ src/common/logic/salutation/letters/animation/layout/transform/reply/sender/shy_sender_injections.cpp
@@ -1,5 +1,5 @@
 #include "./shy_sender_injections.h"
 
-#include "../../../../shy_animation_injections.h"
+#include "common/logic/salutation/letters/animation/shy_animation_injections.h"
 
 #include "./shy_sender.hpp"
