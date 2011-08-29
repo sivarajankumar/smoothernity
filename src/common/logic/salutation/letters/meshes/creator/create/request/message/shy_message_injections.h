8cccf4382254568eaea130cb2f72614a57d06520
olegus8 <olegus8@localhost>
2011-08-29 17:42:38 +0400

    making same include base for logic salutation

--- src/common/logic/salutation/letters/meshes/creator/create/request/message/shy_message_injections.h
+++ src/common/logic/salutation/letters/meshes/creator/create/request/message/shy_message_injections.h
@@ -1,7 +1,7 @@
 #ifndef _shy_message_common_logic_salutation_letters_meshes_creator_create_request_injections_included
 #define _shy_message_common_logic_salutation_letters_meshes_creator_create_request_injections_included
 
-#include "../../../../../../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
+#include "injections/platform/math/type/num_whole/shy_num_whole.h"
 
 #include "./shy_message.h"
 
