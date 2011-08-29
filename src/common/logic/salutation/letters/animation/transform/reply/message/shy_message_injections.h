8167fc0ec14d1f7b08cb31e6c9eb60418d3fdb9f
olegus8 <olegus8@localhost>
2011-08-29 17:27:02 +0400

    making same include base for logic salutation

--- src/common/logic/salutation/letters/animation/transform/reply/message/shy_message_injections.h
+++ src/common/logic/salutation/letters/animation/transform/reply/message/shy_message_injections.h
@@ -1,8 +1,9 @@
 #ifndef _shy_message_common_logic_salutation_letters_animation_transform_reply_injections_included
 #define _shy_message_common_logic_salutation_letters_animation_transform_reply_injections_included
 
-#include "../../../../../../../../injections/platform/math/type/num_whole/shy_num_whole.h"
-#include "../../../../../../../../injections/platform/matrix/type/data/shy_data.h"
+#include "injections/platform/math/type/num_whole/shy_num_whole.h"
+#include "injections/platform/matrix/type/data/shy_data.h"
+
 #include "./shy_message.h"
 
 typedef shy_message_common_logic_salutation_letters_animation_transform_reply so_called_message_common_logic_salutation_letters_animation_transform_reply ;
--- src/common/logic/salutation/letters/consts/shy_consts_injections.cpp
+++ src/common/logic/salutation/letters/consts/shy_consts_injections.cpp
@@ -1,5 +1,5 @@
 #include "./shy_consts_injections.h"
 
-#include "../../../../../injections/platform/math/shy_math.h"
+#include "injections/platform/math/shy_math.h"
 
 #include "./shy_consts.hpp"
