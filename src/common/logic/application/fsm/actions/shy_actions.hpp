d94808f35b9918530e561a30e5ed1084b4060000
olegus8 <olegus8@localhost>
2011-08-29 19:42:48 +0400

    making postfix notation for logic_salutation senders

--- src/common/logic/application/fsm/actions/shy_actions.hpp
+++ src/common/logic/application/fsm/actions/shy_actions.hpp
@@ -60,7 +60,7 @@ void shy_common_logic_application_fsm_actions :: logic_salutation_animation_upda
 
 void shy_common_logic_application_fsm_actions :: logic_salutation_animation_zoom_play ( )
 {
-    so_called_sender_common_logic_salutation_animation_zoom_play :: send ( so_called_message_common_logic_salutation_animation_zoom_play ( ) ) ;
+    so_called_common_logic_salutation_animation_zoom_play_sender :: send ( so_called_message_common_logic_salutation_animation_zoom_play ( ) ) ;
 }
 
 void shy_common_logic_application_fsm_actions :: logic_salutation_letters_animation_appear_play ( )
--- src/common/logic/salutation/animation/zoom/play/sender/shy_sender.hpp
+++ src/common/logic/salutation/animation/zoom/play/sender/shy_sender.hpp
@@ -1,4 +1,4 @@
-void shy_sender_common_logic_salutation_animation_zoom_play :: send ( so_called_message_common_logic_salutation_animation_zoom_play msg )
+void shy_common_logic_salutation_animation_zoom_play_sender :: send ( so_called_message_common_logic_salutation_animation_zoom_play msg )
 {
     so_called_common_logic_salutation_animation_zoom :: receive ( msg ) ;
 }
