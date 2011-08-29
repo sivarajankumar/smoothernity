3bff0af6e772237579752a551bc33763217a2b49
olegus8 <olegus8@localhost>
2011-08-29 17:15:26 +0400

    making same include base for logic salutation

--- src/common/logic/salutation/letters/animation/shy_animation_injections.h
+++ src/common/logic/salutation/letters/animation/shy_animation_injections.h
@@ -1,10 +1,10 @@
 #ifndef _shy_common_logic_salutation_letters_animation_injections_included
 #define _shy_common_logic_salutation_letters_animation_injections_included
 
-#include "./layout/transform/reply/message/shy_message_injections.h"
-#include "./transform/request/message/shy_message_injections.h"
-#include "../../../../message/init/shy_init_injections.h"
-#include "../../../../../injections/platform/scheduler/shy_scheduler.h"
+#include "common/logic/salutation/letters/animation/layout/transform/reply/message/shy_message_injections.h"
+#include "common/logic/salutation/letters/animation/transform/request/message/shy_message_injections.h"
+#include "common/message/init/shy_init_injections.h"
+#include "injections/platform/scheduler/shy_scheduler.h"
 
 #include "./shy_animation.h"
 
