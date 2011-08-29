e346938575cfbf1eb913dde150e1a73c85fa55a8
olegus8 <olegus8@localhost>
2011-08-29 17:49:59 +0400

    making same include base for logic salutation

--- src/common/logic/salutation/letters/meshes/generator/shy_generator_injections.h
+++ src/common/logic/salutation/letters/meshes/generator/shy_generator_injections.h
@@ -1,12 +1,12 @@
 #ifndef _shy_common_logic_salutation_letters_meshes_generator_injections_included
 #define _shy_common_logic_salutation_letters_meshes_generator_injections_included
 
-#include "./generate/message/shy_message_injections.h"
-#include "./update/message/shy_message_injections.h"
-#include "../creator/create/reply/message/shy_message_injections.h"
-#include "../../text/storage/size/reply/message/shy_message_injections.h"
-#include "../../../../../message/init/shy_init_injections.h"
-#include "../../../../../../injections/platform/scheduler/shy_scheduler.h"
+#include "common/logic/salutation/letters/meshes/creator/create/reply/message/shy_message_injections.h"
+#include "common/logic/salutation/letters/meshes/generator/generate/message/shy_message_injections.h"
+#include "common/logic/salutation/letters/meshes/generator/update/message/shy_message_injections.h"
+#include "common/logic/salutation/letters/text/storage/size/reply/message/shy_message_injections.h"
+#include "common/message/init/shy_init_injections.h"
+#include "injections/platform/scheduler/shy_scheduler.h"
 
 #include "./shy_generator.h"
 
