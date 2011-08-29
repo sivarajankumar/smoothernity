7192ebd32c50e5e3e966c3fc7a02a3b622bdacf0
olegus8 <olegus8@localhost>
2011-08-29 19:23:07 +0400

    making postfix notation for logic_salutation senders

--- src/common/logic/salutation/animation/transform/request/sender/shy_sender.h
+++ src/common/logic/salutation/animation/transform/request/sender/shy_sender.h
@@ -1,4 +1,4 @@
-class shy_sender_common_logic_salutation_animation_transform_request
+class shy_common_logic_salutation_animation_transform_request_sender
 {
 public :
     static void send ( so_called_message_common_logic_salutation_animation_transform_request ) ;
