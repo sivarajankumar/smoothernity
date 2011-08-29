5b38f41c47ca24205cfbd50a8b4edcab6a96cd51
olegus8 <olegus8@localhost>
2011-08-29 19:23:07 +0400

    making postfix notation for logic_salutation senders

--- src/common/engine/taker/helper/shy_helper.h
+++ src/common/engine/taker/helper/shy_helper.h
@@ -5,3 +5,11 @@
         , so_called_message_common_##what##_request \
         , so_called_sender_common_##what##_request \
         >
+
+#define shy_common_engine_taker_helper_new(what) \
+    so_called_common_engine_taker \
+        < so_called_common_##what##_checker \
+        , so_called_message_common_##what##_reply \
+        , so_called_message_common_##what##_request \
+        , so_called_common_##what##_request_sender \
+        >
