20f05414952515ec7ad7d0e36f747e377f51432f
olegus8 <olegus8@localhost>
2011-08-29 18:59:48 +0400

    making same include base for logic salutation

--- src/common/logic/salutation/timer/appear/shy_appear_injections.cpp
+++ src/common/logic/salutation/timer/appear/shy_appear_injections.cpp
@@ -1,10 +1,10 @@
 #include "./shy_appear_injections.h"
 
-#include "./run/finished/sender/shy_sender_injections.h"
-#include "../consts/shy_consts_injections.h"
-#include "../../../../../injections/platform/conditions/shy_conditions.h"
-#include "../../../../../injections/platform/consts/shy_consts.h"
-#include "../../../../../injections/platform/math/consts/shy_consts.h"
-#include "../../../../../injections/platform/math/shy_math.h"
+#include "common/logic/salutation/timer/appear/run/finished/sender/shy_sender_injections.h"
+#include "common/logic/salutation/timer/consts/shy_consts_injections.h"
+#include "injections/platform/conditions/shy_conditions.h"
+#include "injections/platform/consts/shy_consts.h"
+#include "injections/platform/math/consts/shy_consts.h"
+#include "injections/platform/math/shy_math.h"
 
 #include "./shy_appear.hpp"
