24c016b12def7ec4a08e450787ea7451839ca06f
olegus8 <olegus8@localhost>
2011-08-29 17:35:47 +0400

    making same include base for logic salutation

--- src/common/logic/salutation/letters/meshes/cleaner/shy_cleaner_injections.h
+++ src/common/logic/salutation/letters/meshes/cleaner/shy_cleaner_injections.h
@@ -1,12 +1,12 @@
 #ifndef _shy_common_logic_salutation_letters_meshes_cleaner_injections_included
 #define _shy_common_logic_salutation_letters_meshes_cleaner_injections_included
 
-#include "./clean/message/shy_message_injections.h"
-#include "./update/message/shy_message_injections.h"
-#include "../storage/mesh/reply/message/shy_message_injections.h"
-#include "../storage/size/reply/message/shy_message_injections.h"
-#include "../../../../../message/init/shy_init_injections.h"
-#include "../../../../../../injections/platform/scheduler/shy_scheduler.h"
+#include "common/logic/salutation/letters/meshes/cleaner/clean/message/shy_message_injections.h"
+#include "common/logic/salutation/letters/meshes/cleaner/update/message/shy_message_injections.h"
+#include "common/logic/salutation/letters/meshes/storage/mesh/reply/message/shy_message_injections.h"
+#include "common/logic/salutation/letters/meshes/storage/size/reply/message/shy_message_injections.h"
+#include "common/message/init/shy_init_injections.h"
+#include "injections/platform/scheduler/shy_scheduler.h"
 
 #include "./shy_cleaner.h"
 
