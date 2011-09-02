821bfd2ff116ac28a4783ba8b4c4c94be0521e31
olegus8 <olegus8@localhost>
2011-09-02 17:20:49 +0400

    working on paths normalizer

--- src/loadable/consts/generator/shy_generator_injections.cpp
+++ src/loadable/consts/generator/shy_generator_injections.cpp
@@ -1,9 +1,9 @@
 #ifdef shy_build_loadable_way
 
-    #include "injections/lib/std/char/shy_char.h"
     #include "loadable/consts/content/shy_content_injections.h"
     #include "loadable/generator/consts/shy_consts_injections.h"
     #include "loadable/generator/python/shy_python_injections.h"
+    #include "loadable/generator/shy_generator_injections.h"
 
     #include "./shy_generator_injections.h"
     #include "./shy_generator.hpp"
--- src/loadable/generator/shy_generator.hpp
+++ src/loadable/generator/shy_generator.hpp
@@ -1,3 +1,12 @@
+namespace shy_guts
+{
+    namespace consts
+    {
+        static so_called_lib_std_char slash = '/' ;
+        static so_called_lib_std_char underscore = '_' ;
+    }
+}
+
 void shy_loadable_generator :: generate ( so_called_lib_std_string & result )
 {
     so_called_lib_std_string consts_logic ;
@@ -9,3 +18,14 @@ void shy_loadable_generator :: generate ( so_called_lib_std_string & result )
     so_called_loadable_generator_python :: main_script ( result , consts_logic + fsm_logic ) ;
 }
 
+void shy_loadable_generator :: module_path ( so_called_lib_std_string & result , so_called_lib_std_string module_name )
+{
+    result . clear ( ) ;
+    for ( so_called_lib_std_int32_t i = 0 ; i < so_called_lib_std_int32_t ( module_name . size ( ) ) ; ++ i )
+    {
+        if ( module_name [ i ] == shy_guts :: consts :: underscore )
+            result += shy_guts :: consts :: slash ;
+        else
+            result += module_name [ i ] ;
+    }
+}
