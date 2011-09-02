7d6ef1fae002883d8b5052ae385bdb6525ddaf86
olegus8 <olegus8@localhost>
2011-09-02 16:46:01 +0400

    working on paths normalizer

--- src/loadable/consts/generator/shy_generator_injections.cpp
+++ src/loadable/consts/generator/shy_generator_injections.cpp
@@ -1,7 +1,7 @@
 #ifdef shy_build_loadable_way
 
+    #include "injections/lib/std/char/shy_char.h"
     #include "loadable/consts/content/shy_content_injections.h"
-
     #include "loadable/generator/consts/shy_consts_injections.h"
     #include "loadable/generator/python/shy_python_injections.h"
 
