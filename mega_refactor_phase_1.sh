b5058ef9416bc66a667491b36f601655cae5678d
olegus8 <olegus8@localhost>
2011-09-02 15:43:02 +0400

    mega refactor script

---
+++ mega_refactor_phase_1.sh
@@ -0,0 +1,14 @@
+/usr/local/bin/python -B shy_normalize_paths.py src/common>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/facade>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/injections>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/lib>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/loadable>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/main/iphone/lib>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/main/iphone/platform>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/main/macosx/lib>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/main/macosx/platform>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/main/win/lib>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/main/win/platform>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/platform>>mega_refactor_phase_2.sh
+/usr/local/bin/python -B shy_normalize_paths.py src/trace>>mega_refactor_phase_2.sh
+
