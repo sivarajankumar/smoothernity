85b789c93ddfe8cf6a4a2fd8103975a8b0b3a3a1
olegus8 <olegus8@localhost>
2011-09-06 19:06:48 +0400

    working salutation letters layout

---
+++ src/loadable/consts/reflection/logic/salutation/letters/animation/shy_animation_injections.h
@@ -0,0 +1,8 @@
+#ifndef _shy_loadable_consts_reflection_logic_salutation_letters_animation_injections_included
+#define _shy_loadable_consts_reflection_logic_salutation_letters_animation_injections_included
+
+#include "./shy_animation.h"
+
+typedef shy_loadable_consts_reflection_logic_salutation_letters_animation so_called_loadable_consts_reflection_logic_salutation_letters_animation ;
+
+#endif
