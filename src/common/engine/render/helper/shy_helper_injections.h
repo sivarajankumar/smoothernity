2b34c9e3f65db9a5c8614dc31de4e0be0857d8c7
olegus8 <olegus8@localhost>
2011-09-06 19:23:23 +0400

    engine render helper

---
+++ src/common/engine/render/helper/shy_helper_injections.h
@@ -0,0 +1,12 @@
+#ifndef _shy_common_engine_render_helper_injections_included
+#define _shy_common_engine_render_helper_injections_included
+
+#include "common/engine/render/mesh/id/type/shy_type_injections.h"
+#include "injections/platform/math/num/fract/type/shy_type.h"
+#include "injections/platform/math/num/whole/type/shy_type.h"
+
+#include "./shy_helper.h"
+
+typedef shy_common_engine_render_helper so_called_common_engine_render_helper ;
+
+#endif
