7ade2de781248092969561ef0cc0b26ced9d9ad2
olegus8 <olegus8@localhost>
2011-09-06 17:57:28 +0400

    working salutation letters layout

--- src/common/engine/rect/stateless/shy_stateless.h
+++ src/common/engine/rect/stateless/shy_stateless.h
@@ -1,6 +1,17 @@
 class shy_common_engine_rect_stateless
 {
 public :
+    static void add_border
+        ( so_called_common_engine_rect_type & result
+        , so_called_common_engine_rect_type to_what
+        , so_called_platform_math_num_fract_type border_width
+        , so_called_platform_math_num_fract_type border_height
+        ) ;
+    static void dims
+        ( so_called_platform_math_num_fract_type & width
+        , so_called_platform_math_num_fract_type & height
+        , so_called_common_engine_rect_type of_what
+        ) ;
     static void layout_letters_in_a_row
         ( so_called_platform_vector_data_type & letter_position
         , so_called_platform_math_num_fract_type & letter_scale
@@ -14,25 +25,14 @@ public :
         , so_called_platform_math_num_fract_type aspect_width
         , so_called_platform_math_num_fract_type aspect_height
         ) ;
-    static void add_border
-        ( so_called_common_engine_rect_type & result
-        , so_called_common_engine_rect_type to_what
-        , so_called_platform_math_num_fract_type border_width
-        , so_called_platform_math_num_fract_type border_height
-        ) ;
-    static void dims
-        ( so_called_platform_math_num_fract_type & width
-        , so_called_platform_math_num_fract_type & height
-        , so_called_common_engine_rect_type of_what
+    static void rect_from_axes
+        ( so_called_common_engine_rect_type & rect
+        , so_called_platform_math_num_fract_type half_width
+        , so_called_platform_math_num_fract_type half_height
         ) ;
     static void rect_from_dims
         ( so_called_common_engine_rect_type & rect
         , so_called_platform_math_num_fract_type width
         , so_called_platform_math_num_fract_type height
-        ) ;
-    static void rect_from_axes
-        ( so_called_common_engine_rect_type & rect
-        , so_called_platform_math_num_fract_type half_width
-        , so_called_platform_math_num_fract_type half_height
         ) ;
 } ;
--- src/common/logic/salutation/letters/animation/layout/shy_layout_injections.cpp
+++ src/common/logic/salutation/letters/animation/layout/shy_layout_injections.cpp
@@ -1,13 +1,16 @@
 #include "./shy_layout_injections.h"
 
+#include "common/engine/rect/stateless/shy_stateless_injections.h"
 #include "common/engine/render/aspect/checker/shy_checker_injections.h"
 #include "common/engine/render/aspect/request/sender/shy_sender_injections.h"
 #include "common/engine/taker/helper/shy_helper_injections.h"
 #include "common/logic/salutation/letters/animation/layout/transform/reply/sender/shy_sender_injections.h"
+#include "common/logic/salutation/letters/meshes/consts/shy_consts_injections.h"
 #include "common/logic/salutation/letters/meshes/storage/size/checker/shy_checker_injections.h"
 #include "common/logic/salutation/letters/meshes/storage/size/request/sender/shy_sender_injections.h"
 #include "injections/platform/conditions/shy_conditions.h"
 #include "injections/platform/math/consts/shy_consts.h"
+#include "injections/platform/math/shy_math.h"
 #include "injections/platform/vector/shy_vector.h"
 
 #include "./shy_layout.hpp"
