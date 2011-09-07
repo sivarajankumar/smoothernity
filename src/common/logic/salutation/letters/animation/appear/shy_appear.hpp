07c7944a3eaeddc5fd47d6875ba7e9e779ab49e1
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-07 19:21:55 +0400

    logic_salutation_letters_animation_appear transformation

--- src/common/logic/salutation/letters/animation/appear/shy_appear.hpp
+++ src/common/logic/salutation/letters/animation/appear/shy_appear.hpp
@@ -18,9 +18,30 @@ void _shy_common_logic_salutation_letters_animation_appear :: receive ( so_calle
 
 void _shy_common_logic_salutation_letters_animation_appear :: receive ( so_called_common_logic_salutation_letters_animation_appear_transform_request_message msg )
 {
+    so_called_platform_math_num_fract_type scale ;
+    so_called_platform_math_num_fract_type letter_index ;
+    so_called_platform_math_num_fract_type letter_time_shift ;
+    so_called_platform_math_num_fract_type shifted_time ;
+    so_called_platform_math_num_fract_type time_shift_per_letter ;
+
+    time_shift_per_letter = so_called_common_logic_salutation_letters_animation_consts :: appear_time_shift_per_letter ;
+
+    so_called_platform_math :: make_fract_from_whole ( letter_index , msg . letter ) ;
+    so_called_platform_math :: mul_fracts ( letter_time_shift , letter_index , time_shift_per_letter ) ;
+    so_called_platform_math :: sub_fracts ( shifted_time , shy_guts :: time , letter_time_shift ) ;
+
+    so_called_common_engine_math_stateless :: hard_in_easy_out
+        ( scale
+        , shifted_time
+        , so_called_common_logic_salutation_letters_animation_consts :: appear_scale_begin
+        , so_called_platform_math_consts :: fract_0
+        , so_called_common_logic_salutation_letters_animation_consts :: appear_scale_end
+        , so_called_common_logic_salutation_letters_animation_consts :: appear_time
+        ) ;
+
     so_called_common_logic_salutation_letters_animation_appear_transform_reply_message msg_reply ;
     msg_reply . letter = msg . letter ;
-    msg_reply . scale = so_called_platform_math_consts :: fract_1 ;
+    msg_reply . scale = scale ;
     so_called_common_logic_salutation_letters_animation_appear_transform_reply_sender :: send ( msg_reply ) ;
 }
 
