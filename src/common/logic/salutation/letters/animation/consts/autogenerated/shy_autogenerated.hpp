0e4be328e7a41198bb9521817c7b734c52ff4f05
olegus8 <olegus8@localhost>
2011-09-07 14:14:12 +0400

    working on salutation letters appear animation

--- src/common/logic/salutation/letters/animation/consts/autogenerated/shy_autogenerated.hpp
+++ src/common/logic/salutation/letters/animation/consts/autogenerated/shy_autogenerated.hpp
@@ -2,7 +2,7 @@
 
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: appear_scale_begin = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: appear_scale_end = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
-const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: appear_time = so_called_platform_math :: init_num_fract ( 1 , 2 ) ;
+const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: appear_time = so_called_platform_math :: init_num_fract ( 5 , 10 ) ;
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: appear_time_shift_per_letter = so_called_platform_math :: init_num_fract ( 1 , 10 ) ;
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: layout_border_height = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: layout_border_width = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
