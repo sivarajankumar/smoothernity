6ca95d07e5a850435e42a572e6f8d10cfeb015a7
olegus8 <olegus8@localhost>
2011-09-07 13:55:34 +0400

    ignore xcode files

--- src/main/macosx/build_development.sh
+++ src/main/macosx/build_development.sh
@@ -1,4 +1,2 @@
 rm ../../../../build/macosx/Development/Smoothernity.app/Contents/MacOS/Smoothernity
-rm ./smoothernity.xcodeproj/*.pbxuser
-rm ./smoothernity.xcodeproj/*.mode1v3
 xcodebuild -project smoothernity.xcodeproj -target "smoothernity" -configuration Development build
