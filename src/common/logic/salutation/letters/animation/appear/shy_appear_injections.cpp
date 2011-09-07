f93072987cf27c5d6a036d74c227b96a41082652
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-07 19:21:55 +0400

    logic_salutation_letters_animation_appear transformation

--- src/common/logic/salutation/letters/animation/appear/shy_appear_injections.cpp
+++ src/common/logic/salutation/letters/animation/appear/shy_appear_injections.cpp
@@ -2,6 +2,8 @@
 
 #include "common/engine/math/stateless/shy_stateless_injections.h"
 #include "common/logic/salutation/letters/animation/appear/transform/reply/sender/shy_sender_injections.h"
+#include "common/logic/salutation/letters/animation/consts/shy_consts_injections.h"
 #include "injections/platform/math/consts/shy_consts.h"
+#include "injections/platform/math/shy_math.h"
 
 #include "./shy_appear.hpp"
