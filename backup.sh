f8ba868df29f66def11a417c9088fe94ab9a8bf2
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-07 17:20:34 +0400

    backup wiki too

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 FILE=smoothernity-backup-`date "+%Y-%m-%d"`.tar.bz2
 rm $FILE
 echo Creating backup \"$FILE\"...
-tar --exclude .hg -cjf $FILE `dirname $0`
+tar --exclude .hg -cjf $FILE `dirname $0`/../repo `dirname $0`/../repo.wiki
 echo Done.
 md5 $FILE
