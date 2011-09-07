7e3efd4d2320e7b36941b7ec411d28e1993df5d6
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-07 17:15:42 +0400

    xcode files revert

--- src/main/macosx/build_deployment.sh
+++ src/main/macosx/build_deployment.sh
@@ -1,3 +1,5 @@
 rm ../../../../build/macosx/Deployment/Smoothernity.app/Contents/MacOS/Smoothernity
+rm ./smoothernity.xcodeproj/*.pbxuser
+rm ./smoothernity.xcodeproj/*.mode1v3
 xcodebuild -project smoothernity.xcodeproj -target "smoothernity" -configuration Deployment build
 strip -SXx ../../../../build/macosx/Deployment/Smoothernity.app/Contents/MacOS/Smoothernity
