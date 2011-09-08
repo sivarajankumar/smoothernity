04064ad57a750cc6fae3bd788342c9dbbc759b2b
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-08 13:05:25 +0400

    logic_salutation_letters_animation_disappear consts

--- src/loadable/consts/reflection/logic/salutation/letters/animation/shy_animation.hpp
+++ src/loadable/consts/reflection/logic/salutation/letters/animation/shy_animation.hpp
@@ -23,6 +23,26 @@ void shy_loadable_consts_reflection_logic_salutation_letters_animation :: prepar
     shy_guts_bind_value_min ( 0 , 1 ) ;
     shy_guts_bind_value_max ( 10 , 1 ) ;
 
+    shy_guts_bind_value ( disappear_scale_begin ) ;
+    shy_guts_bind_value_min ( 0 , 1 ) ;
+    shy_guts_bind_value_max ( 10 , 1 ) ;
+
+    shy_guts_bind_value ( disappear_scale_cut_off ) ;
+    shy_guts_bind_value_min ( 0 , 1 ) ;
+    shy_guts_bind_value_max ( 10 , 1 ) ;
+
+    shy_guts_bind_value ( disappear_scale_end ) ;
+    shy_guts_bind_value_min ( 0 , 1 ) ;
+    shy_guts_bind_value_max ( 10 , 1 ) ;
+
+    shy_guts_bind_value ( disappear_time ) ;
+    shy_guts_bind_value_min ( 1 , 10 ) ;
+    shy_guts_bind_value_max ( 10 , 1 ) ;
+
+    shy_guts_bind_value ( disappear_time_shift_per_letter ) ;
+    shy_guts_bind_value_min ( 0 , 1 ) ;
+    shy_guts_bind_value_max ( 10 , 1 ) ;
+
     shy_guts_bind_value ( layout_border_height ) ;
     shy_guts_bind_value_min ( 0 , 1 ) ;
     shy_guts_bind_value_max ( 10 , 1 ) ;
