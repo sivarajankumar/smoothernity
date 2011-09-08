32d8c6f3aced1a5ac8920dd60b79f926db6a321a
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-08 13:05:25 +0400

    logic_salutation_letters_animation_disappear consts

--- src/common/logic/salutation/letters/animation/consts/autogenerated/shy_autogenerated.hpp
+++ src/common/logic/salutation/letters/animation/consts/autogenerated/shy_autogenerated.hpp
@@ -4,6 +4,11 @@ const so_called_platform_math_num_fract_type so_called_common_logic_salutation_l
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: appear_scale_end = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: appear_time = so_called_platform_math :: init_num_fract ( 5 , 10 ) ;
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: appear_time_shift_per_letter = so_called_platform_math :: init_num_fract ( 1 , 10 ) ;
+const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: disappear_scale_begin = so_called_platform_math :: init_num_fract ( 1 , 1 ) ;
+const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: disappear_scale_cut_off = so_called_platform_math :: init_num_fract ( 1 , 10 ) ;
+const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: disappear_scale_end = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
+const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: disappear_time = so_called_platform_math :: init_num_fract ( 5 , 10 ) ;
+const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: disappear_time_shift_per_letter = so_called_platform_math :: init_num_fract ( 1 , 10 ) ;
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: layout_border_height = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: layout_border_width = so_called_platform_math :: init_num_fract ( 0 , 1 ) ;
 const so_called_platform_math_num_fract_type so_called_common_logic_salutation_letters_animation_consts :: layout_step = so_called_platform_math :: init_num_fract ( 1 , 10 ) ;
--- src/common/logic/salutation/letters/animation/consts/shy_consts.h
+++ src/common/logic/salutation/letters/animation/consts/shy_consts.h
@@ -5,6 +5,11 @@ public :
     static const so_called_platform_math_num_fract_type appear_scale_end ;
     static const so_called_platform_math_num_fract_type appear_time ;
     static const so_called_platform_math_num_fract_type appear_time_shift_per_letter ;
+    static const so_called_platform_math_num_fract_type disappear_scale_begin ;
+    static const so_called_platform_math_num_fract_type disappear_scale_cut_off ;
+    static const so_called_platform_math_num_fract_type disappear_scale_end ;
+    static const so_called_platform_math_num_fract_type disappear_time ;
+    static const so_called_platform_math_num_fract_type disappear_time_shift_per_letter ;
     static const so_called_platform_math_num_fract_type layout_border_height ;
     static const so_called_platform_math_num_fract_type layout_border_width ;
     static const so_called_platform_math_num_fract_type layout_step ;
