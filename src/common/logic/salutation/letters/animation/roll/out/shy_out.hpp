e2148cf64a9b0743cc03730594bdf154d3e2bc3d
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-12 19:58:42 +0400

    logic_salutation_letters_animation_roll_in transform

--- src/common/logic/salutation/letters/animation/roll/out/shy_out.hpp
+++ src/common/logic/salutation/letters/animation/roll/out/shy_out.hpp
@@ -23,11 +23,50 @@ void _shy_common_logic_salutation_letters_animation_roll_out :: receive ( so_cal
 
 void _shy_common_logic_salutation_letters_animation_roll_out :: receive ( so_called_common_logic_salutation_letters_animation_roll_out_transform_request_message msg )
 {
+    so_called_platform_math_num_fract_type position_radius ;
+    so_called_platform_math_num_fract_type position_spin_periods ;
+    so_called_platform_math_num_fract_type rotation_periods ;
+    so_called_platform_math_num_fract_type shifted_time ;
+
+    so_called_common_engine_math_stateless :: shift
+        ( shifted_time
+        , shy_guts :: time
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_out_time_shift_per_letter
+        , msg . letter
+        ) ;
+
+    so_called_common_engine_math_stateless :: hard_in_easy_out
+        ( position_radius
+        , shifted_time
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_out_position_radius_begin
+        , so_called_platform_math_consts :: fract_0
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_out_position_radius_end
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_out_time
+        ) ;
+
+    so_called_common_engine_math_stateless :: hard_in_easy_out
+        ( position_spin_periods
+        , shifted_time
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_out_position_spin_periods_begin
+        , so_called_platform_math_consts :: fract_0
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_out_position_spin_periods_end
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_out_time
+        ) ;
+
+    so_called_common_engine_math_stateless :: hard_in_easy_out
+        ( rotation_periods
+        , shifted_time
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_out_rotation_periods_begin
+        , so_called_platform_math_consts :: fract_0
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_out_rotation_periods_end
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_out_time
+        ) ;
+
     so_called_common_logic_salutation_letters_animation_roll_out_transform_reply_message msg_reply ;
     msg_reply . letter = msg . letter ;
-    msg_reply . position_radius = so_called_platform_math_consts :: fract_0 ;
-    msg_reply . position_spin_periods = so_called_platform_math_consts :: fract_0 ;
-    msg_reply . rotation_periods = so_called_platform_math_consts :: fract_0 ;
+    msg_reply . position_radius = position_radius ;
+    msg_reply . position_spin_periods = position_spin_periods ;
+    msg_reply . rotation_periods = rotation_periods ;
     so_called_common_logic_salutation_letters_animation_roll_out_transform_reply_sender :: send ( msg_reply ) ;
 }
 
