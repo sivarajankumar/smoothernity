f9eca059ec6a8238783a2cd99e1788986eb97cf2
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-12 19:54:50 +0400

    logic_salutation_letters_animation_roll_in transform

--- src/common/logic/salutation/letters/animation/roll/in/shy_in.hpp
+++ src/common/logic/salutation/letters/animation/roll/in/shy_in.hpp
@@ -26,10 +26,41 @@ void _shy_common_logic_salutation_letters_animation_roll_in :: receive ( so_call
     so_called_platform_math_num_fract_type position_radius ;
     so_called_platform_math_num_fract_type position_spin_periods ;
     so_called_platform_math_num_fract_type rotation_periods ;
+    so_called_platform_math_num_fract_type shifted_time ;
 
-    position_radius = so_called_platform_math_consts :: fract_0 ;
-    position_spin_periods = so_called_platform_math_consts :: fract_0 ;
-    rotation_periods = so_called_platform_math_consts :: fract_0 ;
+    so_called_common_engine_math_stateless :: shift
+        ( shifted_time
+        , shy_guts :: time
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_in_time_shift_per_letter
+        , msg . letter
+        ) ;
+
+    so_called_common_engine_math_stateless :: hard_in_easy_out
+        ( position_radius
+        , shifted_time
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_in_position_radius_begin
+        , so_called_platform_math_consts :: fract_0
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_in_position_radius_end
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_in_time
+        ) ;
+
+    so_called_common_engine_math_stateless :: hard_in_easy_out
+        ( position_spin_periods
+        , shifted_time
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_in_position_spin_periods_begin
+        , so_called_platform_math_consts :: fract_0
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_in_position_spin_periods_end
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_in_time
+        ) ;
+
+    so_called_common_engine_math_stateless :: hard_in_easy_out
+        ( rotation_periods
+        , shifted_time
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_in_rotation_periods_begin
+        , so_called_platform_math_consts :: fract_0
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_in_rotation_periods_end
+        , so_called_common_logic_salutation_letters_animation_consts :: roll_in_time
+        ) ;
 
     so_called_common_logic_salutation_letters_animation_roll_in_transform_reply_message msg_reply ;
     msg_reply . letter = msg . letter ;
--- src/common/logic/salutation/letters/animation/roll/in/shy_in_injections.cpp
+++ src/common/logic/salutation/letters/animation/roll/in/shy_in_injections.cpp
@@ -1,6 +1,7 @@
 #include "./shy_in_injections.h"
 
 #include "common/engine/math/stateless/shy_stateless_injections.h"
+#include "common/logic/salutation/letters/animation/consts/shy_consts_injections.h"
 #include "common/logic/salutation/letters/animation/roll/in/transform/reply/sender/shy_sender_injections.h"
 #include "injections/platform/math/consts/shy_consts.h"
 
