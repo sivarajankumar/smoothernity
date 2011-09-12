7d66a50d9a48e85885e51f89f9b331c0773d299e
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-12 19:58:42 +0400

    logic_salutation_letters_animation_roll_in transform

--- src/common/logic/salutation/letters/animation/roll/out/shy_out_injections.cpp
+++ src/common/logic/salutation/letters/animation/roll/out/shy_out_injections.cpp
@@ -1,6 +1,7 @@
 #include "./shy_out_injections.h"
 
 #include "common/engine/math/stateless/shy_stateless_injections.h"
+#include "common/logic/salutation/letters/animation/consts/shy_consts_injections.h"
 #include "common/logic/salutation/letters/animation/roll/out/transform/reply/sender/shy_sender_injections.h"
 #include "injections/platform/math/consts/shy_consts.h"
 
