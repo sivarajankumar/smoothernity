693c8a8b667b60c246b6788339f610dcaf300834
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-12 19:23:14 +0400

    time step for logic_salutation_letters_animation_roll in and out

--- src/common/logic/salutation/letters/animation/roll/in/shy_in_injections.cpp
+++ src/common/logic/salutation/letters/animation/roll/in/shy_in_injections.cpp
@@ -1,5 +1,6 @@
 #include "./shy_in_injections.h"
 
+#include "common/engine/math/stateless/shy_stateless_injections.h"
 #include "common/logic/salutation/letters/animation/roll/in/transform/reply/sender/shy_sender_injections.h"
 #include "injections/platform/math/consts/shy_consts.h"
 
