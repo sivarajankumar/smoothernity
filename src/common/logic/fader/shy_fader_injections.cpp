d29a1d9470ecfffe99592d44486b1595b013bdd8
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-14 19:17:42 +0400

    fader logic

--- src/common/logic/fader/shy_fader_injections.cpp
+++ src/common/logic/fader/shy_fader_injections.cpp
@@ -1,7 +1,10 @@
 #include "./shy_fader_injections.h"
 
+#include "common/engine/math/stateless/shy_stateless_injections.h"
 #include "common/engine/render/clear/screen/sender/shy_sender_injections.h"
 #include "common/logic/fader/consts/shy_consts_injections.h"
 #include "common/logic/fader/finished/sender/shy_sender_injections.h"
+#include "injections/platform/conditions/shy_conditions.h"
+#include "injections/platform/math/consts/shy_consts.h"
 
 #include "./shy_fader.hpp"
