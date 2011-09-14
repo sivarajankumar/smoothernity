f9fa19e8b72ef8c1606af9839b1b47579a9aca85
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-14 19:06:59 +0400

    fader consts

--- src/loadable/consts/reflection/logic/fader/shy_fader.hpp
+++ src/loadable/consts/reflection/logic/fader/shy_fader.hpp
@@ -7,15 +7,31 @@ void shy_loadable_consts_reflection_logic_fader :: prepare ( )
 {
     shy_guts_bind_module ( logic_fader ) ;
 
-    shy_guts_bind_value ( color_r ) ;
+    shy_guts_bind_value ( color_begin_r ) ;
     shy_guts_bind_value_min ( 0 , 1 ) ;
     shy_guts_bind_value_max ( 1 , 1 ) ;
 
-    shy_guts_bind_value ( color_g ) ;
+    shy_guts_bind_value ( color_begin_g ) ;
     shy_guts_bind_value_min ( 0 , 1 ) ;
     shy_guts_bind_value_max ( 1 , 1 ) ;
 
-    shy_guts_bind_value ( color_b ) ;
+    shy_guts_bind_value ( color_begin_b ) ;
     shy_guts_bind_value_min ( 0 , 1 ) ;
     shy_guts_bind_value_max ( 1 , 1 ) ;
+
+    shy_guts_bind_value ( color_end_r ) ;
+    shy_guts_bind_value_min ( 0 , 1 ) ;
+    shy_guts_bind_value_max ( 1 , 1 ) ;
+
+    shy_guts_bind_value ( color_end_g ) ;
+    shy_guts_bind_value_min ( 0 , 1 ) ;
+    shy_guts_bind_value_max ( 1 , 1 ) ;
+
+    shy_guts_bind_value ( color_end_b ) ;
+    shy_guts_bind_value_min ( 0 , 1 ) ;
+    shy_guts_bind_value_max ( 1 , 1 ) ;
+
+    shy_guts_bind_value ( fade_time ) ;
+    shy_guts_bind_value_min ( 1 , 10 ) ;
+    shy_guts_bind_value_max ( 10 , 1 ) ;
 }
