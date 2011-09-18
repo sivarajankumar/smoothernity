243af14fed5add421c6b71ba402cd40cbd8dd293
?????????? <??????????@??????????-??>
2011-09-18 17:08:27 +0400

    ansi output on windows

--- src/main/win/platform/terminal/consts/shy_consts_injections.cpp
+++ src/main/win/platform/terminal/consts/shy_consts_injections.cpp
@@ -1,3 +1,3 @@
 #ifdef shy_build_with_trace
-    #include "src/platform/terminal/consts/stub/shy_stub_injections.hpp"
+    #include "src/platform/terminal/consts/ansi/shy_ansi_injections.hpp"
 #endif
