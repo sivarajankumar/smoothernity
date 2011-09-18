ce5d3c0d527f451a2466cc4d18936016376b5117
?????????? <??????????@??????????-??>
2011-09-18 17:08:27 +0400

    ansi output on windows

--- src/main/win/platform/terminal/consts/shy_consts_injections.h
+++ src/main/win/platform/terminal/consts/shy_consts_injections.h
@@ -2,8 +2,8 @@
 #define _shy_win_platform_terminal_consts_injections_included
 
 #ifdef shy_build_with_trace
-    #include "src/platform/terminal/consts/stub/shy_stub_injections.h"
-    typedef so_called_platform_terminal_consts_stub so_called_platform_terminal_consts ;
+    #include "src/platform/terminal/consts/ansi/shy_ansi_injections.h"
+    typedef so_called_platform_terminal_consts_ansi so_called_platform_terminal_consts ;
 #endif
 
 #endif
