3beaaad778c4ce5da59325ff8caaa87d67033337
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-20 16:44:54 +0400

    math profiler stub

--- src/platform/profile/insider/shy_insider.h
+++ src/platform/profile/insider/shy_insider.h
@@ -1,7 +1,9 @@
-class shy_platform_profiler
+class shy_platform_profile_insider
 {
 public :
+    static void flush ( ) ;
     static void id_value_add ( so_called_platform_math_num_whole_type & , so_called_platform_math_num_whole_type ) ;
     static void id_value_max ( so_called_platform_math_num_whole_type & , so_called_platform_math_num_whole_type ) ;
-    static void make_name_id ( so_called_platform_math_num_whole_type & , const so_called_lib_std_char * ) ;
+    static void next_frame ( ) ;
+    static void register_name_id ( so_called_platform_math_num_whole_type , const so_called_lib_std_char * ) ;
 } ;
---
+++ src/profile/platform/math/shy_math.hpp
@@ -0,0 +1,48 @@
+void shy_profile_platform_math :: fract_add_sub ( )
+{
+}
+
+void shy_profile_platform_math :: fract_div ( )
+{
+}
+
+void shy_profile_platform_math :: fract_make ( )
+{
+}
+
+void shy_profile_platform_math :: fract_make_from_whole ( )
+{
+}
+
+void shy_profile_platform_math :: fract_mul ( )
+{
+}
+
+void shy_profile_platform_math :: fract_sin_cos ( )
+{
+}
+
+void shy_profile_platform_math :: whole_add_sub ( )
+{
+}
+
+void shy_profile_platform_math :: whole_bitwise ( )
+{
+}
+
+void shy_profile_platform_math :: whole_div_mod ( )
+{
+}
+
+void shy_profile_platform_math :: whole_make ( )
+{
+}
+
+void shy_profile_platform_math :: whole_make_from_fract ( )
+{
+}
+
+void shy_profile_platform_math :: whole_mul ( )
+{
+}
+
