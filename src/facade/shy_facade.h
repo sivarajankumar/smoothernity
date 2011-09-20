f57fba8dc91969250239b13e4f3e7745fe3e02df
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-20 17:48:55 +0400

    next_frame event for facade

--- src/facade/shy_facade.h
+++ src/facade/shy_facade.h
@@ -3,7 +3,6 @@ class shy_facade
 public :
     static void init ( ) ;
     static void done ( ) ;
-    static void update ( ) ;
-    static void render ( ) ;
+    static void next_frame ( ) ;
     static void video_mode_changed ( ) ;
 } ;
