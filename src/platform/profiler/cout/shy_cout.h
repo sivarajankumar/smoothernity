b5889c5960c29a83fc436ab4422ab9d7c7801485
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-20 15:47:09 +0400

    profiler draft

---
+++ src/platform/profiler/cout/shy_cout.h
@@ -0,0 +1,7 @@
+class shy_platform_profiler_cout
+{
+public :
+    static void id_value_add ( so_called_platform_math_num_whole_type & , so_called_platform_math_num_whole_type ) ;
+    static void id_value_max ( so_called_platform_math_num_whole_type & , so_called_platform_math_num_whole_type ) ;
+    static void make_name_id ( so_called_platform_math_num_whole_type & , const so_called_lib_std_char * ) ;
+} ;
