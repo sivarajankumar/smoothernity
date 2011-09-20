a304b6ec784ac03820420a2ed47c24ea1c643ed0
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-20 20:00:27 +0400

    did finish launching event

--- src/main/macosx/shy_application_delegate.mm
+++ src/main/macosx/shy_application_delegate.mm
@@ -1,11 +1,17 @@
 #include "./shy_application_delegate.h"
+#include <iostream>
 
 @implementation shy_application_delegate
 
 - ( NSApplicationTerminateReply ) applicationShouldTerminate : ( NSApplication * ) sender
 {
-    NSLog ( @"should terminate" ) ;
+    std :: cerr << "should terminate" << std :: endl ;
     return NSTerminateNow ;
+}
+
+- ( void ) applicationDidFinishLaunching : ( NSNotification * ) notification
+{
+    std :: cerr << "did finish launching" << std :: endl ;
 }
 
 @end
