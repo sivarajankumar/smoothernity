91a49d4801db9f25f32e2a53b6eccd0d6bf43395
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-21 18:53:17 +0400

    universal file generation via python

--- src/loadable/generator/python/shy_python_injections.cpp
+++ src/loadable/generator/python/shy_python_injections.cpp
@@ -3,6 +3,7 @@
     #include "src/injections/lib/std/bool/shy_bool.h"
     #include "src/injections/lib/std/true/shy_true.h"
     #include "src/injections/platform/trace/consts/shy_consts.h"
+    #include "src/loadable/generator/consts/shy_consts_injections.h"
     #include "src/trace/shy_trace_injections.h"
     #include "./shy_python.hpp"
 #endif
