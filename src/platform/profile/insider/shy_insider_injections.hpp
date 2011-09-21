5bb088a0146fd6af372351b4d03c6c084f173deb
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-21 18:28:53 +0400

    profiler counting facilities

--- src/platform/profile/insider/shy_insider_injections.hpp
+++ src/platform/profile/insider/shy_insider_injections.hpp
@@ -3,6 +3,7 @@
 #include "src/injections/lib/std/int32/t/shy_t.h"
 #include "src/injections/lib/std/map/shy_map.h"
 #include "src/injections/lib/std/string/shy_string.h"
+#include "src/injections/lib/std/vector/shy_vector.h"
 #include "src/injections/platform/math/insider/shy_insider.h"
 
 #include "./shy_insider.hpp"
