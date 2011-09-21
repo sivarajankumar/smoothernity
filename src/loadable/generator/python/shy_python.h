d9eed305999d3470c162a1d103512150794db631
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-21 19:12:29 +0400

    accumulating python generator

--- src/loadable/generator/python/shy_python.h
+++ src/loadable/generator/python/shy_python.h
@@ -1,6 +1,7 @@
 class shy_loadable_generator_python
 {
 public :
+    static void append ( so_called_lib_std_string ) ;
     static void generate_file ( so_called_lib_std_string & , so_called_lib_std_string , so_called_lib_std_string ) ;
-    static void main_script ( so_called_lib_std_string & , so_called_lib_std_string ) ;
+    static void main_script ( so_called_lib_std_string & ) ;
 } ;
