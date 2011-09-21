7733e844ac43d9f10e1e6dfd926c2db5c90c0f35
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-21 17:50:58 +0400

    id generation for profiler

--- src/platform/profile/insider/shy_insider_injections.hpp
+++ src/platform/profile/insider/shy_insider_injections.hpp
@@ -1,2 +1,8 @@
 #include "./shy_insider_injections.h"
+
+#include "src/injections/lib/std/int32/t/shy_t.h"
+#include "src/injections/lib/std/map/shy_map.h"
+#include "src/injections/lib/std/string/shy_string.h"
+#include "src/injections/platform/math/insider/shy_insider.h"
+
 #include "./shy_insider.hpp"
