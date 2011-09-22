5857f960fbc69635a44c7eb36a770fd36f164dc8
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-22 20:05:55 +0400

    more precise validation

--- src/define/validation/shy_validation.h
+++ src/define/validation/shy_validation.h
@@ -1,17 +1,48 @@
 #ifdef shy_build_loadable_way
     #ifndef shy_build_with_stl
-        #error Loadable build requires STL.
+        #error "Loadable build requires STL."
     #endif
 #endif
 
 #ifdef shy_build_with_trace
     #ifndef shy_build_with_stl
-        #error Build with tracing requires STL.
+        #error "Build with tracing requires STL."
     #endif
 #endif
 
 #ifdef shy_build_with_profile
     #ifndef shy_build_with_stl
-        #error Build with profiling requires STL.
+        #error "Build with profiling requires STL."
     #endif
+#endif
+
+#if ! ( ( (   defined shy_build_loadable_way ) && ( ! defined shy_build_static_way ) ) \
+     || ( ( ! defined shy_build_loadable_way ) && (   defined shy_build_static_way ) ) \
+      )
+    #error "External data handling style must be specified."
+#endif
+
+#if ! ( ( (   defined shy_build_with_profile ) && ( ! defined shy_build_without_profile ) ) \
+     || ( ( ! defined shy_build_with_profile ) && (   defined shy_build_without_profile ) ) \
+      )
+    #error "Profiling mode must be specified."
+#endif
+
+#if ! ( ( (   defined shy_build_with_trace ) && ( ! defined shy_build_without_trace ) ) \
+     || ( ( ! defined shy_build_with_trace ) && (   defined shy_build_without_trace ) ) \
+      )
+    #error "Tracing mode must be specified."
+#endif
+
+#if ! ( ( (   defined shy_scheduling_mode_random ) && ( ! defined shy_scheduling_mode_direct_call ) ) \
+     || ( ( ! defined shy_scheduling_mode_random ) && (   defined shy_scheduling_mode_direct_call ) ) \
+      )
+    #error "Scheduling mode must be specified."
+#endif
+
+#if ! ( ( (   defined shy_build_for_iphone ) && ( ! defined shy_build_for_macosx ) && ( ! defined shy_build_for_win ) ) \
+     || ( ( ! defined shy_build_for_iphone ) && (   defined shy_build_for_macosx ) && ( ! defined shy_build_for_win ) ) \
+     || ( ( ! defined shy_build_for_iphone ) && ( ! defined shy_build_for_macosx ) && (   defined shy_build_for_win ) ) \
+      )
+    #error "Platform for build must be specified."
 #endif
