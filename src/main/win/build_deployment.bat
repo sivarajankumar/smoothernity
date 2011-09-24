7b47d70d6565005f76a1c6fbbcf0bc413aae5ea2
olegus8 <olegus8@localhost>
2011-09-24 18:51:26 +0400

    projects for parallel build

--- src/main/win/build_deployment.bat
+++ src/main/win/build_deployment.bat
@@ -1,6 +1,7 @@
 del /q /s ..\..\..\..\build\windows\release
 msbuild smoothernity.sln /p:Configuration=Release
 del /q /s /a ipch
+del /q /s /a *.user
 del /q /s /a *.suo
 del /q /s /a *.sdf
 del /q /s /a *.opensdf
--- src/main/win/build_development.bat
+++ src/main/win/build_development.bat
@@ -1,6 +1,7 @@
 del /q /s ..\..\..\..\build\windows\debug
 msbuild smoothernity.sln /p:Configuration=Debug
 del /q /s /a ipch
+del /q /s /a *.user
 del /q /s /a *.suo
 del /q /s /a *.sdf
 del /q /s /a *.opensdf
