895b80962d3e8a6b2ca21c89dd5db60ddfe14a76
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-26 19:07:47 +0400

    platform_math prefix for profile

--- src/profile/platform/math/shy_math.hpp
+++ src/profile/platform/math/shy_math.hpp
@@ -23,7 +23,7 @@ void shy_guts :: profile ( so_called_platform_math_num_whole_type id )
 
 void shy_profile_platform_math :: init ( )
 {
-#define make_name_id_helper(x) so_called_platform_profile :: make_name_id ( shy_guts :: id_##x , #x )
+#define make_name_id_helper(x) so_called_platform_profile :: make_name_id ( shy_guts :: id_##x , "platform_math_" #x )
     make_name_id_helper ( fract_add_sub ) ;
     make_name_id_helper ( fract_div ) ;
     make_name_id_helper ( fract_make ) ;
