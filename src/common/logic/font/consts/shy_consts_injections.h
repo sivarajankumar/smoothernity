e4408f96028041724c183c39974440f148d1d650
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-09-30 18:49:39 +0400

    font generation takes real time

---
+++ src/common/logic/font/consts/shy_consts_injections.h
@@ -0,0 +1,10 @@
+#ifndef _shy_common_logic_font_consts_injections_included
+#define _shy_common_logic_font_consts_injections_included
+
+#include "src/injections/platform/math/const/int/32/type/shy_type.h"
+#include "src/injections/platform/math/num/whole/type/shy_type.h"
+#include "./shy_consts.h"
+
+typedef shy_common_logic_font_consts so_called_common_logic_font_consts ;
+
+#endif
--- src/common/logic/font/mesh/generator/shy_generator_injections.cpp
+++ src/common/logic/font/mesh/generator/shy_generator_injections.cpp
@@ -1,5 +1,11 @@
 #include "./shy_generator_injections.h"
 
+#include "src/common/engine/math/stateless/shy_stateless_injections.h"
+#include "src/common/logic/font/consts/shy_consts_injections.h"
+#include "src/common/logic/font/mesh/consts/shy_consts_injections.h"
 #include "src/common/logic/font/mesh/generator/generate/finished/sender/shy_sender_injections.h"
+#include "src/injections/platform/conditions/shy_conditions.h"
+#include "src/injections/platform/math/consts/shy_consts.h"
+#include "src/injections/platform/math/shy_math.h"
 
 #include "./shy_generator.hpp"
