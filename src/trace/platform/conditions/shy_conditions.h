8d330780903d8e4de0f0e659acc346e00adfaac2
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-10-04 19:22:23 +0400

    platform conditions args check

---
+++ src/trace/platform/conditions/shy_conditions.h
@@ -0,0 +1,6 @@
+class shy_trace_platform_conditions
+{
+public :
+    static void check_uninitialized ( so_called_platform_math_num_fract_type ) ;
+    static void check_uninitialized ( so_called_platform_math_num_whole_type ) ;
+} ;
---
+++ src/trace/platform/conditions/shy_conditions_injections.h
@@ -0,0 +1,11 @@
+#ifndef _shy_trace_platform_conditions_injections_included
+#define _shy_trace_platform_conditions_injections_included
+
+#ifdef shy_build_with_trace
+    #include "src/injections/platform/math/num/fract/type/shy_type.h"
+    #include "src/injections/platform/math/num/whole/type/shy_type.h"
+    #include "./shy_conditions.h"
+    typedef shy_trace_platform_conditions so_called_trace_platform_conditions ;
+#endif
+
+#endif
