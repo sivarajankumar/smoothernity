30027152e76063f2942f2cc65c39645085eae132
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-10-06 18:59:55 +0400

    check uninitialized values refactored

--- src/trace/platform/conditions/shy_conditions_injections.cpp
+++
@@ -1,8 +0,0 @@
-#ifdef shy_build_with_trace
-    #include "./shy_conditions_injections.h"
-    #include "src/injections/lib/std/bool/shy_bool.h"
-    #include "src/injections/lib/std/true/shy_true.h"
-    #include "src/injections/platform/math/insider/shy_insider.h"
-    #include "src/injections/platform/trace/shy_trace.h"
-    #include "./shy_conditions.hpp"
-#endif
