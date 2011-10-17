99cbc60806aae7a11d2a1f652c2f1211527c30e3
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-10-17 19:06:06 +0400

    platform vector check uninitialized args

--- src/platform/vector/float/shy_float_injections.hpp
+++ src/platform/vector/float/shy_float_injections.hpp
@@ -5,6 +5,9 @@
 #include "src/injections/platform/math/insider/shy_insider.h"
 #include "src/profile/platform/vector/shy_vector_injections.h"
 #include "src/profile/shy_profile_injections.h"
+#include "src/trace/platform/math/shy_math_injections.h"
+#include "src/trace/platform/vector/shy_vector_injections.h"
+#include "src/trace/shy_trace_injections.h"
 
 #include "./shy_float.hpp"
 
