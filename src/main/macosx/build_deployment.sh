5c9937cf8215f9859f849cf0f6dc5a8c6605f6f9
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-10-25 09:45:23 +0400

    project upgrade

--- src/main/macosx/build_deployment.sh
+++ src/main/macosx/build_deployment.sh
@@ -1,5 +1,7 @@
 rm ../../../../build/macosx/Deployment/Smoothernity.app/Contents/MacOS/Smoothernity
 rm ./smoothernity.xcodeproj/*.pbxuser
 rm ./smoothernity.xcodeproj/*.mode1v3
+rm -rf ./smoothernity.xcodeproj/xcuserdata/
+rm -rf ./smoothernity.xcodeproj/project.xcworkspace/xcuserdata/
 xcodebuild -project smoothernity.xcodeproj -target "smoothernity" -configuration Deployment build
 strip -SXx ../../../../build/macosx/Deployment/Smoothernity.app/Contents/MacOS/Smoothernity
