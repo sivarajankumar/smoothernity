6442c71b151c1fa257687c8c17df35bcfeae8f31
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-11-28 18:47:38 +0400

    platform render texture loader check load_resource args

--- src/trace/platform/render/texture/loader/shy_loader.h
+++ src/trace/platform/render/texture/loader/shy_loader.h
@@ -1,5 +1,31 @@
 class shy_trace_platform_render_texture_loader
 {
 public :
+    template < typename texels_array >
+    static void check_args_load_resource
+        ( so_called_platform_render_texture_loader_cocoa_resource_id_type resource_id 
+        , so_called_platform_math_num_whole_type size_pow2_base 
+        , texels_array & data 
+        ) ;
+
     static void check_resource_id_uninitialized ( so_called_platform_render_texture_loader_resource_id_type ) ;
+
+private :
+    static void _check_args_load_resource
+        ( so_called_platform_render_texture_loader_cocoa_resource_id_type resource_id 
+        , so_called_platform_math_num_whole_type size_pow2_base 
+        , so_called_lib_std_int32_t texels_count
+        ) ;
 } ;
+
+template < typename texels_array >
+void shy_trace_platform_render_texture_loader :: check_args_load_resource
+    ( so_called_platform_render_texture_loader_cocoa_resource_id_type resource_id 
+    , so_called_platform_math_num_whole_type size_pow2_base 
+    , texels_array & data 
+    )
+{
+    so_called_lib_std_int32_t texels_count = 0 ;
+    so_called_platform_static_array_insider :: template elements_count < texels_array > ( texels_count ) ;
+    _check_args_load_resource ( resource_id , size_pow2_base , texels_count ) ;
+}
