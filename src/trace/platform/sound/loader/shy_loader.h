5376267db316f42593293f0c8bbd9f370bf1a607
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-11-28 19:52:07 +0400

    platform sound loader check create_stereo_resource_id args

--- src/trace/platform/sound/loader/shy_loader.h
+++ src/trace/platform/sound/loader/shy_loader.h
@@ -3,6 +3,7 @@ class shy_trace_platform_sound_loader
 public :
     template < typename samples_array >
     static void check_args_load_stereo_sample_data ( so_called_platform_sound_loader_stereo_resource_id_type ) ;
+    static void check_args_create_stereo_resource_id ( so_called_platform_math_num_whole_type ) ;
     static void check_stereo_resource_id_uninitialized ( so_called_platform_sound_loader_stereo_resource_id_type ) ;
 private :
     static void _check_args_load_stereo_sample_data ( so_called_lib_std_int32_t samples_count , so_called_platform_sound_loader_stereo_resource_id_type resource_id ) ;
