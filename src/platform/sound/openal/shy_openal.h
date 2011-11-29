ae85a66324eb43fdc322d3119a48129bd29eb84a
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-11-29 20:09:15 +0400

    platform sound check create_mono/stereo_buffer args

--- src/platform/sound/openal/shy_openal.h
+++ src/platform/sound/openal/shy_openal.h
@@ -49,11 +49,10 @@ void shy_platform_sound_openal :: create_mono_buffer
     , so_called_platform_math_num_whole_type samples_count 
     )
 {
+    so_called_trace ( so_called_trace_platform_sound :: template check_args_create_mono_buffer < samples_array > ( samples_count ) ) ;
+    so_called_profile ( so_called_profile_platform_sound :: buffer_create ( ) ) ;
     so_called_lib_std_int32_t size = 0 ;
     so_called_platform_static_array_insider :: template elements_count < samples_array > ( size ) ;
-    so_called_trace ( so_called_trace_platform_math :: check_num_whole_exceeds_range_int ( samples_count , 1 , size ) ) ;
-    so_called_trace ( so_called_trace_platform_math :: check_num_whole_uninitialized ( samples_count ) ) ;
-    so_called_profile ( so_called_profile_platform_sound :: buffer_create ( ) ) ;
     const so_called_platform_sound_sample_mono_type * samples_ptr = 0 ;
     so_called_platform_static_array_insider :: elements_ptr ( samples_ptr , samples ) ;
 
@@ -67,11 +66,10 @@ void shy_platform_sound_openal :: create_stereo_buffer
     , so_called_platform_math_num_whole_type samples_count 
     )
 {
+    so_called_trace ( so_called_trace_platform_sound :: template check_args_create_stereo_buffer < samples_array > ( samples_count ) ) ;
+    so_called_profile ( so_called_profile_platform_sound :: buffer_create ( ) ) ;
     so_called_lib_std_int32_t size = 0 ;
     so_called_platform_static_array_insider :: template elements_count < samples_array > ( size ) ;
-    so_called_trace ( so_called_trace_platform_math :: check_num_whole_exceeds_range_int ( samples_count , 1 , size ) ) ;
-    so_called_trace ( so_called_trace_platform_math :: check_num_whole_uninitialized ( samples_count ) ) ;
-    so_called_profile ( so_called_profile_platform_sound :: buffer_create ( ) ) ;
     const so_called_platform_sound_sample_stereo_type * samples_ptr = 0 ;
     so_called_platform_static_array_insider :: elements_ptr ( samples_ptr , samples ) ;
 
