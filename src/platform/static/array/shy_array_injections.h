a99f250f13992d3e9659a9e5f56b50d210250e0d
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-12-01 19:21:13 +0400

    platform static array args check

--- src/platform/static/array/shy_array_injections.h
+++ src/platform/static/array/shy_array_injections.h
@@ -5,7 +5,7 @@
 #include "src/injections/platform/pointer/shy_pointer.h"
 #include "src/profile/platform/static/array/shy_array_injections.h"
 #include "src/profile/shy_profile_injections.h"
-#include "src/trace/platform/math/shy_math_injections.h"
+#include "src/trace/platform/static/array/shy_array_injections.h"
 #include "src/trace/shy_trace_injections.h"
 
 #include "./shy_array.h"
