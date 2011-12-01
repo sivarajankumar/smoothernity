72ae7cd9eef284ec0f1de097a9ece7ec894a4254
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-12-01 19:21:13 +0400

    platform static array args check

--- src/platform/static/array/shy_array.h
+++ src/platform/static/array/shy_array.h
@@ -8,8 +8,7 @@ public :
 template < typename array_type , typename pointer_type >
 void shy_platform_static_array :: element_ptr ( pointer_type & element_ptr , array_type & array , so_called_platform_math_num_whole_type index )
 {
-    so_called_trace ( so_called_trace_platform_math :: check_num_whole_exceeds_range_int ( index , 0 , array_type :: _array_size - 1 ) ) ;
-    so_called_trace ( so_called_trace_platform_math :: check_num_whole_uninitialized ( index ) ) ;
+    so_called_trace ( so_called_trace_platform_static_array :: template check_args_element_ptr < array_type > ( index ) ) ;
     so_called_profile ( so_called_profile_platform_static_array :: element ( ) ) ;
     so_called_lib_std_int32_t index_int = 0 ;
     so_called_platform_math_insider :: num_whole_value_get ( index_int , index ) ;
