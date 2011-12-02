0cd8961de628477d79f3947bcabb81a6fb126db9
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-12-02 18:46:59 +0400

    platform conditions args check

--- src/platform/conditions/shy_conditions_injections.hpp
+++ src/platform/conditions/shy_conditions_injections.hpp
@@ -7,7 +7,7 @@
 #include "src/injections/platform/math/insider/shy_insider.h"
 #include "src/profile/platform/conditions/shy_conditions_injections.h"
 #include "src/profile/shy_profile_injections.h"
-#include "src/trace/platform/math/shy_math_injections.h"
+#include "src/trace/platform/conditions/shy_conditions_injections.h"
 #include "src/trace/shy_trace_injections.h"
 
 #include "./shy_conditions.hpp"
