440761a14377607b3dc008658c6b541247075487
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-12-05 20:33:02 +0400

    working on continuations draft

--- draft/continuation.cpp
+++ draft/continuation.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 
+int g_instructions = 0 ;
+int g_arg = 0 ;
+
+void envoke ( int arg )
+{
+    g_arg = arg ;
+    // start continuation
+}
+
+bool continuate ( int instructions )
+{
+    // continue continuation
+    return false ;
+}
+
 int main ( int , char * * )
 {
+    int iteration = 0 ;
+
+    iteration = 0 ;
+    envoke ( 111 ) ;
+    do
+    {
+        std :: cerr << "iteration: " << iteration ++ << std :: endl ;
+    }
+    while ( continuate ( 1 ) ) ;
+
+    iteration = 0 ;
+    envoke ( 222 ) ;
+    do
+    {
+        std :: cerr << "iteration: " << iteration ++ << std :: endl ;
+    }
+    while ( continuate ( 3 ) ) ;
+
     std :: cerr << "hello world" << std :: endl ;
     return 0 ;
 }
