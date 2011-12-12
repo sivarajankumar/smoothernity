67f7325b53ec46ebde89292794b358441b6f240c
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-12-12 18:30:52 +0400

    working on codegen

--- src/codegen/shy_codegen_tests.py
+++ src/codegen/shy_codegen_tests.py
@@ -12,6 +12,20 @@ class shy_codegen_test_case ( unittest . TestCase ) :
             '    static void done ( ) ;\n'
             '    static void next_frame ( ) ;\n'
             '} ;\n' )
+    def test_common_hpp ( self ) :
+        fs = shy_codegen . shy_generate ( [ ] )
+        self . assertEqual ( fs [ 'src/common/shy_common.hpp' ] ,
+            'void shy_common :: init ( )\n'
+            '{\n'
+            '}\n'
+            '\n'
+            'void shy_common :: done ( )\n'
+            '{\n'
+            '}\n'
+            '\n'
+            'void shy_common :: next_frame ( )\n'
+            '{\n'
+            '}\n' )
 
 if __name__ == '__main__' :
     unittest . main ( )
