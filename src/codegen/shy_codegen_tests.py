b599b1a9baf49be0c89b293685b2fe5ef36669b7
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-12-12 19:00:28 +0400

    working on codegen

--- src/codegen/shy_codegen_tests.py
+++ src/codegen/shy_codegen_tests.py
@@ -1,10 +1,11 @@
 import shy_codegen
 import unittest
 
-class shy_codegen_test_case ( unittest . TestCase ) :
+class shy_essential_files_test_case ( unittest . TestCase ) :
+    def setUp ( self ) :
+        self . fs = shy_codegen . shy_generate ( [ ] )
     def test_common_h ( self ) :
-        fs = shy_codegen . shy_generate ( [ ] )
-        self . assertEqual ( fs [ 'src/common/shy_common.h' ] ,
+        self . assertEqual ( self . fs [ 'src/common/shy_common.h' ] ,
             'class shy_common\n'
             '{\n'
             'public :\n'
@@ -13,8 +14,7 @@ class shy_codegen_test_case ( unittest . TestCase ) :
             '    static void next_frame ( ) ;\n'
             '} ;\n' )
     def test_common_hpp ( self ) :
-        fs = shy_codegen . shy_generate ( [ ] )
-        self . assertEqual ( fs [ 'src/common/shy_common.hpp' ] ,
+        self . assertEqual ( self . fs [ 'src/common/shy_common.hpp' ] ,
             'void shy_common :: init ( )\n'
             '{\n'
             '}\n'
@@ -27,43 +27,37 @@ class shy_codegen_test_case ( unittest . TestCase ) :
             '{\n'
             '}\n' )
     def test_common_injections_h ( self ) :
-        fs = shy_codegen . shy_generate ( [ ] )
-        self . assertEqual ( fs [ 'src/common/shy_common_injections.h' ] ,
+        self . assertEqual ( self . fs [ 'src/common/shy_common_injections.h' ] ,
             '#ifndef _shy_common_injections_included\n'
             '#define _shy_common_injections_included\n'
             '\n'
             '#include "src/common/shy_common.h"\n'
             'typedef shy_common so_called_common ;\n' )
     def test_common_injections_cpp ( self ) :
-        fs = shy_codegen . shy_generate ( [ ] )
-        self . assertEqual ( fs [ 'src/common/shy_common_injections.cpp' ] ,
+        self . assertEqual ( self . fs [ 'src/common/shy_common_injections.cpp' ] ,
             '#include "src/common/shy_common_injections.h"\n'
             '#include "src/common/shy_common.hpp"\n' )
     def test_loadable_h ( self ) :
-        fs = shy_codegen . shy_generate ( [ ] )
-        self . assertEqual ( fs [ 'src/loadable/shy_loadable.h' ] ,
+        self . assertEqual ( self . fs [ 'src/loadable/shy_loadable.h' ] ,
             'class shy_loadable\n'
             '{\n'
             'public :\n'
             '    static void load ( ) ;\n'
             '} ;\n' )
     def test_loadable_hpp ( self ) :
-        fs = shy_codegen . shy_generate ( [ ] )
-        self . assertEqual ( fs [ 'src/loadable/shy_loadable.hpp' ] ,
+        self . assertEqual ( self . fs [ 'src/loadable/shy_loadable.hpp' ] ,
             'void shy_loadable :: load ( )\n'
             '{\n'
             '}\n' )
     def test_loadable_injections_h ( self ) :
-        fs = shy_codegen . shy_generate ( [ ] )
-        self . assertEqual ( fs [ 'src/loadable/shy_loadable_injections.h' ] ,
+        self . assertEqual ( self . fs [ 'src/loadable/shy_loadable_injections.h' ] ,
             '#ifndef _shy_loadable_injections_included\n'
             '#define _shy_loadable_injections_included\n'
             '\n'
             '#include "src/loadable/shy_loadable.h"\n'
             'typedef shy_loadable so_called_loadable ;\n' )
     def test_loadable_injections_cpp ( self ) :
-        fs = shy_codegen . shy_generate ( [ ] )
-        self . assertEqual ( fs [ 'src/loadable/shy_loadable_injections.cpp' ] ,
+        self . assertEqual ( self . fs [ 'src/loadable/shy_loadable_injections.cpp' ] ,
             '#include "src/loadable/shy_loadable_injections.h"\n'
             '#include "src/loadable/shy_loadable.hpp"\n' )
 
