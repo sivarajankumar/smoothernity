1b0b6893f3a9231d5fc1959bf410922c83e9ff53
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-12-14 19:49:01 +0400

    working on codegen

--- src/codegen/shy_codegen.py
+++ src/codegen/shy_codegen.py
@@ -1,16 +1,16 @@
-def shy_generate ( lines ) :
+def generate ( lines ) :
     res = { }
-    res . update ( _shy_generate_common_h ( ) )
-    res . update ( _shy_generate_common_hpp ( ) )
-    res . update ( _shy_generate_common_injections_h ( ) )
-    res . update ( _shy_generate_common_injections_cpp ( ) )
-    res . update ( _shy_generate_loadable_h ( ) )
-    res . update ( _shy_generate_loadable_hpp ( ) )
-    res . update ( _shy_generate_loadable_injections_h ( ) )
-    res . update ( _shy_generate_loadable_injections_cpp ( ) )
+    res . update ( _generate_common_h ( ) )
+    res . update ( _generate_common_hpp ( ) )
+    res . update ( _generate_common_injections_h ( ) )
+    res . update ( _generate_common_injections_cpp ( ) )
+    res . update ( _generate_loadable_h ( ) )
+    res . update ( _generate_loadable_hpp ( ) )
+    res . update ( _generate_loadable_injections_h ( ) )
+    res . update ( _generate_loadable_injections_cpp ( ) )
     return res
 
-def _shy_generate_common_h ( ) :
+def _generate_common_h ( ) :
     return { 'src/common/shy_common.h' :
         'class shy_common\n'
         '{\n'
@@ -20,7 +20,7 @@ def _shy_generate_common_h ( ) :
         '    static void next_frame ( ) ;\n'
         '} ;\n' }
 
-def _shy_generate_common_hpp ( ) :
+def _generate_common_hpp ( ) :
     return { 'src/common/shy_common.hpp' :
         'void shy_common :: init ( )\n'
         '{\n'
@@ -34,7 +34,7 @@ def _shy_generate_common_hpp ( ) :
         '{\n'
         '}\n' }
 
-def _shy_generate_common_injections_h ( ) :
+def _generate_common_injections_h ( ) :
     return { 'src/common/shy_common_injections.h' :
         '#ifndef _shy_common_injections_included\n'
         '#define _shy_common_injections_included\n'
@@ -42,12 +42,12 @@ def _shy_generate_common_injections_h ( ) :
         '#include "src/common/shy_common.h"\n'
         'typedef shy_common so_called_common ;\n' }
 
-def _shy_generate_common_injections_cpp ( ) :
+def _generate_common_injections_cpp ( ) :
     return { 'src/common/shy_common_injections.cpp' :
         '#include "src/common/shy_common_injections.h"\n'
         '#include "src/common/shy_common.hpp"\n' }
 
-def _shy_generate_loadable_h ( ) :
+def _generate_loadable_h ( ) :
     return { 'src/loadable/shy_loadable.h' :
         'class shy_loadable\n'
         '{\n'
@@ -55,13 +55,13 @@ def _shy_generate_loadable_h ( ) :
         '    static void load ( ) ;\n'
         '} ;\n' }
 
-def _shy_generate_loadable_hpp ( ) :
+def _generate_loadable_hpp ( ) :
     return { 'src/loadable/shy_loadable.hpp' :
         'void shy_loadable :: load ( )\n'
         '{\n'
         '}\n' }
 
-def _shy_generate_loadable_injections_h ( ) :
+def _generate_loadable_injections_h ( ) :
     return { 'src/loadable/shy_loadable_injections.h' :
         '#ifndef _shy_loadable_injections_included\n'
         '#define _shy_loadable_injections_included\n'
@@ -69,7 +69,7 @@ def _shy_generate_loadable_injections_h ( ) :
         '#include "src/loadable/shy_loadable.h"\n'
         'typedef shy_loadable so_called_loadable ;\n' }
 
-def _shy_generate_loadable_injections_cpp ( ) :
+def _generate_loadable_injections_cpp ( ) :
     return { 'src/loadable/shy_loadable_injections.cpp' :
         '#include "src/loadable/shy_loadable_injections.h"\n'
         '#include "src/loadable/shy_loadable.hpp"\n' }
