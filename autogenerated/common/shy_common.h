7f7872a8d38eebb9d371fb4e73e883b703e736e3
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-12-15 21:41:15 +0400

    working on codegen

---
+++ autogenerated/common/shy_common.h
@@ -0,0 +1,7 @@
+class shy_common
+{
+public :
+    static void init ( ) ;
+    static void done ( ) ;
+    static void next_frame ( ) ;
+} ;
---
+++ autogenerated/common/shy_common.hpp
@@ -0,0 +1,11 @@
+void shy_common :: init ( )
+{
+}
+
+void shy_common :: done ( )
+{
+}
+
+void shy_common :: next_frame ( )
+{
+}
