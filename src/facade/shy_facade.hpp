073eb49d0635cb2e6aafab269527e3476e2e9cd2
Oleg Plakhotnyuk <olegus8@gmail.com>
2011-12-16 18:48:08 +0400

    codegen project integration

--- src/facade/shy_facade.hpp
+++ src/facade/shy_facade.hpp
@@ -2,8 +2,7 @@ void shy_facade :: application_init ( )
 {
     so_called_profile ( so_called_platform_profile :: init ( ) ) ;
     so_called_profile ( so_called_profile_init :: init ( ) ) ;
-
-    so_called_loadable ( so_called_loadable_loader :: load ( ) ) ;
+    so_called_loadable :: load ( ) ;
 }
 
 void shy_facade :: application_done ( )
@@ -18,19 +17,12 @@ void shy_facade :: game_init ( )
     so_called_platform_render_texture_loader :: init ( ) ;
     so_called_platform_scheduler :: init ( ) ;
     so_called_platform_sound_loader :: init ( ) ;
-    so_called_common_init :: init ( ) ;
-
-    so_called_common_init_sender :: send ( so_called_common_init_message ( ) ) ;
-    so_called_platform_scheduler :: run ( ) ;
+    so_called_common :: init ( ) ;
 }
 
 void shy_facade :: game_done ( )
 {
-    so_called_common_done_sender :: send ( so_called_common_done_message ( ) ) ;
-    so_called_platform_scheduler :: run ( ) ;
-
-    so_called_common_done :: done ( ) ;
-
+    so_called_common :: done ( ) ;
     so_called_platform_render :: done ( ) ;
     so_called_platform_render_texture_loader :: done ( ) ;
     so_called_platform_scheduler :: done ( ) ;
@@ -41,7 +33,5 @@ void shy_facade :: next_frame ( )
 {
     so_called_profile ( so_called_platform_profile_insider :: next_frame ( ) ) ;
     so_called_trace ( so_called_platform_trace_insider :: next_frame ( ) ) ;
-
-    so_called_common_next_frame_sender :: send ( so_called_common_next_frame_message ( ) ) ;
-    so_called_platform_scheduler :: run ( ) ;
+    so_called_common :: next_frame ( ) ;
 }
