833cdcda67b3a60aaa3a5d84c6f7ce4ae2b1a10f
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-01 23:40:25 +0400

    preprocess test

--- src/main/macosx/build_development.sh
+++ src/main/macosx/build_development.sh
@@ -3,5 +3,5 @@ rm ./smoothernity.xcodeproj/*.pbxuser
 rm ./smoothernity.xcodeproj/*.mode1v3
 rm -rf ./smoothernity.xcodeproj/xcuserdata/
 rm -rf ./smoothernity.xcodeproj/project.xcworkspace/xcuserdata/
-find ../../../data -name *.shy -exec cat {} \; | python -B ../../codegen/shy_codegen.py ../../../
+./generate.sh
 xcodebuild -project smoothernity.xcodeproj -target "smoothernity" -configuration Development build
