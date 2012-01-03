bf22bb6db39925053ca19b5b9214179132c94ca5
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-04 00:00:55 +0400

    lexer tests

--- src/codegen/show_tests.py
+++ src/codegen/show_tests.py
@@ -6,38 +6,34 @@ class show_test_case ( unittest . TestCase ) :
         self . s = show . show ( )
         self . s . set_context ( 3 )
         self . s . set_source ( [ 1 , 22 , 333 , 4444 , 55555 ] )
-    def test_context_line ( self ) :
-        cl = self . s . context_line
-        self . assertEqual ( cl ( 0 ) ,
-            [ '1'
-            , '^'
-            , '22'
-            , '333' ] )
-        self . assertEqual ( cl ( 3 ) ,
-            [ '22'
-            , '333'
-            , '4444'
-            , '^^^^' ] )
-        self . assertEqual ( cl ( 4 ) ,
-            [ '333'
-            , '4444'
-            , '55555'
-            , '^^^^^' ] )
+    def test_context_line_first ( self ) :
+        c = self . s . context_line
+        ae = self . assertEqual
+        ae ( c ( 0 ) , [ '1' , '^' , '22' , '333' ] )
+    def test_context_line_middle ( self ) :
+        c = self . s . context_line
+        ae = self . assertEqual
+        ae ( c ( 3 ) , [ '22' , '333' , '4444' , '^^^^' ] )
+    def test_context_line_last ( self ) :
+        c = self . s . context_line
+        ae = self . assertEqual
+        ae ( c ( 4 ) , [ '333' , '4444' , '55555' , '^^^^^' ] )
+    def test_context_line_raises ( self ) :
+        c = self . s . context_line
         ar = self . assertRaises
-        ar ( show . line_exception , cl , - 1 )
-        ar ( show . line_exception , cl , 5 )
-        ar ( show . line_exception , cl , 10 )
+        ar ( show . line_exception , c , - 1 )
+        ar ( show . line_exception , c , 5 )
+        ar ( show . line_exception , c , 10 )
     def test_context_line_col ( self ) :
-        clc = self . s . context_line_col
-        self . assertEqual ( clc ( 3 , 2 ) ,
-            [ '22'
-            , '333'
-            , '4444'
-            , '  ^' ] )
+        c = self . s . context_line_col
+        ae = self . assertEqual
+        ae ( c ( 3 , 2 ) , [ '22' , '333' , '4444' , '  ^' ] )
+    def test_context_line_col_raises ( self ) :
+        c = self . s . context_line_col
         ar = self . assertRaises
-        ar ( show . line_col_exception , clc , 3 , - 1 )
-        ar ( show . line_col_exception , clc , 3 , 4 )
-        ar ( show . line_col_exception , clc , 3 , 10 )
+        ar ( show . line_col_exception , c , 3 , - 1 )
+        ar ( show . line_col_exception , c , 3 , 4 )
+        ar ( show . line_col_exception , c , 3 , 10 )
 
 if __name__ == '__main__' :
     unittest . main ( )
