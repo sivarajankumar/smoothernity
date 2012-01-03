7f9692a5d128039a9edca3ee376b296794562d96
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-03 16:26:59 +0400

    working on lexer

--- src/codegen/lexer.py
+++ src/codegen/lexer.py
@@ -1,5 +1,9 @@
 import re
 
+class lexer_exception ( Exception ) :
+    def __init__ ( self , line ) :
+        Exception . __init__ ( self , 'Lexical error: "%s"' + line )
+
 class lexer :
     def __init__ ( self ) :
         self . _token_patterns = [ ]
@@ -9,11 +13,16 @@ class lexer :
         res = [ ]
         for line in lines :
             while line :
+                was_match = False
                 for pats in self . _token_patterns :
                     for token , match in pats . items ( ) :
                         rem = re . match ( match , line )
                         if rem :
                             value = line [ rem . start ( ) : rem . end ( ) ]
                             line = line [ rem . end ( ) : ]
-                            res . append ( { 'type' : token , 'value' : value } )
+                            res . append ( { 'type' : token
+                                           , 'value' : value } )
+                            was_match = True
+                if not was_match :
+                    raise lexer_exception ( line )
         return res
