9602893d2d899d07891def5216f81ae4b464e96e
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-03 16:18:19 +0400

    working on lexer

--- src/codegen/lexer_tests.py
+++ src/codegen/lexer_tests.py
@@ -1,13 +1,27 @@
 import unittest
-from lexer import lexer
+import lexer
 
 class lexer_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
-        pass
+        self . l = lexer . lexer ( )
     def tearDown ( self ) :
         pass
     def test_empty ( self ) :
-        self . assertEqual ( lexer ( ) . parse ( [ ] ) , [ ] )
+        self . assertEqual ( self . l . parse ( [ ] ) , [ ] )
+    def test_tokens ( self ) :
+        self . l . set_token_patterns ( [ 
+            { 'word' : r'[a-z]+'
+            , 'number' : r'[0-9]+'
+            , 'whitespace' : r' +'
+            } ] )
+        self . assertEqual ( self . l . parse (
+            [ 'test1 test2' ] ) ,
+            [ { 'type' : 'word' , 'value' : 'test' }
+            , { 'type' : 'number' , 'value' : '1' }
+            , { 'type' : 'whitespace' , 'value' : ' ' }
+            , { 'type' : 'word' , 'value' : 'test' }
+            , { 'type' : 'number' , 'value' : '2' }
+            ] )
 
 if __name__ == '__main__' :
     unittest . main ( )
