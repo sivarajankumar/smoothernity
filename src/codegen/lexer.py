279dc9aae1635c1917fc093a6cd2a84b09dcae8f
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-03 18:01:04 +0400

    working on lexer

--- src/codegen/lexer.py
+++ src/codegen/lexer.py
@@ -1,34 +1,71 @@
 import re
 
-class lexer_exception ( Exception ) :
+class token_exception ( Exception ) :
     def __init__ ( self , line ) :
-        Exception . __init__ ( self , 'Lexical error: "%s"' % line )
+        Exception . __init__ ( self , 'Unknown token: "%s"' % line )
+
+class whitespace_exception ( Exception ) :
+    def __init__ ( self , line ) :
+        Exception . __init__ ( self ,
+            'Missing whitespace before next token: "%s"' % line )
+
+class indent_exception ( Exception ) :
+    def __init__ ( self , line ) :
+        Exception . __init__ ( self ,
+            'Dedent exceeds first indent: "%s"' % line )
 
 class lexer :
     def __init__ ( self ) :
         self . _token_patterns = [ ]
+        self . _indent_token = None
+        self . _dedent_token = None
+        self . _indents = [ ]
+        self . _tokens = [ ]
     def set_token_patterns ( self , pats ) :
         self . _token_patterns = pats
+    def set_indent_tokens ( self , indent , dedent ) :
+        self . _indent_token = indent
+        self . _dedent_token = dedent
     def parse ( self , lines ) :
-        res = [ ]
+        self . _tokens = [ ]
+        self . _indents = [ ]
         for line in lines :
-            line = line . strip ( ' ' )
+            spaces , line = self . _resplit ( r' *' , line )
+            indent = len ( spaces )
+            while self . _indents :
+                if self . _indents [ - 1 ] < indent :
+                    self . _tokens . append (
+                        { 'type' : self . _indent_token } )
+                    self . _indents . append ( indent )
+                elif self . _indents [ - 1 ] > indent :
+                    if len ( self . _indents ) == 1 :
+                        raise indent_exception ( line )
+                    self . _tokens . append (
+                        { 'type' : self . _dedent_token } )
+                    self . _indents . pop ( )
+                else :
+                    break
+            else :
+                self . _indents . append ( indent )
             while line :
                 was_match = False
                 for pats in self . _token_patterns :
                     for token , matches in pats . items ( ) :
                         for match in matches :
-                            rem = re . match ( match , line )
-                            if rem :
-                                value = line [ rem . start ( )
-                                             : rem . end ( ) ]
-                                line = line [ rem . end ( ) : ]
+                            value , line = self . _resplit ( match , line )
+                            if value :
                                 if line and line [ 0 ] != ' ' :
-                                    raise lexer_exception ( line )
+                                    raise whitespace_exception ( line )
                                 line = line . strip ( ' ' )
-                                res . append ( { 'type' : token
-                                               , 'value' : value } )
+                                self . _tokens . append (
+                                    { 'type' : token , 'value' : value } )
                                 was_match = True
                 if not was_match :
-                    raise lexer_exception ( line )
-        return res
+                    raise token_exception ( line )
+        return self . _tokens
+    def _resplit ( self , match , line ) :
+        rem = re . match ( match , line )
+        if rem :
+            value = line [ rem . start ( ) : rem . end ( ) ]
+            return value , line [ rem . end ( ) : ]
+        return '' , line
--- src/codegen/lexer_tests.py
+++ src/codegen/lexer_tests.py
@@ -11,17 +11,36 @@ class lexer_test_case ( unittest . TestCase ) :
     def test_tokens ( self ) :
         self . l . set_token_patterns ( [ 
             { 'id' : [ r'[a-z][a-z0-9_]*' ]
-            , 'number' : [ r'[-]? *[0-9]+' ]
-            } ] )
+            , 'number' : [ r'[-]? *[0-9]+' ] } ] )
         self . assertEqual ( self . l . parse (
             [ 'test1 test_2 - 22 33' ] ) ,
             [ { 'type' : 'id' , 'value' : 'test1' }
             , { 'type' : 'id' , 'value' : 'test_2' }
             , { 'type' : 'number' , 'value' : '- 22' }
-            , { 'type' : 'number' , 'value' : '33' }
-            ] )
-        self . assertRaises ( lexer . lexer_exception , self . l . parse , [ 'A' ] )
-        self . assertRaises ( lexer . lexer_exception , self . l . parse , [ '2test' ] )
+            , { 'type' : 'number' , 'value' : '33' } ] )
+        self . assertRaises ( lexer . token_exception , self . l . parse , [ 'A' ] )
+        self . assertRaises ( lexer . whitespace_exception , self . l . parse , [ '2test' ] )
+    def test_indent ( self ) :
+        self . l . set_indent_tokens ( 'indent' , 'dedent' )
+        self . l . set_token_patterns ( [ { 'id' : [ r'[a-z0-9]+' ] } ] )
+        self . assertEqual ( self . l . parse (
+            [ 'test1'
+            , '  test2'
+            , '      test3'
+            , '      test4'
+            , 'test5' ] ) , 
+            [ { 'type' : 'id' , 'value' : 'test1' }
+            , { 'type' : 'indent' }
+            , { 'type' : 'id' , 'value' : 'test2' }
+            , { 'type' : 'indent' }
+            , { 'type' : 'id' , 'value' : 'test3' }
+            , { 'type' : 'id' , 'value' : 'test4' }
+            , { 'type' : 'dedent' }
+            , { 'type' : 'dedent' }
+            , { 'type' : 'id' , 'value' : 'test5' } ] )
+        self . assertRaises ( lexer . indent_exception , self . l . parse ,
+            [ '  test1'
+            , 'test2' ] )
 
 if __name__ == '__main__' :
     unittest . main ( )
