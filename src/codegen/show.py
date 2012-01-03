f1d20af9446f68ee02bb856c2bba114613c6fee6
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-03 23:34:42 +0400

    working on show

--- src/codegen/show.py
+++ src/codegen/show.py
@@ -2,6 +2,11 @@ class line_exception ( Exception ) :
     def __init__ ( self , line ) :
         Exception . __init__ ( self , 'Line number %s is out of range' % line )
 
+class line_col_exception ( Exception ) :
+    def __init__ ( self , line , col ) :
+        Exception . __init__ ( self ,
+            'Col number %s is out of range in line %s' % ( col , line ) )
+
 class show :
     def __init__ ( self ) :
         self . _context = None
@@ -11,10 +16,19 @@ class show :
     def set_source ( self , source ) :
         self . _source = source
     def context_line ( self , line ) :
+        return self . _context_line_col ( line , None )
+    def context_line_col ( self , line , col ) :
+        return self . _context_line_col ( line , col )
+    def _context_line_col ( self , line , col ) :
         if line < 0 or line >= len ( self . _source ) :
             raise line_exception ( line )
         marked = self . _source [ : line + 1 ]
-        marked += [ '^' * len ( str ( self . _source [ line ] ) ) ]
+        if col == None :
+            marked += [ '^' * len ( str ( self . _source [ line ] ) ) ]
+        else :
+            if col < 0 or col >= len ( str ( self . _source [ line ] ) ) :
+                raise line_col_exception ( line , col )
+            marked += [ str ( ' ' * col ) + '^' ]
         marked += self . _source [ line + 1 : ]
         start = max ( 0 , line - self . _context + 1 )
         end = start + self . _context + 1
