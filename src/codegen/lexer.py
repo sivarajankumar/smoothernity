d65ec9da039b8705ac91ab1b8fd366d137e7fb3c
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-03 16:18:19 +0400

    working on lexer

--- src/codegen/lexer.py
+++ src/codegen/lexer.py
@@ -1,3 +1,19 @@
+import re
+
 class lexer :
+    def __init__ ( self ) :
+        self . _token_patterns = [ ]
+    def set_token_patterns ( self , pats ) :
+        self . _token_patterns = pats
     def parse ( self , lines ) :
-        return [ ]
+        res = [ ]
+        for line in lines :
+            while line :
+                for pats in self . _token_patterns :
+                    for token , match in pats . items ( ) :
+                        rem = re . match ( match , line )
+                        if rem :
+                            value = line [ rem . start ( ) : rem . end ( ) ]
+                            line = line [ rem . end ( ) : ]
+                            res . append ( { 'type' : token , 'value' : value } )
+        return res
