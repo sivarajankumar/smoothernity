e5eec5ea3e25275214e3347376ca51b9388d17e0
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-05 22:12:46 +0400

    working on recognizer

---
+++ src/codegen/recognizer/Backend.py
@@ -0,0 +1,173 @@
+# $ANTLR 3.4 grammar/Backend.g 2012-01-05 21:59:03
+
+import sys
+from antlr3 import *
+from antlr3.tree import *
+
+from antlr3.compat import set, frozenset
+
+
+
+# for convenience in actions
+HIDDEN = BaseRecognizer.HIDDEN
+
+# token types
+EOF=-1
+T__7=7
+ID=4
+NEWLINE=5
+WHITESPACE=6
+
+# token names
+tokenNames = [
+    "<invalid>", "<EOR>", "<DOWN>", "<UP>",
+    "ID", "NEWLINE", "WHITESPACE", "'module'"
+]
+
+
+
+
+class Backend(TreeParser):
+    grammarFileName = "grammar/Backend.g"
+    api_version = 1
+    tokenNames = tokenNames
+
+    def __init__(self, input, state=None, *args, **kwargs):
+        if state is None:
+            state = RecognizerSharedState()
+
+        super(Backend, self).__init__(input, state, *args, **kwargs)
+
+
+
+
+        self.delegates = []
+
+
+
+
+
+
+    # $ANTLR start "start"
+    # grammar/Backend.g:10:1: start returns [ value ] : ( module )* ;
+    def start(self, ):
+        value = None
+
+
+        module1 = None
+
+
+        value = dict ( ) 
+        try:
+            try:
+                # grammar/Backend.g:13:5: ( ( module )* )
+                # grammar/Backend.g:13:9: ( module )*
+                pass 
+                # grammar/Backend.g:13:9: ( module )*
+                while True: #loop1
+                    alt1 = 2
+                    LA1_0 = self.input.LA(1)
+
+                    if (LA1_0 == 7) :
+                        alt1 = 1
+
+
+                    if alt1 == 1:
+                        # grammar/Backend.g:13:11: module
+                        pass 
+                        self._state.following.append(self.FOLLOW_module_in_start79)
+                        module1 = self.module()
+
+                        self._state.following.pop()
+
+                        #action start
+                                    
+                        if 'module' not in value :
+                            value [ 'module' ] = dict ( )
+                        value [ 'module' ] [ module1 ] = { }
+                                    
+                        #action end
+
+
+
+                    else:
+                        break #loop1
+
+
+
+
+
+            except RecognitionException, re:
+                self.reportError(re)
+                self.recover(self.input, re)
+
+        finally:
+            pass
+        return value
+
+    # $ANTLR end "start"
+
+
+
+    # $ANTLR start "module"
+    # grammar/Backend.g:22:1: module returns [ value ] : ^( 'module' ID ) ;
+    def module(self, ):
+        value = None
+
+
+        ID2 = None
+
+        try:
+            try:
+                # grammar/Backend.g:24:5: ( ^( 'module' ID ) )
+                # grammar/Backend.g:24:9: ^( 'module' ID )
+                pass 
+                self.match(self.input, 7, self.FOLLOW_7_in_module135)
+
+                self.match(self.input, DOWN, None)
+                ID2 = self.match(self.input, ID, self.FOLLOW_ID_in_module137)
+
+                self.match(self.input, UP, None)
+
+
+                #action start
+                value = ID2.text 
+                #action end
+
+
+
+
+
+            except RecognitionException, re:
+                self.reportError(re)
+                self.recover(self.input, re)
+
+        finally:
+            pass
+        return value
+
+    # $ANTLR end "module"
+
+
+
+ 
+
+    FOLLOW_module_in_start79 = frozenset([1, 7])
+    FOLLOW_7_in_module135 = frozenset([2])
+    FOLLOW_ID_in_module137 = frozenset([3])
+
+
+
+def main(argv, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr):
+    from antlr3.main import WalkerMain
+    main = WalkerMain(Backend)
+
+    main.stdin = stdin
+    main.stdout = stdout
+    main.stderr = stderr
+    main.execute(argv)
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ src/codegen/recognizer/FrontendParser.py
@@ -0,0 +1,264 @@
+# $ANTLR 3.4 grammar/Frontend.g 2012-01-05 21:59:01
+
+import sys
+from antlr3 import *
+from antlr3.compat import set, frozenset
+
+from antlr3.tree import *
+
+
+
+
+# for convenience in actions
+HIDDEN = BaseRecognizer.HIDDEN
+
+# token types
+EOF=-1
+T__7=7
+ID=4
+NEWLINE=5
+WHITESPACE=6
+
+# token names
+tokenNames = [
+    "<invalid>", "<EOR>", "<DOWN>", "<UP>",
+    "ID", "NEWLINE", "WHITESPACE", "'module'"
+]
+
+
+
+
+class FrontendParser(Parser):
+    grammarFileName = "grammar/Frontend.g"
+    api_version = 1
+    tokenNames = tokenNames
+
+    def __init__(self, input, state=None, *args, **kwargs):
+        if state is None:
+            state = RecognizerSharedState()
+
+        super(FrontendParser, self).__init__(input, state, *args, **kwargs)
+
+
+
+
+        self.delegates = []
+
+	self._adaptor = None
+	self.adaptor = CommonTreeAdaptor()
+
+
+
+    def getTreeAdaptor(self):
+        return self._adaptor
+
+    def setTreeAdaptor(self, adaptor):
+        self._adaptor = adaptor
+
+    adaptor = property(getTreeAdaptor, setTreeAdaptor)
+
+
+    class start_return(ParserRuleReturnScope):
+        def __init__(self):
+            super(FrontendParser.start_return, self).__init__()
+
+            self.tree = None
+
+
+
+
+
+    # $ANTLR start "start"
+    # grammar/Frontend.g:10:1: start : ( module )* ;
+    def start(self, ):
+        retval = self.start_return()
+        retval.start = self.input.LT(1)
+
+
+        root_0 = None
+
+        module1 = None
+
+
+
+        try:
+            try:
+                # grammar/Frontend.g:10:7: ( ( module )* )
+                # grammar/Frontend.g:10:9: ( module )*
+                pass 
+                root_0 = self._adaptor.nil()
+
+
+                # grammar/Frontend.g:10:9: ( module )*
+                while True: #loop1
+                    alt1 = 2
+                    LA1_0 = self.input.LA(1)
+
+                    if (LA1_0 == 7) :
+                        alt1 = 1
+
+
+                    if alt1 == 1:
+                        # grammar/Frontend.g:10:9: module
+                        pass 
+                        self._state.following.append(self.FOLLOW_module_in_start52)
+                        module1 = self.module()
+
+                        self._state.following.pop()
+                        self._adaptor.addChild(root_0, module1.tree)
+
+
+
+                    else:
+                        break #loop1
+
+
+
+
+                retval.stop = self.input.LT(-1)
+
+
+                retval.tree = self._adaptor.rulePostProcessing(root_0)
+                self._adaptor.setTokenBoundaries(retval.tree, retval.start, retval.stop)
+
+
+
+            except RecognitionException, re:
+                self.reportError(re)
+                self.recover(self.input, re)
+                retval.tree = self._adaptor.errorNode(self.input, retval.start, self.input.LT(-1), re)
+
+        finally:
+            pass
+        return retval
+
+    # $ANTLR end "start"
+
+
+    class module_return(ParserRuleReturnScope):
+        def __init__(self):
+            super(FrontendParser.module_return, self).__init__()
+
+            self.tree = None
+
+
+
+
+
+    # $ANTLR start "module"
+    # grammar/Frontend.g:11:1: module : 'module' ID NEWLINE -> ^( 'module' ID ) ;
+    def module(self, ):
+        retval = self.module_return()
+        retval.start = self.input.LT(1)
+
+
+        root_0 = None
+
+        string_literal2 = None
+        ID3 = None
+        NEWLINE4 = None
+
+        string_literal2_tree = None
+        ID3_tree = None
+        NEWLINE4_tree = None
+        stream_7 = RewriteRuleTokenStream(self._adaptor, "token 7")
+        stream_NEWLINE = RewriteRuleTokenStream(self._adaptor, "token NEWLINE")
+        stream_ID = RewriteRuleTokenStream(self._adaptor, "token ID")
+
+        try:
+            try:
+                # grammar/Frontend.g:11:8: ( 'module' ID NEWLINE -> ^( 'module' ID ) )
+                # grammar/Frontend.g:11:10: 'module' ID NEWLINE
+                pass 
+                string_literal2 = self.match(self.input, 7, self.FOLLOW_7_in_module62) 
+                stream_7.add(string_literal2)
+
+
+                ID3 = self.match(self.input, ID, self.FOLLOW_ID_in_module64) 
+                stream_ID.add(ID3)
+
+
+                NEWLINE4 = self.match(self.input, NEWLINE, self.FOLLOW_NEWLINE_in_module66) 
+                stream_NEWLINE.add(NEWLINE4)
+
+
+                # AST Rewrite
+                # elements: ID, 7
+                # token labels: 
+                # rule labels: retval
+                # token list labels: 
+                # rule list labels: 
+                # wildcard labels: 
+                retval.tree = root_0
+                if retval is not None:
+                    stream_retval = RewriteRuleSubtreeStream(self._adaptor, "rule retval", retval.tree)
+                else:
+                    stream_retval = RewriteRuleSubtreeStream(self._adaptor, "token retval", None)
+
+
+                root_0 = self._adaptor.nil()
+                # 11:30: -> ^( 'module' ID )
+                # grammar/Frontend.g:11:33: ^( 'module' ID )
+                root_1 = self._adaptor.nil()
+                root_1 = self._adaptor.becomeRoot(
+                stream_7.nextNode()
+                , root_1)
+
+                self._adaptor.addChild(root_1, 
+                stream_ID.nextNode()
+                )
+
+                self._adaptor.addChild(root_0, root_1)
+
+
+
+
+                retval.tree = root_0
+
+
+
+
+
+                retval.stop = self.input.LT(-1)
+
+
+                retval.tree = self._adaptor.rulePostProcessing(root_0)
+                self._adaptor.setTokenBoundaries(retval.tree, retval.start, retval.stop)
+
+
+
+            except RecognitionException, re:
+                self.reportError(re)
+                self.recover(self.input, re)
+                retval.tree = self._adaptor.errorNode(self.input, retval.start, self.input.LT(-1), re)
+
+        finally:
+            pass
+        return retval
+
+    # $ANTLR end "module"
+
+
+
+ 
+
+    FOLLOW_module_in_start52 = frozenset([1, 7])
+    FOLLOW_7_in_module62 = frozenset([4])
+    FOLLOW_ID_in_module64 = frozenset([5])
+    FOLLOW_NEWLINE_in_module66 = frozenset([1])
+
+
+
+def main(argv, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr):
+    from antlr3.main import ParserMain
+    main = ParserMain("FrontendLexer", FrontendParser)
+
+    main.stdin = stdin
+    main.stdout = stdout
+    main.stderr = stderr
+    main.execute(argv)
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
