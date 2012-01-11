1ac60347c00331691de949a23662d5df6f7ec7ba
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-11 20:05:10 +0400

    working on indenter

--- src/codegen/indenter.py
+++ src/codegen/indenter.py
@@ -1,3 +1,32 @@
 class indenter :
+    def __init__ ( self ) :
+        self . _res = None
+        self . _indent_token = None
+        self . _dedent_token = None
+    def set_indent_token ( self , token ) :
+        self . _indent_token = token
+    def set_dedent_token ( self , token ) :
+        self . _dedent_token = token
     def run ( self , lines ) :
-        return lines
+        self . _res = ''
+        indents = [ 0 ]
+        for line in lines :
+            line = line . rstrip ( )
+            indent = len ( line ) - len ( line . lstrip ( ) )
+            line = line . lstrip ( )
+            if line :
+                while indent < indents [ - 1 ] :
+                    self . _append ( self . _dedent_token )
+                    indents . pop ( )
+                while indent > indents [ - 1 ] :
+                    self . _append ( self . _indent_token )
+                    indents . append ( indent )
+                self . _append ( line )
+        while len ( indents ) > 1 :
+            self . _append ( self . _dedent_token )
+            indents . pop ( )
+        return self . _res
+    def _append ( self , s ) :
+        if self . _res :
+            self . _res += ' '
+        self . _res += s
--- src/codegen/indenter_tests.py
+++ src/codegen/indenter_tests.py
@@ -4,10 +4,48 @@ import unittest
 class indenter_tests ( unittest . TestCase ) :
     def setUp ( self ) :
         self . i = indenter . indenter ( )
+        self . i . set_indent_token ( 'indent' )
+        self . i . set_dedent_token ( 'dedent' )
     def test_empty ( self ) :
         ae = self . assertEqual
         r = self . i . run
-        ae ( r ( [ '' ] ) , [ '' ] )
+        ae ( r ( [ ] ) , '' )
+        ae ( r ( [ '' ] ) , '' )
+        ae ( r ( [ '' , '' ] ) , '' )
+        ae ( r ( [ '\n' , '\r' ] ) , '' )
+    def test_no_indent ( self ) :
+        ae = self . assertEqual
+        r = self . i . run
+        ae ( r ( [ 'test1' , 'test2' ] ) , 'test1 test2' )
+    def test_trailing_strip ( self ) :
+        ae = self . assertEqual
+        r = self . i . run
+        ae ( r ( [ 'test1  ' , 'test2\r' , 'test3\n' , 'test4\r\n' ] ) ,
+            'test1 test2 test3 test4' )
+    def test_single_indent ( self ) :
+        ae = self . assertEqual
+        r = self . i . run
+        ae ( r ( [ 'test1' , ' test2' ] ) ,
+            'test1 indent test2 dedent' )
+        ae ( r ( [ 'test1 test2' , ' test3 test4' ] ) ,
+            'test1 test2 indent test3 test4 dedent' )
+        ae ( r ( [ '  test1' , '  test2' ] ) ,
+            'indent test1 test2 dedent' )
+    def test_same_indent ( self ) :
+        ae = self . assertEqual
+        r = self . i . run
+        ae ( r ( [ 'test1' , ' test2' , ' test3' ] ) ,
+            'test1 indent test2 test3 dedent' )
+    def test_multi_indent ( self ) :
+        ae = self . assertEqual
+        r = self . i . run
+        ae ( r ( [ 'test1' , ' test2' , '  test3' ] ) ,
+            'test1 indent test2 indent test3 dedent dedent' )
+    def test_different_indent ( self ) :
+        ae = self . assertEqual
+        r = self . i . run
+        ae ( r ( [ 'test1' , '  test2' , ' test3' ] ) ,
+            'test1 indent test2 dedent indent test3 dedent' )
 
 if __name__ == '__main__' :
     unittest . main ( )
