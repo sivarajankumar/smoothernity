3f2e84071acfd3148f387d6acf85432b2b456d29
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-23 19:39:31 +0400

    working on procs

--- src/codegen/reader_tests.py
+++ src/codegen/reader_tests.py
@@ -152,6 +152,31 @@ class proc_test_case ( unittest . TestCase ) :
             { 'stateless' : { 'test1' :
                 { 'proc1' : { 'ops' : [ { 'call1' : [ ] } ] } } } } )
 
+class statement_call_test_case ( unittest . TestCase ) :
+    def setUp ( self ) :
+        self . h = helper ( )
+    def test_no_args ( self ) :
+        ae = self . assertEqual
+        r = self . h . rec
+        ae ( r ( 'stateless test1\n proc proc1\n  ops\n'
+            '   call1\n' ) ,
+            { 'stateless' : { 'test1' :
+                { 'proc1' : { 'ops' : [ { 'call1' : [ ] } ] } } } } )
+    def test_args_one_line ( self ) :
+        ae = self . assertEqual
+        r = self . h . rec
+        ae ( r ( 'stateless test1\n proc proc1\n  ops\n'
+            '   call1 arg1 arg2\n' ) ,
+            { 'stateless' : { 'test1' : { 'proc1' : { 'ops' : [
+                { 'call1' : [ 'arg1' , 'arg2' ] } ] } } } } )
+    def test_args_indented ( self ) :
+        ae = self . assertEqual
+        r = self . h . rec
+        ae ( r ( 'stateless test1\n proc proc1\n  ops\n'
+            '   call1\n    arg1 arg2\n    arg3' ) ,
+            { 'stateless' : { 'test1' : { 'proc1' : { 'ops' : [
+                { 'call1' : [ 'arg1' , 'arg2' , 'arg3' ] } ] } } } } )
+
 class consts_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
         self . h = helper ( )
