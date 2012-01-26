89037f5b6d6633fe255de73cf126de04190de01f
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-26 19:51:43 +0400

    working on with

--- src/codegen/reader_tests.py
+++ src/codegen/reader_tests.py
@@ -152,6 +152,17 @@ class proc_test_case ( unittest . TestCase ) :
             { 'stateless' : { 'test1' :
                 { 'proc1' : { 'ops' : [ { 'call' : [ 'call1' ] } ] } } } } )
 
+class statement_with_test_case ( unittest . TestCase ) :
+    def setUp ( self ) :
+        self . h = helper ( )
+    def test_simple ( self ) :
+        ae = self . assertEqual
+        r = self . h . rec
+        ae ( r ( 'stateless test1\n proc proc1\n  ops\n'
+            '   with call1\n    call2\n    call3\n' ) ,
+            { 'stateless' : { 'test1' : { 'proc1' : { 'ops' : [ { 'with' :
+                { 'call1' : [ { 'call' : [ 'call1' ] } ] } } ] } } } } )
+
 class statement_assign_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
         self . h = helper ( )
