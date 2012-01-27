ee77613a7b7c762464679c8bfc2e611cdad9380a
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-27 20:18:49 +0400

    working on reader tests separation

--- src/codegen/reader_tests.py
+++ src/codegen/reader_tests.py
@@ -1,40 +1,13 @@
 import unittest
 import fractions
 import reader
-from reader_tests_cases . copy_paste import copy_paste_test_case
-from reader_tests_cases . ids import ids_test_case
-from reader_tests_cases . lexer import lexer_test_case
-from reader_tests_cases . modules import modules_test_case
-from reader_tests_cases . stateless import stateless_test_case
+from reader_tests_cases . copy_paste import *
+from reader_tests_cases . ids import *
+from reader_tests_cases . lexer import *
+from reader_tests_cases . modules import *
+from reader_tests_cases . proc import *
+from reader_tests_cases . stateless import *
 from reader_tests_cases . helper import helper
-
-class proc_test_case ( unittest . TestCase ) :
-    def setUp ( self ) :
-        self . h = helper ( )
-    def test_empty ( self ) :
-        ae = self . assertEqual
-        r = self . h . rec
-        ae ( r ( 'stateless test1\n proc proc1\n' ) ,
-            { 'stateless' : { 'test1' :
-                { 'proc1' : { } } } } )
-    def test_args ( self ) :
-        ae = self . assertEqual
-        r = self . h . rec
-        ae ( r ( 'stateless test1\n proc proc1\n  args arg1\n' ) ,
-            { 'stateless' : { 'test1' :
-                { 'proc1' : { 'args' : { 'arg1' : { } } } } } } )
-    def test_vars ( self ) :
-        ae = self . assertEqual
-        r = self . h . rec
-        ae ( r ( 'stateless test1\n proc proc1\n  vars arg1\n' ) ,
-            { 'stateless' : { 'test1' :
-                { 'proc1' : { 'vars' : { 'arg1' : { } } } } } } )
-    def test_ops ( self ) :
-        ae = self . assertEqual
-        r = self . h . rec
-        ae ( r ( 'stateless test1\n proc proc1\n  ops\n   call1\n' ) ,
-            { 'stateless' : { 'test1' :
-                { 'proc1' : { 'ops' : [ { 'call' : [ 'call1' ] } ] } } } } )
 
 class statement_with_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
