2b2e3734bc2bdb70331d5450a1c4200d5af6f407
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-27 20:19:42 +0400

    working on reader tests separation

--- src/codegen/reader_tests.py
+++ src/codegen/reader_tests.py
@@ -7,20 +7,8 @@ from reader_tests_cases . lexer import *
 from reader_tests_cases . modules import *
 from reader_tests_cases . proc import *
 from reader_tests_cases . stateless import *
+from reader_tests_cases . statement_with import *
 from reader_tests_cases . helper import helper
-
-class statement_with_test_case ( unittest . TestCase ) :
-    def setUp ( self ) :
-        self . h = helper ( )
-    def test_simple ( self ) :
-        ae = self . assertEqual
-        r = self . h . rec
-        ae ( r ( 'stateless test1\n proc proc1\n  ops\n'
-            '   with call1\n    call2\n    call3\n' ) ,
-            { 'stateless' : { 'test1' : { 'proc1' : { 'ops' : [ { 'with' :
-                { 'call1' :
-                    [ { 'call' : [ 'call2' ] }
-                    , { 'call' : [ 'call3' ] } ] } } ] } } } } )
 
 class statement_assign_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
