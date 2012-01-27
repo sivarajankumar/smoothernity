b6d9f325f8ec2d58fb3f044e5d540d3ad857cfd5
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-27 20:24:11 +0400

    working on reader tests separation

--- src/codegen/reader_tests.py
+++ src/codegen/reader_tests.py
@@ -1,7 +1,6 @@
 import unittest
-import fractions
-import reader
 from reader_tests_cases . conditions import *
+from reader_tests_cases . consts import *
 from reader_tests_cases . copy_paste import *
 from reader_tests_cases . ids import *
 from reader_tests_cases . lexer import *
@@ -13,42 +12,6 @@ from reader_tests_cases . statement_call import *
 from reader_tests_cases . statement_if import *
 from reader_tests_cases . statement_with import *
 from reader_tests_cases . helper import helper
-
-class consts_test_case ( unittest . TestCase ) :
-    def setUp ( self ) :
-        self . h = helper ( )
-    def test_raises ( self ) :
-        ar = self . assertRaises
-        r = self . h . rec
-        re = reader . exception
-        ar ( re , r , 'consts test1\nconsts test2\n' )
-        ar ( re , r , 'consts test1\nconst1 11\n' )
-        ar ( re , r , 'consts test1\n const1 11\nconst2 11\n' )
-        ar ( re , r , 'consts test1\n const1 11\n  const2 11\n' )
-    def test_num_whole ( self ) :
-        ae = self . assertEqual
-        r = self . h . rec
-        ae ( r ( 'consts test1\n const1 11\n const2 - 22\n' ) ,
-            { 'consts' : { 'test1' : { 'const1' : 11 , 'const2' : - 22 } } } )
-    def test_num_fract ( self ) :
-        ae = self . assertEqual
-        r = self . h . rec
-        f = fractions . Fraction
-        ae ( r ( 'consts test1\n const1 11 / 22\n const2 - 22 / 33\n' ) ,
-            { 'consts' : { 'test1' : 
-                { 'const1' : f ( 11 , 22 ) 
-                , 'const2' : f ( - 22 , 33 ) } } } )
-    def test_expression ( self ) :
-        ae = self . assertEqual
-        r = self . h . rec
-        ae ( r ( 'consts test1\n const1 [ 2 + 2 ]\n const2 []\n' ) ,
-            { 'consts' : { 'test1' : 
-                { 'const1' : '[ 2 + 2 ]' , 'const2' : '[]' } } } )
-    def test_combine ( self ) :
-        ae = self . assertEqual
-        r = self . h . rec
-        ae ( r ( 'consts test1\n const1 11\nconsts test1\n  const2 22\n' ) ,
-            { 'consts' : { 'test1' : { 'const1' : 11 , 'const2' : 22 } } } )
 
 class types_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
