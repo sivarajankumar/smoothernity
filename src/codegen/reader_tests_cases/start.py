3bd614017141f5f4ed11d83eedd8608e6a7a9f5e
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-01-31 19:59:39 +0400

    working on module procs

--- src/codegen/reader_tests_cases/start.py
+++ src/codegen/reader_tests_cases/start.py
@@ -4,7 +4,7 @@ from reader_tests_cases . helper import helper
 class start_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
         self . h = helper ( )
-    def test_combine ( self ) :
+    def test_combine_dict ( self ) :
         ae = self . assertEqual
         r = self . h . rec
         ae ( r ( 'types test1\n type1 atr1\n'
@@ -12,3 +12,10 @@ class start_test_case ( unittest . TestCase ) :
             { 'types' : { 'test1' : 
                 { 'type1' : { 'atr1' : { } }
                 , 'type2' : { 'atr2' : { } } } } } )
+    def test_combine_list ( self ) :
+        ae = self . assertEqual
+        r = self . h . rec
+        ae ( r ( 'module test1\n proc proc1\n'
+                 'module test1\n proc proc2\n' ) ,
+            { 'modules' : { 'module1' : 
+                { 'procs' : { 'proc1' : { } , 'proc2' : { } } } } } )
