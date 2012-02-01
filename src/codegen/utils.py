eabcc34741dc893dbb131da6899f1091ba35dd3e
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-01 18:25:38 +0400

    working on utils

--- src/codegen/utils.py
+++ src/codegen/utils.py
@@ -1,6 +1,11 @@
-class exception ( Exception ) :
-    def __init__ ( self , text ) :
-        Exception . __init__ ( self , text )
-
-def merge ( * args ) :
-    return args [ 0 ]
+def merge ( dst , src ) :
+    types = ( type ( dst ) , type ( src ) )
+    if types == ( dict , dict ) :
+        for k , v in src . items ( ) :
+            dst [ k ] = merge ( dst [ k ] , v ) if k in dst else v
+        return dst
+    elif types == ( list , list ) :
+        dst += src
+        return dst
+    else :
+        return src
