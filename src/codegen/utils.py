c715e7ff9a636f0fba462db2523b9918a7322718
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-01 08:26:57 +0400

    working on utils

--- src/codegen/utils.py
+++ src/codegen/utils.py
@@ -1,2 +1,6 @@
+class exception ( Exception ) :
+    def __init__ ( self , text ) :
+        Exception . __init__ ( self , text )
+
 def merge ( * args ) :
     return args [ 0 ]
--- src/codegen/utils_tests.py
+++ src/codegen/utils_tests.py
@@ -2,6 +2,13 @@ import unittest
 import utils
 
 class merge_test_case ( unittest . TestCase ) :
+    def test_raises ( self ) :
+        ar = self . assertRaises
+        m = utils . merge
+        ue = utils . exception
+        ar ( ue , m , { } , [ ] )
+        ar ( ue , m , 1 , 2 )
+        ar ( ue , m , 'test1' , 'test2' )
     def test_empty ( self ) :
         ae = self . assertEqual
         m = utils . merge
@@ -9,6 +16,24 @@ class merge_test_case ( unittest . TestCase ) :
         ae ( m ( { } , { } ) , { } )
         ae ( m ( { } , { } , { } ) , { } )
         ae ( m ( [ ] , [ ] , [ ] ) , [ ] )
+    def test_value ( self ) :
+        ae = self . assertEqual
+        m = utils . merge
+        ae ( m ( None ) , None )
+        ae ( m ( 1 ) , 1 )
+        ae ( m ( 'test' ) , 'test' )
+    def test_lists ( self ) :
+        ae = self . assertEqual
+        m = utils . merge
+        ae ( m ( [ 1 ] , [ 2 , 3 ] ) , [ 1 , 2 , 3 ] )
+    def test_dicts ( self ) :
+        ae = self . assertEqual
+        m = utils . merge
+        ae ( m ( { 1 : 2 } , { 3 : 4 } ) , { 1 : 2 , 3 : 4 } )
+    def test_dict_of_lists ( self ) :
+        ae = self . assertEqual
+        m = utils . merge
+        ae ( m ( { 1 : [ 2 ] } , { 1 : [ 3 ] } ) , { 1 : [ 2 , 3 ] } )
 
 if __name__ == '__main__' :
     unittest . main ( )
