7e0ced148c5a4dd99a71f63bfdcab84077515c8e
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-01 19:27:59 +0400

    working on assign

--- src/codegen/reader_tests_cases/statement_assign.py
+++ src/codegen/reader_tests_cases/statement_assign.py
@@ -4,15 +4,7 @@ from reader_tests_cases . helper import helper
 class statement_assign_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
         self . h = helper ( )
-    def test_single ( self ) :
-        ae = self . assertEqual
-        r = self . h . rec
-        ae ( r ( 'stateless test1\n proc proc1\n  ops\n'
-            '   dst1 <- src1\n' ) ,
-            { 'stateless' : { 'test1' : { 'proc1' : { 'ops' : [ { 'assign' :
-                { 'from' : [ 'src1' ] 
-                , 'to' : [ 'dst1' ] } } ] } } } } )
-    def test_multi ( self ) :
+    def test_left ( self ) :
         ae = self . assertEqual
         r = self . h . rec
         ae ( r ( 'stateless test1\n proc proc1\n  ops\n'
