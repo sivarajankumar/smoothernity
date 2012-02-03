978c7b8581429c119fa9b6e4c15eae187cd88723
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-03 19:41:51 +0400

    working on expressions

--- src/codegen/normalizer_tests_cases/expression.py
+++ src/codegen/normalizer_tests_cases/expression.py
@@ -4,6 +4,14 @@ import unittest
 class expression_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
         self . n = normalizer . normalizer ( )
+    def test_raises ( self ) :
+        ar = self . assertRaises
+        ae = self . assertEqual
+        r = self . n . run
+        p = self . n . path
+        ne = normalizer . exception
+        ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ 1 + ]' } } } )
+        ae ( p ( ) , [ 'consts' , 'test1' , 'test2' ] )
     def test_number ( self ) :
         ae = self . assertEqual
         r = self . n . run
