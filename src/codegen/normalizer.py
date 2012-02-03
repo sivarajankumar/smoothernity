af672e19ba83f30e97074691a759775d2d0df57d
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-03 19:41:51 +0400

    working on expressions

--- src/codegen/normalizer.py
+++ src/codegen/normalizer.py
@@ -1,6 +1,12 @@
+class exception ( Exception ) :
+    def __init__ ( self , s ) :
+        Exception . __init__ ( self , s )
+
 class normalizer :
     def __init__ ( self ) :
-        pass
+        self . _path = None
+    def path ( self ) :
+        return self . _path
     def run ( self , src ) :
         res = dict ( )
         if 'consts' in src :
@@ -9,9 +15,13 @@ class normalizer :
                 res [ 'consts' ] [ module ] = dict ( )
                 for k , v in consts . items ( ) :
                     if type ( v ) is str :
+                        self . _path = [ 'consts' , module , k ]
                         assert ( v [ 0 ] , v [ - 1 ] ) == ( '[' , ']' )
                         env = { }
-                        exec ( '_expr = ' + v [ 1 : - 1 ] ) in env
+                        try :
+                            exec ( '_expr = ' + v [ 1 : - 1 ] ) in env
+                        except Exception as e :
+                            raise exception ( str ( e ) )
                         v = env [ '_expr' ]
                     res [ 'consts' ] [ module ] [ k ] = v
         return res
