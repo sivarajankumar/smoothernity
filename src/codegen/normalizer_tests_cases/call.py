e7edbc1a3c53d9df839064e05faf9b732427eeab
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-05 21:17:55 +0400

    working on calls normalization

--- src/codegen/normalizer_tests_cases/call.py
+++ src/codegen/normalizer_tests_cases/call.py
@@ -19,12 +19,13 @@ class call_test_case ( unittest . TestCase ) :
         ne = normalizer . exception
         bf ( 'func1' , [ { } , { } ] )
         try :
-            r ( { 'stateless' : { 'st1' : { 'proc1' : { 'ops' :
-                [ { 'call' : [ 'func1' , 'a1' ] } ] } } } } )
+            r ( { 'stateless' : { 'st1' : { 'proc1' : { 'ops' : [ { 'if' : [ {
+                'any' : [ { 'call' : [ 'func1' , 'a1' ] } ] } ] } ] } } } } )
         except ne as e :
             pass
         gp = e . get_path ( )
-        ae ( gp , [ 'stateless' , 'st1' , 'proc1' , 'ops' , 0 ] )
+        ae ( gp , [ 'stateless' , 'st1' , 'proc1' ,
+            'ops' , 0 , 'if' , 0 , 'any' , 0 ] )
     def test_split ( self ) :
         ae = self . assertEqual
         r = self . n . run
@@ -37,3 +38,13 @@ class call_test_case ( unittest . TestCase ) :
             [ { 'call' : [ 'func1' , 'a1' , 'a2' ] }
             , { 'call' : [ 'func1' , 'a3' , 'a4' ] }
             ] } } } } )
+    def test_no_args ( self ) :
+        ae = self . assertEqual
+        r = self . n . run
+        bf = self . n . bind_func
+        bf ( 'func1' , [ ] )
+        ae ( r ( { 'stateless' : { 'st1' : { 'proc1' : { 'ops' :
+            [ { 'call' : [ 'func1' ] }
+            ] } } } } ) ,
+            { 'stateless' : { 'st1' : { 'proc1' : { 'ops' :
+            [ { 'call' : [ 'func1' ] } ] } } } } )
