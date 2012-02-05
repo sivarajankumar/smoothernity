30185a7f8d9f5ca72de977135cfcfaf5624da715
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-05 21:07:48 +0400

    working on calls normalization

--- src/codegen/normalizer_tests_cases/call.py
+++ src/codegen/normalizer_tests_cases/call.py
@@ -4,6 +4,27 @@ import unittest
 class call_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
         self . n = normalizer . normalizer ( )
+    def test_raise ( self ) :
+        ar = self . assertRaises
+        r = self . n . run
+        bf = self . n . bind_func
+        ne = normalizer . exception
+        bf ( 'func1' , [ { } , { } ] )
+        ar ( ne , r , { 'stateless' : { 'st1' : { 'proc1' : { 'ops' :
+            [ { 'call' : [ 'func1' , 'a1' ] } ] } } } } )
+    def test_exception_path ( self ) :
+        ae = self . assertEqual
+        r = self . n . run
+        bf = self . n . bind_func
+        ne = normalizer . exception
+        bf ( 'func1' , [ { } , { } ] )
+        try :
+            r ( { 'stateless' : { 'st1' : { 'proc1' : { 'ops' :
+                [ { 'call' : [ 'func1' , 'a1' ] } ] } } } } )
+        except ne as e :
+            pass
+        gp = e . get_path ( )
+        ae ( gp , [ 'stateless' , 'st1' , 'proc1' , 'ops' , 0 ] )
     def test_split ( self ) :
         ae = self . assertEqual
         r = self . n . run
