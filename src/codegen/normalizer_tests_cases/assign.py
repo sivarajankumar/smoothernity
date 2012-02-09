521050e010350c77a7c1896c76438bb3f75180eb
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-09 10:43:26 +0400

    working on assign normalization

--- src/codegen/normalizer_tests_cases/assign.py
+++ src/codegen/normalizer_tests_cases/assign.py
@@ -6,7 +6,7 @@ from normalizer_tests_cases . helper import merge_skeleton_proc as mproc
 class assign_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
         self . n = normalizer . normalizer ( )
-    def test_augment ( self ) :
+    def test_split ( self ) :
         ae = self . assertEqual
         r = self . n . run
         ae ( r ( { 'anywhere' : { 'anywhere' : [ { 'assign' :
