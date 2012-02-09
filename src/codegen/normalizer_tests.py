1da95a2bb7554ccaf760e300b732024ed92ea04e
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-09 19:58:49 +0400

    working on with normalization

--- src/codegen/normalizer_tests.py
+++ src/codegen/normalizer_tests.py
@@ -4,6 +4,7 @@ from normalizer_tests_cases . call import *
 from normalizer_tests_cases . consts import *
 from normalizer_tests_cases . expression import *
 from normalizer_tests_cases . skeleton import *
+from normalizer_tests_cases . with_ import *
 
 if __name__ == '__main__' :
     unittest . main ( )
