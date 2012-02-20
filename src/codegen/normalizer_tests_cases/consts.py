7f931afac093e42c71a0cc29dcca5281ad6f07e6
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-20 20:15:10 +0400

    working on normalizer tests separation

--- src/codegen/normalizer_tests_cases/consts.py
+++ src/codegen/normalizer_tests_cases/consts.py
@@ -5,27 +5,10 @@ from normalizer_tests_cases . helper import merge_skeleton_root as mroot
 class consts_test_case ( unittest . TestCase ) :
     def setUp ( self ) :
         self . n = normalizer . normalizer ( )
-        bf = self . n . bind_func
-        bf ( 'func1' , [ { } ] )
-    def test_number ( self ) :
-        ae = self . assertEqual
-        r = self . n . run
-        ae ( r (
-            { 'consts' : { 'test1' : { 'test2' : 1 } }
-            , 'anywhere' : { 'anywhere' : [ { 'call' :
-                [ 'func1' , 'test1_consts_test2' ] } ] } } ) ,
-            mroot (
-                { 'consts' : { 'test1' : { 'test2' : 1 } }
-                , 'anywhere' : { 'anywhere' : [ { 'call' :
-                    [ 'func1' , 1 ] } ] } } ) )
     def test_expression ( self ) :
         ae = self . assertEqual
-        r = self . n . run
-        ae ( r (
-            { 'consts' : { 'test1' : { 'test2' : '[ 1 ]' } }
-            , 'anywhere' : { 'anywhere' : [ { 'call' :
-                [ 'func1' , 'test1_consts_test2' ] } ] } } ) ,
-            mroot (
-                { 'consts' : { 'test1' : { 'test2' : 1 } }
-                , 'anywhere' : { 'anywhere' : [ { 'call' :
-                    [ 'func1' , 1 ] } ] } } ) )
+        r = self . n . run_consts
+        ae ( r ( mroot ( { 'consts' : { 'test1' :
+                { 'test2' : 1 , 'test3' : '[ test2 ]' } } } ) ) ,
+            mroot ( { 'consts' : { 'test1' :
+                { 'test2' : 1 , 'test3' : 1 } } } ) )
