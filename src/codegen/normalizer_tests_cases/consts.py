a707a582d0b12a1ea35c47226a314077aa53f7ac
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-20 20:42:33 +0400

    working on normalizer tests separation

--- src/codegen/normalizer_tests_cases/consts.py
+++ src/codegen/normalizer_tests_cases/consts.py
@@ -7,37 +7,54 @@ class consts_test_case ( unittest . TestCase ) :
         self . n = normalizer . normalizer ( )
     def test_raises ( self ) :
         ar = self . assertRaises
+        r = lambda x : self . n . run_consts ( mroot ( x ) )
         ne = normalizer . exception
-        r = self . n . run_consts
-        ar ( ne , r , mroot ( { 'consts' : { 'test1' :
-            { 'test2' : '[ unknown ]' } } } ) )
-    def test_local_ref ( self ) :
+        ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ ]' } } } )
+        ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ 1 + ]' } } } )
+        ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ 1 / 0 ]' } } } )
+        ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ t3 ]' } } } )
+    def test_exception_path ( self ) :
         ae = self . assertEqual
         r = self . n . run_consts
-        ae ( r ( mroot ( { 'consts' : { 'test1' :
-                { 'test2' : 1
-                , 'test3' : '[ test2 ]' } } } ) ) ,
-            mroot ( { 'consts' : { 'test1' :
-                { 'test2' : 1
-                , 'test3' : 1 } } } ) )
+        try :
+            r ( mroot ( { 'consts' : { 't1' : { 't2' : '[ 1 + ]' } } } ) )
+        except Exception as e :
+            pass
+        ae ( e . get_path ( ) , [ 'consts' , 't1' , 't2' ] )
+    def test_unicode ( self ) :
+        ae = self . assertEqual
+        r = self . n . run_consts
+        ae ( r ( mroot ( { 'consts' : { 't1' : { 't2' : u'[ 1 ]' } } } ) ) ,
+            mroot ( { 'consts' : { 't1' : { 't2' : 1 } } } ) )
+    def test_ref_to_expressions ( self ) :
+        ae = self . assertEqual
+        r = self . n . run_consts
+        ae ( r ( mroot ( { 'consts' : { 'consts1' :
+            { 'test1' : '[ test2 + 1 ]' , 'test2' : '[ 1 ]' } } } ) ) ,
+            mroot ( { 'consts' : { 'consts1' :
+            { 'test1' : 2 , 'test2' : 1 } } } ) )
+    def test_ref_ref ( self ) :
+        ae = self . assertEqual
+        r = self . n . run_consts
+        ae ( r ( mroot ( { 'consts' : { 'consts1' :
+            { 'test1' : 1
+            , 'test2' : 1
+            , 'test3' : '[ test1 + test2 ]' } } } ) ) ,
+            mroot ( { 'consts' : { 'consts1' :
+            { 'test1' : 1 , 'test2' : 1 , 'test3' : 2 } } } ) )
+    def test_pure_ref ( self ) :
+        ae = self . assertEqual
+        r = self . n . run_consts
+        ae ( r ( mroot ( { 'consts' : { 'consts1' :
+            { 'test1' : 1 , 'test2' : '[ test1 ]' } } } ) ) ,
+            mroot ( { 'consts' : { 'consts1' :
+            { 'test1' : 1 , 'test2' : 1 } } } ) )
     def test_global_ref ( self ) :
         ae = self . assertEqual
         r = self . n . run_consts
         ae ( r ( mroot ( { 'consts' :
-                { 'test1' : { 'const1' : 1 }
-                , 'test2' : { 'const2' : '[ test1_consts_const1 ]' } } } ) ) ,
+            { 'consts1' : { 'test1' : 1 }
+            , 'consts2' : { 'test2' : '[ consts1_consts_test1 ]' } } } ) ) ,
             mroot ( { 'consts' :
-                { 'test1' : { 'const1' : 1 }
-                , 'test2' : { 'const2' : 1 } } } ) )
-    def test_inter_ref ( self ) :
-        ae = self . assertEqual
-        r = self . n . run_consts
-        ae ( r ( mroot ( { 'consts' : { 'test1' :
-                { 'test2' : 1
-                , 'test3' : '[ test4 ]'
-                , 'test4' : '[ test2 ]'
-                } } } ) ) ,
-            mroot ( { 'consts' : { 'test1' :
-                { 'test2' : 1
-                , 'test3' : 1
-                , 'test4' : 1 } } } ) )
+            { 'consts1' : { 'test1' : 1 }
+            , 'consts2' : { 'test2' : 1 } } } ) )
