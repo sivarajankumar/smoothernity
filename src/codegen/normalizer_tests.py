fcfb047f495b1ab0305daf7f087717c409e6ab97
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-22 18:02:26 +0400

    working on normalizer tests split

--- src/codegen/normalizer_tests.py
+++ src/codegen/normalizer_tests.py
@@ -1,10 +1,9 @@
 import unittest
 from normalizer_tests_cases . assign import *
-from normalizer_tests_cases . call import *
 from normalizer_tests_cases . calls_split import *
 from normalizer_tests_cases . consts import *
 from normalizer_tests_cases . expression import *
-from normalizer_tests_cases . send import *
+from normalizer_tests_cases . sends_split import *
 from normalizer_tests_cases . skeleton import *
 from normalizer_tests_cases . values import *
 from normalizer_tests_cases . with_ import *
---
+++ src/codegen/normalizer_tests_cases/sends_split.py
@@ -0,0 +1,69 @@
+import normalizer
+import unittest
+from normalizer_tests_cases . helper import merge_skeleton_root as mroot
+from normalizer_tests_cases . helper import merge_skeleton_proc as mproc
+from normalizer_tests_cases . helper import merge_skeleton_messages as mmsg
+
+class sends_split_test_case ( unittest . TestCase ) :
+    def setUp ( self ) :
+        self . n = normalizer . normalizer ( )
+    def test_raise ( self ) :
+        ar = self . assertRaises
+        r = self . n . run_sends_split
+        ne = normalizer . exception
+        ar ( ne , r , mroot (
+            { 'messages' : { 'test1' : mmsg ( { 'receive' :
+                { 'msg1' : [ { } , { } ] } } ) }
+            , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : mproc (
+                { 'ops' : [ { 'send' : [ 'test1_msg1' , 'a' ] } ]
+            } ) } } } } ) )
+        ar ( ne , r , mroot (
+            { 'messages' : { 'test1' : mmsg ( { 'receive' :
+                { 'msg1' : [ { } , { } ] } } ) }
+            , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : mproc (
+                { 'ops' : [ { 'send' : [ 'unknown' , 'a' ] } ]
+            } ) } } } } ) )
+    def test_exception_path ( self ) :
+        ae = self . assertEqual
+        r = self . n . run_sends_split
+        ne = normalizer . exception
+        try :
+            r ( mroot (
+                { 'messages' : { 'test1' : mmsg ( { 'receive' :
+                    { 'msg1' : [ { } , { } ] } } ) }
+                , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : mproc (
+                    { 'ops' : [ { 'if' : [ { 'any' : [ { 'send' :
+                        [ 'test1_msg1' , 'a' ] } ] } ] } ] } ) } } } } ) )
+        except ne as e :
+            pass
+        gp = e . get_path ( )
+        ae ( gp , [ 'stateless' , 'st1' , 'proc' , 'proc1' ,
+            'ops' , 0 , 'if' , 0 , 'any' , 0 ] )
+    def test_split_args ( self ) :
+        ae = self . assertEqual
+        r = self . n . run_sends_split
+        ae ( r ( mroot (
+            { 'messages' : { 'test1' : mmsg (
+                { 'receive' : { 'msg1' : [ { } , { } ] } } ) }
+            , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : mproc (
+                { 'ops' : [ { 'send' :
+                    [ 'test1_msg1' , 'a1' , 'a2' , 'a3' , 'a4' ] } ]
+            } ) } } } } ) ) , mroot (
+            { 'messages' : { 'test1' : mmsg (
+                { 'receive' : { 'msg1' : [ { } , { } ] } } ) }
+            , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : mproc (
+                { 'ops' :
+                    [ { 'send' : [ 'test1_msg1' , 'a1' , 'a2' ] }
+                    , { 'send' : [ 'test1_msg1' , 'a3' , 'a4' ] }
+            ] } ) } } } } ) )
+    def test_no_args ( self ) :
+        ae = self . assertEqual
+        r = self . n . run_sends_split
+        s = mroot (
+            { 'messages' : { 'test1' : mmsg (
+                { 'receive' : { 'msg1' : [ ] } } ) }
+            , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : mproc (
+                { 'ops' :
+                    [ { 'send' : [ 'test1_msg1' ] } ]
+            } ) } } } } )
+        ae ( r ( s ) , s )
