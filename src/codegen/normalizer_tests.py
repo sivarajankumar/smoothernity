fb6042bc5e3b788bea1944038192050aa7d1c27b
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-02-27 20:49:02 +0400

    working on normalizer tests separation

--- src/codegen/normalizer_tests.py
+++ src/codegen/normalizer_tests.py
@@ -3,6 +3,7 @@ from normalizer_tests_cases . assigns import *
 from normalizer_tests_cases . calls import *
 from normalizer_tests_cases . consts import *
 from normalizer_tests_cases . expression import *
+from normalizer_tests_cases . names import *
 from normalizer_tests_cases . sends import *
 from normalizer_tests_cases . skeleton import *
 from normalizer_tests_cases . values import *
