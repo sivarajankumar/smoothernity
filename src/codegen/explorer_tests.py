56529945657c4d487bddb66aa31f82d52375dfec
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-01 19:48:00 +0400

    working on explorer

--- src/codegen/explorer_tests.py
+++ src/codegen/explorer_tests.py
@@ -12,6 +12,14 @@ class explorer_test_case ( unittest . TestCase ) :
             , 'st2' : { 'proc' : { 'proc2' : 'test2' } } } } ) ,
             { 'st1_stateless_proc1' : 'test1'
             , 'st2_stateless_proc2' : 'test2' } )
+    def test_get_trace_procs ( self ) :
+        g = lambda x : self . e ( x ) . get_trace_procs ( )
+        ae = self . assertEqual
+        ae ( g ( { 'trace' :
+            { 'st1' : { 'proc' : { 'proc1' : 'test1' } }
+            , 'st2' : { 'proc' : { 'proc2' : 'test2' } } } } ) ,
+            { 'st1_trace_proc1' : 'test1'
+            , 'st2_trace_proc2' : 'test2' } )
 
 if __name__ == '__main__' :
     unittest . main ( )
