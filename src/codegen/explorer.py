c8ecb516229e86cee7704ce20808a0bf49290093
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-01 19:41:39 +0400

    working on explorer get stateless procs

---
+++ src/codegen/explorer.py
@@ -0,0 +1,5 @@
+class explorer :
+    def __init__ ( self , storage ) :
+        self . _storage = storage
+    def get_stateless_procs ( self ) :
+        pass
--- src/codegen/explorer_tests.py
+++ src/codegen/explorer_tests.py
@@ -2,7 +2,16 @@ import unittest
 import explorer
 
 class explorer_test_case ( unittest . TestCase ) :
-    pass
+    def setUp ( self ) :
+        self . e = explorer . explorer
+    def test_get_stateless_procs ( self ) :
+        g = lambda x : self . e ( x ) . get_stateless_procs ( )
+        ae = self . assertEqual
+        ae ( g ( { 'stateless' :
+            { 'st1' : { 'proc' : { 'proc1' : 'test1' } }
+            , 'st2' : { 'proc' : { 'proc2' : 'test2' } } } } ) ,
+            { 'st1_stateless_proc1' : 'test1'
+            , 'st2_stateless_proc2' : 'test2' } )
 
 if __name__ == '__main__' :
     unittest . main ( )
