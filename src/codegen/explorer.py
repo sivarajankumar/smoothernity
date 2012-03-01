cbb888805e46c0755279d841777d69c966e314a3
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-01 23:29:40 +0400

    working on explorer

--- src/codegen/explorer.py
+++ src/codegen/explorer.py
@@ -1,3 +1,27 @@
+class explorer :
+    def __init__ ( self , storage ) :
+        self . _storage = storage
+        self . _messages_receives = _extract_messages_receives ( storage )
+        self . _platform_procs = _extract_platform_procs ( storage )
+        self . _stateless_procs = _extract_stateless_procs ( storage )
+        self . _trace_procs = _extract_trace_procs ( storage )
+    def get_messages_receives ( self ) :
+        return self . _messages_receives
+    def get_platform_procs ( self ) :
+        return self . _platform_procs
+    def get_stateless_procs ( self ) :
+        return self . _stateless_procs
+    def get_trace_procs ( self ) :
+        return self . _trace_procs
+    def get_local_procs ( self , path ) :
+        return _extract_local_procs ( self . _storage , path )
+    def get_local_stateless_procs ( self , path ) :
+        return _extract_local_some_procs \
+            ( self . _storage , path , 'stateless' )
+    def get_local_trace_procs ( self , path ) :
+        return _extract_local_some_procs \
+            ( self . _storage , path , 'trace' )
+
 def _extract_stateless_procs ( storage ) :
     res = { }
     for k , v in storage [ 'stateless' ] . items ( ) :
@@ -22,26 +46,11 @@ def _extract_messages_receives ( storage ) :
             res [ '%s_%s' % ( k , kk ) ] = vv
     return res
 
-class explorer :
-    def __init__ ( self , storage ) :
-        self . _storage = storage
-        self . _messages_receives = _extract_messages_receives ( storage )
-        self . _platform_procs = _extract_platform_procs ( storage )
-        self . _stateless_procs = _extract_stateless_procs ( storage )
-        self . _trace_procs = _extract_trace_procs ( storage )
-    def get_messages_receives ( self ) :
-        return self . _messages_receives
-    def get_platform_procs ( self ) :
-        return self . _platform_procs
-    def get_stateless_procs ( self ) :
-        return self . _stateless_procs
-    def get_trace_procs ( self ) :
-        return self . _trace_procs
-    def get_local_procs ( self , path ) :
-        return self . _storage [ path [ 0 ] ] [ path [ 1 ] ] [ 'proc' ]
-    def get_local_stateless_procs ( self , path ) :
-        res = { }
-        for kk , vv in self . _storage [ 'stateless' ] \
-                        [ path [ 1 ] ] [ 'proc' ] . items ( ) :
-            res [ 'stateless_%s' % kk ] = vv
-        return vv
+def _extract_local_some_procs ( storage , path , some ) :
+    res = { }
+    for kk , vv in storage [ some ] [ path [ 1 ] ] [ 'proc' ] . items ( ) :
+        res [ '%s_%s' % ( some , kk ) ] = vv
+    return res
+
+def _extract_local_procs ( storage , path ) :
+    return storage [ path [ 0 ] ] [ path [ 1 ] ] [ 'proc' ]
