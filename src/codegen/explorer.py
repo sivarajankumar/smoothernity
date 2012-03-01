0d4088f72d1e0f20261e9f05c8bd6f14f0ed9489
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-01 19:52:53 +0400

    working on explorer

--- src/codegen/explorer.py
+++ src/codegen/explorer.py
@@ -14,11 +14,20 @@ def _extract_trace_procs ( storage ) :
                 res [ '%s_trace_%s' % ( k , kk ) ] = vv
     return res
 
+def _extract_platform_procs ( storage ) :
+    if 'platform_procs' in storage :
+        return storage [ 'platform_procs' ]
+    else :
+        return { }
+
 class explorer :
     def __init__ ( self , storage ) :
         self . _storage = storage
+        self . _platform_procs = _extract_platform_procs ( storage )
         self . _stateless_procs = _extract_stateless_procs ( storage )
         self . _trace_procs = _extract_trace_procs ( storage )
+    def get_platform_procs ( self ) :
+        return self . _platform_procs
     def get_stateless_procs ( self ) :
         return self . _stateless_procs
     def get_trace_procs ( self ) :
--- src/codegen/explorer_tests.py
+++ src/codegen/explorer_tests.py
@@ -7,6 +7,7 @@ class explorer_test_case ( unittest . TestCase ) :
     def test_get_stateless_procs ( self ) :
         g = lambda x : self . e ( x ) . get_stateless_procs ( )
         ae = self . assertEqual
+        ae ( g ( { } ) , { } )
         ae ( g ( { 'stateless' :
             { 'st1' : { 'proc' : { 'proc1' : 'test1' } }
             , 'st2' : { 'proc' : { 'proc2' : 'test2' } } } } ) ,
@@ -15,11 +16,21 @@ class explorer_test_case ( unittest . TestCase ) :
     def test_get_trace_procs ( self ) :
         g = lambda x : self . e ( x ) . get_trace_procs ( )
         ae = self . assertEqual
+        ae ( g ( { } ) , { } )
         ae ( g ( { 'trace' :
             { 'st1' : { 'proc' : { 'proc1' : 'test1' } }
             , 'st2' : { 'proc' : { 'proc2' : 'test2' } } } } ) ,
             { 'st1_trace_proc1' : 'test1'
             , 'st2_trace_proc2' : 'test2' } )
+    def test_get_platform_procs ( self ) :
+        g = lambda x : self . e ( x ) . get_platform_procs ( )
+        ae = self . assertEqual
+        ae ( g ( { } ) , { } )
+        ae ( g ( { 'platform_procs' :
+            { 'func1' : 'test1'
+            , 'func2' : 'test2' } } ) ,
+            { 'func1' : 'test1'
+            , 'func2' : 'test2' } )
 
 if __name__ == '__main__' :
     unittest . main ( )
