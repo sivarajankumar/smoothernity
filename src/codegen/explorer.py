556e24018f36e2602ebe76720e29632c0f81cefe
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-01 19:46:58 +0400

    working on explorer get stateless procs

--- src/codegen/explorer.py
+++ src/codegen/explorer.py
@@ -1,5 +1,14 @@
+def _extract_stateless_procs ( storage ) :
+    res = { }
+    if 'stateless' in storage :
+        for k , v in storage [ 'stateless' ] . items ( ) :
+            for kk , vv in v [ 'proc' ] . items ( ) :
+                res [ '%s_stateless_%s' % ( k , kk ) ] = vv
+    return res
+
 class explorer :
     def __init__ ( self , storage ) :
         self . _storage = storage
+        self . _stateless_procs = _extract_stateless_procs ( storage )
     def get_stateless_procs ( self ) :
-        pass
+        return self . _stateless_procs
