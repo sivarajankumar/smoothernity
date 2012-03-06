2a257ce6958a98d19ff0c751b17c20c83f69cace
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-06 20:24:10 +0400

    working on normalizer

--- src/codegen/normalizer/__init__.py
+++ src/codegen/normalizer/__init__.py
@@ -1,16 +1,7 @@
 import operator
 from utils import merge
 from itertools import combinations
-
-class exception ( Exception ) :
-    def __init__ ( self , text , src , path ) :
-        Exception . __init__ ( self , text )
-        self . _path = path
-        self . _src = src
-    def get_path ( self ) :
-        return self . _path
-    def get_src ( self ) :
-        return self . _src
+from normalizer . exception import exception
 
 def _is_text ( s ) :
     return type ( s ) in ( str , unicode )
--- src/codegen/normalizer_tests_cases/consts.py
+++ src/codegen/normalizer_tests_cases/consts.py
@@ -1,41 +1,35 @@
-import normalizer
 import unittest
+from normalizer . consts import run
+from normalizer . exception import exception
 from normalizer_tests_cases . helper import merge_skeleton_root as mroot
 
 class consts_test_case ( unittest . TestCase ) :
-    def setUp ( self ) :
-        self . n = normalizer . normalizer ( )
     def test_raises ( self ) :
         ar = self . assertRaises
-        r = lambda x : self . n . run_consts ( mroot ( x ) )
-        ne = normalizer . exception
+        r = lambda x : run ( mroot ( x ) )
+        ne = exception
         ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ ]' } } } )
         ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ 1 + ]' } } } )
         ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ 1 / 0 ]' } } } )
         ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ t3 ]' } } } )
     def test_exception_path ( self ) :
         ae = self . assertEqual
-        r = self . n . run_consts
+        r = run
         try :
             r ( mroot ( { 'consts' : { 't1' : { 't2' : '[ 1 + ]' } } } ) )
-        except Exception as e :
+        except exception as e :
             pass
         ae ( e . get_path ( ) , [ 'consts' , 't1' , 't2' ] )
-    def test_unicode ( self ) :
-        ae = self . assertEqual
-        r = self . n . run_consts
-        ae ( r ( mroot ( { 'consts' : { 't1' : { 't2' : u'[ 1 ]' } } } ) ) ,
-            mroot ( { 'consts' : { 't1' : { 't2' : 1 } } } ) )
     def test_ref_to_expressions ( self ) :
         ae = self . assertEqual
-        r = self . n . run_consts
+        r = run
         ae ( r ( mroot ( { 'consts' : { 'consts1' :
             { 'test1' : '[ test2 + 1 ]' , 'test2' : '[ 1 ]' } } } ) ) ,
             mroot ( { 'consts' : { 'consts1' :
             { 'test1' : 2 , 'test2' : 1 } } } ) )
     def test_ref_ref ( self ) :
         ae = self . assertEqual
-        r = self . n . run_consts
+        r = run
         ae ( r ( mroot ( { 'consts' : { 'consts1' :
             { 'test1' : 1
             , 'test2' : 1
@@ -44,14 +38,14 @@ class consts_test_case ( unittest . TestCase ) :
             { 'test1' : 1 , 'test2' : 1 , 'test3' : 2 } } } ) )
     def test_pure_ref ( self ) :
         ae = self . assertEqual
-        r = self . n . run_consts
+        r = run
         ae ( r ( mroot ( { 'consts' : { 'consts1' :
             { 'test1' : 1 , 'test2' : '[ test1 ]' } } } ) ) ,
             mroot ( { 'consts' : { 'consts1' :
             { 'test1' : 1 , 'test2' : 1 } } } ) )
     def test_global_ref ( self ) :
         ae = self . assertEqual
-        r = self . n . run_consts
+        r = run
         ae ( r ( mroot ( { 'consts' :
             { 'consts1' : { 'test1' : 1 }
             , 'consts2' : { 'test2' : '[ consts1_consts_test1 ]' } } } ) ) ,
