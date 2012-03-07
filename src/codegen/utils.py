4a2023e033bac5d3d30532cc98c0eed0e7555273
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-07 18:30:08 +0400

    working on ambiguity detection

--- src/codegen/utils.py
+++ src/codegen/utils.py
@@ -1,11 +1,20 @@
 from copy import deepcopy
 
-def merge ( dst_ , src_ ) :
+class merge_exception ( Exception ) :
+    def __init__ ( self , txt , key ) :
+        Exception . __init__ ( self , txt )
+        self . _key = key
+    def get_key ( self ) :
+        return self . _key
+
+def merge ( dst_ , src_ , overwrite = True ) :
     dst = deepcopy ( dst_ )
     src = deepcopy ( src_ )
     types = ( type ( dst ) , type ( src ) )
     if types == ( dict , dict ) :
         for k , v in src . items ( ) :
+            if not overwrite and k in dst :
+                raise merge_exception ( 'Key overwrite' , k )
             dst [ k ] = merge ( dst [ k ] , v ) if k in dst else v
         return dst
     elif types == ( list , list ) :
