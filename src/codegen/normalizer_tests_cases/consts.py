a478788f796986401cc8feb337c6d7d6921d3f4d
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-13 19:31:15 +0400

    removing normalize skeleton helpers

--- src/codegen/normalizer_tests_cases/consts.py
+++ src/codegen/normalizer_tests_cases/consts.py
@@ -1,12 +1,12 @@
 import unittest
 from normalizer . consts import run
 from normalizer . exception import exception
-from normalizer_tests_cases . helper import merge_skeleton_root as mroot
+from normalizer . skeleton import run as rskel
 
 class consts_test_case ( unittest . TestCase ) :
     def test_raises ( self ) :
         ar = self . assertRaises
-        r = lambda x : run ( mroot ( x ) )
+        r = lambda x : run ( rskel ( x ) )
         ne = exception
         ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ ]' } } } )
         ar ( ne , r , { 'consts' : { 'test1' : { 'test2' : '[ 1 + ]' } } } )
@@ -16,39 +16,39 @@ class consts_test_case ( unittest . TestCase ) :
         ae = self . assertEqual
         r = run
         try :
-            r ( mroot ( { 'consts' : { 't1' : { 't2' : '[ 1 + ]' } } } ) )
+            r ( rskel ( { 'consts' : { 't1' : { 't2' : '[ 1 + ]' } } } ) )
         except exception as e :
             pass
         ae ( e . get_path ( ) , [ 'consts' , 't1' , 't2' ] )
     def test_ref_to_expressions ( self ) :
         ae = self . assertEqual
         r = run
-        ae ( r ( mroot ( { 'consts' : { 'consts1' :
+        ae ( r ( rskel ( { 'consts' : { 'consts1' :
             { 'test1' : '[ test2 + 1 ]' , 'test2' : '[ 1 ]' } } } ) ) ,
-            mroot ( { 'consts' : { 'consts1' :
+            rskel ( { 'consts' : { 'consts1' :
             { 'test1' : 2 , 'test2' : 1 } } } ) )
     def test_ref_ref ( self ) :
         ae = self . assertEqual
         r = run
-        ae ( r ( mroot ( { 'consts' : { 'consts1' :
+        ae ( r ( rskel ( { 'consts' : { 'consts1' :
             { 'test1' : 1
             , 'test2' : 1
             , 'test3' : '[ test1 + test2 ]' } } } ) ) ,
-            mroot ( { 'consts' : { 'consts1' :
+            rskel ( { 'consts' : { 'consts1' :
             { 'test1' : 1 , 'test2' : 1 , 'test3' : 2 } } } ) )
     def test_pure_ref ( self ) :
         ae = self . assertEqual
         r = run
-        ae ( r ( mroot ( { 'consts' : { 'consts1' :
+        ae ( r ( rskel ( { 'consts' : { 'consts1' :
             { 'test1' : 1 , 'test2' : '[ test1 ]' } } } ) ) ,
-            mroot ( { 'consts' : { 'consts1' :
+            rskel ( { 'consts' : { 'consts1' :
             { 'test1' : 1 , 'test2' : 1 } } } ) )
     def test_global_ref ( self ) :
         ae = self . assertEqual
         r = run
-        ae ( r ( mroot ( { 'consts' :
+        ae ( r ( rskel ( { 'consts' :
             { 'consts1' : { 'test1' : 1 }
             , 'consts2' : { 'test2' : '[ consts1_consts_test1 ]' } } } ) ) ,
-            mroot ( { 'consts' :
+            rskel ( { 'consts' :
             { 'consts1' : { 'test1' : 1 }
             , 'consts2' : { 'test2' : 1 } } } ) )
