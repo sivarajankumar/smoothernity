e441b6392e883c1968b95ec09dbc5fca5b409550
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-27 20:56:33 +0400

    working on calls normalization

--- src/codegen/normalizer_tests_cases/calls.py
+++ src/codegen/normalizer_tests_cases/calls.py
@@ -1,3 +1,6 @@
+from normalizer . calls import run
+from normalizer . skeleton import run as rskel
+from normalizer . exception import exception
 import normalizer
 import unittest
 
@@ -6,19 +9,19 @@ class calls_test_case ( unittest . TestCase ) :
         self . n = normalizer . normalizer ( )
     def test_raise ( self ) :
         ar = self . assertRaises
-        r = self . n . run_calls
-        ne = normalizer . exception
+        r = lambda x : run ( rskel ( x ) )
+        ne = exception
         ar ( ne , r ,
-            { 'platform_procs' : { 'func1' : [ { } , { } ] }
+            { 'platform_procs' : { 'func1' : { 'args' : [ { } , { } ] } }
             , 'stateless' : { 'st1' : { 'proc' :
                 { 'proc1' : { 'ops' : [ { 'call' : [ 'func1' ] } ] } } } } } )
         ar ( ne , r ,
-            { 'platform_procs' : { 'func1' : [ { } , { } ] }
+            { 'platform_procs' : { 'func1' : { 'args' : [ { } , { } ] } }
             , 'stateless' : { 'st1' : { 'proc' :
                 { 'proc1' : { 'ops' : [ { 'call' : [ 'func1' , 'a' ] } ] } } }
             } } )
         ar ( ne , r ,
-            { 'platform_procs' : { 'func2' : [ ] }
+            { 'platform_procs' : { 'func2' : { } }
             , 'stateless' : { 'st1' : { 'proc' :
                 { 'proc1' : { 'ops' : [ { 'call' : [ 'func2' , 'a' ] } ]
                 } } } } } )
@@ -45,13 +48,14 @@ class calls_test_case ( unittest . TestCase ) :
             } ] } } } } } )
     def test_exception_path ( self ) :
         ae = self . assertEqual
-        r = self . n . run_calls
-        ne = normalizer . exception
+        r = run
+        ne = exception
         try :
-            r ( { 'platform_procs' : { 'func1' : [ { } , { } ] }
+            r ( rskel (
+                { 'platform_procs' : { 'func1' : { 'args' : [ { } , { } ] } }
                 , 'stateless' : { 'st1' : { 'proc' : { 'proc1' :
                     { 'ops' : [ { 'if' : [ { 'any' : [ { 'call' :
-                        [ 'func1' , 'a' ] } ] } ] } ] } } } } } )
+                        [ 'func1' , 'a' ] } ] } ] } ] } } } } } ) )
         except ne as e :
             pass
         gp = e . get_path ( )
@@ -59,72 +63,79 @@ class calls_test_case ( unittest . TestCase ) :
             'ops' , 0 , 'if' , 0 , 'any' , 0 ] )
     def test_split_args ( self ) :
         ae = self . assertEqual
-        r = self . n . run_calls
-        ae ( r (
-            { 'platform_procs' : { 'func1' : [ { } , { } ] }
+        r = run
+        ae ( r ( rskel (
+            { 'platform_procs' : { 'func1' : { 'args' : [ { } , { } ] } }
             , 'stateless' : { 'st1' : { 'proc' : { 'proc1' :
                 { 'ops' :
                     [ { 'call' : [ 'func1' , 'a1' , 'a2' , 'a3' , 'a4' ] }
-                ] } } } } } ) , 
-            { 'platform_procs' : { 'func1' : [ { } , { } ] }
+                ] } } } } } ) ) , rskel (
+            { 'platform_procs' : { 'func1' : { 'args' : [ { } , { } ] } }
             , 'stateless' : { 'st1' : { 'proc' : { 'proc1' :
                 { 'ops' :
                         [ { 'call' : [ 'func1' , 'a1' , 'a2' ] }
                         , { 'call' : [ 'func1' , 'a3' , 'a4' ] }
-                ] } } } } } )
+                ] } } } } } ) )
     def test_no_args ( self ) :
         ae = self . assertEqual
-        r = self . n . run_calls
-        s = { 'platform_procs' : { 'func1' : [ ] }
+        r = run
+        s = rskel (
+            { 'platform_procs' : { 'func1' : { } }
             , 'stateless' : { 'st1' : { 'proc' : { 'proc1' :
-                { 'ops' : [ { 'call' : [ 'func1' ] } ] } } } } }
+                { 'ops' : [ { 'call' : [ 'func1' ] } ] } } } } } )
         ae ( r ( s ) , s )
     def test_bind_func ( self ) :
         ae = self . assertEqual
-        r = self . n . run_calls
-        s = { 'platform_procs' : { 'func1' : [ { } ] }
+        r = run
+        s = rskel (
+            { 'platform_procs' : { 'func1' : { 'args' : [ { } ] } }
             , 'stateless' : { 'st1' : { 'proc' : { 'proc1' :
-                { 'ops' : [ { 'call' : [ 'func1' , 'a' ] } ] } } } } }
+                { 'ops' : [ { 'call' : [ 'func1' , 'a' ] } ] } } } } } )
         ae ( r ( s ) , s )
     def test_proc_local ( self ) :
         ae = self . assertEqual
-        r = self . n . run_calls
-        s = { 'stateless' : { 'st1' : { 'proc' :
+        r = run
+        s = rskel (
+            { 'stateless' : { 'st1' : { 'proc' :
                 { 'proc1' : { 'args' : [ { } ] }
                 , 'proc2' : { 'ops' : [ { 'call' : [ 'proc1' , 'a' ] }
-                ] } } } } }
+                ] } } } } } )
         ae ( r ( s ) , s )
     def test_proc_stateless ( self ) :
         ae = self . assertEqual
-        r = self . n . run_calls
-        s = { 'stateless' :
+        r = run
+        s = rskel (
+            { 'stateless' :
                 { 'st1' : { 'proc' : { 'proc1' : { 'args' : [ { } ] } } }
                 , 'st2' : { 'proc' : { 'proc2' : { 'ops' : [ { 'call' :
-                    [ 'st1_stateless_proc1' , 'a' ] } ] } } } } }
+                    [ 'st1_stateless_proc1' , 'a' ] } ] } } } } } )
         ae ( r ( s ) , s )
     def test_proc_trace ( self ) :
         ae = self . assertEqual
-        r = self . n . run_calls
-        s = { 'trace' :
-                { 'st1' : { 'proc' : { 'proc1' : { 'args' : [ { } ] } } } }
+        r = run
+        s = rskel (
+            { 'trace' :
+                { 'st1' : { 'proc' : { 'proc1' : { 'args' : [ { } ] } } }
                 , 'st2' : { 'proc' : { 'proc2' : { 'ops' : [ { 'call' :
-                    [ 'st1_trace_proc1' , 'a' ] } ] } } } }
+                    [ 'st1_trace_proc1' , 'a' ] } ] } } } } } )
         ae ( r ( s ) , s )
     def test_proc_stateless_local ( self ) :
         ae = self . assertEqual
-        r = self . n . run_calls
-        s = { 'stateless' :
+        r = run
+        s = rskel (
+            { 'stateless' :
                 { 'st1' : { 'proc' : { 'proc1' : { 'args' : [ { } ] } } } }
             , 'trace' :
                 { 'st1' : { 'proc' : { 'proc2' : { 'ops' : [ { 'call' :
-                    [ 'stateless_proc1' , 'a' ] } ] } } } } }
+                    [ 'stateless_proc1' , 'a' ] } ] } } } } } )
         ae ( r ( s ) , s )
     def test_proc_trace_local ( self ) :
         ae = self . assertEqual
-        r = self . n . run_calls
-        s = { 'trace' :
+        r = run
+        s = rskel (
+            { 'trace' :
                 { 'st1' : { 'proc' : { 'proc1' : { 'args' : [ { } ] } } } }
             , 'stateless' :
                 { 'st1' : { 'proc' : { 'proc2' : { 'ops' : [ { 'call' :
-                        [ 'trace_proc1' , 'a' ] } ] } } } } }
+                        [ 'trace_proc1' , 'a' ] } ] } } } } } )
         ae ( r ( s ) , s )
