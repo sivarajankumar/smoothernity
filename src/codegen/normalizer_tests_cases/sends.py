fbb8274d1142a28c55ef0609325d52715a766543
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-27 19:20:48 +0400

    working on sends normalization

--- src/codegen/normalizer_tests_cases/sends.py
+++ src/codegen/normalizer_tests_cases/sends.py
@@ -1,4 +1,5 @@
 from normalizer . sends import run
+from normalizer . skeleton import run as rskel
 import normalizer
 import unittest
 
@@ -39,20 +40,20 @@ class sends_test_case ( unittest . TestCase ) :
     def test_split_args ( self ) :
         ae = self . assertEqual
         r = run
-        ae ( r ( 
+        ae ( r ( rskel (
             { 'messages' : { 'test1' : 
                 { 'receive' : { 'msg1' : [ { } , { } ] } } }
             , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : 
                 { 'ops' : [ { 'send' :
                     [ 'test1_msg1' , 'a1' , 'a2' , 'a3' , 'a4' ] } ]
-            } } } } } ) , 
+            } } } } } ) ) , rskel (
             { 'messages' : { 'test1' : 
                 { 'receive' : { 'msg1' : [ { } , { } ] } } }
             , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : 
                 { 'ops' :
                     [ { 'send' : [ 'test1_msg1' , 'a1' , 'a2' ] }
                     , { 'send' : [ 'test1_msg1' , 'a3' , 'a4' ] }
-            ] } } } } } )
+            ] } } } } } ) )
     def test_no_args ( self ) :
         ae = self . assertEqual
         r = self . n . run_sends
