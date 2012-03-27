644148f5ae046112c93cb5127f9d44918e06450c
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-27 19:20:48 +0400

    working on sends normalization

--- src/codegen/normalizer/sends.py
+++ src/codegen/normalizer/sends.py
@@ -1,2 +1,20 @@
-def run ( src ) :
+from utils import rewrite
+from explorer import explorer
+
+def split_args ( what , where , src , path ) :
+    if type ( src ) is dict and what in src :
+        res = list ( )
+        name , args = src [ what ] [ 0 ] , src [ what ] [ 1 : ]
+        need_args = where [ name ]
+        la , lna = len ( args ) , len ( need_args )
+        while True :
+            res . append ( { what : [ name ] + args [ : lna ] } )
+            args = args [ lna : ]
+            if not args :
+                break
+        return res if len ( res ) > 1 else res [ 0 ]
     return src
+
+def run ( src ) :
+    e = explorer ( src ) . get_messages_receives ( )
+    return rewrite ( src , lambda s , p : split_args ( 'send' , e , s , p ) )
