58fbc849eb3e29aff6fd20dd4e86b41cf96a8ee9
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-28 19:54:38 +0400

    working on split args tests

--- src/codegen/normalizer/sends.py
+++ src/codegen/normalizer/sends.py
@@ -1,8 +1,8 @@
 from explorer import explorer
-from normalizer . split_args import split_args
+from normalizer . split_args import run as run_split_args
 
 def run ( src ) :
     e = explorer ( src )
-    return split_args ( 'send'
+    return run_split_args ( 'send'
         , lambda n , p : e . get_messages_receives ( ) [ n ]
         , src )
