484518f52dda19f6a17b6ab7a338f22bb6704cf0
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-03-28 20:29:07 +0400

    working on split args tests

--- src/codegen/normalizer_tests_cases/sends.py
+++ src/codegen/normalizer_tests_cases/sends.py
@@ -4,38 +4,6 @@ from normalizer . exception import exception
 import unittest
 
 class sends_test_case ( unittest . TestCase ) :
-    def test_raise ( self ) :
-        ar = self . assertRaises
-        r = run
-        ne = exception
-        ar ( ne , r , rskel (
-            { 'messages' : { 'test1' : { 'receive' :
-                { 'msg1' : [ { } , { } ] } } }
-            , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : 
-                { 'ops' : [ { 'send' : [ 'test1_msg1' , 'a' ] } ]
-            } } } } } ) )
-        ar ( ne , r , rskel (
-            { 'messages' : { 'test1' : { 'receive' :
-                { 'msg1' : [ { } , { } ] } } }
-            , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : 
-                { 'ops' : [ { 'send' : [ 'unknown' , 'a' ] } ]
-            } } } } } ) )
-    def test_exception_path ( self ) :
-        ae = self . assertEqual
-        r = run
-        ne = exception
-        try :
-            r ( rskel (
-                { 'messages' : { 'test1' : { 'receive' :
-                    { 'msg1' : [ { } , { } ] } } }
-                , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : 
-                    { 'ops' : [ { 'if' : [ { 'any' : [ { 'send' :
-                        [ 'test1_msg1' , 'a' ] } ] } ] } ] } } } } } ) )
-        except ne as e :
-            pass
-        gp = e . get_path ( )
-        ae ( gp , [ 'stateless' , 'st1' , 'proc' , 'proc1' ,
-            'ops' , 0 , 'if' , 0 , 'any' , 0 ] )
     def test_split_args ( self ) :
         ae = self . assertEqual
         r = run
@@ -53,14 +21,3 @@ class sends_test_case ( unittest . TestCase ) :
                     [ { 'send' : [ 'test1_msg1' , 'a1' , 'a2' ] }
                     , { 'send' : [ 'test1_msg1' , 'a3' , 'a4' ] }
             ] } } } } } ) )
-    def test_no_args ( self ) :
-        ae = self . assertEqual
-        r = run
-        s = rskel (
-            { 'messages' : { 'test1' : 
-                { 'receive' : { 'msg1' : [ ] } } }
-            , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : 
-                { 'ops' :
-                    [ { 'send' : [ 'test1_msg1' ] } ]
-            } } } } } )
-        ae ( r ( s ) , s )
