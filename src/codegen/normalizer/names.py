afa00c4862e1e232ae066edc6e9d7a49fcb76537
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-04-07 12:32:17 +0400

    working on names normalization

--- src/codegen/normalizer/names.py
+++ src/codegen/normalizer/names.py
@@ -4,33 +4,26 @@ from itertools import combinations
 from normalizer . exception import exception
 
 def run ( src ) :
+    where = explorer ( src ) . get_everything
     try :
-        a = explorer ( src ) . get_everything
-        return rewrite ( src ,
-            lambda s , p : _use_withs ( s , p , a ( p ) ) if is_text ( s ) else s )
+        return rewrite ( src , lambda s , p : _rewriter ( where , s , p ) )
     except exception as e :
         raise exception ( str ( e ) , src , e . get_path ( ) )
 
-def _use_withs ( name , path , names ) :
-    prefixes = _with_prefixes ( path )
-    candidates = _candidates ( name , names , prefixes )
-    if len ( candidates ) > 1 :
-        raise exception ( "Ambiguous identifiers: '%s'" %
-            ( ', ' . join ( candidates ) ) , None , path )
-    elif candidates :
-        return candidates [ 0 ]
+def _rewriter ( where , src , path ) :
+    if is_text ( src ) :
+        prefixes = [ path [ i + 1 ]
+            for i in xrange ( len ( path ) - 1 ) if path [ i ] == 'with' ]
+        candidates = filter ( lambda x : x in where ( path ) ,
+            [ src ] + [ '_' . join ( c ) + '_' + src
+                for i in xrange ( len ( prefixes ) )
+                    for c in combinations ( prefixes , i + 1 ) ] )
+        if len ( candidates ) > 1 :
+            raise exception ( "Ambiguous identifiers: '%s'" %
+                ( ', ' . join ( candidates ) ) , None , path )
+        elif candidates :
+            return candidates [ 0 ]
+        else :
+            return src
     else :
-        return name
-
-def _with_prefixes ( path ) :
-    res = list ( )
-    for i in xrange ( len ( path ) - 1 ) :
-        if path [ i ] == 'with' :
-            res . append ( path [ i + 1 ] )
-    return res
-
-def _candidates ( name , names , prefixes ) :
-    return filter ( lambda x : x in names ,
-        [ name ] + [ '_' . join ( c ) + '_' + name
-            for i in xrange ( len ( prefixes ) )
-                for c in combinations ( prefixes , i + 1 ) ] )
+        return src
