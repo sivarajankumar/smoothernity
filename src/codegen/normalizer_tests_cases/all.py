a43cccc42771b8cda01cf1f8063bd4e6795e8822
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-04-07 13:15:07 +0400

    working on normalizer integrity tests

--- src/codegen/normalizer_tests_cases/all.py
+++ src/codegen/normalizer_tests_cases/all.py
@@ -3,21 +3,19 @@ from normalizer import run
 from normalizer . skeleton import run as rskel
 
 class all_test_case ( unittest . TestCase ) :
-    def test_withs_after_names ( self ) :
+    def test_calls_after_names ( self ) :
         r = run
         ae = self . assertEqual
         ae ( r (
             { 'platform_procs' : { 'test1_func1' : { } }
             , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : { 'ops' :
-                [ { 'with' : { 'test1' : [ 'func1' ] } } ]
+                [ { 'with' : { 'test1' : [ { 'call' : [ 'func1' ] } ] } } ]
             } } } } } ) , rskel (
             { 'platform_procs' : { 'test1_func1' : { } }
             , 'stateless' : { 'st1' : { 'proc' : { 'proc1' : { 'ops' :
-                [ 'test1_func1' ]
+                [ { 'call' : [ 'test1_func1' ] } ]
             } } } } } ) )
-    def test_calls_after_withs ( self ) :
-        pass
-    def test_sends_after_withs ( self ) :
+    def test_sends_after_names ( self ) :
         pass
     def test_assigns ( self ) :
         r = run
