38fd2fb22e48f527dcb74594b7d9df7bce4bc70a
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-04-30 20:20:15 +0400

    working on check assigns args

--- src/codegen/normalizer_tests_cases/assigns_check.py
+++ src/codegen/normalizer_tests_cases/assigns_check.py
@@ -4,7 +4,17 @@ import unittest
 
 class assigns_check_test_case ( unittest . TestCase ) :
     def test_raise ( self ) :
-        pass
+        r = run
+        ne = exception
+        ar = self . assertRaises
+        ar ( ne , r ,
+            { 'platform_consts' : { 'const1' : 1 }
+            , 'anywhere' : { 'anywhere' :
+                { 'vars' : [ { 'var1' : { } } ]
+                , 'ops' : [ { 'assign' :
+                    { 'from' : [ 'var1' ]
+                    , 'to' : [ 'const1' ]
+                    } } ] } } } )
     def test_exception_args ( self ) :
         pass
     def test_correct ( self ) :
