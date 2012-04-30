884358336d32539953ea90232730cc3a5e2b06e0
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-04-30 20:32:42 +0400

    working on check assigns args

--- src/codegen/normalizer/assigns_check.py
+++ src/codegen/normalizer/assigns_check.py
@@ -1,14 +1,20 @@
+from explorer import explorer
 from utils import rewrite
 from normalizer . exception import exception
 
 def run ( src ) :
+    x = explorer ( src )
     try :
-        return rewrite ( src , _rewriter )
+        return rewrite ( src , lambda s , p : _rewriter ( s , p , x ) )
     except exception as e :
         raise exception ( str ( e ) , src , e . get_path ( ) )
 
-def _rewriter ( src , path ) :
+def _rewriter ( src , path , expl ) :
     if type ( src ) is dict and 'assign' in src :
         froms = src [ 'assign' ] [ 'from' ]
         tos = src [ 'assign' ] [ 'to' ]
+        for x in tos :
+            if x in expl . get_consts ( path ) :
+                raise exception ( "Assign to constant '%s'" % x
+                                , None , path )
     return src
--- src/codegen/normalizer_tests_cases/assigns_check.py
+++ src/codegen/normalizer_tests_cases/assigns_check.py
@@ -1,21 +1,48 @@
 from normalizer . assigns_check import run
 from normalizer . exception import exception
+from normalizer . skeleton import run as rskel
 import unittest
 
 class assigns_check_test_case ( unittest . TestCase ) :
-    def test_raise ( self ) :
+    def test_exception_args ( self ) :
+        r = run
+        ne = exception
+        s = rskel (
+            { 'platform_consts' : { 'const1' : 1 }
+            , 'path1' : { 'path2' :
+                { 'vars' : [ { 'var1' : { } } ]
+                , 'ops' : [ { 'assign' :
+                    { 'from' : [ 'var1' ]
+                    , 'to' : [ 'const1' ]
+                    } } ] } } } )
+        try :
+            r ( s )
+        except ne as e :
+            pass
+        ae = self . assertEqual
+        ae ( e . get_src ( ) , s )
+        ae ( e . get_path ( ) , [ 'path1' , 'path2' , 'ops' , 0 ] )
+    def test_assign_to_const ( self ) :
         r = run
         ne = exception
         ar = self . assertRaises
-        ar ( ne , r ,
+        ar ( ne , r , rskel (
             { 'platform_consts' : { 'const1' : 1 }
             , 'anywhere' : { 'anywhere' :
                 { 'vars' : [ { 'var1' : { } } ]
                 , 'ops' : [ { 'assign' :
                     { 'from' : [ 'var1' ]
                     , 'to' : [ 'const1' ]
+                    } } ] } } } ) )
+    def test_assign_to_value ( self ) :
+        r = run
+        s = rskel (
+            { 'platform_consts' : { 'const1' : 1 }
+            , 'anywhere' : { 'anywhere' :
+                { 'vars' : [ { 'var1' : { } } ]
+                , 'ops' : [ { 'assign' :
+                    { 'from' : [ 'const1' ]
+                    , 'to' : [ 'var1' ]
                     } } ] } } } )
-    def test_exception_args ( self ) :
-        pass
-    def test_correct ( self ) :
-        pass
+        ae = self . assertEqual
+        ae ( r ( s ) , s )
