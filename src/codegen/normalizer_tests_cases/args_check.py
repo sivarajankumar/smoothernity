f1fb309fbfa714169aff5149038f9aaa0819be01
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-05-07 21:01:10 +0400

    working on check args

--- src/codegen/normalizer_tests_cases/args_check.py
+++ src/codegen/normalizer_tests_cases/args_check.py
@@ -4,4 +4,20 @@ from normalizer . exception import exception
 import unittest
 
 class args_check_test_case ( unittest . TestCase ) :
-    pass
+    def test_raise ( self ) :
+        r = lambda x , y : run ( 'check' , lambda n , p : n in x , y )
+        s = { 'path1' : { 'path2' :
+                { 'check' : [ 'func1' , 'a' ] } } }
+        try :
+            r ( set ( ) , s )
+        except exception as e :
+            ae = self . assertEqual
+            ae ( e . get_path ( )  , [ 'path1' , 'path2' ] )
+            ae ( e . get_src ( )  , s )
+        else :
+            self . fail ( )
+    def test_success ( self ) :
+        r = lambda x , y : run ( 'check' , lambda n , p : n in x , y )
+        s = { 'anywhere' : [ { 'check' : [ 'func1' , 'a1' , 'a2' ] } ] }
+        ae = self . assertEqual
+        ae ( r ( set ( [ 'a1' , 'a2' ] ) , s ) , s )
