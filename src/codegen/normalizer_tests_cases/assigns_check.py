bfb88ad7dccf28d2198fee060d0d017b45030924
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-05-09 17:53:14 +0400

    working on readables and writables

--- src/codegen/normalizer_tests_cases/assigns_check.py
+++ src/codegen/normalizer_tests_cases/assigns_check.py
@@ -73,6 +73,38 @@ class assigns_check_test_case ( unittest . TestCase ) :
                         , 'to' : [ 'var1' ]
                         } } ] } } } ) :
             ae ( r ( rskel ( s ) ) , rskel ( s ) )
+    def test_assign_to_readable ( self ) :
+        r = run
+        ne = exception
+        ar = self . assertRaises
+        ar ( ne , r , rskel (
+            { 'messages' : { 'group1' : { 'receive' :
+                { 'msg1' : [ { 'arg1' : { } } ] } } }
+            , 'types' : { 'group1' :
+                { 'type1' : [ { 'field1' : { } } ] } }
+            , 'anywhere' : { 'group1' : { 'receive' :
+                { 'msg1' :
+                    { 'vars' : [ { 'var1' : { } } ]
+                    , 'ops' : [ { 'assign' :
+                        { 'from' : [ 'var1' ]
+                        , 'to' : [ 'msg_arg1_field1' ]
+                        } } ] } } } } } ) )
+    def test_assign_from_readable ( self ) :
+        r = run
+        s = rskel (
+            { 'messages' : { 'group1' : { 'receive' :
+                { 'msg1' : [ { 'arg1' : { } } ] } } }
+            , 'types' : { 'group1' :
+                { 'type1' : [ { 'field1' : { } } ] } }
+            , 'anywhere' : { 'group1' : { 'receive' :
+                { 'msg1' :
+                    { 'vars' : [ { 'var1' : { } } ]
+                    , 'ops' : [ { 'assign' :
+                        { 'from' : [ 'msg_arg1_field1' ]
+                        , 'to' : [ 'var1' ]
+                        } } ] } } } } } )
+        ae = self . assertEqual
+        ae ( r ( s ) , s )
     def test_assign_from_value_to_value ( self ) :
         r = run
         s = rskel (
