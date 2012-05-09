1bb565fffa0d48153a5774689304ea45a4c73683
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-05-09 17:53:14 +0400

    working on readables and writables

--- src/codegen/normalizer/assigns_check.py
+++ src/codegen/normalizer/assigns_check.py
@@ -2,6 +2,7 @@ from explorer import explorer
 from utils import rewrite , is_text
 from normalizer . exception import exception
 from fractions import Fraction
+from operator import and_
 
 def run ( src ) :
     x = explorer ( src )
@@ -12,16 +13,18 @@ def run ( src ) :
 
 def _rewriter ( src , path , expl ) :
     if type ( src ) is dict and 'assign' in src :
-        froms = src [ 'assign' ] [ 'from' ]
-        tos = src [ 'assign' ] [ 'to' ]
-        for x in tos :
-            if type ( x ) in ( int , Fraction ) \
-            or x in expl . get_consts ( path ) :
-                raise exception ( "Assign to constant '%s'" % x
+        for x in src [ 'assign' ] [ 'to' ] :
+            if not is_text ( x ) or reduce ( and_ ,
+                [ y [ 0 ] not in expl . get_writables ( path )
+                    for y in expl . split_value_fields ( path , x ) + [ [ x ] ]
+                ] , True ) :
+                raise exception ( "Assign to unknown writable '%s'" % x
                                 , None , path )
-        for x in froms + tos :
-            if is_text ( x ) and not expl . split_value_fields ( path , x ) \
-            and x not in expl . get_consts ( path ) :
-                raise exception ( "Unknown identifier in assign: '%s'" % x
+        for x in src [ 'assign' ] [ 'from' ] :
+            if is_text ( x ) and reduce ( and_ ,
+                [ y [ 0 ] not in expl . get_readables ( path )
+                    for y in expl . split_value_fields ( path , x ) + [ [ x ] ]
+                ] , True ) :
+                raise exception ( "Assign from unknown readable '%s'" % x
                                 , None , path )
     return src
