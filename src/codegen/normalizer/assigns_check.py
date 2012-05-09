5c83c9a2c205fea89c7c33ec38cff20cfa9f258c
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-05-09 17:57:28 +0400

    working on readables and writables

--- src/codegen/normalizer/assigns_check.py
+++ src/codegen/normalizer/assigns_check.py
@@ -1,7 +1,7 @@
 from explorer import explorer
 from utils import rewrite , is_text
 from normalizer . exception import exception
-from operator import and_
+from operator import or_
 
 def run ( src ) :
     x = explorer ( src )
@@ -13,17 +13,17 @@ def run ( src ) :
 def _rewriter ( src , path , expl ) :
     if type ( src ) is dict and 'assign' in src :
         for x in src [ 'assign' ] [ 'to' ] :
-            if not is_text ( x ) or reduce ( and_ ,
-                [ y [ 0 ] not in expl . get_writables ( path )
+            if not is_text ( x ) or not reduce ( or_ ,
+                [ y [ 0 ] in expl . get_writables ( path )
                     for y in expl . split_value_fields ( path , x ) + [ [ x ] ]
-                ] , True ) :
+                ] , False ) :
                 raise exception ( "Assign to unknown writable '%s'" % x
                                 , None , path )
         for x in src [ 'assign' ] [ 'from' ] :
-            if is_text ( x ) and reduce ( and_ ,
-                [ y [ 0 ] not in expl . get_readables ( path )
+            if is_text ( x ) and not reduce ( or_ ,
+                [ y [ 0 ] in expl . get_readables ( path )
                     for y in expl . split_value_fields ( path , x ) + [ [ x ] ]
-                ] , True ) :
+                ] , False ) :
                 raise exception ( "Assign from unknown readable '%s'" % x
                                 , None , path )
     return src
