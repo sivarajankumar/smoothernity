ef89fe24ce96d0c20559d11805a79125f919ce9f
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-05-15 17:48:34 +0400

    working on args check

--- src/codegen/normalizer/args_check.py
+++ src/codegen/normalizer/args_check.py
@@ -1,17 +1,19 @@
 from utils import rewrite
 from normalizer . exception import exception
+from explorer import explorer
 
-def run ( what , where , src ) :
+def run ( what , src ) :
+    x = explorer ( src )
     try :
         return rewrite ( src ,
-            lambda s , p : _rewriter ( what , where , s , p ) )
+            lambda s , p : _rewriter ( s , p , what , x ) )
     except exception as e :
         raise exception ( str ( e ) , src , e . get_path ( ) )
 
-def _rewriter ( what , where , src , path ) :
+def _rewriter ( src , path , what , expl ) :
     if type ( src ) is dict and what in src :
         for arg in src [ what ] [ 1 : ] :
-            if not where ( arg , path ) :
+            if not expl . is_passable ( path , arg ) :
                 raise exception ( "Unknown %sable argument '%s'"
                     % ( what , arg ) , None , path )
     return src
