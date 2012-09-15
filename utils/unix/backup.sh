f5a3a8dd53684c2097db20eb7071812fcff8cf21
Oleg Plakhotnyuk <olegus8@gmail.com>
2012-09-14 23:57:07 -0500

    linux friendly backup

--- utils/unix/backup.sh
+++ utils/unix/backup.sh
@@ -3,4 +3,4 @@ rm $FILE
 echo Creating backup \"$FILE\"...
 tar --exclude .hg -cjf $FILE `dirname $0`/../../../repo `dirname $0`/../../../repo.wiki `dirname $0`/../../../repo.stable
 echo Done.
-md5 $FILE
+md5sum $FILE
