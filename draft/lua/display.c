e838d043669ff6a0f660a925bfdb0600e49dccd5
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-07 23:30:41 -0600

    working on lua draft

--- draft/lua/display.c
+++ draft/lua/display.c
@@ -1,5 +1,6 @@
 #include <SDL.h>
 #include <GL/gl.h>
+#include <GL/glu.h>
 #include "display.h"
 #include "tween.h"
 
@@ -17,6 +18,7 @@ int display_set_mode(int width, int height)
     int bpp;
     int flags;
     const SDL_VideoInfo *info;
+    float ratio;
 
     info = SDL_GetVideoInfo();
     if (info == 0)
@@ -38,8 +40,27 @@ int display_set_mode(int width, int height)
     g_display.clear_color_tween[1] = -1;
     g_display.clear_color_tween[2] = -1;
 
+    glShadeModel(GL_SMOOTH);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+    glEnable(GL_CULL_FACE);
+    glViewport(0, 0, width, height);
+
+    ratio = (float)width / (float)height;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    /*
+        TODO: Rewrite using proper mechanics
+    */
+
+    gluPerspective(60.0, ratio, 1.0, 1024.0);
+
     return 0;
 }
+
+static void tutorial_draw(void);
 
 void display_update(void)
 {
@@ -53,7 +74,13 @@ void display_update(void)
             color[i] = g_display.clear_color[i];
     }
     glClearColor(color[0], color[1], color[2], 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    /*
+        TODO: Rewrite using proper mechanics
+    */
+
+    tutorial_draw();
 }
 
 void display_show(void)
@@ -73,4 +100,130 @@ void display_tween_clear_color(int r, int g, int b)
     g_display.clear_color_tween[0] = r;
     g_display.clear_color_tween[1] = g;
     g_display.clear_color_tween[2] = b;
+}
+
+static void tutorial_draw(void)
+{
+    /* Our angle of rotation. */
+    static float angle = 0.0f;
+
+    static GLfloat v0[] = { -1.0f, -1.0f,  1.0f };
+    static GLfloat v1[] = {  1.0f, -1.0f,  1.0f };
+    static GLfloat v2[] = {  1.0f,  1.0f,  1.0f };
+    static GLfloat v3[] = { -1.0f,  1.0f,  1.0f };
+    static GLfloat v4[] = { -1.0f, -1.0f, -1.0f };
+    static GLfloat v5[] = {  1.0f, -1.0f, -1.0f };
+    static GLfloat v6[] = {  1.0f,  1.0f, -1.0f };
+    static GLfloat v7[] = { -1.0f,  1.0f, -1.0f };
+    static GLubyte red[]    = { 255,   0,   0, 255 };
+    static GLubyte green[]  = {   0, 255,   0, 255 };
+    static GLubyte blue[]   = {   0,   0, 255, 255 };
+    static GLubyte white[]  = { 255, 255, 255, 255 };
+    static GLubyte yellow[] = {   0, 255, 255, 255 };
+    static GLubyte black[]  = {   0,   0,   0, 255 };
+    static GLubyte orange[] = { 255, 255,   0, 255 };
+    static GLubyte purple[] = { 255,   0, 255,   0 };
+
+    /* We don't want to modify the projection matrix. */
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity( );
+
+    /* Move down the z-axis. */
+    glTranslatef( 0.0, 0.0, -5.0 );
+
+    /* Rotate. */
+    glRotatef( angle, 0.0, 1.0, 0.0 );
+
+    if ( ++angle > 360.0f ) {
+        angle = 0.0f;
+    }
+
+    /* Send our triangle data to the pipeline. */
+    glBegin( GL_TRIANGLES );
+
+    glColor4ubv( red );
+    glVertex3fv( v0 );
+    glColor4ubv( green );
+    glVertex3fv( v1 );
+    glColor4ubv( blue );
+    glVertex3fv( v2 );
+
+    glColor4ubv( red );
+    glVertex3fv( v0 );
+    glColor4ubv( blue );
+    glVertex3fv( v2 );
+    glColor4ubv( white );
+    glVertex3fv( v3 );
+
+    glColor4ubv( green );
+    glVertex3fv( v1 );
+    glColor4ubv( black );
+    glVertex3fv( v5 );
+    glColor4ubv( orange );
+    glVertex3fv( v6 );
+
+    glColor4ubv( green );
+    glVertex3fv( v1 );
+    glColor4ubv( orange );
+    glVertex3fv( v6 );
+    glColor4ubv( blue );
+    glVertex3fv( v2 );
+
+    glColor4ubv( black );
+    glVertex3fv( v5 );
+    glColor4ubv( yellow );
+    glVertex3fv( v4 );
+    glColor4ubv( purple );
+    glVertex3fv( v7 );
+
+    glColor4ubv( black );
+    glVertex3fv( v5 );
+    glColor4ubv( purple );
+    glVertex3fv( v7 );
+    glColor4ubv( orange );
+    glVertex3fv( v6 );
+
+    glColor4ubv( yellow );
+    glVertex3fv( v4 );
+    glColor4ubv( red );
+    glVertex3fv( v0 );
+    glColor4ubv( white );
+    glVertex3fv( v3 );
+
+    glColor4ubv( yellow );
+    glVertex3fv( v4 );
+    glColor4ubv( white );
+    glVertex3fv( v3 );
+    glColor4ubv( purple );
+    glVertex3fv( v7 );
+
+    glColor4ubv( white );
+    glVertex3fv( v3 );
+    glColor4ubv( blue );
+    glVertex3fv( v2 );
+    glColor4ubv( orange );
+    glVertex3fv( v6 );
+
+    glColor4ubv( white );
+    glVertex3fv( v3 );
+    glColor4ubv( orange );
+    glVertex3fv( v6 );
+    glColor4ubv( purple );
+    glVertex3fv( v7 );
+
+    glColor4ubv( green );
+    glVertex3fv( v1 );
+    glColor4ubv( red );
+    glVertex3fv( v0 );
+    glColor4ubv( yellow );
+    glVertex3fv( v4 );
+
+    glColor4ubv( green );
+    glVertex3fv( v1 );
+    glColor4ubv( yellow );
+    glVertex3fv( v4 );
+    glColor4ubv( black );
+    glVertex3fv( v5 );
+
+    glEnd( );
 }
