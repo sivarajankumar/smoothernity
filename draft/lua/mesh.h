e79339364ba5db70111900fc300681cf803c708e
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-08 00:42:55 -0600

    working on lua draft

--- draft/lua/mesh.h
+++ draft/lua/mesh.h
@@ -3,8 +3,7 @@
 int mesh_init(int len);
 void mesh_done(void);
 
-int mesh_triangle_fan(int vb, int ib, int tx);
-int mesh_triangle_strip(int vb, int ib, int tx);
-int mesh_triangle_list(int vb, int ib, int tx);
-
+int mesh_spawn(int vb, int ib, int tx, int type, int frame);
 void mesh_kill(int mesh);
+
+void mesh_draw(void);
