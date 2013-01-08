68557d5ac0c5fd7081cf4f4183adcd8c0737c64a
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-07 18:17:12 -0600

    working on lua draft

--- draft/lua/display.c
+++ draft/lua/display.c
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <GL/gl.h>
 #include "display.h"
 
 struct display_t
@@ -10,11 +11,28 @@ struct display_t
 
 static struct display_t g_display;
 
-int display_set_mode(int width, int height, int bpp)
+int display_set_mode(int width, int height)
 {
-	g_display.screen = SDL_SetVideoMode(width, height, bpp, SDL_HWSURFACE);
+    int bpp;
+    int flags;
+    const SDL_VideoInfo *info;
+
+    info = SDL_GetVideoInfo();
+    if (info == 0)
+        return 1;
+
+    bpp = info->vfmt->BitsPerPixel;
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    flags = SDL_OPENGL | SDL_FULLSCREEN;
+
+    g_display.screen = SDL_SetVideoMode(width, height, bpp, flags);
     if (g_display.screen == 0)
         return 1;
+
     g_display.width = width;
     g_display.height = height;
     return 0;
@@ -24,4 +42,9 @@ void display_get_mode(int *width, int *height)
 {
     *width = g_display.width;
     *height = g_display.height;
+}
+
+void display_show(void)
+{
+    SDL_GL_SwapBuffers();
 }
--- draft/lua/display.h
+++ draft/lua/display.h
@@ -1,4 +1,5 @@
 #pragma once
 
-int display_set_mode(int, int, int);
+int display_set_mode(int, int);
 void display_get_mode(int *, int *);
+void display_show(void);
