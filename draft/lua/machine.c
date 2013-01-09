e54b0f64af4c2497cf8bd34757150e90b5c0eb8f
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-09 16:28:30 -0600

    working on lua draft

--- draft/lua/machine.c
+++ draft/lua/machine.c
@@ -14,8 +14,80 @@ struct machine_t
     int sleep;
 };
 
-#include "machine_api_text.c"
-#include "machine_api.c"
+static int state_resume(struct machine_t *machine)
+{
+    int status;
+    status = lua_resume(machine->thread, 1);
+    if (status && status != LUA_YIELD)
+    {
+        fprintf(stderr, "Failed to resume thread: %s\n",
+                lua_tostring(machine->thread, -1));
+        return 1;
+    }
+    return 0;
+}
+
+static int api_yield(lua_State *lua)
+{
+    struct machine_t *machine;
+    if (lua_gettop(lua) == 1 && lua_islightuserdata(lua, -1))
+    {
+        machine = lua_touserdata(lua, -1);
+        lua_pop(lua, 1);
+        machine->next_state = state_resume;
+        return lua_yield(lua, 0);
+    }
+    else
+    {
+        lua_pushstring(lua, "api_yield: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+}
+
+static int api_sleep(lua_State *lua)
+{
+    struct machine_t *machine;
+    if (lua_gettop(lua) == 1 && lua_islightuserdata(lua, -1))
+    {
+        machine = lua_touserdata(lua, -1);
+        lua_pop(lua, 1);
+        machine->sleep = 1;
+        machine->next_state = state_resume;
+        return lua_yield(lua, 0);
+    }
+    else
+    {
+        lua_pushstring(lua, "api_sleep: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+}
+
+static int api_time(lua_State *lua)
+{
+    struct machine_t *machine;
+    if (lua_gettop(lua) == 1 && lua_islightuserdata(lua, -1))
+    {
+        machine = lua_touserdata(lua, -1);
+        lua_pop(lua, 1);
+        lua_pushinteger(lua, timer_passed(machine->run_timer));
+        return 1;
+    }
+    else
+    {
+        lua_pushstring(lua, "api_time: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+}
+
+void machine_init(lua_State *lua)
+{
+    lua_register(lua, "api_yield", api_yield);
+    lua_register(lua, "api_sleep", api_sleep);
+    lua_register(lua, "api_time", api_time);
+}
 
 int machine_step(struct machine_t *machine, int timeout)
 {
