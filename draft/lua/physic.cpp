82c3754b321f534fee5d482b99c8acdad869e8eb
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-12 00:19:58 -0600

    working on mpool

--- draft/lua/physic.cpp
+++ draft/lua/physic.cpp
@@ -1,22 +1,14 @@
-#include <iostream>
 #include <lua.h>
+#include <iostream>
 #include <LinearMath/btAlignedAllocator.h>
 
-struct physics_t
+extern "C"
+int physic_init(lua_State *, void *(*memalloc)(size_t), void (*memfree)(void*))
 {
-    struct mpool_t *mpool;
-};
-
-struct physics_t g_physics;
-
-extern "C" int physic_init(lua_State *, struct mpool_t *mpool)
-{
-    std::cout << "physic_init" << std::endl;
-    g_physics.mpool = mpool;
+    btAlignedAllocSetCustom(memalloc, memfree);
     return 0;
 }
 
 extern "C" void physic_done(void)
 {
-    std::cout << "physic_done" << std::endl;
 }
