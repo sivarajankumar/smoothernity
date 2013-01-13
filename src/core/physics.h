6cd281f352c00f62c1adf59517a5bb5adffd3db4
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-13 15:30:35 -0600

    working on debug draw

--- src/core/physics.h
+++ src/core/physics.h
@@ -5,4 +5,5 @@
 int physics_init(lua_State *lua, int cs_count, int rb_count);
 void physics_done(void);
 void physics_update(float dt);
+void physics_ddraw(void);
 int physics_rb_get_new_matrix(int rbi, float *matrix);
--- src/physics/physcpp.h
+++ src/physics/physcpp.h
@@ -4,6 +4,8 @@ int physcpp_init(void *(*memalloc)(size_t), void (*memfree)(void*),
                  int cs_count, int rb_count);
 void physcpp_done(void);
 void physcpp_update(float dt);
+void physcpp_ddraw(void);
+void physcpp_ddraw_set_mode(int mode);
 void physcpp_set_gravity(float *v);
 void physcpp_query(int *cs_left, int *rb_left);
 int physcpp_cs_alloc_box(int *csi, float mass, float *size);
