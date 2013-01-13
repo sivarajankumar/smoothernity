2fa3cda57edbde6010a115c6dc20c59feb08d2a8
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-12 18:09:07 -0600

    working on physics

--- src/physics/physcpp.h
+++ src/physics/physcpp.h
@@ -3,4 +3,9 @@
 int physcpp_init(void *(*memalloc)(size_t), void (*memfree)(void*),
                  int cs_count, int rb_count);
 void physcpp_done(void);
-
+void physcpp_update(float dt);
+int physcpp_cs_alloc_box(int *csi, float mass, float *size);
+int physcpp_cs_free(int csi);
+int physcpp_rb_alloc(int *rbi, int csi, float *matrix);
+int physcpp_rb_free(int rbi);
+int physcpp_rb_get_new_matrix(int rbi, float *matrix);
