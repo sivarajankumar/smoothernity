823c68e37d5ac754132e19f509e28b6714a29453
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-15 02:44:16 -0600

    working on vehicle

--- src/physics/mstate.cpp
+++ src/physics/mstate.cpp
@@ -11,11 +11,11 @@ mstate_c::~mstate_c()
 
 void mstate_c::getWorldTransform(btTransform &t) const
 {
-    t = get;
+    t = m;
 }
 
 void mstate_c::setWorldTransform(const btTransform &t)
 {
-    set = t;
+    m = t;
     was_set = 1;
 }
