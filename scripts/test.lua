377a6c17b458d57d2aadca3bbbd7fbcc73a6862c
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-14 20:57:41 -0600

    script refactoring

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,106 +1,9 @@
 quit = false
 
-API_INPUT_KEY_ESCAPE = 0
-API_INPUT_KEY_UP = 1
-API_INPUT_KEY_DOWN = 2
-API_INPUT_KEY_LEFT = 3
-API_INPUT_KEY_RIGHT = 4
-API_INPUT_KEY_PAGEUP = 5
-API_INPUT_KEY_PAGEDOWN = 6
-API_INPUT_KEY_MINUS = 7
-API_INPUT_KEY_EQUALS = 8
-API_INPUT_KEY_SPACE = 9
-API_INPUT_KEY_LSHIFT = 10
-API_INPUT_KEY_RSHIFT = 11
-API_INPUT_KEY_LALT = 12
-API_INPUT_KEY_RALT = 13
-API_INPUT_KEY_LCTRL = 14
-API_INPUT_KEY_RCTRL = 15
-API_INPUT_KEY_TAB = 16
-API_INPUT_KEY_F1 = 17
-API_INPUT_KEY_F2 = 18
-API_INPUT_KEY_F3 = 19
-API_INPUT_KEY_F4 = 20
-API_INPUT_KEY_F5 = 21
-API_INPUT_KEY_F6 = 22
-API_INPUT_KEY_F7 = 23
-API_INPUT_KEY_F8 = 24
-API_INPUT_KEY_F9 = 25
-API_INPUT_KEY_F10 = 26
-API_INPUT_KEY_F11 = 27
-API_INPUT_KEY_F12 = 28
-API_INPUT_KEY_1 = 29
-API_INPUT_KEY_2 = 30
-API_INPUT_KEY_3 = 31
-API_INPUT_KEY_4 = 32
-API_INPUT_KEY_5 = 33
-API_INPUT_KEY_6 = 34
-API_INPUT_KEY_7 = 35
-API_INPUT_KEY_8 = 36
-API_INPUT_KEY_9 = 37
-API_INPUT_KEY_0 = 38
-API_INPUT_KEY_A = 39
-API_INPUT_KEY_B = 40
-API_INPUT_KEY_C = 41
-API_INPUT_KEY_D = 42
-API_INPUT_KEY_E = 43
-API_INPUT_KEY_F = 44
-API_INPUT_KEY_G = 45
-API_INPUT_KEY_H = 46
-API_INPUT_KEY_I = 47
-API_INPUT_KEY_J = 48
-API_INPUT_KEY_K = 49
-API_INPUT_KEY_L = 50
-API_INPUT_KEY_M = 51
-API_INPUT_KEY_N = 52
-API_INPUT_KEY_O = 53
-API_INPUT_KEY_P = 54
-API_INPUT_KEY_Q = 55
-API_INPUT_KEY_R = 56
-API_INPUT_KEY_S = 57
-API_INPUT_KEY_T = 58
-API_INPUT_KEY_U = 59
-API_INPUT_KEY_V = 60
-API_INPUT_KEY_W = 61
-API_INPUT_KEY_X = 62
-API_INPUT_KEY_Y = 63
-API_INPUT_KEY_Z = 64
-
-API_VECTOR_IPL_LINEAR = 0
-API_VECTOR_IPL_SPLINE = 1
-
-API_MESH_TRIANGLE_STRIP = 0
-API_MESH_TRIANGLE_FAN = 1
-API_MESH_TRIANGLES = 2
-
-API_MATRIX_AXIS_X = 0
-API_MATRIX_AXIS_Y = 1
-API_MATRIX_AXIS_Z = 2
-
-API_PHYSICS_NO_DEBUG = 0
-API_PHYSICS_DRAW_WIREFRAME = math.pow(2, 0)
-API_PHYSICS_DRAW_AABB = math.pow(2, 1)
-API_PHYSICS_DRAW_FEATURES_TEXT = math.pow(2, 2)
-API_PHYSICS_DRAW_CONTACT_POINTS = math.pow(2, 3)
-API_PHYSICS_NO_DEACTIVATION = math.pow(2, 4)
-API_PHYSICS_NO_HELP_TEXT = math.pow(2, 5)
-API_PHYSICS_DRAW_TEXT = math.pow(2, 6)
-API_PHYSICS_PROFILE_TIMINGS = math.pow(2, 7)
-API_PHYSICS_ENABLE_SAT_COMPARISON = math.pow(2, 8)
-API_PHYSICS_DISABLE_BULLET_LCP = math.pow(2, 9)
-API_PHYSICS_ENABLE_CCD = math.pow(2, 10)
-API_PHYSICS_DRAW_CONSTRAINTS = math.pow(2, 11)
-API_PHYSICS_DRAW_CONSTRAINT_LIMITS = math.pow(2, 12)
-API_PHYSICS_FAST_WIREFRAME = math.pow(2, 13)
-API_PHYSICS_DRAW_NORMALS = math.pow(2, 14)
-
-API_TEXT_FONT_8_BY_13 = 0
-API_TEXT_FONT_9_BY_15 = 1
-API_TEXT_FONT_TIMES_ROMAN_10 = 2
-API_TEXT_FONT_TIMES_ROMAN_24 = 3
-API_TEXT_FONT_HELVETICA_10 = 4
-API_TEXT_FONT_HELVETICA_12 = 5
-API_TEXT_FONT_HELVETICA_18 = 6
+CAMERA_MOVE_SLOW = 0.05
+CAMERA_MOVE_FAST = 0.25
+CAMERA_ROTATE_SLOW = 0.01
+CAMERA_ROTATE_FAST = 0.05
 
 function configure()
     return {["mpool_sizes"] = function() return  1000, 10000, 100000, 1000000, 10000000 end,
@@ -127,151 +30,6 @@ function configure()
             ["vehicle_count"] = 10,
             ["buf_size"] = 10000,
             ["buf_count"] = 10}
-end
-
-demo = {}
-demo["ddraw_switcher_create"] = function ()
-    return {
-        ["debug"] = 0,
-        ["pressed"] = 0,
-        ["update"] = function(self)
-            if self["pressed"] == 0 then
-                if api_input_key(API_INPUT_KEY_F1) == 1 then
-                    self["pressed"] = 1
-                    if self["debug"] == 0 then
-                        self["debug"] = 1
-                        api_physics_set_ddraw(API_PHYSICS_DRAW_WIREFRAME)
-                        api_display_draw_scene(0)
-                    else
-                        self["debug"] = 0
-                        api_physics_set_ddraw(API_PHYSICS_NO_DEBUG)
-                        api_display_draw_scene(1)
-                    end
-                end
-            elseif self["pressed"] == 1 then
-                if api_input_key(API_INPUT_KEY_F1) == 0 then
-                    self["pressed"] = 0
-                end
-            end
-        end
-    }
-end
-
-demo["matrix_pos_stop"] = function(pos_xyz)
-    local m = api_matrix_alloc()
-    local pos = api_vector_alloc()
-    local scl = api_vector_alloc()
-    local rot = api_vector_alloc()
-    api_vector_const(pos, pos_xyz, 0)
-    api_vector_const(scl, 1, 1, 1, 0)
-    api_vector_const(rot, 0, 0, 0, 0)
-    api_matrix_pos_scl_rot(m, pos, scl, rot, API_MATRIX_AXIS_X, 0)
-    api_matrix_stop(m)
-    api_vector_free(pos)
-    api_vector_free(scl)
-    api_vector_free(rot)
-    return m
-end
-
-demo["matrix_rot_stop"] = function(axis, angle)
-    local m = api_matrix_alloc()
-    local pos = api_vector_alloc()
-    local scl = api_vector_alloc()
-    local rot = api_vector_alloc()
-    api_vector_const(pos, 0, 0, 0, 0)
-    api_vector_const(scl, 1, 1, 1, 0)
-    api_vector_const(rot, angle, 0, 0, 0)
-    api_matrix_pos_scl_rot(m, pos, scl, rot, axis, 0)
-    api_matrix_stop(m)
-    api_vector_free(pos)
-    api_vector_free(scl)
-    api_vector_free(rot)
-    return m
-end
-
-demo["matrix_move"] = function(m, offset)
-    local dm = matrix_pos_stop(offset)
-    api_matrix_mul_stop(m, m, dm)
-    api_matrix_free(dm)
-end
-
-demo["matrix_rotate"] = function(m, axis, angle)
-    local dm = matrix_rot_stop(axis, angle)
-    api_matrix_mul_stop(m, m, dm)
-    api_matrix_free(dm)
-end
-
-CAMERA_MOVE_SLOW = 0.05
-CAMERA_MOVE_FAST = 0.25
-CAMERA_ROTATE_SLOW = 0.01
-CAMERA_ROTATE_FAST = 0.05
-
-demo["free_camera_create"] = function(start_pos)
-    local new_cam = {
-        ["matrix"] = nil,
-        ["construct"] = function(self)
-            self["matrix"] = matrix_pos_stop(start_pos)
-        end,
-        ["destruct"] = function(self)
-            api_matrix_free(self["matrix"])
-            self["matrix"] = nil
-        end,
-        ["update"] = function(self)
-            local ofs = CAMERA_MOVE_SLOW
-            local ang = CAMERA_ROTATE_SLOW
-            if api_input_key(API_INPUT_KEY_SHIFT) == 1 then
-                ofs = CAMERA_MOVE_FAST
-                ang = CAMERA_ROTATE_FAST
-            end
-        
-            if api_input_key(API_INPUT_KEY_E) == 1 then
-                matrix_move(self["matrix"], 0, 0, -ofs)
-            end
-            if api_input_key(API_INPUT_KEY_D) == 1 then
-                matrix_move(self["matrix"], 0, 0, ofs)
-            end
-            if api_input_key(API_INPUT_KEY_S) == 1 then
-                matrix_move(self["matrix"], -ofs, 0, 0)
-            end
-            if api_input_key(API_INPUT_KEY_F) == 1 then
-                matrix_move(self["matrix"], ofs, 0, 0)
-            end
-            if api_input_key(API_INPUT_KEY_A) == 1 then
-                matrix_move(self["matrix"], 0, ofs, 0)
-            end
-            if api_input_key(API_INPUT_KEY_Z) == 1 then
-                matrix_move(self["matrix"], 0, -ofs, 0)
-            end
-            if api_input_key(API_INPUT_KEY_LEFT) == 1 then
-                matrix_rotate(self["matrix"], API_MATRIX_AXIS_Y, rot)
-            end
-            if api_input_key(API_INPUT_KEY_RIGHT) == 1 then
-                matrix_rotate(self["matrix"], API_MATRIX_AXIS_Y, -rot)
-            end
-            if api_input_key(API_INPUT_KEY_UP) == 1 then
-                matrix_rotate(self["matrix"], API_MATRIX_AXIS_X, rot)
-            end
-            if api_input_key(API_INPUT_KEY_DOWN) == 1 then
-                matrix_rotate(self["matrix"], API_MATRIX_AXIS_X, -rot)
-            end
-            if api_input_key(API_INPUT_KEY_PAGEUP) == 1 then
-                matrix_rotate(self["matrix"], API_MATRIX_AXIS_Z, rot)
-            end
-            if api_input_key(API_INPUT_KEY_PAGEDOWN) == 1 then
-                matrix_rotate(self["matrix"], API_MATRIX_AXIS_Z, -rot)
-            end
-        end
-    }
-    new_cam["construct"](new_cam)
-    return new_cam
-end
-
-demo["wait"] = function(state, us)
-    local time = api_time(state)
-    while api_time(state) - time < us
-    do
-        api_sleep(state)
-    end
 end
 
 function control(state)
@@ -576,6 +334,248 @@ function work(state)
         if api_input_key(API_INPUT_KEY_PAGEDOWN) == 1 then
             api_matrix_mul_stop(cam, cam, roll_right)
         end
+        api_sleep(state)
+    end
+end
+
+API_INPUT_KEY_ESCAPE = 0
+API_INPUT_KEY_UP = 1
+API_INPUT_KEY_DOWN = 2
+API_INPUT_KEY_LEFT = 3
+API_INPUT_KEY_RIGHT = 4
+API_INPUT_KEY_PAGEUP = 5
+API_INPUT_KEY_PAGEDOWN = 6
+API_INPUT_KEY_MINUS = 7
+API_INPUT_KEY_EQUALS = 8
+API_INPUT_KEY_SPACE = 9
+API_INPUT_KEY_LSHIFT = 10
+API_INPUT_KEY_RSHIFT = 11
+API_INPUT_KEY_LALT = 12
+API_INPUT_KEY_RALT = 13
+API_INPUT_KEY_LCTRL = 14
+API_INPUT_KEY_RCTRL = 15
+API_INPUT_KEY_TAB = 16
+API_INPUT_KEY_F1 = 17
+API_INPUT_KEY_F2 = 18
+API_INPUT_KEY_F3 = 19
+API_INPUT_KEY_F4 = 20
+API_INPUT_KEY_F5 = 21
+API_INPUT_KEY_F6 = 22
+API_INPUT_KEY_F7 = 23
+API_INPUT_KEY_F8 = 24
+API_INPUT_KEY_F9 = 25
+API_INPUT_KEY_F10 = 26
+API_INPUT_KEY_F11 = 27
+API_INPUT_KEY_F12 = 28
+API_INPUT_KEY_1 = 29
+API_INPUT_KEY_2 = 30
+API_INPUT_KEY_3 = 31
+API_INPUT_KEY_4 = 32
+API_INPUT_KEY_5 = 33
+API_INPUT_KEY_6 = 34
+API_INPUT_KEY_7 = 35
+API_INPUT_KEY_8 = 36
+API_INPUT_KEY_9 = 37
+API_INPUT_KEY_0 = 38
+API_INPUT_KEY_A = 39
+API_INPUT_KEY_B = 40
+API_INPUT_KEY_C = 41
+API_INPUT_KEY_D = 42
+API_INPUT_KEY_E = 43
+API_INPUT_KEY_F = 44
+API_INPUT_KEY_G = 45
+API_INPUT_KEY_H = 46
+API_INPUT_KEY_I = 47
+API_INPUT_KEY_J = 48
+API_INPUT_KEY_K = 49
+API_INPUT_KEY_L = 50
+API_INPUT_KEY_M = 51
+API_INPUT_KEY_N = 52
+API_INPUT_KEY_O = 53
+API_INPUT_KEY_P = 54
+API_INPUT_KEY_Q = 55
+API_INPUT_KEY_R = 56
+API_INPUT_KEY_S = 57
+API_INPUT_KEY_T = 58
+API_INPUT_KEY_U = 59
+API_INPUT_KEY_V = 60
+API_INPUT_KEY_W = 61
+API_INPUT_KEY_X = 62
+API_INPUT_KEY_Y = 63
+API_INPUT_KEY_Z = 64
+
+API_VECTOR_IPL_LINEAR = 0
+API_VECTOR_IPL_SPLINE = 1
+
+API_MESH_TRIANGLE_STRIP = 0
+API_MESH_TRIANGLE_FAN = 1
+API_MESH_TRIANGLES = 2
+
+API_MATRIX_AXIS_X = 0
+API_MATRIX_AXIS_Y = 1
+API_MATRIX_AXIS_Z = 2
+
+API_PHYSICS_NO_DEBUG = 0
+API_PHYSICS_DRAW_WIREFRAME = math.pow(2, 0)
+API_PHYSICS_DRAW_AABB = math.pow(2, 1)
+API_PHYSICS_DRAW_FEATURES_TEXT = math.pow(2, 2)
+API_PHYSICS_DRAW_CONTACT_POINTS = math.pow(2, 3)
+API_PHYSICS_NO_DEACTIVATION = math.pow(2, 4)
+API_PHYSICS_NO_HELP_TEXT = math.pow(2, 5)
+API_PHYSICS_DRAW_TEXT = math.pow(2, 6)
+API_PHYSICS_PROFILE_TIMINGS = math.pow(2, 7)
+API_PHYSICS_ENABLE_SAT_COMPARISON = math.pow(2, 8)
+API_PHYSICS_DISABLE_BULLET_LCP = math.pow(2, 9)
+API_PHYSICS_ENABLE_CCD = math.pow(2, 10)
+API_PHYSICS_DRAW_CONSTRAINTS = math.pow(2, 11)
+API_PHYSICS_DRAW_CONSTRAINT_LIMITS = math.pow(2, 12)
+API_PHYSICS_FAST_WIREFRAME = math.pow(2, 13)
+API_PHYSICS_DRAW_NORMALS = math.pow(2, 14)
+
+API_TEXT_FONT_8_BY_13 = 0
+API_TEXT_FONT_9_BY_15 = 1
+API_TEXT_FONT_TIMES_ROMAN_10 = 2
+API_TEXT_FONT_TIMES_ROMAN_24 = 3
+API_TEXT_FONT_HELVETICA_10 = 4
+API_TEXT_FONT_HELVETICA_12 = 5
+API_TEXT_FONT_HELVETICA_18 = 6
+
+demo = {}
+demo["ddraw_switcher_create"] = function ()
+    return {
+        ["debug"] = 0,
+        ["pressed"] = 0,
+        ["update"] = function(self)
+            if self["pressed"] == 0 then
+                if api_input_key(API_INPUT_KEY_F1) == 1 then
+                    self["pressed"] = 1
+                    if self["debug"] == 0 then
+                        self["debug"] = 1
+                        api_physics_set_ddraw(API_PHYSICS_DRAW_WIREFRAME)
+                        api_display_draw_scene(0)
+                    else
+                        self["debug"] = 0
+                        api_physics_set_ddraw(API_PHYSICS_NO_DEBUG)
+                        api_display_draw_scene(1)
+                    end
+                end
+            elseif self["pressed"] == 1 then
+                if api_input_key(API_INPUT_KEY_F1) == 0 then
+                    self["pressed"] = 0
+                end
+            end
+        end
+    }
+end
+
+demo["matrix_pos_stop"] = function(pos_xyz)
+    local m = api_matrix_alloc()
+    local pos = api_vector_alloc()
+    local scl = api_vector_alloc()
+    local rot = api_vector_alloc()
+    api_vector_const(pos, pos_xyz, 0)
+    api_vector_const(scl, 1, 1, 1, 0)
+    api_vector_const(rot, 0, 0, 0, 0)
+    api_matrix_pos_scl_rot(m, pos, scl, rot, API_MATRIX_AXIS_X, 0)
+    api_matrix_stop(m)
+    api_vector_free(pos)
+    api_vector_free(scl)
+    api_vector_free(rot)
+    return m
+end
+
+demo["matrix_rot_stop"] = function(axis, angle)
+    local m = api_matrix_alloc()
+    local pos = api_vector_alloc()
+    local scl = api_vector_alloc()
+    local rot = api_vector_alloc()
+    api_vector_const(pos, 0, 0, 0, 0)
+    api_vector_const(scl, 1, 1, 1, 0)
+    api_vector_const(rot, angle, 0, 0, 0)
+    api_matrix_pos_scl_rot(m, pos, scl, rot, axis, 0)
+    api_matrix_stop(m)
+    api_vector_free(pos)
+    api_vector_free(scl)
+    api_vector_free(rot)
+    return m
+end
+
+demo["matrix_move"] = function(m, offset)
+    local dm = matrix_pos_stop(offset)
+    api_matrix_mul_stop(m, m, dm)
+    api_matrix_free(dm)
+end
+
+demo["matrix_rotate"] = function(m, axis, angle)
+    local dm = matrix_rot_stop(axis, angle)
+    api_matrix_mul_stop(m, m, dm)
+    api_matrix_free(dm)
+end
+
+demo["free_camera_create"] = function(start_pos)
+    local new_cam = {
+        ["matrix"] = nil,
+        ["construct"] = function(self)
+            self["matrix"] = matrix_pos_stop(start_pos)
+        end,
+        ["destruct"] = function(self)
+            api_matrix_free(self["matrix"])
+            self["matrix"] = nil
+        end,
+        ["update"] = function(self)
+            local ofs = CAMERA_MOVE_SLOW
+            local ang = CAMERA_ROTATE_SLOW
+            if api_input_key(API_INPUT_KEY_SHIFT) == 1 then
+                ofs = CAMERA_MOVE_FAST
+                ang = CAMERA_ROTATE_FAST
+            end
+        
+            if api_input_key(API_INPUT_KEY_E) == 1 then
+                matrix_move(self["matrix"], 0, 0, -ofs)
+            end
+            if api_input_key(API_INPUT_KEY_D) == 1 then
+                matrix_move(self["matrix"], 0, 0, ofs)
+            end
+            if api_input_key(API_INPUT_KEY_S) == 1 then
+                matrix_move(self["matrix"], -ofs, 0, 0)
+            end
+            if api_input_key(API_INPUT_KEY_F) == 1 then
+                matrix_move(self["matrix"], ofs, 0, 0)
+            end
+            if api_input_key(API_INPUT_KEY_A) == 1 then
+                matrix_move(self["matrix"], 0, ofs, 0)
+            end
+            if api_input_key(API_INPUT_KEY_Z) == 1 then
+                matrix_move(self["matrix"], 0, -ofs, 0)
+            end
+            if api_input_key(API_INPUT_KEY_LEFT) == 1 then
+                matrix_rotate(self["matrix"], API_MATRIX_AXIS_Y, rot)
+            end
+            if api_input_key(API_INPUT_KEY_RIGHT) == 1 then
+                matrix_rotate(self["matrix"], API_MATRIX_AXIS_Y, -rot)
+            end
+            if api_input_key(API_INPUT_KEY_UP) == 1 then
+                matrix_rotate(self["matrix"], API_MATRIX_AXIS_X, rot)
+            end
+            if api_input_key(API_INPUT_KEY_DOWN) == 1 then
+                matrix_rotate(self["matrix"], API_MATRIX_AXIS_X, -rot)
+            end
+            if api_input_key(API_INPUT_KEY_PAGEUP) == 1 then
+                matrix_rotate(self["matrix"], API_MATRIX_AXIS_Z, rot)
+            end
+            if api_input_key(API_INPUT_KEY_PAGEDOWN) == 1 then
+                matrix_rotate(self["matrix"], API_MATRIX_AXIS_Z, -rot)
+            end
+        end
+    }
+    new_cam["construct"](new_cam)
+    return new_cam
+end
+
+demo["wait"] = function(state, us)
+    local time = api_time(state)
+    while api_time(state) - time < us
+    do
         api_sleep(state)
     end
 end
