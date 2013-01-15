d9d759650bbe4413e58bf51039a4f7f9f9eb5241
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-14 18:52:12 -0600

    working on vehicle

--- src/physics/physcpp.h
+++ src/physics/physcpp.h
@@ -16,17 +16,15 @@ int physcpp_rb_alloc(int *rbi, int csi, float *matrix,
                      float frict, float roll_frict);
 int physcpp_rb_free(int rbi);
 int physcpp_rb_fetch_tm(int rbi, float *matrix);
-/*
 int physcpp_veh_alloc(int *vehi, int csi, float *matrix,
                       float ch_frict, float ch_roll_frict,
                       float sus_stif, float sus_comp, float sus_damp,
                       float sus_trav, float sus_force, float slip_frict);
 int physcpp_veh_free(int vehi);
-int physcpp_veh_add_wheel(int &wheel, int vehi, float *pos, float *dir,
+int physcpp_veh_add_wheel(int *wheel, int vehi, float *pos, float *dir,
                           float *axl, float sus_rest, float roll,
                           float radius, int front);
 int physcpp_veh_set_wheel(int vehi, int wheel, float engine,
                           float brake, float steer);
 int physcpp_veh_fetch_chassis_tm(int vehi, float *matrix);
 int physcpp_veh_fetch_wheel_tm(int vehi, int wheel, float *matrix);
-*/
