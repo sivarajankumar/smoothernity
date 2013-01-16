222986ac1920a6b72ec8dd83c3fce2734ff9aace
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-15 19:15:42 -0600

    main.c refactoring

--- src/core/machine.h
+++ src/core/machine.h
@@ -4,8 +4,8 @@
 
 struct machine_t;
 
-void machine_init(lua_State *);
-struct machine_t * machine_create(lua_State *, const char *);
-int machine_step(struct machine_t *, int);
-int machine_running(struct machine_t *);
-void machine_destroy(struct machine_t *);
+void machine_init(lua_State*);
+struct machine_t * machine_create(lua_State*, const char*);
+int machine_step(struct machine_t*, float);
+int machine_running(struct machine_t*);
+void machine_destroy(struct machine_t*);
--- src/core/timer.h
+++ src/core/timer.h
@@ -2,5 +2,5 @@ struct timer_t;
 
 struct timer_t * timer_create(void);
 void timer_destroy(struct timer_t *);
-int timer_passed(struct timer_t *);
+float timer_passed(struct timer_t *);
 void timer_reset(struct timer_t *);
