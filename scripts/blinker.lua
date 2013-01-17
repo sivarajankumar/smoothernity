e46523a493794256f46086b96ce4c1a118d4e55a
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-17 04:04:52 -0600

    scripts refactoring

---
+++ scripts/blinker.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+function M.alloc()
+    local self = {}
+    local v = api_vector_alloc()
+    local buf = api_buf_alloc()
+    function self.free()
+        api_vector_free(v)
+        api_buf_free(buf)
+    end
+    api_buf_set(buf, 0,   0,0.05,0,1,1,   0,0,0.05,1,1)
+    api_vector_seq(v, buf, 0, 2, 1, API_VECTOR_IPL_SPLINE)
+    api_display_clear_color(v)
+    return self
+end
+
+return M
