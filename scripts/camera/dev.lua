594500cac9288190d170d34dda6d6237aafc5829
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-21 02:38:33 -0600

    working on world move

--- scripts/camera/dev.lua
+++ scripts/camera/dev.lua
@@ -27,40 +27,40 @@ function M.alloc(x, y, z)
         end
     
         if api_input_key(API_INPUT_KEY_E) == 1 then
-            util.matrix_move(matrix, 0, 0, -ofs)
+            util.matrix_move_local(matrix, 0, 0, -ofs)
         end
         if api_input_key(API_INPUT_KEY_D) == 1 then
-            util.matrix_move(matrix, 0, 0, ofs)
+            util.matrix_move_local(matrix, 0, 0, ofs)
         end
         if api_input_key(API_INPUT_KEY_S) == 1 then
-            util.matrix_move(matrix, -ofs, 0, 0)
+            util.matrix_move_local(matrix, -ofs, 0, 0)
         end
         if api_input_key(API_INPUT_KEY_F) == 1 then
-            util.matrix_move(matrix, ofs, 0, 0)
+            util.matrix_move_local(matrix, ofs, 0, 0)
         end
         if api_input_key(API_INPUT_KEY_A) == 1 then
-            util.matrix_move(matrix, 0, ofs, 0)
+            util.matrix_move_local(matrix, 0, ofs, 0)
         end
         if api_input_key(API_INPUT_KEY_Z) == 1 then
-            util.matrix_move(matrix, 0, -ofs, 0)
+            util.matrix_move_local(matrix, 0, -ofs, 0)
         end
         if api_input_key(API_INPUT_KEY_LEFT) == 1 then
-            util.matrix_rotate(matrix, API_MATRIX_AXIS_Y, ang)
+            util.matrix_rotate_local(matrix, API_MATRIX_AXIS_Y, ang)
         end
         if api_input_key(API_INPUT_KEY_RIGHT) == 1 then
-            util.matrix_rotate(matrix, API_MATRIX_AXIS_Y, -ang)
+            util.matrix_rotate_local(matrix, API_MATRIX_AXIS_Y, -ang)
         end
         if api_input_key(API_INPUT_KEY_UP) == 1 then
-            util.matrix_rotate(matrix, API_MATRIX_AXIS_X, ang)
+            util.matrix_rotate_local(matrix, API_MATRIX_AXIS_X, ang)
         end
         if api_input_key(API_INPUT_KEY_DOWN) == 1 then
-            util.matrix_rotate(matrix, API_MATRIX_AXIS_X, -ang)
+            util.matrix_rotate_local(matrix, API_MATRIX_AXIS_X, -ang)
         end
         if api_input_key(API_INPUT_KEY_PAGEUP) == 1 then
-            util.matrix_rotate(matrix, API_MATRIX_AXIS_Z, ang)
+            util.matrix_rotate_local(matrix, API_MATRIX_AXIS_Z, ang)
         end
         if api_input_key(API_INPUT_KEY_PAGEDOWN) == 1 then
-            util.matrix_rotate(matrix, API_MATRIX_AXIS_Z, -ang)
+            util.matrix_rotate_local(matrix, API_MATRIX_AXIS_Z, -ang)
         end
     end
 
