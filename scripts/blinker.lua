51127b46d9ee78f7abcbfea680d252326c610fb0
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-23 20:56:03 -0600

    more noticable blinker

--- scripts/blinker.lua
+++ scripts/blinker.lua
@@ -8,7 +8,7 @@ function M.alloc()
     function self.free()
         api_buf_free(buf)
     end
-    api_buf_set(buf, 0,   0,0.05,0,1,1,   0,0,0.05,1,1)
+    api_buf_set(buf, 0,   0,0.1,0,1,1,   0,0,0.1,1,1)
     api_vector_seq(render.visual.vclrcol, buf, 0, 2, 1, API_VECTOR_IPL_SPLINE)
     return self
 end
