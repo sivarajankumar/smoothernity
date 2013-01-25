75f53fe711beab7f5bb5c57c7d7a10508bb19809
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-24 18:52:20 -0600

    working on physical worlds

--- src/physics/world.cpp
+++ src/physics/world.cpp
@@ -120,3 +120,87 @@ void world_done(void)
     free(g_worlds.pool);
     g_worlds.pool = 0;
 }
+
+int world_update(float dt)
+{
+    world_t *wld;
+    for (wld = g_worlds.active; wld; wld = wld->next)
+    {
+        if (wld->time_scale == 0.0f)
+            continue;
+        try {
+            wld->world->stepSimulation(dt * wld->time_scale);
+        } catch (...) {
+            return PHYSRES_INTERNAL;
+        }
+    }
+    return PHYSRES_OK;
+}
+
+int world_alloc(int *worldi)
+{
+    world_t *wld;
+    if (g_worlds.vacant == 0)
+        return PHYSRES_OUT_OF_WLD;
+
+    ++g_worlds.allocs;
+    --g_worlds.left;
+    if (g_worlds.left < g_worlds.left_min)
+        g_worlds.left_min = g_worlds.left;
+
+    wld = g_worlds.vacant;
+    g_worlds.vacant = g_worlds.vacant->next;
+
+    wld->prev = 0;
+    wld->next = g_worlds.active;
+    if (g_worlds.active)
+        g_worlds.active->prev = wld;
+    g_worlds.active = wld;
+
+    wld->time_scale = 1;
+    wld->vacant = 0;
+
+    *worldi = wld - g_worlds.pool;
+
+    return PHYSRES_OK;
+}
+
+int world_free(world_t *wld)
+{
+    if (wld->vacant == 1)
+        return PHYSRES_INVALID_WLD;
+    try {
+        if (wld->world->getNumCollisionObjects() > 0)
+            return PHYSRES_INVALID_WLD;
+    } catch (...) {
+        return PHYSRES_INTERNAL;
+    }
+
+    ++g_worlds.frees;
+    ++g_worlds.left;
+
+    if (wld->prev)
+        wld->prev->next = wld->next;
+    if (wld->next)
+        wld->next->prev = wld->prev;
+    if (wld == g_worlds.active)
+        g_worlds.active = wld->next;
+
+    wld->prev = 0;
+    wld->next = g_worlds.vacant;
+    if (g_worlds.vacant)
+        g_worlds.vacant->prev = wld;
+    g_worlds.vacant = wld;
+
+    wld->vacant = 1;
+
+    return PHYSRES_OK;
+}
+
+world_t * world_get(int worldi)
+{
+    if (worldi >= 0 && worldi < g_worlds.count)
+        return g_worlds.pool + worldi;
+    else
+        return 0;
+}
