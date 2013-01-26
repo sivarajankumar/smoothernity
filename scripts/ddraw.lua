4b32cdc93c3176e5926644365c2ee702991c6824
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-26 15:42:28 -0600

    eagle eye mode

--- scripts/ddraw.lua
+++ scripts/ddraw.lua
@@ -3,19 +3,26 @@ local M = {}
 local render = require 'render'
 local pwld = require 'physwld'
 
-local debug = 0
+local MODE_VISUAL = 0
+local MODE_EAGLE = 1
+local MODE_DEBUG = 2
+
+local mode = MODE_VISUAL
 local pressed = 0
 
 function M.update()
     if pressed == 0 then
         if api_input_key(API_INPUT_KEY_F1) == 1 then
             pressed = 1
-            if debug == 0 then
-                debug = 1
+            if mode == MODE_VISUAL then
+                mode = MODE_EAGLE
+                render.eagle.engage()
+            elseif mode == MODE_EAGLE then
+                mode = MODE_DEBUG
                 render.debug.engage()
                 api_physics_wld_ddraw_mode(pwld.wld, API_PHYSICS_DRAW_WIREFRAME + API_PHYSICS_DRAW_AABB)
-            else
-                debug = 0
+            elseif mode == MODE_DEBUG then
+                mode = MODE_VISUAL
                 render.visual.engage()
             end
         end
