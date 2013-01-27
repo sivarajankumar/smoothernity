f9b534d33e3a805a512a31447d510014bd9611af
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-26 18:36:16 -0600

    working on world planes

--- scripts/plane.lua
+++ scripts/plane.lua
@@ -1,40 +1,13 @@
 local M = {}
 
-local land = require 'land' 
-local noise = require 'noise'
-local pwld = require 'physwld'
-local cfg = require 'config'
-local meshes = require 'meshes'
-
-local SCENE = 50
-local SIZE = cfg.VIS_RANGE
-local RES = 30
-
-local function move_alloc()
+function M.alloc(noise, move, group, landalloc, vis_range, size, res, centx, centy, centz)
     local self = {}
-    self.x, self.y, self.z = 0, 0, 0
-    return self
-end
 
-function M.alloc(x, y, z)
-    local self = {}
     local lands = {}
-
-    local nse = noise.alloc()
-    local centx, centy, centz = x, y, z
-    local move = move_alloc()
-    local size = SIZE
-    local res = RES
-    local vplayer = api_vector_alloc()
-    local frames = 0
-    local text
-    local bound_front, bound_back, bound_left, bound_right
+    local bound_front, bound_back = 0, 1
+    local bound_left, bound_right = 0, 1
 
     function self.free()
-        if text ~= nil then
-            api_text_free(text)
-        end
-        api_vector_free(vplayer)
         for z, xs in pairs(lands) do
             for x, lnd in pairs(xs) do
                 lnd.free()
@@ -54,43 +27,23 @@ function M.alloc(x, y, z)
         return centx + x * size, centy, centz + z * size
     end
 
-    function self.attach(mplayer)
-        api_vector_mpos(vplayer, mplayer)
-        if bound_back == nil or bound_front == nil or bound_left == nil or bound_right == nil then
-            api_vector_update(vplayer)
-            local x, y, z = world_to_grid(scene_to_world(api_vector_get(vplayer)))
-            bound_back = z + 1
-            bound_front = z
-            bound_left = x
-            bound_right = x + 1
-        end
-    end
-
     local function add_land(mach, z, x)
         if lands[z] == nil then
             lands[z] = {}
         end
         if lands[z][x] == nil then
             local wx, wy, wz = grid_to_world(x, 0, z)
-            lands[z][x] = land.phys_alloc(mach, nse, move, meshes.GROUP_NEAR,
-                                          size, res, wx, wy, wz)
+            lands[z][x] = landalloc(mach, noise, move, group,
+                                    size, res, wx, wy, wz)
         end
     end
 
-    function self.generate(mach)
+    function self.generate(mach, px, py, pz)
         local gx, gy, gz
 
         -- align
         do
-            api_vector_update(vplayer)
-            local px, py, pz, pw = api_vector_get(vplayer)
-            if text ~= nil then
-                api_text_free(text)
-            end
             gx, gy, gz = world_to_grid(scene_to_world(px, py, pz))
-            text = api_text_alloc(string.format('(%i, %i, %i) (%i, %i, %i)',
-                                                px, py, pz, gx, gy, gz),
-                                  API_TEXT_FONT_8_BY_13, 20, 40)
             while gx < bound_left do
                 bound_left = bound_left - 1
                 bound_right = bound_right - 1
@@ -150,38 +103,11 @@ function M.alloc(x, y, z)
         end
     end
 
-    function self.move(car, camc)
-        api_vector_update(vplayer)
-        local x, y, z, w = api_vector_get(vplayer)
-        local dx, dz = 0, 0
-        while x + dx < -SCENE do
-            dx = dx + SCENE
-        end
-        while x + dx > SCENE do
-            dx = dx - SCENE
-        end
-        while z + dz < -SCENE do
-            dz = dz + SCENE
-        end
-        while z + dz > SCENE do
-            dz = dz - SCENE
-        end
-        if dx ~= 0 or dz ~= 0 then
-            local dv = api_vector_alloc()
-            api_vector_const(dv, dx, 0, dz, 0)
-            api_physics_wld_move(pwld.wld, dv)
-            car.move(dv)
-            camc.move(dv)
-            api_vector_free(dv)
-
-            for z, xs in pairs(lands) do
-                for x, lnd in pairs(xs) do
-                    lnd.move()
-                end
+    function self.move()
+        for z, xs in pairs(lands) do
+            for x, lnd in pairs(xs) do
+                lnd.move()
             end
-
-            move.x = move.x + dx
-            move.z = move.z + dz
         end
     end
 
@@ -191,7 +117,7 @@ function M.alloc(x, y, z)
         for z, xs in pairs(lands) do
             for x, lnd in pairs(xs) do
                 local lx, ly, lz = grid_to_world(x, 0, z)
-                if math.max(math.abs(px-lx), math.abs(pz-lz)) <= cfg.VIS_RANGE then
+                if math.max(math.abs(px-lx), math.abs(pz-lz)) <= vis_range then
                     lnd.show()
                 else
                     lnd.hide()
