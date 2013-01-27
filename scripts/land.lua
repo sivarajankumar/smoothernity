2eed249b9005dc5e33f0953787ddbb130ed64fb2
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-26 23:39:15 -0600

    working on lods

--- scripts/land.lua
+++ scripts/land.lua
@@ -3,27 +3,14 @@ local M = {}
 local util = require 'util'
 local pwld = require 'physwld'
 local cfg = require 'config'
+local lod = require 'lod'
+local meshes = require 'meshes'
 
-function M.world_height(noise, lod, z, x)
-    local n = 0
-    if lod <= 0 then
-        n = n + 0.975*noise.get(z * 0.001, x * 0.001)
-    elseif lod <= 1 then
-        n = n + 0.015
-        n = n + 0.975*noise.get(z * 0.001, x * 0.001)
-        n = n + 0.004*noise.get(z * 0.01, x * 0.01)
-    else
-        n = n + 0.020
-        n = n + 0.975*noise.get(z * 0.001, x * 0.001)
-        n = n + 0.004*noise.get(z * 0.01, x * 0.01)
-        n = n + 0.001*noise.get(z * 0.05, x * 0.05)
-    end
-    return n
-end
-
-local function common_alloc(mach, noise, move, group, lod, size, res, basx, basy, basz)
+local function common_alloc(mach, noise, move, lodi, basx, basy, basz)
     local self = {}
 
+    self.size = lod.lods[lodi].size
+    self.res = lod.lods[lodi].res
     self.mmesh = api_matrix_alloc()
     self.hmap = {}
     local vb = api_vbuf_alloc()
@@ -48,37 +35,26 @@ local function common_alloc(mach, noise, move, group, lod, size, res, basx, basy
 
     function self.show()
         if mesh == nil then
-            mesh = api_mesh_alloc(group, API_MESH_TRIANGLES, vb, ib, -1,
-                                  self.mmesh, 0, 6 * (res - 1) * (res - 1))
+            mesh = api_mesh_alloc(meshes.lod_group(lodi), API_MESH_TRIANGLES, vb, ib, -1,
+                                  self.mmesh, 0, 6 * (self.res - 1) * (self.res - 1))
         end
     end
 
     local function to_world(z, x)
-        return basz + (z * size / (res-1)),
-               basx + (x * size / (res-1))
+        return basz + (z * self.size / (self.res-1)),
+               basx + (x * self.size / (self.res-1))
     end
 
     local function color_noise(z, x, ofsz, ofsx)
         local wz, wx = to_world(z, x)
         wz = wz + ofsz
         wx = wx + ofsx
-        local n = 0
-        if lod <= 0 then
-            n = noise.get(wz * 0.002, wx * 0.002)
-        elseif lod <= 1 then
-            n = n + 0.8*noise.get(wz * 0.002, wx * 0.002)
-            n = n + 0.2*noise.get(wz * 0.020, wx * 0.020)
-        else
-            n = n + 0.6*noise.get(wz * 0.002, wx * 0.002)
-            n = n + 0.2*noise.get(wz * 0.020, wx * 0.020)
-            n = n + 0.2*noise.get(wz * 0.500, wx * 0.500)
-        end
-        return n
+        return lod.lods[lodi].colorfunc(noise, wz, wx)
     end
 
     local function height_noise(z, x)
         local wz, wx = to_world(z, x)
-        return M.world_height(noise, lod, wz, wx)
+        return lod.lods[lodi].heightfunc(noise, wz, wx)
     end
 
     local function color(z, x)
@@ -98,9 +74,9 @@ local function common_alloc(mach, noise, move, group, lod, size, res, basx, basy
 
     -- height map
     do
-        for z = 0, res - 1 do
+        for z = 0, self.res - 1 do
             self.hmap[z] = {}
-            for x = 0, res - 1 do
+            for x = 0, self.res - 1 do
                 self.hmap[z][x] = util.lerp(height_noise(z, x), 0, 1,
                                             -0.5*cfg.LAND_HEIGHT, 0.5*cfg.LAND_HEIGHT)
                 api_machine_yield(mach)
@@ -110,13 +86,13 @@ local function common_alloc(mach, noise, move, group, lod, size, res, basx, basy
 
     -- vertex buffer
     do
-        for z = 0, res - 1 do
-            for x = 0, res - 1 do
+        for z = 0, self.res - 1 do
+            for x = 0, self.res - 1 do
                 local r, g, b, a = color(z, x)
-                api_vbuf_set(vb, x + z * res,
-                             x - 0.5 * (res - 1),
+                api_vbuf_set(vb, x + z * self.res,
+                             x - 0.5 * (self.res - 1),
                              self.hmap[z][x],
-                             z - 0.5 * (res - 1),
+                             z - 0.5 * (self.res - 1),
                              r, g, b, a,
                              0, 0)
                 api_machine_yield(mach)
@@ -127,13 +103,13 @@ local function common_alloc(mach, noise, move, group, lod, size, res, basx, basy
 
     -- index buffer
     do
-        for z = 0, res - 2 do
-            for x = 0, res - 2 do
-                local i00 = x + z * res
-                local i01 = x + (z + 1) * res
-                local i10 = (x + 1) + z * res
-                local i11 = (x + 1) + (z + 1) * res
-                local i = (x + z * (res - 1)) * 6
+        for z = 0, self.res - 2 do
+            for x = 0, self.res - 2 do
+                local i00 = x + z * self.res
+                local i01 = x + (z + 1) * self.res
+                local i10 = (x + 1) + z * self.res
+                local i11 = (x + 1) + (z + 1) * self.res
+                local i = (x + z * (self.res - 1)) * 6
                 api_ibuf_set(ib, i,  i00,i01,i10,  i10,i01,i11)
                 api_machine_yield(mach)
             end
@@ -144,11 +120,11 @@ local function common_alloc(mach, noise, move, group, lod, size, res, basx, basy
     return self
 end
 
-function M.phys_alloc(mach, noise, move, group, lod, size, res, basx, basy, basz)
+function M.phys_alloc(mach, noise, move, lodi, basx, basy, basz)
     local self = {}
 
-    local common = common_alloc(mach, noise, move, group, lod, size, res, basx, basy, basz)
-    local scale = size / (res - 1)
+    local common = common_alloc(mach, noise, move, lodi, basx, basy, basz)
+    local scale = common.size / (common.res - 1)
     local buf = api_buf_alloc()
     local mvis = util.matrix_scl_stop(scale, 1, scale)
     local mrb = api_matrix_alloc()
@@ -178,16 +154,16 @@ function M.phys_alloc(mach, noise, move, group, lod, size, res, basx, basy, basz
     do
         local vsize = api_vector_alloc()
         api_vector_const(vsize, scale, 1, scale, 0)
-        for z = 0, res - 1 do
-            for x = 0, res - 1 do
-                api_buf_set(buf, x + z * res, common.hmap[z][x])
+        for z = 0, common.res - 1 do
+            for x = 0, common.res - 1 do
+                api_buf_set(buf, x + z * common.res, common.hmap[z][x])
                 api_machine_yield(mach)
             end
         end
-        local mpos = util.matrix_pos_stop(basx + move.x + 0.5*size,
+        local mpos = util.matrix_pos_stop(basx + move.x + 0.5*common.size,
                                           basy + move.y,
-                                          basz + move.z + 0.5*size)
-        cs = api_physics_cs_alloc_hmap(buf, 0, res, res,
+                                          basz + move.z + 0.5*common.size)
+        cs = api_physics_cs_alloc_hmap(buf, 0, common.res, common.res,
                                        -0.5 * cfg.LAND_HEIGHT, 0.5 * cfg.LAND_HEIGHT, vsize)
         rb = api_physics_rb_alloc(pwld.wld, cs, mpos, 0, 1, 1)
         api_vector_free(vsize)
@@ -203,9 +179,9 @@ function M.phys_alloc(mach, noise, move, group, lod, size, res, basx, basy, basz
     return self
 end
 
-function M.vis_alloc(mach, noise, move, group, lod, size, res, basx, basy, basz)
+function M.vis_alloc(mach, noise, move, lodi, basx, basy, basz)
     local self = {}
-    local common = common_alloc(mach, noise, move, group, lod, size, res, basx, basy, basz)
+    local common = common_alloc(mach, noise, move, lodi, basx, basy, basz)
 
     function self.free()
         common.free()
@@ -220,10 +196,10 @@ function M.vis_alloc(mach, noise, move, group, lod, size, res, basx, basy, basz)
     end
 
     function self.move()
-        local scale = size / (res - 1)
-        local m = util.matrix_pos_scl_stop(basx + move.x + 0.5*size,
+        local scale = common.size / (common.res - 1)
+        local m = util.matrix_pos_scl_stop(basx + move.x + 0.5*common.size,
                                            basy + move.y,
-                                           basz + move.z + 0.5*size,
+                                           basz + move.z + 0.5*common.size,
                                            scale, 1, scale)
         api_matrix_copy(common.mmesh, m)
         api_matrix_free(m)
