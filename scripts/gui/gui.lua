8fb4bba1f7e62330333241318c9190b7a5a5a023
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-27 17:04:22 -0600

    working on existence bar

--- scripts/gui/gui.lua
+++ scripts/gui/gui.lua
@@ -3,10 +3,13 @@ local M = {}
 local bar = require 'gui.bar'
 local util = require 'util'
 
-local genbar
+local genbar, exbar
 
 function M.gen_progress(value)
     genbar.set(value)
+end
+
+function M.existence(value)
 end
 
 function M.init()
@@ -15,14 +18,23 @@ function M.init()
     -- generation progress bar
     do
         local sx, sy = util.camera_dims()
+        local sizex, sizey = 0.5, 0.05
         local posx, posy = -sx + 0.1, -sy + 0.1
-        local sizex, sizey = 0.5, 0.1
         genbar = bar.alloc(posx, posy, posx + sizex, posy + sizey)
+    end
+
+    -- existence bar
+    do
+        local sx, sy = util.camera_dims()
+        local sizex, sizey = 0.5, 0.05
+        local posx, posy = -sx + 0.1, -sy + 0.1 + sizey + 0.05
+        exbar = bar.alloc(posx, posy, posx + sizex, posy + sizey)
     end
 end
 
 function M.done()
     genbar.free()
+    exbar.free()
     bar.done()
 end
 
