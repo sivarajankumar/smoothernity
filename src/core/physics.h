21e7c29dd37fc44ce31da8174fac0c6d57b2ed4c
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-01-27 20:40:45 -0600

    working on casting

--- src/core/physics.h
+++ src/core/physics.h
@@ -7,6 +7,7 @@ int physics_init(lua_State *lua, int wld_count, int cs_count,
 void physics_done(void);
 int physics_update(float dt);
 int physics_wld_ddraw(int wldi);
+int physics_wld_cast(int wldi, int csi, float *mfrom, float *mto, float *vout);
 int physics_rb_fetch_tm(int rbi, float *matrix);
 int physics_veh_fetch_chassis_tm(int vehi, float *matrix);
 int physics_veh_fetch_wheel_tm(int vehi, int wheel, float *matrix);
