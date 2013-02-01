c38d7e9d00eb6fcef80eb799e461e18de9842cce
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-02-01 03:19:57 -0600

    all logic goes to game

--- scripts/game.lua
+++ scripts/game.lua
@@ -13,41 +13,124 @@ local render = require 'render'
 local pwld = require 'physwld'
 local pause = require 'pause'
 local gui = require 'gui.gui'
+local quit = require 'quit'
+local ddraw = require 'ddraw'
+local perf = require 'perf'
+local key = require 'key'
 
-local blink, wld, cbs, car, camc, camd, camsw
-local pressed = 0
-
+local LOGIC_TIME = 0.015
+local GC_STEP = 10
 local START_X = 0
 local START_Y = 0
 local START_Z = 0
 
-local function start_pos()
+local function start_pos(wld)
     local wx, wy, wz = wld.scene_to_world(START_X, 0, START_Z)
     wy = wld.height(wz, wx) + 10
     return wld.world_to_scene(wx, wy, wz)
 end
 
-function M.init()
+local function run_co(co, start_time, max_time)
+    while coroutine.status(co) ~= 'dead' do
+        local res, arg = coroutine.resume(co)
+        if res and arg then
+            break
+        elseif not res then
+            io.write('Coroutine\n', debug.traceback(co), '\n')
+            error(arg)
+        end
+        if api_timer() - start_time > max_time then
+            break
+        end
+    end
+end
+
+local function tick(f)
+    local t = api_timer()
+    f()
+    return api_timer() - t
+end 
+
+function M.run()
     pwld.init()
     render.init()
     gui.init()
     util.set_gravity(0, -10, 0)
-    blink = blinker.alloc()
-    wld = world.alloc(START_X, START_Y, START_Z)
 
-    local sx, sy, sz = start_pos()
-    cbs = cubes.alloc(sx, sy, sz - 5)
-    car = vehicle.alloc(sx, sy, sz + 5)
-    camc = camcord.alloc(sx, sy, sz + 10)
-    camd = camdev.alloc(sx, sy, sz)
+    local prf = perf.alloc()
+    local frame_time = api_timer()
+    local blink = blinker.alloc()
+    local wld = world.alloc(START_X, START_Y, START_Z)
+    local sx, sy, sz = start_pos(wld)
+    local cbs = cubes.alloc(sx, sy, sz - 5)
+    local car = vehicle.alloc(sx, sy, sz + 5)
+    local camc = camcord.alloc(sx, sy, sz + 10)
+    local camd = camdev.alloc(sx, sy, sz)
+    local camsw = camswitch.alloc(camc, camd)
+    local keyctl = key.alloc(function() return API_INPUT_KEY_F10 end,
+        function()
+            local sx, sy, sz = start_pos(wld)
+            cbs.free()
+            cbs = cubes.alloc(sx, sy, sz - 5)
+            car.free()
+            car = vehicle.alloc(sx, sy, sz + 5)
+            camc.attach(car.mchassis)
+            wld.attach(car.mchassis)
+        end, function() end)
 
-    camsw = camswitch.alloc(camc, camd)
     camc.attach(car.mchassis)
     wld.attach(car.mchassis)
     render.visual.engage()
-end
 
-function M.done()
+    local control = coroutine.create(
+        function()
+            while not quit.requested()
+            do
+                quit.control()
+                ddraw.update()
+                keyctl.update()
+                car.update()
+                camd.update()
+                camsw.update()
+                wld.move(car, camc)
+                wld.showhide()
+                pause.control()
+                gui.gen_progress(wld.gen_progress())
+
+                local edist = wld.edge_dist()
+                gui.edge_dist(edist)
+                car.restrain(edist)
+                coroutine.yield(true)
+            end
+        end)
+
+    local work = coroutine.create(
+        function()
+            while not quit.requested()
+            do
+                wld.generate()
+                coroutine.yield(true)
+            end
+        end)
+
+    while coroutine.status(control) ~= 'dead'
+       or coroutine.status(work) ~= 'dead'
+    do
+        local logic_time = api_timer()
+        prf.sample('physics', tick(function() api_physics_update(cfg.FRAME_TIME) end))
+        prf.sample('input', tick(function() api_input_update() end))
+        prf.sample('gc', tick(function() api_main_gc_step(GC_STEP) end))
+        prf.sample('control', tick(function() run_co(control, logic_time, 0) end))
+        prf.sample('work', tick(function() run_co(work, logic_time, LOGIC_TIME) end))
+        prf.sample('rupdate', tick(function() render.update() end))
+        prf.sample('rdraw', tick(function() render.draw() end))
+        prf.sample('frame', api_timer() - frame_time)
+        prf.update()
+        gui.frame_time(api_timer() - frame_time)
+        frame_time = api_timer()
+    end
+
+    prf.free()
     blink.free()
     wld.free()
     cbs.free()
@@ -57,40 +140,6 @@ function M.done()
     gui.done()
     render.done()
     pwld.done()
-end
-
-function M.control()
-    if pressed == 0 then
-        if api_input_key(API_INPUT_KEY_F10) == 1 then
-            pressed = 1
-            local sx, sy, sz = start_pos()
-            cbs.free()
-            cbs = cubes.alloc(sx, sy, sz - 5)
-            car.free()
-            car = vehicle.alloc(sx, sy, sz + 5)
-            camc.attach(car.mchassis)
-            wld.attach(car.mchassis)
-        end
-    elseif pressed == 1 then
-        if api_input_key(API_INPUT_KEY_F10) == 0 then
-            pressed = 0
-        end
-    end
-    car.update()
-    camd.update()
-    camsw.update()
-    wld.move(car, camc)
-    wld.showhide()
-    pause.control()
-    gui.gen_progress(wld.gen_progress())
-
-    local edist = wld.edge_dist()
-    gui.edge_dist(edist)
-    car.restrain(edist)
-end
-
-function M.work()
-    wld.generate()
 end
 
 return M
