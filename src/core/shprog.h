30a8eff3494af628f69af2b94263d2b05de33447
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-02-08 17:30:43 -0600

    working on shaders

---
+++ src/core/shprog.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <lua.h>
+
+int shprog_init(lua_State *lua, int count);
+void shprog_done(void);
---
+++ src/core/shuni.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <lua.h>
+
+int shuni_init(lua_State *lua, int count);
+void shuni_done(void);
