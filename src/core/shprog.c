277a3b11b0ded0a71b0a7c5e51be016c47d617ec
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-02-08 21:46:13 -0600

    working on shaders

--- src/core/shprog.c
+++ src/core/shprog.c
@@ -1,4 +1,9 @@
 #include "shprog.h"
+#include "../util/util.h"
+#include <string.h>
+#include <stdio.h>
+
+static const size_t SHPROG_SIZE = 32;
 
 struct shprogs_t
 {
@@ -13,8 +18,150 @@ struct shprogs_t
 
 static struct shprogs_t g_shprogs;
 
+struct shprog_t * shprog_get(int ishprog)
+{
+    if (ishprog >= 0 && ishprog < g_shprogs.count)
+        return (struct shprog_t*)(g_shprogs.pool + SHPROG_SIZE * ishprog);
+    else
+        return 0;
+}
+
+static int api_shprog_left(lua_State *lua)
+{
+    if (lua_gettop(lua) != 0)
+    {
+        lua_pushstring(lua, "api_shprog_left: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+    lua_pushinteger(lua, g_shprogs.left);
+    return 1;
+}
+
+static int api_shprog_alloc(lua_State *lua)
+{
+    struct shprog_t *shprog;
+    if (lua_gettop(lua) != 0)
+    {
+        lua_pushstring(lua, "api_shprog_alloc: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+    if (g_shprogs.vacant == 0)
+    {
+        lua_pushstring(lua, "api_shprog_alloc: out of shprogs");
+        lua_error(lua);
+        return 0;
+    }
+
+    ++g_shprogs.allocs;
+    --g_shprogs.left;
+    if (g_shprogs.left < g_shprogs.left_min)
+        g_shprogs.left_min = g_shprogs.left;
+
+    shprog = g_shprogs.vacant;
+    g_shprogs.vacant = g_shprogs.vacant->next;
+    shprog->next = 0;
+    shprog->state = SHPROG_CREATED;
+
+    lua_pushinteger(lua, ((char*)shprog - g_shprogs.pool) / SHPROG_SIZE);
+    return 1;
+}
+
+static int api_shprog_free(lua_State *lua)
+{
+    struct shprog_t *shprog;
+    if (lua_gettop(lua) != 1 || !lua_isnumber(lua, 1))
+    {
+        lua_pushstring(lua, "api_shprog_free: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+    shprog = shprog_get(lua_tointeger(lua, 1));
+    lua_pop(lua, 1);
+    if (shprog == 0 || shprog->state == SHPROG_VACANT)
+    {
+        lua_pushstring(lua, "api_shprog_free: invalid shprog");
+        lua_error(lua);
+        return 0;
+    }
+
+    ++g_shprogs.frees;
+    ++g_shprogs.left;
+
+    shprog->state = SHPROG_VACANT;
+    shprog->next = g_shprogs.vacant;
+    g_shprogs.vacant = shprog;
+    return 0;
+}
+
+static int api_shprog_attach(lua_State *lua)
+{
+    struct shprog_t *shprog;
+    if (lua_gettop(lua) != 1 || !lua_isnumber(lua, 1))
+    {
+        lua_pushstring(lua, "api_shprog_attach: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+    shprog = shprog_get(lua_tointeger(lua, 1));
+    lua_pop(lua, 1);
+    if (shprog == 0 || shprog->state != SHPROG_CREATED)
+    {
+        lua_pushstring(lua, "api_shprog_attach: invalid shprog");
+        lua_error(lua);
+        return 0;
+    }
+    return 0;
+}
+
+static int api_shprog_link(lua_State *lua)
+{
+    struct shprog_t *shprog;
+    if (lua_gettop(lua) != 1 || !lua_isnumber(lua, 1))
+    {
+        lua_pushstring(lua, "api_shprog_link: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+    shprog = shprog_get(lua_tointeger(lua, 1));
+    lua_pop(lua, 1);
+    if (shprog == 0 || shprog->state != SHPROG_CREATED)
+    {
+        lua_pushstring(lua, "api_shprog_link: invalid shprog");
+        lua_error(lua);
+        return 0;
+    }
+    shprog->state = SHPROG_LINKED;
+    return 0;
+}
+
 int shprog_init(lua_State *lua, int count)
 {
+    int i;
+    struct shprog_t *shprog;
+    if (sizeof(struct shprog_t) > SHPROG_SIZE)
+    {
+        fprintf(stderr, "Invalid size:\nsizeof(struct shprog_t) == %i\n",
+                (int)sizeof(struct shprog_t));
+        return 1;
+    }
+    g_shprogs.pool = util_malloc(SHPROG_SIZE, SHPROG_SIZE * count);
+    if (g_shprogs.pool == 0)
+        return 1;
+    memset(g_shprogs.pool, 0, SHPROG_SIZE * count);
+    g_shprogs.count = count;
+    g_shprogs.left = count;
+    g_shprogs.left_min = count;
+    g_shprogs.vacant = shprog_get(0);
+    for (i = 0; i < count; ++i)
+    {
+        shprog = shprog_get(i);
+        if (i < count)
+            shprog->next = shprog_get(i + 1);
+        shprog->state = SHPROG_VACANT;
+    }
+    lua_register(lua, "api_shprog_left", api_shprog_left);
     lua_register(lua, "api_shprog_alloc", api_shprog_alloc);
     lua_register(lua, "api_shprog_free", api_shprog_free);
     lua_register(lua, "api_shprog_attach", api_shprog_attach); 
@@ -24,12 +171,17 @@ int shprog_init(lua_State *lua, int count)
 
 void shprog_done(void)
 {
-}
-
-struct shprog_t * shprog_get(int)
-{
+    if (g_shprogs.pool == 0)
+        return;
+    printf("Shader programs usage: %i/%i, allocs/frees: %i/%i\n",
+           g_shprogs.count - g_shprogs.left_min, g_shprogs.count,
+           g_shprogs.allocs, g_shprogs.frees);
+    util_free(g_shprogs.pool);
+    g_shprogs.pool = 0;
 }
 
 void shprog_select(struct shprog_t *shprog)
 {
+    if (shprog->state == SHPROG_LINKED)
+        printf("TODO: do select");
 }
--- src/core/shprog.h
+++ src/core/shprog.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <lua.h>
-#include "GL/gl.h"
 
 enum shprog_e
 {
@@ -12,7 +11,6 @@ enum shprog_e
 
 struct shprog_t
 {
-    GLuint prog_id;
     enum shprog_e state;
     struct shprog_t *next;
     struct shuni_t *shunis;
