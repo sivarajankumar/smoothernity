c5be51330de6e7bb8ccf78ef60e949801e69e38d
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-02-11 17:51:02 -0600

    working on textures

--- src/core/tex.c
+++ src/core/tex.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static const size_t TEX_SIZE = 8;
+static const size_t TEX_SIZE = 16;
 
 struct tex_t
 {
@@ -33,6 +33,82 @@ static struct tex_t * tex_get(int texi)
         return 0;
 }
 
+static int api_tex_left(lua_State *lua)
+{
+    if (lua_gettop(lua) != 0)
+    {
+        lua_pushstring(lua, "api_tex_left: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+    lua_pushinteger(lua, g_texs.left);
+    return 1;
+}
+
+static int api_tex_alloc(lua_State *lua)
+{
+    struct tex_t *tex;
+    if (lua_gettop(lua) != 0)
+    {
+        lua_pushstring(lua, "api_tex_alloc: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+    if (g_texs.vacant == 0)
+    {
+        lua_pushstring(lua, "api_tex_alloc: out of textures");
+        lua_error(lua);
+        return 0;
+    }
+
+    ++g_texs.allocs;
+    --g_texs.left;
+    if (g_texs.left < g_texs.left_min)
+        g_texs.left_min = g_texs.left;
+
+    tex = g_texs.vacant;
+    g_texs.vacant = g_texs.vacant->next;
+    tex->next = 0;
+    tex->vacant = 0;
+
+    lua_pushinteger(lua, ((char*)tex - g_texs.pool) / TEX_SIZE);    
+    return 1;
+}
+
+static int api_tex_free(lua_State *lua)
+{
+    struct tex_t *tex;
+    if (lua_gettop(lua) != 1 || !lua_isnumber(lua, 1))
+    {
+        lua_pushstring(lua, "api_tex_free: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+    tex = tex_get(lua_tointeger(lua, 1));
+    lua_pop(lua, 1);
+    if (tex == 0 || tex->vacant == 1)
+    {
+        lua_pushstring(lua, "api_tex_free: invalid texture");
+        lua_error(lua);
+        return 0;
+    }
+    
+    ++g_texs.frees;
+    ++g_texs.left;
+
+    tex->vacant = 1;
+    tex->next = g_texs.vacant;
+    g_texs.vacant = tex;
+    return 0;
+}
+
+static int api_tex_set(lua_State *lua)
+{
+    /* TODO */
+    lua_error(lua);
+    return 0;
+}
+
 int tex_init(lua_State *lua, int size, int count)
 {
     struct tex_t *tex;
@@ -47,10 +123,10 @@ int tex_init(lua_State *lua, int size, int count)
                 size);
         return 1;
     }
-    g_tex.pool = util_malloc(TEX_SIZE, TEX_SIZE * count);
-    if (g_tex.pool == 0)
+    g_texs.pool = util_malloc(TEX_SIZE, TEX_SIZE * count);
+    if (g_texs.pool == 0)
         return 1;
-    memset(g_tex.pool, 0, TEX_SIZE * count);
+    memset(g_texs.pool, 0, TEX_SIZE * count);
     g_texs.size = size;
     g_texs.count = count;
     g_texs.left = count;
@@ -66,8 +142,16 @@ int tex_init(lua_State *lua, int size, int count)
     lua_register(lua, "api_tex_alloc", api_tex_alloc);
     lua_register(lua, "api_tex_free", api_tex_free);
     lua_register(lua, "api_tex_set", api_tex_set);
+    return 0;
 }
 
 void tex_done(void)
 {
+    if (g_texs.pool == 0)
+        return;
+    printf("Textures usage: %i/%i, allocs/frees: %i/%i\n",
+           g_texs.count - g_texs.left_min, g_texs.count,
+           g_texs.allocs, g_texs.frees);
+    util_free(g_texs.pool);
+    g_texs.pool = 0;
 }
