a101b039927518dd07a8e63a1d70cf9c43be6011
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-02-12 23:56:54 -0600

    working on profiler

--- scripts/gui/prof.lua
+++ scripts/gui/prof.lua
@@ -49,21 +49,25 @@ function M.alloc(xmin, ymin, xmax, ymax, refvalue, ...)
         end
         local limit = math.max(refvalue, maxsum)
         barref.set(refvalue, limit)
-        barmax.set(augment(limit, ...))
-        barmin.set(augment(limit, ...))
+        barmax.set(augment(limit, unpack(maxvals)))
+        barmin.set(augment(limit, unpack(minvals)))
         barcur.set(augment(limit, ...))
     end
 
     do
         local b = BORDER * math.min(xmax - xmin, ymax - ymin)
         local h = (ymax-ymin - b*4) / 3
+        local cols = {...}
+        table.insert(cols, COLOR_BACK)
         barback = bar.alloc(xmin, ymin, xmax, ymax, COLOR_BACK)
-        barref = bar.alloc(xmin+b, ymin+b, xmax-b, ymax-b, COLOR_REF, COLOR_BACK)
-        barmax = bar.alloc(xmin+b, ymin + b*1 + h*0, xmax-b, ymin + b*1 + h*1, ...)
-        barcur = bar.alloc(xmin+b, ymin + b*2 + h*1, xmax-b, ymin + b*2 + h*2, ...)
-        barmin = bar.alloc(xmin+b, ymin + b*3 + h*2, xmax-b, ymin + b*3 + h*3, ...)
+        barref = bar.alloc(xmin+b, ymin+0.5*b, xmax-b, ymax-0.5*b, COLOR_REF, COLOR_BACK)
+        barmax = bar.alloc(xmin+b, ymin + b*1 + h*0, xmax-b, ymin + b*1 + h*1, unpack(cols))
+        barcur = bar.alloc(xmin+b, ymin + b*2 + h*1, xmax-b, ymin + b*2 + h*2, unpack(cols))
+        barmin = bar.alloc(xmin+b, ymin + b*3 + h*2, xmax-b, ymin + b*3 + h*3, unpack(cols))
         barback.set(1)
     end
+
+    return self
 end
 
 return M
