778de92e59b2a0ea8e1e20681b657276c00651c9
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-02-12 20:14:55 -0600

    working on queries

--- src/core/query.c
+++ src/core/query.c
@@ -6,10 +6,17 @@
 
 static const size_t QUERY_SIZE = 32;
 
+enum query_state_e
+{
+    QUERY_STATE_VACANT,
+    QUERY_STATE_WAITING,
+    QUERY_STATE_READY
+};
+
 struct query_t
 {
     GLuint query_id;
-    int vacant;
+    enum query_state_e state;
     struct query_t *next;
 };
 
@@ -46,18 +53,18 @@ static int api_query_left(lua_State *lua)
     return 1;
 }
 
-static int api_query_alloc(lua_State *lua)
+static int api_query_alloc_time(lua_State *lua)
 {
     struct query_t *query;
     if (lua_gettop(lua) != 0)
     {
-        lua_pushstring(lua, "api_query_alloc: incorrect argument");
+        lua_pushstring(lua, "api_query_alloc_time: incorrect argument");
         lua_error(lua);
         return 0;
     }
     if (g_queries.vacant == 0)
     {
-        lua_pushstring(lua, "api_query_alloc: out of queries");
+        lua_pushstring(lua, "api_query_alloc_time: out of queries");
         lua_error(lua);
         return 0;
     }
@@ -70,7 +77,8 @@ static int api_query_alloc(lua_State *lua)
     query = g_queries.vacant;
     g_queries.vacant = g_queries.vacant->next;
     query->next = 0;
-    query->vacant = 0;
+    query->state = QUERY_STATE_WAITING;
+    glQueryCounter(query->query_id, GL_TIMESTAMP);
     lua_pushinteger(lua, ((char*)query - g_queries.pool) / QUERY_SIZE);
     return 1;
 }
@@ -86,7 +94,7 @@ static int api_query_free(lua_State *lua)
     }
     query = query_get(lua_tointeger(lua, 1));
     lua_pop(lua, 1);
-    if (query == 0 || query->vacant == 1)
+    if (query == 0 || query->state != QUERY_STATE_READY)
     {
         lua_pushstring(lua, "api_query_free: invalid query");
         lua_error(lua);
@@ -94,7 +102,7 @@ static int api_query_free(lua_State *lua)
     }
     ++g_queries.frees;
     ++g_queries.left;
-    query->vacant = 1;
+    query->state = QUERY_STATE_VACANT;
     query->next = g_queries.vacant;
     g_queries.vacant = query;
     return 0;
@@ -102,6 +110,7 @@ static int api_query_free(lua_State *lua)
 
 static int api_query_ready(lua_State *lua)
 {
+    GLint ready;
     struct query_t *query;
     if (lua_gettop(lua) != 1 || !lua_isnumber(lua, 1))
     {
@@ -111,13 +120,39 @@ static int api_query_ready(lua_State *lua)
     }
     query = query_get(lua_tointeger(lua, 1));
     lua_pop(lua, 1);
-    if (query == 0 || query->vacant == 1)
+    if (query == 0 || query->state == QUERY_STATE_VACANT)
     {
         lua_pushstring(lua, "api_query_ready: invalid query");
         lua_error(lua);
         return 0;
     }
-    lua_pushinteger(lua, 0);
+    glGetQueryObjectiv(query->query_id, GL_QUERY_RESULT_AVAILABLE, &ready);
+    if (ready == GL_TRUE)
+        query->state = QUERY_STATE_READY;
+    lua_pushinteger(lua, query->state == QUERY_STATE_READY);
+    return 1;
+}
+
+static int api_query_result(lua_State *lua)
+{
+    GLuint64 result;
+    struct query_t *query;
+    if (lua_gettop(lua) != 1 || !lua_isnumber(lua, 1))
+    {
+        lua_pushstring(lua, "api_query_ready: incorrect argument");
+        lua_error(lua);
+        return 0;
+    }
+    query = query_get(lua_tointeger(lua, 1));
+    lua_pop(lua, 1);
+    if (query == 0 || query->state != QUERY_STATE_READY)
+    {
+        lua_pushstring(lua, "api_query_ready: invalid query");
+        lua_error(lua);
+        return 0;
+    }
+    glGetQueryObjectui64v(query->query_id, GL_QUERY_RESULT, &result);
+    lua_pushnumber(lua, result);
     return 1;
 }
 
@@ -143,13 +178,14 @@ int query_init(lua_State *lua, int count)
     {
         query = query_get(i);
         query->next = query_get(i + 1);
-        query->vacant = 1;
+        query->state = QUERY_STATE_VACANT;
         glGenQueries(1, &query->query_id);
     }
     lua_register(lua, "api_query_left", api_query_left);
-    lua_register(lua, "api_query_alloc", api_query_alloc);
+    lua_register(lua, "api_query_alloc_time", api_query_alloc_time);
     lua_register(lua, "api_query_free", api_query_free);
     lua_register(lua, "api_query_ready", api_query_ready);
+    lua_register(lua, "api_query_result", api_query_result);
     return 0;
 }
 
