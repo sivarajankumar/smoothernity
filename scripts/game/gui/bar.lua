6e7770568a7d51c203fb0d01bf2caa7584cf6b0d
olegus8 <olegus8@localhost>
2013-03-08 00:52:21 +0000

    shader.shader -> game.shader

--- scripts/game/gui/bar.lua
+++ scripts/game/gui/bar.lua
@@ -2,7 +2,7 @@ local M = {}
 
 local util = require 'core.util'
 local meshes = require 'meshes'
-local shader = require 'shader.shader'
+local shader = require 'game.shader'
 local twinibuf = require 'twin.ibuf'
 local twinvbuf = require 'twin.vbuf'
 local twinmesh = require 'twin.mesh'
