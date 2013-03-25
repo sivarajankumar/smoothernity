909fa094a260efbad3d06058e70368f625fa2886
olegus8 <olegus8@localhost>
2013-03-25 03:08:17 +0000

    one-to-one pbufs

--- src/core/pbuf.h
+++ src/core/pbuf.h
@@ -15,6 +15,7 @@ enum pbuf_e
 struct pbuf_t
 {
     GLuint buf_id;
+    int size;
     int mapped_ofs;
     int mapped_len;
     GLvoid *mapped;
@@ -26,17 +27,7 @@ struct pbuf_data_t
     GLubyte color[4];
 };
 
-struct pbufs_t
-{
-    int size;
-    int count;
-    char *pool;
-    struct thread_mutex_t *mutex;
-};
-
-extern struct pbufs_t g_pbufs;
-
-int pbuf_init(lua_State *lua, int size, int count);
+int pbuf_init(lua_State *lua, int *sizes, int count);
 void pbuf_done(void);
 void pbuf_reg_thread(lua_State *lua);
 struct pbuf_t * pbuf_get(int);
