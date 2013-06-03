abb58c74d49897e6afa9d0e3f4b3f56b5cf0f7c4
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-06-02 19:24:16 -0500

    style

--- src/core/timer.c
+++ src/core/timer.c
@@ -2,10 +2,8 @@
 #include "../platform/timer.h"
 #include "SDL.h"
 
-static int api_timer(lua_State *lua)
-{
-    if (lua_gettop(lua))
-    {
+static int api_timer(lua_State *lua) {
+    if (lua_gettop(lua)) {
         lua_pushstring(lua, "api_timer: incorrect argument");
         lua_error(lua);
         return 0;
@@ -14,10 +12,8 @@ static int api_timer(lua_State *lua)
     return 1;
 }
 
-static int api_timer_delay(lua_State *lua)
-{
-    if (lua_gettop(lua) != 1 || !lua_isnumber(lua, 1))
-    {
+static int api_timer_delay(lua_State *lua) {
+    if (lua_gettop(lua) != 1 || !lua_isnumber(lua, 1)) {
         lua_pushstring(lua, "api_timer_delay: incorrect argument");
         lua_error(lua);
         return 0;
@@ -27,14 +23,12 @@ static int api_timer_delay(lua_State *lua)
     return 0;
 }
 
-void timer_reg_thread(lua_State *lua)
-{
+void timer_reg_thread(lua_State *lua) {
     lua_register(lua, "api_timer", api_timer);
     lua_register(lua, "api_timer_delay", api_timer_delay);
 }
 
-int timer_init(lua_State *lua)
-{
+int timer_init(lua_State *lua) {
     timer_reg_thread(lua);
     return pfm_timer_init();
 }
