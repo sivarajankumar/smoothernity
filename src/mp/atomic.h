9057ecd7a5a1d974f23350d5b219186387d22c3f
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-06-06 01:31:38 -0500

    atomics

---
+++ src/mp/atomic.h
@@ -0,0 +1,13 @@
+#ifndef MP_ATOMIC_H
+#define MP_ATOMIC_H
+
+struct atomic_int_t;
+
+struct atomic_int_t * atomic_int_create(void);
+void atomic_int_destroy(struct atomic_int_t*);
+int atomic_int_load(struct atomic_int_t*);
+void atomic_int_store(struct atomic_int_t*, int);
+void atomic_int_add(struct atomic_int_t*, int);
+void atomic_int_sub(struct atomic_int_t*, int);
+
+#endif /* MP_ATOMIC.H */
