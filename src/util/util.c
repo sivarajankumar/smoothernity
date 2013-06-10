dda488da5dab8f590490b6b9f638cc833cd95fe8
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-06-10 17:28:35 -0500

    no null

--- src/util/util.c
+++ src/util/util.c
@@ -8,8 +8,7 @@ static_assert(sizeof(size_t) == sizeof(ptrdiff_t),
 void * util_malloc(size_t align, size_t size) {
     void *mem, **ptr;
     size_t addr;
-    if (align & (align - 1) ||
-    (mem = malloc(size + align + sizeof(void*))) == NULL)
+    if (align & (align - 1) || !(mem = malloc(size + align + sizeof(void*))))
         return 0;
     addr = (char*)mem - (char*)0;
     addr = (addr + align + sizeof(void*)) & ~(align - 1);
