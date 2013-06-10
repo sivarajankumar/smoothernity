60bd1f733c9c429a1d2015447b57914badacc39f
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-06-09 21:47:33 -0500

    cosmetics

--- src/util/util.c
+++ src/util/util.c
@@ -4,10 +4,8 @@ void * util_malloc(size_t align, size_t size) {
     void *mem;
     void **ptr;
     size_t addr;
-    if (align & (align - 1))
-        return 0;
-    mem = malloc(size + align + sizeof(void*));
-    if (mem == NULL)
+    if (align & (align - 1) ||
+    (mem = malloc(size + align + sizeof(void*))) == NULL)
         return 0;
     addr = (char*)mem - (char*)0;
     addr = (addr + align + sizeof(void*)) & ~(align - 1);
