cdc71a15c595ec03ce2d263bde9a690a8a71e7c7
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-06-15 21:54:58 -0500

    core.run w/o io.write

--- scripts/core/run.lua
+++ scripts/core/run.lua
@@ -1,12 +1,13 @@
 local M = {}
 
+local log = require 'core.log'
+
 function M.run(mod, func)
     local errmsg
-    io.output(io.stderr)
     xpcall(require(mod)[func],
         function(msg)
-            io.write(string.format('\n"%s.%s" execution failed%s\n',
-                                   mod, func, debug.traceback('', 2)))
+            log.err('"%s.%s" execution failed%s',
+                    mod, func, debug.traceback('', 2))
             errmsg = msg
         end)
     if errmsg ~= nil then
