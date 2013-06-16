14da3da7834cc976336020179fd3cdeb1b0ab8f2
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-06-15 21:28:38 -0500

    lua log wrapper

---
+++ scripts/core/log.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+function log(level, ...)
+    local d = debug.getinfo(3)
+    api_log_out(level, d.short_src, d.currentline, string.format(...))
+end
+
+function M.error(...)
+    log(API_LOG_ERROR, ...)
+end
+
+function M.info(...)
+    log(API_LOG_INFO, ...)
+end
+
+return M
