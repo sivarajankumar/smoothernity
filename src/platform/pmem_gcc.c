880e1e7293b3f2c1d44a326554e34cbd68a1429c
Oleg Plakhotnyuk <olegus8@gmail.com>
2013-06-25 18:39:01 -0500

    mem alloc size check

--- src/platform/pmem_gcc.c
+++ src/platform/pmem_gcc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void * pmem_alloc(size_t align, size_t size) {
-    if (align & (align - 1) || align > _Alignof(max_align_t))
+    if (align & (align - 1) || align > _Alignof(max_align_t) || size % align)
         return 0;
     return aligned_alloc(align, size);
 }
