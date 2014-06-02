c54618731f3b829dddc2e0a59489a4e7badd49ca
Oleg Plakhotnyuk <olegus8@gmail.com>
2014-06-02 11:22:28 -0500

    sandbox draft

--- draft/lua/sandbox.lua
+++ draft/lua/sandbox.lua
@@ -1,30 +1,22 @@
 local env = {myrequire = require}
 local index = 0
 
-local run = function(code)
-    index = index + 1
-    local name = string.format('untrusted_string%i.lua', index)
-    if name then
-        f = io.open(name, 'w')
-        f:write(code)
-        f:close()
-    end
-    code = string.format("myrequire 'luacov'\n%s", code)
-    print(name)
-    print(code)
-    local fn, msg = load(code, name, 't', env)
+local runfile = function(name)
+    local fn, msg = loadfile(name, 't', env)
     if not fn then
-        return nil, message
+        return nil, msg
     end
     return pcall(fn)
 end
 
-local runfile = function(name)
-    local fn, msg = loadfile(name, 't', env)
-    if not fn then
-        return nil, message
-    end
-    return pcall(fn)
+local run = function(code)
+    code = string.format("myrequire 'luacov'\n%s", code)
+    index = index + 1
+    local name = string.format('untrusted_string%i.lua', index)
+    f = io.open(name, 'w')
+    f:write(code)
+    f:close()
+    return runfile(name)
 end
 
 -- test
@@ -36,4 +28,3 @@ assert(run [[
 x = 1
 y = 2
 ]])
-assert(runfile('untrusted_file.lua'))
